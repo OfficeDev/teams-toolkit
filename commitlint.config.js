f5d8aec58a0b73be767ecb9cf25e4bc357a944c9
zyxiaoyuer <zhangyu930102@163.com>
2021-03-17 10:30:46 +0800

    feat: enable eslint for mono repo

---
+++ commitlint.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    "type-enum": [2, "always", ["chore", "docs", "feat", "fix", "refactor", "style", "test"]],
+    "scope-empty": [2, "never"]
+  };
+  

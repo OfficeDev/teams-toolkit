e7ef35a653d99850ad0ea005d64d1a16adfb1e4c
Qinen Zhu <qinzhu@microsoft.com>
2021-03-29 11:13:37 +0800

    feat: add eslint rules for typescript project

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,1 +1,21 @@
-module.exports = {}
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    extends: [
+        "plugin:@typescript-eslint/recommended"
+    ],
+    plugins: [
+        "@typescript-eslint/eslint-plugin"
+    ],
+    rules: {
+        quotes: ["error", "double", { "allowTemplateLiterals": true }],
+        semi: ['error', 'always'],
+    },
+};

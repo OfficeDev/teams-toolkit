6242a09963d8f69013f76506a943f06b7a0db3f7
Qianhao Dong <qidon@microsoft.com>
2021-03-30 15:31:36 +0800

    test: add commonlib tests

---
+++ packages/api/tests/log.test.ts
@@ -0,0 +1,89 @@
+import { assert } from "chai";
+import { LogProvider, LogLevel } from "../src/utils/log";
+
+class TestLogProvider implements LogProvider {
+    async trace({}: string): Promise<boolean> {
+        return true;
+    }
+    async debug({}: string): Promise<boolean> {
+        return true;
+    }
+    async info({}: string): Promise<boolean> {
+        return true;
+    }
+    async warning({}: string): Promise<boolean> {
+        return true;
+    }
+    async error({}: string): Promise<boolean> {
+        return true;
+    }
+    async fatal({}: string): Promise<boolean> {
+        return true;
+    }
+    async log({}: LogLevel, {}: string): Promise<boolean> {
+        return true;
+    }
+}
+
+class TestLogProvider2 implements LogProvider {
+    async trace({}: string): Promise<boolean> {
+        return false;
+    }
+    async debug({}: string): Promise<boolean> {
+        return false;
+    }
+    async info({}: string): Promise<boolean> {
+        return false;
+    }
+    async warning({}: string): Promise<boolean> {
+        return false;
+    }
+    async error({}: string): Promise<boolean> {
+        return false;
+    }
+    async fatal({}: string): Promise<boolean> {
+        return false;
+    }
+    async log({}: LogLevel, {}: string): Promise<boolean> {
+        return false;
+    }
+}
+
+describe("log", function () {
+    describe("logProvider", function () {
+        it("happy path", async () => {
+            const logProvider = new TestLogProvider();
+            const logResult = await logProvider.log(LogLevel.Debug, "123");
+            assert.equal(true, logResult);
+            const traceResult = await logProvider.trace("123");
+            assert.equal(true, traceResult);
+            const debugResult = await logProvider.debug("123");
+            assert.equal(true, debugResult);
+            const infoResult = await logProvider.info("123");
+            assert.equal(true, infoResult);
+            const warningResult = await logProvider.warning("123");
+            assert.equal(true, warningResult);
+            const errorResult = await logProvider.error("123");
+            assert.equal(true, errorResult);
+            const criticalResult = await logProvider.fatal("123");
+            assert.equal(true, criticalResult);
+        }),
+        it("sad path", async () => {
+            const logProvider = new TestLogProvider2();
+            const logResult = await logProvider.log(LogLevel.Debug, "123");
+            assert.equal(false, logResult);
+            const traceResult = await logProvider.trace("123");
+            assert.equal(false, traceResult);
+            const debugResult = await logProvider.debug("123");
+            assert.equal(false, debugResult);
+            const infoResult = await logProvider.info("123");
+            assert.equal(false, infoResult);
+            const warningResult = await logProvider.warning("123");
+            assert.equal(false, warningResult);
+            const errorResult = await logProvider.error("123");
+            assert.equal(false, errorResult);
+            const criticalResult = await logProvider.fatal("123");
+            assert.equal(false, criticalResult);
+        });
+    });
+});
---
+++ packages/api/tests/telemetry.test.ts
@@ -0,0 +1,90 @@
+import chai from "chai";
+import spies from "chai-spies";
+import { TelemetryReporter, PluginTelemetryReporter } from "../src/utils/telemetry";
+
+chai.use(spies);
+const expect = chai.expect;
+
+const pluginName = "The way that can be told of is not an unvarying way";
+const sandbox = chai.spy.sandbox();
+
+class MockTelemetryReporter implements TelemetryReporter {
+    sendTelemetryErrorEvent({}: string, {}: { [p: string]: string }, {}: { [p: string]: number }, {}: string[]): void {
+        // do nothing
+    }
+
+    sendTelemetryEvent({}: string, {}: { [p: string]: string }, {}: { [p: string]: number }): void {
+        // do nothing
+    }
+
+    sendTelemetryException({}: Error, {}: { [p: string]: string }, {}: { [p: string]: number }): void {
+        // do nothing
+    }
+}
+
+describe("telemetry", () => {
+    let reporter: TelemetryReporter;
+    let proxy: TelemetryReporter;
+
+    beforeEach(() => {
+        reporter = new MockTelemetryReporter();
+        sandbox.on(reporter, ["sendTelemetryErrorEvent", "sendTelemetryEvent", "sendTelemetryException"]);
+        proxy = new PluginTelemetryReporter(reporter, pluginName);
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it("sendTelemetryEvent", () => {
+        proxy.sendTelemetryEvent("sampleEvent", {"stringProp": "some string"}, {"numericMeasure": 123});
+
+        expect(reporter.sendTelemetryEvent)
+            .to.have.been.called
+            .with(
+                pluginName + "-" + "sampleEvent",
+                {"stringProp": "some string", "pluginName": pluginName},
+                {"numericMeasure": 123}
+            );
+    });
+
+    it("sendTelemetryErrorEvent", () => {
+        proxy.sendTelemetryErrorEvent("sampleErrorEvent", {
+            "stringProp": "some string",
+            "stackProp": "some user stack trace"
+        }, {"numericMeasure": 123}, ["stackProp"]);
+
+        expect(reporter.sendTelemetryErrorEvent)
+            .to.have.been.called
+            .with(
+                pluginName + "-" + "sampleErrorEvent",
+                {
+                    "stringProp": "some string",
+                    "pluginName": pluginName,
+                    "stackProp": "some user stack trace"
+                },
+                {"numericMeasure": 123},
+                ["stackProp"]
+            );
+    });
+
+    it("sendTelemetryException", () => {
+        const error = new Error("error for test");
+        proxy.sendTelemetryException(error, {"stringProp": "some string"}, {"numericMeasure": 123});
+
+        expect(reporter.sendTelemetryException)
+            .to.have.been.called
+            .with(
+                error,
+                {"stringProp": "some string", "pluginName": pluginName},
+                {"numericMeasure": 123}
+            );
+    });
+
+    it("invoke times", () => {
+        proxy.sendTelemetryEvent("sampleEvent", {"stringProp": "some string"}, {"numericMeasure": 123});
+
+        expect(reporter.sendTelemetryEvent)
+            .to.have.been.called.once;
+    });
+});

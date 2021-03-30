118d9a428d8239194b3063bda973f2337f253c78
huajiezhang <huajiezhang@microsoft.com>
2021-03-30 09:38:31 +0800

    fix: ignore

--- packages/api/build/plugin.js
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=plugin.js.map

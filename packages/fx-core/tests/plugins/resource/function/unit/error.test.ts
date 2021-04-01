fd7f36f3a75cbee724c214b749d6ce6bdf46e544
Heng Zhang <henzhang@microsoft.com>
2021-04-01 18:51:26 +0800

    feat(function): init function plugin

---
+++ packages/fx-core/tests/plugins/resource/function/unit/error.test.ts
@@ -0,0 +1,46 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+import "mocha";
+import { SystemError } from "teamsfx-api";
+import { UserError } from "teamsfx-api";
+import { expect } from "chai";
+
+import { ErrorType, FunctionPluginError, runWithErrorCatchAndThrow } from "../../src/resources/errors";
+import { FunctionPluginInfo } from "../../src/constants";
+
+describe(FunctionPluginInfo.pluginName, async () => {
+    describe("Error Catch Test", async () => {
+        it("Test catch UserError", async () => {
+            // Arrange
+            const errorOld = new UserError("ut-name", "ut-msg", "ut-source");
+            const errorNew = new FunctionPluginError(ErrorType.System, "ut-code", "ut-msg", []);
+            // Act
+            const res = runWithErrorCatchAndThrow(errorNew, () => new Promise((_, reject) => reject(errorOld)));
+
+            // Assert
+            expect(res).rejectedWith(errorOld);
+        });
+
+        it("Test catch SystemError", async () => {
+            // Arrange
+            const errorOld = new SystemError("ut-name", "ut-msg", "ut-source");
+            const errorNew = new FunctionPluginError(ErrorType.System, "ut-code", "ut-msg", []);
+            // Act
+            const res = runWithErrorCatchAndThrow(errorNew, () => new Promise((_, reject) => reject(errorOld)));
+
+            // Assert
+            expect(res).rejectedWith(errorOld);
+        });
+
+        it("Test catch Error", async () => {
+            // Arrange
+            const errorOld = new Error("ut-name");
+            const errorNew = new FunctionPluginError(ErrorType.System, "ut-code", "ut-msg", []);
+            // Act
+            const res = runWithErrorCatchAndThrow(errorNew, () => new Promise((_, reject) => reject(errorOld)));
+
+            // Assert
+            expect(res).rejectedWith(errorNew);
+        });
+    });
+});

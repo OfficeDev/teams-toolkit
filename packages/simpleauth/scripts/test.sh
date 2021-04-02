a59436f7fec602d9ab01b27a79d4e498138b1af8
wenytang-ms <wenyutang@microsoft.com>
2021-04-02 14:00:57 +0800

    chore: simple auth dotnet CI

---
+++ packages/simpleauth/scripts/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+DIR="$(cd `dirname $0`; pwd)"
+pushd "$DIR"
+
+# Get secret from system environment
+export TEAMS_SIMPLE_AUTH_IntegrationTestSettings__AdminClientSecret=$SimpleAuthAdminClientSecret
+export TEAMS_SIMPLE_AUTH_IntegrationTestSettings__TestPassword=$SimpleAuthPassword
+
+
+# Install chrome driver
+apt-get update && apt-get install -y chromium-chromedriver
+
+cd ..
+dotnet test TeamsFxSimpleAuth.sln
+popd
+
+exit 0

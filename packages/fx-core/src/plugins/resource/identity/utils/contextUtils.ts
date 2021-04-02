abe5d1aab91013afcda231253eb91c644a0bba2d
xzf0587 <xzf0587@gmail.com>
2021-04-02 15:27:40 +0800

    feat(identity): add identity (#33)
    * feat(identity): add identity
    
    Co-authored-by: Zhaofeng Xu <zhaofengxu@microsoft.com>

---
+++ packages/fx-core/src/plugins/resource/identity/utils/contextUtils.ts
@@ -0,0 +1,16 @@
+import {
+    PluginContext,
+} from "teamsfx-api";
+
+export class ContextUtils {
+    static ctx: PluginContext;
+
+    public static init(ctx: PluginContext) {
+        ContextUtils.ctx = ctx;
+    }
+
+    public static getConfigString(plugin: string, key: string): string {
+        const pluginConfig = ContextUtils.ctx.configOfOtherPlugins.get(plugin);
+        return pluginConfig!.get(key) as string;
+    }
+}

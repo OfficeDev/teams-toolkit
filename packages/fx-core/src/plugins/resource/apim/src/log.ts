b498326428a19a304b07c15392302280cc2305db
Xiaofu Huang <xiaofhua@microsoft.com>
2021-04-07 14:18:11 +0800

    refactor(apim): remove format change

--- packages/fx-core/src/plugins/resource/apim/src/log.ts
+++ packages/fx-core/src/plugins/resource/apim/src/log.ts
@@ -7,31 +7,31 @@ import { capitalizeFirstLetter } from "./util";
 export class LogMessages {
     public static readonly operationStarts = (operation: IName, resourceType: IName, resourceId?: string): string =>
         !resourceId
-            ? `[${ProjectConstants.pluginDisplayName}] Start to ${operation.displayName} ${resourceType.displayName}.`
-            : `[${ProjectConstants.pluginDisplayName}] Start to ${operation.displayName} ${resourceType.displayName} '${resourceId}'.`;
+            ? `[${ProjectConstants.pluginShortName}] Start to ${operation.displayName} ${resourceType.displayName}.`
+            : `[${ProjectConstants.pluginShortName}] Start to ${operation.displayName} ${resourceType.displayName} '${resourceId}'.`;
 
     public static readonly operationSuccess = (operation: IName, resourceType: IName, resourceId?: string): string =>
         !resourceId
-            ? `[${ProjectConstants.pluginDisplayName}] ${capitalizeFirstLetter(operation.displayName)} ${resourceType.displayName} successfully.`
-            : `[${ProjectConstants.pluginDisplayName}] ${capitalizeFirstLetter(operation.displayName)} ${
+            ? `[${ProjectConstants.pluginShortName}] ${capitalizeFirstLetter(operation.displayName)} ${resourceType.displayName} successfully.`
+            : `[${ProjectConstants.pluginShortName}] ${capitalizeFirstLetter(operation.displayName)} ${
                   resourceType.displayName
               } '${resourceId}' successfully.`;
 
     public static readonly operationFailed = (operation: IName, resourceType: IName, resourceId?: string): string =>
         !resourceId
-            ? `[${ProjectConstants.pluginDisplayName}] Failed to ${operation.displayName} ${resourceType.displayName}`
-            : `[${ProjectConstants.pluginDisplayName}] Failed to ${operation.displayName} ${resourceType.displayName} '${resourceId}'.`;
+            ? `[${ProjectConstants.pluginShortName}] Failed to ${operation.displayName} ${resourceType.displayName}`
+            : `[${ProjectConstants.pluginShortName}] Failed to ${operation.displayName} ${resourceType.displayName} '${resourceId}'.`;
 
     public static readonly resourceNotFound = (resourceType: IName, resourceId?: string): string =>
         !resourceId
-            ? `[${ProjectConstants.pluginDisplayName}] Could not found resource ${resourceType.displayName} in Azure.`
-            : `[${ProjectConstants.pluginDisplayName}] Could not found resource ${resourceType.displayName} '${resourceId} in Azure.`;
+            ? `[${ProjectConstants.pluginShortName}] Could not found resource ${resourceType.displayName} in Azure.`
+            : `[${ProjectConstants.pluginShortName}] Could not found resource ${resourceType.displayName} '${resourceId} in Azure.`;
 
     public static readonly accessFileFailed = (dir: string, file: string): string =>
-        `[${ProjectConstants.pluginDisplayName}] Cannot access '${file}' in directory '${dir}'`;
+        `[${ProjectConstants.pluginShortName}] Cannot access '${file}' in directory '${dir}'`;
 
     public static readonly openApiDocumentExists = (fileName: string): string =>
-        `[${ProjectConstants.pluginDisplayName}] OpenAPI document '${fileName}' already exists in the project. Skip to scaffold OpenAPI document.`;
+        `[${ProjectConstants.pluginShortName}] OpenAPI document '${fileName}' already exists in the project. Skip to scaffold OpenAPI document.`;
 
-    public static readonly useDefaultUserId = `[${ProjectConstants.pluginDisplayName}] Failed to get user information. Use default user '${ApimDefaultValues.userId}' to create API Management service.`;
+    public static readonly useDefaultUserId = `[${ProjectConstants.pluginShortName}] Failed to get user information. Use default user '${ApimDefaultValues.userId}' to create API Management service.`;
 }

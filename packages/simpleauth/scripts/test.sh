b6e92ec115f5b2e7dc672d396ca2a8769140b9d5
wenytang-ms <wenyutang@microsoft.com>
2021-04-08 10:34:13 +0800

    chore: add simple auth ci:

---
+++ packages/simpleauth/scripts/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+DIR="$(cd `dirname $0`; pwd)"
+pushd "$DIR"
+
+# Get secret from system environment
+export TEAMS_SIMPLE_AUTH_IntegrationTestSettings__AdminClientSecret=$SimpleAuthAdminClientSecret
+export TEAMS_SIMPLE_AUTH_IntegrationTestSettings__TestPassword=$SimpleAuthPassword
+
+chromium-browser --product-version
+
+# Install chrome driver
+sudo rm /var/lib/dpkg/lock && sudo rm /var/lib/apt/lists/lock
+sudo apt-get update && sudo apt-get install -y chromium-chromedriver
+
+cd ..
+dotnet test TeamsFxSimpleAuth.sln
+popd
+
+exit 0

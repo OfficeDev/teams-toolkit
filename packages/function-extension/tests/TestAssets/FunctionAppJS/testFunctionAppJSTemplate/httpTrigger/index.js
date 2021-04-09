77c003090a02ba17a84d6ffe0690b5dce7bc8d69
Chaoyi Yuan <chyuan@microsoft.com>
2021-04-09 14:34:27 +0800

    feat: update output of azure functions binding (#125)
    Update TeamsFx binding output based on latest SDK change
    Fix CI pipeline

--- packages/function-extension/tests/TestAssets/FunctionAppJS/testFunctionAppJSTemplate/httpTrigger/index.js
+++ packages/function-extension/tests/TestAssets/FunctionAppJS/testFunctionAppJSTemplate/httpTrigger/index.js
@@ -1,9 +1,9 @@
-module.exports = async function (context, req, TeamsFxConfig) {
+module.exports = async function (context, req, TeamsFxContext) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.res = {
         status: 200, /* Defaults to 200 */
-        body: JSON.stringify(TeamsFxConfig)
+        body: JSON.stringify(TeamsFxContext)
     };
     context.done();
 }

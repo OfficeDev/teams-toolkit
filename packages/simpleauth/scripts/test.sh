38d7cc27118b14280a67456c3e4b804fe48c7876
wenytang-ms <wenyutang@microsoft.com>
2021-04-09 10:35:16 +0800

    chore: fix build.sh error

--- packages/simpleauth/scripts/test.sh
+++ packages/simpleauth/scripts/test.sh
@@ -2,15 +2,12 @@
 set -e
 DIR="$(cd `dirname $0`; pwd)"
 pushd "$DIR"
-sudo rm /var/lib/dpkg/lock && sudo rm /var/lib/apt/lists/lock
 # Get secret from system environment
 export TEAMS_SIMPLE_AUTH_IntegrationTestSettings__AdminClientSecret=$SimpleAuthAdminClientSecret
 export TEAMS_SIMPLE_AUTH_IntegrationTestSettings__TestPassword=$SimpleAuthPassword
 
-chromium-browser --product-version
-
 # Install chrome driver
-sudo apt-get update && sudo apt-get install -y chromium-chromedriver
+apt-get update && apt-get install -y chromium-chromedriver
 
 cd ..
 dotnet test TeamsFxSimpleAuth.sln --filter TestCategory="P0"

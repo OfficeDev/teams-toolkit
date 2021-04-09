a29b4584af4120af73dbd6c4f97b9a53033b417c
Chaoyi Yuan <chyuan@microsoft.com>
2021-04-09 10:07:12 +0800

    feat: update test script per feature update

--- packages/function-extension/scripts/start_js_function.sh
+++ packages/function-extension/scripts/start_js_function.sh
@@ -34,8 +34,6 @@ cd $function_app_dir
 test_space_main=$function_app_dir/test_space_main
 test_space_emptyClientId=$function_app_dir/test_space_emptyClientId
 test_space_nullClientId=$function_app_dir/test_space_nullClientId
-test_space_emptyStringProperties=$function_app_dir/test_space_emptyStringProperties
-test_space_nullProperties=$function_app_dir/test_space_nullProperties
 
 test_function_app_js_template_dir=$function_app_dir/testFunctionAppJSTemplate
 test_local_settings_json_files_dir=$function_app_dir/testLocalSettingJsonFiles
@@ -54,15 +52,10 @@ echo "## Prepare test workspace"
 rm -rf $test_space_main && mkdir $test_space_main && cp -r $test_function_app_js_template_dir/* $test_space_main/
 rm -rf $test_space_emptyClientId && mkdir $test_space_emptyClientId && cp -r $test_function_app_js_template_dir/* $test_space_emptyClientId/
 rm -rf $test_space_nullClientId && mkdir $test_space_nullClientId && cp -r $test_function_app_js_template_dir/* $test_space_nullClientId/
-rm -rf $test_space_emptyStringProperties && mkdir $test_space_emptyStringProperties && cp -r $test_function_app_js_template_dir/* $test_space_emptyStringProperties/
-rm -rf $test_space_nullProperties && mkdir $test_space_nullProperties && cp -r $test_function_app_js_template_dir/* $test_space_nullProperties/
 
 sudo cp $local_settings_json_dir/main_local.settings.json $test_space_main/$local_settings_json_file_name
 sudo cp $local_settings_json_dir/emptyClientId_local.settings.json $test_space_emptyClientId/$local_settings_json_file_name
-sudo cp $local_settings_json_dir/nullClientId_local.settings.json $test_space_nulllientId/$local_settings_json_file_name
-sudo cp $local_settings_json_dir/emptyStringProperties_local.settings.json $test_space_emptyStringProperties/$local_settings_json_file_name
-sudo cp $local_settings_json_dir/nullProperties_local.settings.json $test_space_nullProperties/$local_settings_json_file_name
-
+sudo cp $local_settings_json_dir/nullClientId_local.settings.json $test_space_nullClientId/$local_settings_json_file_name
 
 echo "## For each test space: 1. Sync function extensions; 2. start function app"
 cd $test_space_main
@@ -73,17 +66,9 @@ cd $test_space_emptyClientId
 func extensions install --package Microsoft.Azure.WebJobs.Extensions.TeamsFx --version $nuget_package_version --source $local_nuget_path
 func host start --port 7072 &
 
-cd $test_space_emptyStringProperties
-func extensions install --package Microsoft.Azure.WebJobs.Extensions.TeamsFx --version $nuget_package_version --source $local_nuget_path
-func host start --port 7073 &
-
 cd $test_space_emptyClientId
 func extensions install --package Microsoft.Azure.WebJobs.Extensions.TeamsFx --version $nuget_package_version --source $local_nuget_path
 func host start --port 7074 &
-
-cd $test_space_nullProperties
-func extensions install --package Microsoft.Azure.WebJobs.Extensions.TeamsFx --version $nuget_package_version --source $local_nuget_path
-func host start --port 7075 &
 
 
 popd

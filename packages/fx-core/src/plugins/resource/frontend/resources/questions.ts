6a913ac0c131f690eb20aabc65e9e1c1179c9744
zhijie <zhihuan@microsoft.com>
2021-04-12 10:21:13 +0800

    fix: align question key and name

--- packages/fx-core/src/plugins/resource/frontend/resources/questions.ts
+++ packages/fx-core/src/plugins/resource/frontend/resources/questions.ts
@@ -5,11 +5,11 @@ import { NodeType, QTreeNode } from "fx-api";
 import { TabLanguage } from "./templateInfo";
 
 export class QuestionKey {
-    static readonly TabLanguage = "tabLanguage";
+    static readonly TabLanguage = "TabLanguage";
 }
 
 export const tabLanguageQuestion = new QTreeNode({
-    name: "TabLanguage",
+    name: QuestionKey.TabLanguage,
     description: "Select language for tab frontend project",
     type: NodeType.singleSelect,
     option: [TabLanguage.JavaScript, TabLanguage.TypeScript],

0effc4054a0886e1e542eae24124b3a4047a951d
zhijie <zhihuan@microsoft.com>
2021-04-13 14:48:49 +0800

    fix: remove language in questions

--- packages/fx-core/src/plugins/resource/frontend/resources/questions.ts
+++ packages/fx-core/src/plugins/resource/frontend/resources/questions.ts
@@ -2,7 +2,6 @@
 // Licensed under the MIT license.
 
 import { NodeType, QTreeNode } from "fx-api";
-import { TabLanguage } from "./templateInfo";
 
 export class QuestionKey {
     static readonly TabLanguage = "TabLanguage";
@@ -13,13 +12,6 @@ export class TabScope {
     static readonly PersonalTab = "personal tab";
     static readonly GroupTab = "group tab";
 }
-
-export const tabLanguageQuestion = new QTreeNode({
-    name: QuestionKey.TabLanguage,
-    description: "Select language for tab frontend project",
-    type: NodeType.singleSelect,
-    option: [TabLanguage.JavaScript, TabLanguage.TypeScript],
-});
 
 export const tabScopeQuestion = new QTreeNode({
     name: QuestionKey.TabScope,

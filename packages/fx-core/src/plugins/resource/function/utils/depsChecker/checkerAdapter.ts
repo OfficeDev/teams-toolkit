2407921981605c34673ecba68fe2666c43b200e0
Alex Wang <a1exwang@users.noreply.github.com>
2021-04-13 16:57:49 +0800

    feat(env-checker): [part-1] copy checker files and implement checkerAdapter in function plugin (#204)
    * feat(env-checker): add env checker files to function plugin
    
    * chore: replace symlink with file copy and add scripts
    
    * chore: fix string
    
    * chore: move to tools dir
    
    * chore: fix line endings for shell scripts
    
    * chore: remove dialog for now
    
    * chore: add comment to prevent edit the wrong file

---
+++ packages/fx-core/src/plugins/resource/function/utils/depsChecker/checkerAdapter.ts
@@ -0,0 +1,42 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import * as path from "path";
+import { Logger } from "../logger";
+import { openUrl } from "./common";
+
+export { cpUtils } from "./cpUtils";
+export const logger = Logger;
+
+export function dotnetCheckerEnabled(): boolean {
+  // TODO: implement me
+  return false;
+}
+
+export async function runWithProgressIndicator(
+  callback: () => Promise<void>
+): Promise<void> {
+  // TODO: implement progress indicator in plugin
+  await callback();
+}
+
+export async function displayLearnMore(message: string, link: string): Promise<boolean> {
+  // TODO: implement learn more popup in plugin
+  return true;
+}
+
+export async function displayWarningMessage(
+  message: string,
+  buttonText: string,
+  action: () => Promise<boolean>
+): Promise<boolean> {
+  return await action();
+}
+
+export function showOutputChannel(): void {
+  // TODO: find a way to implement in plugin
+}
+
+export function getResourceDir(): string {
+  return path.resolve(path.join(__dirname, "..", "..", "..", "..", "..", "..", "resource", "plugins", "resource", "function"));
+}

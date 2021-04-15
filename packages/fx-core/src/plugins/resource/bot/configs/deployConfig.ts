3575432a647ffe84b26ea0b40c7a385aff1d1db8
Ivan Jobs <ruhe@microsoft.com>
2021-04-15 11:28:38 +0800

    chore: update todo

--- packages/fx-core/src/plugins/resource/bot/configs/deployConfig.ts
+++ packages/fx-core/src/plugins/resource/bot/configs/deployConfig.ts
@@ -6,7 +6,8 @@ import { PluginBot } from "../resources/strings";
 import * as utils from "../utils/common";
 
 export class DeployConfig {
-    public unPackFlag = "true"; // ToDo: remove keytar module if keytar issue is resolved.
+    // TODO: (ruhe) remove keytar module if keytar issue is resolved
+    public unPackFlag = "true";
 
     public async restoreConfigFromContext(context: PluginContext): Promise<void> {
 

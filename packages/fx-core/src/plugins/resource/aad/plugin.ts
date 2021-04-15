d578032d23f9f5326750b1edba371a0d13764a39
KennethBWSong <bowsong@microsoft.com>
2021-04-15 11:24:35 +0800

    fix(aad): remove aad secret if create a new aad

--- packages/fx-core/src/plugins/resource/aad/plugin.ts
+++ packages/fx-core/src/plugins/resource/aad/plugin.ts
@@ -152,6 +152,7 @@ export class AadAppForTeamsImpl {
     } else {
       DialogUtils.progress?.next(ProgressDetail.ProvisionAadApp);
       await AadAppClient.createAadApp(config);
+      ctx.config.set(ConfigKeys.clientSecret, undefined);
       ctx.logProvider?.info(Messages.getLog(Messages.CreateAadAppSuccess));
     }
 

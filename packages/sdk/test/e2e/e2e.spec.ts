d39aa545121eee11936a1e11057de25128977196
Ning Tang <nintan@microsoft.com>
2021-04-16 14:27:06 +0800

    test(sdk): add e2e test

---
+++ packages/sdk/test/e2e/e2e.spec.ts
@@ -0,0 +1,46 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { assert, use as chaiUse } from "chai";
+import chaiPromises from "chai-as-promised";
+import { createNewProject, deployTab, getLoginEnvironment, getTeamsUrl, TIMEOUT } from "../helper";
+
+chaiUse(chaiPromises);
+
+describe("End to End Test in Teams", () => {
+  it("Tab app with Graph API", async function() {
+    const project = await createNewProject("sdkTabGraphE2E");
+    await deployTab(project);
+
+    const { browser, page } = await getLoginEnvironment();
+    const selectors = {
+      addButton: `ts-add-app-dialog-add-button`,
+      grantButton: `button.ui-button`,
+      accept: `input[type=submit]`,
+      objectId: `div:below(b:text("UPN:"))`
+    };
+
+    const appUrl = getTeamsUrl(project);
+    await page.goto(appUrl, { timeout: TIMEOUT });
+    await page.waitForSelector(selectors.addButton, { timeout: TIMEOUT });
+    await page.click(selectors.addButton);
+
+    const frame = await (await page.waitForSelector(`iframe`)).contentFrame();
+
+    // Check grant button
+    const grantButton = await frame.waitForSelector(selectors.grantButton);
+    await frame.click(selectors.grantButton, { delay: 2000 });
+    const consentPage = await page.waitForEvent("popup");
+    await consentPage.waitForSelector(selectors.accept, { state: "visible" });
+    await consentPage.focus(selectors.accept);
+    await consentPage.click(selectors.accept, { delay: 2000 });
+
+    // Check data from Graph API
+    const upn = await frame.waitForSelector(selectors.objectId);
+    assert.strictEqual(await upn.innerText(), "Object id: 2a61c4c3-ecf9-49eb-b717-6673fffd892d");
+
+    await browser.close();
+
+    // TODO: delete resources
+  });
+});
--- packages/sdk/test/helper.ts
+++ packages/sdk/test/helper.ts
@@ -4,14 +4,27 @@
 import { exec } from "child_process";
 import { promisify } from "util";
 import * as path from "path";
+import os from "os";
 import fs from "fs-extra";
+import { chromium, ChromiumBrowser, Page } from "playwright-chromium";
 
 const execAsync = promisify(exec);
+export const TIMEOUT = 30000;
+const username = process.env.TEST_USER_NAME;
+const password = process.env.TEST_USER_PASSWORD;
+const testProjectFolder = "testProjects";
+const subscription = "1756abc0-3554-4341-8d6a-46674962ea19";
+
+function getTestFolder(): string {
+  const folder = path.join(os.homedir(), testProjectFolder);
+  fs.ensureDirSync(folder);
+  return folder;
+}
 
 /**
  * Copy function folder to the api folder under project and deploy.
  *
- * @param projectPath - folder path of test project
+ * @param projectPath - folder path of project
  * @param functionSrcFolder - folder path of function api
  */
 export async function deployFunction(
@@ -20,6 +33,107 @@ export async function deployFunction(
 ): Promise<void> {
   fs.copySync(functionSrcFolder, path.join(projectPath, "api"), { overwrite: true });
   await callCli(`teamsfx deploy --folder ${projectPath} --deploy-plugin fx-resource-function`);
+}
+
+/**
+ * Create a new project that is provisioned.
+ *
+ * @param name - project name
+ * @returns the project folder path
+ */
+export async function createNewProject(name: string): Promise<string> {
+  const folder = getTestFolder();
+  const projectFolder = path.join(folder, name);
+  if (!(await callCli(`teamsfx new --app-name ${name} --folder ${folder} --interactive false`))) {
+    if (await fs.pathExists(projectFolder)) {
+      await fs.emptyDir(projectFolder);
+    }
+    throw new Error(`Create project ${name} failed`);
+  }
+  if (
+    !(await callCli(`teamsfx provision --folder ${projectFolder} --subscription ${subscription}`))
+  ) {
+    throw new Error(`Provision project ${name} failed`);
+  }
+  return projectFolder;
+}
+
+/**
+ * Copy tab folder to the project and deploy frontend resource.
+ *
+ * @param projectPath - folder path of project
+ * @param tabSrcFolder - tab folder path, skip copying if it's undefined
+ */
+export async function deployTab(projectPath: string, tabSrcFolder?: string): Promise<void> {
+  if (tabSrcFolder) {
+    fs.copySync(tabSrcFolder, path.join(projectPath, "tabs"), { overwrite: true });
+  }
+  await callCli(
+    `teamsfx deploy --folder ${projectPath} --deploy-plugin fx-resource-frontend-hosting`
+  );
+}
+
+/**
+ * Get URL for Teams app sideloading.
+ *
+ * @param projectPath - folder path of project
+ * @returns remote sideloading URL
+ */
+export function getTeamsUrl(projectPath: string): string {
+  const env = fs.readJsonSync(path.join(projectPath, ".fx/env.default.json"));
+  return `https://teams.microsoft.com/_#/l/app/${env.solution.remoteTeamsAppId}?installAppPackage=true`;
+}
+
+let browser: ChromiumBrowser;
+let page: Page;
+
+/**
+ * Login Teams and return the browser page for testing.
+ *
+ * @returns browser and page instance
+ */
+export async function getLoginEnvironment(): Promise<{
+  browser: ChromiumBrowser;
+  page: Page;
+}> {
+  if (!browser) {
+    await loginTestUser();
+  }
+  return { browser, page };
+}
+
+async function loginTestUser(): Promise<void> {
+  browser = await chromium.launch({ headless: false });
+  const TEAMS_URL = `https://teams.microsoft.com`;
+  const selectors = {
+    username: `input[name=loginfmt]`,
+    passwordOption: `div.optionButtonContainer:has(span#FormsAuthentication)`,
+    passwordOption2: `span#FormsAuthentication`,
+    password: `input[name=Password]`,
+    submit: `input[type=submit]`,
+    title: `h2[title="Join or create a team"]`
+  };
+
+  const context = await browser.newContext();
+  page = await context.newPage();
+  await page.goto(TEAMS_URL, { timeout: TIMEOUT });
+  await page.waitForSelector(selectors.username, { timeout: TIMEOUT });
+  await page.click(selectors.username);
+  await page.type(selectors.username, username);
+  await page.press(selectors.username, "Enter");
+  await page.waitForSelector(selectors.passwordOption2, { timeout: TIMEOUT, state: "visible" });
+  try {
+    // Click password option is not stable, try twice here
+    await page.click(selectors.passwordOption, { delay: 5000, timeout: 10000 });
+    await page.click(selectors.passwordOption2, { delay: 5000, timeout: 10000 });
+  } catch (e) {}
+  await page.waitForSelector(selectors.password, { timeout: TIMEOUT });
+  await page.click(selectors.password);
+  await page.type(selectors.password, password);
+  await page.press(selectors.password, "Enter");
+  await page.waitForSelector(selectors.submit);
+  await page.click(selectors.submit);
+  await page.waitForSelector(selectors.title, { timeout: TIMEOUT });
 }
 
 async function callCli(command: string): Promise<boolean> {

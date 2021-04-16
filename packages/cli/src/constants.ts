d782efb3cc80f58119980ca5ae4136b245d3b303
jiayi-ruan <72644308+jiayi-ruan@users.noreply.github.com>
2021-04-16 11:07:18 +0800

    feat(cli): add build & test cmd (#327)

--- packages/cli/src/constants.ts
+++ packages/cli/src/constants.ts
@@ -31,6 +31,8 @@ export const resourceConfigureAadParamPath = path.resolve(
 export const provisionParamPath = path.resolve(paramFolder, "provisionParam.json");
 export const deployParamPath = path.resolve(paramFolder, "deployParam.json");
 export const publishParamPath = path.resolve(paramFolder, "publishParam.json");
+export const buildParamPath = path.resolve(paramFolder, "buildParam.json");
+export const testParamPath = path.resolve(paramFolder, "testParam.json");
 
 export const RootFolderNode = new QTreeNode({
   type: NodeType.folder,

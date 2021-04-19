db92bfcdca0cc3b181e4ed84e922b20357a658e7
Ning Liu <71362691+nliu-ms@users.noreply.github.com>
2021-04-19 11:25:13 +0800

    fix: move dialog from solution to plugin (#360)

--- packages/fx-core/src/plugins/resource/appstudio/index.ts
+++ packages/fx-core/src/plugins/resource/appstudio/index.ts
@@ -1,7 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { ConfigFolderName, FxError, NodeType, ok, err, Platform, Plugin, PluginContext, QTreeNode, Result, Stage } from "fx-api";
+import {
+    ConfigFolderName, FxError, NodeType, ok, err, Platform, Plugin, PluginContext, QTreeNode, Result, Stage,
+    DialogMsg, DialogType, MsgLevel
+} from "fx-api";
 import { AppStudioPluginImpl } from "./plugin";
 import { Constants } from "./constants";
 import { AppStudioError } from "./errors";
@@ -51,8 +54,24 @@ export class AppStudioPlugin implements Plugin {
     public async validateManifest(ctx: PluginContext, manifestString: string): Promise<Result<string[], FxError>> {
         const validationResult = await this.appStudioPluginImpl.validateManifest(ctx, manifestString);
         if (validationResult.length > 0) {
-            return err(AppStudioResultFactory.UserError(AppStudioError.ValidationFailedError.name, AppStudioError.ValidationFailedError.message(validationResult)));
+            const errMessage = AppStudioError.ValidationFailedError.message(validationResult);
+            ctx.logProvider?.error("[Teams Toolkit] Manifest Validation failed!");
+            await ctx.dialog?.communicate(
+                new DialogMsg(DialogType.Show, {
+                    description: errMessage,
+                    level: MsgLevel.Error,
+                }),
+            );
+            return err(AppStudioResultFactory.UserError(AppStudioError.ValidationFailedError.name, errMessage));
         }
+        const validationSuccess = "[Teams Toolkit] Manifest Validation succeed!";
+        ctx.logProvider?.info(validationSuccess);
+        await ctx.dialog?.communicate(
+            new DialogMsg(DialogType.Show, {
+                description: validationSuccess,
+                level: MsgLevel.Info,
+            }),
+        );
         return ok(validationResult);
     }
 
@@ -61,9 +80,28 @@ export class AppStudioPlugin implements Plugin {
      * @param {string} appDirectory - The directory contains manifest.remote.json and two images
      * @returns {string} - Path of built appPackage.zip
      */
-    public async buildTeamsPackage(appDirectory: string, manifestString: string): Promise<Result<string, FxError>> {
-        const appPackagePath = await this.appStudioPluginImpl.buildTeamsAppPackage(appDirectory, manifestString);
-        return ok(appPackagePath);
+    public async buildTeamsPackage(ctx: PluginContext, appDirectory: string, manifestString: string): Promise<Result<string, FxError>> {
+        try {
+            const appPackagePath = await this.appStudioPluginImpl.buildTeamsAppPackage(appDirectory, manifestString);
+            const builtSuccess = `[Teams Toolkit] Teams Package ${appPackagePath} built successfully!`;
+            ctx.logProvider?.info(builtSuccess);
+            await ctx.dialog?.communicate(
+                new DialogMsg(DialogType.Show, {
+                    description: builtSuccess,
+                    level: MsgLevel.Info,
+                }),
+            );
+            return ok(appPackagePath);
+        } catch (error) {
+            ctx.logProvider?.error("[Teams Toolkit] Teams Package built failed!");
+            await ctx.dialog?.communicate(
+                new DialogMsg(DialogType.Show, {
+                    description: error.message,
+                    level: MsgLevel.Error,
+                }),
+            );
+            return err(error);
+        }
     }
 
     /**

2f70bbeadeb684134430ed937434ebfa68dd37c0
wenytang-ms <wenyutang@microsoft.com>
2021-04-20 09:42:02 +0800

    test: add getAccessToken for SDK integration test

--- packages/sdk/test/helper.ts
+++ packages/sdk/test/helper.ts
@@ -5,6 +5,7 @@ import { exec } from "child_process";
 import { promisify } from "util";
 import * as path from "path";
 import fs from "fs-extra";
+import * as msal from "@azure/msal-node";
 
 const execAsync = promisify(exec);
 
@@ -29,4 +30,37 @@ async function callCli(command: string): Promise<boolean> {
     timeout: 0
   });
   return result.stderr === "";
+}
+
+/**
+ * Get Access Token from a specific AAD app client id.
+ * @param clientId - remote or local AAD App id.
+ * @param userName - Azure account login name.
+ * @param passWord - Azure account login password.
+ * @param hostName - AAD App scope host, such as "localhost" or "****.web.core.windows.net"
+ * @param tenantId - AAD App tenant id.
+ */
+export async function getAccessToken(
+  clientId: string,
+  userName: string,
+  passWord: string,
+  hostName: string,
+  tenantId: string
+): Promise<string> {
+  const msalConfig = {
+    auth: {
+      clientId: clientId,
+      authority: `https://login.microsoftonline.com/${tenantId}`
+    }
+  };
+  // this scope is required.
+  const scopes = [`api://${hostName}/${clientId}/access_as_user`];
+  const pca = new msal.PublicClientApplication(msalConfig);
+  const usernamePasswordRequest = {
+    scopes: scopes,
+    username: userName,
+    password: passWord
+  };
+  const response = await pca.acquireTokenByUsernamePassword(usernamePasswordRequest);
+  return response!.accessToken;
 }

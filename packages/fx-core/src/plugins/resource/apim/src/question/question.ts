70c06c75a1cc5e127ca1b218cc3aad963bbcf3e5
Xiaofu Huang <xiaofhua@microsoft.com>
2021-04-21 14:50:25 +0800

    feat(apim): add cli question (#374)
    * feat(apim): add question for cli
    
    * fix(apim): enable cli question
    
    * fix(apim): change comments
    
    * refactor(apim): delete namespace to meet the lint rules
    
    * fix(apim): change question description
    
    * fix(apim): fix test case
    
    * fix(apim): move api name tips to prompt

---
+++ packages/fx-core/src/plugins/resource/apim/src/question/question.ts
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+import {
+    LogProvider,
+    OptionItem,
+    Validation,
+    PluginContext,
+    TelemetryReporter,
+    Question
+} from "fx-api";
+
+export interface IQuestionService {
+    // Control whether the question is displayed to the user.
+    condition?(parentAnswerPath: string): { target?: string; } & Validation;
+
+    // Define the method name
+    funcName?: string;
+
+    // Generate the options / default value / answer of the question.
+    executeFunc?(ctx: PluginContext): Promise<string | OptionItem | OptionItem[]>;
+
+    // Generate the question
+    getQuestion(): Question;
+}
+
+export class BaseQuestionService {
+    protected readonly logger?: LogProvider;
+    protected readonly telemetryReporter?: TelemetryReporter;
+
+    constructor(telemetryReporter?: TelemetryReporter, logger?: LogProvider) {
+        this.telemetryReporter = telemetryReporter;
+        this.logger = logger;
+    }
+}

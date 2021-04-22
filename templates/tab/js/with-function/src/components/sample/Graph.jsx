64a17ca1194f16db1b5b2b0beb33735fb39554f7
Zhenya <zhenyasav@users.noreply.github.com>
2021-04-22 02:24:25 -0700

    [WIP]  new hello world app templates (#149)
    * wip new template
    
    * added a few other sections and hooked up to teamsfx data path
    
    * feat(tab): template with new sdk (#362)
    
    * feat: integrate new sdk for new hello world template
    
    Co-authored-by: Zhenya <zhenyasa@microsoft.com>
    
    * add short link to docs
    
    * remove todo
    
    Co-authored-by: Zhijie Huang <zhihuan@microsoft.com>

---
+++ templates/tab/js/with-function/src/components/sample/Graph.jsx
@@ -0,0 +1,52 @@
+import React, { useRef } from "react";
+import { Button, Avatar, Loader } from "@fluentui/react-northstar";
+import { useData } from "./lib/useData";
+import {
+  TeamsUserCredential,
+  createMicrosoftGraphClient,
+} from "teamsdev-client";
+
+export function Graph() {
+  const graph = useRef(null);
+  const { loading, error, data, reload } = useData(
+    async () => {
+      if (!graph.current) {
+        const credential = new TeamsUserCredential();
+        const scope = ["User.Read"];
+        await credential.login(scope);
+        graph.current = await createMicrosoftGraphClient(credential, scope);
+      }
+      const profile = await graph.current.api("/me").get();
+      const photo = await graph.current.api("/me/photo/$value").get();
+      return {
+        profile,
+        photo,
+      };
+    },
+    { auto: false }
+  );
+  return (
+    <div>
+      <h2>Get the user's profile photo</h2>
+      <p>
+        Click below to authorize this app to read your profile photo using
+        Microsoft Graph.
+      </p>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Button primary content="Authorize" onClick={reload} />
+      )}
+      {!loading && error && <div className="error">{error.toString()}</div>}
+      {!loading && data && (
+        <div className="profile">
+          <Avatar
+            image={URL.createObjectURL(data.photo)}
+            name={data.profile.displayName}
+          />{" "}
+          <em>{data.profile.displayName}</em>
+        </div>
+      )}
+    </div>
+  );
+}

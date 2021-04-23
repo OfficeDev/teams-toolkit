4cd400a430fe6d91185c0350f10a1423e4efe385
Ning Liu <nliu@microsoft.com>
2021-04-23 17:00:10 +0800

    feat: support manually publish option

---
+++ packages/fx-core/src/plugins/resource/appstudio/questions.ts
@@ -0,0 +1,13 @@
+import { OptionItem } from "fx-api";
+
+export const manuallySubmitOption: OptionItem = {
+    id: "ManuallySubmit",
+    label: "Manually submit Teams packge file",
+    detail: "Build Teams package file and manually send it to your amin to check"
+};
+
+export const autoPublishOption: OptionItem = {
+    id: "AutoPublish",
+    label: "Install for you organization",
+    detail: "Send your app to the admin portal for your admin to approve. Once approved, your app will be available for your organization"
+};

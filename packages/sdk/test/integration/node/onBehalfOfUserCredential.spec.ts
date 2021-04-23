ed71ca93815e5ea5a604ac7f062b995ac50bb55b
turenlong <rentu@microsoft.com>
2021-04-23 14:29:49 +0800

    fix: fix integration test error

--- packages/sdk/test/integration/node/onBehalfOfUserCredential.spec.ts
+++ packages/sdk/test/integration/node/onBehalfOfUserCredential.spec.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+import { AccessToken } from "@azure/identity";
 import { assert, expect, use as chaiUse } from "chai";
 
 import chaiPromises from "chai-as-promised";
@@ -25,38 +26,38 @@ describe("onBehalfOfUserCredential Test: Node", () => {
     loadConfiguration();
 
     ssoToken = await getAccessToken(
-      process.env.SDK_INTEGRATIONTEST_AAD_CLIENTID_SSO,
-      process.env.SDK_INTEGRATIONTEST_TEST_USERNAME,
-      process.env.SDK_INTEGRATIONTEST_TEST_PASSWORD,
-      process.env.SDK_INTEGRATIONTEST_AAD_TENANTID,
-      process.env.SDK_INTEGRATIONTEST_SCOPES_SSO
+      process.env.SDK_INTEGRATIONTEST_AAD_CLIENTID_SSO!,
+      process.env.SDK_INTEGRATIONTEST_TEST_USERNAME!,
+      process.env.SDK_INTEGRATIONTEST_TEST_PASSWORD!,
+      process.env.SDK_INTEGRATIONTEST_AAD_TENANTID!,
+      process.env.SDK_INTEGRATIONTEST_SCOPES_SSO!
     );
   });
 
-  it("Test onBehalfOfUserCredential get SSO token success", async function() {
+  it("Test onBehalfOfUserCredential get SSO token success", async function () {
     const credential = new OnBehalfOfUserCredential(ssoToken);
-    let ssoTokenFromCredential = await credential.getToken([]);
+    let ssoTokenFromCredential: AccessToken = await credential.getToken([]);
     assert.strictEqual(ssoTokenFromCredential.token, ssoToken);
 
     ssoTokenFromCredential = await credential.getToken("");
     assert.strictEqual(ssoTokenFromCredential.token, ssoToken);
   });
 
-  it("Test onBehalfOfUserCredential get user info success", async function() {
+  it("Test onBehalfOfUserCredential get user info success", async function () {
     const credential = new OnBehalfOfUserCredential(ssoToken);
     const userInfo = await credential.getUserInfo();
-    assert.strictEqual(userInfo.preferredUserName, process.env.SDK_INTEGRATIONTEST_TEST_USERNAME);
+    assert.strictEqual(userInfo.preferredUserName, process.env.SDK_INTEGRATIONTEST_TEST_USERNAME!);
   });
 
-  it("Test onBehalfOfUserCredential get access token success", async function() {
+  it("Test onBehalfOfUserCredential get access token success", async function () {
     const credential = new OnBehalfOfUserCredential(ssoToken);
-    const graphToken = await credential.getToken("https://graph.microsoft.com/User.Read");
+    const graphToken: AccessToken = await credential.getToken("https://graph.microsoft.com/User.Read");
     const tokenObject = parseJwt(graphToken.token);
     const userInfo = await credential.getUserInfo();
     assert.strictEqual(tokenObject.oid, userInfo.objectId);
   });
 
-  it("Test onBehalfOfUserCredential get access token without permission", async function() {
+  it("Test onBehalfOfUserCredential get access token without permission", async function () {
     const credential = new OnBehalfOfUserCredential(ssoToken);
     await expect(credential.getToken("https://graph.microsoft.com/Calendars.Read"))
       .to.eventually.be.rejectedWith(ErrorWithCode)

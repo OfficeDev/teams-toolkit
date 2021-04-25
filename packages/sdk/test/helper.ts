2c49d0584accbe50fc65177e78fa3dfe48825fe2
wenytang-ms <wenyutang@microsoft.com>
2021-04-25 13:57:44 +0800

    test(sdk): modify the proces env for better readable

--- packages/sdk/test/helper.ts
+++ packages/sdk/test/helper.ts
@@ -48,10 +48,11 @@ export async function getAccessToken(
   tenantId: string,
   scope?: string
 ): Promise<string> {
+  const defaultAuthorityHost = process.env.SDK_INTEGRATION_TEST_AAD_AUTHORITY_HOST;
   const msalConfig = {
     auth: {
       clientId: clientId,
-      authority: `https://login.microsoftonline.com/${tenantId}`
+      authority: defaultAuthorityHost.concat("/", tenantId)
     }
   };
   let scopes: string[];
@@ -59,7 +60,8 @@ export async function getAccessToken(
   if (scope) {
     scopes = [scope];
   } else {
-    scopes = [`api://localhost/${clientId}/access_as_user`];
+    const defaultScope = process.env.SDK_INTEGRATION_TEST_TEAMS_SCOPES_SSO;
+    scopes = [defaultScope!];
   }
   const pca = new msal.PublicClientApplication(msalConfig);
   const usernamePasswordRequest = {
@@ -71,10 +73,14 @@ export async function getAccessToken(
   return response!.accessToken;
 }
 
+/**
+ * Mapping environment variables from CI process to current environment for demo.
+ * Once invoke MockEnvironmentVariables, mock the variables in it with another value, it will take effect immediately.
+ */
 export function MockEnvironmentVariable() {
   restore = mockedEnv({
-    M365_CLIENT_ID : process.env.SDK_INTEGRATION_TEST_AAD_CLIENTID_REMOTE,
-    M365_CLIENT_SECRET : process.env.SDK_INTEGRATION_TEST_APP_CLIENT_SECRET_REMOTE,
+    M365_CLIENT_ID : process.env.SDK_INTEGRATION_TEST_M365_AAD_CLIENT_ID,
+    M365_CLIENT_SECRET : process.env.SDK_INTEGRATION_TEST_M365_AAD_CLIENT_SECRET,
     M365_TENANT_ID : process.env.SDK_INTEGRATION_TEST_AAD_TENANTID,
     M365_AUTHORITY_HOST : process.env.SDK_INTEGRATION_TEST_AAD_AUTHORITY_HOST,
 
@@ -85,6 +91,10 @@ export function MockEnvironmentVariable() {
   });
 }
 
+/**
+ * restore the mapping process environment variables.
+ * once invoke this method, all mock environment above will be restored.
+ */
 export function RestoreEnvironmentVariable() {
   restore();
 }

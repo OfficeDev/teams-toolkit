394d71cf9907cb8490118927f7e128a59fc08b24
Di Lin <dilin@microsoft.com>
2021-04-26 10:48:49 +0800

    test(sdk): fix compilation error

--- packages/sdk/test/helper.ts
+++ packages/sdk/test/helper.ts
@@ -103,11 +103,11 @@ export async function getSsoTokenFromTeams() {
     throw new Error("Environment variables are missing: " +  missingConfigurations.join(", "));
   }
   return await getAccessToken(
-    process.env.SDK_INTEGRATION_TEST_TEAMS_AAD_CLIENT_ID,
-    process.env.SDK_INTEGRATION_TEST_ACCOUNT_NAME,
-    process.env.SDK_INTEGRATION_TEST_ACCOUNT_PASSWORD,
-    process.env.SDK_INTEGRATION_TEST_AAD_TENANT_ID,
-    process.env.SDK_INTEGRATION_TEST_TEAMS_ACCESS_AS_USER_SCOPE
+    process.env.SDK_INTEGRATION_TEST_TEAMS_AAD_CLIENT_ID!,
+    process.env.SDK_INTEGRATION_TEST_ACCOUNT_NAME!,
+    process.env.SDK_INTEGRATION_TEST_ACCOUNT_PASSWORD!,
+    process.env.SDK_INTEGRATION_TEST_AAD_TENANT_ID!,
+    process.env.SDK_INTEGRATION_TEST_TEAMS_ACCESS_AS_USER_SCOPE!
   );
 }
 

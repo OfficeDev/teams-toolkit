7ee8721eed4ef1569d687ce1ab9265214aab2f83
timngmsft <timngmsft@hotmail.com>
2021-04-27 09:13:29 -0700

    Revert the strings coming from Azure
    I don't think we should be duplicating the strings from Azure ... but it is what it is.

--- packages/fx-core/src/plugins/resource/simpleauth/constants.ts
+++ packages/fx-core/src/plugins/resource/simpleauth/constants.ts
@@ -88,9 +88,9 @@ export class Constants {
     };
 
     static readonly FreeServerFarmsQuotaErrorFromAzure =
-        "The maximum number of free ServerFarms in an Azure subscription is 10";
+        "The maximum number of Free ServerFarms allowed in a Subscription is 10";
     static readonly FreeServerFarmsQuotaErrorToUser =
-        "The maximum number of free App Service plans in an Azure subscription is 10. Delete a free App Service plan and try again.";
+        "The maximum number of Free App Service Plan allowed in a Subscription is 10. Delete a free App Service plan and try again.";
     static readonly FreeServerFarmsQuotaErrorHelpLink = "https://aka.ms/teamsfx-sa-help#freeserverfarmsquotaerror";
 }
 

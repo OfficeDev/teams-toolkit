3272eb9cd97380c471e930921413e3c40fcadece
zhijie <zhihuan@microsoft.com>
2021-04-28 11:07:28 +0800

    feat: add ts template

---
+++ templates/tab/ts/default/src/components/sample/Deploy.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Deploy.css";
+import { Image } from "@fluentui/react-northstar";
+
+export function Deploy(props: { docsUrl?: string }) {
+  const { docsUrl } = {
+    docsUrl: "https://aka.ms/todo",
+    ...props,
+  };
+  return (
+    <div className="deploy page">
+      <h2>Deploy to the Cloud</h2>
+      <p>
+        Before publishing your app to Teams App Catalog, you may want to
+        provision and deploy your app's resources to the cloud to make sure your
+        app will be running smoothly!
+      </p>
+      <p>
+        To provision your resources, you can either use our CLI command "TeamsFx
+        provision" or apply “TeamsFx: Provision in the Cloud" in Command
+        palette.
+      </p>
+      <p>
+        To deploy your app, you can either use our CLI command "TeamsFx deploy"
+        or apply “TeamsFx: Deploy to the cloud" in Command palette.
+      </p>
+      <Image src="deploy.png" />
+      <p>
+        For more information, see the <a href={docsUrl}>doc</a>.
+      </p>
+    </div>
+  );
+}

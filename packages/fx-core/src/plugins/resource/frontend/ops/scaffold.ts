c071c3a9d7def915305987ee19150a64f250cdca
zhijie <zhihuan@microsoft.com>
2021-04-29 12:45:30 +0800

    style: refine indent

--- packages/fx-core/src/plugins/resource/frontend/ops/scaffold.ts
+++ packages/fx-core/src/plugins/resource/frontend/ops/scaffold.ts
@@ -8,15 +8,15 @@ import fs from "fs-extra";
 import semver from "semver";
 
 import {
-  FetchTemplateManifestError,
-  FetchTemplatePackageError,
-  InvalidTemplateManifestError,
-  runWithErrorCatchAndThrow,
+    FetchTemplateManifestError,
+    FetchTemplatePackageError,
+    InvalidTemplateManifestError,
+    runWithErrorCatchAndThrow,
 } from "../resources/errors";
 import {
-  Constants,
-  FrontendPathInfo,
-  FrontendPluginInfo as PluginInfo,
+    Constants,
+    FrontendPathInfo,
+    FrontendPluginInfo as PluginInfo,
 } from "../constants";
 import { Logger } from "../utils/logger";
 import { Messages } from "../resources/messages";
@@ -26,131 +26,131 @@ import { telemetryHelper } from "../utils/telemetry-helper";
 import { TemplateInfo, TemplateVariable } from "../resources/templateInfo";
 
 export type Manifest = {
-  [key: string]: {
     [key: string]: {
-      [key: string]: {
-        version: string;
-        url: string;
-      }[];
+        [key: string]: {
+            [key: string]: {
+                version: string;
+                url: string;
+            }[];
+        };
     };
-  };
 };
 
 export class FrontendScaffold {
-  public static async fetchTemplateManifest(url: string): Promise<Manifest> {
-    const result = await runWithErrorCatchAndThrow(
-      new FetchTemplatePackageError(),
-      () =>
-        Utils.requestWithRetry(async () => {
-          return axios.get(url);
-        })
-    );
-    if (!result) {
-      throw new FetchTemplatePackageError();
+    public static async fetchTemplateManifest(url: string): Promise<Manifest> {
+        const result = await runWithErrorCatchAndThrow(
+            new FetchTemplatePackageError(),
+            () =>
+                Utils.requestWithRetry(async () => {
+                    return axios.get(url);
+                })
+        );
+        if (!result) {
+            throw new FetchTemplatePackageError();
+        }
+        return result.data;
     }
-    return result.data;
-  }
 
-  public static async getTemplateURL(
-    manifestUrl: string,
-    group: string,
-    language: string,
-    scenario: string,
-    version: string
-  ): Promise<string> {
-    const manifest: Manifest = await this.fetchTemplateManifest(manifestUrl);
-    return runWithErrorCatchAndThrow(new InvalidTemplateManifestError(), () => {
-      const urls: { version: string; url: string }[] = manifest[group][
-        language
-      ][scenario]
-        .filter((x) => semver.satisfies(x.version, version))
-        .sort((a, b) => -semver.compare(a.version, b.version));
-      return urls[0].url;
-    });
-  }
-
-  public static async fetchZipFromUrl(url: string): Promise<AdmZip> {
-    const result = await runWithErrorCatchAndThrow(
-      new FetchTemplateManifestError(),
-      () =>
-        Utils.requestWithRetry(async () => {
-          return axios.get(url, {
-            responseType: "arraybuffer",
-            timeout: Constants.RequestTimeoutInMS,
-          });
-        })
-    );
-
-    if (!result) {
-      throw new FetchTemplatePackageError();
+    public static async getTemplateURL(
+        manifestUrl: string,
+        group: string,
+        language: string,
+        scenario: string,
+        version: string
+    ): Promise<string> {
+        const manifest: Manifest = await this.fetchTemplateManifest(manifestUrl);
+        return runWithErrorCatchAndThrow(new InvalidTemplateManifestError(), () => {
+            const urls: { version: string; url: string }[] = manifest[group][
+                language
+            ][scenario]
+                .filter((x) => semver.satisfies(x.version, version))
+                .sort((a, b) => -semver.compare(a.version, b.version));
+            return urls[0].url;
+        });
     }
-    return new AdmZip(result.data);
-  }
 
-  public static getTemplateZipFromLocal(templateInfo: TemplateInfo): AdmZip {
-    const templatePath = path.resolve(
-      FrontendPathInfo.RootDir,
-      templateInfo.localTemplatePath
-    );
-    return new AdmZip(templatePath);
-  }
+    public static async fetchZipFromUrl(url: string): Promise<AdmZip> {
+        const result = await runWithErrorCatchAndThrow(
+            new FetchTemplateManifestError(),
+            () =>
+                Utils.requestWithRetry(async () => {
+                    return axios.get(url, {
+                        responseType: "arraybuffer",
+                        timeout: Constants.RequestTimeoutInMS,
+                    });
+                })
+        );
 
-  public static async getTemplateZip(
-    ctx: PluginContext,
-    templateInfo: TemplateInfo
-  ): Promise<AdmZip> {
-    try {
-      // Temporarily hard code template language as JavaScript
-      const templateUrl = await FrontendScaffold.getTemplateURL(
-        PluginInfo.templateManifestURL,
-        templateInfo.group,
-        templateInfo.language,
-        templateInfo.scenario,
-        templateInfo.version
-      );
-      return await FrontendScaffold.fetchZipFromUrl(templateUrl);
-    } catch (e) {
-      telemetryHelper.sendErrorEvent(ctx, Messages.FailedFetchTemplate(), e);
-      Logger.warning(Messages.FailedFetchTemplate());
-      return FrontendScaffold.getTemplateZipFromLocal(templateInfo);
+        if (!result) {
+            throw new FetchTemplatePackageError();
+        }
+        return new AdmZip(result.data);
     }
-  }
 
-  public static fulfill(
-    filePath: string,
-    data: Buffer,
-    variables: TemplateVariable
-  ): string | Buffer {
-    if (path.extname(filePath) === FrontendPathInfo.TemplateFileExt) {
-      return Mustache.render(data.toString(), variables);
+    public static getTemplateZipFromLocal(templateInfo: TemplateInfo): AdmZip {
+        const templatePath = path.resolve(
+            FrontendPathInfo.RootDir,
+            templateInfo.localTemplatePath
+        );
+        return new AdmZip(templatePath);
     }
-    return data;
-  }
 
-  public static async scaffoldFromZip(
-    zip: AdmZip,
-    dstPath: string,
-    nameReplaceFn?: (filePath: string, data: Buffer) => string,
-    dataReplaceFn?: (filePath: string, data: Buffer) => string | Buffer
-  ): Promise<void> {
-    await Promise.all(
-      zip
-        .getEntries()
-        .filter((entry) => !entry.isDirectory)
-        .map(async (entry) => {
-          const data: string | Buffer = dataReplaceFn
-            ? dataReplaceFn(entry.name, entry.getData())
-            : entry.getData();
+    public static async getTemplateZip(
+        ctx: PluginContext,
+        templateInfo: TemplateInfo
+    ): Promise<AdmZip> {
+        try {
+            // Temporarily hard code template language as JavaScript
+            const templateUrl = await FrontendScaffold.getTemplateURL(
+                PluginInfo.templateManifestURL,
+                templateInfo.group,
+                templateInfo.language,
+                templateInfo.scenario,
+                templateInfo.version
+            );
+            return await FrontendScaffold.fetchZipFromUrl(templateUrl);
+        } catch (e) {
+            telemetryHelper.sendErrorEvent(ctx, Messages.FailedFetchTemplate(), e);
+            Logger.warning(Messages.FailedFetchTemplate());
+            return FrontendScaffold.getTemplateZipFromLocal(templateInfo);
+        }
+    }
 
-          const filePath = path.join(
-            dstPath,
-            nameReplaceFn
-              ? nameReplaceFn(entry.entryName, entry.getData())
-              : entry.entryName
-          );
-          await fs.ensureDir(path.dirname(filePath));
-          await fs.writeFile(filePath, data);
-        })
-    );
-  }
+    public static fulfill(
+        filePath: string,
+        data: Buffer,
+        variables: TemplateVariable
+    ): string | Buffer {
+        if (path.extname(filePath) === FrontendPathInfo.TemplateFileExt) {
+            return Mustache.render(data.toString(), variables);
+        }
+        return data;
+    }
+
+    public static async scaffoldFromZip(
+        zip: AdmZip,
+        dstPath: string,
+        nameReplaceFn?: (filePath: string, data: Buffer) => string,
+        dataReplaceFn?: (filePath: string, data: Buffer) => string | Buffer
+    ): Promise<void> {
+        await Promise.all(
+            zip
+                .getEntries()
+                .filter((entry) => !entry.isDirectory)
+                .map(async (entry) => {
+                    const data: string | Buffer = dataReplaceFn
+                        ? dataReplaceFn(entry.name, entry.getData())
+                        : entry.getData();
+
+                    const filePath = path.join(
+                        dstPath,
+                        nameReplaceFn
+                            ? nameReplaceFn(entry.entryName, entry.getData())
+                            : entry.entryName
+                    );
+                    await fs.ensureDir(path.dirname(filePath));
+                    await fs.writeFile(filePath, data);
+                })
+        );
+    }
 }
--- packages/fx-core/src/plugins/resource/frontend/utils.ts
+++ packages/fx-core/src/plugins/resource/frontend/utils.ts
@@ -11,125 +11,125 @@ import fs from "fs-extra";
 import klaw from "klaw";
 
 export class Utils {
-  static async delays(millisecond: number): Promise<void> {
-    return await new Promise<void>(
-      (resolve: () => void): NodeJS.Timer => setTimeout(resolve, millisecond)
-    );
-  }
-
-  static generateStorageAccountName(
-    appName: string,
-    resourceNameSuffix: string,
-    suffix: string
-  ): string {
-    const paddingLength: number =
-      Constants.AzureStorageAccountNameLenMax -
-      resourceNameSuffix.length -
-      suffix.length;
-    const normalizedAppName: string = appName
-      .replace(Constants.FrontendAppNamePattern, Constants.EmptyString)
-      .toLowerCase();
-    return (
-      normalizedAppName.substr(0, paddingLength) + suffix + resourceNameSuffix
-    );
-  }
-
-  static async requestWithRetry(
-    request: () => Promise<AxiosResponse<any>>,
-    maxTryCount = Constants.RequestRetryTimes
-  ): Promise<AxiosResponse<any> | undefined> {
-    // !status means network error, see https://github.com/axios/axios/issues/383
-    const canTry = (status: number | undefined) =>
-      !status || (status >= 500 && status < 600);
-
-    let tryCount = 0;
-    let error: Error = new Error();
-    while (tryCount++ < maxTryCount) {
-      try {
-        const result = await request();
-        if (result.status === 200 || result.status === 201) {
-          return result;
-        }
-
-        error = new Error(`HTTP Request failed: ${JSON.stringify(result)}`);
-        if (!canTry(result.status)) {
-          break;
-        }
-      } catch (e) {
-        error = e;
-        if (!canTry(e.response.status)) {
-          break;
-        }
-      }
+    static async delays(millisecond: number): Promise<void> {
+        return await new Promise<void>(
+            (resolve: () => void): NodeJS.Timer => setTimeout(resolve, millisecond)
+        );
     }
-    throw error;
-  }
 
-  static async execute(
-    command: string,
-    workingDir?: string,
-    ignoreError = false
-  ): Promise<string> {
-    return new Promise((resolve, reject) => {
-      Logger.info(`Start to run command: "${command}".`);
+    static generateStorageAccountName(
+        appName: string,
+        resourceNameSuffix: string,
+        suffix: string
+    ): string {
+        const paddingLength: number =
+            Constants.AzureStorageAccountNameLenMax -
+            resourceNameSuffix.length -
+            suffix.length;
+        const normalizedAppName: string = appName
+            .replace(Constants.FrontendAppNamePattern, Constants.EmptyString)
+            .toLowerCase();
+        return (
+            normalizedAppName.substr(0, paddingLength) + suffix + resourceNameSuffix
+        );
+    }
 
-      exec(command, { cwd: workingDir }, (error, standardOutput) => {
-        Logger.debug(standardOutput);
-        if (error) {
-          Logger.error(`Failed to run command: "${command}".`);
-          if (!ignoreError) {
-            Logger.error(error.message);
-            reject(error);
-          }
-          Logger.warning(error.message);
+    static async requestWithRetry(
+        request: () => Promise<AxiosResponse<any>>,
+        maxTryCount = Constants.RequestRetryTimes
+    ): Promise<AxiosResponse<any> | undefined> {
+        // !status means network error, see https://github.com/axios/axios/issues/383
+        const canTry = (status: number | undefined) =>
+            !status || (status >= 500 && status < 600);
+
+        let tryCount = 0;
+        let error: Error = new Error();
+        while (tryCount++ < maxTryCount) {
+            try {
+                const result = await request();
+                if (result.status === 200 || result.status === 201) {
+                    return result;
+                }
+
+                error = new Error(`HTTP Request failed: ${JSON.stringify(result)}`);
+                if (!canTry(result.status)) {
+                    break;
+                }
+            } catch (e) {
+                error = e;
+                if (!canTry(e.response.status)) {
+                    break;
+                }
+            }
         }
-        resolve(standardOutput);
-      });
-    });
-  }
+        throw error;
+    }
 
-  public static async listFilePaths(
-    directoryPath: string,
-    ignorePattern?: string
-  ): Promise<string[]> {
-    return new Promise<string[]>((resolve, reject) => {
-      const ignore: string = ignorePattern
-        ? path.join(directoryPath, ignorePattern)
-        : "";
-      glob(
-        path.join(directoryPath, "**"),
-        {
-          dot: true, // Include .dot files
-          nodir: true, // Only match files
-          ignore,
-        },
-        (error, filePaths) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(filePaths);
-          }
-        }
-      );
-    });
-  }
+    static async execute(
+        command: string,
+        workingDir?: string,
+        ignoreError = false
+    ): Promise<string> {
+        return new Promise((resolve, reject) => {
+            Logger.info(`Start to run command: "${command}".`);
 
-  public static async forEachFileAndDir(
-    root: string,
-    callback: (itemPath: string, stats: fs.Stats) => boolean | void,
-    filter?: (itemPath: string) => boolean
-  ): Promise<void> {
-    await new Promise((resolve, reject) => {
-      const stream: klaw.Walker = klaw(root, { filter: filter });
-      stream
-        .on("data", (item) => {
-          if (callback(item.path, item.stats)) {
-            stream.emit("close");
-          }
-        })
-        .on("end", () => resolve({}))
-        .on("error", (err) => reject(err))
-        .on("close", () => resolve({}));
-    });
-  }
+            exec(command, { cwd: workingDir }, (error, standardOutput) => {
+                Logger.debug(standardOutput);
+                if (error) {
+                    Logger.error(`Failed to run command: "${command}".`);
+                    if (!ignoreError) {
+                        Logger.error(error.message);
+                        reject(error);
+                    }
+                    Logger.warning(error.message);
+                }
+                resolve(standardOutput);
+            });
+        });
+    }
+
+    public static async listFilePaths(
+        directoryPath: string,
+        ignorePattern?: string
+    ): Promise<string[]> {
+        return new Promise<string[]>((resolve, reject) => {
+            const ignore: string = ignorePattern
+                ? path.join(directoryPath, ignorePattern)
+                : "";
+            glob(
+                path.join(directoryPath, "**"),
+                {
+                    dot: true, // Include .dot files
+                    nodir: true, // Only match files
+                    ignore,
+                },
+                (error, filePaths) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(filePaths);
+                    }
+                }
+            );
+        });
+    }
+
+    public static async forEachFileAndDir(
+        root: string,
+        callback: (itemPath: string, stats: fs.Stats) => boolean | void,
+        filter?: (itemPath: string) => boolean
+    ): Promise<void> {
+        await new Promise((resolve, reject) => {
+            const stream: klaw.Walker = klaw(root, { filter: filter });
+            stream
+                .on("data", (item) => {
+                    if (callback(item.path, item.stats)) {
+                        stream.emit("close");
+                    }
+                })
+                .on("end", () => resolve({}))
+                .on("error", (err) => reject(err))
+                .on("close", () => resolve({}));
+        });
+    }
 }

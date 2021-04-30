4bf4841af476eac401551b475e75f6251d05c361
wenytang-ms <wenyutang@microsoft.com>
2021-04-30 11:29:23 +0800

    test(sdk): run success

--- packages/sdk/test/integration/scripts/test.js
+++ packages/sdk/test/integration/scripts/test.js
@@ -1,8 +1,7 @@
 const { spawn } = require('child_process');
 const path = require('path');
-const { pid } = require('process');
 const simpleAuthDir = path.resolve(__dirname, "../../../../simpleauth/src/TeamsFxSimpleAuth");
-
+require('dotenv').config();
 process.env.CLIENT_ID= process.env.SDK_INTEGRATION_TEST_M365_AAD_CLIENT_ID;
 process.env.CLIENT_SECRET=process.env.SDK_INTEGRATION_TEST_M365_AAD_CLIENT_SECRET;
 process.env.IDENTIFIER_URI=`api://localhost/${process.env.CLIENT_ID}`;
@@ -11,19 +10,4 @@ process.env.AAD_METADATA_ADDRESS=`https://login.microsoftonline.com/${process.en
 process.env.ALLOWED_APP_IDS="1fec8e78-bce4-4aaf-ab1b-5451cc387264;5e3ce6c0-2b1f-4285-8d4b-75ee78787346;3d6e5c14-3406-4de2-8c00-e647aa15705a";
 process.env.TAB_APP_ENDPOINT="*";
 
-let ls = spawn('dotnet', ['run', '--project', `${simpleAuthDir}/Microsoft.TeamsFx.SimpleAuth.csproj`], {detached:true});
-
-ls.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
-
-ls.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
-});
-
-ls.on('close', (code) => {
-  console.log(`child process exits ${code}`);
-});
-
-// let pid_ex = ls.pid;
-// export {pid_ex};
+let ls = spawn('dotnet', ['run', '--project', `${simpleAuthDir}/Microsoft.TeamsFx.SimpleAuth.csproj`]);

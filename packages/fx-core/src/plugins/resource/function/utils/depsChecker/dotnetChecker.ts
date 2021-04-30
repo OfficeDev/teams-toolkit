67432b9a9eea4ad189d15115868649d7b2559505
Aocheng Wang <aochengwang@microsoft.com>
2021-04-30 11:39:30 +0800

    chore: remove cmd escape

--- packages/fx-core/src/plugins/resource/function/utils/depsChecker/dotnetChecker.ts
+++ packages/fx-core/src/plugins/resource/function/utils/depsChecker/dotnetChecker.ts
@@ -400,7 +400,7 @@ export class DotnetChecker implements IDepsChecker {
         '-ExecutionPolicy',
         'unrestricted',
         '-Command',
-        `"& { [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12 ; & ${command.join(' ')} }`
+        `& { [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12 ; & ${command.join(' ')} }`
       ];
     } else {
       return command;

3c60e856788a22380849be391b7db6b33eb4a6e7
huajiezhang <huajiezhang@microsoft.com>
2021-04-30 13:49:17 +0800

    fix: fix

--- packages/fx-core/src/plugins/solution/fx-solution/question.ts
+++ packages/fx-core/src/plugins/solution/fx-solution/question.ts
@@ -180,7 +180,8 @@ export const ProgrammingLanguageQuestion: SingleSelectQuestion = {
     option: (previousAnswers?: ConfigMap) : StaticOption => {  
         const hostType = previousAnswers?.getString(AzureSolutionQuestionNames.HostType);
         if(HostTypeOptionSPFx.id === hostType) return [{id:"typescript", label:"TypeScript"}];
-        return [{id:"javascript", label: "JavaScript"}, {id:"typescript", label:"TypeScript"}];
+        else if(HostTypeOptionAzure.id === hostType) return [{id:"javascript", label: "JavaScript"}, {id:"typescript", label:"TypeScript"}];
+        return [];
     },
     default: "javascript",
     placeholder: (previousAnswers?: ConfigMap) : string|undefined => {  

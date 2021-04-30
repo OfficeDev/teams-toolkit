8226a736af491d0058f4ef962fc966242ce3b00a
wenytang-ms <wenyutang@microsoft.com>
2021-04-30 17:01:29 +0800

    test(sdk): fix code

--- packages/sdk/test/integration/browser/teamsUserCredential.browser.spec.ts
+++ packages/sdk/test/integration/browser/teamsUserCredential.browser.spec.ts
@@ -9,14 +9,18 @@ import { getSSOToken } from "../helper.browser";
 
 chaiUse(chaiPromises);
 const env = (window as any).__env__;
+
+const TEST_USER_OBJECT_ID = "77675783-a922-4a3c-a4bb-269b4dd94d7d";
+const FAKE_LOGIN_ENDPOINT =  "FakeLoginEndpoint";
+
 describe("TeamsUserCredential Integration Test - browser", () => {
     let ssoToken: string;
-    const fakeLoginEndpoint:string =  "FakeLoginEndpoint";
+    let expire_time: number;
     before(async () => {
-        ssoToken = await getSSOToken();
+        [ssoToken, expire_time] = await getSSOToken();
         loadConfiguration({
             authentication: {
-                initiateLoginEndpoint: fakeLoginEndpoint,
+                initiateLoginEndpoint: FAKE_LOGIN_ENDPOINT,
                 simpleAuthEndpoint: "http://localhost:5000",
                 clientId: env.SDK_INTEGRATION_TEST_M365_AAD_CLIENT_ID
               }
@@ -26,7 +30,7 @@ describe("TeamsUserCredential Integration Test - browser", () => {
                 return new Promise((resolve, reject) => {
                     resolve({
                         token: ssoToken!,
-                        expiresOnTimestamp: Date.now() + 10 * 60 * 1000
+                        expiresOnTimestamp: expire_time!
                     });
                 });
             }
@@ -34,21 +38,23 @@ describe("TeamsUserCredential Integration Test - browser", () => {
     });
     after(()=>{
         sinon.restore();
-    })
+    });
     
     it("GetUserInfo should success with SSOToken", async function () {
         const credential: TeamsUserCredential = new TeamsUserCredential();
         const info = await credential.getUserInfo();
         assert.strictEqual(info.preferredUserName, env.SDK_INTEGRATION_TEST_ACCOUNT_NAME);
+        assert.strictEqual(info.displayName, env.SDK_INTEGRATION_TEST_ACCOUNT_NAME.split("@", 1)[0]);
+        assert.strictEqual(info.objectId, TEST_USER_OBJECT_ID);
     });
 
     it("GetToken should success with consent scope", async function(){
         const credential: TeamsUserCredential = new TeamsUserCredential();
-        await expect(credential.getToken(["User.Read"])).to.be.eventually.have.property('token');
+        await expect(credential.getToken(["User.Read"])).to.be.eventually.have.property("token");
     });
 
     it("GetToken should throw UiRequiredError with unconsent scope", async function() {
         const credential: TeamsUserCredential = new TeamsUserCredential();
-        await expect(credential.getToken(["Calendars.Read"])).to.eventually.be.rejectedWith(ErrorWithCode).and.property("code", ErrorCode.UiRequiredError);;
-    })
+        await expect(credential.getToken(["Calendars.Read"])).to.eventually.be.rejectedWith(ErrorWithCode).and.property("code", ErrorCode.UiRequiredError);
+    });
 });
--- packages/sdk/test/integration/helper.browser.ts
+++ packages/sdk/test/integration/helper.browser.ts
@@ -4,30 +4,29 @@ import axios from "axios";
 /**
  * Get SSO Token from a specific AAD app client id.
  */
-export async function getSSOToken(): Promise<string> {
-  const env = (window as any).__env__;
-  let details = {
-    username: env.SDK_INTEGRATION_TEST_ACCOUNT_NAME,
-    password: env.SDK_INTEGRATION_TEST_ACCOUNT_PASSWORD,
-    client_id: env.SDK_INTEGRATION_TEST_M365_AAD_CLIENT_ID,
-    scope: env.SDK_INTEGRATION_TEST_TEAMS_ACCESS_AS_USER_SCOPE,
-    grant_type: "password"
-  };
-  let formBody = [];
-  for (let [key, value] of Object.entries(details)) {
-    let encodedKey = encodeURIComponent(key);
-    let encodedValue = encodeURIComponent(value);
-    formBody.push(encodedKey + "=" + encodedValue);
-  }
-  const body = formBody.join("&");
-  const response = await axios.post(
-    `https://login.microsoftonline.com/${env.SDK_INTEGRATION_TEST_AAD_TENANT_ID}/oauth2/v2.0/token`,
-    body,
-    {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      }
+export async function getSSOToken(): Promise<[string, number]>  {
+    const env = (window as any).__env__;
+    const details = {
+        username: env.SDK_INTEGRATION_TEST_ACCOUNT_NAME,
+        password: env.SDK_INTEGRATION_TEST_ACCOUNT_PASSWORD,
+        client_id: env.SDK_INTEGRATION_TEST_TEAMS_AAD_CLIENT_ID,
+        scope: env.SDK_INTEGRATION_TEST_TEAMS_ACCESS_AS_USER_SCOPE,
+        grant_type: "password"
+    };
+    const formBody = [];
+    for (const [key ,value] of Object.entries(details)) {
+        const encodedKey = encodeURIComponent(key);
+        const encodedValue = encodeURIComponent(value);
+        formBody.push(encodedKey + "=" + encodedValue);
     }
-  );
-  return (response.data as any)["access_token"];
+    const body = formBody.join("&");
+    const response =  await axios.post(`https://login.microsoftonline.com/${env.SDK_INTEGRATION_TEST_AAD_TENANT_ID}/oauth2/v2.0/token`,
+    body,{
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+    });
+    const token = (response.data as any)["access_token"];
+    const expiresTime = (response.data as any)["expires_in"];
+    return [token, expiresTime];
 }

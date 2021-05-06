3d12097be8b1aef08e108006ffd30786a9edf520
Zhijie Huang <zhihuan@microsoft.com>
2021-05-06 09:30:31 +0800

    feat(tab): improve UI and refine useGraph (#678)
    * feat: update ui and useGraph for ts
    
    * feat: update js

--- templates/tab/js/default/src/components/sample/Graph.jsx
+++ templates/tab/js/default/src/components/sample/Graph.jsx
@@ -4,7 +4,15 @@ import { useGraph } from "./lib/useGraph";
 import { ProfileCard } from "./ProfileCard";
 
 export function Graph() {
-  const { loading, error, data, reload } = useGraph();
+  const { loading, error, data, reload } = useGraph(
+    async (graph) => {
+      const profile = await graph.api("/me").get();
+      const photo = await graph.api("/me/photo/$value").get();
+      return { profile, photo };
+    },
+    { scope: ["User.Read"] }
+  );
+
   return (
     <div>
       <h2>Get the user's profile photo</h2>

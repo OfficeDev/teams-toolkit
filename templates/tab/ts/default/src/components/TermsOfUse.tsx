63fe48eabc6a71fa45211ab190ce841729052d53
zhijie <zhihuan@microsoft.com>
2021-05-06 16:51:37 +0800

    feat: remove header from templates

--- templates/tab/ts/default/src/components/TermsOfUse.tsx
+++ templates/tab/ts/default/src/components/TermsOfUse.tsx
@@ -1,6 +1,3 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
 import React from 'react';
 /**
  * This component is used to display the required

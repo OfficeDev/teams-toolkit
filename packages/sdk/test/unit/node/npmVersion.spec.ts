0868a300df75fab3805ab9ce839df5ce25d6a426
Ning Tang <nintan@microsoft.com>
2021-05-06 10:24:05 +0800

    fix(sdk): ignore package-lock.json if not exist

--- packages/sdk/test/unit/node/npmVersion.spec.ts
+++ packages/sdk/test/unit/node/npmVersion.spec.ts
@@ -8,10 +8,12 @@ import * as fs from "fs-extra";
 describe("Package Meta Test - Node", () => {
   it("has same value with package.json", () => {
     const packageData = fs.readJSONSync("package.json");
-    const packageLockData = fs.readJSONSync("package-lock.json");
     assert.strictEqual(name, packageData.name);
     assert.strictEqual(version, packageData.version);
-    assert.strictEqual(name, packageLockData.name);
-    assert.strictEqual(version, packageLockData.version);
+    if (fs.existsSync("package-lock.json")) {
+      const packageLockData = fs.readJSONSync("package-lock.json");
+      assert.strictEqual(name, packageLockData.name);
+      assert.strictEqual(version, packageLockData.version);
+    }
   });
 });

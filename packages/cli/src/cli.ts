6e6e5ec2a8bd1b8fc402cdc5bfd1d442e4611492
chagong <chagon@microsoft.com>
2021-05-06 16:26:23 +0800

    fix: fix get version

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -11,6 +11,7 @@ import yargs from "yargs";
 import { commands } from "./cmds";
 import * as constants from "./constants";
 import path from "path";
+import fs from "fs";
 
 /**
  * registers cli and partner commands with yargs.
@@ -25,25 +26,6 @@ export function register(yargs: yargs.Argv): void {
       command.handler.bind(command)
     );
   });
-}
-
-function getVersionString(): string {
-  let version = "teamsfx-cli: ";
-  let json = JSON.parse(readFileSync(path.join(__dirname, "/../package.json"), "utf8"));
-  version += json.version;
-  version += "\n";
-
-  version += "built with ";
-  const dirs = readdirSync(path.join(__dirname + "/../node_modules"));
-  const api = dirs.find((dir) => dir === "fx-api");
-  json = JSON.parse(readFileSync(path.join(__dirname, "/../node_modules/" + api + "/package.json"), "utf8"));
-  version += api + ": " + json.version + ", ";
-
-  const core = dirs.find((dir) => dir === "fx-core");
-  json = JSON.parse(readFileSync(path.join(__dirname, "/../node_modules/" + core + "/package.json"), "utf8"));
-  version += core + ": " + json.version;
-
-  return version;
 }
 
 (async () => {
@@ -65,7 +47,7 @@ function getVersionString(): string {
     .strict()
     .alias("help", "h")
     .alias("v", "version")
-    .version(getVersionString())
+    .version()
     .epilogue(
       "For more information about the Teams Toolkit - https://aka.ms/teamsfx-learn."
     ).argv;

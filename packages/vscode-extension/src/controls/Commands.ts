95e130837cda84cc4758d13e04d5dcf8d6060e90
Huihui Wu <huihuiwu@microsoft.com>
2021-05-06 14:28:21 +0800

    feat: display teams toolkit command in quick start

--- packages/vscode-extension/src/controls/Commands.ts
+++ packages/vscode-extension/src/controls/Commands.ts
@@ -5,7 +5,7 @@ export enum Commands {
   OpenExternalLink = "open-external-link",
   CloneSampleApp = "clone-sample-app",
   DisplayCommandPalette = "display-command-palette",
-  DisplayCliCommands = "display-cli-commands",
+  DisplayCommands = "display-commands",
   SigninM365 = "sign-in-m365",
   SigninAzure = "sign-in-azure",
   CreateNewProject = "create-new-project",

be0f6b26df578f32e499f20e7da74e226f443447
Wenyu Tang <wenyutang@microsoft.com>
2021-05-07 14:24:33 +0800

    chore(sdk): download simple auth locally

--- packages/sdk/test/integration/scripts/StartSimpleAuth.js
+++ packages/sdk/test/integration/scripts/StartSimpleAuth.js
@@ -1,22 +1,50 @@
-const { spawn, spawnSync } = require('child_process');
-const fs = require('fs');
-require("dotenv").config();
 const admZip = require('adm-zip');
+const { BlobServiceClient } = require('@azure/storage-blob');
+const fs = require('fs');
 const path = require('path');
-let simpleAuthDir = path.resolve(__dirname, "../../../../fx-core/templates/plugins/resource/simpleauth");
-let simpleAuthZip = `${simpleAuthDir}/SimpleAuth.zip`;
-if(!fs.existsSync(simpleAuthZip)) {
-    spawnSync("powershell.exe",[path.resolve(__dirname, "./downloadSimpleAuth.ps1.ps1")]);
+const { spawn } = require('child_process');
+require("dotenv").config();
+
+function setupEnv() {
+    process.env.CLIENT_ID = process.env.SDK_INTEGRATION_TEST_M365_AAD_CLIENT_ID;
+    process.env.CLIENT_SECRET = process.env.SDK_INTEGRATION_TEST_M365_AAD_CLIENT_SECRET;
+    process.env.IDENTIFIER_URI = `api://localhost/${process.env.CLIENT_ID}`;
+    process.env.OAUTH_AUTHORITY = `https://login.microsoftonline.com/${process.env.SDK_INTEGRATION_TEST_AAD_TENANT_ID}`;
+    process.env.AAD_METADATA_ADDRESS = `https://login.microsoftonline.com/${process.env.SDK_INTEGRATION_TEST_AAD_TENANT_ID}/v2.0/.well-known/openid-configuration`;
+    process.env.ALLOWED_APP_IDS = "1fec8e78-bce4-4aaf-ab1b-5451cc387264;5e3ce6c0-2b1f-4285-8d4b-75ee78787346;3d6e5c14-3406-4de2-8c00-e647aa15705a";
+    process.env.TAB_APP_ENDPOINT = "*";
+    process.env.SDK_INTEGRATION_TEST_USER_OBJECT_ID = process.env.SDK_INTEGRATION_TEST_USER_OBJECT_ID;
 }
-simpleAuthZip = new admZip(`${simpleAuthZip}`);
-simpleAuthZip.extractAllTo(`${simpleAuthDir}/SimpleAuthUnzipOutput`, true);
-simpleAuthDir = path.resolve(simpleAuthDir, "SimpleAuthUnzipOutput");
-process.env.CLIENT_ID= process.env.SDK_INTEGRATION_TEST_M365_AAD_CLIENT_ID;
-process.env.CLIENT_SECRET=process.env.SDK_INTEGRATION_TEST_M365_AAD_CLIENT_SECRET;
-process.env.IDENTIFIER_URI=`api://localhost/${process.env.CLIENT_ID}`;
-process.env.OAUTH_AUTHORITY=`https://login.microsoftonline.com/${process.env.SDK_INTEGRATION_TEST_AAD_TENANT_ID}`;
-process.env.AAD_METADATA_ADDRESS=`https://login.microsoftonline.com/${process.env.SDK_INTEGRATION_TEST_AAD_TENANT_ID}/v2.0/.well-known/openid-configuration`;
-process.env.ALLOWED_APP_IDS="1fec8e78-bce4-4aaf-ab1b-5451cc387264;5e3ce6c0-2b1f-4285-8d4b-75ee78787346;3d6e5c14-3406-4de2-8c00-e647aa15705a";
-process.env.TAB_APP_ENDPOINT="*";
-process.env.SDK_INTEGRATION_TEST_USER_OBJECT_ID = process.env.SDK_INTEGRATION_TEST_USER_OBJECT_ID;
-let ls = spawn('dotnet', [`${simpleAuthDir}/Microsoft.TeamsFx.SimpleAuth.dll`]);
+
+async function downloadSimpleAuth() {
+    const versionFilePath = path.join(__dirname, "../../../../fx-core/templates/plugins/resource/simpleauth/version.txt");
+    const version = fs.readFileSync(versionFilePath, "utf-8");
+    const fileName = `Microsoft.TeamsFx.SimpleAuth_${version}.zip`;
+    const endpoint = process.env.SIMPLE_AUTH_ENDPOINT.slice(0, -8);
+    const blobUrlWithCredential = `${endpoint}?${process.env.SIMPLE_AUTH_SAS_TOKEN}`;
+    try {
+        const blobClient = new BlobServiceClient(blobUrlWithCredential).getContainerClient("release").getBlobClient(fileName);
+        await blobClient.downloadToFile(__dirname + '/SimpleAuth.zip');
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+async function startSimpleAuth() {
+    let simpleAuthZip = `${__dirname}/SimpleAuth.zip`;
+    setupEnv();
+    if (!fs.existsSync(simpleAuthZip)) {
+        try {
+            await downloadSimpleAuth();
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+    simpleAuthZip = new admZip(`${simpleAuthZip}`);
+    simpleAuthZip.extractAllTo(`${__dirname}/SimpleAuthUnzipOutput`, true);
+    spawn('dotnet', [`${__dirname}/SimpleAuthUnzipOutput/Microsoft.TeamsFx.SimpleAuth.dll`]);
+}
+
+(async () => {
+    await startSimpleAuth();
+})();
--- packages/sdk/test/integration/scripts/downloadSimpleAuth.ps1
+++
@@ -1,6 +0,0 @@
-$simpleAuthPath = Join-Path $PSScriptRoot  ../../../../fx-core/templates/plugins/resource/simpleauth
-echo $simpleAuthPath
-$version=Get-Content $simpleAuthPath/version.txt
-$fileName="Microsoft.TeamsFx.SimpleAuth_$version.zip"
-$url=$env:SIMPLE_AUTH_ENDPOINT+'/'+$fileName+'?'+$env:SIMPLE_AUTH_SAS_TOKEN
-Invoke-WebRequest $url -OutFile $simpleAuthPath/SimpleAuth.zip

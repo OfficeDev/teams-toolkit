e224fd1d583e968191f70c730d21743340cdd380
tianyuan <tianyuan@microsoft.com>
2021-05-07 13:50:01 +0800

    feat: add sigingIn status

--- packages/vscode-extension/src/commonlib/common/constant.ts
+++ packages/vscode-extension/src/commonlib/common/constant.ts
@@ -5,5 +5,7 @@
 
 export const signedIn = "SignedIn";
 export const signedOut = "SignedOut";
+export const signingIn = "SigningIn";
 export const loggedOut = "LoggedOut";
 export const loggedIn = "LoggedIn";
+export const loggingIn = "LoggingIn";

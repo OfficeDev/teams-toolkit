7eb449b19366e61a1c32f6b91175d5dd0aaf587a
Ivan Jobs <ruhe@microsoft.com>
2021-05-08 09:55:16 +0800

    chore: wip

---
+++ packages/cli/tests/e2e/bot/Provision Resource project with new bot.ts
@@ -0,0 +1,151 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import fs from "fs-extra";
+import path from "path";
+
+import { AadValidator, FunctionValidator, SimpleAuthValidator } from "fx-api";
+
+import {
+  execAsync,
+  getSubscriptionId,
+  getTestFolder,
+  getUniqueAppName,
+  setBotSkuNameToB1,
+  cleanUp,
+} from "../commonUtils";
+import AppStudioLogin from "../../../src/commonlib/appStudioLogin";
+
+describe("Test Add Function", function () {
+  const testFolder = getTestFolder();
+  const appName = getUniqueAppName();
+  const subscription = getSubscriptionId();
+  const projectPath = path.resolve(testFolder, appName);
+
+  it(`Create Tab Then Add Function`, async function () {
+    await execAsync(
+      `teamsfx new --interactive false --app-name ${appName} --capabilities bot`,
+      {
+        cwd: testFolder,
+        env: process.env,
+        timeout: 0
+      }
+    );
+    console.log(`[Successfully] scaffold to ${projectPath}`);
+
+    await setBotSkuNameToB1(projectPath);
+
+    // set subscription
+    await execAsync(
+      `teamsfx account set --subscription ${subscription}`,
+      {
+        cwd: projectPath,
+        env: process.env,
+        timeout: 0
+      }
+    );
+
+    console.log(`[Successfully] set subscription for ${projectPath}`);
+
+    // provision
+    await execAsync(
+      `teamsfx provision`,
+      {
+        cwd: projectPath,
+        env: process.env,
+        timeout: 0
+      }
+    );
+
+    console.log(`[Successfully] provision for ${projectPath}`);
+
+    {
+      // Validate provision
+      // Get context
+      const context = await fs.readJSON(`${projectPath}/.fx/env.default.json`);
+
+      // Validate Aad App
+      const aad = AadValidator.init(context, false, AppStudioLogin);
+      await AadValidator.validate(aad);
+
+      // Validate Simple Auth
+      const simpleAuth = SimpleAuthValidator.init(context);
+      await SimpleAuthValidator.validate(simpleAuth, aad);
+
+      // Validate Function App
+      const func = FunctionValidator.init(context);
+      await FunctionValidator.validateProvision(func, false);
+    }
+
+    // deploy
+    await execAsync(
+      `teamsfx deploy bot`,
+      {
+        cwd: projectPath,
+        env: process.env,
+        timeout: 0
+      }
+    );
+    console.log(`[Successfully] deploy for ${projectPath}`);
+
+    {
+      // Validate deployment
+
+      // Get context
+      const context = await fs.readJSON(`${projectPath}/.fx/env.default.json`);
+
+      // Validate Function App
+      const func = FunctionValidator.init(context);
+      await FunctionValidator.validateDeploy(func);
+    }
+
+
+    // test (validate)
+    await execAsync(
+      `teamsfx test`,
+      {
+        cwd: projectPath,
+        env: process.env,
+        timeout: 0
+      }
+    );
+
+    {
+      /// TODO: add check for validate
+    }
+
+    // build
+    await execAsync(
+      `teamsfx build`,
+      {
+        cwd: projectPath,
+        env: process.env,
+        timeout: 0
+      }
+    );
+
+    {
+      /// TODO: add check for build
+    }
+
+    // publish
+    await execAsync(
+      `teamsfx publish`,
+      {
+        cwd: projectPath,
+        env: process.env,
+        timeout: 0
+      }
+    );
+
+    {
+      /// TODO: add check for publish
+    }
+  });
+
+  after(async () => {
+    // clean up
+    console.log(`[Successfully] start to clean up for ${projectPath}`);
+    await cleanUp(appName, projectPath);
+  });
+});

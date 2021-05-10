df243ca16709c39163ab694668e4c0470ed58422
Qianhao Dong <qidon@microsoft.com>
2021-05-10 17:14:35 +0800

    chore: clean up unused code

--- packages/api/tests/telemetry.test.ts
+++ packages/api/tests/telemetry.test.ts
@@ -5,7 +5,7 @@
 import "mocha";
 import chai from "chai";
 import spies from "chai-spies";
-import {TelemetryReporter, PluginTelemetryReporter} from "../src/utils/telemetry";
+import { TelemetryReporter } from "../src/utils/telemetry";
 
 chai.use(spies);
 const expect = chai.expect;
@@ -29,65 +29,18 @@ class MockTelemetryReporter implements TelemetryReporter {
 
 describe("telemetry", () => {
     let reporter: TelemetryReporter;
-    let proxy: TelemetryReporter;
 
     beforeEach(() => {
         reporter = new MockTelemetryReporter();
         sandbox.on(reporter, ["sendTelemetryErrorEvent", "sendTelemetryEvent", "sendTelemetryException"]);
-        proxy = new PluginTelemetryReporter(reporter, pluginName);
     });
 
     afterEach(() => {
         sandbox.restore();
     });
 
-    it("sendTelemetryEvent", () => {
-        proxy.sendTelemetryEvent("sampleEvent", {"stringProp": "some string"}, {"numericMeasure": 123});
-
-        expect(reporter.sendTelemetryEvent)
-            .to.have.been.called
-            .with(
-                pluginName + "-" + "sampleEvent",
-                {"stringProp": "some string", "pluginName": pluginName},
-                {"numericMeasure": 123}
-            );
-    });
-
-    it("sendTelemetryErrorEvent", () => {
-        proxy.sendTelemetryErrorEvent("sampleErrorEvent", {
-            "stringProp": "some string",
-            "stackProp": "some user stack trace"
-        }, {"numericMeasure": 123}, ["stackProp"]);
-
-        expect(reporter.sendTelemetryErrorEvent)
-            .to.have.been.called
-            .with(
-                pluginName + "-" + "sampleErrorEvent",
-                {
-                    "stringProp": "some string",
-                    "pluginName": pluginName,
-                    "stackProp": "some user stack trace"
-                },
-                {"numericMeasure": 123},
-                ["stackProp"]
-            );
-    });
-
-    it("sendTelemetryException", () => {
-        const error = new Error("error for test");
-        proxy.sendTelemetryException(error, {"stringProp": "some string"}, {"numericMeasure": 123});
-
-        expect(reporter.sendTelemetryException)
-            .to.have.been.called
-            .with(
-                error,
-                {"stringProp": "some string", "pluginName": pluginName},
-                {"numericMeasure": 123}
-            );
-    });
-
     it("invoke times", () => {
-        proxy.sendTelemetryEvent("sampleEvent", {"stringProp": "some string"}, {"numericMeasure": 123});
+        reporter.sendTelemetryEvent("sampleEvent", {"stringProp": "some string"}, {"numericMeasure": 123});
 
         expect(reporter.sendTelemetryEvent)
             .to.have.been.called.once;

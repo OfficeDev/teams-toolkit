5ffef46c1c2b5f56acd1328d1735d0eb6edf4aeb
Huajie Zhang <zhjay23@qq.com>
2021-05-10 22:29:41 +0800

    fix: 🐛 fix "provision" when "Publish to Teams" does not ask for input sql admin name and password (#833)
    * fix: 🐛 fix 9892082
    
    * fix: lock

--- packages/vscode-extension/src/handlers.ts
+++ packages/vscode-extension/src/handlers.ts
@@ -332,7 +332,19 @@ export async function runCommand(stage: Stage): Promise<Result<null, FxError>> {
       );
     }
   } catch (e) {
-    result = wrapError(e);
+    if(e instanceof UserError){
+      const ue:UserError = e as UserError;
+      if(ue.name === "DoProvisionFirst"){
+        runningTasks.delete(stage);
+        ExtTelemetry.sendTelemetryEvent(eventName, {
+          [TelemetryProperty.Success]: TelemetrySuccess.No
+        });
+        return await provisionHandler();
+      }
+    }
+    else {
+      result = wrapError(e);
+    }
   }
 
   // 7. unlock

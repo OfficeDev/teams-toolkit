5541e46243641e22e10736410340056a9dcb7a3d
Ivan Jobs <ruhe@microsoft.com>
2021-05-10 14:01:26 +0800

    chore(bot): adapt ut

--- packages/fx-core/tests/plugins/resource/bot/unit/utils/resourceNameFactory.test.ts
+++ packages/fx-core/tests/plugins/resource/bot/unit/utils/resourceNameFactory.test.ts
@@ -2,30 +2,12 @@
 // Licensed under the MIT license.
 import "mocha";
 import * as chai from "chai";
-import * as sinon from "sinon";
 
 import { ResourceNameFactory } from "../../../../../../src/plugins/resource/bot/utils/resourceNameFactory";
-import * as utils from "../../../../../../src/plugins/resource/bot/utils/common";
 
 describe("Resource Name Factory", () => {
     describe("createCommonName", () => {
-        it("Undefined ResourceNameSuffix", () => {
-            // Arrange
-            const appName = "demo0329";
-            const limit = 10;
-
-            sinon.stub(utils, "genUUID").returns("abcdefg");
-
-            // Act
-            const name = ResourceNameFactory.createCommonName(appName, undefined, limit);
-
-            // Assert
-            const expectName = "9btabcdefg";
-            chai.assert.lengthOf(name, limit);
-            chai.assert.isTrue(name === expectName);
-        });
-
-        it("Valid ResourceNameSuffix", () => {
+        it("Happy Path", () => {
             // Arrange
             const appName = "demo0329";
             const resourceNameSuffix = "abcdefg";

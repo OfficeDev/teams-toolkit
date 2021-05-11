83b07ad43f3be34bad11754bb7e7f2ff4cdcc29d
Aocheng Wang <aochengwang@microsoft.com>
2021-05-11 16:08:27 +0800

    fix: fix comments

--- packages/fx-core/src/plugins/resource/function/plugin.ts
+++ packages/fx-core/src/plugins/resource/function/plugin.ts
@@ -39,7 +39,7 @@ import { PostProvisionSteps, PreDeploySteps, ProvisionSteps, StepGroup, step } f
 import { functionNameQuestion } from "./questions";
 import { DotnetChecker } from "./utils/depsChecker/dotnetChecker";
 import { Messages, isLinux, dotnetManualInstallHelpLink } from "./utils/depsChecker/common";
-import { DepsCheckerError } from "./utils/depsChecker/errors";
+import { BackendExtensionsInstallError, DepsCheckerError } from "./utils/depsChecker/errors";
 import { getNodeVersion } from "./utils/node-version";
 import { funcPluginAdapter } from "./utils/depsChecker/funcPluginAdapter";
 import { funcPluginLogger } from "./utils/depsChecker/funcPluginLogger";
@@ -426,7 +426,7 @@ export class FunctionPluginImpl {
         // NOTE: make sure this step is before using `dotnet` command if you refactor this code.
         await this.handleDotnetChecker(ctx);
 
-        await this.handleBackendExtensionsInstall(workingPath, functionLanguage)
+        await this.handleBackendExtensionsInstall(workingPath, functionLanguage);
 
         await runWithErrorCatchAndThrow(new InstallNpmPackageError(), async () =>
             await step(StepGroup.PreDeployStepGroup, PreDeploySteps.npmPrepare, async () =>
@@ -603,13 +603,15 @@ export class FunctionPluginImpl {
     }
 
     private async handleBackendExtensionsInstall(workingPath: string, functionLanguage: FunctionLanguage): Promise<void> {
-        await step(StepGroup.PreDeployStepGroup, PreDeploySteps.installTeamsfxBinding, async () => {
-            try {
-                await FunctionDeploy.installFuncExtensions(workingPath, functionLanguage)
-            } catch (error) {
-                // wrap the original error to UserError so the extensibility model will pop-up a dialog correctly
-                funcPluginAdapter.handleDotnetError(error);
-            }
-        });
+        await runWithErrorCatchAndThrow(new InstallTeamsfxBindingError(), async () =>
+            await step(StepGroup.PreDeployStepGroup, PreDeploySteps.installTeamsfxBinding, async () => {
+                try {
+                    await FunctionDeploy.installFuncExtensions(workingPath, functionLanguage)
+                } catch (error) {
+                    // wrap the original error to UserError so the extensibility model will pop-up a dialog correctly
+                    funcPluginAdapter.handleDotnetError(error);
+                }
+            })
+        );
     }
 }

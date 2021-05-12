68ac9261bcb3e24546fad842338d35878f7d41b0
Xiaofu Huang <xiaofhua@microsoft.com>
2021-05-12 17:18:40 +0800

    fix(apim): change override to overridden

--- packages/fx-core/src/plugins/resource/apim/constants.ts
+++ packages/fx-core/src/plugins/resource/apim/constants.ts
@@ -144,7 +144,7 @@ export class ValidationConstants {
     public static readonly CLI = {
         invalidOptionMessage: (optionName: string) => `The value of option '--${optionName}' is invalid.`,
         emptyOptionMessage: (optionName: string) => `Option '--${optionName}' is required. Set the value of '--${optionName}'`,
-        overrideOptionMessage: (optionName: string) => `Option '--${optionName}' cannot be override. Remove option '--${optionName}'`,
+        overrideOptionMessage: (optionName: string) => `Option '--${optionName}' cannot be overridden. Remove option '--${optionName}'`,
     }
 }
 

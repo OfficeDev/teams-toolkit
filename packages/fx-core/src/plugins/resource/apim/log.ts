6df1de134033e29ac84dc5d3564cf36ccaad8f76
Xiaofu Huang <xiaofhua@microsoft.com>
2021-05-13 11:41:24 +0800

    fix(apim): remove upper first case

--- packages/fx-core/src/plugins/resource/apim/log.ts
+++ packages/fx-core/src/plugins/resource/apim/log.ts
@@ -2,7 +2,6 @@
 // Licensed under the MIT license.
 import { ApimDefaultValues, ProjectConstants } from "./constants";
 import { IName } from "./interfaces/IName";
-import { capitalizeFirstLetter } from "./utils/commonUtils";
 
 export class LogMessages {
     public static readonly operationStarts = (operation: IName, resourceType: IName, resourceId?: string): string =>
@@ -12,9 +11,8 @@ export class LogMessages {
 
     public static readonly operationSuccess = (operation: IName, resourceType: IName, resourceId?: string): string =>
         !resourceId
-            ? `[${ProjectConstants.pluginDisplayName}] Succeeded: ${capitalizeFirstLetter(operation.displayName)} ${resourceType.displayName}.`
-            : `[${ProjectConstants.pluginDisplayName}] Succeeded: ${capitalizeFirstLetter(operation.displayName)} ${resourceType.displayName
-            } '${resourceId}'.`;
+            ? `[${ProjectConstants.pluginDisplayName}] Succeeded: ${operation.displayName} ${resourceType.displayName}.`
+            : `[${ProjectConstants.pluginDisplayName}] Succeeded: ${operation.displayName} ${resourceType.displayName} '${resourceId}'.`;
 
     public static readonly operationFailed = (operation: IName, resourceType: IName, resourceId?: string): string =>
         !resourceId

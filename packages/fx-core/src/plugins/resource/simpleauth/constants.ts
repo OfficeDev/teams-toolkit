eddc1c8c40b8861a82e364733a1b29fd22700758
wenyt <75360946+wenytang-ms@users.noreply.github.com>
2021-05-13 10:42:17 +0800

    chore(CI): download simple auth from github release (#866)
    * chore(CI): download simple auth from github release
    
    * chore(fx-core): download version
    
    * chore(fx-core): remove simple auth constants not use
    
    * chore: update code
    
    * chore: update simple auth version file type

--- packages/fx-core/src/plugins/resource/simpleauth/constants.ts
+++ packages/fx-core/src/plugins/resource/simpleauth/constants.ts
@@ -55,6 +55,8 @@ export class Constants {
     static readonly ResourcesFolderName: string = path.join("templates", "plugins", "resource", "simpleauth");
     static readonly SimpleAuthFileName: string = "SimpleAuth.zip";
     static readonly SimpleAuthZipName = (version: string): string  => `Microsoft.TeamsFx.SimpleAuth_${version}.zip`;
+    static readonly SimpleAuthTag = (version: string): string => `simpleauth@${version}`;
+    static readonly SimpleAuthReleaseUrl = (tagName: string, fileName: string): string => `https://github.com/OfficeDev/TeamsFx/releases/download/${tagName}/${fileName}`;
     static readonly VersionFileName: string = "version.txt";
 
     static readonly ResourceNameMaxLength = 40;

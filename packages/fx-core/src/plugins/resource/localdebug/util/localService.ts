08bb5166457df3d39bf0c20b741caff419dbd140
Qianhao Dong <qidon@microsoft.com>
2021-05-14 14:21:39 +0800

    fix: update auth bits when new version (#1017)

--- packages/fx-core/src/plugins/resource/localdebug/util/localService.ts
+++ packages/fx-core/src/plugins/resource/localdebug/util/localService.ts
@@ -5,18 +5,44 @@
 import AdmZip from "adm-zip";
 import * as fs from "fs-extra";
 import * as os from "os";
+import * as path from "path";
 import { ConfigFolderName } from "@microsoft/teamsfx-api";
 
 export async function prepareLocalAuthService(zipPath: string): Promise<string> {
     const toolkitHome = `${os.homedir()}/.${ConfigFolderName}`;
+    const zipFolder = path.dirname(zipPath);
+    const versionPath = `${zipFolder}/version.txt`;
     const authServiceFolder = `${toolkitHome}/localauth`;
     const authServiceDll = `${authServiceFolder}/Microsoft.TeamsFx.SimpleAuth.dll`;
-    if (!await fs.pathExists(authServiceDll))
+    const authServiceVersion = `${toolkitHome}/localauth-version.txt`;
+
+    const expectedVersion = await tryGetVersion(versionPath);
+    const actualVersion = await tryGetVersion(authServiceVersion);
+    if (!expectedVersion || !actualVersion || expectedVersion !== actualVersion || !await fs.pathExists(authServiceDll))
     {
         const zip = new AdmZip(zipPath);
         await fs.ensureDir(authServiceFolder);
         zip.extractAllTo(authServiceFolder, true);
+
+        if (expectedVersion) {
+            await fs.writeFile(authServiceVersion, expectedVersion, "utf8");
+        }
     }
 
     return authServiceFolder;
+}
+
+async function tryGetVersion(versionFile: string): Promise<string | undefined> {
+    try {
+        if (await fs.pathExists(versionFile)) {
+            const version = (await fs.readFile(versionFile, "utf8")).trim();
+            return version.length > 0 ? version : undefined;
+        }
+
+        return undefined;
+    }
+    catch {
+        // ignore error and return undefine
+        return undefined;
+    }
 }

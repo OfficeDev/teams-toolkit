c93567fcd03f03e40c04f807929e206d0e0dad2d
Wang Yefu <yefuwang@microsoft.com>
2021-05-18 13:53:13 +0800

    fix(solution): change package name

--- packages/fx-core/tests/plugins/solution/solution.update.test.ts
+++ packages/fx-core/tests/plugins/solution/solution.update.test.ts
@@ -3,30 +3,40 @@
 import { expect } from "chai";
 import { it } from "mocha";
 import { TeamsAppSolution } from " ../../../src/plugins/solution";
-import { ConfigMap, SolutionConfig, SolutionContext, TeamsAppManifest } from "fx-api";
-import { GLOBAL_CONFIG, PROGRAMMING_LANGUAGE, SolutionError } from "../../../src/plugins/solution/fx-solution/constants";
-import { AzureSolutionQuestionNames, BotOptionItem } from "../../../src/plugins/solution/fx-solution/question";
+import {
+  ConfigMap,
+  SolutionConfig,
+  SolutionContext,
+  TeamsAppManifest,
+} from "@microsoft/teamsfx-api";
+import {
+  GLOBAL_CONFIG,
+  PROGRAMMING_LANGUAGE,
+  SolutionError,
+} from "../../../src/plugins/solution/fx-solution/constants";
+import {
+  AzureSolutionQuestionNames,
+  BotOptionItem,
+} from "../../../src/plugins/solution/fx-solution/question";
 
 function mockSolutionContext(): SolutionContext {
-    let config: SolutionConfig = new Map;
-    return {
-        root: ".",
-        app: new TeamsAppManifest,
-        config,
-        answers: new ConfigMap,
-        projectSettings: undefined
-    }
+  const config: SolutionConfig = new Map();
+  return {
+    root: ".",
+    app: new TeamsAppManifest(),
+    config,
+    answers: new ConfigMap(),
+    projectSettings: undefined,
+  };
 }
 
 describe("update()", () => {
-    let solution = new TeamsAppSolution();
-    it("should return error if answers is undefined", async () => {
-        let solution = new TeamsAppSolution();
-        let mockedCtx = mockSolutionContext();
-        mockedCtx.answers = undefined;
-        let result = await solution.create(mockedCtx);
-        expect(result.isErr()).equals(true);
-        expect(result._unsafeUnwrapErr().name).equals(SolutionError.InternelError);
-    });
-
+  it("should return internal error if answers is undefined", async () => {
+    const solution = new TeamsAppSolution();
+    const mockedCtx = mockSolutionContext();
+    mockedCtx.answers = undefined;
+    const result = await solution.create(mockedCtx);
+    expect(result.isErr()).equals(true);
+    expect(result._unsafeUnwrapErr().name).equals(SolutionError.InternelError);
+  });
 });

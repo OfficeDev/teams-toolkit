b22c92ce16adf5e1f8dca002fe2a8feabfcb716b
Meifans Zhao <pengfeizhao@microsoft.com>
2021-05-18 15:19:25 +0800

    feat(env-checker): fix missing env checker popup when run f5 from WSL (#1085)
    * feat: fix missing env checker popup when run f5 from WSL
    
    * fix: remove opn in vscode-extension
    
    * fix: use await
    
    * fix: update package-lock.json
    
    * chore: regen lock files
    
    Co-authored-by: LongOddCode <haolong@microsoft.com>

--- packages/fx-core/src/plugins/resource/function/utils/depsChecker/common.ts
+++ packages/fx-core/src/plugins/resource/function/utils/depsChecker/common.ts
@@ -10,15 +10,6 @@
 // to copy you changes to function plugin.
 
 import * as os from "os";
-const opn = require("opn");
-
-export async function openUrl(url: string): Promise<void> {
-  // Using this functionality is blocked by https://github.com/Microsoft/vscode/issues/25852
-  // Specifically, opening the Live Metrics Stream for Linux Function Apps doesn't work in this extension.
-  // await vscode.env.openExternal(vscode.Uri.parse(url));
-
-  opn(url);
-}
 
 export function isWindows(): boolean {
   return os.type() === "Windows_NT";

8ac87565379054451f753040647feb9ceb081d76
huajiezhang <huajiezhang@microsoft.com>
2021-05-20 14:08:08 +0800

    style: format

--- packages/vscode-extension/src/qm/vsc_ui.ts
+++ packages/vscode-extension/src/qm/vsc_ui.ts
@@ -9,7 +9,7 @@ import {
   QuickPick,
   QuickPickItem,
   Uri,
-  window,
+  window
 } from "vscode";
 import {
   FxInputBoxOption,
@@ -19,7 +19,7 @@ import {
   InputResultType,
   OptionItem,
   returnSystemError,
-  UserInterface,
+  UserInterface
 } from "@microsoft/teamsfx-api";
 import { ExtensionErrors, ExtensionSource } from "../error";
 import { ext } from "../extensionVariables";
@@ -35,7 +35,7 @@ export class VsCodeUI implements UserInterface {
 
     const okButton: QuickInputButton = {
       iconPath: Uri.file(ext.context.asAbsolutePath("media/ok.svg")),
-      tooltip: "ok",
+      tooltip: "ok"
     };
     const disposables: Disposable[] = [];
     try {
@@ -52,147 +52,149 @@ export class VsCodeUI implements UserInterface {
       // quickPick.step = option.step;
       // quickPick.totalSteps = option.totalSteps;
       let previousSelectedItems: FxQuickPickItem[] = [];
-      return await new Promise<InputResult>(async (resolve): Promise<void> => {
-        const onDidAccept = async () => {
-          let selectedItems = quickPick.selectedItems as FxQuickPickItem[];
-          if (option.canSelectMany) {
-            const strArray = Array.from(selectedItems.map((i) => i.id));
-            if (option.validation) {
-              const validateRes = await option.validation(strArray);
-              if (validateRes) {
-                return;
+      return await new Promise<InputResult>(
+        async (resolve): Promise<void> => {
+          const onDidAccept = async () => {
+            let selectedItems = quickPick.selectedItems as FxQuickPickItem[];
+            if (option.canSelectMany) {
+              const strArray = Array.from(selectedItems.map((i) => i.id));
+              if (option.validation) {
+                const validateRes = await option.validation(strArray);
+                if (validateRes) {
+                  return;
+                }
               }
+              let result: OptionItem[] | string[] = strArray;
+              if (option.returnObject) {
+                result = selectedItems.map((i) => {
+                  const item: OptionItem = {
+                    id: i.id,
+                    label: i.label,
+                    description: i.description,
+                    detail: i.detail,
+                    data: i.data
+                  };
+                  return item;
+                });
+              }
+              resolve({
+                type: InputResultType.sucess,
+                result: result
+              });
+            } else {
+              if (!selectedItems || selectedItems.length === 0)
+                selectedItems = [quickPick.items[0] as FxQuickPickItem];
+              const item: FxQuickPickItem = selectedItems[0] as FxQuickPickItem;
+              let result: string | OptionItem = item.id;
+              if (option.returnObject) {
+                result = {
+                  id: item.id,
+                  label: item.label,
+                  description: item.description,
+                  detail: item.detail,
+                  data: item.data
+                };
+              }
+              resolve({ type: InputResultType.sucess, result: result });
             }
-            let result: OptionItem[] | string[] = strArray;
-            if (option.returnObject) {
-              result = selectedItems.map((i) => {
-                const item: OptionItem = {
+          };
+
+          disposables.push(
+            quickPick.onDidAccept(onDidAccept),
+            quickPick.onDidHide(() => {
+              resolve({ type: InputResultType.cancel });
+            })
+          );
+          disposables.push(
+            quickPick.onDidTriggerButton((button) => {
+              if (button === QuickInputButtons.Back) resolve({ type: InputResultType.back });
+              else onDidAccept();
+            })
+          );
+          try {
+            const optionIsString = !!(typeof option.items[0] === "string");
+            /// set items
+            if (optionIsString) {
+              quickPick.items = (option.items as string[]).map((i: string) => {
+                return { label: i, id: i };
+              });
+            } else {
+              quickPick.items = (option.items as OptionItem[]).map((i: OptionItem) => {
+                return {
                   id: i.id,
                   label: i.label,
                   description: i.description,
                   detail: i.detail,
-                  data: i.data,
+                  data: i.data
                 };
-                return item;
               });
             }
-            resolve({
-              type: InputResultType.sucess,
-              result: result,
-            });
-          } else {
-            if (!selectedItems || selectedItems.length === 0)
-              selectedItems = [quickPick.items[0] as FxQuickPickItem];
-            const item: FxQuickPickItem = selectedItems[0] as FxQuickPickItem;
-            let result: string | OptionItem = item.id;
-            if (option.returnObject) {
-              result = {
-                id: item.id,
-                label: item.label,
-                description: item.description,
-                detail: item.detail,
-                data: item.data,
-              };
-            }
-            resolve({ type: InputResultType.sucess, result: result });
-          }
-        };
 
-        disposables.push(
-          quickPick.onDidAccept(onDidAccept),
-          quickPick.onDidHide(() => {
-            resolve({ type: InputResultType.cancel });
-          })
-        );
-        disposables.push(
-          quickPick.onDidTriggerButton((button) => {
-            if (button === QuickInputButtons.Back) resolve({ type: InputResultType.back });
-            else onDidAccept();
-          })
-        );
-        try {
-          const optionIsString = !!(typeof option.items[0] === "string");
-          /// set items
-          if (optionIsString) {
-            quickPick.items = (option.items as string[]).map((i: string) => {
-              return { label: i, id: i };
-            });
-          } else {
-            quickPick.items = (option.items as OptionItem[]).map((i: OptionItem) => {
-              return {
-                id: i.id,
-                label: i.label,
-                description: i.description,
-                detail: i.detail,
-                data: i.data,
-              };
-            });
-          }
-
-          const items = quickPick.items as FxQuickPickItem[];
-          const optionMap = new Map<string, FxQuickPickItem>();
-          for (const item of items) {
-            optionMap.set(item.id, item);
-          }
-
-          /// set default values
-          if (option.defaultValue) {
             const items = quickPick.items as FxQuickPickItem[];
-            if (option.canSelectMany) {
-              const ids = option.defaultValue as string[];
-              quickPick.selectedItems = ids.map((id) => optionMap.get(id)!);
-              previousSelectedItems = ids.map((id) => optionMap.get(id)!);
-            } else {
-              const defaultStringValue = option.defaultValue as string;
-              const newitems = items.filter((i) => i.id !== defaultStringValue);
-              for (const i of items) {
-                if (i.id === defaultStringValue) {
-                  newitems.unshift(i);
-                  break;
-                }
-              }
-              quickPick.items = newitems;
+            const optionMap = new Map<string, FxQuickPickItem>();
+            for (const item of items) {
+              optionMap.set(item.id, item);
             }
-          }
 
-          if (option.onDidChangeSelection) {
-            const changeHandler = async function (items: QuickPickItem[]): Promise<any> {
-              const optionItems: OptionItem[] = items.map((i) => {
-                const fxitem: FxQuickPickItem = i as FxQuickPickItem;
-                return {
-                  id: fxitem.id,
-                  label: fxitem.label,
-                  description: fxitem.description,
-                  detail: fxitem.detail,
-                  data: fxitem.data,
-                };
-              });
-              const oldIds = quickPick.selectedItems
-                .map((i) => {
-                  return (i as FxQuickPickItem).id;
-                })
-                .sort();
-              if (option.onDidChangeSelection) {
-                const newIds: string[] = (
-                  await option.onDidChangeSelection(optionItems, previousSelectedItems)
-                ).sort();
-                previousSelectedItems = newIds.map((id) => optionMap.get(id)!);
-                if (oldIds.join(",") !== newIds.join(",")) {
-                  quickPick.selectedItems = newIds.map((id) => optionMap.get(id)!);
+            /// set default values
+            if (option.defaultValue) {
+              const items = quickPick.items as FxQuickPickItem[];
+              if (option.canSelectMany) {
+                const ids = option.defaultValue as string[];
+                quickPick.selectedItems = ids.map((id) => optionMap.get(id)!);
+                previousSelectedItems = ids.map((id) => optionMap.get(id)!);
+              } else {
+                const defaultStringValue = option.defaultValue as string;
+                const newitems = items.filter((i) => i.id !== defaultStringValue);
+                for (const i of items) {
+                  if (i.id === defaultStringValue) {
+                    newitems.unshift(i);
+                    break;
+                  }
                 }
+                quickPick.items = newitems;
               }
-            };
-            disposables.push(quickPick.onDidChangeSelection(changeHandler));
-          }
+            }
 
-          quickPick.show();
-        } catch (err) {
-          resolve({
-            type: InputResultType.error,
-            error: returnSystemError(err, ExtensionSource, ExtensionErrors.UnknwonError),
-          });
+            if (option.onDidChangeSelection) {
+              const changeHandler = async function(items: QuickPickItem[]): Promise<any> {
+                const optionItems: OptionItem[] = items.map((i) => {
+                  const fxitem: FxQuickPickItem = i as FxQuickPickItem;
+                  return {
+                    id: fxitem.id,
+                    label: fxitem.label,
+                    description: fxitem.description,
+                    detail: fxitem.detail,
+                    data: fxitem.data
+                  };
+                });
+                const oldIds = quickPick.selectedItems
+                  .map((i) => {
+                    return (i as FxQuickPickItem).id;
+                  })
+                  .sort();
+                if (option.onDidChangeSelection) {
+                  const newIds: string[] = (
+                    await option.onDidChangeSelection(optionItems, previousSelectedItems)
+                  ).sort();
+                  previousSelectedItems = newIds.map((id) => optionMap.get(id)!);
+                  if (oldIds.join(",") !== newIds.join(",")) {
+                    quickPick.selectedItems = newIds.map((id) => optionMap.get(id)!);
+                  }
+                }
+              };
+              disposables.push(quickPick.onDidChangeSelection(changeHandler));
+            }
+
+            quickPick.show();
+          } catch (err) {
+            resolve({
+              type: InputResultType.error,
+              error: returnSystemError(err, ExtensionSource, ExtensionErrors.UnknwonError)
+            });
+          }
         }
-      });
+      );
     } finally {
       disposables.forEach((d) => {
         d.dispose();
@@ -203,7 +205,7 @@ export class VsCodeUI implements UserInterface {
   async showInputBox(option: FxInputBoxOption): Promise<InputResult> {
     const okButton: QuickInputButton = {
       iconPath: Uri.file(ext.context.asAbsolutePath("media/ok.svg")),
-      tooltip: "ok",
+      tooltip: "ok"
     };
     const disposables: Disposable[] = [];
     try {
@@ -220,13 +222,13 @@ export class VsCodeUI implements UserInterface {
       // inputBox.step = option.step;
       // inputBox.totalSteps = option.totalSteps;
       if (option.number) {
-        const numberValidation = async function (input: string): Promise<string | undefined> {
+        const numberValidation = async function(input: string): Promise<string | undefined> {
           if (!input || input.trim() === "" || isNaN(Number(input)))
             return `'${input}' is not a valid number`;
           return undefined;
         };
         const oldValidation = option.validation;
-        const newValidation = async function (input: string): Promise<string | undefined> {
+        const newValidation = async function(input: string): Promise<string | undefined> {
           const res = oldValidation ? await oldValidation(input) : undefined;
           if (res !== undefined) return res;
           return await numberValidation(input);
@@ -278,7 +280,7 @@ export class VsCodeUI implements UserInterface {
   async showOpenDialog(option: FxOpenDialogOption): Promise<InputResult> {
     const okButton: QuickInputButton = {
       iconPath: Uri.file(ext.context.asAbsolutePath("media/ok.svg")),
-      tooltip: "ok",
+      tooltip: "ok"
     };
     const disposables: Disposable[] = [];
     let fileSelectorIsOpen = false;
@@ -294,63 +296,65 @@ export class VsCodeUI implements UserInterface {
       quickPick.canSelectMany = false;
       // quickPick.step = option.step;
       // quickPick.totalSteps = option.totalSteps;
-      const res = await new Promise<InputResult>(async (resolve): Promise<void> => {
-        const onDidAccept = async () => {
-          const result = quickPick.items[0].detail;
-          if (result && result.length > 0)
-            resolve({ type: InputResultType.sucess, result: result });
-        };
+      const res = await new Promise<InputResult>(
+        async (resolve): Promise<void> => {
+          const onDidAccept = async () => {
+            const result = quickPick.items[0].detail;
+            if (result && result.length > 0)
+              resolve({ type: InputResultType.sucess, result: result });
+          };
 
-        const onDidChangeSelection = async function (items: QuickPickItem[]): Promise<any> {
-          const defaultUrl = items[0].detail;
+          const onDidChangeSelection = async function(items: QuickPickItem[]): Promise<any> {
+            const defaultUrl = items[0].detail;
+            fileSelectorIsOpen = true;
+            const uri = await window.showOpenDialog({
+              defaultUri: defaultUrl ? Uri.file(defaultUrl) : undefined,
+              canSelectFiles: false,
+              canSelectFolders: true,
+              canSelectMany: false,
+              title: option.title
+            });
+            fileSelectorIsOpen = false;
+            const res = uri && uri.length > 0 ? uri[0].fsPath : undefined;
+            if (res) {
+              quickPick.items = [{ label: "Select the workspace folder", detail: res }];
+              resolve({ type: InputResultType.sucess, result: res });
+            }
+          };
+
+          disposables.push(
+            // quickPick.onDidAccept(onDidAccept),
+            quickPick.onDidHide(() => {
+              if (fileSelectorIsOpen === false) resolve({ type: InputResultType.cancel });
+            }),
+            quickPick.onDidTriggerButton((button) => {
+              if (button === QuickInputButtons.Back) resolve({ type: InputResultType.back });
+              else onDidAccept();
+            }),
+            quickPick.onDidChangeSelection(onDidChangeSelection)
+          );
+
+          quickPick.items = [{ label: "Select the workspace folder", detail: option.defaultUri }];
+          quickPick.show();
+
+          // pop up the dialog automatically
           fileSelectorIsOpen = true;
           const uri = await window.showOpenDialog({
-            defaultUri: defaultUrl ? Uri.file(defaultUrl) : undefined,
+            defaultUri: option.defaultUri ? Uri.file(option.defaultUri) : undefined,
             canSelectFiles: false,
             canSelectFolders: true,
             canSelectMany: false,
-            title: option.title,
+            title: option.title
           });
+
           fileSelectorIsOpen = false;
           const res = uri && uri.length > 0 ? uri[0].fsPath : undefined;
           if (res) {
-            quickPick.items = [{ label: "Select the workspace folder", detail: res }];
+            quickPick.items = [{ label: "path", detail: res }];
             resolve({ type: InputResultType.sucess, result: res });
           }
-        };
-
-        disposables.push(
-          // quickPick.onDidAccept(onDidAccept),
-          quickPick.onDidHide(() => {
-            if (fileSelectorIsOpen === false) resolve({ type: InputResultType.cancel });
-          }),
-          quickPick.onDidTriggerButton((button) => {
-            if (button === QuickInputButtons.Back) resolve({ type: InputResultType.back });
-            else onDidAccept();
-          }),
-          quickPick.onDidChangeSelection(onDidChangeSelection)
-        );
-
-        quickPick.items = [{ label: "Select the workspace folder", detail: option.defaultUri }];
-        quickPick.show();
-
-        // pop up the dialog automatically
-        fileSelectorIsOpen = true;
-        const uri = await window.showOpenDialog({
-          defaultUri: option.defaultUri ? Uri.file(option.defaultUri) : undefined,
-          canSelectFiles: false,
-          canSelectFolders: true,
-          canSelectMany: false,
-          title: option.title,
-        });
-
-        fileSelectorIsOpen = false;
-        const res = uri && uri.length > 0 ? uri[0].fsPath : undefined;
-        if (res) {
-          quickPick.items = [{ label: "path", detail: res }];
-          resolve({ type: InputResultType.sucess, result: res });
         }
-      });
+      );
       return res;
     } finally {
       disposables.forEach((d) => {

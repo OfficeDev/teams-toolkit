96b6343b221d1e9fd336866f88d1f40d20f8e3d2
Heng Zhang <74335596+henzhang-ms@users.noreply.github.com>
2021-05-25 09:51:41 +0800

    feat(templates): add daily/prerelease steps for templates in cd (#1167)

--- .github/scripts/template-zip-autogen.sh
+++ .github/scripts/template-zip-autogen.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
 set -x
 
-RAW_TAG=$1
-
-# A sample of raw tag is refs/tags/templates@0.1.0
-RAW_TAG_INFO=($(echo $RAW_TAG | tr "/" "\n"))
-TAG=${RAW_TAG_INFO[2]}
-TAG_INFO=($(echo $TAG | tr "@" "\n"))
-TAG_PREFIX=${TAG_INFO[0]}
-VERSION=${TAG_INFO[1]}
-
-if [[ ${TAG_PREFIX} != "templates" ]]; then
-    echo "Invalid tag prefix: ${TAG_PREFIX}."
-    exit -1
-fi
-
 LANGUAGE_LIST=(js ts)
 
 TEMPLATE_LIST=(

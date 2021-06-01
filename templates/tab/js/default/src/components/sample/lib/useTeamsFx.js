5566ef7f3f9a121e65a5acacb8cf017af9d9aa6f
Ning Tang <tecton69@gmail.com>
2021-06-01 16:35:30 +0800

    chore(template): prevent too many sdk logs by setting Error log level

--- templates/tab/js/default/src/components/sample/lib/useTeamsFx.js
+++ templates/tab/js/default/src/components/sample/lib/useTeamsFx.js
@@ -1,4 +1,4 @@
-import { loadConfiguration, ResourceType } from "@microsoft/teamsfx";
+import { loadConfiguration, ResourceType, LogLevel, setLogLevel } from "@microsoft/teamsfx";
 import { useData } from "./useData";
 import { useTeams } from "msteams-react-base-component";
 
@@ -14,6 +14,7 @@ export function useTeamsFx() {
   const [result] = useTeams({});
   const { error, loading } = useData(async () => {
     if (!initialized) {
+      setLogLevel(LogLevel.Error);
       loadConfiguration({
         authentication: {
           initiateLoginEndpoint: startLoginPageUrl,

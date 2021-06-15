5042467e20f5b85dbfd79b612859f6fb4f45694a
huajiezhang <huajiezhang@microsoft.com>
2021-06-15 16:29:41 +0800

    test(core): compatible for ui change

--- packages/fx-core/tests/core/utils.ts
+++ packages/fx-core/tests/core/utils.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { Solution, SolutionContext,FxError, Result, QTreeNode, Func, Stage, LogProvider, LogLevel, AzureAccountProvider, GraphTokenProvider, Tools, TokenProvider, AppStudioTokenProvider, TelemetryReporter, Dialog, UserInteraction, DialogMsg, IProgressHandler, SingleSelectConfig, MultiSelectConfig, InputTextConfig, SelectFileConfig, SelectFilesConfig, SelectFolderConfig, SingleSelectResult, MultiSelectResult, InputTextResult, SelectFileResult, SelectFilesResult, SelectFolderResult, RunnableTask, TaskConfig, SubscriptionInfo, Inputs, ProjectSettings, AzureSolutionSettings, ok, Void, ConfigMap, DialogType } from "@microsoft/teamsfx-api";
+import { Solution, SolutionContext,FxError, Result, QTreeNode, Func, Stage, LogProvider, LogLevel, AzureAccountProvider, GraphTokenProvider, Tools, TokenProvider, AppStudioTokenProvider, TelemetryReporter, Dialog, UserInteraction, DialogMsg, IProgressHandler, SingleSelectConfig, MultiSelectConfig, InputTextConfig, SelectFileConfig, SelectFilesConfig, SelectFolderConfig, SingleSelectResult, MultiSelectResult, InputTextResult, SelectFileResult, SelectFilesResult, SelectFolderResult, RunnableTask, TaskConfig, SubscriptionInfo, Inputs, ProjectSettings, AzureSolutionSettings, ok, Void, ConfigMap, DialogType, Colors } from "@microsoft/teamsfx-api";
 import {TokenCredential} from "@azure/core-auth";
 import {TokenCredentialsBase} from "@azure/ms-rest-nodeauth";
 import { SolutionLoader } from "../../src/core/loader";
@@ -226,14 +226,28 @@ class  MockUserInteraction implements UserInteraction {
   openUrl(link: string): Promise<Result<boolean,FxError>>{
     throw new Error("Method not implemented.");
   }
-  showMessage(
+  async showMessage(
     level: "info" | "warn" | "error",
     message: string,
     modal: boolean,
     ...items: string[]
+  ): Promise<Result<string|undefined,FxError>>;
+
+  async showMessage(
+    level: "info" | "warn" | "error",
+    message: Array<{content: string, color: Colors}>,
+    modal: boolean,
+    ...items: string[]
+  ): Promise<Result<string|undefined,FxError>>;
+
+  async showMessage(
+    level: "info" | "warn" | "error",
+    message: string | Array<{content: string, color: Colors}>,
+    modal: boolean,
+    ...items: string[]
   ): Promise<Result<string|undefined,FxError>>{
     throw new Error("Method not implemented.");
-  }
+  } 
   runWithProgress<T>(task: RunnableTask<T>, config: TaskConfig, ...args:any): Promise<Result<T,FxError>>{
     throw new Error("Method not implemented.");
   }

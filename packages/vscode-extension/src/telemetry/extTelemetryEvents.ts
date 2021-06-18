789c6e8524fdaf3b4b7349a69b45d2c1b7e0194d
Tian Yuan <tianyuan@microsoft.com>
2021-06-18 10:10:13 +0800

    feat: add extension logout telemetry (#1364)
    * feat: add extension logout telemetry
    
    * fix: pr comments

--- packages/vscode-extension/src/telemetry/extTelemetryEvents.ts
+++ packages/vscode-extension/src/telemetry/extTelemetryEvents.ts
@@ -11,6 +11,7 @@ export enum TelemetryEvent {
   Login = "login",
 
   SignOutStart = "sign-out-start",
+  SignOut = "sign-out",
 
   CreateProjectStart = "create-project-start",
   CreateProject = "create-project",

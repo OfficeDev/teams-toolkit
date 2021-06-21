0420a1ed67bd6bbd7a0d0b0b0b66bbdbe9a5f2de
Kuojian Lu <kuojianlu@gmail.com>
2021-06-21 16:18:29 +0800

    feat: reuse spawn callback for simplicity

--- packages/cli/src/cmds/preview/task.ts
+++ packages/cli/src/cmds/preview/task.ts
@@ -1,10 +1,5 @@
 import { ChildProcess, spawn, SpawnOptions } from "child_process";
 
-function delay(ms: number) {
-    // tslint:disable-next-line no-string-based-set-timeout
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 interface TaskOptions {
     cwd?: string;
     env?: NodeJS.ProcessEnv,
@@ -18,70 +13,86 @@ export interface TaskResult {
 }
 
 export class Task {
-    private task: ChildProcess;
-    private stdout: string = "";
-    private stderr: string = "";
+    private command: string;
+    private options: TaskOptions;
 
     constructor(command: string, options: TaskOptions) {
-        const spawnOptions: SpawnOptions = {
-            shell: true,
-            cwd: options.cwd,
-            env: options.env,
-        };
-        this.task = spawn(command, spawnOptions);
-        this.task.stdout?.on("data", (data) => {
-            // TODO: log
-            this.stdout += data;
-        });
-        this.task.stderr?.on("data", (data) => {
-            // TODO: log
-            this.stderr += data;
-        });
+        this.command = command;
+        this.options = options;
     }
 
     /**
      * wait for the task to end
      */
     public async wait(): Promise<TaskResult> {
-        while (true) {
-            if (this.task.exitCode !== null) {
+        const spawnOptions: SpawnOptions = {
+            shell: true,
+            cwd: this.options.cwd,
+            env: this.options.env,
+        };
+        const task = spawn(this.command, spawnOptions);
+        let stdout = "", stderr = "";
+        return new Promise((resolve, reject) => {
+            task.stdout?.on("data", (data) => {
+                // TODO: log
+                stdout += data;
+            });
+            task.stderr?.on("data", (data) => {
+                // TODO: log
+                stderr += data;
+            });
+            task.on("exit", () => {
                 const result: TaskResult = {
-                    success: this.task.exitCode === 0,
-                    stdout: this.stdout,
-                    stderr: this.stderr,
-                    exitCode: this.task.exitCode,
+                    success: task.exitCode === 0,
+                    stdout: stdout,
+                    stderr: stderr,
+                    exitCode: task.exitCode,
                 };
-                return result;
-            }
-            await delay(1000);
-        }
+                resolve(result);
+            });
+        });
     }
 
     /**
      * wait until stdout of the task matches the pattern or the task ends
      */
     public async waitFor(pattern: RegExp): Promise<TaskResult> {
-        while (true) {
-            if (this.task.exitCode != null) {
+        const spawnOptions: SpawnOptions = {
+            shell: true,
+            cwd: this.options.cwd,
+            env: this.options.env,
+        };
+        const task = spawn(this.command, spawnOptions);
+        let stdout = "", stderr = "";
+        return new Promise((resolve, reject) => {
+            task.stdout?.on("data", (data) => {
+                // TODO: log
+                stdout += data;
+                const match = pattern.test(stdout);
+                if (match) {
+                    const result: TaskResult = {
+                        success: true,
+                        stdout: stdout,
+                        stderr: stderr,
+                        exitCode: null,
+                    };
+                    resolve(result);
+                }
+            });
+            task.stderr?.on("data", (data) => {
+                // TODO: log
+                stderr += data;
+            });
+
+            task.on("exit", () => {
                 const result: TaskResult = {
                     success: false,
-                    stdout: this.stdout,
-                    stderr: this.stderr,
-                    exitCode: this.task.exitCode,
+                    stdout: stdout,
+                    stderr: stderr,
+                    exitCode: task.exitCode,
                 };
-                return result;
-            }
-            const match = pattern.test(this.stdout);
-            if (match) {
-                const result: TaskResult = {
-                    success: true,
-                    stdout: this.stdout,
-                    stderr: this.stderr,
-                    exitCode: null,
-                }
-                return result;
-            }
-            await delay(1000);
-        }
+                resolve(result);
+            });
+        });
     }
 }

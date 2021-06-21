8fd1bccbcc06c91c661358cebd7b2b14b5e19398
huajiezhang <huajiezhang@microsoft.com>
2021-06-21 12:34:18 +0800

    test(api): manifest ut

--- packages/api/tests/manifest.test.ts
+++ packages/api/tests/manifest.test.ts
@@ -8,22 +8,6 @@ import { assert } from "chai";
 
 
 describe("Manifest", () => {
-  
-  it("IDeveloper", () => {
-     const developer:IDeveloper = {
-      name:"MS",
-      websiteUrl: "http://test.com",
-      privacyUrl: "http://test.com",
-      termsOfUseUrl: "http://test.com"
-     };
-     assert.deepEqual(developer, {
-      name:"MS",
-      websiteUrl: "http://test.com",
-      privacyUrl: "http://test.com",
-      termsOfUseUrl: "http://test.com"
-     })
-  });
-
   it("TeamsAppManifest", () => {
     const manifest:TeamsAppManifest = new TeamsAppManifest();
     assert.isTrue(manifest.manifestVersion === "1.8");

8b6e56b54114e4236a2acdd5eca8bafd615d1d57
Kuojian Lu <kuojianlu@gmail.com>
2021-06-22 16:49:33 +0800

    feat(cli): init preview command (#1399)
    * feat(cli): init preview command

---
+++ packages/cli/src/cmds/preview/constants.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+"use strict";
+
+export const settingsFileName = "settings.json";
+
+export const FrontendHostingPluginName = "fx-resource-frontend-hosting";
+export const FunctionPluginName = "fx-resource-function";
+export const BotPluginName = "fx-resource-bot";
--- packages/cli/src/cmds/preview/task.ts
+++ packages/cli/src/cmds/preview/task.ts
@@ -1,3 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+"use strict";
+
 import { spawn, SpawnOptions } from "child_process";
 
 interface TaskOptions {

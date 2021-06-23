31c16d3fd5f067033c91389ce145376d77a6258d
huajiezhang <huajiezhang@microsoft.com>
2021-06-23 15:48:07 +0800

    docs(api): comments for validation.ts

--- packages/api/src/qm/validation.ts
+++ packages/api/src/qm/validation.ts
@@ -1,51 +1,108 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-import * as jsonschema from "jsonschema";  
+import * as jsonschema from "jsonschema";
 import { Inputs } from "../types";
-  
+
 
 
 /**
  * Validation for Any Instance Type
  * JSON Schema Validation reference: http://json-schema.org/draft/2019-09/json-schema-validation.html
  */
- export interface StaticValidation {
-  required?: boolean; // default value is true
+export interface StaticValidation {
+  /**
+   * whether the value is required or not, default value is true if it is undefined
+   */
+  required?: boolean;
+  /**
+   * An instance validates successfully against this keyword if its value is equal to the value of the keyword.
+   */
   equals?: unknown;
 }
 
 /**
-* //Validation for Strings
+* Validation for Strings
 */
 export interface StringValidation extends StaticValidation {
+  /**
+   * A string instance is valid against this keyword if its length is less than, or equal to, the value of this keyword.
+   */
   maxLength?: number;
+  /**
+   * A string instance is valid against this keyword if its length is greater than, or equal to, the value of this keyword.
+   */
   minLength?: number;
+  /**
+   * A string instance is considered valid if the regular expression matches the instance successfully.
+   */
   pattern?: string;
+  /**
+   * A string instance validates successfully against this keyword if its value is equal to one of the elements in this keyword's array value.
+   */
   enum?: string[]; // the value must be contained in this list
-  startsWith?: string; //non-standard
-  endsWith?: string; //non-standard
-  includes?: string; //non-standard
-  equals?: string; //non-standard
+  /**
+   * A string instance is valid against this keyword if the string starts with the value of this keyword.
+   */
+  startsWith?: string;
+  /**
+   * A string instance is valid against this keyword if the string ends with the value of this keyword.
+   */
+  endsWith?: string;
+  /**
+   * A string instance is valid against this keyword if the string contains the value of this keyword.
+   */
+  includes?: string;
+  /**
+   * An instance validates successfully against this keyword if its value is equal to the value of the keyword.
+   */
+  equals?: string;
 }
 
 /**
 * Validation for String Arrays
 */
 export interface StringArrayValidation extends StaticValidation {
+  /**
+   * The value of this keyword MUST be a non-negative integer.
+   * An array instance is valid against "maxItems" if its size is less than, or equal to, the value of this keyword.
+   */
   maxItems?: number;
+  /**
+   * The value of this keyword MUST be a non-negative integer.
+   * An array instance is valid against "minItems" if its size is greater than, or equal to, the value of this keyword.
+   */
   minItems?: number;
+  /**
+   * If this keyword has boolean value false, the instance validates successfully. If it has boolean value true, the instance validates successfully if all of its elements are unique.
+   */
   uniqueItems?: boolean;
-  equals?: string[]; //non-standard
-  enum?: string[]; // non-standard all the values must be contained in this list
-  contains?: string; ////non-standard
-  containsAll?: string[]; ///non-standard, the values must contains all items in the array
+  /**
+   * An instance validates successfully against this string array if they have the exactly the same elements.
+   */
+  equals?: string[];
+  /**
+   * An array instance is valid against "enum" array if all of the elements of the array is contained in the `enum` array.
+   */
+  enum?: string[];
+
+  /**
+   * An array instance is valid against "contains" if it contains the value of `contains`
+   */
+  contains?: string;
+  /**
+   * An array instance is valid against "containsAll" array if it contains all of the elements of `containsAll` array.
+   */
+  containsAll?: string[];
+  /**
+   * An array instance is valid against "containsAny" array if it contains any one of the elements of `containsAny` array.
+   */
   containsAny?: string[]; ///non-standard, the values must contains any one in the array
 }
 
 /**
 * The validation is checked by a validFunc provided by user
 */
-export interface FuncValidation<T extends string|string[]|undefined> {
+export interface FuncValidation<T extends string | string[] | undefined> {
   /**
    * A function that will be called to validate input and to give a hint to the user.
    *
@@ -53,11 +110,11 @@ export interface FuncValidation<T extends string|string[]|undefined> {
    * @return A human-readable string which is presented as diagnostic message.
    * Return `undefined` when 'value' is valid.
    */
-  validFunc: (input: T, previousInputs?: Inputs) => string | undefined|Promise<string | undefined>;
+  validFunc: (input: T, previousInputs?: Inputs) => string | undefined | Promise<string | undefined>;
 }
 
 /**
- * Definition of validation schema
+ * Definition of validation schema, which is a union of `StringValidation`, `StringArrayValidation` and `FuncValidation<any>`
  */
 export type ValidationSchema =
   | StringValidation
@@ -65,16 +122,30 @@ export type ValidationSchema =
   | FuncValidation<any>;
 
 
-export function getValidationFunction<T extends string|string[]|undefined>(
+/**
+ * A function to return a validation function according the validation schema
+ * @param validation validation schema
+ * @param inputs object to carry all user inputs
+ * @returns a validation function
+ */
+export function getValidationFunction<T extends string | string[] | undefined>(
   validation: ValidationSchema,
   inputs: Inputs
-): (input: T)  => string | undefined|Promise<string | undefined> {
-  return function(input: T): string | undefined | Promise<string | undefined> {
+): (input: T) => string | undefined | Promise<string | undefined> {
+  return function (input: T): string | undefined | Promise<string | undefined> {
     return validate(validation, input, inputs);
   };
 }
 
-export async function validate<T extends string|string[]|undefined>(
+/**
+ * Implementation of validation function
+ * @param validSchema validation schema
+ * @param value value to validate
+ * @param inputs user inputs object, which works as the context of the validation
+ * @returns A human-readable string which is presented as diagnostic message.
+   * Return `undefined` when 'value' is valid.
+ */
+export async function validate<T extends string | string[] | undefined>(
   validSchema: ValidationSchema,
   value: T,
   inputs?: Inputs
@@ -87,9 +158,9 @@ export async function validate<T extends string|string[]|undefined>(
       return res as string;
     }
   }
-  
-  if(!value){
-    if((validSchema as StaticValidation).required === true)
+
+  if (!value) {
+    if ((validSchema as StaticValidation).required === true)
       return `input value is required.`;
   }
 

7b60fcf6b054414c3f5136ea11b655061e93fe1f
Yitong Feng <yife@microsoft.com>
2021-06-30 15:15:51 +0800

    refactor(bot): merge bot-msgext template code automatically

--- .github/scripts/template-zip-autogen.sh
+++ .github/scripts/template-zip-autogen.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -x
 
-python ./.github/scripts/merge.py
+# Generate code from mustache template
+# Download
+curl -sSL https://git.io/get-mo -o mo
+# Make executable
+chmod +x mo
 
+TEMPLATE_FILE_PREFIX=./templates/mustache-templates/teamsBot
 LANGUAGE_LIST=(js ts)
 
 TEMPLATE_LIST=(
@@ -33,6 +38,24 @@ for LANGUAGE in ${LANGUAGE_LIST[@]}; do
         if [ ! -d ./templates/${SCOPE}/${LANGUAGE}/${SCENARIO} ]; then
             echo "The folder ./templates/${SCOPE}/${LANGUAGE}/${SCENARIO} does not exist."
             exit -1
+        fi
+
+        # Copy bot code to msgext-bot, except readme and images
+        rsync -avz --recursive --exclude "*.md" --exclude "*images/*" ./templates/bot/ ./templates/bot-msgext/
+        
+        # Generate code from Mustache templates
+        if [ ${SCOPE} == "bot" ]; then           
+            export IS_BOT=true
+            mo ${TEMPLATE_FILE_PREFIX}.${LANGUAGE}.mustache > ./templates/${SCOPE}/${LANGUAGE}/${SCENARIO}/teamsBot.${LANGUAGE}
+        fi
+        if [ ${SCOPE} == "msgext" ]; then
+            export IS_ME=true
+            mo ${TEMPLATE_FILE_PREFIX}.${LANGUAGE}.mustache > ./templates/${SCOPE}/${LANGUAGE}/${SCENARIO}/messageExtensionBot.${LANGUAGE}
+        fi
+        if [ ${SCOPE} == "bot-msgext" ]; then
+            export IS_ME=true
+            export IS_BOT=true
+            mo ${TEMPLATE_FILE_PREFIX}.${LANGUAGE}.mustache > ./templates/${SCOPE}/${LANGUAGE}/${SCENARIO}/teamsBot.${LANGUAGE}
         fi
 
         cd ./templates/${SCOPE}/${LANGUAGE}/${SCENARIO}

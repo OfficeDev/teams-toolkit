1aba50ab971cd50803f4adc0eb17d5464029e2ac
Zhiyu You <zhiyou@microsoft.com>
2021-07-01 10:50:09 +0800

    test: add UI unit tests

---
+++ packages/cli/tests/unit/utils.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+
+chai.use(chaiAsPromised);
+export const expect = chai.expect;

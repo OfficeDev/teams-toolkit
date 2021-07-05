909acc27bfd58abf5a3d32e7b8accff16af8bc24
Kuojian Lu <kuojianlu@gmail.com>
2021-07-05 10:32:28 +0800

    feat(cli-preview): show service log path (#1503)
    * feat(cli-preview): show service log path
    
    - save service log under ~/.fx/cli-log/local-preview
    - show service log path

--- packages/cli/src/cmds/preview/constants.ts
+++ packages/cli/src/cmds/preview/constants.ts
@@ -8,6 +8,7 @@ export const sideloadingUrl =
 export const teamsAppIdPlaceholder = "${teamsAppId}";
 export const accountHintPlaceholder = "${account-hint}";
 
+export const serviceLogHintMessage = "A complete log of this task can be found in:";
 export const waitCtrlPlusC =
   "WARN: Closing browser will not terminate the preview process, please press Ctrl+C to terminate.";
 

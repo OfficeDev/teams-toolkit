a64bd6713030eca398aba6f24ca4ddf5c7cac336
HuihuiWu-Microsoft <73154171+HuihuiWu-Microsoft@users.noreply.github.com>
2021-07-07 13:30:23 +0800

    test: add unit test for extension - part I (#1511)
    * test: add unit test for extension - part I
    
    * test: don't enable ut on ci for now

--- packages/vscode-extension/test/index.integration.ts
+++ packages/vscode-extension/test/index.integration.ts
@@ -15,7 +15,7 @@ export async function run(): Promise<void> {
     reporterOptions: {
       reporterEnabled: "spec, mocha-junit-reporter",
       mochaJunitReporterReporterOptions: {
-        mochaFile: path.resolve(__dirname, "..", "..", "test-results.nofolder.xml"),
+        mochaFile: path.resolve(__dirname, "..", "..", "test-results.integration.xml"),
       },
     },
   };
@@ -28,7 +28,7 @@ export async function run(): Promise<void> {
   const testsRoot = path.resolve(__dirname);
 
   const files: string[] = await new Promise((resolve, reject) => {
-    glob("suite/**/**.noFolder.test.js", { cwd: testsRoot }, (err, result) => {
+    glob("suite/integration/**.test.js", { cwd: testsRoot }, (err, result) => {
       err ? reject(err) : resolve(result);
     });
   });

63f45bf37e01c91255bdb3715b81b134bec686ba
Xiaofu Huang <xiaofhua@microsoft.com>
2021-07-07 13:00:00 +0800

    fix(localdebug): Do not change run icon display status when opening a new editor (#1532)
    * fix(localdebug): change run icon only when active the extension
    
    * fix(localdebug): disable run icon

--- packages/vscode-extension/src/debug/runIconHandler.ts
+++ packages/vscode-extension/src/debug/runIconHandler.ts
@@ -5,10 +5,10 @@ import { ExtensionErrors, ExtensionSource } from "../error";
 import * as vscode from "vscode";
 import * as StringResources from "../resources/Strings.json";
 
-export async function selectAndDebug(args?: any[]): Promise<Result<null, FxError>> {
+export async function selectAndDebug(): Promise<Result<null, FxError>> {
   if (ext.workspaceUri && isValidProject(ext.workspaceUri.fsPath)) {
-    vscode.commands.executeCommand("workbench.view.debug");
-    vscode.commands.executeCommand("workbench.action.debug.selectandstart");
+    await vscode.commands.executeCommand("workbench.view.debug");
+    await vscode.commands.executeCommand("workbench.action.debug.selectandstart");
     return ok(null);
   } else {
     const error = returnUserError(
@@ -22,7 +22,6 @@ export async function selectAndDebug(args?: any[]): Promise<Result<null, FxError
 }
 
 export function registerRunIcon(): void {
-  ext.context.subscriptions.push(vscode.workspace.onDidOpenTextDocument(enableRunIcon));
   ext.context.subscriptions.push(vscode.workspace.onDidChangeWorkspaceFolders(enableRunIcon));
   enableRunIcon();
 }
@@ -30,4 +29,8 @@ export function registerRunIcon(): void {
 function enableRunIcon(): void {
   const validProject = ext.workspaceUri && isValidProject(ext.workspaceUri.fsPath);
   vscode.commands.executeCommand("setContext", "fx-extension.runIconActive", validProject);
+}
+
+export function disableRunIcon(): void {
+  vscode.commands.executeCommand("setContext", "fx-extension.runIconActive", false);
 }

9fd0e658ae80712a72e4598d39d49fcf1374c90a
KennethBWSong <bowsong@microsoft.com>
2021-07-07 11:30:38 +0800

    fix(core): add error handling and ouput

--- packages/fx-core/src/core/middleware/projectUpgrader.ts
+++ packages/fx-core/src/core/middleware/projectUpgrader.ts
@@ -5,66 +5,87 @@ import { Middleware, NextFunction } from "@feathersjs/hooks";
 import { ConfigFolderName, err, Inputs, Json, ProjectSettings } from "@microsoft/teamsfx-api";
 import * as path from "path";
 import * as fs from "fs-extra";
-import { CoreHookContext, FxCore, NoProjectOpenedError, PathNotExistError } from "..";
+import {
+  CoreHookContext,
+  FxCore,
+  NoProjectOpenedError,
+  PathNotExistError,
+  UpgradeContextError,
+} from "..";
 import { clearContextAndUserData, deserializeDict, moveContextToUserData } from "../..";
 import { serializeDict } from "../../common";
 
 export const ProjectUpgraderMW: Middleware = async (ctx: CoreHookContext, next: NextFunction) => {
-  const inputs = ctx.arguments[ctx.arguments.length - 1] as Inputs;
-  if (!inputs.projectPath) {
-    ctx.result = err(NoProjectOpenedError());
-    return;
-  }
-  const projectPathExist = await fs.pathExists(inputs.projectPath);
-  if (!projectPathExist) {
-    ctx.result = err(PathNotExistError(inputs.projectPath));
-    return;
-  }
-
-  await upgradeContext(inputs);
+  await upgradeContext(ctx);
   await next();
 };
 
-export async function upgradeContext(inputs: Inputs) {
-  const confFolderPath = path.resolve(inputs.projectPath!, `.${ConfigFolderName}`);
-  const settingsFile = path.resolve(confFolderPath, "settings.json");
-  const projectSettings: ProjectSettings = await fs.readJson(settingsFile);
-  const envName = projectSettings.currentEnv;
+export async function upgradeContext(ctx: CoreHookContext): Promise<void> {
+  try {
+    const inputs = ctx.arguments[ctx.arguments.length - 1] as Inputs;
+    if (!inputs.projectPath) {
+      ctx.result = err(NoProjectOpenedError());
+      return;
+    }
+    const projectPathExist = await fs.pathExists(inputs.projectPath);
+    if (!projectPathExist) {
+      ctx.result = err(PathNotExistError(inputs.projectPath));
+      return;
+    }
+    const confFolderPath = path.resolve(inputs.projectPath!, `.${ConfigFolderName}`);
+    const settingsFile = path.resolve(confFolderPath, "settings.json");
+    const projectSettings: ProjectSettings = await fs.readJson(settingsFile);
+    const envName = projectSettings.currentEnv;
 
-  const userData = await getUserData(confFolderPath, envName as string);
-  const env = await getEnv(confFolderPath, envName as string);
+    const userData = await getUserData(confFolderPath, envName as string);
+    const env = await getEnv(confFolderPath, envName as string);
 
-  // Check local AAD object id in userdata
-  // If in, means this project is upgraded
-  if (checkAppIdInUserData(userData)) {
+    // Check local AAD object id in userdata
+    // If in, means this project is upgraded
+    if (checkAppIdInUserData(userData)) {
+      return;
+    }
+
+    // Check secret in userdata
+    // If in, means this project is not upgraded and has run debug before
+    if (!checkSecretInUserData(userData)) {
+      // Clear and save userdata and env.default.
+      clearContextAndUserData(userData, env);
+      await saveContextAndUserData(confFolderPath, envName as string, env, userData);
+      return;
+    }
+
+    const solutionContext: any = env["solution"];
+    const teamsAppId = solutionContext["localDebugTeamsAppId"] as string | undefined;
+
+    // Check teamsAppId in env
+    // If meets this pattern, means env file is upgraded
+    if (!teamsAppId || (teamsAppId!.startsWith("{{") && teamsAppId!.endsWith("}}"))) {
+      // Clear userdata and env.default.json
+      clearContextAndUserData(userData, env);
+      await saveContextAndUserData(confFolderPath, envName as string, env, userData);
+    } else {
+      // Send log to output channel
+      const core = ctx.self as FxCore;
+      const logger =
+        core !== undefined && core.tools !== undefined && core.tools.logProvider !== undefined
+          ? core.tools.logProvider
+          : undefined;
+      if (logger) {
+        logger.info(
+          "[core]: context version is too low. Will update context and move some config from env to userdata."
+        );
+      }
+
+      // Move info from env.default.json to userdata
+      moveContextToUserData(userData, env);
+      await saveContextAndUserData(confFolderPath, envName as string, env, userData);
+    }
+
     return;
+  } catch (error) {
+    ctx.result = err(UpgradeContextError(error));
   }
-
-  // Check secret in userdata
-  // If in, means this project is not upgraded and has run debug before
-  if (!checkSecretInUserData(userData)) {
-    // Clear and save userdata and env.default.
-    clearContextAndUserData(userData, env);
-    await saveContextAndUserData(confFolderPath, envName as string, env, userData);
-    return;
-  }
-
-  const solutionContext: any = env["solution"];
-  const teamsAppId = solutionContext["localDebugTeamsAppId"] as string | undefined;
-
-  // Check teamsAppId in env
-  // If meets this pattern, means env file is upgraded
-  if (!teamsAppId || (teamsAppId!.startsWith("{{") && teamsAppId!.endsWith("}}"))) {
-    // Clear userdata and env.default.json
-    clearContextAndUserData(userData, env);
-    await saveContextAndUserData(confFolderPath, envName as string, env, userData);
-  } else {
-    // Move info from env.default.json to userdata
-    moveContextToUserData(userData, env);
-    await saveContextAndUserData(confFolderPath, envName as string, env, userData);
-  }
-
-  return;
 }
 
 export async function getUserData(

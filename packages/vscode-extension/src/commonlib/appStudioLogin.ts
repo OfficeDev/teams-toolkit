7a5035489f3d74ef5cac9c18eb8f4f8d9cc2dd80
Tian Yuan <tiany1992@sina.com>
2021-07-08 10:41:44 +0800

    fix: add reload appStudio logic

--- packages/vscode-extension/src/commonlib/appStudioLogin.ts
+++ packages/vscode-extension/src/commonlib/appStudioLogin.ts
@@ -82,6 +82,7 @@ export class AppStudioLogin extends login implements AppStudioTokenProvider {
    * Get team access token
    */
   async getAccessToken(showDialog = true): Promise<string | undefined> {
+    await AppStudioLogin.codeFlowInstance.reloadCache();
     if (!AppStudioLogin.codeFlowInstance.account) {
       if (showDialog) {
         const userConfirmation: boolean = await this.doesUserConfirmLogin();

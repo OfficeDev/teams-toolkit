de3cdabd6ffa05187305e75e8b575c3d4b6dd7a0
KennethBWSong <63089166+KennethBWSong@users.noreply.github.com>
2021-07-09 12:38:53 +0800

    feat(collaborate): add local collaborate logic

--- packages/fx-core/src/core/middleware/projectUpgrader.ts
+++ packages/fx-core/src/core/middleware/projectUpgrader.ts
@@ -1,11 +1,15 @@
 import { Middleware, NextFunction } from "@feathersjs/hooks";
 import { ConfigFolderName, err, Inputs, Json, ProjectSettings } from "@microsoft/teamsfx-api";
-import { CoreHookContext, NoProjectOpenedError, PathNotExistError } from "..";
+import { CoreHookContext, FxCore, NoProjectOpenedError, PathNotExistError } from "..";
 import * as fs from "fs-extra";
 import * as path from "path";
 import { deserializeDict, serializeDict } from "../..";
 
 const contextUpgrade = [
+  {
+    plugin: "solution",
+    relatedKeys: ["localDebugTeamsAppId", "teamsAppTenantId"],
+  },
   {
     plugin: "fx-resource-aad-app-for-teams",
     secret: "local_clientSecret",
@@ -51,26 +55,43 @@ export async function upgradeContext(ctx: CoreHookContext): Promise<void> {
   for (const item of contextUpgrade) {
     // Secret not in userdata file, means has not run local debug before.
     // Will delete related keys if exists.
-    if (!userData[getUserDataKey(item.plugin, item.secret)]) {
+    if (item.secret && !userData[getUserDataKey(item.plugin, item.secret)]) {
       deleteKeysFromContext(context, item.plugin, item.relatedKeys);
     }
 
     // Secret in userdata file.
     // Move keys from context to userdata, and will add key in context.
-    // TODO: move keys, return: whether overwrite
+    const keyMoved = moveKeysFromContextToUserData(
+      userData,
+      context,
+      item.plugin,
+      item.relatedKeys
+    );
 
     // Check whether secret is complete
     let keyCompleted = true;
     for (const relatedKey of item.relatedKeys) {
-      // TODO: add style check
-      if (!userData[relatedKey]) {
+      if (!userData[relatedKey] || isSecretPattern(userData[relatedKey])) {
         keyCompleted = false;
         break;
       }
     }
 
+    // If key is complete, which means all configs are set.
     if (keyCompleted) {
-      // Log?
+      // If some key is moved in moveKeysFromContextToUserData, will send log to inform user context is upgraded.
+      if (keyMoved) {
+        const core = ctx.self as FxCore;
+        const logger =
+          core !== undefined && core.tools !== undefined && core.tools.logProvider !== undefined
+            ? core.tools.logProvider
+            : undefined;
+        if (logger) {
+          logger.info(
+            "[core]: context version is too low. Will update context and move some config from env to userdata."
+          );
+        }
+      }
     } else {
       // Key missing.
       // Will delete context and key
@@ -126,15 +147,47 @@ function deleteKeysFromContext(context: Json, plugin: string, keys: string[]): J
 function deleteSecretFromUserData(
   userData: Record<string, string>,
   plugin: string,
-  secret: string
+  secret?: string
 ): Record<string, string> {
-  if (userData[getUserDataKey(plugin, secret)]) {
+  if (secret && userData[getUserDataKey(plugin, secret)]) {
     delete userData[getUserDataKey(plugin, secret)];
   }
 
   return userData;
 }
 
+function moveKeysFromContextToUserData(
+  userData: Record<string, string>,
+  context: Json,
+  plugin: string,
+  keys: string[]
+): boolean {
+  let keyMoved = false;
+  const pluginContext: any = context[plugin];
+  if (!pluginContext) {
+    return false;
+  }
+
+  for (const key in keys) {
+    const value = pluginContext[key];
+    if (value && !isSecretPattern(value)) {
+      keyMoved = true;
+      userData[getUserDataKey(plugin, key)] = value;
+      pluginContext[key] = getSecretPattern(plugin, key);
+    }
+  }
+
+  return keyMoved;
+}
+
 function getUserDataKey(plugin: string, key: string) {
   return `${plugin}.${key}`;
+}
+
+function isSecretPattern(value: string) {
+  return value.startsWith("{{") && value.endsWith("}}");
+}
+
+function getSecretPattern(plugin: string, key: string) {
+  return `{{${getUserDataKey(plugin, key)}}}`;
 }

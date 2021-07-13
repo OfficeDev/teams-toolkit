b129487185a4bacf1b7757f2391f9db4d678bf74
Ivan He <ruhe@microsoft.com>
2021-07-13 11:02:48 +0800

    feat: add default env

--- packages/github-actions/teams-app-cicd/src/utils/exec.ts
+++ packages/github-actions/teams-app-cicd/src/utils/exec.ts
@@ -2,6 +2,9 @@ import * as exec from '@actions/exec'
 
 export async function Execute(cmd: string, workdir: string): Promise<number> {
   return await exec.exec(cmd, undefined, {
-    cwd: workdir
+    cwd: workdir,
+    env: {
+      "CI_ENABLED": "true"
+    }
   })
 }

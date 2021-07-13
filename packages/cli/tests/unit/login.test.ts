545d6d31f13af1a6278a1f6a57af55396ee57f9e
Tian Yuan <tiany1992@sina.com>
2021-07-13 16:04:38 +0800

    fix: add unit test cases

---
+++ packages/cli/tests/unit/login.test.ts
@@ -0,0 +1,23 @@
+
+import AzureAccountManager from "../../src/commonlib/azureLogin";
+import sinon from "sinon";
+
+describe("Help Parameter Tests", function () {
+    const sandbox = sinon.createSandbox();
+
+    before(() => {
+        sandbox.stub(AzureAccountManager, "getJsonObject");
+    });
+
+    it("Azure login/getAccountInfo", () => {
+        AzureAccountManager.getAccountInfo();
+    });
+
+    it("Azure login/getSelectedSubscription", () => {
+        AzureAccountManager.getSelectedSubscription();
+    });
+
+    it("Azure login/selectSubscription", () => {
+        AzureAccountManager.selectSubscription();
+    });
+});

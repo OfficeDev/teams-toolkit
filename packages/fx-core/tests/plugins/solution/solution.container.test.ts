bddcaa545ca6526f69259e093f1186afa558cc85
huajiezhang <huajiezhang@microsoft.com>
2021-07-13 14:56:48 +0800

    test: ut

--- packages/fx-core/tests/plugins/solution/solution.container.test.ts
+++ packages/fx-core/tests/plugins/solution/solution.container.test.ts
@@ -3,27 +3,7 @@
 
 import { assert } from "chai";
 import "mocha";
-import {
-  err,
-  FxError,
-  Result,
-  ok,
-  Inputs,
-  Platform,
-  Stage,
-  SolutionContext,
-  QTreeNode,
-  Func,
-  InputTextConfig,
-  InputTextResult,
-  SelectFolderConfig,
-  SelectFolderResult,
-  SingleSelectConfig,
-  SingleSelectResult,
-  OptionItem,
-  traverse,
-  AzureSolutionSettings,
-} from "@microsoft/teamsfx-api";
+import { Inputs, Platform, AzureSolutionSettings } from "@microsoft/teamsfx-api";
 import fs from "fs-extra";
 import * as path from "path";
 import * as os from "os";
@@ -36,7 +16,12 @@ import {
 } from "../../../src/plugins/solution/fx-solution/ResourcePluginContainer";
 import {
   AzureResourceSQL,
+  AzureSolutionQuestionNames,
+  BotOptionItem,
+  FrontendHostTypeQuestion,
   HostTypeOptionAzure,
+  HostTypeOptionSPFx,
+  ProgrammingLanguageQuestion,
   TabOptionItem,
 } from "../../../src/plugins/solution/fx-solution/question";
 import { Container } from "typedi";
@@ -67,5 +52,54 @@ describe("Resource plugin container", () => {
     const names = plugins.map((p) => p.name);
     assert.isTrue(names.includes(Container.get<Plugin>(ResourcePlugins.FrontendPlugin).name));
     assert.isTrue(names.includes(Container.get<Plugin>(ResourcePlugins.SqlPlugin).name));
+  });
+
+  it("solution FrontendHostTypeQuestion", async () => {
+    let inputs: Inputs = {
+      platform: Platform.VSCode,
+      [AzureSolutionQuestionNames.Capabilities]: [BotOptionItem.id],
+    };
+    if (FrontendHostTypeQuestion.dynamicOptions) {
+      let options = FrontendHostTypeQuestion.dynamicOptions(inputs);
+      assert.deepEqual(options, [HostTypeOptionAzure]);
+
+      inputs = {
+        platform: Platform.VSCode,
+        [AzureSolutionQuestionNames.Capabilities]: [TabOptionItem.id],
+      };
+      options = FrontendHostTypeQuestion.dynamicOptions(inputs);
+      assert.deepEqual(options, [HostTypeOptionAzure, HostTypeOptionSPFx]);
+    }
+  });
+
+  it("solution ProgrammingLanguageQuestion", async () => {
+    let inputs: Inputs = {
+      platform: Platform.VSCode,
+      [AzureSolutionQuestionNames.HostType]: HostTypeOptionSPFx.id,
+    };
+    if (
+      ProgrammingLanguageQuestion.dynamicOptions &&
+      ProgrammingLanguageQuestion.placeholder &&
+      typeof ProgrammingLanguageQuestion.placeholder === "function"
+    ) {
+      let options = ProgrammingLanguageQuestion.dynamicOptions(inputs);
+      assert.deepEqual([{ id: "typescript", label: "TypeScript" }], options);
+      let placeholder = ProgrammingLanguageQuestion.placeholder(inputs);
+      assert.equal("SPFx is currently supporting TypeScript only.", placeholder);
+      inputs = {
+        platform: Platform.VSCode,
+        [AzureSolutionQuestionNames.HostType]: HostTypeOptionAzure.id,
+      };
+      options = ProgrammingLanguageQuestion.dynamicOptions(inputs);
+      assert.deepEqual(
+        [
+          { id: "javascript", label: "JavaScript" },
+          { id: "typescript", label: "TypeScript" },
+        ],
+        options
+      );
+      placeholder = ProgrammingLanguageQuestion.placeholder(inputs);
+      assert.equal("Select a programming language.", placeholder);
+    }
   });
 });

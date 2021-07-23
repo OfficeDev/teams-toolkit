3bce5fb1f4b1386aff865d0a8f04af1aa25b857d
huajiezhang <huajiezhang@microsoft.com>
2021-07-23 16:53:11 +0800

    fix: ut

--- packages/fx-core/tests/plugins/resource/spfx/unit/scaffold.test.ts
+++ packages/fx-core/tests/plugins/resource/spfx/unit/scaffold.test.ts
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-
+import "mocha";
 import * as fs from "fs-extra";
 import * as path from "path";
 import { expect } from "chai";

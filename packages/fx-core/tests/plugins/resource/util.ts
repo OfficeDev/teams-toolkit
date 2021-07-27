a7a8213a2155b3f58fb5f904927c736ed0180076
Di Lin <dilin@microsoft.com>
2021-07-27 16:05:38 +0800

    fix(arm support): add generic resource plugin id constant string

--- packages/fx-core/tests/plugins/resource/util.ts
+++ packages/fx-core/tests/plugins/resource/util.ts
@@ -58,3 +58,14 @@ export function mockSolutionUpdateArmTemplates(
 export class ConstantString {
   static readonly UTF8Encoding = "utf-8";
 }
+
+export class ResourcePlugins {
+  static readonly Aad = "fx-resource-aad-app-for-teams";
+  static readonly FrontendHosting = "fx-resource-frontend-hosting";
+  static readonly SimpleAuth = "fx-resource-simple-auth";
+  static readonly Bot = "fx-resource-bot";
+  static readonly LocalDebug = "fx-resource-local-debug";
+  static readonly AzureSQL = "fx-resource-azure-sql";
+  static readonly Function = "fx-resource-function";
+  static readonly Identity = "fx-resource-identity";
+}

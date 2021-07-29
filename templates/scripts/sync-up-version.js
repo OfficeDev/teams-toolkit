451bdbb620eb4b1d845fe5185175f4bda0f3f6d3
wenytang-ms <wenyutang@microsoft.com>
2021-07-29 15:01:58 +0800

    chore: fix commits

--- templates/scripts/sync-up-version.js
+++ templates/scripts/sync-up-version.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
-var path = require("path");
+const path = require("path");
 const loadJsonFile = require("load-json-file");
 const writePkg = require("write-pkg");
 const semver = require("semver");
 
-const sdk_version = require(path.join(__dirname, "../../packages/sdk/package.json")).version;
-const sdk_name = require(path.join(__dirname, "../../packages/sdk/package.json")).name;
+const sdkVersion = require(path.join(__dirname, "../../packages/sdk/package.json")).version;
+const sdkNname = require(path.join(__dirname, "../../packages/sdk/package.json")).name;
 
 function listFile(dir, list = []) {
   var arr = fs.readdirSync(dir);
@@ -22,20 +22,20 @@ function listFile(dir, list = []) {
   return list;
 }
 
-const template_dir = path.join(__dirname, "..");
-const dep_pkgs = listFile(template_dir);
-for (file of dep_pkgs) {
+const templateDir = path.join(__dirname, "..");
+const depPkgs = listFile(templateDir);
+for (file of depPkgs) {
   let pkg_ = loadJsonFile.sync(file);
   let dep = pkg_.dependencies;
   if (dep) {
-    let dep_map = new Map(Object.entries(dep));
-    if (dep_map.get(sdk_name)) {
-      if (semver.prerelease(sdk_version)) {
-        dep_map.set(sdk_name, sdk_version);
-      } else if (!semver.intersects(dep_map.get(sdk_name), sdk_version)) {
-        dep_map.set(sdk_name, `^${sdk_version}`);
+    let depMap = new Map(Object.entries(dep));
+    if (depMap.get(sdkNname)) {
+      if (semver.prerelease(sdkVersion)) {
+        depMap.set(sdkNname, sdkVersion);
+      } else if (!semver.intersects(depMap.get(sdkNname), sdkVersion)) {
+        depMap.set(sdkNname, `^${sdkVersion}`);
       }
-      pkg_.dependencies = Object.fromEntries(dep_map);
+      pkg_.dependencies = Object.fromEntries(depMap);
       writePkg(file, pkg_);
     }
   }

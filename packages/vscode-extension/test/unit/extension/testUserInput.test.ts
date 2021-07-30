b19213ee9a2c1f3d4d88e64df2d62e0e04d9be63
huajiezhang <huajiezhang@microsoft.com>
2021-07-30 17:41:52 +0800

    fix: ut

--- packages/vscode-extension/test/unit/extension/testUserInput.test.ts
+++ packages/vscode-extension/test/unit/extension/testUserInput.test.ts
@@ -3,8 +3,20 @@
 "use strict";
 
 import * as chai from "chai";
+import {
+  FxError,
+  ok,
+  Result,
+  GroupOfTasks,
+  RunnableTask,
+  err,
+  UserCancelError,
+} from "@microsoft/teamsfx-api";
+import { sleep } from "../../../src/utils/commonUtils";
 
 import { EInputType, IUserInputItem, TestUserInput } from "./mocks/testUserInput";
+import { VsCodeUI } from "../../../src/qm/vsc_ui";
+import { ExtensionContext } from "vscode";
 
 suite("Mock Tests", () => {
   suite("User Input", () => {
@@ -39,6 +51,80 @@ suite("Mock Tests", () => {
       chai.assert.equal(await ui.showQuickPick(["a", "b"], { placeHolder: "test" }), undefined);
 
       // TODO: add other mock tests.
+    });
+
+    test("ui", async () => {
+      const VS_CODE_UI = new VsCodeUI(<ExtensionContext>{});
+      const task1: RunnableTask<undefined> = {
+        name: "task1",
+        run: async (...args: any): Promise<Result<undefined, FxError>> => {
+          await sleep(3000);
+          return ok(undefined);
+        },
+      };
+
+      const task2: RunnableTask<undefined> = {
+        name: "task2",
+        run: async (...args: any): Promise<Result<undefined, FxError>> => {
+          await sleep(3000);
+          return err(UserCancelError);
+        },
+      };
+      const task3: RunnableTask<undefined> = {
+        name: "task3",
+        run: async (...args: any): Promise<Result<undefined, FxError>> => {
+          await sleep(3000);
+          return ok(undefined);
+        },
+      };
+      const sequentialRes = await VS_CODE_UI.selectOption({
+        name: "sequential",
+        title: "sequential",
+        options: ["Yes", "No"],
+      });
+      if (sequentialRes.isErr()) {
+        VS_CODE_UI.showMessage("error", sequentialRes.error.name, false);
+        return;
+      }
+      const fastFailRes = await VS_CODE_UI.selectOption({
+        name: "fastFail",
+        title: "fastFail",
+        options: ["Yes", "No"],
+      });
+      if (fastFailRes.isErr()) {
+        VS_CODE_UI.showMessage("error", fastFailRes.error.name, false);
+        return;
+      }
+      const showProgressRes = await VS_CODE_UI.selectOption({
+        name: "showProgress",
+        title: "showProgress",
+        options: ["Yes", "No"],
+      });
+      if (showProgressRes.isErr()) {
+        VS_CODE_UI.showMessage("error", showProgressRes.error.name, false);
+        return;
+      }
+      const cancellableRes = await VS_CODE_UI.selectOption({
+        name: "cancellable",
+        title: "cancellable",
+        options: ["Yes", "No"],
+      });
+      if (cancellableRes.isErr()) {
+        VS_CODE_UI.showMessage("error", cancellableRes.error.name, false);
+        return;
+      }
+      const sequential = sequentialRes.value.result === "Yes";
+      const fastFail = fastFailRes.value.result === "Yes";
+      const showProgress = showProgressRes.value.result === "Yes";
+      const cancellable = cancellableRes.value.result === "Yes";
+      const group = new GroupOfTasks<undefined>([task1, task2, task3], {
+        sequential: sequential,
+        fastFail: fastFail,
+      });
+      await VS_CODE_UI.runWithProgress(group, {
+        showProgress: showProgress,
+        cancellable: cancellable,
+      });
     });
   });
 });
---
+++ packages/vscode-extension/test/unit/extension/userInterface.test.ts
@@ -0,0 +1,39 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+"use strict";
+
+import { ExtensionContext } from "vscode";
+
+import { ext } from "../../../src/extensionVariables";
+import { TestUserInput } from "./mocks/testUserInput";
+import { sleep } from "../../../src/utils/commonUtils";
+import { VsCodeUI } from "../../../src/qm/vsc_ui";
+
+suite("UI Unit Tests", async () => {
+  suiteSetup(() => {
+    // Mock user input.
+    ext.ui = new TestUserInput();
+  });
+
+  suite("Manually", () => {
+    test("Show Progress 2", async function (this: Mocha.Context) {
+      this.timeout(0);
+      const VS_CODE_UI = new VsCodeUI(<ExtensionContext>{});
+      const handler = VS_CODE_UI.createProgressBar("Test Progress Bar", 3);
+
+      await handler.start("Prepare");
+      await sleep(2 * 1000);
+
+      await handler.next("First step");
+      await sleep(2 * 1000);
+
+      await handler.next("Second step");
+      await sleep(2 * 1000);
+
+      await handler.next("Third step");
+      await sleep(2 * 1000);
+
+      await handler.end();
+    });
+  });
+});

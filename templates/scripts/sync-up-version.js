b2a5a4199f824fc38392ad2a8e7bcf7aa6d94175
wenytang-ms <wenyutang@microsoft.com>
2021-07-30 10:19:36 +0800

    chore: udpate code

--- templates/scripts/sync-up-version.js
+++ templates/scripts/sync-up-version.js
@@ -1,22 +1,21 @@
-const fs = require("fs");
 const path = require("path");
-const loadJsonFile = require("load-json-file");
 const writePkg = require("write-pkg");
 const semver = require("semver");
+const fse = require("fs-extra");
 
 const sdkVersion = require(path.join(__dirname, "../../packages/sdk/package.json")).version;
-const sdkNname = require(path.join(__dirname, "../../packages/sdk/package.json")).name;
+const sdkName = require(path.join(__dirname, "../../packages/sdk/package.json")).name;
 
 function listFile(dir, list = []) {
-  const arr = fs.readdirSync(dir);
+  const arr = fse.readdirSync(dir);
   arr.forEach(function (item) {
     if (item === "node_modules") return list;
     const fullpath = path.join(dir, item);
-    const stats = fs.statSync(fullpath);
+    const stats = fse.statSync(fullpath);
     if (stats.isDirectory()) {
       listFile(fullpath, list);
-    } else {
-      if (item === "package.json") list.push(fullpath);
+    } else if (item === "package.json") {
+      list.push(fullpath);
     }
   });
   return list;
@@ -25,20 +24,17 @@ function listFile(dir, list = []) {
 const templateDir = path.join(__dirname, "..");
 const depPkgs = listFile(templateDir);
 for (let file of depPkgs) {
-  const pkg_ = loadJsonFile.sync(file);
+  const pkg_ = fse.readJsonSync(file);
   const dep = pkg_.dependencies;
-  if (dep) {
-    let depMap = new Map(Object.entries(dep));
-    if (depMap.get(sdkNname)) {
-      if (semver.prerelease(sdkVersion)) {
-        depMap.set(sdkNname, sdkVersion);
-      } else if (!semver.intersects(depMap.get(sdkNname), sdkVersion)) {
-        depMap.set(sdkNname, `^${sdkVersion}`);
-      } else {
-        continue;
-      }
-      pkg_.dependencies = Object.fromEntries(depMap);
-      writePkg(file, pkg_);
+  if (dep && dep[sdkName]) {
+    if (semver.prerelease(sdkVersion)) {
+      dep[sdkName] = sdkVersion;
+    } else if (!semver.intersects(dep[sdkName], sdkVersion)) {
+      dep[sdkName] = `^${sdkVersion}`;
+    } else {
+      continue;
     }
+    pkg_.dependencies = dep;
+    writePkg(file, pkg_);
   }
 }

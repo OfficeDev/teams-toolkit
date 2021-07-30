4bc3d9f335a3f39e335779dc23b80eeb452233c3
wenytang-ms <wenyutang@microsoft.com>
2021-07-30 16:23:04 +0800

    chore: logic

--- packages/sdk/tools/postversion.sh
+++ packages/sdk/tools/postversion.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if [[ -z "$(git tag --points-at HEAD | grep templates)" && ! -z "$(git diff HEAD^ -- ../../templates/package.json|grep version)" ]]
 then
+    echo "need to tag on templates cause templates has no tags but bump up version"
     git tag "templates@$(node -p "require('../../templates/package.json').version")"
 fi

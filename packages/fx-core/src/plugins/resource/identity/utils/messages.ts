d5f8e5d35cc16b5ecb7014c08cc8baded322634a
Zhaofeng Xu <zhaofengxu@microsoft.com>
2021-08-02 14:53:22 +0800

    chore(identity): add resource provider check

--- packages/fx-core/src/plugins/resource/identity/utils/messages.ts
+++ packages/fx-core/src/plugins/resource/identity/utils/messages.ts
@@ -5,7 +5,7 @@ export class Message {
   public static readonly endProvision = `[${Constants.pluginName}] end provision`;
   public static readonly provisionIdentity = `[${Constants.pluginName}] provision identity`;
   public static readonly getIdentityId = `[${Constants.pluginName}] get identity id`;
-  public static readonly checkProvider = `[${Constants.pluginName}] check SQL resource provider`;
+  public static readonly checkProvider = `[${Constants.pluginName}] check identity resource provider`;
 
   public static readonly identityName = (name: string) =>
     `[${Constants.pluginName}] identity name is ${name}`;

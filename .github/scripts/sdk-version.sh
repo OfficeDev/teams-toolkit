1a9436bcfb0553ccc39c1f358a34680d902497b4
wenyt <75360946+wenytang-ms@users.noreply.github.com>
2021-08-09 15:36:33 +0800

    chore: move scripts to .github folder (#1878)
    * chore: syncup sdk version with templates
    
    * chore: remove write-pkg devdep
    
    Co-authored-by: wenytang-ms-123 <wenytang@wenytang.onmicrosoft.com>

--- .github/scripts/sdk-version.sh
+++ .github/scripts/sdk-version.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ -z "$(git diff -- ../../templates)" ]; then
 echo "need bump up templates version since templates don not bump up by self"
-node ./tools/sync-up-version.js yes;
+node ../../.github/scripts/sdk-sync-up-version.js yes;
 else 
 echo "no need to bump up templates version"
-node ./tools/sync-up-version.js 
+node ../../.github/scripts/sdk-sync-up-version.js 
 fi
 git add ../../templates

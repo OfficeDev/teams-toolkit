6da3d8e2332928458dc032cc6fb56ac2e4c939da
Dooriya Li <dol@microsoft.com>
2021-08-11 14:02:55 +0800

    refactor(localdebug): add middleware in core for localSettings loader and writer (#1896)
    * refactor: add middleware in core for localSettings loader and writer
    
    * fix: bugs in the localSettings loader&writer
    
    * fix: naming for consistency
    
    * fix: remove non-related code change in this pr

--- packages/fx-core/src/core/index.ts
+++ packages/fx-core/src/core/index.ts
@@ -76,6 +76,8 @@ import { ProjectUpgraderMW } from "./middleware/projectUpgrader";
 import { globalStateUpdate } from "../common/globalState";
 import { EnvInfoLoaderMW } from "./middleware/envInfoLoader";
 import { EnvInfoWriterMW } from "./middleware/envInfoWriter";
+import { LocalSettingsLoaderMW } from "./middleware/localSettingsLoader";
+import { LocalSettingsWriterMW } from "./middleware/localSettingsWriter";
 
 export interface CoreHookContext extends HookContext {
   projectSettings?: ProjectSettings;
@@ -326,11 +328,13 @@ export class FxCore implements Core {
     ProjectUpgraderMW,
     ProjectSettingsLoaderMW,
     EnvInfoLoaderMW,
+    LocalSettingsLoaderMW,
     SolutionLoaderMW(defaultSolutionLoader),
     QuestionModelMW,
     ContextInjecterMW,
     ProjectSettingsWriterMW,
     EnvInfoWriterMW,
+    LocalSettingsWriterMW,
   ])
   async localDebug(inputs: Inputs, ctx?: CoreHookContext): Promise<Result<Void, FxError>> {
     return await ctx!.solution!.localDebug(ctx!.solutionContext!);
@@ -356,11 +360,13 @@ export class FxCore implements Core {
     ConcurrentLockerMW,
     ProjectSettingsLoaderMW,
     EnvInfoLoaderMW,
+    LocalSettingsLoaderMW,
     SolutionLoaderMW(defaultSolutionLoader),
     QuestionModelMW,
     ContextInjecterMW,
     ProjectSettingsWriterMW,
     EnvInfoWriterMW,
+    LocalSettingsWriterMW,
   ])
   async executeUserTask(
     func: Func,
@@ -439,6 +445,7 @@ export class FxCore implements Core {
     return ok({
       settings: ctx!.solutionContext!.projectSettings,
       config: ctx!.solutionContext!.config,
+      localSettings: ctx!.solutionContext!.localSettings,
     });
   }
 

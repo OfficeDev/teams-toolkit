4389898dea30bce3bb8d97c2fa8503b852a3e39d
dooriya <dol@microsoft.com>
2021-08-11 11:49:01 +0800

    fix: bugs in the localSettings loader&writer

--- packages/fx-core/src/core/middleware/localSettingsLoader.ts
+++ packages/fx-core/src/core/middleware/localSettingsLoader.ts
@@ -14,33 +14,36 @@ export const localSettingsLoaderMW: Middleware = async (
   ctx: CoreHookContext,
   next: NextFunction
 ) => {
-  if (!isMultiEnvEnabled()) return;
-  if (ctx.solutionContext == undefined) return;
+  if (isMultiEnvEnabled() && ctx.solutionContext) {
+    const inputs = ctx.arguments[ctx.arguments.length - 1] as Inputs;
+    if (!inputs.projectPath) {
+      ctx.result = err(NoProjectOpenedError());
+      return;
+    }
 
-  const inputs = ctx.arguments[ctx.arguments.length - 1] as Inputs;
-  if (!inputs.projectPath) {
-    ctx.result = err(NoProjectOpenedError());
-    return;
-  }
+    const projectPathExist = await fs.pathExists(inputs.projectPath);
+    if (!projectPathExist) {
+      ctx.result = err(PathNotExistError(inputs.projectPath));
+      return;
+    }
 
-  const projectPathExist = await fs.pathExists(inputs.projectPath);
-  if (!projectPathExist) {
-    ctx.result = err(PathNotExistError(inputs.projectPath));
-    return;
-  }
+    const solutionSettings = ctx.projectSettings?.solutionSettings as AzureSolutionSettings;
+    const selectedPlugins: Plugin[] = getActivatedResourcePlugins(solutionSettings);
 
-  const solutionSettings = ctx.projectSettings?.solutionSettings as AzureSolutionSettings;
-  const selectedPlugins: Plugin[] = getActivatedResourcePlugins(solutionSettings);
+    const hasFrontend = selectedPlugins?.some((plugin) => plugin.name === PluginNames.FE);
+    const hasBackend = selectedPlugins?.some((plugin) => plugin.name === PluginNames.FUNC);
+    const hasBot = selectedPlugins?.some((plugin) => plugin.name === PluginNames.BOT);
 
-  const hasFrontend = selectedPlugins?.some((plugin) => plugin.name === PluginNames.FE);
-  const hasBackend = selectedPlugins?.some((plugin) => plugin.name === PluginNames.FUNC);
-  const hasBot = selectedPlugins?.some((plugin) => plugin.name === PluginNames.BOT);
-
-  const localSettingsProvider = new LocalSettingsProvider(inputs.projectPath);
-  if (await fs.pathExists(localSettingsProvider.localSettingsFilePath)) {
-    ctx.solutionContext.localSettings = await localSettingsProvider.load();
-  } else {
-    ctx.solutionContext.localSettings = localSettingsProvider.init(hasFrontend, hasBackend, hasBot);
+    const localSettingsProvider = new LocalSettingsProvider(inputs.projectPath);
+    if (await fs.pathExists(localSettingsProvider.localSettingsFilePath)) {
+      ctx.solutionContext.localSettings = await localSettingsProvider.load();
+    } else {
+      ctx.solutionContext.localSettings = localSettingsProvider.init(
+        hasFrontend,
+        hasBackend,
+        hasBot
+      );
+    }
   }
 
   await next();
--- packages/fx-core/src/core/middleware/localSettingsWriter.ts
+++ packages/fx-core/src/core/middleware/localSettingsWriter.ts
@@ -15,30 +15,30 @@ export const LocalSettingsWriterMW: Middleware = async (
   ctx: CoreHookContext,
   next: NextFunction
 ) => {
-  if (!isMultiEnvEnabled()) return;
-
   try {
     await next();
   } finally {
-    const lastArg = ctx.arguments[ctx.arguments.length - 1];
-    const inputs: Inputs = lastArg === ctx ? ctx.arguments[ctx.arguments.length - 2] : lastArg;
-    if (
-      !inputs.projectPath ||
-      inputs.ignoreConfigPersist === true ||
-      StaticPlatforms.includes(inputs.platform)
-    )
-      return;
+    if (isMultiEnvEnabled()) {
+      const lastArg = ctx.arguments[ctx.arguments.length - 1];
+      const inputs: Inputs = lastArg === ctx ? ctx.arguments[ctx.arguments.length - 2] : lastArg;
+      if (
+        !inputs.projectPath ||
+        inputs.ignoreConfigPersist === true ||
+        StaticPlatforms.includes(inputs.platform)
+      )
+        return;
 
-    const solutionContext = ctx.solutionContext;
-    if (solutionContext === undefined || solutionContext.localSettings === undefined) return;
+      const solutionContext = ctx.solutionContext;
+      if (solutionContext === undefined || solutionContext.localSettings === undefined) return;
 
-    // persistent localSettings.json.
-    const localSettingsProvider = new LocalSettingsProvider(inputs.projectPath);
-    await localSettingsProvider.save(solutionContext.localSettings);
+      // persistent localSettings.json.
+      const localSettingsProvider = new LocalSettingsProvider(inputs.projectPath);
+      await localSettingsProvider.save(solutionContext.localSettings);
 
-    const core = ctx.self as FxCore;
-    core.tools.logProvider.debug(
-      `[core] persist local settings profile: ${localSettingsProvider.localSettingsFilePath}`
-    );
+      const core = ctx.self as FxCore;
+      core.tools.logProvider.debug(
+        `[core] persist local settings profile: ${localSettingsProvider.localSettingsFilePath}`
+      );
+    }
   }
 };

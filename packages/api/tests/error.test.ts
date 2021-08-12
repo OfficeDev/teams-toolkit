10772a22d230fc77fdb6e8d35a796f9a6c34362f
huajiezhang <huajiezhang@microsoft.com>
2021-08-12 12:48:36 +0800

    fix: ut

--- packages/api/tests/error.test.ts
+++ packages/api/tests/error.test.ts
@@ -39,7 +39,7 @@ describe("error", function () {
         chai.assert.equal(temp.name, myName);
         chai.assert.equal(temp.message, myMessage);
         chai.assert.equal(temp.source, mySource);
-        chai.assert.equal(temp.stack, myStack);
+        chai.assert.isTrue(temp.stack !== undefined && temp.stack.includes("error.test.ts"));
         chai.assert.equal(temp.helpLink, myHelpLink);
         chai.assert.equal(temp.innerError, myInnerError);
       });
@@ -89,7 +89,7 @@ describe("error", function () {
         chai.assert.equal(temp.name, myName);
         chai.assert.equal(temp.message, myMessage);
         chai.assert.equal(temp.source, mySource);
-        chai.assert.equal(temp.stack, myStack);
+        chai.assert.isTrue(temp.stack !== undefined && temp.stack.includes("error.test.ts"));
         chai.assert.equal(temp.issueLink, myIssueLink);
         chai.assert.equal(temp.innerError, myInnerError);
       });

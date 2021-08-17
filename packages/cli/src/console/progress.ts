b4d90f9c77c70418b8d441602f507c6579c190aa
Alive-Fish <547850391@qq.com>
2021-08-17 15:41:46 +0800

    refactor: cli progress implementation (#1951)
    * refactor: cli progress implementation
    
    * fix: progress message is too long
    
    Co-authored-by: Zhiyu You <zhiyou@microsoft.com>

---
+++ packages/cli/src/console/progress.ts
@@ -0,0 +1,160 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import figures from "figures";
+
+import { Colors, IProgressHandler } from "@microsoft/teamsfx-api";
+
+import { getColorizedString } from "../utils";
+import ScreenManager, { Row } from "./screen";
+
+export default class Progress implements IProgressHandler {
+  private static instances: Progress[] = [];
+  private static rows: Row[] = [];
+  private static finishedRows: Row[] = [];
+
+  static readonly barSize = 20;
+
+  private static add(instance: Progress) {
+    this.instances.push(instance);
+    this.rows.push(ScreenManager.addProgress(instance.wholeMessage.bind(instance)));
+  }
+
+  private static finish(instance: Progress) {
+    const idx = this.findIndex(instance);
+    if (idx > -1) {
+      this.rows[idx].update();
+      this.rows[idx].removeCB();
+      this.finishedRows.push(this.rows[idx]);
+      this.instances.splice(idx, 1);
+      this.rows.splice(idx, 1);
+    }
+
+    if (this.instances.length === 0 && this.finishedRows.length > 0) {
+      for (const row of this.finishedRows) {
+        row.freeze();
+      }
+      this.finishedRows = [];
+    }
+  }
+
+  static end(success: boolean) {
+    const instances = [...this.instances];
+    instances.forEach((i) => i.end(success));
+  }
+
+  private static findIndex(instance: Progress) {
+    return this.instances.findIndex((i) => i === instance);
+  }
+
+  private readonly title: string;
+  private totalSteps: number;
+  private currentStep: number;
+  private currentPercentage: number;
+  private detail?: string;
+  private status?: "done" | "error" | "running";
+
+  constructor(title: string, totalSteps: number) {
+    this.totalSteps = totalSteps;
+    this.title = title;
+    this.currentStep = 0;
+    this.currentPercentage = 0;
+  }
+
+  async start(detail?: string) {
+    this.status = "running";
+    this.detail = detail;
+    this.currentStep = 0;
+    if (Progress.findIndex(this) < 0) {
+      Progress.add(this);
+    }
+  }
+
+  async end(success: boolean) {
+    this.status = success ? "done" : "error";
+    if (success) this.currentPercentage = 100;
+    if (Progress.findIndex(this) > -1) {
+      Progress.finish(this);
+    }
+  }
+
+  async next(detail?: string) {
+    this.detail = detail;
+    this.currentStep++;
+    if (this.totalSteps < this.currentStep) this.totalSteps = this.currentStep;
+  }
+
+  private updatePercentage() {
+    const needArrivedPercentage = ((this.currentStep - 1) / this.totalSteps) * 100;
+    const nextArrivedPercentage = (this.currentStep / this.totalSteps) * 100;
+    if (this.currentPercentage < needArrivedPercentage) {
+      const diff = needArrivedPercentage - this.currentPercentage;
+      this.currentPercentage += diff / ScreenManager.fps >= 5 ? diff / ScreenManager.fps : 5;
+    } else if (this.currentPercentage < nextArrivedPercentage) {
+      const diff = nextArrivedPercentage - this.currentPercentage;
+      this.currentPercentage += diff / ScreenManager.fps / 10;
+    }
+    this.currentPercentage = Math.min(this.currentPercentage, 100);
+  }
+
+  wholeMessage(): string {
+    this.updatePercentage();
+    const message =
+      this.status === "done"
+        ? this.doneMessage
+        : this.status === "error"
+        ? this.errorMessage
+        : this.message;
+    return getColorizedString([
+      {
+        content: `${this.barStatus}  ${Math.round(this.currentPercentage)}% ${
+          this.runningChar
+        } ${message}`,
+        color: Colors.BRIGHT_WHITE,
+      },
+    ]);
+  }
+
+  get barStatus(): string {
+    const completeSize = Math.round((this.currentPercentage / 100) * Progress.barSize);
+    return "█".repeat(completeSize) + "▒".repeat(Progress.barSize - completeSize);
+  }
+
+  get runningChar() {
+    const chars = ["|", "/", "-", "\\"];
+    return chars[this.status === "running" ? Math.floor(Date.now() / 1000) % 4 : 0];
+  }
+
+  get doneMessage(): string {
+    return getColorizedString([
+      {
+        content: `[${this.totalSteps}/${this.totalSteps}] ${this.title} `,
+        color: Colors.BRIGHT_WHITE,
+      },
+      { content: `(${figures.tick}) Done.`, color: Colors.BRIGHT_GREEN },
+    ]);
+  }
+
+  get errorMessage(): string {
+    return getColorizedString([
+      {
+        content: `[${this.currentStep}/${this.totalSteps}] ${this.title}: ${
+          this.detail || "starting."
+        }`,
+        color: Colors.BRIGHT_WHITE,
+      },
+      { content: ` (${figures.cross}) Failed.`, color: Colors.BRIGHT_RED },
+    ]);
+  }
+
+  get message(): string {
+    return getColorizedString([
+      {
+        content: `[${this.currentStep}/${this.totalSteps}] ${this.title}: ${
+          this.detail || "starting."
+        }`,
+        color: Colors.BRIGHT_WHITE,
+      },
+    ]);
+  }
+}
---
+++ packages/cli/tests/unit/console/progress.tests.ts
@@ -0,0 +1,109 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import sinon from "sinon";
+
+import Progress from "../../../src/console/progress";
+import ScreenManager, { Row } from "../../../src/console/screen";
+import * as Utils from "../../../src/utils";
+import { expect } from "../utils";
+
+describe("Progress", () => {
+  const sandbox = sinon.createSandbox();
+
+  beforeEach(() => {
+    Progress["instances"] = [];
+    Progress["rows"] = [];
+    Progress["finishedRows"] = [];
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it("static add", () => {
+    sandbox.stub(ScreenManager, "addProgress").returns(new Row(() => "Test static add"));
+    const instance = new Progress("Test static add", 3);
+    Progress["add"](instance);
+    expect(Progress["instances"]).deep.equals([instance]);
+  });
+
+  it("static finish", () => {
+    const updateStub = sandbox.stub(Row.prototype, "update");
+    const romoveCBStub = sandbox.stub(Row.prototype, "removeCB");
+    const freezeStub = sandbox.stub(Row.prototype, "freeze");
+    const instance = new Progress("Test static finish", 3);
+    const row = new Row(() => "Test static finish");
+    Progress["instances"] = [instance];
+    Progress["rows"] = [row];
+    Progress["finish"](instance);
+    sinon.assert.calledOnce(updateStub);
+    sinon.assert.calledOnce(romoveCBStub);
+    sinon.assert.calledOnce(freezeStub);
+    expect(Progress["instances"]).deep.equals([]);
+    expect(Progress["rows"]).deep.equals([]);
+    expect(Progress["finishedRows"]).deep.equals([]);
+  });
+
+  it("static end", () => {
+    const endStub = sandbox.stub(Progress.prototype, "end");
+    const instance = new Progress("Test static end", 3);
+    Progress["instances"] = [instance];
+    Progress["end"](true);
+    sinon.assert.calledOnce(endStub);
+  });
+
+  it("start", async () => {
+    const addStub = sandbox.stub<any, any>(Progress, "add");
+    const instance = new Progress("Test start", 3);
+    await instance.start();
+    expect(instance["status"]).equals("running");
+    expect(instance["detail"]).equals(undefined);
+    expect(instance["currentStep"]).equals(0);
+    sinon.assert.calledOnce(addStub);
+  });
+
+  it("end", async () => {
+    const finishStub = sandbox.stub<any, any>(Progress, "finish");
+    const instance = new Progress("Test finish", 3);
+    Progress["instances"] = [instance];
+    await instance.end(true);
+    expect(instance["status"]).equals("done");
+    expect(instance["currentPercentage"]).equals(100);
+    sinon.assert.calledOnce(finishStub);
+  });
+
+  it("next", async () => {
+    const instance = new Progress("Test next", 3);
+    instance["currentStep"] = 3;
+    await instance.next("step 1");
+    expect(instance["currentStep"]).equals(4);
+    expect(instance["totalSteps"]).equals(4);
+  });
+
+  it("updatePercentage", () => {
+    const instance = new Progress("Test next", 3);
+    instance["currentPercentage"] = 0;
+    instance["currentStep"] = 1;
+    instance["updatePercentage"]();
+    expect(instance["currentPercentage"]).gt(0).lte(100);
+    instance["currentStep"] = 2;
+    instance["updatePercentage"]();
+    expect(instance["currentPercentage"]).gt(5).lte(100);
+  });
+
+  it("wholeMessage", () => {
+    sandbox.stub(Utils, "getColorizedString").callsFake((messages) => {
+      return messages.map((m) => m.content).join("");
+    });
+    sandbox.stub<any, any>(Progress.prototype, "updatePercentage");
+    const instance = new Progress("Test next", 3);
+    instance["status"] = "running";
+    expect(instance.wholeMessage()).not.contains("Failed");
+    expect(instance.wholeMessage()).not.contains("Done");
+    instance["status"] = "done";
+    expect(instance.wholeMessage()).contains("Done");
+    instance["status"] = "error";
+    expect(instance.wholeMessage()).contains("Failed");
+  });
+});

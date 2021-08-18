507fd939e050a6c1b58bfa691a7183927c0f7fc0
HuihuiWu-Microsoft <73154171+HuihuiWu-Microsoft@users.noreply.github.com>
2021-08-18 15:43:25 +0800

    chore: update treatment variable code (#1966)

--- packages/vscode-extension/src/exp/treatmentVariables.ts
+++ packages/vscode-extension/src/exp/treatmentVariables.ts
@@ -2,4 +2,9 @@ export class TreatmentVariables {
   public static readonly VSCodeConfig = "vscode";
   public static readonly TreeView = "treeview";
   public static readonly DynamicTreeView = "dynamictreeview";
+  public static readonly ExpandCreateCard = "expandcreatecard";
+}
+
+export class TreatmentVariableValue {
+  public static isExpandCard: boolean | undefined = undefined;
 }

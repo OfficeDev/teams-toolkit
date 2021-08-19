0ead877eb0a8eb0cc00b7d291d817b3f46678073
zhijie <zhihuan@microsoft.com>
2021-08-19 19:51:00 +0800

    test: add ut

--- packages/fx-core/tests/plugins/resource/frontend/unit/environment-utils.test.ts
+++ packages/fx-core/tests/plugins/resource/frontend/unit/environment-utils.test.ts
@@ -4,6 +4,7 @@ import "mocha";
 import * as chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 import fs, { PathLike } from "fs-extra";
+import * as faker from "faker";
 
 import sinon from "sinon";
 import { EnvironmentUtils } from "../../../../../src/plugins/resource/frontend/utils/environment-utils";
@@ -11,32 +12,64 @@ import { EnvironmentUtils } from "../../../../../src/plugins/resource/frontend/u
 chai.use(chaiAsPromised);
 
 describe("EnvironmentUtils", async () => {
-  afterEach(() => {
-    sinon.restore();
-  });
+  const fakePath = faker.system.filePath();
 
-  const fakePath = "fake-path";
-  const fakePropertyKey = "fake-property-key";
-  const fakePropertyValue = "fake-property-value";
-  const fakeVariables = { [fakePropertyKey]: fakePropertyValue };
-  const fakeEnv = `${fakePropertyKey}=${fakePropertyValue}\r\n`;
+  const newPropertyKey = faker.unique(faker.lorem.word);
+  const newPropertyValue = faker.lorem.word();
+  const fakeVariables = { [newPropertyKey]: newPropertyValue };
 
-  it("write environments", async () => {
-    sinon.stub(fs, "ensureFile").resolves(Buffer.from(""));
-    sinon.stub(fs, "readFile").resolves(Buffer.from(""));
-    sinon.stub(fs, "writeFile").callsFake((path: number | PathLike, data: any) => {
-      chai.assert.equal(fakeEnv, data);
+  const length = 3;
+  const existingPropertyKeys = Array.from(Array(length), () => faker.unique(faker.lorem.word));
+  const existingPropertyValues = Array.from(Array(length), () => faker.lorem.word());
+  const fakeEnv = existingPropertyKeys
+    .map((v, i) => `${v}=${existingPropertyValues[i]}\r\n`)
+    .join("");
+
+  describe("write environments", async () => {
+    beforeEach(() => {
+      sinon.stub(fs, "ensureFile").resolves(Buffer.from(""));
+      sinon.stub(fs, "readFile").resolves(Buffer.from(fakeEnv));
     });
-    EnvironmentUtils.writeEnvironments(fakePath, fakeVariables);
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it("happy path", async () => {
+      sinon.stub(fs, "writeFile").callsFake((path: number | PathLike, data: any) => {
+        chai.assert.equal(`${fakeEnv}${newPropertyKey}=${newPropertyValue}\r\n`, data);
+      });
+
+      await EnvironmentUtils.writeEnvironments(fakePath, fakeVariables);
+    });
+
+    it("skip writing", async () => {
+      sinon.stub(fs, "writeFile").callsFake((path: number | PathLike, data: any) => {
+        chai.assert.fail("Should skip writing environments in this case.");
+      });
+
+      const existingVariables = { [existingPropertyKeys[0]]: existingPropertyValues[0] };
+      await EnvironmentUtils.writeEnvironments(fakePath, existingVariables);
+    });
   });
 
-  it("read environments", async () => {
-    sinon.stub(fs, "pathExists").resolves(true);
-    sinon.stub(fs, "readFile").resolves(Buffer.from(fakeEnv));
-    const envs = await EnvironmentUtils.readEnvironments(fakePath);
-    if (envs === undefined) {
-      chai.assert.fail("Read environments failed with undefined value");
-    }
-    chai.assert.equal(envs[fakePropertyKey], fakePropertyValue);
+  describe("read environments", async () => {
+    beforeEach(() => {
+      sinon.stub(fs, "pathExists").resolves(true);
+      sinon.stub(fs, "readFile").resolves(Buffer.from(fakeEnv));
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it("happy path", async () => {
+      const envs = await EnvironmentUtils.readEnvironments(fakePath);
+
+      if (envs === undefined) {
+        chai.assert.fail("Read environments failed with undefined value.");
+      }
+      chai.assert.equal(envs[existingPropertyKeys[0]], existingPropertyValues[0]);
+    });
   });
 });

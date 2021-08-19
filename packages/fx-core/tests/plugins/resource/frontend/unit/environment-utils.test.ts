60e24c8b7570a6915a0bca244d06c08d06ccfdf8
zhijie <zhihuan@microsoft.com>
2021-08-19 17:03:58 +0800

    test: update ut

--- packages/fx-core/tests/plugins/resource/frontend/unit/environment-utils.test.ts
+++ packages/fx-core/tests/plugins/resource/frontend/unit/environment-utils.test.ts
@@ -4,11 +4,9 @@ import "mocha";
 import * as chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 import fs, { PathLike } from "fs-extra";
-import * as dotenv from "dotenv";
 
 import sinon from "sinon";
 import { EnvironmentUtils } from "../../../../../src/plugins/resource/frontend/utils/environment-utils";
-import { EnvironmentVariables } from "../../../../../src/plugins/resource/frontend/constants";
 
 chai.use(chaiAsPromised);
 
@@ -25,6 +23,7 @@ describe("EnvironmentUtils", async () => {
 
   it("write environments", async () => {
     sinon.stub(fs, "ensureFile").resolves(Buffer.from(""));
+    sinon.stub(fs, "readFile").resolves(Buffer.from(""));
     sinon.stub(fs, "writeFile").callsFake((path: number | PathLike, data: any) => {
       chai.assert.equal(fakeEnv, data);
     });
@@ -39,37 +38,5 @@ describe("EnvironmentUtils", async () => {
       chai.assert.fail("Read environments failed with undefined value");
     }
     chai.assert.equal(envs[fakePropertyKey], fakePropertyValue);
-  });
-
-  it("update environment", async () => {
-    sinon.stub(fs, "ensureFile").resolves(Buffer.from(""));
-    sinon.stub(fs, "readFile").resolves(Buffer.from(fakeEnv));
-
-    const functionEnv = {
-      defaultName: "fake-function-name",
-      endpoint: "fake-function-endpoint",
-    };
-
-    const runtimeEnv = {
-      endpoint: "fake-runtime-endpoint",
-      startLoginPageUrl: "fake-start-login-page-url",
-    };
-
-    const aadEnv = {
-      clientId: "fake-aad-client-id",
-    };
-    sinon.stub(fs, "writeFile").callsFake((path: number | PathLike, data: any) => {
-      const configs = dotenv.parse(data);
-      chai.assert.equal(configs[fakePropertyKey], fakePropertyValue);
-      chai.assert.equal(configs[EnvironmentVariables.FuncName], functionEnv["defaultName"]);
-      chai.assert.equal(configs[EnvironmentVariables.FuncEndpoint], functionEnv["endpoint"]);
-      chai.assert.equal(
-        configs[EnvironmentVariables.StartLoginPage],
-        runtimeEnv["startLoginPageUrl"]
-      );
-      chai.assert.equal(configs[EnvironmentVariables.RuntimeEndpoint], runtimeEnv["endpoint"]);
-      chai.assert.equal(configs[EnvironmentVariables.ClientID], aadEnv.clientId);
-    });
-    await EnvironmentUtils.updateEnvironment(fakePath, runtimeEnv, aadEnv, functionEnv);
   });
 });

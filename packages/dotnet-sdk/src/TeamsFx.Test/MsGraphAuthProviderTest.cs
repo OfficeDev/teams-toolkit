0068831d0a18b8055102ff3de433edb00e887368
Ning Tang <nintan@microsoft.com>
2021-08-24 13:57:46 +0800

    feat(dotnet-sdk): add NuGet package

---
+++ packages/dotnet-sdk/src/TeamsFx.Test/MsGraphAuthProviderTest.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.JSInterop;
+using Microsoft.TeamsFx.Test.Helper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Microsoft.TeamsFx.Test
+{
+    [TestClass]
+    public class MsGraphAuthProviderTest
+    {
+        private static Mock<IJSObjectReference> teamsUserCredentialModuleMock;
+        private static TeamsUserCredential teamsCredential;
+        private static MsGraphAuthProvider msGraphAuthProvider;
+
+        [ClassInitialize]
+        public static void TestFixtureSetup(TestContext context)
+        {
+            // Executes once for the test class. (Optional)
+            var jsRuntimeMock = new Mock<IJSRuntime>();
+            var moduleMock = new Mock<IJSObjectReference>();
+            teamsUserCredentialModuleMock = new Mock<IJSObjectReference>();
+            jsRuntimeMock.Setup(r => r.InvokeAsync<IJSObjectReference>("import", It.IsAny<object[]>())).ReturnsAsync(() => moduleMock.Object);
+            moduleMock.Setup(m => m.InvokeAsync<IJSObjectReference>("createTeamsUserCredential", It.IsAny<object[]>())).ReturnsAsync(() => teamsUserCredentialModuleMock.Object);
+            teamsCredential = new TeamsUserCredential(jsRuntimeMock.Object);
+            msGraphAuthProvider = new MsGraphAuthProvider(teamsCredential);
+        }
+
+        [TestMethod]
+        public void TestCreateMsGraphAuthProvider()
+        {
+            var defaultScopeArray = new string[] { ".default" };
+
+            var msGraphAuthProvider1 = new MsGraphAuthProvider(teamsCredential);
+            CollectionAssert.AreEqual(msGraphAuthProvider1._scopes, defaultScopeArray);
+
+            var msGraphAuthProvider2 = new MsGraphAuthProvider(teamsCredential, "");
+            CollectionAssert.AreEqual(msGraphAuthProvider2._scopes, defaultScopeArray);
+
+            var msGraphAuthProvider3 = new MsGraphAuthProvider(teamsCredential, System.Array.Empty<string>());
+            CollectionAssert.AreEqual(msGraphAuthProvider3._scopes, defaultScopeArray);
+
+            var msGraphAuthProvider4 = new MsGraphAuthProvider(teamsCredential, new string[] { "" });
+            CollectionAssert.AreEqual(msGraphAuthProvider4._scopes, defaultScopeArray);
+        }
+
+        [TestMethod]
+        public async Task TestAuthenticateRequestAsync()
+        {
+            TokenMockHelper.MockUpGetToken(teamsUserCredentialModuleMock);
+            var request = new HttpRequestMessage();
+            await msGraphAuthProvider.AuthenticateRequestAsync(request);
+            Assert.IsNotNull(request.Headers.Authorization);
+            teamsUserCredentialModuleMock.VerifyAll();
+        }
+
+        [TestMethod]
+        public async Task TestGetAccessTokenAsync()
+        {
+            TokenMockHelper.MockUpGetToken(teamsUserCredentialModuleMock);
+            var result = await msGraphAuthProvider.GetAccessTokenAsync();
+            Assert.AreEqual(TokenMockHelper.accessTokenJS.Token, result);
+            teamsUserCredentialModuleMock.VerifyAll();
+        }
+
+        [TestMethod]
+        public async Task TestGetAccessTokenAsyncException()
+        {
+            TokenMockHelper.MockUpGetEmptyToken(teamsUserCredentialModuleMock);
+            var ex = await Assert.ThrowsExceptionAsync<ExceptionWithCode>(async () => await msGraphAuthProvider.GetAccessTokenAsync());
+            Assert.AreEqual(ex.Code, ExceptionCode.InternalError);
+            teamsUserCredentialModuleMock.VerifyAll();
+        }
+    }
+}
---
+++ packages/dotnet-sdk/src/TeamsFx.Test/TeamsUserCrendentialTest.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Azure.Core;
+using Microsoft.JSInterop;
+using Microsoft.TeamsFx.Model;
+using Microsoft.TeamsFx.Test.Helper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.TeamsFx.Test
+{
+    [TestClass]
+    public class TeamsUserCrendentialTest
+    {
+        private static TeamsUserCredential teamsCredential;
+        private static Mock<IJSObjectReference> instanceTask;
+
+        [ClassInitialize]
+        public static void TestFixtureSetup(TestContext context)
+        {
+            // Executes once for the test class. (Optional)
+            var jsRuntimeMock = new Mock<IJSRuntime>();
+            var moduleMock = new Mock<IJSObjectReference>();
+            instanceTask = new Mock<IJSObjectReference>();
+            jsRuntimeMock.Setup(r => r.InvokeAsync<IJSObjectReference>("import", It.IsAny<object[]>())).ReturnsAsync(() => moduleMock.Object);
+            moduleMock.Setup(m => m.InvokeAsync<IJSObjectReference>("createTeamsUserCredential", It.IsAny<object[]>())).ReturnsAsync(() => instanceTask.Object);
+            teamsCredential = new TeamsUserCredential(jsRuntimeMock.Object);
+        }
+
+        [TestMethod]
+        public async Task TestGetUserInfoAsync()
+        {
+            var result = await teamsCredential.GetUserInfoAsync();
+            Assert.IsNull(result);
+            instanceTask.Verify(m => m.InvokeAsync<UserInfo>("getUserInfo", It.IsAny<object[]>()));
+        }
+
+        [TestMethod]
+        public async Task TestLoginAsync()
+        {
+            await teamsCredential.LoginAsync(".default");
+            instanceTask.Verify(m => m.InvokeAsync<object>("login", It.Is<object[]>(x => (string)x[0] == ".default")));
+        }
+
+        [TestMethod]
+        public async Task TestGetTokenAsync()
+        {
+            TokenMockHelper.MockUpGetToken(instanceTask);
+            var result = await teamsCredential.GetTokenAsync(
+                        new TokenRequestContext(new string[] { "User.Read" }),
+                        new CancellationToken());
+            Assert.AreEqual(TokenMockHelper.accessTokenJS.Token, result.Token);
+            Assert.AreEqual(TokenMockHelper.accessTokenJS.ExpiresOn, result.ExpiresOn);
+            instanceTask.VerifyAll();
+        }
+
+        [TestMethod]
+        public async Task TestGetTokenAsyncException()
+        {
+            TokenMockHelper.MockUpGetTokenThrowException(instanceTask);
+            var ex = await Assert.ThrowsExceptionAsync<ExceptionWithCode>(
+                async () => await teamsCredential.GetTokenAsync(
+                        new TokenRequestContext(new string[] { "User.Read" }),
+                        new CancellationToken()));
+            Assert.AreEqual(ex.Code, ExceptionCode.ServiceError);
+        }
+    }
+}

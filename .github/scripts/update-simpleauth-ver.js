5b02a06bfb1d2a2a2eb1cce07610f287dbe05e83
Wenyu Tang <wenyutang@microsoft.com>
2021-08-24 21:18:59 +0800

    chore: update

--- .github/scripts/update-simpleauth-ver.js
+++ .github/scripts/update-simpleauth-ver.js
@@ -38,3 +38,38 @@ fs.readFile(csprojFile, "utf-8", (err, data) => {
 
     });
 });
+
+const synup = process.env.SkipSyncup
+if(synup && synup.includes("fx-core")) {
+    return;
+}
+
+// update fx-core tempaltes version.txt file
+const fxCorePath = path.join(__dirname, "../../packages/fx-core");
+const simpleauthVerTxt = path.join(fxCorePath, "./templates/plugins/resource/simpleauth/version.txt")
+fs.writeFileSync(simpleauthVerTxt, simpleauthVer, "utf8")
+
+// only rc and stable release bump up version on main branch, rarely run.
+let needBumpUp = process.argv[2] === "yes" ? true : false;
+if (needBumpUp) {
+    let file = path.join(fxCorePath, "package.json");
+    let pkg_ = fse.readJsonSync(file);
+    let ver = pkg_.version;
+    if (semver.prerelease(simpleauthVer)) {
+        ver = semver.inc(ver, "prerelease", "rc");
+    } else {
+        ver = semver.inc(ver, "patch");
+    }
+
+    pkg_.version = ver;
+    fse.writeFileSync(file, JSON.stringify(pkg_, null, 4));
+
+    file = path.join(fxCorePath, "package-lock.json");
+    if (file) {
+        pkg_ = fse.readJsonSync(file);
+        pkg_.version = ver;
+        fse.writeFileSync(file, JSON.stringify(pkg_, null, 4))
+    }
+
+    console.log("bump up fx-core version as ", ver);
+}

8940a7ac22ef8bfce9027518ed80cdb2de7c444d
Qinen Zhu <qinzhu@microsoft.com>
2021-08-26 10:40:55 +0800

    refactor(multi-env): support to create new env config

--- packages/fx-core/src/core/environment.ts
+++ packages/fx-core/src/core/environment.ts
@@ -14,6 +14,8 @@ import {
   Result,
   SystemError,
   InputConfigsFolderName,
+  EnvConfigFileNameTemplate,
+  envNamePlaceholder,
 } from "@microsoft/teamsfx-api";
 import path, { basename } from "path";
 import fs from "fs-extra";
@@ -52,13 +54,18 @@ export interface EnvProfileFiles {
 }
 
 class EnvironmentManager {
-  public readonly defaultEnvName = "default";
-  public readonly defaultEnvNameNew = "dev";
   public readonly envNameRegex = /^[\w\d-_]+$/;
   public readonly envConfigNameRegex = /config\.(?<envName>[\w\d-_]+)\.json/i;
   public readonly envProfileNameRegex = /profile\.(?<envName>[\w\d-_]+)\.json/i;
 
+  private readonly defaultEnvName = "default";
+  private readonly defaultEnvNameNew = "dev";
   private readonly ajv;
+  private readonly schema =
+    "https://github.com/OfficeDev/TeamsFx/blob/dev/packages/api/src/schemas/envConfig.json";
+  private readonly manifestConfigDescription =
+    `You can customize the 'values' object to customize Teams app manifest for different environments.` +
+    ` Visit https://aka.ms/teamsfx-config to learn more about this.`;
 
   constructor() {
     this.ajv = new Ajv();
@@ -86,6 +93,45 @@ class EnvironmentManager {
     }
 
     return ok({ envName, config: configResult.value, profile: profileResult.value });
+  }
+
+  public newEnvConfigData(): EnvConfig {
+    const envConfig: EnvConfig = {
+      $schema: this.schema,
+      azure: {},
+      manifest: {
+        description: this.manifestConfigDescription,
+        values: {},
+      },
+    };
+
+    return envConfig;
+  }
+
+  public async writeEnvConfig(
+    projectPath: string,
+    envConfig: EnvConfig,
+    envName?: string
+  ): Promise<Result<string, FxError>> {
+    if (!(await fs.pathExists(projectPath))) {
+      return err(PathNotExistError(projectPath));
+    }
+
+    const envConfigsFolder = this.getEnvConfigsFolder(projectPath);
+    if (!(await fs.pathExists(envConfigsFolder))) {
+      await fs.ensureDir(envConfigsFolder);
+    }
+
+    envName = envName ?? this.getDefaultEnvName();
+    const envConfigPath = this.getEnvConfigPath(envName, projectPath);
+
+    try {
+      await fs.writeFile(envConfigPath, JSON.stringify(envConfig, null, 4));
+    } catch (error) {
+      return err(WriteFileError(error));
+    }
+
+    return ok(envConfigPath);
   }
 
   public async writeEnvProfile(
@@ -154,7 +200,7 @@ class EnvironmentManager {
 
   public getEnvConfigPath(envName: string, projectPath: string): string {
     const basePath = this.getEnvConfigsFolder(projectPath);
-    return path.resolve(basePath, `config.${envName}.json`);
+    return path.resolve(basePath, EnvConfigFileNameTemplate.replace(envNamePlaceholder, envName));
   }
 
   public getEnvProfileFilesPath(envName: string, projectPath: string): EnvProfileFiles {
@@ -162,68 +208,12 @@ class EnvironmentManager {
     const envProfile = path.resolve(
       basePath,
       isMultiEnvEnabled()
-        ? EnvProfileFileNameTemplate.replace("@envName", envName)
+        ? EnvProfileFileNameTemplate.replace(envNamePlaceholder, envName)
         : `env.${envName}.json`
     );
     const userDataFile = path.resolve(basePath, `${envName}.userdata`);
 
     return { envProfile, userDataFile };
-  }
-  
-  private async loadEnvConfig(
-    projectPath: string,
-    envName: string
-  ): Promise<Result<EnvConfig, FxError>> {
-    const envConfigPath = this.getEnvConfigPath(envName, projectPath);
-
-    if (!(await fs.pathExists(envConfigPath))) {
-      return isMultiEnvEnabled()
-        ? err(ProjectEnvNotExistError(envName))
-        : ok({
-            azure: {},
-            manifest: { values: {} },
-          });
-    }
-
-    const validate = this.ajv.compile<EnvConfig>(envConfigSchema);
-    const data = await fs.readJson(envConfigPath);
-    if (validate(data)) {
-      return ok(data);
-    }
-
-    // never throw error while loading config.xxx.json if the feature flag of multi env is disabled.
-    return isMultiEnvEnabled()
-      ? err(InvalidEnvConfigError(envName, JSON.stringify(validate.errors)))
-      : ok({
-          azure: {},
-          manifest: { values: {} },
-        });
-  }
-
-  private async loadEnvProfile(
-    projectPath: string,
-    envName: string,
-    cryptoProvider?: CryptoProvider
-  ): Promise<Result<Map<string, any>, FxError>> {
-    const envFiles = this.getEnvProfileFilesPath(envName, projectPath);
-    const userDataResult = await this.loadUserData(envFiles.userDataFile, cryptoProvider);
-    if (userDataResult.isErr()) {
-      return err(userDataResult.error);
-    }
-    const userData = userDataResult.value;
-
-    if (!(await fs.pathExists(envFiles.envProfile))) {
-      const data = new Map<string, any>([[GLOBAL_CONFIG, new ConfigMap()]]);
-
-      return ok(data);
-    }
-
-    const envData = await readJson(envFiles.envProfile);
-
-    mergeSerectData(userData, envData);
-    const data = objectToMap(envData);
-
-    return ok(data);
   }
 
   private async loadEnvConfig(

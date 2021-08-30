77b6360e071db24fcbd0fb947d37b015d08a4458
huajiezhang <huajiezhang@microsoft.com>
2021-08-30 15:24:34 +0800

    feat: convert some SystemError to UserError

--- packages/fx-core/src/core/middleware/errorHandler.ts
+++ packages/fx-core/src/core/middleware/errorHandler.ts
@@ -2,28 +2,74 @@
 // Licensed under the MIT license.
 "use strict";
 
-import { HookContext, NextFunction, Middleware } from "@feathersjs/hooks"; 
-import { assembleError, err, Inputs } from "@microsoft/teamsfx-api";
+import { HookContext, NextFunction, Middleware } from "@feathersjs/hooks";
+import { assembleError, err, Inputs, SystemError, UserError } from "@microsoft/teamsfx-api";
 import { FxCore } from "..";
 
 /**
  * in case there're some uncatched exceptions, this middleware will act as a guard
  * to catch exceptions and return specific error.
  */
-export const ErrorHandlerMW: Middleware = async (
-  ctx: HookContext,
-  next: NextFunction
-) => {
+export const ErrorHandlerMW: Middleware = async (ctx: HookContext, next: NextFunction) => {
   const core = ctx.self as FxCore;
   const inputs = ctx.arguments[ctx.arguments.length - 1] as Inputs;
-  const logger = (core !== undefined && core.tools!== undefined && core.tools.logProvider!== undefined) ? core.tools.logProvider:undefined;
+  const logger =
+    core !== undefined && core.tools !== undefined && core.tools.logProvider !== undefined
+      ? core.tools.logProvider
+      : undefined;
   try {
-    if(logger)
-      logger.info(`[core] start task:${ctx.method}, inputs:${JSON.stringify(inputs)}`);
+    if (logger) logger.info(`[core] start task:${ctx.method}, inputs:${JSON.stringify(inputs)}`);
     await next();
-    if(logger)
-      logger.info(`[core] finish task:${ctx.method}`);
+    if (logger) logger.info(`[core] finish task:${ctx.method}`);
   } catch (e) {
-    ctx.result = err(assembleError(e));
+    let fxError = assembleError(e);
+    if (fxError instanceof SystemError) {
+      fxError = await tryConvertToUserError(fxError);
+    }
+    ctx.result = err(fxError);
   }
 };
+
+const Reg1 =
+  /The client '.+' with object id '.+' does not have authorization to perform action '.+' over scope '.+' or the scope is invalid. If access was recently granted, please refresh your credentials\./;
+const Reg2 = /"resourceGroupName" with value ".+" should satisfy the constraint "Pattern"/;
+const Reg3 = /Resource '.+' was disallowed by policy./;
+const Reg4 =
+  /The subscription '.+' is disabled and therefore marked as read only. You cannot perform any write actions on this subscription until it is re-enabled\./;
+const Reg5 =
+  /The current subscription type is not permitted to perform operations on any provider namespace. Please use a different subscription\./;
+const Reg6 =
+  /The provided location '.+' is not available for resource group\. List of available regions is '.+'\./;
+const Reg7 = /The subscription '.+' could not be found\./;
+const Reg8 =
+  /Invalid resource group location '.+'. The Resource group already exists in location '.+'\./;
+const Reg9 = /The access token is from the wrong issuer '.+'\./;
+const Reg10 = /Entry not found in cache\./;
+const Reg11 = /request to .+ failed, reason: .+/;
+
+// const Reg12 = /ENOENT: no such file or directory/;
+// const Reg13 = /EBUSY: resource busy or locked/;
+// const Reg14 = /Lock is not .+ by you/;
+// const Reg15 = /EPERM: operation not permitted/;
+
+const Regs = [Reg1, Reg2, Reg3, Reg4, Reg5, Reg6, Reg7, Reg8, Reg9, Reg10, Reg11];
+
+async function tryConvertToUserError(err: SystemError): Promise<UserError | SystemError> {
+  const msg = err.message;
+  if (!msg) return err;
+  for (const reg of Regs) {
+    if (reg.test(msg) === true) {
+      const userError = new UserError(
+        err.name,
+        err.message,
+        err.source,
+        undefined,
+        undefined,
+        err.innerError
+      );
+      userError.stack = err.stack;
+      return userError;
+    }
+  }
+  return err;
+}

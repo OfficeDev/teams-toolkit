c36fce9e323f3e396b354ecf743a0470b26b787e
LongOddCode <haolong@microsoft.com>
2021-08-31 14:45:07 +0800

    chore: add base64encode function

--- packages/fx-core/tests/core/tools.test.ts
+++ packages/fx-core/tests/core/tools.test.ts
@@ -1,14 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-import {
-  base64Encode,
-  isValidProject,
-  newEnvInfo,
-  validateV1PackageSettings,
-} from "../../src/core/tools";
+import { base64Encode, isValidProject, newEnvInfo } from "../../src/core/tools";
 import "mocha";
-
-import { EnvInfo } from "@microsoft/teamsfx-api";
 
 import { expect } from "chai";
 
@@ -24,11 +17,6 @@ describe("tools", () => {
     expect(result.envName).to.be.not.empty;
     expect(result.config).to.be.not.null;
     expect(result.profile).to.be.not.null;
-  });
-
-  it("validateV1PackageSettings should return false if msteams if undefined", () => {
-    expect(validateV1PackageSettings(undefined)).to.be.false;
-    expect(validateV1PackageSettings({ msteams: undefined })).to.be.false;
   });
 
   it("is not valid project", () => {

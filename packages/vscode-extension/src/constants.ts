39934c5e35ac0d24e74213c92318e5a64fc48a9a
Kuojian Lu <kuojianlu@gmail.com>
2021-08-31 16:02:58 +0800

    fix(migrate-v1): add learn more button to migrating v1 notification

--- packages/vscode-extension/src/constants.ts
+++ packages/vscode-extension/src/constants.ts
@@ -3,3 +3,6 @@ export enum ConfigurationKey {
   ArmSupportEnabled = "armSupportEnabled",
   BicepEnvCheckerEnable = "validateBicep",
 }
+
+export const migrateV1DocUrl =
+  "https://github.com/OfficeDev/TeamsFx/blob/dev/docs/vscode-extension/migrate-v1/migrate-v1.md";

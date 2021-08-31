ec99344c1113874aa9bbd282f3e43f88c9b7d171
Kuojian Lu <kuojianlu@gmail.com>
2021-08-31 16:39:16 +0800

    fix: use aka link

--- packages/vscode-extension/src/constants.ts
+++ packages/vscode-extension/src/constants.ts
@@ -4,5 +4,4 @@ export enum ConfigurationKey {
   BicepEnvCheckerEnable = "validateBicep",
 }
 
-export const migrateV1DocUrl =
-  "https://github.com/OfficeDev/TeamsFx/blob/dev/docs/vscode-extension/migrate-v1/migrate-v1.md";
+export const migrateV1DocUrl = "https://aka.ms//teamsfx-migrate-v1";

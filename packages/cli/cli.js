19d97edef5babd3505889b83ac328d7347d00715
Zhiyu You <zhiyou@microsoft.com>
2021-09-01 10:20:14 +0800

    fix: change node version error in cli

--- packages/cli/cli.js
+++ packages/cli/cli.js
@@ -3,5 +3,20 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+const chalk = require("chalk");
+
+process.on("uncaughtException", (err) => {
+  if (err.message.includes("async_hooks")) {
+    console.error(
+      chalk.redBright(
+        "TeamsFx CLI requires to use node version higher than 12.x, please update your node version."
+      )
+    );
+  } else {
+    console.error(err);
+  }
+  process.exit(1);
+});
+
 const cli = require("./lib");
 cli.start();

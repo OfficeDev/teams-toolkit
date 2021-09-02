434caff2ba7591779cafb16dece778b28f35afa1
wenytang-ms <wenyutang@microsoft.com>
2021-09-02 11:17:17 +0800

    chore: update secrets

--- .github/scripts/setup-testing-env.sh
+++ .github/scripts/setup-testing-env.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
-# clientId=$(echo $creds | jq -r '.clientId')
-# clientSecret=$(echo $creds | jq -r '.clientSecret')
-# subscriptionId=$(echo $creds | jq -r '.subscriptionId')
-# tenantId=$(echo $creds | jq -r '.tenantId')
-# tokencc=$(curl -X POST -F "grant_type=client_credentials" -F "client_id=$clientId" -F "client_secret=$clientSecret" -F "scope=https://vault.azure.net/.default" "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" | jq -r '.access_token') 
+clientId=$(echo $creds | jq -r '.clientId')
+clientSecret=$(echo $creds | jq -r '.clientSecret')
+subscriptionId=$(echo $creds | jq -r '.subscriptionId')
+tenantId=$(echo $creds | jq -r '.tenantId')
+token=$(curl -X POST -F "grant_type=client_credentials" -F "client_id=$clientId" -F "client_secret=$clientSecret" -F "scope=https://vault.azure.net/.default" "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" | jq -r '.access_token') 
 if [ $1 == 'e2e' ]; then
 declare -A E2EMap=(["E2E-AZURE-ACCOUNT-NAME"]="AZURE_ACCOUNT_NAME" ["E2E-AZURE-ACCOUNT-PASSWORD"]="AZURE_ACCOUNT_PASSWORD"\
- ["E2E-AZURE-SUBSCRIPTION-ID"]="AZURE_SUBSCRIPTION_ID" ["E2E-AZURE-TENANT-ID"]="AZURE_TENANT_ID" ["M365_ACCOUNT_NAME"]="E2E-M365-ACCOUNT-NAME" \
+ ["E2E-AZURE-SUBSCRIPTION-ID"]="AZURE_SUBSCRIPTION_ID" ["E2E-AZURE-TENANT-ID"]="AZURE_TENANT_ID" ["E2E-M365-ACCOUNT-NAME"]="M365_ACCOUNT_NAME" \
  ["E2E-M365-ACCOUNT-PASSWORD"]="M365_ACCOUNT_PASSWORD" ["E2E-M365-TENANT-ID"]="M365_TENANT_ID" ["E2E-TEST-COLLABORATOR-USER-NAME"]="M365_ACCOUNT_COLLABORATOR")
-# value=$(curl -H 'Accept: application/json' -H "Authorization: Bearer $tokencc" https://e2etestenv.vault.azure.net/secrets/$1?api-version=2016-10-01 |jq -r '.value')
-# echo value
 for key in ${!E2EMap[*]}; do
-    echo $key
-    echo ${E2EMap[$key]}
+    value=$(curl -H 'Accept: application/json' -H "Authorization: Bearer $token" "https://e2etestenv.vault.azure.net/secrets/$key?api-version=2016-10-01" |jq -r '.value')
+    echo "${E2EMap[$key]}=$value" >> $2
 done
-elif [ $1 == 'simpleauth']; then
-
+elif [ $1 == 'simpleauth' ]; then
+declare -A SimpleAuthMap=(["SIMPLE-AUTH-TEST-ADMIN-CLIENT-ID"]="TEAMS_SIMPLE_AUTH_IntegrationTestSettings__AdminClientId" ["SIMPLE-AUTH-TEST-ADMIN-CLIENT-SECRET"]="TEAMS_SIMPLE_AUTH_IntegrationTestSettings__AdminClientSecret"\
+ ["SIMPLE-AUTH-TEST-TENANT-ID"]="TEAMS_SIMPLE_AUTH_IntegrationTestSettings__TenantId" ["SIMPLE-AUTH-TEST-USER-NAME"]="TEAMS_SIMPLE_AUTH_IntegrationTestSettings__TestUserName" ["SIMPLE-AUTH-TEST-PASSWORD"]="TEAMS_SIMPLE_AUTH_IntegrationTestSettings__TestPassword"\
+ ["SIMPLE-AUTH-TEST-USER-NAME-2"]="TEAMS_SIMPLE_AUTH_IntegrationTestSettings__TestUserName2" ["SIMPLE-AUTH-TEST-PASSWORD-2"]="TEAMS_SIMPLE_AUTH_IntegrationTestSettings__TestPassword2") 
+for key in ${!SimpleAuthMap[*]}; do
+    value=$(curl -H 'Accept: application/json' -H "Authorization: Bearer $token" "https://e2etestenv.vault.azure.net/secrets/$key?api-version=7.0" |jq -r '.value')
+    echo "${SimpleAuthMap[$key]}=$value" >> $2
+done
 elif [ $1 == 'function-extension' ]; then
+
 fi

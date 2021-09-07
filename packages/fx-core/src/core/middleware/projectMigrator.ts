703ab9ab86d3acce099de722d9eee18c55f1c3d5
pengfeizhao <pengfeizhao@microsoft.com>
2021-09-07 16:47:32 +0800

    chore: add base structure for ProjectMigrator Middleware

--- packages/fx-core/src/core/middleware/projectMigrator.ts
+++ packages/fx-core/src/core/middleware/projectMigrator.ts
@@ -6,5 +6,33 @@ import { CoreHookContext } from "../..";
 import { Middleware, NextFunction } from "@feathersjs/hooks/lib";
 
 export const ProjectMigratorMW: Middleware = async (ctx: CoreHookContext, next: NextFunction) => {
+  if (await needMigrateToArmAndMultiEnv(ctx)) {
+    // TODO: ui - user confirm
+    const isMigrationSelected = false;
+    if (isMigrationSelected) {
+      await migrateToArmAndMultiEnv(ctx);
+      await next();
+    }
+    return;
+  }
   await next();
 };
+
+async function needMigrateToArmAndMultiEnv(ctx: CoreHookContext): Promise<boolean> {
+  return false;
+}
+
+async function migrateToArmAndMultiEnv(ctx: CoreHookContext): Promise<void> {
+  try {
+    await migrateArm(ctx);
+    await migrateMultiEnv(ctx);
+  } finally {
+    await cleanup(ctx);
+  }
+}
+
+async function migrateMultiEnv(ctx: CoreHookContext) {}
+
+async function migrateArm(ctx: CoreHookContext) {}
+
+async function cleanup(ctx: CoreHookContext) {}

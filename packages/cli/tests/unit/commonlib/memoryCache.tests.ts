ff8cb10982b55f62cc3b90fc8a0a816eab52a8ae
LongOddCode <haolong@microsoft.com>
2021-09-07 14:53:13 +0800

    chore: sync cli e2e tests to testplan

--- packages/cli/tests/unit/commonlib/memoryCache.tests.ts
+++ packages/cli/tests/unit/commonlib/memoryCache.tests.ts
@@ -28,4 +28,26 @@ describe("memory cache tests", () => {
 
     expect(m.size()).equal(0);
   });
+
+  it("we can add/find/remove stuff in memorycache", () => {
+    /* eslint-disable @typescript-eslint/ban-ts-comment */
+
+    // @ts-ignore
+
+    const m = new MemoryCache();
+
+    m.add(1, (e: Error, y: boolean) => {
+      expect(e).is.null;
+
+      expect(y).is.true;
+    });
+
+    m.find(1, (e: Error) => {
+      expect(e).is.null;
+    });
+
+    m.remove(1, (e: Error) => {
+      expect(e).is.undefined;
+    });
+  });
 });

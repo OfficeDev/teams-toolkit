d62db88522d1159eed2a39e25d7df5f5cdf84bcf
Zihong <zihch@microsoft.com>
2021-09-14 22:06:32 +0800

    chore(vsc): add upgrade user propery in telemetry (#2313)
    * chore(vsc): add upgrade user propery in telemetry
    
    * fix(vsc): fix unit test
    
    * fix(vsc): fix unit test
    
    * fix(vsc): fix typo
    
    * chore(vsc): add version util unit test

--- packages/vscode-extension/src/constants.ts
+++ packages/vscode-extension/src/constants.ts
@@ -9,3 +9,7 @@ export const migrateV1DocUrl = "https://aka.ms/teamsfx-migrate-v1";
 export enum SyncedState {
   Version = "teamsToolkit:synced:version",
 }
+
+export enum UserState {
+  IsExisting = "teamsToolkit:user:isExisting",
+}

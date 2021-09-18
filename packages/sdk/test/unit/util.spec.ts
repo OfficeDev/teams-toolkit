44f4d1ac0dc38ce09e97205d58feb25abb51737c
wenytang <wenyutang@microsoft.com>
2021-09-18 11:09:50 +0800

    chore: test

---
+++ packages/sdk/test/unit/util.spec.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { assert, expect } from "chai";
+import { ErrorWithCode, ErrorCode } from "../../src";
+import { validateScopesType } from "../../src/util/utils";
+
+console.log("just for test lint-pr");

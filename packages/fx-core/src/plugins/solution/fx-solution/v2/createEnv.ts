524a2744398e2d634c37056a2ccf5b5f765d4499
Huajie Zhang <zhjay23@qq.com>
2021-09-22 12:07:41 +0800

    fix(solution): v2 createEnv (#2406)
    * fix(solution): v2 createEnv
    
    * fix: context

--- packages/fx-core/src/plugins/solution/fx-solution/v2/createEnv.ts
+++ packages/fx-core/src/plugins/solution/fx-solution/v2/createEnv.ts
@@ -20,10 +20,15 @@ export async function createEnv(ctx: v2.Context, inputs: Inputs): Promise<Result
     isAzureProject(ctx.projectSetting.solutionSettings as AzureSolutionSettings)
   ) {
     const solutionContext: SolutionContext = {
+      root: inputs.projectPath || "",
       envInfo: newEnvInfo(inputs.targetEnvName),
-      root: inputs.projectPath!,
-      ...ctx,
+      projectSettings: ctx.projectSetting,
       answers: inputs,
+      logProvider: ctx.logProvider,
+      telemetryReporter: ctx.telemetryReporter,
+      cryptoProvider: ctx.cryptoProvider,
+      permissionRequestProvider: ctx.permissionRequestProvider,
+      ui: ctx.userInteraction,
     };
     try {
       if (inputs.copy === true) {

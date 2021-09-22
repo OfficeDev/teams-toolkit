c469aabd75ad1a76ca6b75fec019f79e380be35a
Qinen Zhu <qinzhu@microsoft.com>
2021-09-22 12:52:57 +0800

    fix(multi-env): process env is not ready when hooks init

--- packages/fx-core/src/core/index.ts
+++ packages/fx-core/src/core/index.ts
@@ -455,7 +455,7 @@ export class FxCore implements Core {
     ConcurrentLockerMW,
     ProjectMigratorMW,
     ProjectSettingsLoaderMW,
-    EnvInfoLoaderMW(isMultiEnvEnabled()),
+    EnvInfoLoaderMW(false),
     SolutionLoaderMW(),
     QuestionModelMW,
     ContextInjectorMW,
@@ -503,7 +503,7 @@ export class FxCore implements Core {
     ConcurrentLockerMW,
     ProjectMigratorMW,
     ProjectSettingsLoaderMW,
-    EnvInfoLoaderMW(isMultiEnvEnabled()),
+    EnvInfoLoaderMW(false),
     SolutionLoaderMW(),
     QuestionModelMW,
     ContextInjectorMW,
@@ -536,7 +536,7 @@ export class FxCore implements Core {
     ProjectMigratorMW,
     ProjectUpgraderMW,
     ProjectSettingsLoaderMW,
-    EnvInfoLoaderMW(false),
+    EnvInfoLoaderMW(true),
     LocalSettingsLoaderMW,
     SolutionLoaderMW(),
     QuestionModelMW,
@@ -588,7 +588,7 @@ export class FxCore implements Core {
     ConcurrentLockerMW,
     ProjectMigratorMW,
     ProjectSettingsLoaderMW,
-    EnvInfoLoaderMW(isMultiEnvEnabled()),
+    EnvInfoLoaderMW(false),
     SolutionLoaderMW(),
     QuestionModelMW,
     ContextInjectorMW,
@@ -618,7 +618,7 @@ export class FxCore implements Core {
     ConcurrentLockerMW,
     ProjectMigratorMW,
     ProjectSettingsLoaderMW,
-    EnvInfoLoaderMW(isMultiEnvEnabled()),
+    EnvInfoLoaderMW(false),
     LocalSettingsLoaderMW,
     SolutionLoaderMW(),
     QuestionModelMW,
@@ -666,7 +666,7 @@ export class FxCore implements Core {
   @hooks([
     ErrorHandlerMW,
     ProjectSettingsLoaderMW,
-    EnvInfoLoaderMW(false),
+    EnvInfoLoaderMW(true),
     SolutionLoaderMW(),
     ContextInjectorMW,
     EnvInfoWriterMW(),
@@ -704,7 +704,7 @@ export class FxCore implements Core {
   @hooks([
     ErrorHandlerMW,
     ProjectSettingsLoaderMW,
-    EnvInfoLoaderMW(false),
+    EnvInfoLoaderMW(true),
     SolutionLoaderMW(),
     ContextInjectorMW,
     EnvInfoWriterMW(),
@@ -736,7 +736,7 @@ export class FxCore implements Core {
     ConcurrentLockerMW,
     ProjectMigratorMW,
     ProjectSettingsLoaderMW,
-    EnvInfoLoaderMW(isMultiEnvEnabled()),
+    EnvInfoLoaderMW(false),
     LocalSettingsLoaderMW,
     ContextInjectorMW,
   ])
@@ -763,7 +763,7 @@ export class FxCore implements Core {
     ErrorHandlerMW,
     ProjectMigratorMW,
     ProjectSettingsLoaderMW,
-    EnvInfoLoaderMW(isMultiEnvEnabled()),
+    EnvInfoLoaderMW(false),
     SolutionLoaderMW(),
     QuestionModelMW,
     ContextInjectorMW,
@@ -777,7 +777,7 @@ export class FxCore implements Core {
     ErrorHandlerMW,
     ProjectMigratorMW,
     ProjectSettingsLoaderMW,
-    EnvInfoLoaderMW(isMultiEnvEnabled()),
+    EnvInfoLoaderMW(false),
     SolutionLoaderMW(),
     QuestionModelMW,
     ContextInjectorMW,
@@ -791,7 +791,7 @@ export class FxCore implements Core {
     ErrorHandlerMW,
     ProjectMigratorMW,
     ProjectSettingsLoaderMW,
-    EnvInfoLoaderMW(isMultiEnvEnabled()),
+    EnvInfoLoaderMW(false),
     SolutionLoaderMW(),
     QuestionModelMW,
     ContextInjectorMW,
@@ -906,7 +906,7 @@ export class FxCore implements Core {
   @hooks([
     ErrorHandlerMW,
     ProjectSettingsLoaderMW,
-    EnvInfoLoaderMW(false),
+    EnvInfoLoaderMW(true),
     ContextInjectorMW,
     EnvInfoWriterMW(),
   ])
@@ -921,7 +921,7 @@ export class FxCore implements Core {
   @hooks([
     ErrorHandlerMW,
     ProjectSettingsLoaderMW,
-    EnvInfoLoaderMW(false),
+    EnvInfoLoaderMW(true),
     ContextInjectorMW,
     EnvInfoWriterMW(),
   ])
@@ -941,7 +941,7 @@ export class FxCore implements Core {
     ErrorHandlerMW,
     ProjectSettingsLoaderMW,
     SolutionLoaderMW(),
-    EnvInfoLoaderMW(isMultiEnvEnabled()),
+    EnvInfoLoaderMW(false),
     ContextInjectorMW,
   ])
   async createEnv(inputs: Inputs, ctx?: CoreHookContext): Promise<Result<Void, FxError>> {
@@ -965,7 +965,6 @@ export class FxCore implements Core {
     const createEnvResult = await this.createEnvCopy(
       createEnvCopyInput.targetEnvName,
       createEnvCopyInput.sourceEnvName,
-      projectSettings,
       inputs,
       core
     );
@@ -1021,7 +1020,6 @@ export class FxCore implements Core {
   async createEnvCopy(
     targetEnvName: string,
     sourceEnvName: string,
-    projectSettings: ProjectSettings,
     inputs: Inputs,
     core: FxCore
   ): Promise<Result<Void, FxError>> {

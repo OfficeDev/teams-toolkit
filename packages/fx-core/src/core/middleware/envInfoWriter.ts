cce8005514e3a54c62705035d9f75a1bbe17e1a2
huajiezhang <huajiezhang@microsoft.com>
2021-09-29 10:40:54 +0800

    fix(core): failure of Error catching

--- packages/fx-core/src/core/middleware/envInfoWriter.ts
+++ packages/fx-core/src/core/middleware/envInfoWriter.ts
@@ -10,63 +10,79 @@ import { PluginNames } from "../../plugins/solution/fx-solution/constants";
 import { environmentManager } from "../environment";
 
 /**
- * This middleware will help to persist environment profile if necessary.
+ * This middleware will help to persist environment profile even if lifecycle task throws Error.
  */
 export function EnvInfoWriterMW(skip = false): Middleware {
   return async (ctx: CoreHookContext, next: NextFunction) => {
-    await next();
-    if (skip && isMultiEnvEnabled()) {
-      return;
+    let error1: any = undefined;
+    try {
+      await next();
+    } catch (e) {
+      error1 = e;
     }
-
-    const lastArg = ctx.arguments[ctx.arguments.length - 1];
-    const inputs: Inputs = lastArg === ctx ? ctx.arguments[ctx.arguments.length - 2] : lastArg;
-    if (
-      !inputs.projectPath ||
-      inputs.ignoreConfigPersist === true ||
-      inputs.ignoreEnvInfo === true ||
-      StaticPlatforms.includes(inputs.platform)
-    )
-      return;
-
-    if (isV2()) {
-      const provisionOutputs = ctx.envInfoV2?.profile;
-      if (provisionOutputs === undefined) return;
-      // DO NOT persist local debug plugin config.
-      if (isMultiEnvEnabled() && provisionOutputs[PluginNames.LDEBUG]) {
-        delete provisionOutputs[PluginNames.LDEBUG];
-      }
-      const envProfilePath = await environmentManager.writeEnvProfile(
-        provisionOutputs,
-        inputs.projectPath,
-        ctx.envName,
-        ctx.contextV2?.cryptoProvider
-      );
-
-      if (envProfilePath.isOk()) {
-        const core = ctx.self as FxCore;
-        core.tools.logProvider.debug(`[core] persist env profile: ${envProfilePath.value}`);
-      }
-    } else {
-      const solutionContext = ctx.solutionContext;
-      if (solutionContext === undefined) return;
-
-      // DO NOT persist local debug plugin config.
-      if (isMultiEnvEnabled() && solutionContext.envInfo.profile.has(PluginNames.LDEBUG)) {
-        solutionContext.envInfo.profile.delete(PluginNames.LDEBUG);
-      }
-
-      const envProfilePath = await environmentManager.writeEnvProfile(
-        solutionContext.envInfo.profile,
-        inputs.projectPath,
-        solutionContext.envInfo?.envName,
-        solutionContext.cryptoProvider
-      );
-
-      if (envProfilePath.isOk()) {
-        const core = ctx.self as FxCore;
-        core.tools.logProvider.debug(`[core] persist env profile: ${envProfilePath.value}`);
-      }
+    let error2: any = undefined;
+    try {
+      await writeEnvInfo(ctx, skip);
+    } catch (e) {
+      error2 = e;
     }
+    if (error1) throw error1;
+    if (error2) throw error2;
   };
+}
+
+async function writeEnvInfo(ctx: CoreHookContext, skip: boolean) {
+  if (skip && isMultiEnvEnabled()) {
+    return;
+  }
+
+  const lastArg = ctx.arguments[ctx.arguments.length - 1];
+  const inputs: Inputs = lastArg === ctx ? ctx.arguments[ctx.arguments.length - 2] : lastArg;
+  if (
+    !inputs.projectPath ||
+    inputs.ignoreConfigPersist === true ||
+    inputs.ignoreEnvInfo === true ||
+    StaticPlatforms.includes(inputs.platform)
+  )
+    return;
+
+  if (isV2()) {
+    const provisionOutputs = ctx.envInfoV2?.profile;
+    if (provisionOutputs === undefined) return;
+    // DO NOT persist local debug plugin config.
+    if (isMultiEnvEnabled() && provisionOutputs[PluginNames.LDEBUG]) {
+      delete provisionOutputs[PluginNames.LDEBUG];
+    }
+    const envProfilePath = await environmentManager.writeEnvProfile(
+      provisionOutputs,
+      inputs.projectPath,
+      ctx.envName,
+      ctx.contextV2?.cryptoProvider
+    );
+
+    if (envProfilePath.isOk()) {
+      const core = ctx.self as FxCore;
+      core.tools.logProvider.debug(`[core] persist env profile: ${envProfilePath.value}`);
+    }
+  } else {
+    const solutionContext = ctx.solutionContext;
+    if (solutionContext === undefined) return;
+
+    // DO NOT persist local debug plugin config.
+    if (isMultiEnvEnabled() && solutionContext.envInfo.profile.has(PluginNames.LDEBUG)) {
+      solutionContext.envInfo.profile.delete(PluginNames.LDEBUG);
+    }
+
+    const envProfilePath = await environmentManager.writeEnvProfile(
+      solutionContext.envInfo.profile,
+      inputs.projectPath,
+      solutionContext.envInfo?.envName,
+      solutionContext.cryptoProvider
+    );
+
+    if (envProfilePath.isOk()) {
+      const core = ctx.self as FxCore;
+      core.tools.logProvider.debug(`[core] persist env profile: ${envProfilePath.value}`);
+    }
+  }
 }

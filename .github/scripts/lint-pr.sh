a697954287466edd0485b7ab1c1feafe17f02636
wenytang-ms <wenyutang@microsoft.com>
2021-10-09 10:46:22 +0800

    chore: update

--- .github/scripts/lint-pr.sh
+++ .github/scripts/lint-pr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VAR=$(git diff --diff-filter=MARC $1...HEAD --name-only --relative -- .| xargs)
+VAR=$(git diff --diff-filter=MARC $1...HEAD --name-only --relative -- .| grep -E '.js$|.ts$|.jsx$|.tsx$' | xargs)
 echo $VAR
 if [ ! -z "$VAR" ]
 then 

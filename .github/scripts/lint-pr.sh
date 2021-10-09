881dec78a812e5992e92a681ae8f1a37e6c2fe69
wenytang-ms <wenyutang@microsoft.com>
2021-10-09 10:18:49 +0800

    chore: set prettier as eslint plugin

--- .github/scripts/lint-pr.sh
+++ .github/scripts/lint-pr.sh
@@ -3,5 +3,6 @@ VAR=$(git diff --diff-filter=MARC $1...HEAD --name-only --relative -- .| xargs)
 echo $VAR
 if [ ! -z "$VAR" ]
 then 
-    npx prettier --config .prettierrc.js --write $VAR --ignore-path .prettierignore
+    # npx prettier --config .prettierrc.js --write $VAR --ignore-path .prettierignore
+    npx eslint $VAR --quiet --fix
 fi

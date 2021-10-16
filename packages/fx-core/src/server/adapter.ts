49b6ce44f52c7cf67a98b0dcde13f23f74230857
huajiezhang <huajiezhang@microsoft.com>
2021-10-16 12:07:55 +0800

    feat: server adapter and UT

--- packages/fx-core/src/server/adapter.ts
+++ packages/fx-core/src/server/adapter.ts
@@ -1,30 +1,159 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { FuncValidation, Inputs, Json, Platform, QTreeNode } from "@microsoft/teamsfx-api";
-import { QuestionAppName } from "../core/question";
+import {
+  assembleError,
+  err,
+  FuncQuestion,
+  FxError,
+  Inputs,
+  Json,
+  LocalFunc,
+  ok,
+  OnSelectionChangeFunc,
+  Platform,
+  QTreeNode,
+  Question,
+  Result,
+  SystemError,
+  ValidateFunc,
+} from "@microsoft/teamsfx-api";
 
-async function test() {
-  const functionMap = new Map<string, any>();
+export let GlobalFuncId = 0;
+export type FuncType = LocalFunc<any> | ValidateFunc<any> | OnSelectionChangeFunc;
 
-  const question = QuestionAppName;
+export const GlobalFuncMap = new Map<number, FuncType>();
 
-  const funcValidation = question.validation as FuncValidation<string>;
+export function setFunc(func: FuncType): number {
+  ++GlobalFuncId;
+  GlobalFuncMap.set(GlobalFuncId, func);
+  return GlobalFuncId;
+}
 
-  const functionName = `validFunc-${new Date().getTime()}`;
+export function getFunc(id: number): FuncType | undefined {
+  const func = GlobalFuncMap.get(id);
+  return func;
+}
 
-  functionMap.set(functionName, funcValidation.validFunc);
-
-  (question as Json).validation = { validFunc: functionName };
-
-  console.log(JSON.stringify(question, null, 4));
-
-  const func = functionMap.get(functionName);
-
+export async function callFunc(rpc: Rpc, ...params: any[]): Promise<Result<any, FxError>> {
+  const func = getFunc(rpc.id);
   if (func) {
-    const inputs: Inputs = { platform: Platform.VSCode, folder: "." };
-    console.log(await func("123abc123", inputs));
+    let result: any;
+    try {
+      if (rpc.type === "LocalFunc") {
+        result = await (func as LocalFunc<any>)(params[0]);
+      } else if (rpc.type === "ValidateFunc") {
+        result = await (func as ValidateFunc<any>)(params[0], params[1]);
+      } else if (rpc.type === "OnSelectionChangeFunc") {
+        result = await (func as OnSelectionChangeFunc)(
+          new Set<string>(params[0]),
+          new Set<string>(params[1])
+        );
+      }
+      return ok(result);
+    } catch (e) {
+      return err(assembleError(e));
+    }
+  }
+  return err(new SystemError("FuncNotFound", `Function not found, id:${rpc.id}`, "FxCoreServer"));
+}
+
+export function visit(question: Question): void {
+  if (question.type === "func") {
+    const id = setFunc(question.func);
+    (question as any).func = { type: "LocalFunc", id: id } as Rpc;
+  } else {
+    if (typeof question.default === "function") {
+      const id = setFunc(question.default);
+      (question as any).default = { type: "LocalFunc", id: id } as Rpc;
+    }
+    if (typeof question.placeholder === "function") {
+      const id = setFunc(question.placeholder);
+      (question as any).placeholder = { type: "LocalFunc", id: id } as Rpc;
+    }
+    if (typeof question.prompt === "function") {
+      const id = setFunc(question.prompt);
+      (question as any).prompt = { type: "LocalFunc", id: id } as Rpc;
+    }
+    if (question.validation) {
+      if ((question.validation as any).validFunc) {
+        const id = setFunc((question.validation as any).validFunc);
+        (question.validation as any).validFunc = { type: "ValidateFunc", id: id } as Rpc;
+      }
+    }
+    if (question.type === "singleSelect" || question.type === "multiSelect") {
+      if (question.dynamicOptions) {
+        const id = setFunc(question.dynamicOptions);
+        (question as any).dynamicOptions = { type: "LocalFunc", id: id } as Rpc;
+      }
+      if (question.type === "multiSelect") {
+        if (question.onDidChangeSelection) {
+          const id = setFunc(question.onDidChangeSelection);
+          (question as any).onDidChangeSelection = { type: "OnSelectionChangeFunc", id: id } as Rpc;
+        }
+      }
+    }
   }
 }
 
-test();
+export interface Rpc {
+  type: "LocalFunc" | "ValidateFunc" | "OnSelectionChangeFunc";
+  id: number;
+}
+
+export function traverseToJson(root: QTreeNode): void {
+  if (root.condition) {
+    const cond: any = root.condition;
+    if (cond.validFunc) {
+      const id = setFunc(cond.validFunc);
+      cond.validFunc = { type: "ValidateFunc", id: id } as Rpc;
+    }
+  }
+  if (root.data.type !== "group") {
+    const question = root.data as Question;
+    visit(question);
+  }
+  if (root.children) {
+    for (const child of root.children) {
+      if (!child) continue;
+      traverseToJson(child);
+    }
+  }
+}
+
+export function questionToJson(root: QTreeNode): Json {
+  const copy = deepCopy(root);
+  traverseToJson(copy);
+  return copy as Json;
+}
+
+export const deepCopy = <T>(target: T): T => {
+  if (target === null) {
+    return target;
+  }
+  if (target instanceof Date) {
+    return new Date(target.getTime()) as any;
+  }
+  if (target instanceof Array) {
+    const cp = [] as any[];
+    (target as any[]).forEach((v) => {
+      cp.push(v);
+    });
+    return cp.map((n: any) => deepCopy<any>(n)) as any;
+  }
+  if (typeof target === "object" && target !== {}) {
+    const cp = { ...(target as { [key: string]: any }) } as {
+      [key: string]: any;
+    };
+    Object.keys(cp).forEach((k) => {
+      cp[k] = deepCopy<any>(cp[k]);
+    });
+    return cp as T;
+  }
+  return target;
+};
+
+export function reset(): void {
+  GlobalFuncId = 0;
+  GlobalFuncMap.clear();
+}
---
+++ packages/fx-core/tests/server/adapter.test.ts
@@ -0,0 +1,226 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import {
+  FuncQuestion,
+  Inputs,
+  isAutoSkipSelect,
+  Json,
+  MultiFileQuestion,
+  MultiSelectQuestion,
+  Platform,
+  QTreeNode,
+  StaticOptions,
+  TextInputQuestion,
+} from "@microsoft/teamsfx-api";
+import { assert } from "chai";
+import "mocha";
+import sinon from "sinon";
+import { callFunc, questionToJson, reset, Rpc } from "../../src/server/adapter";
+
+describe("Core basic APIs", () => {
+  const sandbox = sinon.createSandbox();
+  const func1 = async (inputs: Inputs): Promise<any> => {
+    return inputs.platform;
+  };
+  const func2 = async (input: string, inputs?: Inputs): Promise<string | undefined> => {
+    if (input.length < 5) return "length < 5";
+    return undefined;
+  };
+  beforeEach(() => {});
+
+  afterEach(async () => {
+    reset();
+  });
+
+  it("condition is function, question is func type", async () => {
+    const question: FuncQuestion = {
+      name: "test-question",
+      type: "func",
+      func: func1,
+    };
+
+    const node = new QTreeNode(question);
+    node.condition = {
+      validFunc: func2,
+    };
+
+    const json = questionToJson(node);
+
+    const expected: Json = {
+      data: {
+        name: "test-question",
+        type: "func",
+        func: {
+          type: "LocalFunc",
+          id: 2,
+        },
+      },
+      condition: {
+        validFunc: {
+          type: "ValidateFunc",
+          id: 1,
+        },
+      },
+    };
+
+    assert.deepEqual(json, expected);
+
+    {
+      const real = await callFunc(json.data.func as Rpc, { platform: Platform.VSCode });
+      const expected = await func1({ platform: Platform.VSCode });
+      if (real.isOk()) {
+        assert.equal(real.value, expected);
+      }
+    }
+    {
+      const real = await callFunc(json.condition.validFunc as Rpc, "12a123", {
+        platform: Platform.VSCode,
+      });
+      const expected = await func2("12a123", { platform: Platform.VSCode });
+      if (real.isOk()) {
+        assert.equal(real.value, expected);
+      }
+    }
+
+    {
+      json.condition.validFunc.id = 100;
+      const real = await callFunc(json.condition.validFunc as Rpc, "12a123", {
+        platform: Platform.VSCode,
+      });
+      assert.isTrue(real.isErr() && real.error.name === "FuncNotFound");
+    }
+  });
+
+  it("default, placeholder, prompt, validation is function", async () => {
+    const question: TextInputQuestion = {
+      name: "test-question",
+      type: "text",
+      title: "title",
+      default: func1,
+      placeholder: func1,
+      prompt: func1,
+      validation: { validFunc: func2 },
+    };
+    const node = new QTreeNode(question);
+    const json = questionToJson(node);
+    const expected: Json = {
+      data: {
+        name: "test-question",
+        type: "text",
+        title: "title",
+        default: {
+          type: "LocalFunc",
+          id: 1,
+        },
+        placeholder: {
+          type: "LocalFunc",
+          id: 2,
+        },
+        prompt: {
+          type: "LocalFunc",
+          id: 3,
+        },
+        validation: {
+          validFunc: {
+            type: "ValidateFunc",
+            id: 4,
+          },
+        },
+      },
+    };
+
+    assert.deepEqual(json, expected);
+
+    {
+      const real = await callFunc(json.data.default as Rpc, { platform: Platform.VSCode });
+      const expected = await func1({ platform: Platform.VSCode });
+      if (real.isOk()) {
+        assert.equal(real.value, expected);
+      }
+    }
+    {
+      const real = await callFunc(json.data.placeholder as Rpc, { platform: Platform.VSCode });
+      const expected = await func1({ platform: Platform.VSCode });
+      if (real.isOk()) {
+        assert.equal(real.value, expected);
+      }
+    }
+    {
+      const real = await callFunc(json.data.prompt as Rpc, { platform: Platform.VSCode });
+      const expected = await func1({ platform: Platform.VSCode });
+      if (real.isOk()) {
+        assert.equal(real.value, expected);
+      }
+    }
+    {
+      const real = await callFunc(json.data.validation.validFunc as Rpc, "12a123", {
+        platform: Platform.VSCode,
+      });
+      const expected = await func2("12a123", { platform: Platform.VSCode });
+      if (real.isOk()) {
+        assert.equal(real.value, expected);
+      }
+    }
+  });
+
+  it("multi-selection: onSelectChangeFunc, dynamicOptions", async () => {
+    const func1 = async (inputs: Inputs): Promise<StaticOptions> => {
+      return ["111", "222"];
+    };
+    const func2 = async (
+      currentSelectedIds: Set<string>,
+      previousSelectedIds: Set<string>
+    ): Promise<Set<string>> => {
+      return currentSelectedIds;
+    };
+
+    const question: MultiSelectQuestion = {
+      name: "test-question",
+      type: "multiSelect",
+      title: "title",
+      staticOptions: [],
+      dynamicOptions: func1,
+      onDidChangeSelection: func2,
+    };
+    const node = new QTreeNode(question);
+    const json = questionToJson(node);
+    const expected: Json = {
+      data: {
+        name: "test-question",
+        type: "multiSelect",
+        title: "title",
+        staticOptions: [],
+        dynamicOptions: {
+          type: "LocalFunc",
+          id: 1,
+        },
+        onDidChangeSelection: {
+          type: "OnSelectionChangeFunc",
+          id: 2,
+        },
+      },
+    };
+
+    assert.deepEqual(json, expected);
+
+    {
+      const real = await callFunc(json.data.dynamicOptions as Rpc, { platform: Platform.VSCode });
+      const expected = await func1({ platform: Platform.VSCode });
+      if (real.isOk()) {
+        assert.deepEqual(real.value, expected);
+      }
+    }
+    {
+      const real = await callFunc(
+        json.data.onDidChangeSelection as Rpc,
+        new Set<string>(["1", "2"]),
+        new Set<string>(["3", "4"])
+      );
+      const expected = await func2(new Set<string>(["1", "2"]), new Set<string>(["3", "4"]));
+      if (real.isOk()) {
+        assert.deepEqual(real.value, expected);
+      }
+    }
+  });
+});

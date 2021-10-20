13bdf78502e83b51eb70ffcf6064dfac0fbc394c
KennethBWSong <bowsong@microsoft.com>
2021-10-20 11:45:26 +0800

    fix: remove useless code

--- packages/fx-core/src/plugins/resource/simpleauth/utils/common.ts
+++ packages/fx-core/src/plugins/resource/simpleauth/utils/common.ts
@@ -1,13 +1,12 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-import { FxError, LogProvider, PluginContext } from "@microsoft/teamsfx-api";
+import { LogProvider, PluginContext } from "@microsoft/teamsfx-api";
 import * as path from "path";
 import * as fs from "fs-extra";
 import { Constants, Message } from "../constants";
 import {
   EndpointInvalidError,
   NoConfigError,
-  PluginError,
   VersionFileNotExist,
   ZipDownloadError,
 } from "../errors";

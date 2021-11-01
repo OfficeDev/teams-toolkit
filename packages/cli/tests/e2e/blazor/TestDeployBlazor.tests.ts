bb110600e8feccf400fb050598f7ff7b4e62d32f
zhijie <zhihuan@microsoft.com>
2021-11-01 15:47:35 +0800

    test: register e2e test for blazor scenario

---
+++ packages/cli/tests/e2e/blazor/TestDeployBlazor.tests.ts
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import * as chai from "chai";
+
+describe("Provision Azure Resource for Blazor Project and Deploy Blazor Project.", function () {
+  // TODO: implement the test case after blazor plugin integrating with solution
+  it(`Blazor`, async function () {
+    chai.assert.isTrue(true);
+  });
+});

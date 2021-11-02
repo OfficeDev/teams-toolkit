da7d41cab52b3e94468f7345414bc9ac80963fde
huajiezhang <huajiezhang@microsoft.com>
2021-11-02 12:55:35 +0800

    feat(api): v3 api

---
+++ packages/api/src/v2/pluginManifest.ts
@@ -0,0 +1,53 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { FxError, Result } from "..";
+import { Inputs, Void } from "../types";
+import { Context } from "./types";
+
+export interface InnerLoopPlugin {
+  runtimeStacks: string[];
+  languages: string[];
+  scaffoldSourceCode: (ctx: Context, inputs: Inputs) => Promise<Result<Void, FxError>>;
+  //localDebug
+}
+
+export interface HostingPlugin {
+  runtimeStacks: string[];
+  //provision
+  //deploy
+}
+
+export interface TeamsAppPluginSettings {
+  tab: {
+    innerLoopPlugins: string[];
+    hostingPlugins: string[];
+    resourcePlugins: string[];
+  };
+  bot: {
+    innerLoopPlugins: string[];
+    hostingPlugins: string[];
+    resourcePlugins: string[];
+  };
+}
+
+export interface ComputingResource {
+  id: string;
+  innerLoopPlugins: string;
+  hostingPlugin: string;
+  runtimeStack: string;
+  resources: string[];
+  programmingLanguage: string;
+}
+
+export interface DatabaseResource {
+  id: string;
+  hostingPlugin: string;
+}
+
+export interface TeamsAppProjectSettings {
+  capabilities: ("Tab" | "Bot" | "MessagingExtension")[];
+  tab: ComputingResource;
+  bot: ComputingResource;
+  resources: (DatabaseResource | ComputingResource)[];
+}

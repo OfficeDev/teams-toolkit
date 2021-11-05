3e94f5578ff010cc07d5b0f3d027a4b4ed485bd9
Xiaofu Huang <xiaofhua@microsoft.com>
2021-11-05 14:56:09 +0800

    fix: update config test case

--- packages/fx-core/tests/plugins/resource/apim/config.test.ts
+++ packages/fx-core/tests/plugins/resource/apim/config.test.ts
@@ -37,9 +37,9 @@ describe("config", () => {
       chai
         .expect(() => solutionConfig.teamsAppTenantId)
         .to.throw(
-          `Project configuration 'apiPath' of 'fx-resource-apim' is missing in '${
+          `Project configuration 'teamsAppTenantId' of 'solution' is missing in '${
             isArmSupportEnabled() ? "state.dev.json" : "env.default.json"
-          }'. Retry deploy to the cloud or set the value manually.`
+          }'. Retry provision in the cloud or set the value manually.`
         );
     });
     it("Error type property", () => {

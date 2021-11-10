baa6c73f7150db2204a0ea724f44d88bb3e8fbd0
huajiezhang <huajiezhang@microsoft.com>
2021-11-10 15:44:44 +0800

    feat(core): upgrade project settings version

--- packages/fx-core/src/core/index.ts
+++ packages/fx-core/src/core/index.ts
@@ -251,7 +251,7 @@ export class FxCore implements Core {
           name: "",
           version: "1.0.0",
         },
-        version: "1.0.0",
+        version: "2.0.0",
         isFromSample: false,
       };
       ctx.projectSettings = projectSettings;
@@ -273,7 +273,6 @@ export class FxCore implements Core {
         }
         ctx.solutionV2 = solution;
         projectSettings.solutionSettings.name = solution.name;
-        projectSettings.version = "2.0.0";
         const contextV2 = createV2Context(this, projectSettings);
         ctx.contextV2 = contextV2;
         const scaffoldSourceCodeRes = await solution.scaffoldSourceCode(contextV2, inputs);

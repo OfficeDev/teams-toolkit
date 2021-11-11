d1e2d23daae9d09338babc935937abb1082f61dd
xzf0587 <xzf0587@gmail.com>
2021-11-11 15:33:25 +0800

    chore(arm): clean useless files (#2917)

--- packages/fx-core/src/common/constants.ts
+++ packages/fx-core/src/common/constants.ts
@@ -13,8 +13,6 @@ export class Bicep {
   static readonly OutputOrchestrationFileName: string = "output.template.bicep";
   static readonly VariablesOrchestrationFileName: string = "variables.template.bicep";
   static readonly ParameterFileName: string = "parameters.json";
-  static readonly ProvisionV2FileName: string = "provision.template.v2.bicep";
-  static readonly ConfigV2FileName: string = "config.template.v2.bicep";
   static readonly ProvisionFileName: string = "provision.template.bicep";
   static readonly ConfigFileName: string = "config.template.bicep";
 }

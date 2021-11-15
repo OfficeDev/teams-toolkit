04e2a9ea8fffff0cc68bdcccff2a221ab8efeafe
Aocheng Wang <aochengwang@microsoft.com>
2021-11-15 17:13:05 +0800

    chore: remove flag

--- packages/fx-core/tests/plugins/solution/solution.scaffold.test.ts
+++ packages/fx-core/tests/plugins/solution/solution.scaffold.test.ts
@@ -186,7 +186,7 @@ describe("Solution scaffold() reading valid manifest file", () => {
   it("should work and generate arm template when project requires Azure services", async () => {
     // add dedicated test case to test ARM feature enabled behavior
     const restore = mockedEnv({
-      TEAMSFX_INSIDER_PREVIEW: "1",
+      __TEAMSFX_INSIDER_PREVIEW: "1",
     });
 
     fileContent.clear();
@@ -249,7 +249,7 @@ describe("Solution scaffold() reading valid manifest file", () => {
   it("should work and not generate arm template when project does not require Azure services", async () => {
     // add dedicated test case to test ARM feature enabled behavior
     const restore = mockedEnv({
-      TEAMSFX_INSIDER_PREVIEW: "1",
+      __TEAMSFX_INSIDER_PREVIEW: "1",
     });
 
     fileContent.clear();

6307a0bb7c2a42c90fd57085af586c9444eb8036
Xiaofu Huang <xiaofhua@microsoft.com>
2021-11-23 12:51:23 +0800

    test(apim): fix apim e2e test cases

--- packages/cli/tests/e2e/apim/TestCreateNewApim.tests.ts
+++ packages/cli/tests/e2e/apim/TestCreateNewApim.tests.ts
@@ -16,92 +16,90 @@ import {
 } from "../commonUtils";
 import AzureLogin from "../../../src/commonlib/azureLogin";
 import GraphLogin from "../../../src/commonlib/graphLogin";
+import { isMultiEnvEnabled } from "@microsoft/teamsfx-core";
 
 describe("Create a new API Management Service", function () {
-  const testProcessEnv = [Object.assign({}, process.env), Object.assign({}, process.env)];
-  testProcessEnv[0]["TEAMSFX_INSIDER_PREVIEW"] = "true";
-  testProcessEnv[0]["SIMPLE_AUTH_SKU_NAME"] = "B1";
+  const testProcessEnv = Object.assign({}, process.env);
+  testProcessEnv["SIMPLE_AUTH_SKU_NAME"] = "B1";
 
-  testProcessEnv.forEach((input) => {
-    const isMultiEnvEnabled = input["TEAMSFX_INSIDER_PREVIEW"] === "true";
-    describe(`Multi env enabled: ${isMultiEnvEnabled}`, () => {
-      const testFolder = getTestFolder();
-      const appName = getUniqueAppName();
-      const subscriptionId = getSubscriptionId();
-      const projectPath = path.resolve(testFolder, appName);
-      it(`Import API into a new API Management Service`, async function () {
-        // new a project
-        let result = await execAsync(`teamsfx new --app-name ${appName} --interactive false`, {
-          cwd: testFolder,
-          env: input,
-          timeout: 0,
-        });
-        console.log(`Create new project. Error message: ${result.stderr}`);
+  const multiEnvEnabled = isMultiEnvEnabled();
+  describe(`Multi env enabled: ${multiEnvEnabled}`, () => {
+    const testFolder = getTestFolder();
+    const appName = getUniqueAppName();
+    const subscriptionId = getSubscriptionId();
+    const projectPath = path.resolve(testFolder, appName);
+    it(`Import API into a new API Management Service`, async function () {
+      // new a project
+      let result = await execAsync(`teamsfx new --app-name ${appName} --interactive false`, {
+        cwd: testFolder,
+        env: testProcessEnv,
+        timeout: 0,
+      });
+      console.log(`Create new project. Error message: ${result.stderr}`);
 
-        await ApimValidator.init(subscriptionId, AzureLogin, GraphLogin);
+      await ApimValidator.init(subscriptionId, AzureLogin, GraphLogin);
 
-        result = await execAsyncWithRetry(
-          `teamsfx resource add azure-apim --subscription ${subscriptionId}`,
-          {
-            cwd: projectPath,
-            env: input,
-            timeout: 0,
-          }
-        );
-        console.log(`Add APIM resource. Error message: ${result.stderr}`);
-
-        result = await execAsyncWithRetry(`teamsfx provision`, {
+      result = await execAsyncWithRetry(
+        `teamsfx resource add azure-apim --subscription ${subscriptionId}`,
+        {
           cwd: projectPath,
-          env: input,
+          env: testProcessEnv,
           timeout: 0,
-        });
-        console.log(`Provision. Error message: ${result.stderr}`);
-
-        let provisionContext;
-        if (isMultiEnvEnabled) {
-          const contextRes = await loadContext(projectPath, "dev");
-          if (contextRes.isOk()) {
-            provisionContext = contextRes.value;
-          } else {
-            throw contextRes.error;
-          }
-        } else {
-          provisionContext = await readContext(projectPath);
         }
+      );
+      console.log(`Add APIM resource. Error message: ${result.stderr}`);
 
-        await ApimValidator.validateProvision(
-          provisionContext,
-          appName,
-          subscriptionId,
-          isMultiEnvEnabled
-        );
-
-        result = await execAsyncWithRetry(
-          `teamsfx deploy apim --open-api-document openapi/openapi.json --api-prefix ${appName} --api-version v1`,
-          {
-            cwd: projectPath,
-            env: input,
-            timeout: 0,
-          },
-          3,
-          `teamsfx deploy apim --open-api-document openapi/openapi.json --api-version v1`
-        );
-        console.log(`Deploy. Error message: ${result.stderr}`);
-
-        const deployContext = await fs.readJSON(getConfigFileName(appName, isMultiEnvEnabled));
-        await ApimValidator.validateDeploy(
-          deployContext,
-          projectPath,
-          appName,
-          "v1",
-          isMultiEnvEnabled
-        );
+      result = await execAsyncWithRetry(`teamsfx provision`, {
+        cwd: projectPath,
+        env: testProcessEnv,
+        timeout: 0,
       });
+      console.log(`Provision. Error message: ${result.stderr}`);
 
-      after(async () => {
-        // clean up
-        await cleanUp(appName, projectPath, true, false, true, isMultiEnvEnabled);
-      });
+      let provisionContext;
+      if (multiEnvEnabled) {
+        const contextRes = await loadContext(projectPath, "dev");
+        if (contextRes.isOk()) {
+          provisionContext = contextRes.value;
+        } else {
+          throw contextRes.error;
+        }
+      } else {
+        provisionContext = await readContext(projectPath);
+      }
+
+      await ApimValidator.validateProvision(
+        provisionContext,
+        appName,
+        subscriptionId,
+        multiEnvEnabled
+      );
+
+      result = await execAsyncWithRetry(
+        `teamsfx deploy apim --open-api-document openapi/openapi.json --api-prefix ${appName} --api-version v1`,
+        {
+          cwd: projectPath,
+          env: testProcessEnv,
+          timeout: 0,
+        },
+        3,
+        `teamsfx deploy apim --open-api-document openapi/openapi.json --api-version v1`
+      );
+      console.log(`Deploy. Error message: ${result.stderr}`);
+
+      const deployContext = await fs.readJSON(getConfigFileName(appName, multiEnvEnabled));
+      await ApimValidator.validateDeploy(
+        deployContext,
+        projectPath,
+        appName,
+        "v1",
+        multiEnvEnabled
+      );
+    });
+
+    after(async () => {
+      // clean up
+      await cleanUp(appName, projectPath, true, false, true, multiEnvEnabled);
     });
   });
 });

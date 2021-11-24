16d43e20eccc6d2c98e80d4c73fdabc5c8cce8ff
Chaoyi Yuan <blackchoey@gmail.com>
2021-11-24 12:49:40 +0800

    test: set simple auth sku in collaboration e2e test (#3103)

--- packages/cli/tests/e2e/collaboration/Permission.tests.ts
+++ packages/cli/tests/e2e/collaboration/Permission.tests.ts
@@ -2,10 +2,8 @@
 // Licensed under the MIT license.
 
 import { expect } from "chai";
-import fs from "fs-extra";
 import path from "path";
-import { isMultiEnvEnabled } from "../../../../fx-core/build";
-import { FeatureFlags } from "../../../src/constants";
+import { environmentManager, isRemoteCollaborateEnabled } from "@microsoft/teamsfx-core";
 import {
   cleanUp,
   execAsync,
@@ -13,8 +11,7 @@ import {
   getSubscriptionId,
   getTestFolder,
   getUniqueAppName,
-  mockTeamsfxMultiEnvFeatureFlag,
-  setSimpleAuthSkuNameToB1,
+  setSimpleAuthSkuNameToB1Bicep,
 } from "../commonUtils";
 
 describe("Collaboration", function () {
@@ -24,23 +21,25 @@ describe("Collaboration", function () {
   const projectPath = path.resolve(testFolder, appName);
   const collaborator = process.env["M365_ACCOUNT_COLLABORATOR"];
   const creator = process.env["M365_ACCOUNT_NAME"];
-  const processEnv = mockTeamsfxMultiEnvFeatureFlag();
 
   it("Collaboration: CLI with permission status and permission grant", async function () {
+    if (!isRemoteCollaborateEnabled()) {
+      return;
+    }
     // new a project
     await execAsync(`teamsfx new --interactive false --app-name ${appName}`, {
       cwd: testFolder,
-      env: processEnv,
+      env: process.env,
       timeout: 0,
     });
     console.log(`[Successfully] scaffold to ${projectPath}`);
 
-    // await setSimpleAuthSkuNameToB1(projectPath);
+    await setSimpleAuthSkuNameToB1Bicep(projectPath, environmentManager.getDefaultEnvName());
 
     // provision
     await execAsyncWithRetry(`teamsfx provision --subscription ${subscription}`, {
       cwd: projectPath,
-      env: processEnv,
+      env: process.env,
       timeout: 0,
     });
     console.log("[Successfully] provision");
@@ -48,7 +47,7 @@ describe("Collaboration", function () {
     // Check Permission
     const checkPermissionResult = await execAsyncWithRetry(`teamsfx permission status`, {
       cwd: projectPath,
-      env: processEnv,
+      env: process.env,
       timeout: 0,
     });
 
@@ -65,7 +64,7 @@ describe("Collaboration", function () {
       `teamsfx permission grant --email ${collaborator}`,
       {
         cwd: projectPath,
-        env: processEnv,
+        env: process.env,
         timeout: 0,
       }
     );
@@ -82,7 +81,7 @@ describe("Collaboration", function () {
       `teamsfx permission status --list-all-collaborators`,
       {
         cwd: projectPath,
-        env: processEnv,
+        env: process.env,
         timeout: 0,
       }
     );
@@ -91,7 +90,7 @@ describe("Collaboration", function () {
     // When collaborator account is guest account in the tenant. Account name pattern will change.
     // e.g. Guest account "account@example.com" will appear as "account_example#EXT#@exampleTenant.onmicrosoft.com" under tenant "exampleTenant".
     // Thus here will check the account name only.
-    if (!isMultiEnvEnabled()) {
+    if (!isRemoteCollaborateEnabled()) {
       expect(listCollaboratorResult.stdout).to.contains(`Account: ${collaborator?.split("@")[0]}`);
       expect(listCollaboratorResult.stdout).to.contains(`Account: ${creator?.split("@")[0]}`);
     }
@@ -100,6 +99,8 @@ describe("Collaboration", function () {
 
   after(async () => {
     // clean up
-    await cleanUp(appName, projectPath, true, false, false, true);
+    if (isRemoteCollaborateEnabled()) {
+      await cleanUp(appName, projectPath, true, false, false, true);
+    }
   });
 });

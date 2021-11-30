7b40ee61eaa24ab1a29e155083df035061e51b9e
Sébastien Levert <slevert@outlook.com>
2021-11-30 01:39:58 -0500

    Initial changes for MGT to be supported in TeamsFx

--- templates/tab/ts/default/src/components/sample/Graph.tsx
+++ templates/tab/ts/default/src/components/sample/Graph.tsx
@@ -1,36 +1,18 @@
 import React from "react";
-import { Button } from "@fluentui/react-northstar";
-import { useGraph } from "./lib/useGraph";
-import { ProfileCard } from "./ProfileCard";
+import { ProviderState } from '@microsoft/mgt-element';
+import { Person, PersonViewType, PersonCardInteraction } from '@microsoft/mgt-react';
+import { useGraphToolkit } from "./lib/useGraphToolkit";
 
 export function Graph() {
-  const { loading, error, data, reload } = useGraph(
-    async (graph) => {
-      const profile = await graph.api("/me").get();
-      let photoUrl = "";
-      try {
-        const photo = await graph.api("/me/photo/$value").get();
-        photoUrl = URL.createObjectURL(photo);
-      } catch {
-        // Could not fetch photo from user's profile, return empty string as placeholder.
-      }
-      return { profile, photoUrl };
-    },
-    { scope: ["User.Read"] }
-  );
+  const { providerState } = useGraphToolkit();  
 
   return (
     <div>
-      <h2>Get the user's profile photo</h2>
-      <p>Click below to authorize this app to read your profile photo using Microsoft Graph.</p>
-      <Button primary content="Authorize" disabled={loading} onClick={reload} />
-      {loading && ProfileCard(true)}
-      {!loading && error && (
-        <div className="error">
-          Failed to read your profile. Please try again later. <br /> Details: {error.toString()}
-        </div>
-      )}
-      {!loading && data && ProfileCard(false, data)}
+        {providerState === ProviderState.SignedIn && <Person 
+          personQuery="me" 
+          view={PersonViewType.threelines} 
+          personCardInteraction={PersonCardInteraction.hover}>
+        </Person>}
     </div>
   );
 }
---
+++ templates/tab/ts/default/src/components/sample/lib/useGraphToolkit.ts
@@ -0,0 +1,32 @@
+import { LogLevel, setLogLevel, setLogFunction } from "@microsoft/teamsfx";
+import { Providers, ProvidersChangedState, ProviderState } from "@microsoft/mgt-element";
+import { TeamsFxProvider } from "../../../TeamsFxProvider";
+import React from "react";
+
+export function useGraphToolkit() {
+  const [providerState, setProviderState] = React.useState<ProviderState>(ProviderState.Loading);
+      
+  React.useEffect(() => {
+    if (process.env.NODE_ENV === "development") {
+      setLogLevel(LogLevel.Verbose);
+      setLogFunction((level: LogLevel, message: string) => { console.log(message); });
+    }
+
+    Providers.globalProvider = new TeamsFxProvider({      
+      scopes: [
+        "User.Read",
+        "User.ReadBasic.All"
+      ]
+    });
+
+    setProviderState(Providers.globalProvider.state);
+
+    Providers.onProviderUpdated((stateEvent: ProvidersChangedState) => {
+      if(stateEvent === ProvidersChangedState.ProviderStateChanged) {
+        setProviderState(Providers.globalProvider.state);
+      }      
+    });
+  }, []);
+
+  return { providerState };
+}

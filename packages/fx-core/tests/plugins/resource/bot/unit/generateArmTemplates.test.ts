509c7d4c44b369a3be901d51dde97d26b1146e52
wenyutang-ms <wenyutang@microsoft.com>
2021-12-01 11:02:09 +0800

    test: add bot test case

--- packages/fx-core/tests/plugins/resource/bot/unit/generateArmTemplates.test.ts
+++ packages/fx-core/tests/plugins/resource/bot/unit/generateArmTemplates.test.ts
@@ -107,4 +107,69 @@ describe("Bot Generates Arm Templates", () => {
       );
     }
   });
+
+  it("Update bicep arm templates", async () => {
+    // Arrange
+    const activeResourcePlugins = [
+      ResourcePlugins.Aad,
+      ResourcePlugins.Bot,
+      ResourcePlugins.Identity,
+    ];
+    const pluginContext: PluginContext = testUtils.newPluginContext();
+    const azureSolutionSettings = pluginContext.projectSettings!
+      .solutionSettings! as AzureSolutionSettings;
+    azureSolutionSettings.activeResourcePlugins = activeResourcePlugins;
+    pluginContext.projectSettings!.solutionSettings = azureSolutionSettings;
+
+    // Act
+    const result = await botPlugin.generateArmTemplates(pluginContext);
+
+    // Assert
+    const provisionModuleFileName = "botProvision.result.bicep";
+    const configurationModuleFileName = "botConfig.result.bicep";
+    const mockedSolutionDataContext = {
+      Plugins: activeResourcePlugins,
+      PluginOutput: {
+        "fx-resource-bot": {
+          Provision: {
+            bot: {
+              ProvisionPath: `./${provisionModuleFileName}`,
+            },
+          },
+          Configuration: {
+            bot: {
+              ConfigPath: `./${configurationModuleFileName}`,
+            },
+          },
+        },
+        "fx-resource-identity": {
+          References: {
+            identityName: "provisionOutputs.identityOutput.value.identityName",
+            identityClientId: "provisionOutputs.identityOutput.value.identityClientId",
+            identityResourceId: "userAssignedIdentityProvision.outputs.identityResourceId",
+          },
+        },
+      },
+    };
+    chai.assert.isTrue(result.isOk());
+    if (result.isOk()) {
+      const compiledResult = mockSolutionUpdateArmTemplates(
+        mockedSolutionDataContext,
+        result.value
+      );
+      const expectedBicepFileDirectory = path.join(__dirname, "expectedBicepFiles");
+      const configModuleFile = await fs.readFile(
+        path.join(expectedBicepFileDirectory, configurationModuleFileName),
+        ConstantString.UTF8Encoding
+      );
+      chai.assert.strictEqual(compiledResult.Configuration!.Modules!.bot, configModuleFile);
+      chai.assert.notExists(compiledResult.Provision!.Orchestration);
+      chai.assert.notExists(compiledResult.Provision!.Modules);
+      chai.assert.notExists(compiledResult.Configuration!.Orchestration);
+      chai.assert.notExists(compiledResult.Parameters);
+      chai.assert.exists(compiledResult.Provision!.Reference!.resourceId);
+      chai.assert.exists(compiledResult.Provision!.Reference!.hostName);
+      chai.assert.exists(compiledResult.Provision!.Reference!.webAppEndpoint);
+    }
+  });
 });

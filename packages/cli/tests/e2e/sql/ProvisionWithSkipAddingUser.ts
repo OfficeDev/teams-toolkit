243b85ba25507ddee48e202ea6e193a4c98237d1
Zhaofeng Xu <zhaofengxu@microsoft.com>
2021-12-03 10:49:46 +0800

    chore(sql): add e2e test

---
+++ packages/cli/tests/e2e/sql/ProvisionWithSkipAddingUser.ts
@@ -0,0 +1,88 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+/**
+ * @author Zhaofeng Xu <zhaofengxu@microsoft.com>
+ */
+
+import { environmentManager, isMultiEnvEnabled } from "@microsoft/teamsfx-core";
+import fs from "fs-extra";
+import path from "path";
+
+import { SqlValidator } from "../../commonlib";
+
+import {
+  execAsync,
+  execAsyncWithRetry,
+  getSubscriptionId,
+  getTestFolder,
+  getUniqueAppName,
+  setSimpleAuthSkuNameToB1,
+  cleanUp,
+  setSimpleAuthSkuNameToB1Bicep,
+  setSkipAddingSqlUserToConfig,
+  setSkipAddingSqlUser,
+} from "../commonUtils";
+
+describe("Provision to Azure with SQL", function () {
+  const testFolder = getTestFolder();
+  const appName = getUniqueAppName();
+  const subscription = getSubscriptionId();
+  const projectPath = path.resolve(testFolder, appName);
+
+  it(`Provision SQL with skip adding user`, async function () {
+    // new a project ( tab + function + sql )
+    await execAsync(
+      `teamsfx new --interactive false --app-name ${appName} --capabilities tab --azure-resources function sql`,
+      {
+        cwd: testFolder,
+        env: process.env,
+        timeout: 0,
+      }
+    );
+    console.log(`[Successfully] scaffold to ${projectPath}`);
+
+    if (isMultiEnvEnabled()) {
+      await setSimpleAuthSkuNameToB1Bicep(projectPath, environmentManager.getDefaultEnvName());
+      await setSkipAddingSqlUserToConfig(projectPath, environmentManager.getDefaultEnvName());
+    } else {
+      await setSimpleAuthSkuNameToB1(projectPath);
+      await setSkipAddingSqlUser(projectPath);
+    }
+
+    // provision
+    await execAsyncWithRetry(
+      `teamsfx provision --subscription ${subscription} --sql-admin-name Abc123321 --sql-password Cab232332`,
+      {
+        cwd: projectPath,
+        env: process.env,
+        timeout: 0,
+      }
+    );
+
+    if (isMultiEnvEnabled()) {
+      // Get context
+      const context = await fs.readJSON(`${projectPath}/.fx/states/state.dev.json`);
+
+      // Validate Aad App
+      await SqlValidator.init(context);
+      await SqlValidator.validateSql(0);
+    } else {
+      // Get context
+      const context = await fs.readJSON(`${projectPath}/.fx/env.default.json`);
+
+      // Validate Aad App
+      await SqlValidator.init(context);
+      await SqlValidator.validateSql(0);
+    }
+  });
+
+  after(async () => {
+    // clean up
+    if (isMultiEnvEnabled()) {
+      await cleanUp(appName, projectPath, true, false, false, true);
+    } else {
+      await cleanUp(appName, projectPath, true, false, false);
+    }
+  });
+});

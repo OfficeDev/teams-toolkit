b3552b3c526c12b096ff6e85a337f22cdf74139d
huajiezhang <huajiezhang@microsoft.com>
2021-12-07 20:40:57 +0800

    chore(api): improve

--- packages/api/src/v3/core.ts
+++ packages/api/src/v3/core.ts
@@ -1,7 +1,7 @@
-import { Core, FxError, Result, Void } from "..";
+import { Func, FxError, Inputs, Result, Void } from "..";
 import { InputsWithProjectPath } from "../v2";
 
-export interface CoreV3 extends Core {
+export interface ICore {
   /**
    * init means enable TeamsFx feature for current project folder. There are two cases:
    * 1. Init in an empty folder
@@ -12,20 +12,40 @@ export interface CoreV3 extends Core {
   /**
    * scaffold will be an independent stage
    */
-  scaffold: (inputs: InputsWithProjectPath) => Promise<Result<Void, FxError>>;
+  scaffold: (
+    inputs: InputsWithProjectPath & { moduleIndex?: number }
+  ) => Promise<Result<Void, FxError>>;
   /**
    * A module is a connection between the local code and cloud resource for deployment stage.
    * addModule only update project settings while add capability does more.
    */
-  addModule: (inputs: InputsWithProjectPath) => Promise<Result<Void, FxError>>;
-
+  addModule: (
+    inputs: InputsWithProjectPath & { capabilities?: string[] }
+  ) => Promise<Result<Void, FxError>>;
   /**
    * addResource is separated from executeUserTask
    */
-  addResource: (inputs: InputsWithProjectPath) => Promise<Result<Void, FxError>>;
+  addResource: (
+    inputs: InputsWithProjectPath & { moduleIndex?: number }
+  ) => Promise<Result<Void, FxError>>;
 
   /**
-   * addCapability is separated from executeUserTask
+   * provision resources
    */
-  addCapability: (inputs: InputsWithProjectPath) => Promise<Result<Void, FxError>>;
+  provisionResources: (inputs: InputsWithProjectPath) => Promise<Result<Void, FxError>>;
+
+  /**
+   * deploy bits
+   */
+  deployArtifacts: (inputs: InputsWithProjectPath) => Promise<Result<Void, FxError>>;
+
+  /**
+   * publish application
+   */
+  publishApplication: (inputs: InputsWithProjectPath) => Promise<Result<Void, FxError>>;
+
+  /**
+   * execute user customized task
+   */
+  executeUserTask: (func: Func, inputs: Inputs) => Promise<Result<unknown, FxError>>;
 }

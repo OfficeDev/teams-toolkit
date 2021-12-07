96fd6edc1e6cf3eb6c5a2e3d31927eff2043c92a
turenlong <rentu@microsoft.com>
2021-12-07 12:35:10 +0800

    test(collaboration): collaboration add e2e test for SPFX project

--- packages/cli/tests/e2e/collaboration/Permission.tests.ts
+++ packages/cli/tests/e2e/collaboration/Permission.tests.ts
@@ -101,6 +101,80 @@ describe("Collaboration", function () {
     console.log("[Successfully] list collaborator");
   });
 
+  it("Collaboration: CLI with permission status and permission grant - spfx", async function () {
+    if (!isRemoteCollaborateEnabled()) {
+      return;
+    }
+    // new a project
+    await execAsync(
+      `teamsfx new --interactive false --capabilities tab-spfx --app-name ${appName}`,
+      {
+        cwd: testFolder,
+        env: process.env,
+        timeout: 0,
+      }
+    );
+    console.log(`[Successfully] scaffold to ${projectPath}`);
+
+    // provision
+    await execAsyncWithRetry(`teamsfx provision`, {
+      cwd: projectPath,
+      env: process.env,
+      timeout: 0,
+    });
+    console.log("[Successfully] provision");
+
+    // Check Permission
+    const checkPermissionResult = await execAsyncWithRetry(`teamsfx permission status`, {
+      cwd: projectPath,
+      env: process.env,
+      timeout: 0,
+    });
+
+    expect(checkPermissionResult.stdout).to.contains(
+      "Resource Name: Teams App, Permission: Administrator"
+    );
+    console.log("[Successfully] check permission");
+
+    // Grant Permission
+    const grantCollaboratorResult = await execAsyncWithRetry(
+      `teamsfx permission grant --email ${collaborator}`,
+      {
+        cwd: projectPath,
+        env: process.env,
+        timeout: 0,
+      }
+    );
+
+    expect(grantCollaboratorResult.stdout).to.contains(
+      "Administrator permission has been granted to Teams App"
+    );
+    console.log("[Successfully] grant permission");
+
+    const listCollaboratorResult = await execAsync(
+      `teamsfx permission status --list-all-collaborators`,
+      {
+        cwd: projectPath,
+        env: process.env,
+        timeout: 0,
+      }
+    );
+
+    // Check collaborator.
+    // When collaborator account is guest account in the tenant. Account name pattern will change.
+    // e.g. Guest account "account@example.com" will appear as "account_example#EXT#@exampleTenant.onmicrosoft.com" under tenant "exampleTenant".
+    // Thus here will check the account name only.
+    if (!isRemoteCollaborateEnabled()) {
+      expect(listCollaboratorResult.stdout).to.contains(
+        `Teams App Owner: ${collaborator?.split("@")[0]}`
+      );
+      expect(listCollaboratorResult.stdout).to.contains(
+        `Account used to check: ${creator?.split("@")[0]}`
+      );
+    }
+    console.log("[Successfully] list collaborator");
+  });
+
   after(async () => {
     // clean up
     if (isRemoteCollaborateEnabled()) {

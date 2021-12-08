fa86cc7d57646728c101e345abcfbb6390980b98
Huajie Zhang <zhjay23@qq.com>
2021-12-08 10:59:22 +0800

    chore(api): v3 core improve (#3254)
    * feat(api): v3 core improve
    
    * chore(api): improve
    
    * chore(api): improve

--- packages/api/src/v3/index.ts
+++ packages/api/src/v3/index.ts
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
+
 export * from "./types";
 export * from "./resourceStates";
 export * from "./plugins";
--- packages/api/src/v3/solution.ts
+++ packages/api/src/v3/solution.ts
@@ -1,81 +1,141 @@
-import Module from "module";
-import { FxError, Inputs, Json, QTreeNode, Result, TokenProvider, v2, Void } from "..";
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import {
+  AppStudioTokenProvider,
+  Func,
+  FxError,
+  Inputs,
+  Json,
+  QTreeNode,
+  Result,
+  TokenProvider,
+  Void,
+} from "..";
 import { Context, DeepReadonly, InputsWithProjectPath } from "../v2/types";
 import { EnvInfoV3 } from "./types";
 
-export type StrictOmit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+// export type StrictOmit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
-export type SolutionPluginV3 = StrictOmit<v2.SolutionPlugin, "getQuestions"> & {
-  /**
-   * add resource is no more implemented in executeUserTask
-   * steps:
-   * 1. update project settings
-   * 2. update local settings
-   * 3. generate arm template
-   * 4. re-generate local debug scripts
-   */
-  addResource: (
-    ctx: Context,
-    localSettings: Json,
-    inputs: InputsWithProjectPath & { module?: keyof Module }
-  ) => Promise<Result<Void, FxError>>;
+export interface ISolution {
+  name: string;
 
   /**
-   * add capability is no more implemented in executeUserTask
-   * steps:
-   * 1. update project settings
-   * 2. update local settings
-   * 3. re-generate local debug scripts
+   * init
    */
-  addCapability: (
+  getQuestionsForInit?: (
     ctx: Context,
-    localSettings: Json,
     inputs: InputsWithProjectPath
+  ) => Promise<Result<QTreeNode | undefined, FxError>>;
+  init: (ctx: Context, inputs: InputsWithProjectPath) => Promise<Result<Void, FxError>>;
+
+  /**
+   * scaffold
+   */
+  getQuestionsForScaffolding?: (
+    ctx: Context,
+    inputs: InputsWithProjectPath
+  ) => Promise<Result<QTreeNode | undefined, FxError>>;
+  scaffold: (
+    ctx: Context,
+    inputs: InputsWithProjectPath & { moduleIndex?: number }
   ) => Promise<Result<Void, FxError>>;
 
   /**
-   * customize questions needed for add resource operation
+   * addResource
    */
   getQuestionsForAddResource?: (
     ctx: Context,
-    inputs: Inputs
+    inputs: InputsWithProjectPath
   ) => Promise<Result<QTreeNode | undefined, FxError>>;
-  /**
-   * customize questions needed for local debug
-   */
-  getQuestionsForLocalProvision?: (
+  addResource: (
     ctx: Context,
-    inputs: Inputs,
-    localSettings: DeepReadonly<Json>,
-    tokenProvider: TokenProvider
-  ) => Promise<Result<QTreeNode | undefined, FxError>>;
-  /**
-   * customize questions needed for provision
-   */
-  getQuestionsForProvision?: (
-    ctx: Context,
-    inputs: Inputs,
-    envInfo: DeepReadonly<EnvInfoV3>,
-    tokenProvider: TokenProvider
-  ) => Promise<Result<QTreeNode | undefined, FxError>>;
-  /**
-   * customize questions needed for deploy
-   */
-  getQuestionsForDeploy?: (
-    ctx: Context,
-    inputs: Inputs,
-    envInfo: DeepReadonly<EnvInfoV3>,
-    tokenProvider: TokenProvider
-  ) => Promise<Result<QTreeNode | undefined, FxError>>;
+    inputs: InputsWithProjectPath & { moduleIndex?: number }
+  ) => Promise<Result<Void, FxError>>;
 
   /**
-   * 1. modify the envInfo type to EnvInfoV3
-   * 2. modify the return type, to simplify the implementation cost, for partial success scenario, solution will directly update the envInfo input
+   * addModule
    */
-  provisionResource?: (
+  getQuestionsForAddModule?: (
+    ctx: Context,
+    inputs: InputsWithProjectPath
+  ) => Promise<Result<QTreeNode | undefined, FxError>>;
+  addModule: (
+    ctx: Context,
+    inputs: InputsWithProjectPath & { capabilities?: string[] }
+  ) => Promise<Result<Void, FxError>>;
+
+  //provision
+  getQuestionsForProvision?: (
+    ctx: Context,
+    inputs: InputsWithProjectPath,
+    envInfo: DeepReadonly<EnvInfoV3>,
+    tokenProvider: TokenProvider
+  ) => Promise<Result<QTreeNode | undefined, FxError>>;
+  provisionResources?: (
     ctx: Context,
     inputs: InputsWithProjectPath,
     envInfo: EnvInfoV3,
     tokenProvider: TokenProvider
+  ) => Promise<Result<EnvInfoV3, FxError>>;
+
+  //local provision
+  getQuestionsForLocalProvision?: (
+    ctx: Context,
+    inputs: InputsWithProjectPath,
+    localSettings: DeepReadonly<Json>,
+    tokenProvider: TokenProvider
+  ) => Promise<Result<QTreeNode | undefined, FxError>>;
+  provisionLocalResources?: (
+    ctx: Context,
+    inputs: InputsWithProjectPath,
+    localSettings: Json,
+    tokenProvider: TokenProvider
+  ) => Promise<Result<Json, FxError>>;
+
+  //deploy
+  getQuestionsForDeploy?: (
+    ctx: Context,
+    inputs: InputsWithProjectPath,
+    envInfo: DeepReadonly<EnvInfoV3>,
+    tokenProvider: TokenProvider
+  ) => Promise<Result<QTreeNode | undefined, FxError>>;
+  deploy?: (
+    ctx: Context,
+    inputs: InputsWithProjectPath,
+    envInfo: DeepReadonly<EnvInfoV3>,
+    tokenProvider: TokenProvider
   ) => Promise<Result<Void, FxError>>;
-};
+
+  //publish
+  getQuestionsForPublish?: (
+    ctx: Context,
+    inputs: InputsWithProjectPath,
+    envInfo: DeepReadonly<EnvInfoV3>,
+    tokenProvider: AppStudioTokenProvider
+  ) => Promise<Result<QTreeNode | undefined, FxError>>;
+  publishApplication: (
+    ctx: Context,
+    inputs: InputsWithProjectPath,
+    envInfo: DeepReadonly<EnvInfoV3>,
+    tokenProvider: AppStudioTokenProvider
+  ) => Promise<Result<Void, FxError>>;
+
+  //user task
+  getQuestionsForUserTask?: (
+    ctx: Context,
+    inputs: Inputs,
+    func: Func,
+    localSettings: Json,
+    envInfo: DeepReadonly<EnvInfoV3>,
+    tokenProvider: TokenProvider
+  ) => Promise<Result<QTreeNode | undefined, FxError>>;
+  executeUserTask?: (
+    ctx: Context,
+    inputs: Inputs,
+    func: Func,
+    localSettings: Json,
+    envInfo: EnvInfoV3,
+    tokenProvider: TokenProvider
+  ) => Promise<Result<unknown, FxError>>;
+}

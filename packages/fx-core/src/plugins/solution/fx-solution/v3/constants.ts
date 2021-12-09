4845986b2235fc6459036fc9fffa93dbdca9db5b
huajiezhang <huajiezhang@microsoft.com>
2021-12-09 13:17:04 +0800

    feat(solution): v3 init

---
+++ packages/fx-core/src/plugins/solution/fx-solution/v3/constants.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export const SolutionNameV3 = "fx-solution-azure-v3";
+
+export const BuiltInResourcePluginNames = {
+  AppStudio: "fx-resource-appstudio",
+  AAD: "fx-resource-aad",
+};

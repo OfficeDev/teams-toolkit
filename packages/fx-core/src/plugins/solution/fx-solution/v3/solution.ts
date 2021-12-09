546d5d695a3a9ea72d3443b47c71b74979b5b175
huajiezhang <huajiezhang@microsoft.com>
2021-12-09 13:30:02 +0800

    feat(solution): up

--- packages/fx-core/src/plugins/solution/fx-solution/v3/solution.ts
+++ packages/fx-core/src/plugins/solution/fx-solution/v3/solution.ts
@@ -14,24 +14,24 @@ import { getQuestionsForPublish, publishApplication } from "./publish";
 import { getQuestionsForScaffold, scaffold } from "./scaffold";
 import { executeUserTask, getQuestionsForUserTask } from "./userTask";
 
-@Service(SolutionNameV3)
-export class TeamsFxAzureSolution implements v3.ISolution {
-  name = SolutionNameV3;
-  init = init;
-  getQuestionsForScaffold = getQuestionsForScaffold;
-  scaffold = scaffold;
-  getQuestionsForAddResource = getQuestionsForAddResource;
-  addResource = addResource;
-  getQuestionsForAddModule = getQuestionsForAddModule;
-  addModule = addModule;
-  getQuestionsForProvision = getQuestionsForProvision;
-  provisionResources = provisionResources;
-  getQuestionsForLocalProvision = getQuestionsForLocalProvision;
-  provisionLocalResources = provisionLocalResources;
-  getQuestionsForDeploy = getQuestionsForDeploy;
-  deploy = deploy;
-  getQuestionsForPublish = getQuestionsForPublish;
-  publishApplication = publishApplication;
-  getQuestionsForUserTask = getQuestionsForUserTask;
-  executeUserTask = executeUserTask;
-}
+// @Service(SolutionNameV3)
+// export class TeamsFxAzureSolution implements v3.ISolution {
+//   name = SolutionNameV3;
+//   init = init;
+//   getQuestionsForScaffold = getQuestionsForScaffold;
+//   scaffold = scaffold;
+//   getQuestionsForAddResource = getQuestionsForAddResource;
+//   addResource = addResource;
+//   getQuestionsForAddModule = getQuestionsForAddModule;
+//   addModule = addModule;
+//   getQuestionsForProvision = getQuestionsForProvision;
+//   provisionResources = provisionResources;
+//   getQuestionsForLocalProvision = getQuestionsForLocalProvision;
+//   provisionLocalResources = provisionLocalResources;
+//   getQuestionsForDeploy = getQuestionsForDeploy;
+//   deploy = deploy;
+//   getQuestionsForPublish = getQuestionsForPublish;
+//   publishApplication = publishApplication;
+//   getQuestionsForUserTask = getQuestionsForUserTask;
+//   executeUserTask = executeUserTask;
+// }

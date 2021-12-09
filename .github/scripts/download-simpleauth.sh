fbe58a2fab64b772f998bc322622bbdef51c4c66
wenyutang-ms <wenyutang@microsoft.com>
2021-12-09 15:50:35 +0800

    chore: verbose on download simpleauth

--- .github/scripts/download-simpleauth.sh
+++ .github/scripts/download-simpleauth.sh
@@ -3,4 +3,4 @@ version=$(head ./packages/fx-core/templates/plugins/resource/simpleauth/version.
 tag=simpleauth@$version
 fileName=Microsoft.TeamsFx.SimpleAuth_$version.zip
 url=https://github.com/OfficeDev/TeamsFx/releases/download/$tag/$fileName
-curl $url -L -J -o packages/fx-core/templates/plugins/resource/simpleauth/SimpleAuth.zip
+curl $url -L -J -o packages/fx-core/templates/plugins/resource/simpleauth/SimpleAuth.zip --verbose -skw "time_connect: %{time_connect} s\ntime_namelookup: %{time_namelookup} s\ntime_pretransfer: %{time_pretransfer} s\ntime_starttransfer: %{time_starttransfer} s\ntime_redirect: %{time_redirect} s\nspeed_download: %{speed_download} B/s\ntime_total: %{time_total} s\n\n"

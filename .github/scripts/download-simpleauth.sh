f5d6cab58bab3caf228a18eaf8274990f526e0d6
wenyutang-ms <wenyutang@microsoft.com>
2021-12-10 09:33:54 +0800

    chore: check simpleauth md5

--- .github/scripts/download-simpleauth.sh
+++ .github/scripts/download-simpleauth.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+declare -A simpleauthFilesMd5=( ["0.1.0"]="e59ce68a26b74dba473e34c381678910" ["0.1.1"]="01f306767cb689dac8d973deccc1062d" ["0.1.2"]="d329b68923b81217e11230c5700ec5bf")
 version=$(head ./packages/fx-core/templates/plugins/resource/simpleauth/version.txt)
 tag=simpleauth@$version
 fileName=Microsoft.TeamsFx.SimpleAuth_$version.zip
 url=https://github.com/OfficeDev/TeamsFx/releases/download/$tag/$fileName
-curl $url -L -J -o packages/fx-core/templates/plugins/resource/simpleauth/SimpleAuth.zip --verbose -skw "time_connect: %{time_connect} s\ntime_namelookup: %{time_namelookup} s\ntime_pretransfer: %{time_pretransfer} s\ntime_starttransfer: %{time_starttransfer} s\ntime_redirect: %{time_redirect} s\nspeed_download: %{speed_download} B/s\ntime_total: %{time_total} s\n\n"
+curl $url -L -J -o packages/fx-core/templates/plugins/resource/simpleauth/SimpleAuth.zip
+filemd5=$(md5sum packages/fx-core/templates/plugins/resource/simpleauth/SimpleAuth.zip | cut -d ' ' -f1)
+echo "============================================"
+echo $filemd5
+if [ "$filemd5" != ${simpleauthFilesMd5["$version"]} ]
+then
+    echo "SimpleAuth not the right file, md5sum not match".
+    exit -1
+fi

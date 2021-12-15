53cbd9de4c245832f72c5dcd80ae2aff46bb8c01
Zihong Chen <zihch@microsoft.com>
2021-12-15 11:15:13 +0800

    style: add two additional publish link buttons

--- packages/fx-core/src/plugins/resource/appstudio/constants.ts
+++ packages/fx-core/src/plugins/resource/appstudio/constants.ts
@@ -8,9 +8,11 @@ export class Constants {
   public static readonly PUBLISH_PATH_QUESTION = "manifest-folder";
   public static readonly BUILD_OR_PUBLISH_QUESTION = "build-or-publish";
   public static readonly READ_MORE = "Read more";
+  public static readonly LEARN_MORE = "Learn more";
+  public static readonly ADMIN_PORTAL = "Admin portal";
   public static readonly PUBLISH_GUIDE = "https://aka.ms/teamsfx-publish";
-  public static readonly TEAMS_ADMIN_PORTAL =
-    "https://admin.teams.microsoft.com/policies/manage-apps";
+  public static readonly TEAMS_ADMIN_PORTAL = "https://aka.ms/teamsfx-mtac";
+  public static readonly TEAMS_MANAGE_APP_DOC = "https://aka.ms/teamsfx-mtac-doc";
   public static readonly TEAMS_APP_ID = "teamsAppId";
 
   public static readonly PERMISSIONS = {

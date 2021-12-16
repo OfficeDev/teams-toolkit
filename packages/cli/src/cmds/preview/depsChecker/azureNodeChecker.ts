4a30fa668bf68b1d3ef5d98fb49863587f803617
Kuojian Lu <kuojianlu@gmail.com>
2021-12-16 11:06:13 +0800

    fix(env-checker): remove continue anyway button (#3343)
    * fix(env-checker): remove continue anyway button

--- packages/cli/src/cmds/preview/depsChecker/azureNodeChecker.ts
+++ packages/cli/src/cmds/preview/depsChecker/azureNodeChecker.ts
@@ -1,9 +1,29 @@
-import { DepsCheckerEvent, nodeNotFoundHelpLink, nodeNotSupportedForAzureHelpLink } from "./common";
+import {
+  DepsCheckerEvent,
+  nodeNotFoundHelpLink,
+  nodeNotSupportedForAzureHelpLink,
+  nodeNotSupportedForFunctionsHelpLink,
+} from "./common";
+
 import { NodeChecker } from "./nodeChecker";
 
 export class AzureNodeChecker extends NodeChecker {
-  protected readonly _supportedVersions = ["10", "12", "14"];
   protected readonly _nodeNotFoundHelpLink = nodeNotFoundHelpLink;
-  protected readonly _nodeNotSupportedHelpLink = nodeNotSupportedForAzureHelpLink;
   protected readonly _nodeNotSupportedEvent = DepsCheckerEvent.nodeNotSupportedForAzure;
+
+  protected async getNodeNotSupportedHelpLink(): Promise<string> {
+    if (await this._adapter.hasTeamsfxBackend()) {
+      return nodeNotSupportedForFunctionsHelpLink;
+    } else {
+      return nodeNotSupportedForAzureHelpLink;
+    }
+  }
+
+  protected async getSupportedVersions(): Promise<string[]> {
+    if (await this._adapter.hasTeamsfxBackend()) {
+      return ["10", "12", "14"];
+    } else {
+      return ["10", "12", "14", "16"];
+    }
+  }
 }

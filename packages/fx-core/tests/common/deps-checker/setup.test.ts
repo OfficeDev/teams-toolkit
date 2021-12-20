aa6fba0bd62d6998492079a491cdf42e5bebacee
pengfeizhao <pengfeizhao@microsoft.com>
2021-12-20 17:23:19 +0800

    chore: setup it test

---
+++ packages/fx-core/tests/common/deps-checker/setup.test.ts
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { assert } from "chai";
+import "mocha";
+import fs from "fs-extra";
+import sinon from "sinon";
+import { globalStateGet } from "../../../src";
+
+describe("Helloworld", async () => {
+  it("Helloworld", () => {
+    assert.isNotNull("setup IT test");
+  });
+});

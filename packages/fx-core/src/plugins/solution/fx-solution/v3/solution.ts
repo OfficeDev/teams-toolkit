0035d0ad926725410d534025b9b1e6be69213aa4
huajiezhang <huajiezhang@microsoft.com>
2021-12-20 16:48:49 +0800

    fix: spfx

--- packages/fx-core/src/plugins/solution/fx-solution/v3/solution.ts
+++ packages/fx-core/src/plugins/solution/fx-solution/v3/solution.ts
@@ -36,25 +36,3 @@ export class TeamsFxAzureSolution implements v3.ISolution {
   getQuestionsForUserTask = getQuestionsForUserTask;
   executeUserTask = executeUserTask;
 }
-
-@Service("fx-solution-spfx")
-export class TeamsFxSFPxSolution implements v3.ISolution {
-  name = "fx-solution-spfx";
-  init = init;
-  getQuestionsForScaffold = getQuestionsForScaffold;
-  scaffold = scaffold;
-  getQuestionsForAddResource = getQuestionsForAddResource;
-  addResource = addResource;
-  getQuestionsForAddModule = getQuestionsForAddModule;
-  addModule = addModule;
-  getQuestionsForProvision = getQuestionsForProvision;
-  provisionResources = provisionResources;
-  getQuestionsForLocalProvision = getQuestionsForLocalProvision;
-  provisionLocalResources = provisionLocalResources;
-  getQuestionsForDeploy = getQuestionsForDeploy;
-  deploy = deploy;
-  getQuestionsForPublish = getQuestionsForPublish;
-  publishApplication = publishApplication;
-  getQuestionsForUserTask = getQuestionsForUserTask;
-  executeUserTask = executeUserTask;
-}

24029618f093c32c9efd937828e3651b80b395a6
wenyutang-ms <wenyutang@microsoft.com>
2021-12-22 21:29:33 +0800

    chore: eslint check secrets

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -18,6 +18,7 @@ module.exports = {
     plugins: [
         "@typescript-eslint/eslint-plugin",
         "prettier",
+        "no-secrets"
     ],
     rules: {
         'prettier/prettier': 'error',
@@ -34,6 +35,9 @@ module.exports = {
         ],
         "import/no-unresolved": [
             "warn"
-        ]
+        ],
+        "no-secrets/no-secrets": ["error",
+        { "additionalRegexes": { "Basic Auth": "Authorization: Basic [A-Za-z0-9+/=]*" } }
+    ]
     },
 };

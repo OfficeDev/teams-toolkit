356c7bf7720ae84b8fca07b549ffbf805d8b7e8e
Qianhao Dong <qidon@microsoft.com>
2021-12-23 12:33:32 +0800

    refactor: get ports in use

---
+++ packages/fx-core/src/common/local/constants.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+"use strict";
+
+export class FolderName {
+  static readonly Frontend = "tabs";
+  static readonly Bot = "bot";
+  static readonly Function = "api";
+}

yukun-dong/TeamsFx

chore: add desp-checker

packages/fx-core/src/common/deps-checker/depsTelemetry.ts
@@ -1,0 +1,18 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { DepsCheckerEvent } from "./common/telemetry";
+
+export interface DepsTelemetry {
+  sendEvent(
+    eventName: DepsCheckerEvent,
+    properties?: { [p: string]: string },
+    timecost?: number
+  ): void;
+
+  sendEventWithDuration(eventName: DepsCheckerEvent, action: () => Promise<void>): Promise<void>;
+
+  sendUserErrorEvent(eventName: DepsCheckerEvent, errorMessage: string): void;
+
+  sendSystemErrorEvent(eventName: DepsCheckerEvent, errorMessage: string, errorStack: string): void;
+}

packages/fx-core/src/common/deps-checker/internal/ngrokChecker.ts
@@ -1,0 +1,247 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import * as fs from "fs-extra";
+import * as path from "path";
+import * as os from "os";
+import { ConfigFolderName } from "@microsoft/teamsfx-api";
+
+import { ngrokInstallHelpLink } from "../common/helpLink";
+import { DepsCheckerError } from "../common/errors";
+import { runWithProgressIndicator } from "../util/progressIndicator";
+import { cpUtils } from "../util/cpUtils";
+import { isWindows } from "../util/system";
+import { DepsCheckerEvent, TelemtryMessages } from "../common/telemetry";
+import { DepsLogger } from "../depsLogger";
+import { DepsTelemetry } from "../depsTelemetry";
+import { DepsInfo, DepsChecker } from "../depsChecker";
+import { Messages } from "../common/message";
+
+const ngrokName = "ngrok";
+
+const installPackageVersion = "4.2.2";
+const supportedPackageVersions = [">=3.4.0"];
+const supportedBinVersions = ["2.3"];
+const displayNgrokName = `${ngrokName}@${installPackageVersion}`;
+
+const timeout = 5 * 60 * 1000;
+
+export class NgrokChecker implements DepsChecker {
+  private readonly _logger: DepsLogger;
+  private readonly _telemetry: DepsTelemetry;
+
+  constructor(logger: DepsLogger, telemetry: DepsTelemetry) {
+    this._logger = logger;
+    this._telemetry = telemetry;
+  }
+
+  public getDepsInfo(): Promise<DepsInfo> {
+    return Promise.resolve({
+      name: ngrokName,
+      installVersion: installPackageVersion,
+      supportedVersions: supportedPackageVersions,
+      details: new Map<string, string>(),
+    });
+  }
+
+  public async isInstalled(): Promise<boolean> {
+    let isVersionSupported = false,
+      hasSentinel = false;
+    try {
+      const ngrokVersion = await this.queryNgrokBinVersion();
+      isVersionSupported =
+        ngrokVersion !== undefined && supportedBinVersions.includes(ngrokVersion);
+      hasSentinel = await fs.pathExists(this.getSentinelPath());
+    } catch (error) {
+      // do nothing
+      return false;
+    }
+    return isVersionSupported && hasSentinel;
+  }
+
+  public async install(): Promise<void> {
+    if (!(await this.hasNPM())) {
+      this.handleNpmNotFound();
+    }
+
+    await this.cleanup();
+    await this.installNgrok();
+
+    if (!(await this.validate())) {
+      await this.handleInstallNgrokFailed();
+    }
+
+    this._telemetry.sendEvent(DepsCheckerEvent.ngrokInstallCompleted);
+    await this._logger.info(Messages.finishInstallNgrok.replace("@NameVersion", displayNgrokName));
+  }
+
+  public getNgrokBinFolder(): string {
+    return path.join(this.getDefaultInstallPath(), "node_modules", "ngrok", "bin");
+  }
+
+  // TODO: integrate into checker.ts after checker supports linux
+  public async resolve(): Promise<boolean> {
+    try {
+      if (!(await this.isInstalled())) {
+        // TODO: show output in extension
+        // this._adapter.showOutputChannel();
+        await this.install();
+        this._logger.cleanup();
+      }
+    } catch (error) {
+      await this._logger.printDetailLog();
+      this._logger.cleanup();
+      await this._logger.error(`Failed to install 'ngrok', error = '${error}'`);
+      return false;
+    }
+
+    return true;
+  }
+
+  private async handleInstallNgrokFailed(): Promise<void> {
+    await this.cleanup();
+
+    this._telemetry.sendSystemErrorEvent(
+      DepsCheckerEvent.ngrokInstallError,
+      TelemtryMessages.failedToInstallNgrok,
+      Messages.failToValidateNgrok.replace("@NameVersion", displayNgrokName)
+    );
+    throw new DepsCheckerError(
+      Messages.failToInstallNgrok.split("@NameVersion").join(displayNgrokName),
+      ngrokInstallHelpLink
+    );
+  }
+
+  private async validate(): Promise<boolean> {
+    let isVersionSupported = false;
+    let hasSentinel = false;
+    try {
+      const binVersion = await this.queryNgrokBinVersion();
+      isVersionSupported = binVersion !== undefined && supportedBinVersions.includes(binVersion);
+      hasSentinel = await fs.pathExists(this.getSentinelPath());
+    } catch (err) {
+      this._telemetry.sendSystemErrorEvent(
+        DepsCheckerEvent.ngrokValidationError,
+        TelemtryMessages.failedToValidateNgrok,
+        err
+      );
+    }
+
+    if (!isVersionSupported || !hasSentinel) {
+      this._telemetry.sendEvent(DepsCheckerEvent.ngrokValidationError, {
+        "ngrok-v": String(isVersionSupported),
+        sentinel: String(hasSentinel),
+      });
+    }
+    return isVersionSupported && hasSentinel;
+  }
+
+  private handleNpmNotFound() {
+    this._telemetry.sendEvent(DepsCheckerEvent.npmNotFound);
+    throw new DepsCheckerError(
+      Messages.needInstallNgrok.replace("@NameVersion", displayNgrokName),
+      ngrokInstallHelpLink
+    );
+  }
+
+  private getDefaultInstallPath(): string {
+    return path.join(os.homedir(), `.${ConfigFolderName}`, "bin", "ngrok");
+  }
+
+  private getSentinelPath(): string {
+    return path.join(os.homedir(), `.${ConfigFolderName}`, "ngrok-sentinel");
+  }
+
+  private async queryNgrokBinVersion(): Promise<string | undefined> {
+    const output = await cpUtils.executeCommand(
+      undefined,
+      this._logger,
+      {
+        shell: true,
+        env: { PATH: this.getNgrokBinFolder() },
+      },
+      ngrokName,
+      "version"
+    );
+
+    const regex =
+      /ngrok version (?<major_version>\d+)\.(?<minor_version>\d+)\.(?<patch_version>\d+)/gim;
+    const match = regex.exec(output);
+
+    if (!match || !match.groups) {
+      return undefined;
+    }
+
+    return `${match.groups.major_version}.${match.groups.minor_version}`;
+  }
+
+  private async hasNPM(): Promise<boolean> {
+    try {
+      const npmVersion = await cpUtils.executeCommand(
+        undefined,
+        this._logger,
+        { shell: true },
+        "npm",
+        "--version"
+      );
+      this._telemetry.sendEvent(DepsCheckerEvent.npmAlreadyInstalled, {
+        "npm-version": npmVersion,
+      });
+
+      return true;
+    } catch (error) {
+      this._telemetry.sendEvent(DepsCheckerEvent.npmNotFound);
+      return false;
+    }
+  }
+
+  private async cleanup(): Promise<void> {
+    try {
+      await fs.emptyDir(this.getDefaultInstallPath());
+      await fs.remove(this.getSentinelPath());
+    } catch (err) {
+      await this._logger.debug(
+        `Failed to clean up path: ${this.getDefaultInstallPath()}, error: ${err}`
+      );
+    }
+  }
+
+  private async installNgrok(): Promise<void> {
+    await this._telemetry.sendEventWithDuration(
+      DepsCheckerEvent.ngrokInstallScriptCompleted,
+      async () => {
+        await runWithProgressIndicator(async () => await this.doInstallNgrok(), this._logger);
+      }
+    );
+  }
+
+  private async doInstallNgrok(): Promise<void> {
+    await this._logger.info(Messages.startInstallNgrok.replace("@NameVersion", displayNgrokName));
+
+    try {
+      await cpUtils.executeCommand(
+        undefined,
+        this._logger,
+        { timeout: timeout, shell: false },
+        this.getExecCommand("npm"),
+        "install",
+        // not use -f, to avoid npm@6 bug: exit code = 0, even if install fail
+        `${ngrokName}@${installPackageVersion}`,
+        "--prefix",
+        `${this.getDefaultInstallPath()}`
+      );
+
+      await fs.ensureFile(this.getSentinelPath());
+    } catch (error) {
+      this._telemetry.sendSystemErrorEvent(
+        DepsCheckerEvent.ngrokInstallScriptError,
+        TelemtryMessages.failedToInstallNgrok,
+        error
+      );
+    }
+  }
+
+  private getExecCommand(command: string): string {
+    return isWindows() ? `${command}.cmd` : command;
+  }
+}

packages/fx-core/src/common/deps-checker/util/cpUtils.ts
@@ -1,0 +1,144 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import * as cp from "child_process";
+import * as os from "os";
+
+import { DepsLogger } from "../depsLogger";
+
+export namespace cpUtils {
+  export async function executeCommand(
+    workingDirectory: string | undefined,
+    logger: DepsLogger | undefined,
+    options: cp.SpawnOptions | undefined,
+    command: string,
+    ...args: string[]
+  ): Promise<string> {
+    const result: ICommandResult = await tryExecuteCommand(
+      workingDirectory,
+      logger,
+      options,
+      command,
+      ...args
+    );
+    if (result.code !== 0) {
+      const errorMessage = `Failed to run command: "${command} ${result.formattedArgs}", code: "${result.code}",
+                            output: "${result.cmdOutput}", error: "${result.cmdOutputIncludingStderr}"`;
+      await logger?.debug(errorMessage);
+      throw new Error(errorMessage);
+    } else {
+      await logger?.debug(`Finished running command: "${command} ${result.formattedArgs}".`);
+    }
+
+    return result.cmdOutput;
+  }
+
+  export async function tryExecuteCommand(
+    workingDirectory: string | undefined,
+    logger: DepsLogger | undefined,
+    additionalOptions: cp.SpawnOptions | undefined,
+    command: string,
+    ...args: string[]
+  ): Promise<ICommandResult> {
+    return await new Promise(
+      (resolve: (res: ICommandResult) => void, reject: (e: Error) => void): void => {
+        let cmdOutput = "";
+        let cmdOutputIncludingStderr = "";
+        const formattedArgs: string = args.join(" ");
+
+        workingDirectory = workingDirectory || os.tmpdir();
+        const options: cp.SpawnOptions = {
+          cwd: workingDirectory,
+          shell: true,
+        };
+        Object.assign(options, additionalOptions);
+
+        const childProc: cp.ChildProcess = cp.spawn(command, args, options);
+        let timer: NodeJS.Timeout;
+        if (options.timeout && options.timeout > 0) {
+          // timeout only exists for exec not spawn
+          timer = setTimeout(() => {
+            childProc.kill();
+            logger?.debug(
+              `Stop exec due to timeout, command: "${command} ${formattedArgs}", options = '${JSON.stringify(
+                options
+              )}'`
+            );
+            reject(
+              new Error(
+                `Exec command: "${command} ${formattedArgs}" timeout, ${options.timeout} ms`
+              )
+            );
+          }, options.timeout);
+        }
+        logger?.debug(
+          `Running command: "${command} ${formattedArgs}", options = '${JSON.stringify(options)}'`
+        );
+
+        childProc.stdout?.on("data", (data: string | Buffer) => {
+          data = data.toString();
+          cmdOutput = cmdOutput.concat(data);
+          cmdOutputIncludingStderr = cmdOutputIncludingStderr.concat(data);
+        });
+
+        childProc.stderr?.on("data", (data: string | Buffer) => {
+          data = data.toString();
+          cmdOutputIncludingStderr = cmdOutputIncludingStderr.concat(data);
+        });
+
+        childProc.on("error", (error) => {
+          logger?.debug(
+            `Failed to run command '${command} ${formattedArgs}': cmdOutputIncludingStderr: '${cmdOutputIncludingStderr}', error: ${error}`
+          );
+          if (timer) {
+            clearTimeout(timer);
+          }
+          reject(error);
+        });
+        childProc.on("close", (code: number) => {
+          logger?.debug(
+            `Command finished with outputs, cmdOutputIncludingStderr: '${cmdOutputIncludingStderr}'`
+          );
+          if (timer) {
+            clearTimeout(timer);
+          }
+          resolve({
+            code,
+            cmdOutput,
+            cmdOutputIncludingStderr,
+            formattedArgs,
+          });
+        });
+      }
+    );
+  }
+
+  export interface ICommandResult {
+    code: number;
+    cmdOutput: string;
+    cmdOutputIncludingStderr: string;
+    formattedArgs: string;
+  }
+
+  const quotationMark: string = process.platform === "win32" ? '"' : "'";
+
+  /**
+   * Ensures spaces and special characters (most notably $) are preserved
+   */
+  export function wrapArgInQuotes(arg: string): string {
+    return quotationMark + arg + quotationMark;
+  }
+
+  /**
+   * timeout with millisecond
+   */
+  export function withTimeout(millis: number, promise: Promise<any>, msg: string): Promise<any> {
+    return Promise.race([
+      promise,
+      new Promise((resolve, reject) =>
+        setTimeout(() => reject(new Error(`${msg}, ${millis} ms`)), millis)
+      ),
+    ]);
+  }
+}

OfficeDev/teams-toolkit

packages/fx-core/src/common/deps-checker/depsTelemetry.ts
@@ -1,0 +1,18 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { DepsCheckerEvent } from "./constant/telemetry";
+
+export interface DepsTelemetry {
+  sendEvent(
+    eventName: DepsCheckerEvent,
+    properties?: { [p: string]: string },
+    timecost?: number
+  ): void;
+
+  sendEventWithDuration(eventName: DepsCheckerEvent, action: () => Promise<void>): Promise<void>;
+
+  sendUserErrorEvent(eventName: DepsCheckerEvent, errorMessage: string): void;
+
+  sendSystemErrorEvent(eventName: DepsCheckerEvent, errorMessage: string, errorStack: string): void;
+}

packages/fx-core/src/common/deps-checker/internal/ngrokChecker.ts
@@ -1,0 +1,251 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import * as fs from "fs-extra";
+import * as path from "path";
+import * as os from "os";
+import { ConfigFolderName } from "@microsoft/teamsfx-api";
+
+import { ngrokInstallHelpLink } from "../constant/helpLink";
+import { DepsCheckerError } from "../depsError";
+import { runWithProgressIndicator } from "../util/progressIndicator";
+import { cpUtils } from "../util/cpUtils";
+import { isWindows } from "../util/system";
+import { DepsCheckerEvent, TelemtryMessages } from "../constant/telemetry";
+import { DepsLogger } from "../depsLogger";
+import { DepsTelemetry } from "../depsTelemetry";
+import { DepsInfo, DepsChecker } from "../depsChecker";
+import { Messages } from "../constant/message";
+
+const ngrokName = "ngrok";
+
+const installPackageVersion = "4.2.2";
+const supportedPackageVersions = [">=3.4.0"];
+const supportedBinVersions = ["2.3"];
+const displayNgrokName = `${ngrokName}@${installPackageVersion}`;
+
+const timeout = 5 * 60 * 1000;
+
+export class NgrokChecker implements DepsChecker {
+  private readonly _logger: DepsLogger;
+  private readonly _telemetry: DepsTelemetry;
+
+  constructor(logger: DepsLogger, telemetry: DepsTelemetry) {
+    this._logger = logger;
+    this._telemetry = telemetry;
+  }
+
+  public async command(): Promise<string> {
+    throw new Error("Method not implemented.");
+  }
+
+  public getDepsInfo(): Promise<DepsInfo> {
+    return Promise.resolve({
+      name: ngrokName,
+      isLinuxSupported: true,
+      installVersion: installPackageVersion,
+      supportedVersions: supportedPackageVersions,
+      details: new Map<string, string>(),
+    });
+  }
+
+  public async isInstalled(): Promise<boolean> {
+    let isVersionSupported = false,
+      hasSentinel = false;
+    try {
+      const ngrokVersion = await this.queryNgrokBinVersion();
+      isVersionSupported =
+        ngrokVersion !== undefined && supportedBinVersions.includes(ngrokVersion);
+      hasSentinel = await fs.pathExists(this.getSentinelPath());
+    } catch (error) {
+      // do nothing
+      return false;
+    }
+    return isVersionSupported && hasSentinel;
+  }
+
+  public async install(): Promise<void> {
+    if (!(await this.hasNPM())) {
+      this.handleNpmNotFound();
+    }
+
+    await this.cleanup();
+    await this.installNgrok();
+
+    if (!(await this.validate())) {
+      await this.handleInstallNgrokFailed();
+    }
+
+    this._telemetry.sendEvent(DepsCheckerEvent.ngrokInstallCompleted);
+    await this._logger.info(Messages.finishInstallNgrok.replace("@NameVersion", displayNgrokName));
+  }
+
+  public getNgrokBinFolder(): string {
+    return path.join(this.getDefaultInstallPath(), "node_modules", "ngrok", "bin");
+  }
+
+  public async resolve(): Promise<boolean> {
+    try {
+      if (!(await this.isInstalled())) {
+        // TODO: show output in extension
+        // this._adapter.showOutputChannel();
+        await this.install();
+        this._logger.cleanup();
+      }
+    } catch (error) {
+      await this._logger.printDetailLog();
+      this._logger.cleanup();
+      await this._logger.error(`Failed to install 'ngrok', error = '${error}'`);
+      return false;
+    }
+
+    return true;
+  }
+
+  private async handleInstallNgrokFailed(): Promise<void> {
+    await this.cleanup();
+
+    this._telemetry.sendSystemErrorEvent(
+      DepsCheckerEvent.ngrokInstallError,
+      TelemtryMessages.failedToInstallNgrok,
+      Messages.failToValidateNgrok.replace("@NameVersion", displayNgrokName)
+    );
+    throw new DepsCheckerError(
+      Messages.failToInstallNgrok.split("@NameVersion").join(displayNgrokName),
+      ngrokInstallHelpLink
+    );
+  }
+
+  private async validate(): Promise<boolean> {
+    let isVersionSupported = false;
+    let hasSentinel = false;
+    try {
+      const binVersion = await this.queryNgrokBinVersion();
+      isVersionSupported = binVersion !== undefined && supportedBinVersions.includes(binVersion);
+      hasSentinel = await fs.pathExists(this.getSentinelPath());
+    } catch (err) {
+      this._telemetry.sendSystemErrorEvent(
+        DepsCheckerEvent.ngrokValidationError,
+        TelemtryMessages.failedToValidateNgrok,
+        err
+      );
+    }
+
+    if (!isVersionSupported || !hasSentinel) {
+      this._telemetry.sendEvent(DepsCheckerEvent.ngrokValidationError, {
+        "ngrok-v": String(isVersionSupported),
+        sentinel: String(hasSentinel),
+      });
+    }
+    return isVersionSupported && hasSentinel;
+  }
+
+  private handleNpmNotFound() {
+    this._telemetry.sendEvent(DepsCheckerEvent.npmNotFound);
+    throw new DepsCheckerError(
+      Messages.needInstallNgrok.replace("@NameVersion", displayNgrokName),
+      ngrokInstallHelpLink
+    );
+  }
+
+  private getDefaultInstallPath(): string {
+    return path.join(os.homedir(), `.${ConfigFolderName}`, "bin", "ngrok");
+  }
+
+  private getSentinelPath(): string {
+    return path.join(os.homedir(), `.${ConfigFolderName}`, "ngrok-sentinel");
+  }
+
+  private async queryNgrokBinVersion(): Promise<string | undefined> {
+    const output = await cpUtils.executeCommand(
+      undefined,
+      this._logger,
+      {
+        shell: true,
+        env: { PATH: this.getNgrokBinFolder() },
+      },
+      ngrokName,
+      "version"
+    );
+
+    const regex =
+      /ngrok version (?<major_version>\d+)\.(?<minor_version>\d+)\.(?<patch_version>\d+)/gim;
+    const match = regex.exec(output);
+
+    if (!match || !match.groups) {
+      return undefined;
+    }
+
+    return `${match.groups.major_version}.${match.groups.minor_version}`;
+  }
+
+  private async hasNPM(): Promise<boolean> {
+    try {
+      const npmVersion = await cpUtils.executeCommand(
+        undefined,
+        this._logger,
+        { shell: true },
+        "npm",
+        "--version"
+      );
+      this._telemetry.sendEvent(DepsCheckerEvent.npmAlreadyInstalled, {
+        "npm-version": npmVersion,
+      });
+
+      return true;
+    } catch (error) {
+      this._telemetry.sendEvent(DepsCheckerEvent.npmNotFound);
+      return false;
+    }
+  }
+
+  private async cleanup(): Promise<void> {
+    try {
+      await fs.emptyDir(this.getDefaultInstallPath());
+      await fs.remove(this.getSentinelPath());
+    } catch (err) {
+      await this._logger.debug(
+        `Failed to clean up path: ${this.getDefaultInstallPath()}, error: ${err}`
+      );
+    }
+  }
+
+  private async installNgrok(): Promise<void> {
+    await this._telemetry.sendEventWithDuration(
+      DepsCheckerEvent.ngrokInstallScriptCompleted,
+      async () => {
+        await runWithProgressIndicator(async () => await this.doInstallNgrok(), this._logger);
+      }
+    );
+  }
+
+  private async doInstallNgrok(): Promise<void> {
+    await this._logger.info(Messages.startInstallNgrok.replace("@NameVersion", displayNgrokName));
+
+    try {
+      await cpUtils.executeCommand(
+        undefined,
+        this._logger,
+        { timeout: timeout, shell: false },
+        this.getExecCommand("npm"),
+        "install",
+        // not use -f, to avoid npm@6 bug: exit code = 0, even if install fail
+        `${ngrokName}@${installPackageVersion}`,
+        "--prefix",
+        `${this.getDefaultInstallPath()}`
+      );
+
+      await fs.ensureFile(this.getSentinelPath());
+    } catch (error) {
+      this._telemetry.sendSystemErrorEvent(
+        DepsCheckerEvent.ngrokInstallScriptError,
+        TelemtryMessages.failedToInstallNgrok,
+        error
+      );
+    }
+  }
+
+  private getExecCommand(command: string): string {
+    return isWindows() ? `${command}.cmd` : command;
+  }
+}

packages/fx-core/src/common/deps-checker/util/cpUtils.ts
@@ -1,0 +1,146 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import * as cp from "child_process";
+import * as os from "os";
+
+export interface DebugLogger {
+  debug(message: string): Promise<boolean>;
+}
+
+export namespace cpUtils {
+  export async function executeCommand(
+    workingDirectory: string | undefined,
+    logger: DebugLogger | undefined,
+    options: cp.SpawnOptions | undefined,
+    command: string,
+    ...args: string[]
+  ): Promise<string> {
+    const result: ICommandResult = await tryExecuteCommand(
+      workingDirectory,
+      logger,
+      options,
+      command,
+      ...args
+    );
+    if (result.code !== 0) {
+      const errorMessage = `Failed to run command: "${command} ${result.formattedArgs}", code: "${result.code}",
+                            output: "${result.cmdOutput}", error: "${result.cmdOutputIncludingStderr}"`;
+      await logger?.debug(errorMessage);
+      throw new Error(errorMessage);
+    } else {
+      await logger?.debug(`Finished running command: "${command} ${result.formattedArgs}".`);
+    }
+
+    return result.cmdOutput;
+  }
+
+  export async function tryExecuteCommand(
+    workingDirectory: string | undefined,
+    logger: DebugLogger | undefined,
+    additionalOptions: cp.SpawnOptions | undefined,
+    command: string,
+    ...args: string[]
+  ): Promise<ICommandResult> {
+    return await new Promise(
+      (resolve: (res: ICommandResult) => void, reject: (e: Error) => void): void => {
+        let cmdOutput = "";
+        let cmdOutputIncludingStderr = "";
+        const formattedArgs: string = args.join(" ");
+
+        workingDirectory = workingDirectory || os.tmpdir();
+        const options: cp.SpawnOptions = {
+          cwd: workingDirectory,
+          shell: true,
+        };
+        Object.assign(options, additionalOptions);
+
+        const childProc: cp.ChildProcess = cp.spawn(command, args, options);
+        let timer: NodeJS.Timeout;
+        if (options.timeout && options.timeout > 0) {
+          // timeout only exists for exec not spawn
+          timer = setTimeout(() => {
+            childProc.kill();
+            logger?.debug(
+              `Stop exec due to timeout, command: "${command} ${formattedArgs}", options = '${JSON.stringify(
+                options
+              )}'`
+            );
+            reject(
+              new Error(
+                `Exec command: "${command} ${formattedArgs}" timeout, ${options.timeout} ms`
+              )
+            );
+          }, options.timeout);
+        }
+        logger?.debug(
+          `Running command: "${command} ${formattedArgs}", options = '${JSON.stringify(options)}'`
+        );
+
+        childProc.stdout?.on("data", (data: string | Buffer) => {
+          data = data.toString();
+          cmdOutput = cmdOutput.concat(data);
+          cmdOutputIncludingStderr = cmdOutputIncludingStderr.concat(data);
+        });
+
+        childProc.stderr?.on("data", (data: string | Buffer) => {
+          data = data.toString();
+          cmdOutputIncludingStderr = cmdOutputIncludingStderr.concat(data);
+        });
+
+        childProc.on("error", (error) => {
+          logger?.debug(
+            `Failed to run command '${command} ${formattedArgs}': cmdOutputIncludingStderr: '${cmdOutputIncludingStderr}', error: ${error}`
+          );
+          if (timer) {
+            clearTimeout(timer);
+          }
+          reject(error);
+        });
+        childProc.on("close", (code: number) => {
+          logger?.debug(
+            `Command finished with outputs, cmdOutputIncludingStderr: '${cmdOutputIncludingStderr}'`
+          );
+          if (timer) {
+            clearTimeout(timer);
+          }
+          resolve({
+            code,
+            cmdOutput,
+            cmdOutputIncludingStderr,
+            formattedArgs,
+          });
+        });
+      }
+    );
+  }
+
+  export interface ICommandResult {
+    code: number;
+    cmdOutput: string;
+    cmdOutputIncludingStderr: string;
+    formattedArgs: string;
+  }
+
+  const quotationMark: string = process.platform === "win32" ? '"' : "'";
+
+  /**
+   * Ensures spaces and special characters (most notably $) are preserved
+   */
+  export function wrapArgInQuotes(arg: string): string {
+    return quotationMark + arg + quotationMark;
+  }
+
+  /**
+   * timeout with millisecond
+   */
+  export function withTimeout(millis: number, promise: Promise<any>, msg: string): Promise<any> {
+    return Promise.race([
+      promise,
+      new Promise((resolve, reject) =>
+        setTimeout(() => reject(new Error(`${msg}, ${millis} ms`)), millis)
+      ),
+    ]);
+  }
+}
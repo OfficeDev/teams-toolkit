51e03e7054b816a332f9d0ec63ff4eefb0640fe1
Ning Liu <nliu@microsoft.com>
2021-12-24 12:11:40 +0800

    fix: update api

--- packages/fx-core/src/plugins/resource/appstudio/v3/index.ts
+++ packages/fx-core/src/plugins/resource/appstudio/v3/index.ts
@@ -1,30 +1,44 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { Context, Inputs, FxError, Result, ok, err } from "@microsoft/teamsfx-api";
+import {
+  Context,
+  FxError,
+  Result,
+  ok,
+  err,
+  v2,
+  IComposeExtension,
+  IBot,
+  IConfigurableTab,
+  IStaticTab,
+} from "@microsoft/teamsfx-api";
+import { BuiltInResourcePluginNames } from "../../../solution/fx-solution/v3/constants";
+import { Service } from "typedi";
 
+@Service(BuiltInResourcePluginNames.appStudio)
 export class AppStudioPluginV3 {
   // Generate initial manifest template file, for both local debug & remote
-  async init(ctx: Context, inputs: Inputs): Promise<Result<any, FxError>> {
+  async init(ctx: Context, inputs: v2.InputsWithProjectPath): Promise<Result<any, FxError>> {
     return ok(undefined);
   }
 
   // Append to manifest template file
   async addCapabilities(
-    capabilities: ("staticTab" | "configurableTab" | "Bot" | "MessasgeExtension")[]
+    ctx: Context,
+    inputs: v2.InputsWithProjectPath,
+    capabilities: (
+      | { name: "staticTab"; snippet?: IStaticTab }
+      | { name: "configurableTab"; snippet?: IConfigurableTab }
+      | { name: "Bot"; snippet?: IBot }
+      | { name: "MessageExtension"; snippet?: IComposeExtension }
+    )[]
   ): Promise<Result<any, FxError>> {
     capabilities.map((capability) => {
-      if (this.capabilityExceedLimit(capabilities[0])) {
+      if (this.capabilityExceedLimit(capability.name)) {
         return err(new Error("Exeed limit."));
       }
     });
-    return ok(undefined);
-  }
-
-  // Need to add incremental capabilities and append to manifest template
-  async scaffold(
-    capabilities: ("staticTab" | "configurableTab" | "Bot" | "MessasgeExtension")[]
-  ): Promise<Result<any, FxError>> {
     return ok(undefined);
   }
 
@@ -32,7 +46,7 @@ export class AppStudioPluginV3 {
   // The limit of staticTab if 16, others are 1
   // Should check both local & remote manifest template file
   private async capabilityExceedLimit(
-    capability: "staticTab" | "configurableTab" | "Bot" | "MessasgeExtension"
+    capability: "staticTab" | "configurableTab" | "Bot" | "MessageExtension"
   ): Promise<boolean> {
     return false;
   }

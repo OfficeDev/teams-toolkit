8a7e366c13136d3d58d6182b292a62ea2e54029a
pengfeizhao <pengfeizhao@microsoft.com>
2022-01-04 12:55:58 +0800

    chore: add header

--- packages/cli/src/cmds/preview/depsChecker/cliUtils.ts
+++ packages/cli/src/cmds/preview/depsChecker/cliUtils.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import UI from "../../../userInteraction";
 import { CliConfigEnvChecker, UserSettings } from "../../../userSetttings";
 import * as os from "os";

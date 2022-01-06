cb68fcdd406a23b27516e6136b8084e2991aeb9c
huajiezhang <huajiezhang@microsoft.com>
2022-01-06 20:17:37 +0800

    feat(api): test

--- packages/api/src/v3/types.ts
+++ packages/api/src/v3/types.ts
@@ -1,8 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+import { EnvConfig } from "../schemas";
 import { EnvInfoV2 } from "../v2/types";
-import { ResourceStates } from "./resourceStates";
+import { ResourceStates, TeamsFxAzureResourceStates } from "./resourceStates";
 
 /**
  * Upgrade EnvInfoV2, specify the state type as ResourceStates
@@ -10,3 +11,8 @@ import { ResourceStates } from "./resourceStates";
 export interface EnvInfoV3 extends EnvInfoV2 {
   state: ResourceStates;
 }
+
+// export interface TeamsFxAzureEnvInfo extends EnvInfoV3 {
+//   state: TeamsFxAzureResourceStates;
+//   config: EnvConfig;
+// }

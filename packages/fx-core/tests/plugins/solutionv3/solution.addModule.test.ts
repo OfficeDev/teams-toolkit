dda020363ba9411902fd7b6546f69050e8406f74
huajiezhang <huajiezhang@microsoft.com>
2022-01-10 22:51:52 +0800

    fix(solutionv3): ut for addModule and scaffold

--- packages/fx-core/tests/plugins/solutionv3/solution.addModule.test.ts
+++ packages/fx-core/tests/plugins/solutionv3/solution.addModule.test.ts
@@ -1,7 +1,18 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { Platform, ProjectSettings, v2 } from "@microsoft/teamsfx-api";
+import {
+  IConfigurableTab,
+  IStaticTab,
+  ok,
+  Platform,
+  ProjectSettings,
+  v2,
+  FxError,
+  IBot,
+  IComposeExtension,
+  Result,
+} from "@microsoft/teamsfx-api";
 import { assert } from "chai";
 import "mocha";
 import * as uuid from "uuid";
@@ -9,12 +20,45 @@ import {
   addModule,
   getQuestionsForAddModule,
 } from "../../../src/plugins/solution/fx-solution/v3/addModule";
-import { TeamsFxAzureSolutionNameV3 } from "../../../src/plugins/solution/fx-solution/v3/constants";
+import {
+  BuiltInResourcePluginNames,
+  TeamsFxAzureSolutionNameV3,
+} from "../../../src/plugins/solution/fx-solution/v3/constants";
 import { MockedV2Context } from "../solution/util";
 import * as path from "path";
 import * as os from "os";
 import { randomAppName } from "../../core/utils";
+import { Container } from "typedi";
+import { AppStudioPluginV3 } from "../../../src/plugins/resource/appstudio/v3";
+import sinon from "sinon";
 describe("SolutionV3 - addModule", () => {
+  const sandbox = sinon.createSandbox();
+  beforeEach(async () => {
+    const appStudio = Container.get<AppStudioPluginV3>(BuiltInResourcePluginNames.appStudio);
+    sandbox.stub<any, any>(appStudio, "addCapabilities").callsFake(
+      async (
+        ctx: v2.Context,
+        inputs: v2.InputsWithProjectPath,
+        capabilities: (
+          | { name: "staticTab"; snippet?: { local: IStaticTab; remote: IStaticTab } }
+          | {
+              name: "configurableTab";
+              snippet?: { local: IConfigurableTab; remote: IConfigurableTab };
+            }
+          | { name: "Bot"; snippet?: { local: IBot; remote: IBot } }
+          | {
+              name: "MessageExtension";
+              snippet?: { local: IComposeExtension; remote: IComposeExtension };
+            }
+        )[]
+      ): Promise<Result<any, FxError>> => {
+        return ok(undefined);
+      }
+    );
+  });
+  afterEach(async () => {
+    sandbox.restore();
+  });
   it("add tab success", async () => {
     const projectSettings: ProjectSettings = {
       appName: "my app",

ef5400116a5e9e4f2c3ec8945f974f1fc807267b
huajiezhang <huajiezhang@microsoft.com>
2022-01-13 14:57:57 +0800

    fix: export

--- packages/fx-core/src/core/index.ts
+++ packages/fx-core/src/core/index.ts
@@ -121,6 +121,8 @@ import {
   QuestionModelMW,
 } from "./middleware/questionModel";
 import { Logger, setTools, TOOLS } from "./globalVars";
+export * from "./middleware/CoreHookContext";
+
 // TODO: For package.json,
 // use require instead of import because of core building/packaging method.
 // Using import will cause the build folder structure to change.

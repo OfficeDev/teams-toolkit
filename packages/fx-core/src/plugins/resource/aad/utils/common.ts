03839c925a6287c55a0b875b02480e0b35f036d3
huajiezhang <huajiezhang@microsoft.com>
2022-01-14 22:33:44 +0800

    fix: compile error

--- packages/fx-core/src/plugins/resource/aad/utils/common.ts
+++ packages/fx-core/src/plugins/resource/aad/utils/common.ts
@@ -67,13 +67,13 @@ export class Utils {
 
   public static async getCurrentTenantId(graphTokenProvider?: GraphTokenProvider): Promise<string> {
     const tokenObject = await graphTokenProvider?.getJsonObject();
-    const tenantId: string = (tokenObject as any).tid;
+    const tenantId: string = (tokenObject as any)?.tid;
     return tenantId;
   }
 
   public static skipCreateAadForProvision(envInfo: v3.EnvInfoV3): boolean {
     const envConfig: EnvConfig = envInfo.config as EnvConfig;
-    const envState: v3.AADApp = envInfo.state[BuiltInResourcePluginNames.aad] as AADApp;
+    const envState: v3.AADApp = envInfo.state[BuiltInResourcePluginNames.aad] as v3.AADApp;
     const objectId = envConfig.auth?.objectId ? envConfig.auth.objectId : envState.objectId;
     const clientId = envConfig.auth?.clientId ? envConfig.auth.clientId : envState.clientId;
     const clientSecret = envConfig.auth?.clientSecret

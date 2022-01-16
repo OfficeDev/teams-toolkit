29856fa2c06b384f9cbef5df5ac23986108602de
huajiezhang <huajiezhang@microsoft.com>
2022-01-16 14:02:32 +0800

    fix: ut

---
+++ packages/fx-core/tests/plugins/resource/aad/v3/aad.test.ts
@@ -0,0 +1,56 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { assert } from "chai";
+import fs from "fs-extra";
+import "mocha";
+import * as os from "os";
+import * as path from "path";
+import "reflect-metadata";
+import sinon from "sinon";
+import {
+  checkPermissionRequest,
+  createPermissionRequestFile,
+  getPermissionRequest,
+} from "../../../../../src/plugins/resource/aad/v3";
+import { deleteFolder, randomAppName } from "../../../../core/utils";
+describe("AAD resource plugin V3", () => {
+  const sandbox = sinon.createSandbox();
+  beforeEach(async () => {
+    // sandbox
+    //   .stub<any, any>(appStudio, "loadManifest")
+    //   .callsFake(
+    //     async (
+    //       ctx: v2.Context,
+    //       inputs: v2.InputsWithProjectPath
+    //     ): Promise<Result<{ local: TeamsAppManifest; remote: TeamsAppManifest }, FxError>> => {
+    //       return ok({ local: new TeamsAppManifest(), remote: new TeamsAppManifest() });
+    //     }
+    //   );
+    // sandbox
+    //   .stub<any, any>(appStudio, "saveManifest")
+    //   .callsFake(
+    //     async (
+    //       ctx: v2.Context,
+    //       inputs: v2.InputsWithProjectPath,
+    //       manifest: { local: TeamsAppManifest; remote: TeamsAppManifest }
+    //     ): Promise<Result<any, FxError>> => {
+    //       return ok({ local: {}, remote: {} });
+    //     }
+    //   );
+  });
+  afterEach(async () => {
+    sandbox.restore();
+  });
+  it("permission request file", async () => {
+    const projectPath = path.join(os.tmpdir(), randomAppName());
+    await fs.ensureDir(projectPath);
+    const createRes = await createPermissionRequestFile(projectPath);
+    assert.isTrue(createRes.isOk() && createRes.value !== undefined);
+    const checkRes = await checkPermissionRequest(projectPath);
+    assert.isTrue(checkRes.isOk() && createRes.isOk() && checkRes.value === createRes.value);
+    const getRes = await getPermissionRequest(projectPath);
+    assert.isTrue(getRes.isOk() && getRes.value !== undefined);
+    deleteFolder(projectPath);
+  });
+});

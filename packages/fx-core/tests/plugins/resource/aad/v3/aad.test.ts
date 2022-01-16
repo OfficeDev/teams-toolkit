3db302b5818b5ea7e43bd919bb3a23e8e376378f
huajiezhang <huajiezhang@microsoft.com>
2022-01-16 15:10:10 +0800

    fix: ut

--- packages/fx-core/tests/plugins/resource/aad/v3/aad.test.ts
+++ packages/fx-core/tests/plugins/resource/aad/v3/aad.test.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+import { EnvConfig, v2, v3 } from "@microsoft/teamsfx-api";
 import { assert } from "chai";
 import fs from "fs-extra";
 import "mocha";
@@ -8,11 +9,15 @@ import * as os from "os";
 import * as path from "path";
 import "reflect-metadata";
 import sinon from "sinon";
+import { GetSkipAppConfigError } from "../../../../../src/plugins/resource/aad/errors";
+import { Utils } from "../../../../../src/plugins/resource/aad/utils/common";
 import {
   checkPermissionRequest,
   createPermissionRequestFile,
   getPermissionRequest,
 } from "../../../../../src/plugins/resource/aad/v3";
+import { AADAppCheckingError } from "../../../../../src/plugins/resource/bot/errors";
+import { BuiltInResourcePluginNames } from "../../../../../src/plugins/solution/fx-solution/v3/constants";
 import { deleteFolder, randomAppName } from "../../../../core/utils";
 describe("AAD resource plugin V3", () => {
   const sandbox = sinon.createSandbox();
@@ -52,5 +57,114 @@ describe("AAD resource plugin V3", () => {
     const getRes = await getPermissionRequest(projectPath);
     assert.isTrue(getRes.isOk() && getRes.value !== undefined);
     deleteFolder(projectPath);
+  });
+  it("skipCreateAadForProvision skip = true", async () => {
+    const envConfig: EnvConfig = {
+      auth: {
+        objectId: "mockObjectId",
+        clientId: "mockClientId",
+        clientSecret: "mockClientSecret",
+        accessAsUserScopeId: "mockAccessAsUserScopeId",
+      },
+      manifest: {
+        appName: {
+          short: "myApp",
+        },
+      },
+    };
+    const envInfo: v3.EnvInfoV3 = {
+      envName: "dev",
+      config: envConfig,
+      state: {
+        solution: {},
+        [BuiltInResourcePluginNames.aad]: {},
+      },
+    };
+    const skip = await Utils.skipCreateAadForProvision(envInfo);
+    assert.isTrue(skip);
+    const aadResource = envInfo.state[BuiltInResourcePluginNames.aad] as v3.AADApp;
+    assert.isTrue(aadResource.objectId === envConfig.auth?.objectId);
+    assert.isTrue(aadResource.clientId === envConfig.auth?.clientId);
+    assert.isTrue(aadResource.clientSecret === envConfig.auth?.clientSecret);
+    assert.isTrue(aadResource.oauth2PermissionScopeId === envConfig.auth?.accessAsUserScopeId);
+  });
+  it("skipCreateAadForProvision skip = false", async () => {
+    const envConfig: EnvConfig = {
+      auth: {},
+      manifest: {
+        appName: {
+          short: "myApp",
+        },
+      },
+    };
+    const envInfo: v3.EnvInfoV3 = {
+      envName: "dev",
+      config: envConfig,
+      state: {
+        solution: {},
+        [BuiltInResourcePluginNames.aad]: {},
+      },
+    };
+    const skip = await Utils.skipCreateAadForProvision(envInfo);
+    assert.isTrue(skip === false);
+  });
+  it("skipCreateAadForProvision throw error", async () => {
+    const envConfig: EnvConfig = {
+      auth: {},
+      manifest: {
+        appName: {
+          short: "myApp",
+        },
+      },
+    };
+    const envInfo: v3.EnvInfoV3 = {
+      envName: "dev",
+      config: envConfig,
+      state: {
+        solution: {},
+        [BuiltInResourcePluginNames.aad]: {
+          objectId: "mockObjectId",
+        },
+      },
+    };
+    try {
+      await Utils.skipCreateAadForProvision(envInfo);
+    } catch (e) {
+      assert.isTrue(e.name === GetSkipAppConfigError.name);
+    }
+  });
+  it("skipCreateAadForLocalProvision skip = true", async () => {
+    const localSettings: v2.LocalSettings = {
+      teamsApp: {},
+      auth: {
+        objectId: "mockObjectId",
+        clientId: "mockClientId",
+        clientSecret: "mockClientSecret",
+        accessAsUserScopeId: "mockAccessAsUserScopeId",
+      },
+    };
+    const skip = await Utils.skipCreateAadForLocalProvision(localSettings);
+    assert.isTrue(skip);
+  });
+  it("skipCreateAadForLocalProvision skip = false", async () => {
+    const localSettings: v2.LocalSettings = {
+      teamsApp: {},
+      auth: {},
+    };
+    const skip = await Utils.skipCreateAadForLocalProvision(localSettings);
+    assert.isTrue(skip === false);
+  });
+  it("skipCreateAadForProvision throw error", async () => {
+    const localSettings: v2.LocalSettings = {
+      teamsApp: {},
+      auth: {
+        objectId: "mockObjectId",
+      },
+    };
+    try {
+      await Utils.skipCreateAadForLocalProvision(localSettings);
+    } catch (e) {
+      assert.isTrue(e.name === GetSkipAppConfigError.name);
+    }
   });
 });

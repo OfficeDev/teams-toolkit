f105a03d145b0d93208ac20c8fce28979db99469
wenyutang-ms <wenyutang@microsoft.com>
2022-01-18 15:34:19 +0800

    chore: update

--- .github/scripts/fxcore-sync-up-version.js
+++ .github/scripts/fxcore-sync-up-version.js
@@ -4,19 +4,18 @@ const fse = require("fs-extra");
 
 const templatePath = path.join(__dirname, "../../templates")
 const templateVersion = require(path.join(templatePath, "package.json")).version
-const templateName = require(path.join(templatePath, "package.json")).version
 
-console.log(`================== template name: ${templateName}, ============ template version: ${templateVersion}`);
+console.log(`================== template version: ${templateVersion} ==================`);
 
 const fxCorePath = path.join(__dirname, "../../packages/fx-core")
 const templateConfig = path.join(fxCorePath, "./src/common/templates-config.json")
 const templateConfigFile = fse.readJsonSync(templateConfig)
 
-console.log(`================== template version in fx-core configurate as ${templateConfigFile.version}`)
+console.log(`================== template version in fx-core configurate as ${templateConfigFile.version} ==================`)
 
 if(!semver.prerelease(templateVersion)){
     if(!semver.intersects(templateConfigFile.version, templateVersion)){
-        console.log("==================== template config version is not match with template latest release version")
+        console.log("================== template config version is not match with template latest release version, need bump up config version ^${templateVersion} ==================")
 
         templateConfigFile.version = `^${templateVersion}`;
         fse.writeFileSync(templateConfig, JSON.stringify(templateConfigFile, null, 4))

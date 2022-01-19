3bf4b96dccd0a68d0036c7ca542d9f6f1bad0be4
Zhiyu You <zhiyou@microsoft.com>
2022-01-19 16:03:43 +0800

    fix: webpack error

--- packages/server/webpack.config.js
+++ packages/server/webpack.config.js
@@ -84,6 +84,11 @@ const config = {
     new webpack.IgnorePlugin({ resourceRegExp: /@opentelemetry\/tracing/ }),
     new webpack.IgnorePlugin({ resourceRegExp: /applicationinsights-native-metrics/ }),
     new webpack.IgnorePlugin({ resourceRegExp: /original-fs/ }),
+    // ignore node-gyp/bin/node-gyp.js since it's not used in runtime
+    new webpack.NormalModuleReplacementPlugin(
+      /node-gyp[\/\\]bin[\/\\]node-gyp.js/,
+      "@npmcli/node-gyp"
+    ),
   ],
   optimization: {
     minimizer: [

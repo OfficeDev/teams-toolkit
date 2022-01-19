87630ecfb0d09bea193a8f6a1974c59101f697c2
wenyutang-ms <wenyutang@microsoft.com>
2022-01-19 15:21:03 +0800

    test: update test case

--- packages/fx-core/tests/plugins/solution/solution.arm.test.ts
+++ packages/fx-core/tests/plugins/solution/solution.arm.test.ts
@@ -108,34 +108,16 @@ describe("Generate ARM Template for project", () => {
       TestFilePath.armTemplateBaseFolder
     );
     expect(result.isOk()).to.be.true;
+    const mainBicepFilesContent = await fs.readFile(
+      path.join(__dirname, "expectedBiceps", "main.bicep"),
+      fileEncoding
+    );
     expect(
       await fs.readFile(
         path.join(projectArmTemplateFolder, TestFilePath.mainFileName),
         fileEncoding
       )
-    ).equals(
-      `@secure()
-param provisionParameters object
-
-module provision './provision.bicep' = {
-  name: 'provisionResources'
-  params: {
-    provisionParameters: provisionParameters
-  }
-}
-
-module teamsFxConfig './config.bicep' = {
-  name: 'addTeamsFxConfigurations'
-  params: {
-    provisionParameters: provisionParameters
-    provisionOutputs: provision
-  }
-}
-
-output provisionOutput object = provision
-output teamsFxConfigurationOutput object = contains(reference(resourceId('Microsoft.Resources/deployments', teamsFxConfig.name), '2020-06-01'), 'outputs') ? teamsFxConfig : {}
-`
-    );
+    ).equals(mainBicepFilesContent);
 
     expect(
       await fs.readFile(

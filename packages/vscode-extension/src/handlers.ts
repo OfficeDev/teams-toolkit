bf454b97e2997ae0c648994e1c80aa22b1bde01b
Zihong Chen <zihch@microsoft.com>
2022-01-19 19:00:17 +0800

    chore: add comment

--- packages/vscode-extension/src/handlers.ts
+++ packages/vscode-extension/src/handlers.ts
@@ -366,6 +366,8 @@ export async function createNewProjectHandler(args?: any[]): Promise<Result<any,
   const result = await runCommand(Stage.create);
   if (result.isOk()) {
     await ExtTelemetry.dispose();
+    // after calling dispose(), let reder process to while for a while instead of directly call "open folder"
+    // otherwise, the flash operation in dispose() will be interrupted due to shut down the render process.
     setTimeout(() => {
       commands.executeCommand("vscode.openFolder", result.value);
     }, 3000);

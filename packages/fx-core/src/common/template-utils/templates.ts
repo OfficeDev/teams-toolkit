d6f0f08cce781370f46bed27387eeabef4ce4fbc
zhijie <zhihuan@microsoft.com>
2022-01-20 11:36:37 +0800

    chore: special tag for alpha release

--- packages/fx-core/src/common/template-utils/templates.ts
+++ packages/fx-core/src/common/template-utils/templates.ts
@@ -6,10 +6,15 @@ import config from "../templates-config.json";
 
 export const templatesVersion = config.version;
 export const tagPrefix = config.tagPrefix;
+export const alphaReleaseTag = config.alphaReleaseTag;
 export const preRelease = process.env.TEAMSFX_TEMPLATE_PRERELEASE || "";
 export const tagListURL = config.tagListURL;
 
 export function selectTag(tags: string[]): string | undefined {
+  if (preRelease === "alpha") {
+    return alphaReleaseTag;
+  }
+
   const versionPattern = preRelease ? `0.0.0-${preRelease}` : templatesVersion;
   const versionList = tags.map((tag: string) => tag.replace(tagPrefix, ""));
   const selectedVersion = semver.maxSatisfying(versionList, versionPattern);

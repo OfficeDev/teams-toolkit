9062c4036a13a0dcf2e5fb5eaa8f624cbc9adff4
Kuojian Lu <kuojianlu@gmail.com>
2022-01-25 16:22:46 +0800

    feat: run npm install automatically after scaffolding in cli
    - add automatic npm install handler
    - add telemetry event and property for automatic npm install
    - update new command to support automatic npm install

--- packages/cli/src/cmds/config.ts
+++ packages/cli/src/cmds/config.ts
@@ -35,6 +35,8 @@ const GlobalOptions = new Set([
   CliConfigOptions.EnvCheckerValidateNode as string,
   CliConfigOptions.RunFrom as string,
   CliConfigOptions.Interactive as string,
+  // TODO: enable this config
+  // CliConfigOptions.AutomaticNpmInstall as string,
 ]);
 
 export class ConfigGet extends YargsCommand {

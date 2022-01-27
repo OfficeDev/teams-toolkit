f9c4b7ba4351416151b7e2e7fe05b27e8168bcd8
wenyutang-ms <wenyutang@microsoft.com>
2022-01-27 11:16:10 +0800

    chore: format

--- .github/scripts/pkg-version.sh
+++ .github/scripts/pkg-version.sh
@@ -3,29 +3,29 @@ if [ $1 == 'templates' ]; then
     if [[ $SkipSyncup == *"template"* ]]; then
         echo "skip sync up templates version with sdk version"
     elif [[ -z "$(git diff -- ../../templates)" ]]; then
-    echo "need bump up templates version since templates don not bump up by self"
-    node ../../.github/scripts/sdk-sync-up-version.js sdk yes;
+        echo "need bump up templates version since templates don not bump up by self"
+        node ../../.github/scripts/sdk-sync-up-version.js sdk yes;
     else 
-    echo "no need to bump up templates version"
-    node ../../.github/scripts/sdk-sync-up-version.js sdk
+        echo "no need to bump up templates version"
+        node ../../.github/scripts/sdk-sync-up-version.js sdk
     fi
     git add ../../templates
 elif [ $1 == 'fx-core' ]; then
     if [[ -z "$(git diff -- ../fx-core)" ]]; then
-    echo "need bump up fx-core version since fx-core don not bump up by self"
-    node ../../.github/scripts/sync-up-dotnet-ver.js yes;
+        echo "need bump up fx-core version since fx-core don not bump up by self"
+        node ../../.github/scripts/sync-up-dotnet-ver.js yes;
     else 
-    echo "no need to bump up templates version"
-    node ../../.github/scripts/sync-up-dotnet-ver.js
+        echo "no need to bump up templates version"
+        node ../../.github/scripts/sync-up-dotnet-ver.js
     fi
     git add ../fx-core
 elif [ $1 == 'function-extension' ]; then   
     if [[ -z "$(git diff -- ../../templates)" ]]; then
-    echo "need to bump up templates version since templates do not bump up by themselves"
-    node ../../.github/scripts/sync-up-dotnet-ver.js yes
+        echo "need to bump up templates version since templates do not bump up by themselves"
+        node ../../.github/scripts/sync-up-dotnet-ver.js yes
     else 
-    echo "no need to bump up templates version"
-    node ../../.github/scripts/sync-up-dotnet-ver.js
+        echo "no need to bump up templates version"
+        node ../../.github/scripts/sync-up-dotnet-ver.js
     fi
     git add ../../templates
 elif [ $1 == 'core-template' ]; then
@@ -36,11 +36,11 @@ elif [ $1 == 'template-adaptive-card' ]; then
     if [[ $SkipSyncup == *"template"* ]]; then
         echo "skip sync up templates version with adaptive-card version"
     elif [[ -z "$(git diff -- ../../templates)" ]]; then
-    echo "need bump up templates version since templates don not bump up by self"
-    node ../../.github/scripts/sdk-sync-up-version.js adaptivecards-tools-sdk yes;
+        echo "need bump up templates version since templates don not bump up by self"
+        node ../../.github/scripts/sdk-sync-up-version.js adaptivecards-tools-sdk yes;
     else 
-    echo "no need to bump up templates version"
-    node ../../.github/scripts/sdk-sync-up-version.js adaptivecards-tools-sdk
+        echo "no need to bump up templates version"
+        node ../../.github/scripts/sdk-sync-up-version.js adaptivecards-tools-sdk
     fi
     git add ../../templates
 fi

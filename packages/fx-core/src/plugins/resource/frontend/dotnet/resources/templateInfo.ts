3f962c5aa540cf494005fb12bec50c043981a308
JerryYangKai <kaiyan@microsoft.com>
2022-01-27 19:50:27 +0800

    feat(vs): add blazor scaffold, tiny fix after comments

--- packages/fx-core/src/plugins/resource/frontend/dotnet/resources/templateInfo.ts
+++ packages/fx-core/src/plugins/resource/frontend/dotnet/resources/templateInfo.ts
@@ -4,27 +4,27 @@ import { templatesVersion } from "../../../../../common/template-utils/templates
 export type TemplateVariable = { [key: string]: string };
 
 export class Group {
-    static readonly Tab = "tab";
-    static readonly Bot = "bot";
-    static readonly Base = "blazor-base";
+  static readonly Tab = "tab";
+  static readonly Bot = "bot";
+  static readonly Base = "blazor-base";
 }
 
 export class TemplateInfo {
-    group: string;
-    language: string;
-    scenario: string;
-    version: string;
-    variables: TemplateVariable;
+  group: string;
+  language: string;
+  scenario: string;
+  version: string;
+  variables: TemplateVariable;
 
-    constructor(ctx: PluginContext, group: string, templateVariable: { [key: string]: string }) {
-        this.group = group;
-        this.version = TemplateInfo.version;
-        this.language = TemplateInfo.DonetLanguage;
-        this.variables = templateVariable
-        this.scenario = TemplateInfo.DefaultScenario;
-    }
+  constructor(ctx: PluginContext, group: string, templateVariable: TemplateVariable) {
+    this.group = group;
+    this.version = TemplateInfo.version;
+    this.language = TemplateInfo.DonetLanguage;
+    this.variables = templateVariable;
+    this.scenario = TemplateInfo.DefaultScenario;
+  }
 
-    static readonly DonetLanguage = "csharp";
-    static readonly DefaultScenario = "default";
-    static readonly version = templatesVersion;
+  static readonly DonetLanguage = "csharp";
+  static readonly DefaultScenario = "default";
+  static readonly version = templatesVersion;
 }

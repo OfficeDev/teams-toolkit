d5f371f9dd0747409779f2d271fa0fc38261f65c
wenyt <75360946+wenytang-ms@users.noreply.github.com>
2022-01-28 13:24:30 +0800

    fix: bicep config file update (#3808)
    * fix: fix provision update configuration bicep
    
    * feat: update

--- packages/fx-core/tests/plugins/solution/solution.arm.test.ts
+++ packages/fx-core/tests/plugins/solution/solution.arm.test.ts
@@ -483,6 +483,117 @@ Mocked bot configuration orchestration content. Module path: './teamsFx/botConfi
       )
     ).equals(TestFileContent.botConfigurationModule);
   });
+
+  it("add tab capibility to bot to check config bicep update", async () => {
+    // Arrange
+    mockedCtx.projectSettings!.solutionSettings = {
+      hostType: HostTypeOptionAzure.id,
+      name: "azure",
+      activeResourcePlugins: [aadPlugin.name, botPlugin.name, identityPlugin.name],
+      capabilities: [BotOptionItem.id],
+    };
+    TestHelper.mockedFehostGenerateArmTemplates(mocker);
+    TestHelper.mockedAadGenerateArmTemplates(mocker);
+    TestHelper.mockedIdentityGenerateArmTemplates(mocker);
+    TestHelper.mockedBotGenerateArmTemplates(mocker);
+    TestHelper.mockedBotUpdateArmTemplates(mocker);
+    TestHelper.mockedFeHostUpdateArmTemplates(mocker);
+    TestHelper.mockedIdentityUpdateArmTemplates(mocker);
+
+    // Action
+    let result = await generateArmTemplate(mockedCtx, [aadPlugin, botPlugin, identityPlugin]);
+
+    // Assert
+    const projectArmTemplateFolder = path.join(
+      TestHelper.rootDir,
+      TestFilePath.armTemplateBaseFolder
+    );
+    expect(result.isOk()).to.be.true;
+    const projectMainBicep = await fs.readFile(
+      path.join(projectArmTemplateFolder, TestFilePath.mainFileName),
+      fileEncoding
+    );
+    expect(projectMainBicep.replace(/\r?\n/g, os.EOL)).equals(
+      `@secure()
+param provisionParameters object
+
+module provision './provision.bicep' = {
+  name: 'provisionResources'
+  params: {
+    provisionParameters: provisionParameters
+  }
+}
+output provisionOutput object = provision
+module teamsFxConfig './config.bicep' = {
+  name: 'addTeamsFxConfigurations'
+  params: {
+    provisionParameters: provisionParameters
+    provisionOutputs: provision
+  }
+}
+output teamsFxConfigurationOutput object = contains(reference(resourceId('Microsoft.Resources/deployments', teamsFxConfig.name), '2020-06-01'), 'outputs') ? teamsFxConfig : {}
+`.replace(/\r?\n/g, os.EOL)
+    );
+    const projectConfigBicep = await fs.readFile(
+      path.join(projectArmTemplateFolder, TestFilePath.configFileName),
+      fileEncoding
+    );
+    expect(projectConfigBicep.replace(/\r?\n/g, os.EOL)).equals(
+      `@secure()
+param provisionParameters object
+param provisionOutputs object
+Mocked bot configuration orchestration content. Module path: './teamsFx/botConfig.bicep'.`.replace(
+        /\r?\n/g,
+        os.EOL
+      )
+    );
+    let botConfigContent = await fs.readFile(
+      path.join(
+        projectArmTemplateFolder,
+        TestFilePath.configurationFolder,
+        TestFilePath.botConfigFileName
+      ),
+      fileEncoding
+    );
+    expect(botConfigContent.replace(/\r?\n/g, os.EOL)).equals(
+      TestFileContent.botConfigurationModule
+    );
+
+    // Add bot capability
+    (
+      mockedCtx.projectSettings!.solutionSettings as AzureSolutionSettings
+    ).activeResourcePlugins.push(fehostPlugin.name);
+    (mockedCtx.projectSettings!.solutionSettings as AzureSolutionSettings).capabilities.push(
+      TabOptionItem.id
+    );
+    result = await generateArmTemplate(mockedCtx, [fehostPlugin]);
+    expect(result.isOk()).to.be.true;
+    const projectMainBicepWithTab = await fs.readFile(
+      path.join(projectArmTemplateFolder, TestFilePath.mainFileName),
+      fileEncoding
+    );
+    expect(projectMainBicep.replace(/\r?\n/g, os.EOL)).equals(
+      projectMainBicepWithTab.replace(/\r?\n/g, os.EOL)
+    );
+    const projectConfigBicepWithTab = await fs.readFile(
+      path.join(projectArmTemplateFolder, TestFilePath.configFileName),
+      fileEncoding
+    );
+    expect(projectConfigBicepWithTab.replace(/\r?\n/g, os.EOL)).equals(
+      projectConfigBicep.replace(/\r?\n/g, os.EOL)
+    );
+    botConfigContent = await fs.readFile(
+      path.join(
+        projectArmTemplateFolder,
+        TestFilePath.configurationFolder,
+        TestFilePath.botConfigFileName
+      ),
+      fileEncoding
+    );
+    expect(botConfigContent.replace(/\r?\n/g, os.EOL)).equals(
+      TestFileContent.botConfigUpdateModule
+    );
+  });
 });
 
 describe("Deploy ARM Template to Azure", () => {

76ad66592c6a6f050b5b3ab48e4455fdb5ea93fa
huajiezhang <huajiezhang@microsoft.com>
2022-01-28 11:05:35 +0800

    feat: api fix

--- packages/fx-core/src/plugins/resource/bot/v2/index.ts
+++ packages/fx-core/src/plugins/resource/bot/v2/index.ts
@@ -73,18 +73,18 @@ export class BotPluginV2 implements ResourcePlugin {
   async provisionResource(
     ctx: Context,
     inputs: ProvisionInputs,
-    envInfo: Readonly<v2.EnvInfoV2>,
+    envInfo: v2.EnvInfoV2,
     tokenProvider: TokenProvider
-  ): Promise<Result<Json, FxError>> {
+  ): Promise<Result<Void, FxError>> {
     return await provisionResourceAdapter(ctx, inputs, envInfo, tokenProvider, this.plugin);
   }
 
   async configureResource(
     ctx: Context,
     inputs: ProvisionInputs,
-    envInfo: Readonly<v2.EnvInfoV2>,
+    envInfo: v2.EnvInfoV2,
     tokenProvider: TokenProvider
-  ): Promise<Result<Json, FxError>> {
+  ): Promise<Result<Void, FxError>> {
     return await configureResourceAdapter(ctx, inputs, envInfo, tokenProvider, this.plugin);
   }
   async provisionLocalResource(
--- packages/fx-core/src/plugins/resource/sql/v2/index.ts
+++ packages/fx-core/src/plugins/resource/sql/v2/index.ts
@@ -11,6 +11,7 @@ import {
   Result,
   TokenProvider,
   v2,
+  Void,
 } from "@microsoft/teamsfx-api";
 import {
   Context,
@@ -48,18 +49,18 @@ export class SqlPluginV2 implements ResourcePlugin {
   async provisionResource(
     ctx: Context,
     inputs: ProvisionInputs,
-    envInfo: Readonly<v2.EnvInfoV2>,
+    envInfo: v2.EnvInfoV2,
     tokenProvider: TokenProvider
-  ): Promise<Result<Json, FxError>> {
+  ): Promise<Result<Void, FxError>> {
     return await provisionResourceAdapter(ctx, inputs, envInfo, tokenProvider, this.plugin);
   }
 
   async configureResource(
     ctx: Context,
     inputs: ProvisionInputs,
-    envInfo: Readonly<v2.EnvInfoV2>,
+    envInfo: v2.EnvInfoV2,
     tokenProvider: TokenProvider
-  ): Promise<Result<Json, FxError>> {
+  ): Promise<Result<Void, FxError>> {
     return await configureResourceAdapter(ctx, inputs, envInfo, tokenProvider, this.plugin);
   }
 

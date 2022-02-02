8e3e070b9a0b9616dd204058253484eea31f6bbb
huajiezhang <huajiezhang@microsoft.com>
2022-02-02 15:34:58 +0800

    feat: part

--- packages/fx-core/src/plugins/resource/function/v3/index.ts
+++ packages/fx-core/src/plugins/resource/function/v3/index.ts
@@ -1,6 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+import { WebSiteManagementClient } from "@azure/arm-appservice";
+import { Site, StringDictionary } from "@azure/arm-appservice/esm/models";
+import { hooks } from "@feathersjs/hooks/lib";
 import {
   AzureAccountProvider,
   AzureSolutionSettings,
@@ -19,7 +22,13 @@ import * as path from "path";
 import { Service } from "typedi";
 import { ArmTemplateResult } from "../../../../common/armInterface";
 import { Bicep } from "../../../../common/constants";
-import { generateBicepFromFile } from "../../../../common/tools";
+import {
+  generateBicepFromFile,
+  getResourceGroupNameFromResourceId,
+  getSiteNameFromResourceId,
+  getSubscriptionIdFromResourceId,
+} from "../../../../common/tools";
+import { CommonErrorHandlerMW } from "../../../../core/middleware/CommonErrorHandlerMW";
 import { getTemplatesFolder } from "../../../../folder";
 import { AzureResourceFunction } from "../../../solution/fx-solution/question";
 import { BuiltInFeaturePluginNames } from "../../../solution/fx-solution/v3/constants";
@@ -31,11 +40,16 @@ import {
   FunctionPluginPathInfo,
   RegularExpr,
 } from "../constants";
-import { FunctionConfigKey, FunctionLanguage, QuestionKey } from "../enums";
+import { FunctionConfigKey, FunctionLanguage, NodeVersion, QuestionKey } from "../enums";
+import { FunctionProvision } from "../ops/provision";
 import { FunctionScaffold } from "../ops/scaffold";
 import { FunctionConfig } from "../plugin";
 import { functionNameQuestion } from "../question";
+import { runWithErrorCatchAndThrow } from "../resources/errors";
 import { ErrorMessages } from "../resources/message";
+import { PostProvisionSteps, StepHelperFactory } from "../resources/steps";
+import { AzureClientFactory, AzureLib } from "../utils/azure-client";
+import { getNodeVersion } from "../utils/node-version";
 import { FetchConfigError, FunctionNameConflictError, ValidationError } from "./error";
 
 @Service(BuiltInFeaturePluginNames.function)
@@ -77,7 +91,7 @@ export class FunctionPluginV3 implements v3.FeaturePlugin {
     return ok(new QTreeNode(functionNameQuestion));
   }
   private async syncConfigFromContext(
-    ctx: v3.ContextWithManifestProvider,
+    ctx: v2.Context,
     inputs: v2.InputsWithProjectPath,
     envInfo?: v3.EnvInfoV3
   ): Promise<void> {
@@ -164,6 +178,7 @@ export class FunctionPluginV3 implements v3.FeaturePlugin {
     }
     throw new FetchConfigError(key);
   }
+  @hooks([CommonErrorHandlerMW({ telemetry: { component: BuiltInFeaturePluginNames.function } })])
   async scaffold(
     ctx: v3.ContextWithManifestProvider,
     inputs: v2.InputsWithProjectPath
@@ -202,6 +217,7 @@ export class FunctionPluginV3 implements v3.FeaturePlugin {
     }
     return ok(Void);
   }
+  @hooks([CommonErrorHandlerMW({ telemetry: { component: BuiltInFeaturePluginNames.function } })])
   async generateResourceTemplate(
     ctx: v3.ContextWithManifestProvider,
     inputs: v2.InputsWithProjectPath
@@ -254,7 +270,7 @@ export class FunctionPluginV3 implements v3.FeaturePlugin {
     };
     return ok({ kind: "bicep", template: result });
   }
-
+  @hooks([CommonErrorHandlerMW({ telemetry: { component: BuiltInFeaturePluginNames.function } })])
   async afterOtherFeaturesAdded(
     ctx: v3.ContextWithManifestProvider,
     inputs: v3.OtherFeaturesAddedInputs,
@@ -288,7 +304,7 @@ export class FunctionPluginV3 implements v3.FeaturePlugin {
     };
     return ok({ kind: "bicep", template: result });
   }
-
+  @hooks([CommonErrorHandlerMW({ telemetry: { component: BuiltInFeaturePluginNames.function } })])
   async addFeature(
     ctx: v3.ContextWithManifestProvider,
     inputs: v2.InputsWithProjectPath
@@ -305,15 +321,135 @@ export class FunctionPluginV3 implements v3.FeaturePlugin {
     if (!activeResourcePlugins.includes(this.name)) activeResourcePlugins.push(this.name);
     return ok(armRes.value);
   }
+  private getFunctionAppName(): string {
+    return getSiteNameFromResourceId(
+      this.checkAndGet(this.config.functionAppResourceId, FunctionConfigKey.functionAppResourceId)
+    );
+  }
+  private getFunctionAppResourceGroupName(): string {
+    return getResourceGroupNameFromResourceId(
+      this.checkAndGet(this.config.functionAppResourceId, FunctionConfigKey.functionAppResourceId)
+    );
+  }
 
+  private getFunctionAppSubscriptionId(): string {
+    return getSubscriptionIdFromResourceId(
+      this.checkAndGet(this.config.functionAppResourceId, FunctionConfigKey.functionAppResourceId)
+    );
+  }
+  private async getValidNodeVersion(projectPath: string): Promise<NodeVersion> {
+    const currentNodeVersion = await getNodeVersion(this.getFunctionProjectRootPath(projectPath));
+    const candidateNodeVersions = Object.values(NodeVersion);
+    return (
+      candidateNodeVersions.find((v: NodeVersion) => v === currentNodeVersion) ??
+      DefaultValues.nodeVersion
+    );
+  }
+  private async getSite(
+    projectPath: string,
+    client: WebSiteManagementClient,
+    resourceGroupName: string,
+    functionAppName: string
+  ): Promise<Site> {
+    const site = await AzureLib.findFunctionApp(client, resourceGroupName, functionAppName);
+    if (!site) {
+      throw new FindAppError();
+    } else {
+      const nodeVersion = await this.getValidNodeVersion(projectPath);
+      FunctionProvision.pushAppSettings(site, "WEBSITE_NODE_DEFAULT_VERSION", "~" + nodeVersion);
+      return site;
+    }
+  }
+  private collectFunctionAppSettings(envInfo: v3.EnvInfoV3, site: Site): void {
+    const functionEndpoint: string = this.checkAndGet(
+      this.config.functionEndpoint,
+      FunctionConfigKey.functionEndpoint
+    );
+
+    const apimConfig = envInfo.state[BuiltInFeaturePluginNames.apim] as v3.APIM;
+    if (this.isPluginEnabled(ctx, DependentPluginInfo.apimPluginName) && apimConfig) {
+      Logger.info(InfoMessages.dependPluginDetected(DependentPluginInfo.apimPluginName));
+
+      const clientId: string = this.checkAndGet(
+        apimConfig.get(DependentPluginInfo.apimAppId) as string,
+        "APIM app Id"
+      );
+
+      FunctionProvision.ensureFunctionAllowAppIds(site, [clientId]);
+    }
+  }
+  @hooks([CommonErrorHandlerMW({ telemetry: { component: BuiltInFeaturePluginNames.function } })])
   async configureResource(
     ctx: v2.Context,
     inputs: v2.InputsWithProjectPath,
     envInfo: v3.EnvInfoV3,
     tokenProvider: TokenProvider
   ): Promise<Result<Void, FxError>> {
+    await StepHelperFactory.postProvisionStepHelper.start(
+      Object.entries(PostProvisionSteps).length,
+      ctx.userInteraction
+    );
+    await this.syncConfigFromContext(ctx, inputs, envInfo);
+
+    const functionAppName = this.getFunctionAppName();
+    const resourceGroupName = this.getFunctionAppResourceGroupName();
+    const subscriptionId = this.getFunctionAppSubscriptionId();
+    const credential = this.checkAndGet(
+      await tokenProvider.azureAccountProvider.getAccountCredentialAsync(),
+      FunctionConfigKey.credential
+    );
+
+    const webSiteManagementClient: WebSiteManagementClient = await runWithErrorCatchAndThrow(
+      new InitAzureSDKError(),
+      () => AzureClientFactory.getWebSiteManagementClient(credential, subscriptionId)
+    );
+
+    const site = await this.getSite(
+      inputs.projectPath,
+      webSiteManagementClient,
+      resourceGroupName,
+      functionAppName
+    );
+
+    // We must query app settings from azure here, for two reasons:
+    // 1. The site object returned by SDK may not contain app settings.
+    // 2. Azure automatically added some app settings during creation.
+    const res: StringDictionary = await runWithErrorCatchAndThrow(
+      new ConfigFunctionAppError(),
+      async () =>
+        await webSiteManagementClient.webApps.listApplicationSettings(
+          resourceGroupName,
+          functionAppName
+        )
+    );
+
+    if (res.properties) {
+      Object.entries(res.properties).forEach((kv: [string, string]) => {
+        // The site have some settings added in provision step,
+        // which should not be overwritten by queried settings.
+        FunctionProvision.pushAppSettings(site, kv[0], kv[1], false);
+      });
+    }
+
+    this.collectFunctionAppSettings(ctx, site);
+
+    await runWithErrorCatchAndThrow(
+      new ConfigFunctionAppError(),
+      async () =>
+        await step(
+          StepGroup.PostProvisionStepGroup,
+          PostProvisionSteps.updateFunctionSettings,
+          async () =>
+            await webSiteManagementClient.webApps.update(resourceGroupName, functionAppName, site)
+        )
+    );
+    Logger.info(InfoMessages.functionAppSettingsUpdated);
+
+    this.syncConfigToContext(ctx);
+    await StepHelperFactory.postProvisionStepHelper.end(true);
     return ok(Void);
   }
+  @hooks([CommonErrorHandlerMW({ telemetry: { component: BuiltInFeaturePluginNames.function } })])
   async deploy(
     ctx: v2.Context,
     inputs: v2.InputsWithProjectPath,

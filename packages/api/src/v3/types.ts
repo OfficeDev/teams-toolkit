971a8ab9541a5a0b818204a7a00922de3b86d036
Ning Liu <71362691+nliu-ms@users.noreply.github.com>
2022-02-08 15:44:28 +0800

    refactor: existing app manifest template (#3861)
    * refactor: existing app manifest template

--- packages/api/src/v3/types.ts
+++ packages/api/src/v3/types.ts
@@ -1,9 +1,33 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+import { IStaticTab, IConfigurableTab, IBot, IComposeExtension } from "../manifest";
 import { EnvInfoV2 } from "../v2/types";
 import { ResourceStates } from "./resourceStates";
 
 export interface EnvInfoV3 extends EnvInfoV2 {
   state: ResourceStates;
 }
+
+// TODO: consolidate local and remote manifest,
+export type ManifestCapability =
+  | {
+      name: "staticTab";
+      snippet?: { local: IStaticTab; remote: IStaticTab };
+      existingApp?: boolean;
+    }
+  | {
+      name: "configurableTab";
+      snippet?: { local: IConfigurableTab; remote: IConfigurableTab };
+      existingApp?: boolean;
+    }
+  | {
+      name: "Bot";
+      snippet?: { local: IBot; remote: IBot };
+      existingApp?: boolean;
+    }
+  | {
+      name: "MessageExtension";
+      snippet?: { local: IComposeExtension; remote: IComposeExtension };
+      existingApp?: boolean;
+    };

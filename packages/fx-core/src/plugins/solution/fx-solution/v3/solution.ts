a07554744331692042147a0e7525b00e93a2f39c
huajiezhang <huajiezhang@microsoft.com>
2022-02-08 15:12:27 +0800

    feat: add capability & add resource v3

--- packages/fx-core/src/plugins/solution/fx-solution/v3/solution.ts
+++ packages/fx-core/src/plugins/solution/fx-solution/v3/solution.ts
@@ -1,31 +1,68 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { v3 } from "@microsoft/teamsfx-api";
+import { hooks } from "@feathersjs/hooks/lib";
+import {
+  err,
+  Func,
+  FxError,
+  Inputs,
+  NotImplementedError,
+  Result,
+  TokenProvider,
+  v2,
+  v3,
+} from "@microsoft/teamsfx-api";
 import { Service } from "typedi";
+import { CommonErrorHandlerMW } from "../../../../core/middleware/CommonErrorHandlerMW";
 import { addFeature, getQuestionsForAddFeature } from "./addFeature";
-import { TeamsFxAzureSolutionNameV3 } from "./constants";
+import { BuiltInSolutionNames, TeamsFxAzureSolutionNameV3 } from "./constants";
 import { deploy, getQuestionsForDeploy } from "./deploy";
 import { getQuestionsForProvision, provisionResources } from "./provision";
 import { getQuestionsForPublish, publishApplication } from "./publish";
-import { executeUserTask, getQuestionsForUserTask } from "./userTask";
+import { addCapability, addResource, executeUserTask, getQuestionsForUserTask } from "./userTask";
 
 @Service(TeamsFxAzureSolutionNameV3)
 export class TeamsFxAzureSolution implements v3.ISolution {
   name = TeamsFxAzureSolutionNameV3;
 
   getQuestionsForAddFeature = getQuestionsForAddFeature;
+  @hooks([CommonErrorHandlerMW({ telemetry: { component: BuiltInSolutionNames.azure } })])
   addFeature = addFeature;
 
   getQuestionsForProvision = getQuestionsForProvision;
+  @hooks([CommonErrorHandlerMW({ telemetry: { component: BuiltInSolutionNames.azure } })])
   provisionResources = provisionResources;
 
   getQuestionsForDeploy = getQuestionsForDeploy;
+  @hooks([CommonErrorHandlerMW({ telemetry: { component: BuiltInSolutionNames.azure } })])
   deploy = deploy;
 
   getQuestionsForPublish = getQuestionsForPublish;
+  @hooks([CommonErrorHandlerMW({ telemetry: { component: BuiltInSolutionNames.azure } })])
   publishApplication = publishApplication;
 
   getQuestionsForUserTask = getQuestionsForUserTask;
-  executeUserTask = executeUserTask;
+  async executeUserTask(
+    ctx: v2.Context,
+    inputs: Inputs,
+    func: Func,
+    envInfo: v3.EnvInfoV3,
+    tokenProvider: TokenProvider
+  ): Promise<Result<unknown, FxError>> {
+    const method = func.method;
+    if (method === "addCapability") {
+      return this.addCapability(ctx, inputs);
+    }
+    if (method === "addResource") {
+      return this.addResource(ctx, inputs);
+    }
+    return err(new NotImplementedError("Solution", "executeUserTask"));
+  }
+
+  @hooks([CommonErrorHandlerMW({ telemetry: { component: BuiltInSolutionNames.azure } })])
+  addCapability = addCapability;
+
+  @hooks([CommonErrorHandlerMW({ telemetry: { component: BuiltInSolutionNames.azure } })])
+  addResource = addResource;
 }

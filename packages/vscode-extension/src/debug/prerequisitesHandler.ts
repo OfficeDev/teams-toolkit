11cf1eaea7f502cb45a8ac94c8f57d703c2e3ec4
pengfeizhao <pengfeizhao@microsoft.com>
2022-02-08 18:11:57 +0800

    chore: improve doctor ui

--- packages/vscode-extension/src/debug/prerequisitesHandler.ts
+++ packages/vscode-extension/src/debug/prerequisitesHandler.ts
@@ -81,7 +81,7 @@ export async function checkAndInstall(): Promise<Result<any, FxError>> {
     const projectSettings = await localEnvManager.getProjectSettings(workspacePath);
 
     VsCodeLogInstance.outputChannel.show();
-    VsCodeLogInstance.info(doctorConstant.Check);
+    VsCodeLogInstance.outputChannel.appendLine(doctorConstant.Check);
     VsCodeLogInstance.outputChannel.appendLine("");
 
     // node

ab90a1765491d666e3ac85ae3e07f90af9f85431
huajiezhang <huajiezhang@microsoft.com>
2022-02-08 13:01:50 +0800

    feat: ut

--- packages/fx-core/tests/plugins/solutionv3/solution.addFeature.test.ts
+++ packages/fx-core/tests/plugins/solutionv3/solution.addFeature.test.ts
@@ -148,4 +148,37 @@ describe("SolutionV3 - addFeature", () => {
     const res = await getQuestionsForAddFeature(ctx, inputs);
     assert.isTrue(res.isOk());
   });
+  it("addFeature: keyvault", async () => {
+    const projectSettings: ProjectSettings = {
+      appName: "my app",
+      projectId: uuid.v4(),
+      solutionSettings: {
+        name: TeamsFxAzureSolutionNameV3,
+        version: "3.0.0",
+        capabilities: [],
+        hostType: "Azure",
+        azureResources: [],
+        activeResourcePlugins: [],
+      },
+    };
+    const projectPath = path.join(os.tmpdir(), randomAppName());
+    const ctx = new MockedV2Context(projectSettings);
+    const inputs: v3.SolutionAddFeatureInputs = {
+      platform: Platform.VSCode,
+      projectPath: projectPath,
+      module: 0,
+      feature: BuiltInFeaturePluginNames.keyVault,
+    };
+    const res = await addFeature(ctx, inputs);
+    assert.isTrue(res.isOk());
+    assert.deepEqual(projectSettings.solutionSettings, {
+      name: TeamsFxAzureSolutionNameV3,
+      version: "3.0.0",
+      capabilities: [],
+      hostType: "Azure",
+      azureResources: [],
+      activeResourcePlugins: [BuiltInFeaturePluginNames.keyVault],
+    });
+    deleteFolder(projectPath);
+  });
 });

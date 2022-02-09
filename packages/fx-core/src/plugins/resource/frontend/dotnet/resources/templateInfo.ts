2fbb440704f5fc93f1ee46b3cb5350686d773f6d
JerryYangKai <kaiyan@microsoft.com>
2022-02-09 15:35:12 +0800

    feat(vs): add blazor scaffold, change structure after comments 1

--- packages/fx-core/src/plugins/resource/frontend/dotnet/resources/templateInfo.ts
+++ packages/fx-core/src/plugins/resource/frontend/dotnet/resources/templateInfo.ts
@@ -1,13 +1,8 @@
 import { PluginContext } from "@microsoft/teamsfx-api";
+import { Capability } from "./../constants";
 import { templatesVersion } from "../../../../../common/template-utils/templates";
 
 export type TemplateVariable = { [key: string]: string };
-
-export class Group {
-  static readonly Tab = "tab";
-  static readonly Bot = "bot";
-  static readonly Base = "blazor-base";
-}
 
 export class TemplateInfo {
   group: string;
@@ -24,7 +19,33 @@ export class TemplateInfo {
     this.scenario = TemplateInfo.DefaultScenario;
   }
 
+  static readonly SupportCapability = [Capability.tab, Capability.bot];
   static readonly DonetLanguage = "csharp";
   static readonly DefaultScenario = "default";
+  static readonly BaseGroup = "blazor-base";
   static readonly version = templatesVersion;
+}
+
+export function generateTemplateInfos(selectedCapabilities: string[], ctx: PluginContext) {
+  const projectName = ctx.projectSettings!.appName;
+  const templateVariable: TemplateVariable = { BlazorAppServer: projectName };
+  const templateInfoList: TemplateInfo[] = [];
+
+  selectedCapabilities.forEach((capability) => {
+    if (capability in TemplateInfo.SupportCapability) {
+      templateInfoList.push(new TemplateInfo(ctx, capability.toLowerCase(), templateVariable));
+    }
+  });
+
+  // Generate templateInfo for base scenrio.
+  if (selectedCapabilities.includes(Capability.tab)) {
+    templateVariable.IS_TAB = "true";
+  }
+  if (selectedCapabilities.includes(Capability.bot)) {
+    templateVariable.IS_BOT = "true";
+  }
+  const baseTemplateInfo = new TemplateInfo(ctx, TemplateInfo.BaseGroup, templateVariable);
+  templateInfoList.push(baseTemplateInfo);
+
+  return templateInfoList;
 }

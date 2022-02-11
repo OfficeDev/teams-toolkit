9ed2a8ed226c71abb1611c322a797f411620c2c2
Qianhao Dong <qidon@microsoft.com>
2022-02-11 11:27:27 +0800

    fix: stop local debug if no sideloading permission (#3885)
    * fix: stop f5 if no sideloading permission
    
    * chore: wording
    
    * chore: remove unused value

--- packages/vscode-extension/src/debug/prerequisitesHandler.ts
+++ packages/vscode-extension/src/debug/prerequisitesHandler.ts
@@ -22,6 +22,7 @@ import {
   DepsType,
   EmptyLogger,
   FolderName,
+  getSideloadingStatus,
   installExtension,
   LocalEnvManager,
   NodeNotFoundError,
@@ -244,6 +245,17 @@ async function checkM365Account(): Promise<CheckResult> {
         ExtensionSource,
         ExtensionErrors.PrerequisitesValidationError
       );
+    } else {
+      const isSideloadingEnabled = await getSideloadingStatus(token);
+      if (isSideloadingEnabled === false) {
+        // sideloading disabled
+        result = ResultStatus.failed;
+        error = new UserError(
+          ExtensionErrors.PrerequisitesValidationError,
+          StringResources.vsc.accountTree.sideloadingWarningTooltip,
+          ExtensionSource
+        );
+      }
     }
     const tokenObject = (await AppStudioTokenInstance.getStatus())?.accountInfo;
     if (tokenObject && tokenObject.upn) {
@@ -562,12 +574,7 @@ async function handleCheckResults(results: CheckResult[]): Promise<void> {
 
 function outputCheckResultError(result: CheckResult, output: vscode.OutputChannel) {
   if (result.error) {
-    let message: string = result.error.message;
-    if (result.checker === "M365 Account" && message.startsWith("User Cancel")) {
-      message = doctorConstant.SignInCancelled;
-    }
-
-    output.appendLine(`${doctorConstant.WhiteSpace}${message}`);
+    output.appendLine(`${doctorConstant.WhiteSpace}${result.error.message}`);
 
     if (result.error instanceof UserError) {
       const userError = result.error as UserError;

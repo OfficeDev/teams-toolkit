eb6c622ffb272624e4185e37ef87868acf0f73e9
Xiaofu Huang <xiaofhua@microsoft.com>
2022-02-15 15:50:47 +0800

    chore(localdebug): change prerequisite log / progress message (#3947)

--- packages/vscode-extension/src/debug/depsChecker/doctorConstant.ts
+++ packages/vscode-extension/src/debug/depsChecker/doctorConstant.ts
@@ -11,7 +11,7 @@ export const doctorConstant = {
   WhiteSpace: "   ",
   Check:
     "Teams Toolkit is checking if all required prerequisites are installed and will install them if not, if you wish to bypass checking and installing any prerequisistes, you can disable them in Visual Studio Code settings. A summary will be generated for your reference.",
-  CheckNumber: "We are checking total {@number} of prerequisistes for you.",
+  CheckNumber: "We are checking total {@number} of prerequisites for you.",
   Summary: "Prerequisites Check Summary:",
   RestartVSCode:
     "Restart all your Visual Studio Code instances after the installation is finished.",
@@ -19,13 +19,13 @@ export const doctorConstant = {
   NodeNotFound: `Cannot find Node.js. ${InstallNode}`,
   NodeNotSupported: `Node.js (@CurrentVersion) is not in the supported version list (@SupportedVersions). ${InstallNode}`,
   NodeSuccess: `Supported Node.js version (@Version) is installed`,
-  SignInSuccess: `M365 Account (@account) is logged in and sidelaoding enabled`,
+  SignInSuccess: `M365 Account (@account) is logged in and sideloading enabled`,
   Cert: "Development certificate for localhost",
-  CertSuccess: "Devlopment certification for localhost is installed",
+  CertSuccess: "Development certification for localhost is installed",
   NpmInstallSuccess: "NPM Install for @app is executed",
-  NpmInstallFailue: "NPM Install for @app",
+  NpmInstallFailure: "NPM Install for @app",
   LaunchServices:
-    "Services will be launched locally, please check your terminal window for deatils.",
+    "Services will be launched locally, please check your terminal window for details.",
   HelpLink: `Please refer to @Link for more information.`,
   LearnMore: `Visit @Link to learn more about prerequisites check.`,
 };

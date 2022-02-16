fedb0aca807f0321f293bf76707ba408b9ac9afe
Zhijie Huang <zhihuan@microsoft.com>
2022-02-16 13:18:42 +0800

    chore: revert #3933 (#3957)

--- .github/scripts/template-zip-autogen.sh
+++ .github/scripts/template-zip-autogen.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -x
 
-if [ $1 = "" ]; then
-    echo "Must input a path for templates folder"
-    exit -1
-fi
-
 TEMPLATE_TEAMSBOT_FILE_PREFIX=./templates/mustache-templates/teamsBot
 LANGUAGE_LIST=(js ts csharp)
 
@@ -21,9 +16,6 @@ TEMPLATE_LIST=(
 
 # Copy bot code to msgext-bot, except readme and images
 rsync -az --recursive --exclude "*.md" --exclude "*images/*" ./templates/bot/ ./templates/bot-msgext/
-
-# Create temporary templates folder if it does not exist
-mkdir -p $1
 
 for LANGUAGE in ${LANGUAGE_LIST[@]}; do
     for TEMPLATE in ${TEMPLATE_LIST[@]}; do
@@ -44,10 +36,10 @@ for LANGUAGE in ${LANGUAGE_LIST[@]}; do
         if [ ! -d ./templates/${SCOPE}/${LANGUAGE}/${SCENARIO} ]; then
             echo "The folder ./templates/${SCOPE}/${LANGUAGE}/${SCENARIO} does not exist."
             continue
-        fi
-
+        fi        
+        
         # Generate code from Mustache templates for js and ts
-        if [ ${SCOPE} == "bot" ] && [ ${LANGUAGE} != "csharp" ]; then
+        if [ ${SCOPE} == "bot" ] && [ ${LANGUAGE} != "csharp" ]; then           
             IS_BOT=true mo ${TEMPLATE_TEAMSBOT_FILE_PREFIX}.${LANGUAGE}.mustache > ./templates/${SCOPE}/${LANGUAGE}/${SCENARIO}/teamsBot.${LANGUAGE}
         fi
         if [ ${SCOPE} == "msgext" ] && [ ${LANGUAGE} != "csharp" ]; then
@@ -58,7 +50,7 @@ for LANGUAGE in ${LANGUAGE_LIST[@]}; do
         fi
 
         cd ./templates/${SCOPE}/${LANGUAGE}/${SCENARIO}
-        zip -rq $1/${SCOPE}.${LANGUAGE}.${SCENARIO}.zip .
+        zip -rq ../../../../${SCOPE}.${LANGUAGE}.${SCENARIO}.zip .
         cd -
     done
 done

28e61c310cd8c1ccaf3170c5175b20eaf87feb4d
huajiezhang <huajiezhang@microsoft.com>
2022-02-18 11:43:21 +0800

    feat: update solution

--- packages/fx-core/src/plugins/solution/fx-solution/v3/userTask.ts
+++ packages/fx-core/src/plugins/solution/fx-solution/v3/userTask.ts
@@ -101,9 +101,11 @@ export async function getQuestionsForAddResource(
   for (const pair of pluginsWithResources) {
     const pluginName = pair[0];
     const resourceName = pair[1];
-    const plugin = Container.get<v3.FeaturePlugin>(pluginName);
-    if (plugin.getQuestionsForAddFeature) {
-      const res = await plugin.getQuestionsForAddFeature(ctx, inputs);
+    const plugin = Container.get<v3.PluginV3>(pluginName);
+    if (plugin.getQuestionsForAddFeature || plugin.getQuestionsForAddInstance) {
+      const res = plugin.getQuestionsForAddFeature
+        ? await plugin.getQuestionsForAddFeature(ctx, inputs)
+        : await plugin.getQuestionsForAddInstance!(ctx, inputs);
       if (res.isErr()) return res;
       if (res.value) {
         const node = res.value as QTreeNode;

adb843f67f3957338e9570ff69b41f0c09a19e7c
Wang Yefu <yefuwang@microsoft.com>
2022-02-21 16:22:17 +0800

    chore: move manifest types into a standalone package

--- packages/manifest/src/index.ts
+++ packages/manifest/src/index.ts
@@ -1,85 +1,53 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { TeamsAppManifest, IStaticTab, IConfigurableTab } from "@microsoft/teamsfx-api";
-import * as fs from "fs-extra";
+import { TeamsAppManifest } from "./manifest";
 
-export class Manifest {
-  private manifest: TeamsAppManifest;
+export * from "./manifest";
 
-  constructor(manifest?: TeamsAppManifest) {
-    if (manifest) {
-      this.manifest = manifest;
-    } else {
-      this.manifest = new TeamsAppManifest();
-    }
-  }
+/**
+ * Loads the manifest file from the given path.
+ * @param path The path to the manifest file.
+ * @returns The Manifest Object. 
+ */
+export async function loadFromPath(path: string): Promise<TeamsAppManifest> {
+  throw new Error("Method not implemented.");
+}
 
-  static async initWithAppName(appName: string): Promise<Manifest> {
-    const manifest = new TeamsAppManifest();
-    manifest.name.short = appName;
-    manifest.name.full = appName;
-    return new Manifest(manifest);
-  }
-  /**
-   * Load manifest from manifest.json
-   * @param filePath path to the manifest.json file
-   * @throws FileNotFoundError - when file not found
-   * @throws InvalidManifestError - when file is not a valid json or can not be parsed to TeamsAppManifest
-   */
-  static async loadFromPath(filePath: string): Promise<Manifest> {
-    const manifest = await fs.readJson(filePath);
-    return new Manifest(manifest);
-  }
+/**
+ * Writes the manifest object to the given path.
+ * 
+ * @param path Where to write
+ * @param manifest Manifest object to be saved
+ */
+export async function writeToPath(path: string, manifest: TeamsAppManifest): Promise<void> {
+  throw new Error("Method not implemented.");
+}
 
-  /**
-   * Save manifest to .json file
-   * @param filePath path to the manifest.json file
-   */
-  async save(filePath: string): Promise<void> {
-    await fs.writeFile(filePath, JSON.stringify(this.manifest, null, 4));
-  }
+/**
+ * Validate manifest against json schema
+ * @returns {string[]} An array of error string
+ */
+export async function validateManifest(manifest: TeamsAppManifest): Promise<string[]> {
+  throw new Error("Method not implemented.");
+}
 
-  /**
-   * Validate manifest against json schema
-   * @returns {string[]} An array of error string
-   */
-  async validate(): Promise<string[]> {
-    return [];
-  }
+/**
+ * Register a Teams app or update the existing Teams app
+ * @param filePath path to the zip file, with manifest and icons
+ * @param accessToken Access token with https://dev.teams.microsoft.com/AppDefinitions.ReadWrite scope
+ * @returns {string} Teams app id
+ */
+export async function deploy(filePath: string, accessToken: string): Promise<string> {
+  throw new Error("Method not implemented.");
+}
 
-  /**
-   * Register a Teams app or update the existing Teams app
-   * @param filePath path to the zip file, with manifest and icons
-   * @param accessToken Access token with https://dev.teams.microsoft.com/AppDefinitions.ReadWrite scope
-   * @returns {string} Teams app id
-   */
-  static async deploy(filePath: string, accessToken: string): Promise<string> {
-    return "";
-  }
-
-
-  /// strech goal
-
-  /**
-   * Add static tab to manifest
-   * @param tab
-   */
-  async addStaticTab(tab: IStaticTab): Promise<void> {}
-
-  /**
-   * Add configurable tab to manifest
-   * @param tab
-   */
-  async addConfigurableTab( tab: IConfigurableTab): Promise<void> {}
-
-  /**
-   * Publish a Teams app to Teams app catalog
-   * @param filePath path to the zip file, with manifest and icons
-   * @param accessToken Access token with https://dev.teams.microsoft.com/AppDefinitions.ReadWrite scope
-   * @returns {string} Published Teams app id, which is different from Teams app id in Developer Portal
-   */
-  static async publish(filePath: string, accessToken: string): Promise<string> {
-    return "";
-  }
+/**
+ * Publish a Teams app to Teams app catalog
+ * @param filePath path to the zip file, with manifest and icons
+ * @param accessToken Access token with https://dev.teams.microsoft.com/AppDefinitions.ReadWrite scope
+ * @returns {string} Published Teams app id, which is different from Teams app id in Developer Portal
+ */
+export async function publish(filePath: string, accessToken: string): Promise<string> {
+  throw new Error("Method not implemented.");
 }

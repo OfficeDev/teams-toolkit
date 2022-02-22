6e75cabc7c1a215dec4ad5bd1265afb9493ec4f6
Wang Yefu <yefuwang@microsoft.com>
2022-02-22 17:03:34 +0800

    chore: implement functions that don't require auth

--- packages/manifest/src/index.ts
+++ packages/manifest/src/index.ts
@@ -2,34 +2,80 @@
 // Licensed under the MIT license.
 
 import { TeamsAppManifest } from "./manifest";
+import fs from "fs-extra";
+import Ajv from "ajv-draft-04";
+import { JSONSchemaType, Schema } from "ajv"; 
+import axios, { AxiosResponse } from "axios";
 
 export * from "./manifest";
 
 /**
- * Loads the manifest file from the given path.
- * @param path The path to the manifest file.
+ * Loads the manifest from the given path without validating its schema.
+ * 
+ * @param path - The path to the manifest file.
+ * @throws Will propagate any error thrown by the fs-extra#readJson.
+ * 
  * @returns The Manifest Object. 
  */
 export async function loadFromPath(path: string): Promise<TeamsAppManifest> {
-  throw new Error("Method not implemented.");
+  return fs.readJson(path);
 }
 
 /**
  * Writes the manifest object to the given path.
  * 
- * @param path Where to write
- * @param manifest Manifest object to be saved
+ * @param path - Where to write
+ * @param manifest - Manifest object to be saved
+ * @throws Will propagate any error thrown by the fs-extra#writeJson.
+ * 
  */
 export async function writeToPath(path: string, manifest: TeamsAppManifest): Promise<void> {
-  throw new Error("Method not implemented.");
+  return fs.writeJson(path, manifest)
 }
 
 /**
- * Validate manifest against json schema
- * @returns {string[]} An array of error string
+ * Validate manifest against json schema.
+ * 
+ * @param manifest - Manifest object to be validated
+ * @param schema - teams-app-manifest schema
+ * @returns An empty array if validation succeeds, or an array of error string otherwise. 
+ */
+export async function validateManifestAgainstSchema(manifest: TeamsAppManifest, schema: JSONSchemaType<TeamsAppManifest>): Promise<string[]> {
+  const ajv = new Ajv({ formats: { uri: true } });
+  const validate = ajv.compile(schema);
+  const valid = validate(manifest);
+  if (!valid && validate.errors) {
+    return validate.errors?.map((error) => 
+      `${error.instancePath} ${error.message}`
+    );
+  } else {
+    return [];
+  }
+}
+
+/**
+ * Validate manifest against {@link TeamsAppManifest#$schema}.
+ * 
+ * @param manifest - Manifest object to be validated
+ * @returns An empty array if validation succeeds, or an array of error string otherwise. 
  */
 export async function validateManifest(manifest: TeamsAppManifest): Promise<string[]> {
-  throw new Error("Method not implemented.");
+  if (!manifest.$schema) {
+    throw new Error("Manifest does not have a $schema property");
+  }
+  let result: AxiosResponse<any>
+  try {
+    const axiosInstance = axios.create();
+    result = await axiosInstance.get(manifest.$schema);
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      throw new Error(`Failed to get manifest at url ${manifest.$schema} due to: ${e.message}`);
+    } else {
+      throw new Error(`Failed to get manifest at url ${manifest.$schema} due to: unknown error`);
+    }
+  }
+
+  return validateManifestAgainstSchema(manifest, result.data);
 }
 
 /**

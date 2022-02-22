36c0b22df5c9b98139f952cb36208cf5508b9adb
huajiezhang <huajiezhang@microsoft.com>
2022-02-22 18:10:04 +0800

    fix: err

--- packages/fx-core/src/core/index.ts
+++ packages/fx-core/src/core/index.ts
@@ -133,7 +133,6 @@ import {
   getSolutionPluginV2ByName,
 } from "./SolutionPluginContainer";
 import { newEnvInfo, newEnvInfoV3 } from "./tools";
-import { isCreatedFromExistingApp, isPureExistingApp } from "./utils";
 // TODO: For package.json,
 // use require instead of import because of core building/packaging method.
 // Using import will cause the build folder structure to change.

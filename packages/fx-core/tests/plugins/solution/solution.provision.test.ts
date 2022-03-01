5c415fcc496d78093c4b3663374ed34a0e833968
huajiezhang <huajiezhang@microsoft.com>
2022-03-01 19:58:08 +0800

    fix: ut

--- packages/fx-core/tests/plugins/solution/solution.provision.test.ts
+++ packages/fx-core/tests/plugins/solution/solution.provision.test.ts
@@ -3,7 +3,7 @@
 import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 import { it } from "mocha";
-import { SolutionRunningState, TeamsAppSolution } from " ../../../src/plugins/solution";
+import { TeamsAppSolution } from " ../../../src/plugins/solution";
 import {
   ConfigFolderName,
   FxError,
@@ -104,6 +104,7 @@ import { AadAppForTeamsPlugin } from "../../../src";
 import { assert } from "sinon";
 import { resourceGroupHelper } from "../../../src/plugins/solution/fx-solution/utils/ResourceGroupHelper";
 import * as manifestTemplate from "../../../src/plugins/resource/appstudio/manifestTemplate";
+import { SolutionRunningState } from "../../../src/plugins/solution/fx-solution/types";
 
 chai.use(chaiAsPromised);
 const expect = chai.expect;
--- packages/fx-core/tests/plugins/solution/solution.publish.test.ts
+++ packages/fx-core/tests/plugins/solution/solution.publish.test.ts
@@ -3,7 +3,7 @@
 import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 import { it } from "mocha";
-import { SolutionRunningState, TeamsAppSolution } from " ../../../src/plugins/solution";
+import { TeamsAppSolution } from " ../../../src/plugins/solution";
 import {
   ConfigFolderName,
   ConfigMap,
@@ -52,6 +52,7 @@ import { TeamsAppSolutionV2 } from "../../../src/plugins/solution/fx-solution/v2
 import { AppStudioTokenProvider } from "@microsoft/teamsfx-api";
 import { LocalCrypto } from "../../../src/core/crypto";
 import { aadPlugin, botPlugin, fehostPlugin, spfxPlugin, appStudioPlugin } from "../../constants";
+import { SolutionRunningState } from "../../../src/plugins/solution/fx-solution/types";
 
 chai.use(chaiAsPromised);
 const expect = chai.expect;

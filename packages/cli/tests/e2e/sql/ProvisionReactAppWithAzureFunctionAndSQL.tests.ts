000e5bd25d7a8ffd84e783d1609f489f75ea6a88
LongOddCode <haolong@microsoft.com>
2022-03-09 15:30:05 +0800

    test: 💍 sync with test plan cases

--- packages/cli/tests/e2e/sql/ProvisionReactAppWithAzureFunctionAndSQL.tests.ts
+++ packages/cli/tests/e2e/sql/ProvisionReactAppWithAzureFunctionAndSQL.tests.ts
@@ -6,6 +6,7 @@
  */
 
 import { environmentManager } from "@microsoft/teamsfx-core";
+import { describe } from "mocha";
 import fs from "fs-extra";
 import path from "path";
 
@@ -13,6 +14,7 @@ import { SqlValidator } from "../../commonlib";
 import { CliHelper } from "../../commonlib/cliHelper";
 import { Capability } from "../../commonlib/constants";
 import { getUuid } from "../../commonlib/utilities";
+import { it } from "../../commonlib/it";
 
 import {
   getSubscriptionId,
@@ -28,32 +30,36 @@ describe("Provision to Azure with SQL", function () {
   const subscription = getSubscriptionId();
   const projectPath = path.resolve(testFolder, appName);
 
-  it(`Provision react app with Azure Function and SQL`, async function () {
-    // new a project ( tab + function + sql )
-    await CliHelper.createProjectWithCapability(
-      appName,
-      testFolder,
-      Capability.Tab,
-      process.env,
-      "--azure-resources function sql"
-    );
+  it(
+    `Provision react app with Azure Function and SQL`,
+    { testPlanCaseId: 12700953 },
+    async function () {
+      // new a project ( tab + function + sql )
+      await CliHelper.createProjectWithCapability(
+        appName,
+        testFolder,
+        Capability.Tab,
+        process.env,
+        "--azure-resources function sql"
+      );
 
-    await setSimpleAuthSkuNameToB1Bicep(projectPath, environmentManager.getDefaultEnvName());
+      await setSimpleAuthSkuNameToB1Bicep(projectPath, environmentManager.getDefaultEnvName());
 
-    // provision
-    await CliHelper.setSubscription(subscription, projectPath);
-    await CliHelper.provisionProject(
-      projectPath,
-      `--sql-admin-name Abc123321 --sql-password Cab232332${getUuid().substring(0, 6)}`
-    );
+      // provision
+      await CliHelper.setSubscription(subscription, projectPath);
+      await CliHelper.provisionProject(
+        projectPath,
+        `--sql-admin-name Abc123321 --sql-password Cab232332${getUuid().substring(0, 6)}`
+      );
 
-    // Get context
-    const context = await fs.readJSON(`${projectPath}/.fx/states/state.dev.json`);
+      // Get context
+      const context = await fs.readJSON(`${projectPath}/.fx/states/state.dev.json`);
 
-    // Validate Aad App
-    await SqlValidator.init(context);
-    await SqlValidator.validateSql();
-  });
+      // Validate Aad App
+      await SqlValidator.init(context);
+      await SqlValidator.validateSql();
+    }
+  );
 
   after(async () => {
     await cleanUp(appName, projectPath, true, false, false);

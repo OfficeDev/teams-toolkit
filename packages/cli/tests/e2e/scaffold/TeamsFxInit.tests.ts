232d6f0735840f18ad742d536536daecc89da069
Wang Yefu <1yefuwang1@gmail.com>
2022-03-10 14:12:44 +0800

    feat(cli): add e2e test

---
+++ packages/cli/tests/e2e/scaffold/TeamsFxInit.tests.ts
@@ -0,0 +1,49 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+/**
+ * @author Yefu Wang <yefuwang@microsoft.com>
+ */
+
+import path from "path";
+import { BotValidator, FrontendValidator, FunctionValidator } from "../../commonlib";
+import * as chai from "chai";
+
+import { execAsync, getTestFolder, getUniqueAppName, cleanUpLocalProject } from "../commonUtils";
+import * as fs from "fs-extra";
+
+describe("teamsfx init", function () {
+  let testFolder: string;
+  let appName: string;
+  let projectPath: string;
+
+  // Should succeed on the 3rd try
+  this.retries(2);
+
+  beforeEach(() => {
+    testFolder = getTestFolder();
+    appName = getUniqueAppName();
+    projectPath = path.resolve(testFolder, appName);
+  });
+
+  afterEach(async () => {
+    await cleanUpLocalProject(projectPath);
+  });
+
+  it(`should scaffold a basic project`, async function () {
+    // new a project (tab + bot + function) in TypeScript
+    await execAsync(
+      `teamsfx init --interactive false --app-name ${appName} --folder ${testFolder}`,
+      {
+        cwd: testFolder,
+        env: process.env,
+        timeout: 0,
+      }
+    );
+    console.log(`[Successfully] run teamsfx init to ${projectPath}`);
+    const files = ["packge.json", "templates", ".fx"];
+    for (const file of files) {
+      chai.assert.isTrue(await fs.pathExists(path.resolve(projectPath, file)));
+    }
+  });
+});

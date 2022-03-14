b09f6a85e5cc874406622b866fde62b10a6fee43
KennethBWSong <63089166+KennethBWSong@users.noreply.github.com>
2022-03-14 16:05:42 +0800

    perf(frontend): add non-sso template

---
+++ templates/tab/js/non-sso/src/components/sample/lib/useTeamsFx.js
@@ -0,0 +1,40 @@
+import { loadConfiguration, ResourceType, LogLevel, setLogLevel, setLogFunction } from "@microsoft/teamsfx";
+import { useData } from "./useData";
+import { useTeams } from "msteams-react-base-component";
+
+var startLoginPageUrl = process.env.REACT_APP_START_LOGIN_PAGE_URL;
+var functionEndpoint = process.env.REACT_APP_FUNC_ENDPOINT;
+var clientId = process.env.REACT_APP_CLIENT_ID;
+
+// TODO fix this when the SDK stops hiding global state!
+let initialized = false;
+
+export function useTeamsFx() {
+  const [result] = useTeams({});
+  const { error, loading } = useData(async () => {
+    if (!initialized) {
+      if (process.env.NODE_ENV === "development") {
+        setLogLevel(LogLevel.Verbose);
+        setLogFunction((level, message) => { console.log(message); });
+      }
+      loadConfiguration({
+        authentication: {
+          initiateLoginEndpoint: startLoginPageUrl,
+          clientId: clientId,
+        },
+        resources: [
+          {
+            type: ResourceType.API,
+            name: "default",
+            properties: {
+              endpoint: functionEndpoint,
+            },
+          },
+        ],
+      });
+      initialized = true;
+    }
+  });
+  const isInTeams = true;
+  return { error, loading, isInTeams, ...result };
+}

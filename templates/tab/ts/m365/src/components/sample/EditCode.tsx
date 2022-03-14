c7b23b3255ed77df4630b5450b3a3560772fea99
Kuojian Lu <kuojianlu@gmail.com>
2022-03-14 23:13:56 +0800

    feat(m365-tab): add m365 tab template
    Based on current tab template, but update:
    1. @microsoft/teams-js version to 2.0.0-beta.2
    2. @microsoft/teamsfx version to 0.6.0-beta.0
    3. msteams-react-base-component version to 4.0.0-preview5
    5. TabConfig.jsx and TabConfig.tsx to use @microsoft/teams-js v2
    6. auth-start.html and auth-end.html to use @microsoft/teams-js v2

---
+++ templates/tab/ts/m365/src/components/sample/EditCode.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+var functionName = process.env.REACT_APP_FUNC_NAME || "myFunc";
+
+export function EditCode(props: {
+  showFunction?: boolean;
+  tabCodeEntry?: string;
+  functionCodePath?: string;
+}) {
+  const { showFunction, tabCodeEntry, functionCodePath } = {
+    showFunction: true,
+    tabCodeEntry: "tabs/src/index.tsx",
+    functionCodePath: `api/${functionName}/index.ts`,
+    ...props,
+  };
+  return (
+    <div>
+      <h2>Change this code</h2>
+      <p>
+        The front end is a <code>create-react-app</code>. The entry point is{" "}
+        <code>{tabCodeEntry}</code>. Just save any file and this page will reload automatically.
+      </p>
+      {showFunction && (
+        <p>
+          This app contains an Azure Functions backend. Find the code in{" "}
+          <code>{functionCodePath}</code>
+        </p>
+      )}
+    </div>
+  );
+}

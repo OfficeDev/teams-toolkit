aba2c68b96c70745ddad4927ee1405eaf3b8cfb7
Qinen <qinzhu@microsoft.com>
2022-03-14 10:14:38 +0800

    chore: add output info for init command

--- packages/fx-core/src/core/FxCore.ts
+++ packages/fx-core/src/core/FxCore.ts
@@ -123,6 +123,7 @@ import {
 } from "./question";
 import { getAllSolutionPluginsV2, getSolutionPluginV2ByName } from "./SolutionPluginContainer";
 import { CoreHookContext } from "./types";
+import { getLocalizedString } from "../common/localizeUtils";
 
 export class FxCore implements v3.ICore {
   tools: Tools;
@@ -1442,7 +1443,12 @@ export class FxCore implements v3.ICore {
 
   @hooks([ErrorHandlerMW, QuestionModelMW, ContextInjectorMW, ProjectSettingsWriterMW])
   async init(inputs: Inputs, ctx?: CoreHookContext): Promise<Result<string, FxError>> {
-    return this._init(inputs, ctx);
+    const result = await this._init(inputs, ctx);
+    if (result.isOk()) {
+      TOOLS.ui.showMessage("info", getLocalizedString("core.init.successNotice"), false);
+    }
+
+    return result;
   }
 
   @hooks([

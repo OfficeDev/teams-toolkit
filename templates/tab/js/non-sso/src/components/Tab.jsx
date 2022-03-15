34c4ba4bac99906a4861c58b0a9b9e6531604c08
KennethBWSong <63089166+KennethBWSong@users.noreply.github.com>
2022-03-15 10:27:13 +0800

    perf: remove useless code

--- templates/tab/js/non-sso/src/components/Tab.jsx
+++ templates/tab/js/non-sso/src/components/Tab.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import { Welcome } from "./sample/Welcome";
-import { useTeamsFx } from "./sample/lib/useTeamsFx";
-
-var showFunction = Boolean(process.env.REACT_APP_FUNC_NAME);
 
 export default function Tab() {
-  const { themeString } = useTeamsFx();
   return (
-    <div className={themeString === "default" ? "" : "dark"}>
-      <Welcome showFunction={showFunction} />
+    <div>
+      <Welcome />
     </div>
   );
 }

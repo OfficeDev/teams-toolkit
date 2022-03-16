8d279b309c923deec2e5966fa736a898b48deb4d
Alex Wang <a1exwang@users.noreply.github.com>
2022-03-16 15:54:46 +0800

    chore: fix styles (#4265)

--- templates/bot/ts/notification/src/sdk/interfaces.ts
+++ templates/bot/ts/notification/src/sdk/interfaces.ts
@@ -1,59 +1,59 @@
 import { Activity, ChannelInfo, TeamsChannelAccount, TurnContext } from "botbuilder";
 
 export interface WelcomeMessage {
-    message: Partial<Activity>,
-    trigger?: WelcomeMessageTrigger,
+  message: Partial<Activity>;
+  trigger?: WelcomeMessageTrigger;
 }
 
 export enum WelcomeMessageTrigger {
-    BotInstall,
-    NewMemberAdded
+  BotInstall,
+  NewMemberAdded,
 }
 
 export interface TeamsFxBotSettingsProviderOptions {
-    commandName?: string;
-    submitActionKey?: string;
-    submitActionValue?: string;
+  commandName?: string;
+  submitActionKey?: string;
+  submitActionValue?: string;
 }
 
 export interface TeamsFxBotSettingsProvider {
-    commandName: string;
-    submitActionKey: string;
-    submitActionValue: string;
-    handleCardSubmit(context: BotContext, data: any): Promise<TeamsFxBotSettings>;
-    sendSettingsCard(context: BotContext): Promise<any>;
+  commandName: string;
+  submitActionKey: string;
+  submitActionValue: string;
+  handleCardSubmit(context: BotContext, data: any): Promise<TeamsFxBotSettings>;
+  sendSettingsCard(context: BotContext): Promise<any>;
 }
 
 export abstract class BasicTeamsFxBotSettingsProvider implements TeamsFxBotSettingsProvider {
-    readonly commandName: string;
-    readonly submitActionKey: string;
-    readonly submitActionValue: string;
+  readonly commandName: string;
+  readonly submitActionKey: string;
+  readonly submitActionValue: string;
 
-    constructor(options: TeamsFxBotSettingsProviderOptions) {
-        this.commandName = options.commandName ?? "settings";
-        this.submitActionKey = options.submitActionKey ?? "submitAction";
-        this.submitActionValue = options.submitActionValue ?? "updateSettings";
-    }
+  constructor(options: TeamsFxBotSettingsProviderOptions) {
+    this.commandName = options.commandName ?? "settings";
+    this.submitActionKey = options.submitActionKey ?? "submitAction";
+    this.submitActionValue = options.submitActionValue ?? "updateSettings";
+  }
 
-    abstract handleCardSubmit(context: BotContext, data: any): Promise<TeamsFxBotSettings>;
-    abstract sendSettingsCard(context: BotContext): Promise<any>;
+  abstract handleCardSubmit(context: BotContext, data: any): Promise<TeamsFxBotSettings>;
+  abstract sendSettingsCard(context: BotContext): Promise<any>;
 }
 
 export type TeamsFxBotSettings = { [key: string]: any };
 
 export interface TeamsFxMember {
-    subscriber: BotContext,
-    account: TeamsChannelAccount
+  subscriber: BotContext;
+  account: TeamsChannelAccount;
 }
 
 export interface TeamsFxChannel {
-    subscriber: BotContext,
-    info: ChannelInfo
+  subscriber: BotContext;
+  info: ChannelInfo;
 }
 
 export interface BotContext {
-    turnContext: TurnContext;
-    members: Promise<TeamsFxMember[]>;
-    channels: Promise<TeamsFxChannel[]>;
-    settings: Promise<TeamsFxBotSettings>
+  turnContext: TurnContext;
+  members: Promise<TeamsFxMember[]>;
+  channels: Promise<TeamsFxChannel[]>;
+  settings: Promise<TeamsFxBotSettings>;
 }

572b534787ac619674e4b02771cba6d098f5f62e
Aocheng Wang <aochengwang@microsoft.com>
2022-03-16 15:42:59 +0800

    chore: fix styles

--- templates/bot/ts/notification/src/sdk/context.ts
+++ templates/bot/ts/notification/src/sdk/context.ts
@@ -4,56 +4,53 @@ import { BotSettingsStore } from "./store";
 import { Utils } from "./utils";
 
 export class TeamsFxBotContext implements BotContext {
-    public turnContext: TurnContext;
-    private settingsStore: BotSettingsStore;
+  public turnContext: TurnContext;
+  private settingsStore: BotSettingsStore;
 
-    constructor(
-        turnContext: TurnContext,
-        settingsStore: BotSettingsStore
-    ) {
-        this.turnContext = turnContext;
-        this.settingsStore = settingsStore;
-    }
+  constructor(turnContext: TurnContext, settingsStore: BotSettingsStore) {
+    this.turnContext = turnContext;
+    this.settingsStore = settingsStore;
+  }
 
-    public get members(): Promise<TeamsFxMember[]> {
-        return (async () => {
-            const teamsMembers = await TeamsInfo.getMembers(this.turnContext);
-            const teamsfxMembers: TeamsFxMember[] = [];
-            for (const member of teamsMembers) {
-                teamsfxMembers.push({
-                    subscriber: this,
-                    account: member
-                })
-            }
+  public get members(): Promise<TeamsFxMember[]> {
+    return (async () => {
+      const teamsMembers = await TeamsInfo.getMembers(this.turnContext);
+      const teamsfxMembers: TeamsFxMember[] = [];
+      for (const member of teamsMembers) {
+        teamsfxMembers.push({
+          subscriber: this,
+          account: member,
+        });
+      }
 
-            return teamsfxMembers;
-        })();
-    }
+      return teamsfxMembers;
+    })();
+  }
 
-    public get channels(): Promise<TeamsFxChannel[]> {
-        return (async () => {
-            const teamsfxChannels: TeamsFxChannel[] = [];
-            const teamId = Utils.getSubscriberId(this.turnContext);
-            if (!teamId) {
-                return teamsfxChannels;
-            }
+  public get channels(): Promise<TeamsFxChannel[]> {
+    return (async () => {
+      const teamsfxChannels: TeamsFxChannel[] = [];
+      const teamId = Utils.getSubscriberId(this.turnContext);
+      if (!teamId) {
+        return teamsfxChannels;
+      }
 
-            const teamsChannels = await TeamsInfo.getTeamChannels(this.turnContext, teamId);
-            for (const channel of teamsChannels) {
-                teamsfxChannels.push({
-                    subscriber: this,
-                    info: channel
-                })
-            }
+      const teamsChannels = await TeamsInfo.getTeamChannels(this.turnContext, teamId);
+      for (const channel of teamsChannels) {
+        teamsfxChannels.push({
+          subscriber: this,
+          info: channel,
+        });
+      }
 
-            return teamsfxChannels;
-        })();
-    }
+      return teamsfxChannels;
+    })();
+  }
 
-    public get settings(): Promise<TeamsFxBotSettings> {
-        const subscriberId = Utils.getSubscriberId(this.turnContext);
-        return (async () => {
-            return await this.settingsStore.get(subscriberId);
-        })();
-    }
+  public get settings(): Promise<TeamsFxBotSettings> {
+    const subscriberId = Utils.getSubscriberId(this.turnContext);
+    return (async () => {
+      return await this.settingsStore.get(subscriberId);
+    })();
+  }
 }

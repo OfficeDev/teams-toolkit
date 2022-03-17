e0b243b3a0479538c7bc4548a5ed6c937dc7d319
Di Lin <dilin@microsoft.com>
2022-03-17 15:30:00 +0800

    chore: remove useless comments

--- packages/sdk/src/authentication/ApiClient.ts
+++ packages/sdk/src/authentication/ApiClient.ts
@@ -10,38 +10,12 @@ export function createApiClient(apiEndpoint: string, authProvider: IAuthProvider
   axios.interceptors.request.use(
     async function (config) {
       config.url = apiEndpoint;
-
       await authProvider.configureAxiosRequest(config);
-
-      // Do something before request is sent
       return config;
     },
     function (error) {
-      // Do something with request error
       return Promise.reject(error);
     }
   );
   return axios;
 }
-
-// // Loads current app's configuration
-// const teamsFx = new TeamsFx(IdentityType.User).setSsoToken("xxx"); // The SSO token is required to call function with OBO flow
-
-// Initializes a new axios instance to call kudos API
-// const kudosClient = createApiClient(
-//   teamsFx.getConfig["API_KUDOS_ENDPOINT"],
-//   new BasicAuthProvider({
-//     UserName: teamsFx.getConfig["API_KUDOS_USERNAME"],
-//     Password: teamsFx.getConfig["API_KUDOS_PASSWORD"],
-//   })
-// );
-
-// const kudosResult = await kudosClient.get("kudos_api_name");
-
-// // Initializes a new axios instance to call Azure Functions
-
-// const functionClient = createApiClient(teasmFx.getConfig["API_ENDPOINT"], {
-//   Credential: teamsFx.getCredential(),
-// });
-
-// const functionResult = await functionClient.get("function_api_name");

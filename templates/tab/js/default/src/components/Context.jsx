94a528c703d896a5196456fdc8e4383bf139a647
Ning Tang <nintan@microsoft.com>
2022-03-21 13:44:11 +0800

    chore: remove unused codes

--- templates/tab/js/default/src/components/Context.jsx
+++ templates/tab/js/default/src/components/Context.jsx
@@ -1,6 +1,4 @@
-import { TeamsFx } from "@microsoft/teamsfx";
 import { createContext } from "react";
-import { ThemePrepared } from "@fluentui/react-northstar";
 
 export const TeamsFxContext = createContext({
   theme: undefined,

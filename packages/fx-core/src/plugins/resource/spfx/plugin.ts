4461898fd4dce00b9c0dca27ced9fc8cf0a671e2
Huihui Wu <huihuiwu@microsoft.com>
2022-03-23 10:31:00 +0800

    feat: add sharepoint generator checker and use yeoman for scaffolding

--- packages/fx-core/src/plugins/resource/spfx/plugin.ts
+++ packages/fx-core/src/plugins/resource/spfx/plugin.ts
@@ -56,6 +56,8 @@ import { convert2Context } from "../utils4v2";
 import { yeomanScaffoldEnabled } from "../../../core/globalVars";
 import { getLocalizedString } from "../../../common/localizeUtils";
 import { YoChecker } from "./depsChecker/yoChecker";
+import { GeneratorChecker } from "./depsChecker/generatorChecker";
+import { cpUtils } from "../../solution/fx-solution/utils/depsChecker/cpUtils";
 
 export class SPFxPluginImpl {
   public async postScaffold(ctx: PluginContext): Promise<Result<any, FxError>> {
@@ -74,15 +76,49 @@ export class SPFxPluginImpl {
         if (yoRes.isErr()) {
           throw DependencyInstallError("yo");
         }
+        const spGeneratorChecker = new GeneratorChecker(ctx.logProvider!);
+        const spGeneratorRes = await spGeneratorChecker.ensureDependency(ctx);
+        if (spGeneratorRes.isErr()) {
+          throw DependencyInstallError("sharepoint generator");
+        }
 
         const webpartDescription = ctx.answers![SPFXQuestionNames.webpart_desp] as string;
         const framework = ctx.answers![SPFXQuestionNames.framework_type] as string;
         const solutionName = ctx.projectSettings?.appName as string;
-        const command = `yo @microsoft/sharepoint --skip-install true --component-type webpart --component-description ${webpartDescription} --component-name ${webpartName} --framework ${framework} --solution-name ${solutionName} --environment spo --skip-feature-deployment true --is-domain-isolated false`;
         if (ctx.answers?.platform === Platform.VSCode) {
           (ctx.logProvider as any).outputChannel.show();
         }
-        await Utils.execute(command, "SPFx", ctx.root, ctx.logProvider, true);
+
+        const yoEnv: NodeJS.ProcessEnv = process.env;
+        yoEnv.PATH = `${yoChecker.getBinFolder()}${path.delimiter}${process.env.PATH ?? ""}`;
+        await cpUtils.executeCommand(
+          ctx.root,
+          ctx.logProvider,
+          {
+            timeout: 2 * 60 * 1000,
+            env: yoEnv,
+          },
+          "yo",
+          spGeneratorChecker.getSpGeneratorPath(),
+          "--skip-install",
+          "true",
+          "--component-type",
+          "webpart",
+          "--component-description",
+          webpartDescription,
+          "--component-name",
+          webpartName,
+          "--framework",
+          framework,
+          "--solution-name",
+          solutionName,
+          "--environment",
+          "spo",
+          "--skip-feature-deployment",
+          "true",
+          "--is-domain-isolated",
+          "false"
+        );
 
         const currentPath = path.join(ctx.root, solutionName);
         const newPath = path.join(ctx.root, "SPFx");

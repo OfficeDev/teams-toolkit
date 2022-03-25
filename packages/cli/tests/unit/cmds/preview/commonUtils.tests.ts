006371ec3a804c531d128ac50d82c9264f916562
Kuojian Lu <kuojianlu@gmail.com>
2022-03-25 17:29:33 +0800

    test: add ut

--- packages/cli/tests/unit/cmds/preview/commonUtils.tests.ts
+++ packages/cli/tests/unit/cmds/preview/commonUtils.tests.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import { IProgressHandler, err, ok, returnUserError } from "@microsoft/teamsfx-api";
 import * as sinon from "sinon";
 import {
--- packages/cli/tests/unit/cmds/preview/launch.test.ts
+++ packages/cli/tests/unit/cmds/preview/launch.test.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import { IProgressHandler } from "@microsoft/teamsfx-api";
 import * as sinon from "sinon";
 import { expect } from "../../utils";

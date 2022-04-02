4d19a8efa728339d18c7bed8f8be347029124309
wenyutang-ms <wenyutang@microsoft.com>
2022-04-02 18:05:14 +0800

    test: add test cases

--- packages/fx-core/src/plugins/resource/apiconnector/depsHandler.ts
+++ packages/fx-core/src/plugins/resource/apiconnector/depsHandler.ts
@@ -33,34 +33,34 @@ export class DepsHandler {
   public async updateLocalPkgSdkVersion(pkgConfig: Json): Promise<ApiConnectorResult> {
     const localPkgPath = path.join(this.projectRoot, this.componentType, Constants.pkgJsonFile);
     const pkgContent = await fs.readJson(localPkgPath);
-    const needChange = this.sdkVersionCheck(
+    const needUpdate: boolean = this.sdkVersionCheck(
       pkgContent.dependencies,
       pkgConfig.name,
       pkgConfig.version
     );
-    if (needChange) {
+    if (needUpdate) {
       pkgContent.dependencies[pkgConfig.name] = pkgConfig.version;
       await fs.writeFile(localPkgPath, JSON.stringify(pkgContent, null, 4));
     }
     return ResultFactory.Success();
   }
 
-  private sdkVersionCheck(deps: Json, sdkName: string, sdkVersion: string): ApiConnectorResult {
+  private sdkVersionCheck(deps: Json, sdkName: string, sdkVersion: string): boolean {
     // always sync up with alpha version. only happens on alpha/RC version.
     if (semver.prerelease(sdkVersion)) {
-      return ok(true);
+      return true;
     }
     // sdk not in dependencies.
     else if (!deps[sdkName]) {
-      return ok(true);
+      return true;
     }
     // local sdk version intersect with sdk version in config.
     else if (semver.intersects(deps[sdkName], sdkVersion)) {
-      return ok(false);
+      return false;
     }
     // local sdk version lager than sdk version in config.
     else if (semver.gt(semver.minVersion(deps[sdkName])!, semver.minVersion(sdkVersion)!)) {
-      return ok(false);
+      return false;
     } else {
       throw ResultFactory.UserError(
         ErrorMessage.sdkVersionImcompatibleError.name,

3fe502b54f1856da76e96e8bd01471bbf6f3e666
huajiezhang <huajiezhang@microsoft.com>
2022-04-06 09:03:22 +0800

    feat: up

--- packages/fx-core/src/core/workflow/deploy.ts
+++ packages/fx-core/src/core/workflow/deploy.ts
@@ -86,4 +86,63 @@ async function deploy() {
   console.log(context.projectSetting);
 }
 
-deploy();
+async function runDeployJson() {
+  setTools(new MockTools());
+  const projectSetting: ProjectSettingsV3 = {
+    projectId: "123",
+    appName: "test",
+    solutionSettings: { name: "fx", activeResourcePlugins: [] },
+    programmingLanguage: "typescript",
+    resources: [
+      {
+        name: "teams-tab",
+        hostingResource: "azure-storage",
+        framework: "react",
+        folder: "myApp",
+      },
+      {
+        name: "tab-scaffold",
+        build: true,
+        deployType: "zip",
+        folder: "myApp",
+        language: "typescript",
+        framework: "react",
+        hostingResource: "azure-storage",
+      },
+      { name: "azure-storage", provision: true },
+      {
+        name: "teams-bot",
+        scenarios: ["default"],
+        folder: "bot",
+        hostingResource: "azure-web-app",
+      },
+      {
+        name: "bot-scaffold",
+        build: true,
+        deployType: "zip",
+        folder: "bot",
+        language: "typescript",
+        scenarios: ["default"],
+        hostingResource: "azure-web-app",
+      },
+      { name: "azure-web-app", provision: true },
+    ],
+  };
+  const context = createV2Context(projectSetting);
+  const inputs: v2.InputsWithProjectPath = {
+    projectPath: path.join(os.tmpdir(), "myapp"),
+    platform: Platform.VSCode,
+  };
+  const action = fs.readJSONSync("deploy.json");
+  if (action) {
+    await planAction(action, context, cloneDeep(inputs));
+    await executeAction(action, context, inputs);
+  }
+  console.log("inputs:");
+  console.log(inputs);
+  console.log("projectSetting:");
+  console.log(context.projectSetting);
+}
+
+// deploy();
+runDeployJson();

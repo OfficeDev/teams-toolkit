1f263658d3c3325093a82d1006e0c864419a5ab4
Bowen Song <bowsong@microsoft.com>
2022-04-07 10:44:10 +0800

    perf: update add sso (#4533)
    * perf: update add sso
    
    * test: fix ut
    
    * fix: fix migration

--- packages/fx-core/src/core/FxCore.ts
+++ packages/fx-core/src/core/FxCore.ts
@@ -64,7 +64,8 @@ import {
   ExistingTabOptionItem,
   MessageExtensionItem,
   NotificationOptionItem,
-  SsoItem,
+  TabSsoItem,
+  BotSsoItem,
   TabOptionItem,
   TabSPFxItem,
 } from "../plugins/solution/fx-solution/question";
@@ -375,7 +376,10 @@ export class FxCore implements v3.ICore {
         if (!isAadManifestEnabled() && !capabilities.includes(TabSPFxItem.id)) {
           features.push(BuiltInFeaturePluginNames.aad);
         }
-        if (isAadManifestEnabled() && capabilities.includes(SsoItem.id)) {
+        if (
+          isAadManifestEnabled() &&
+          (capabilities.includes(TabSsoItem.id) || capabilities.includes(BotSsoItem.id))
+        ) {
           features.push(BuiltInFeaturePluginNames.aad);
         }
         if (inputs.platform === Platform.VS) {

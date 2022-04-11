fcbe5217e7c3fa90a2c5574f754df820b7f77daa
Qianhao Dong <qidon@microsoft.com>
2022-04-11 10:11:36 +0800

    chore: update notification template (#4589)
    * chore: update notification restify ts template
    
    * chore: update notification function ts template
    
    * chore: update notification restify js template
    
    * chore: update notification function js template

--- templates/bot/js/notification-function-base/src/internal/messageHandler.js
+++ templates/bot/js/notification-function-base/src/internal/messageHandler.js
@@ -1,9 +1,5 @@
-const { TeamsActivityHandler } = require("botbuilder");
-const { adapter } = require("./initialize");
+const { bot } = require("./initialize");
 
-const handler = new TeamsActivityHandler();
 module.exports = async function (context, req) {
-  await adapter.processActivity(req, context.res, async (context) => {
-    await handler.run(context);
-  });
+  await bot.requestHandler(req, context.res);
 };

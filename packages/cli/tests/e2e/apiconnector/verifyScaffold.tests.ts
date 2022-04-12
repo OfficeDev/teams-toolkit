48d76f317a5ed650484f92b8a690d2b38a8b3f83
wenyutang-ms <wenyutang@microsoft.com>
2022-04-12 14:34:49 +0800

    test: add cli test case

---
+++ packages/cli/tests/e2e/apiconnector/verifyScaffold.tests.ts
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import path from "path";
+import "mocha";
+import { expect } from "chai";
+import { getTestFolder, getUniqueAppName, cleanUp } from "../commonUtils";
+import { CliHelper } from "../../commonlib/cliHelper";
+import { Capability } from "../../commonlib/constants";
+import * as fs from "fs-extra";
+
+describe("Add api-connection cli", () => {
+  const testFolder = getTestFolder();
+  const appName = getUniqueAppName();
+  const projectPath = path.resolve(testFolder, appName);
+  before(async () => {});
+  after(async () => {
+    await cleanUp(appName, projectPath, false, false, false);
+  });
+
+  it("Generate Sample codes success - basic auth", async () => {
+    await CliHelper.createProjectWithCapability(appName, testFolder, Capability.Bot);
+    await CliHelper.addExistingApi(
+      projectPath,
+      `--api-connector-auth-type basic --component-select bot --api-connector-user-name basictest --api-connector-endpoint https://localhost.basictest.com --api-connector-name basictest --interactive false`
+    );
+    // Assert
+    expect(await fs.pathExists(path.join(testFolder, "bot", "basictest.js"))).to.be.true;
+    expect(await fs.pathExists(path.join(testFolder, "bot", ".env.teamsfx.local"))).to.be.true;
+    // todo check the content.
+  });
+});

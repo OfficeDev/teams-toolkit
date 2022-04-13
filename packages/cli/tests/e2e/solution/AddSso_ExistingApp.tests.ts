7d0d8f52f0a259d6a57f9bb990a59cd11c2a13dc
KennethBWSong <63089166+KennethBWSong@users.noreply.github.com>
2022-04-13 13:38:20 +0800

    test: add existing app test

--- packages/cli/tests/e2e/solution/AddSso_ExistingApp.tests.ts
+++ packages/cli/tests/e2e/solution/AddSso_ExistingApp.tests.ts
@@ -16,13 +16,15 @@ import {
   setSimpleAuthSkuNameToB1Bicep,
   validateTabAndBotProjectProvision,
   execAsync,
+  getActivePluginsFromProjectSetting,
+  getCapabilitiesFromProjectSetting,
   readContextMultiEnv,
 } from "../commonUtils";
 import { CliHelper } from "../../commonlib/cliHelper";
 import { Capability, PluginId, ProjectSettingKey, TestFilePath } from "../../commonlib/constants";
 import fs from "fs-extra";
 import { expect } from "chai";
-import { AadValidator, BotValidator } from "../../commonlib";
+import { AadValidator } from "../../commonlib";
 import appStudioLogin from "../../../src/commonlib/appStudioLogin";
 
 describe("Add SSO", () => {
@@ -34,29 +36,15 @@ describe("Add SSO", () => {
   const env = Object.assign({}, process.env);
   env["TEAMSFX_AAD_MANIFEST"] = "true";
   env["TEAMSFX_CONFIG_UNIFY"] = "true";
+  env["TEAMSFX_INIT_APP"];
 
   after(async () => {
     await cleanUp(appName, projectPath, true, true, false);
   });
 
-  it("Add SSO to non SSO Bot project", async () => {
+  it("Add SSO to existing app", async () => {
     // Arrange
-    await CliHelper.createProjectWithCapability(appName, testFolder, Capability.Bot, env);
-
-    // Assert
-    {
-      const projectSettings = await fs.readJSON(
-        path.join(projectPath, TestFilePath.configFolder, TestFilePath.projectSettingsFileName)
-      );
-      const activeResourcePlugins =
-        projectSettings[ProjectSettingKey.solutionSettings][
-          ProjectSettingKey.activeResourcePlugins
-        ];
-      const capabilities =
-        projectSettings[ProjectSettingKey.solutionSettings][ProjectSettingKey.capabilities];
-      expect(activeResourcePlugins.includes(PluginId.Aad)).to.be.false;
-      expect(capabilities.includes(Capability.BotSso)).to.be.false;
-    }
+    await CliHelper.createProjectWithCapability(appName, testFolder, Capability.ExistingTab, env);
 
     // Act
     await execAsync(`teamsfx add sso`, {
@@ -67,43 +55,22 @@ describe("Add SSO", () => {
 
     // Assert
     {
-      const projectSettings = await fs.readJSON(
-        path.join(projectPath, TestFilePath.configFolder, TestFilePath.projectSettingsFileName)
-      );
-      const activeResourcePlugins =
-        projectSettings[ProjectSettingKey.solutionSettings][
-          ProjectSettingKey.activeResourcePlugins
-        ];
-      const capabilities =
-        projectSettings[ProjectSettingKey.solutionSettings][ProjectSettingKey.capabilities];
+      const activeResourcePlugins = await getActivePluginsFromProjectSetting(projectPath);
+      const capabilities = await getCapabilitiesFromProjectSetting(projectPath);
       expect(activeResourcePlugins.includes(PluginId.Aad)).to.be.true;
-      expect(capabilities.includes(Capability.BotSso)).to.be.true;
+      expect(capabilities.includes(Capability.TabSso)).to.be.true;
 
-      const readmeFilePath = path.join(projectPath, "auth", "bot", "README.md");
+      const readmeFilePath = path.join(projectPath, "auth", "tab", "README.md");
       const readmeExists = await fs.pathExists(readmeFilePath);
       expect(readmeExists).to.be.true;
     }
 
     await CliHelper.provisionProject(projectPath, "", env);
 
+    // Assert
     const context = await readContextMultiEnv(projectPath, "dev");
     // Validate Aad App
     const aad = AadValidator.init(context, false, appStudioLogin);
     await AadValidator.validate(aad);
-
-    // Validate Bot Provision
-    const bot = new BotValidator(context, projectPath, "dev");
-    await bot.validateProvision();
-
-    // Act
-    try {
-      await execAsync(`teamsfx add sso`, {
-        cwd: projectPath,
-        env: env,
-        timeout: 0,
-      });
-    } catch (error) {
-      expect(error.toString()).to.contains("SsoEnabled");
-    }
   });
 });

bbdc4b6d7913319de365a37f3cfe7a3b01332311
Zhaofeng Xu <zhaofengxu@microsoft.com>
2022-04-13 14:19:21 +0800

    chore(apicon): add api-key and cert

--- packages/fx-core/tests/plugins/resource/apiconnector/expectedFiles/js/cert.js
+++ packages/fx-core/tests/plugins/resource/apiconnector/expectedFiles/js/cert.js
@@ -1,14 +1,14 @@
-import { TeamsFx, createApiClient, ApiKeyProvider, ApiKeyLocation } from "@microsoft/teamsfx";
+const teamsfxSdk = require("@microsoft/teamsfx");
 
 // Loads current app's configuration.
-const teamsFx = new TeamsFx();
+const teamsFx = new teamsfxSdk.TeamsFx();
 // Initializes a new axios instance to call fake API.
-const authProvider = new ApiKeyProvider(
-  "fake_api_key_name",
-  teamsFx.getConfig("TEAMSFX_API_FAKE_API_KEY"),
-  ApiKeyLocation.Header
+const authProvider = new teamsfxSdk.CertificateAuthProvider(
+  // Please add code to read your cert and private key, replace '<your-cert>' and '<your-private-key>' with actual value.
+  // If you have a .pfx cert, you can use 'createPfxCertOption' function to initialize your cert option.
+  teamsfxSdk.createPemCertOption("<your-cert>", "<your-private-key>")
 );
-const fakeClient = createApiClient(teamsFx.getConfig("TEAMSFX_API_FAKE_ENDPOINT"), authProvider);
+const fakeClient = teamsfxSdk.createApiClient(teamsFx.getConfig("API_FAKE_ENDPOINT"), authProvider);
 export { fakeClient };
 
 /* 
@@ -24,7 +24,6 @@ Setting API configuration for cloud environment:
 We have already set the configuration to .env.teamsfx.local based on your answers. 
 Before you deploy your code to cloud using TeamsFx, please follow https://aka.ms/teamsfx-add-appsettings to add following app settings with appropriate value to your Azure environment: 
 TEAMSFX_API_FAKE_ENDPOINT
-TEAMSFX_API_FAKE_API_KEY
 
 You can refer https://aka.ms/teamsfx-connet-api to learn more. 
 */

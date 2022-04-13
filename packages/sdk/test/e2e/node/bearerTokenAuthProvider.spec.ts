4ba0143357bfd435be3a9397a7167f4e3893b2b1
Di Lin <dilin@microsoft.com>
2022-04-13 18:20:19 +0800

    feat(apiconnector): add api key provider (#4615)
    * feat(apiconnector): add api key provider
    
    * docs: update docs for api key provider
    
    * docs: update docs
    
    * test: update api key in query param
    
    * test: add api key test
    
    * test: update package lock json
    
    * test: npx audit fix

--- packages/sdk/test/e2e/node/bearerTokenAuthProvider.spec.ts
+++ packages/sdk/test/e2e/node/bearerTokenAuthProvider.spec.ts
@@ -2,10 +2,10 @@
 // Licensed under the MIT license.
 
 import { assert } from "chai";
-import { BasicAuthProvider, BearerTokenAuthProvider, createApiClient } from "../../../src";
+import { BearerTokenAuthProvider, createApiClient } from "../../../src";
 import * as http from "http";
 
-describe("ApiClient Tests - Node", () => {
+describe("BearerTokenAuthProvider Tests - Node", () => {
   const host = "localhost";
   const port = 53001;
   const apiBaseUrl = `http://${host}:${port}`;
@@ -41,28 +41,5 @@ describe("ApiClient Tests - Node", () => {
     // Assert
     assert.equal(res.data.url, "/foo");
     assert.equal(res.data.requestHeader!["authorization"], "Bearer test-bearer-token");
-  });
-
-  it("can connect to existing API using basic auth provider", async function () {
-    // Arrange
-    const username = "test-username";
-    const password = "test-password";
-    const basicAuthProvider = new BasicAuthProvider(username, password);
-    const apiClient = createApiClient(apiBaseUrl, basicAuthProvider);
-
-    // Act
-    const res = await apiClient.get("/foo");
-
-    // Assert
-    assert.equal(res.data.url, "/foo");
-    const header = res.data.requestHeader!["authorization"] as string;
-    assert.isTrue(header.startsWith("Basic "));
-    const token = header.split(/\s+/).pop() || "";
-    const auth = Buffer.from(token, "base64").toString();
-    const parts = auth.split(/:/);
-    const serverUsername = parts.shift();
-    const serverPassword = parts.join(":");
-    assert.equal(serverUsername, username);
-    assert.equal(serverPassword, password);
   });
 });

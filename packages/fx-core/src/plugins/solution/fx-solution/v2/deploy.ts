82fc22ff53ad04bdb40ee59fb06346fbf35415a5
WANG YEFU <1yefuwang1@gmail.com>
2022-04-18 11:41:31 +0800

    feat: ask for confirmation before deploy (#4715)
    * feat: confirm deployment
    
    * test: fix ut

--- packages/fx-core/src/plugins/solution/fx-solution/v2/deploy.ts
+++ packages/fx-core/src/plugins/solution/fx-solution/v2/deploy.ts
@@ -11,6 +11,7 @@ import {
   SystemError,
   UserError,
   Platform,
+  v3,
 } from "@microsoft/teamsfx-api";
 import { isUndefined } from "lodash";
 import Container from "typedi";
@@ -31,6 +32,7 @@ import {
 } from "../constants";
 import { AzureSolutionQuestionNames } from "../question";
 import { sendErrorTelemetryThenReturnError } from "../utils/util";
+import { askForDeployConsent } from "../v3/provision";
 import { executeConcurrently, NamedThunk } from "./executor";
 import {
   extractSolutionInputs,
@@ -68,6 +70,11 @@ export async function deploy(
         ctx.telemetryReporter
       )
     );
+  }
+
+  const consent = await askForDeployConsent(ctx, envInfo as v3.EnvInfoV3);
+  if (consent.isErr()) {
+    return err(consent.error);
   }
 
   if (!inAzureProject) {
--- packages/fx-core/src/plugins/solution/fx-solution/v2/provision.ts
+++ packages/fx-core/src/plugins/solution/fx-solution/v2/provision.ts
@@ -88,7 +88,7 @@ export async function provisionResource(
       new UserError(
         "Solution",
         SolutionError.TeamsAppTenantIdNotRight,
-        `The signed in M365 account does not match the M365 tenant in config file for '${envInfo.envName}' environment. Please sign out and sign in with the correct M365 account.`
+        getLocalizedString("error.M365AccountNotMatch", envInfo.envName)
       )
     );
   }

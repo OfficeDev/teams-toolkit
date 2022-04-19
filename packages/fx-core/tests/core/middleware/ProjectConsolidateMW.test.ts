34c83028c7843fc503946a1768cd9a1858c43c3a
Tian Yuan <tianyaun@microsoft.com>
2022-04-20 00:00:08 +0800

    chore: update test cases

--- packages/fx-core/tests/core/middleware/ProjectConsolidateMW.test.ts
+++ packages/fx-core/tests/core/middleware/ProjectConsolidateMW.test.ts
@@ -22,7 +22,13 @@ import sinon from "sinon";
 import { setTools } from "../../../src";
 import { ProjectConsolidateMW } from "../../../src/core/middleware/consolidateLocalRemote";
 import { CoreHookContext } from "../../../src/core/types";
-import { MockProjectSettings, MockTools, MockUserInteraction, randomAppName } from "../utils";
+import {
+  MockProjectSettings,
+  MockSPFxProjectSettings,
+  MockTools,
+  MockUserInteraction,
+  randomAppName,
+} from "../utils";
 import * as projectSettingsLoader from "../../../src/core/middleware/projectSettingsLoader";
 import * as projectMigrator from "../../../src/core/middleware/projectMigrator";
 import { environmentManager } from "../../../src";
@@ -72,6 +78,10 @@ describe("Middleware - ProjectSettingsWriterMW", () => {
     sandbox.stub(environmentManager, "writeEnvConfig").resolves(ok(""));
     sandbox.stub(fs, "ensureDir").resolves();
     sandbox.stub(fs, "writeFile").resolves();
+    sandbox.stub(fs, "readFile").resolves(Buffer.from("{}", "utf-8"));
+    sandbox.stub(fs, "copy").resolves();
+    sandbox.stub(fs, "copyFile").resolves();
+    sandbox.stub(fs, "remove").resolves();
     sandbox.stub(projectMigrator, "addPathToGitignore").resolves();
 
     class MyClass {
@@ -94,5 +104,79 @@ describe("Middleware - ProjectSettingsWriterMW", () => {
       projectPath: path.join(os.tmpdir(), appName),
     };
     await my.myMethod(inputs2);
+  });
+
+  it("consolidate spfx happy path", async () => {
+    sandbox.stub(MockUserInteraction.prototype, "showMessage").resolves(ok("Upgrade"));
+    const appName = randomAppName();
+    const projectSettings: ProjectSettings = MockProjectSettings(appName);
+    sandbox.stub(projectSettingsLoader, "loadProjectSettings").resolves(ok(projectSettings));
+    sandbox.stub(environmentManager, "writeEnvConfig").resolves(ok(""));
+    sandbox.stub(fs, "ensureDir").resolves();
+    sandbox.stub(fs, "writeFile").resolves();
+    sandbox.stub(fs, "readFile").resolves(Buffer.from("{}", "utf-8"));
+    sandbox.stub(fs, "copy").resolves();
+    sandbox.stub(fs, "copyFile").resolves();
+    sandbox.stub(fs, "remove").resolves();
+    sandbox.stub(projectMigrator, "addPathToGitignore").resolves();
+
+    class MyClass {
+      async myMethod(inputs: Inputs): Promise<Result<any, FxError>> {
+        return ok("");
+      }
+    }
+
+    hooks(MyClass, {
+      myMethod: [ProjectConsolidateMW],
+    });
+    const my = new MyClass();
+    const inputs1: Inputs = {
+      platform: Platform.VSCode,
+      projectPath: path.join(os.tmpdir(), appName),
+    };
+    await my.myMethod(inputs1);
+    const inputs2: Inputs = {
+      platform: Platform.CLI_HELP,
+      projectPath: path.join(os.tmpdir(), appName),
+    };
+    await my.myMethod(inputs2);
+  });
+
+  it("consolidate happy path", async () => {
+    sandbox.stub(MockUserInteraction.prototype, "showMessage").resolves(ok("Upgrade"));
+    const appName = randomAppName();
+    const projectSettings: ProjectSettings = MockSPFxProjectSettings(appName);
+    sandbox.stub(projectSettingsLoader, "loadProjectSettings").resolves(ok(projectSettings));
+    sandbox.stub(environmentManager, "writeEnvConfig").resolves(ok(""));
+    sandbox.stub(fs, "ensureDir").resolves();
+    sandbox.stub(fs, "writeFile").resolves();
+    sandbox
+      .stub(fs, "readFile")
+      .resolves(
+        Buffer.from(
+          '{"configurableTabs":[{"configurationUrl":"https://{teamSiteDomain}{teamSitePath}/_layouts/15/TeamsLogon.aspx?SPFX=true&dest={teamSitePath}/_layouts/15/teamshostedapp.aspx%3Fteams%26componentId=0626340c-fa14-4681-a976-cedfd24aebc5%26forceLocale={locale}","canUpdateConfiguration":true,"scopes":["team"]}]}',
+          "utf-8"
+        )
+      );
+    sandbox.stub(fs, "copy").resolves();
+    sandbox.stub(fs, "copyFile").resolves();
+    sandbox.stub(fs, "remove").resolves();
+    sandbox.stub(projectMigrator, "addPathToGitignore").resolves();
+
+    class MyClass {
+      async myMethod(inputs: Inputs): Promise<Result<any, FxError>> {
+        return ok("");
+      }
+    }
+
+    hooks(MyClass, {
+      myMethod: [ProjectConsolidateMW],
+    });
+    const my = new MyClass();
+    const inputs1: Inputs = {
+      platform: Platform.VSCode,
+      projectPath: path.join(os.tmpdir(), appName),
+    };
+    await my.myMethod(inputs1);
   });
 });

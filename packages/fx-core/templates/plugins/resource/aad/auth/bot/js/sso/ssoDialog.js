4d5f24d18542609f2ba06a96455c660d5ed6b47c
KennethBWSong <63089166+KennethBWSong@users.noreply.github.com>
2022-04-20 15:33:48 +0800

    fix: fix comment

--- packages/fx-core/templates/plugins/resource/aad/auth/bot/js/sso/ssoDialog.js
+++ packages/fx-core/templates/plugins/resource/aad/auth/bot/js/sso/ssoDialog.js
@@ -101,16 +101,30 @@ class SsoDialog extends ComponentDialog {
   }
 
   async ssoStep(stepContext) {
-    return await stepContext.beginDialog(TEAMS_SSO_PROMPT_ID);
+    try {
+      return await stepContext.beginDialog(TEAMS_SSO_PROMPT_ID);
+    } catch (error) {
+      const context = stepContext.context;
+      await context.sendActivity("Failed to run SSO step");
+      await context.sendActivity(error.message);
+      return await stepContext.endDialog();
+    }
   }
 
   async dedupStep(stepContext) {
-    const tokenResponse = stepContext.result;
-    // Only dedup after ssoStep to make sure that all Teams client would receive the login request
-    if (tokenResponse && (await this.shouldDedup(stepContext.context))) {
-      return Dialog.EndOfTurn;
+    try {
+      const tokenResponse = stepContext.result;
+      // Only dedup after ssoStep to make sure that all Teams client would receive the login request
+      if (tokenResponse && (await this.shouldDedup(stepContext.context))) {
+        return Dialog.EndOfTurn;
+      }
+      return await stepContext.next(tokenResponse);
+    } catch (error) {
+      const context = stepContext.context;
+      await context.sendActivity("Failed to run dedup step");
+      await context.sendActivity(error.message);
+      return await stepContext.endDialog();
     }
-    return await stepContext.next(tokenResponse);
   }
 
   async onEndDialog(context) {

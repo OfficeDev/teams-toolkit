b4c26d1d08e8b62ab340132e3e973eeca18700db
Ning Liu <nliu@microsoft.com>
2022-04-21 16:54:02 +0800

    fix: update wording and message

--- packages/fx-core/src/plugins/resource/appstudio/constants.ts
+++ packages/fx-core/src/plugins/resource/appstudio/constants.ts
@@ -10,6 +10,9 @@ export class Constants {
   public static readonly INCLUDE_APP_MANIFEST = "include-app-manifest";
   public static readonly READ_MORE = "Read more";
   public static readonly LEARN_MORE = "Learn more";
+  public static readonly VIEW_DEVELOPER_PORTAL = "View in Developer Portal";
+  public static readonly DEVELOPER_PORTAL_APP_PACKAGE_URL =
+    "https://dev.teams.microsoft.com/%s/app-package";
   public static readonly ADMIN_PORTAL = "Admin portal";
   public static readonly PUBLISH_GUIDE = "https://aka.ms/teamsfx-publish";
   public static readonly TEAMS_ADMIN_PORTAL = "https://aka.ms/teamsfx-mtac";

669fc6f0844140cb997b668ae45ee46de18e0dcc
Alive-Fish <547850391@qq.com>
2022-04-22 15:13:25 +0800

    Revert "fix(cli): show message error (#4804)"
    This reverts commit 2531dcac606fce60b86df049dc945456162aa77e.

--- packages/cli/src/userInteraction.ts
+++ packages/cli/src/userInteraction.ts
@@ -227,7 +227,7 @@ export class CLIUserInteraction implements UserInteraction {
   private async singleSelect(
     name: string,
     message: string,
-    choices: ChoiceOptions[],
+    choices: string[] | ChoiceOptions[],
     defaultValue?: string,
     validate?: ValidationType<string>
   ): Promise<Result<string, FxError>> {
@@ -239,7 +239,7 @@ export class CLIUserInteraction implements UserInteraction {
   private async multiSelect(
     name: string,
     message: string,
-    choices: ChoiceOptions[],
+    choices: string[] | ChoiceOptions[],
     defaultValue?: string[],
     validate?: ValidationType<string[]>
   ): Promise<Result<string[], FxError>> {
@@ -286,7 +286,10 @@ export class CLIUserInteraction implements UserInteraction {
     return indexes.map((index) => array[index]);
   }
 
-  private toChoices<T>(option: StaticOptions, defaultValue?: T): [ChoiceOptions[], T | undefined] {
+  private toChoices<T>(
+    option: StaticOptions,
+    defaultValue?: T
+  ): [string[] | ChoiceOptions[], T | undefined] {
     if (typeof option[0] === "string") {
       const choices = (option as string[]).map((op) => {
         return {
@@ -370,7 +373,9 @@ export class CLIUserInteraction implements UserInteraction {
       );
       if (result.isOk()) {
         const index = this.findIndex(
-          choices.map((choice) => choice.name),
+          typeof choices[0] === "string"
+            ? (choices as string[])
+            : (choices as ChoiceOptions[]).map((choice) => choice.name),
           result.value
         );
         const anwser = config.options[index];
@@ -404,7 +409,9 @@ export class CLIUserInteraction implements UserInteraction {
       );
       if (result.isOk()) {
         const indexes = this.findIndexes(
-          choices.map((choice) => choice.name),
+          typeof choices[0] === "string"
+            ? (choices as string[])
+            : (choices as ChoiceOptions[]).map((choice) => choice.name),
           result.value
         );
         const anwers = this.getSubArray(config.options as any[], indexes);
@@ -549,15 +556,11 @@ export class CLIUserInteraction implements UserInteraction {
         }
         default: {
           /// default value is set to the first element of items.
-          const [choices, defaultValue] = this.toChoices(
-            modal ? items.concat("Cancel") : items,
-            items[0]
-          );
           const result = await this.singleSelect(
             "MySingleSelectQuestion",
             plainText,
-            choices,
-            defaultValue
+            modal ? items.concat("Cancel") : items,
+            items[0]
           );
           if (result.isOk()) {
             if (result.value !== "Cancel") {

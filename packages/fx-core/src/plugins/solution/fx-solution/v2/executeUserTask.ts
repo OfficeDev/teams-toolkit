854b6ab5ead1a250d2e97c323ee0f8ebcdb73c10
KennethBWSong <63089166+KennethBWSong@users.noreply.github.com>
2022-04-26 16:44:10 +0800

    fix: fix missing success property

--- packages/fx-core/src/plugins/solution/fx-solution/v2/executeUserTask.ts
+++ packages/fx-core/src/plugins/solution/fx-solution/v2/executeUserTask.ts
@@ -1035,6 +1035,7 @@ export async function addSso(
 
   ctx.telemetryReporter.sendTelemetryEvent(SolutionTelemetryEvent.AddSso, {
     [SolutionTelemetryProperty.Component]: SolutionTelemetryComponentName,
+    [SolutionTelemetryProperty.Success]: SolutionTelemetrySuccess.Yes,
   });
 
   return ok({

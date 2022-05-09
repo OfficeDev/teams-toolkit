5e858c041683f0d2e12521fcbc4f96a9720cd79b
Kuojian Lu <kuojianlu@gmail.com>
2022-05-09 15:52:08 +0800

    chore: hide automatic npm install

--- packages/cli/src/cmds/preview/commonUtils.ts
+++ packages/cli/src/cmds/preview/commonUtils.ts
@@ -295,22 +295,23 @@ export function mergeProcessEnv(
 }
 
 export function getAutomaticNpmInstallSetting(): boolean {
-  try {
-    const result = UserSettings.getConfigSync();
-    if (result.isErr()) {
-      throw result.error;
-    }
+  return false;
+  // try {
+  //   const result = UserSettings.getConfigSync();
+  //   if (result.isErr()) {
+  //     throw result.error;
+  //   }
 
-    const config = result.value;
-    const automaticNpmInstallOption = CliConfigOptions.AutomaticNpmInstall;
-    if (!(automaticNpmInstallOption in config)) {
-      return false;
-    }
-    return config[automaticNpmInstallOption] == CliConfigAutomaticNpmInstall.On;
-  } catch (error: any) {
-    cliLogger.warning(`Getting automatic-npm-install setting failed: ${error}`);
-    return false;
-  }
+  //   const config = result.value;
+  //   const automaticNpmInstallOption = CliConfigOptions.AutomaticNpmInstall;
+  //   if (!(automaticNpmInstallOption in config)) {
+  //     return false;
+  //   }
+  //   return config[automaticNpmInstallOption] == CliConfigAutomaticNpmInstall.On;
+  // } catch (error: any) {
+  //   cliLogger.warning(`Getting automatic-npm-install setting failed: ${error}`);
+  //   return false;
+  // }
 }
 
 export async function generateAccountHint(

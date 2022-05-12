cb525d2ce96df1bd80b749247d73e775f4519113
Xiaofu Huang <xiaofhua@microsoft.com>
2022-05-12 11:44:08 +0800

    fix: make isM365 undefined not appear in property

--- packages/cli/src/utils.ts
+++ packages/cli/src/utils.ts
@@ -410,7 +410,7 @@ export function getIsM365(rootFolder: string | undefined): string | undefined {
   try {
     if (isWorkspaceSupported(rootFolder)) {
       const result = readSettingsFileSync(rootFolder);
-      if (result.isOk()) {
+      if (result.isOk() && result.value.isM365) {
         return `${result.value.isM365}`;
       }
     }

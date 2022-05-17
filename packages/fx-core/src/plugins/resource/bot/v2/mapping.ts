436cd2545bc993309cfc887289bbf48c48774795
yukun-dong <dongyukun1995@163.com>
2022-05-17 15:20:24 +0800

    refactor: remove a map

--- packages/fx-core/src/plugins/resource/bot/v2/mapping.ts
+++ packages/fx-core/src/plugins/resource/bot/v2/mapping.ts
@@ -3,12 +3,6 @@
 
 import { ServiceType } from "../../../../common/azure-hosting/interfaces";
 
-export const languageMapping: { [key: string]: string } = {
-  js: "node",
-  ts: "node",
-  csharp: "dotnet",
-};
-
 export const hostServiceTypeMapping: { [key: string]: ServiceType } = {
   "app-service": ServiceType.AppService,
   "azure-functions": ServiceType.Function,

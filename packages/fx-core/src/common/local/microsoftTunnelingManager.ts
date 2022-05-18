7a1f49e0b685345de8bcb908d2aeb51fbdd53955
Aocheng Wang <aochengwang@microsoft.com>
2022-05-18 14:50:47 +0800

    chore: revert unintended commit

--- packages/fx-core/src/common/local/microsoftTunnelingManager.ts
+++ packages/fx-core/src/common/local/microsoftTunnelingManager.ts
@@ -205,7 +205,7 @@ function sleep(millis: number): Promise<void> {
 
 // Tunnels are managed in vscode/cli task and tunnel endpoints are read in solution.
 // So use a global variable to share between task and solution.
-let globalPortEndpoints: Map<number, string>;
-export function getCurrentTunnelPorts(): Map<number, string> {
+let globalPortEndpoints: Map<number, string> | undefined;
+export function getCurrentTunnelPorts(): Map<number, string> | undefined {
   return globalPortEndpoints;
 }

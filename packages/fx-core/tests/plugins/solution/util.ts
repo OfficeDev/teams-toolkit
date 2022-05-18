8b092e21913a507c5df5541e3a5ae9b78b4be564
Ning Tang <nintan@microsoft.com>
2022-05-18 10:07:28 +0800

    chore: fix ut

--- packages/fx-core/tests/plugins/solution/util.ts
+++ packages/fx-core/tests/plugins/solution/util.ts
@@ -194,7 +194,7 @@ export class MockedUserInteraction implements UserInteraction {
 
   async showMessage(
     level: "info" | "warn" | "error",
-    message: string | { content: string; color: Colors }[],
+    message: string | { content: string; color: Colors }[] | { content: string; link?: string }[],
     modal: boolean,
     ...items: string[]
   ): Promise<Result<string, FxError>> {

6c3b1c904c3dbbee4a5418cf311d9cb304d118d3
yukun-dong <dongyukun1995@163.com>
2022-05-19 14:52:42 +0800

    chore: add logic for dotnet post local debug

--- packages/fx-core/src/plugins/resource/bot/dotnet/constants.ts
+++ packages/fx-core/src/plugins/resource/bot/dotnet/constants.ts
@@ -11,4 +11,15 @@ export class PathInfo {
     "bicep"
   );
   public static readonly ProvisionModuleTemplateFileName = "botServiceProvision.template.bicep";
+  static readonly appSettingDevelopment = "appsettings.Development.json";
+}
+
+export class AppSettingsPlaceholders {
+  static readonly botId = "$botId$";
+  static readonly botPassword = "$bot-password$";
+}
+
+export class RegularExpr {
+  static readonly botId = /\$botId\$/g;
+  static readonly botPassword = /\$bot-password\$/g;
 }
--- packages/fx-core/src/plugins/resource/bot/dotnet/plugin.ts
+++ packages/fx-core/src/plugins/resource/bot/dotnet/plugin.ts
@@ -13,8 +13,9 @@ import { FxResult, FxBotPluginResultFactory as ResultFactory } from "../result";
 import { generateBicepFromFile } from "../../../../common/tools";
 import { ArmTemplateResult } from "../../../../common/armInterface";
 import fs from "fs-extra";
-import { PathInfo } from "./constants";
+import { AppSettingsPlaceholders, PathInfo, RegularExpr } from "./constants";
 import { TeamsBotImpl } from "../plugin";
+import { FileIOError } from "../../frontend/dotnet/resources/errors";
 
 // Extends TeamsBotImpl to reuse provision method
 export class DotnetBotImpl extends TeamsBotImpl {
@@ -49,6 +50,22 @@ export class DotnetBotImpl extends TeamsBotImpl {
 
     Logger.info(Messages.SuccessfullyGenerateArmTemplatesBot);
     return ResultFactory.Success(result);
+  }
+
+  public async postLocalDebug(context: PluginContext): Promise<FxResult> {
+    await super.postLocalDebug(context);
+    const appSettingsPath = path.join(context.root, PathInfo.appSettingDevelopment);
+    try {
+      let appSettings = await fs.readFile(appSettingsPath, "utf-8");
+      const botId = AppSettingsPlaceholders.botId;
+      const botPassword = AppSettingsPlaceholders.botPassword;
+      appSettings = appSettings.replace(RegularExpr.botId, botId);
+      appSettings = appSettings.replace(RegularExpr.botPassword, botPassword);
+      await fs.writeFile(appSettingsPath, appSettings, "utf-8");
+    } catch (error) {
+      throw new FileIOError(appSettingsPath);
+    }
+    return ResultFactory.Success();
   }
 
   // Overwrite below lifecycle for dotnet scenario

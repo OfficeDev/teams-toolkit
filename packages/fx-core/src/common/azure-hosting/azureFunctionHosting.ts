52e74b796deab368b67d83127179c3a14b38072e
zhijie <zhihuan@microsoft.com>
2022-05-27 10:32:45 +0800

    chore: add log message

--- packages/fx-core/src/common/azure-hosting/azureFunctionHosting.ts
+++ packages/fx-core/src/common/azure-hosting/azureFunctionHosting.ts
@@ -7,6 +7,7 @@ import { TokenProvider, Void } from "@microsoft/teamsfx-api";
 import { azureWebSiteDeploy } from "./utils";
 import { AzureOperations } from "./azureOps";
 import { getResourceGroupNameFromResourceId, getSiteNameFromResourceId } from "../tools";
+import { Messages } from "./messages";
 
 const functionResourceId = "provisionOutputs.functionOutput.value.botWebAppResourceId";
 const functionHostName = "provisionOutputs.functionOutput.value.validDomain";
@@ -25,12 +26,13 @@ export class AzureFunctionHosting extends AzureHosting {
 
   async deploy(resourceId: string, tokenProvider: TokenProvider, buffer: Buffer): Promise<Void> {
     await super.deploy(resourceId, tokenProvider, buffer);
-    const client = await azureWebSiteDeploy(resourceId, tokenProvider, buffer);
+    const client = await azureWebSiteDeploy(resourceId, tokenProvider, buffer, this.logger);
 
     await AzureOperations.restartWebApp(
       client,
       getResourceGroupNameFromResourceId(resourceId),
-      getSiteNameFromResourceId(resourceId)
+      getSiteNameFromResourceId(resourceId),
+      this.logger
     );
     return Void;
   }

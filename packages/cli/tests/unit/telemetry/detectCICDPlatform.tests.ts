423e18639e6ff49e963054b793b6176a59badb7c
Ivan He <ruhe@microsoft.com>
2022-06-09 15:06:29 +0800

    chore: wip

---
+++ packages/cli/tests/unit/telemetry/detectCICDPlatform.tests.ts
@@ -0,0 +1,56 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import sinon from "sinon";
+import { describe } from "yargs";
+import { tryDetectCICDPlatform } from "../../../src/commonlib/common/cicdPlatformDetector";
+import { CliConfigRunFrom } from "../../../src/userSetttings";
+
+import { expect } from "../utils";
+
+describe("Detect CI/CD Platforms", () => {
+  describe("Detect CI/CD Platforms", () => {
+    const sandbox = sinon.createSandbox();
+
+    afterEach(() => {
+      sandbox.restore();
+    });
+
+    it("No CI/CD Platform Detected", () => {
+      // Arrange
+      // Act
+      const plat = tryDetectCICDPlatform();
+      // Assert
+      expect(plat).to.be.equals(CliConfigRunFrom.Other);
+    });
+
+    it("GitHub Detected", () => {
+      // Arrange
+      sandbox.stub(process.env, "GITHUB_ACTIONS").value("true");
+      // Act
+      const plat = tryDetectCICDPlatform();
+      // Assert
+      expect(plat).to.be.equals(CliConfigRunFrom.GitHub);
+    });
+
+    it("Azure DevOps Detected", () => {
+      // Arrange
+      sandbox.stub(process.env, "BUILD_SOURCEBRANCHNAME").value("anything");
+      sandbox.stub(process.env, "AGENT_BUILDDIRECTORY").value("anything");
+      // Act
+      const plat = tryDetectCICDPlatform();
+      // Assert
+      expect(plat).to.be.equals(CliConfigRunFrom.AzDo);
+    });
+
+    it("Jenkins Detected", () => {
+      // Arrange
+      sandbox.stub(process.env, "JENKINS_URL").value("anything");
+      sandbox.stub(process.env, "BUILD_URL").value("anything");
+      // Act
+      const plat = tryDetectCICDPlatform();
+      // Assert
+      expect(plat).to.be.equals(CliConfigRunFrom.Jenkins);
+    });
+  });
+});

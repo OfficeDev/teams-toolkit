2d30536c4d876a7a18cc3c257d1474c59f84b5d9
Zhaofeng Xu <zhaofengxu@microsoft.com>
2022-06-10 09:57:06 +0800

    chore(v3): update

--- packages/fx-core/src/component/resource/azureSql/utils/checkInput.ts
+++ packages/fx-core/src/component/resource/azureSql/utils/checkInput.ts
@@ -2,7 +2,6 @@
 // Licensed under the MIT license.
 
 import { getLocalizedString } from "../../../../common/localizeUtils";
-import { Message } from "./message";
 export function sqlUserNameValidator(name: string): string | undefined {
   name = name?.trim();
 

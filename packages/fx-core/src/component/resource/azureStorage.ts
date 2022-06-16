009656ebb0664c0b827812e9789e1650d849e008
Huajie Zhang <huajiezhang@microsoft.com>
2022-06-16 15:30:18 +0800

    chore(v3): storage deploy (#5305)
    * feat: tab
    
    * feat: tab build
    
    * feat: tab build
    
    * feat: clean
    
    * feat: storage deploy
    
    * fix: ut

--- packages/fx-core/src/component/resource/azureStorage.ts
+++ packages/fx-core/src/component/resource/azureStorage.ts
@@ -3,18 +3,33 @@
 
 import {
   Action,
+  AzureAccountProvider,
   Bicep,
   CloudResource,
   ContextV3,
+  err,
   FxError,
   InputsWithProjectPath,
   MaybePromise,
   ok,
+  ProvisionContextV3,
   Result,
+  UserError,
+  v3,
 } from "@microsoft/teamsfx-api";
 import "reflect-metadata";
 import { Service } from "typedi";
-
+import * as path from "path";
+import { FrontendConfig } from "../../plugins/resource/frontend/configs";
+import { ComponentNames } from "../constants";
+import {
+  getResourceGroupNameFromResourceId,
+  getStorageAccountNameFromResourceId,
+  getSubscriptionIdFromResourceId,
+} from "../../common/tools";
+import { UnauthenticatedError } from "../../plugins/resource/frontend/v3/error";
+import { AzureStorageClient } from "../../plugins/resource/frontend/clients";
+import { FrontendDeployment } from "../../plugins/resource/frontend/ops/deploy";
 @Service("azure-storage")
 export class AzureStorageResource implements CloudResource {
   readonly name = "azure-storage";
@@ -22,6 +37,14 @@ export class AzureStorageResource implements CloudResource {
     endpoint: {
       key: "endpoint",
       bicepVariable: "provisionOutputs.azureStorageOutput.value.endpoint",
+    },
+    resourceId: {
+      key: "resourceId",
+      bicepVariable: "provisionOutputs.azureStorageOutput.value.resourceId",
+    },
+    location: {
+      key: "location",
+      bicepVariable: "provisionOutputs.azureStorageOutput.value.location",
     },
   };
   readonly finalOutputKeys = ["endpoint"];
@@ -89,25 +112,66 @@ export class AzureStorageResource implements CloudResource {
       name: "azure-storage.deploy",
       type: "function",
       plan: (context: ContextV3, inputs: InputsWithProjectPath) => {
-        const deployInputs = inputs["azure-storage"];
+        const deployDir = path.join(inputs.projectPath, inputs.folder);
         return ok([
           {
             type: "service",
             name: "azure",
-            remarks: `deploy azure storage with path: ${deployInputs.folder}, type: ${deployInputs.type}`,
+            remarks: `deploy azure storage with path: ${deployDir}`,
           },
         ]);
       },
       execute: async (context: ContextV3, inputs: InputsWithProjectPath) => {
+        const ctx = context as ProvisionContextV3;
+        const deployDir = path.join(inputs.projectPath, inputs.folder);
+        const frontendConfigRes = await buildFrontendConfig(
+          ctx.envInfo,
+          ctx.tokenProvider.azureAccountProvider
+        );
+        if (frontendConfigRes.isErr()) {
+          return err(frontendConfigRes.error);
+        }
+        const client = new AzureStorageClient(frontendConfigRes.value);
+        const envName = ctx.envInfo.envName;
+        await FrontendDeployment.doFrontendDeploymentV3(client, deployDir, envName);
         return ok([
           {
             type: "service",
             name: "azure",
-            remarks: `deploy azure storage with path: ${inputs.folder}, type: ${inputs.type}`,
+            remarks: `deploy azure storage with path: ${deployDir}`,
           },
         ]);
       },
     };
     return ok(action);
   }
+}
+
+async function buildFrontendConfig(
+  envInfo: v3.EnvInfoV3,
+  tokenProvider: AzureAccountProvider
+): Promise<Result<FrontendConfig, FxError>> {
+  const credentials = await tokenProvider.getAccountCredentialAsync();
+  if (!credentials) {
+    return err(new UnauthenticatedError());
+  }
+  const storage = envInfo.state[ComponentNames.AzureStorage];
+  const resourceId = storage?.resourceId;
+  if (!resourceId) {
+    return err(
+      new UserError({
+        source: "azure-storage",
+        name: "StateValueMissingError",
+        message: "Missing resourceIf for storage",
+      })
+    );
+  }
+  const frontendConfig = new FrontendConfig(
+    getSubscriptionIdFromResourceId(resourceId),
+    getResourceGroupNameFromResourceId(resourceId),
+    (envInfo.state.solution as v3.AzureSolutionConfig).location,
+    getStorageAccountNameFromResourceId(resourceId),
+    credentials
+  );
+  return ok(frontendConfig);
 }

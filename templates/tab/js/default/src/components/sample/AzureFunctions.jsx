47010df525fd0d13e86dd7100a1a55a120a19546
blackchoey <blackchoey@gmail.com>
2022-06-21 09:18:20 +0800

    chore: add comments for the code

--- templates/tab/js/default/src/components/sample/AzureFunctions.jsx
+++ templates/tab/js/default/src/components/sample/AzureFunctions.jsx
@@ -13,6 +13,7 @@ async function callFunction(teamsfx) {
   try {
     const credential = teamsfx.getCredential();
     const apiBaseUrl = teamsfx.getConfig("apiEndpoint") + "/api/";
+    // createApiClient(...) creates an Axios instance which uses BearerTokenAuthProvider to inject token to request header
     const apiClient = createApiClient(
       apiBaseUrl,
       new BearerTokenAuthProvider(async () => (await credential.getToken("")).token));

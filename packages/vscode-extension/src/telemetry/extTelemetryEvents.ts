c4ff82123e1f5e4fbe003f3a99a187eb6887f479
Alex Wang <a1exwang@users.noreply.github.com>
2022-06-22 14:42:30 +0800

    fix: debug-check-results are redacted sometimes (#5337)
    * fix: debug-check-results are redacted sometimes
    
    * fix: fix undefined error
    
    * chore: do not mix with existing debug-check-results data

--- packages/vscode-extension/src/telemetry/extTelemetryEvents.ts
+++ packages/vscode-extension/src/telemetry/extTelemetryEvents.ts
@@ -215,6 +215,7 @@ export enum TelemetryProperty {
   DebugProjectComponents = "debug-project-components",
   DebugDevCertStatus = "debug-dev-cert-status",
   DebugCheckResults = "debug-check-results",
+  DebugCheckResultsSafe = "debug-check-results-safe",
   DebugErrorCodes = "debug-error-codes",
   DebugNpmInstallName = "debug-npm-install-name",
   DebugNpmInstallExitCode = "debug-npm-install-exit-code",

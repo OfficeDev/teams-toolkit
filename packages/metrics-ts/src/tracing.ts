2115b3c114ea41f3f7d3172d749d498edec391d5
LongOddCode <haolong@microsoft.com>
2022-06-23 16:41:39 +0800

    chore: use uuid

--- packages/metrics-ts/src/tracing.ts
+++ packages/metrics-ts/src/tracing.ts
@@ -1,5 +1,5 @@
-import { nanoid } from "nanoid";
 import { env } from "process";
+import { v4 as uuidv4 } from "uuid";
 
 /**
  * TODO: make it safe
@@ -8,7 +8,7 @@ export function traceId(): string {
   if (env.METAOS_TRACE_ID) {
     return env.METAOS_TRACE_ID;
   } else {
-    const traceId = nanoid();
+    const traceId = uuidv4();
     env.METAOS_TRACE_ID = traceId;
     return traceId;
   }

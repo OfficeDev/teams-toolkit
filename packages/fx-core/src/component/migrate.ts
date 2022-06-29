8ed6c85092ccb3c962228d7188c77363d3282f0e
huajiezhang <huajiezhang@microsoft.com>
2022-06-29 16:45:35 +0800

    feat: migration

--- packages/fx-core/src/component/migrate.ts
+++ packages/fx-core/src/component/migrate.ts
@@ -93,6 +93,18 @@ export function migrateEnvStateToV2(settings: ProjectSettingsV3, envStateV3: Jso
       envStateV2["fx-resource-identity"] = envStateV3[key];
     } else if (key === ComponentNames.AzureSQL) {
       envStateV2["fx-resource-azure-sql"] = envStateV3[key];
+    } else if (key === ComponentNames.AadApp) {
+      envStateV2["fx-resource-aad-app-for-teams"] = envStateV3[key];
+    } else if (key === ComponentNames.Function) {
+      envStateV2["fx-resource-function"] = envStateV2["fx-resource-function"] || {};
+      envStateV2["fx-resource-function"].functionAppResourceId =
+        envStateV3[ComponentNames.Function].resourceId;
+      envStateV2["fx-resource-function"].functionEndpoint =
+        envStateV3[ComponentNames.Function].endpoint;
+    } else if (key === ComponentNames.APIM) {
+      envStateV2["fx-resource-apim"] = envStateV3[key];
+    } else if (key === ComponentNames.KeyVault) {
+      envStateV2["fx-resource-key-vault"] = envStateV3[key];
     } else if (key === ComponentNames.AzureWebApp) {
       const components = settings.components.filter(
         (c) => c.hosting === ComponentNames.AzureWebApp
@@ -100,6 +112,7 @@ export function migrateEnvStateToV2(settings: ProjectSettingsV3, envStateV3: Jso
       if (components.length > 0) {
         const component = components[0];
         if (component.name === ComponentNames.TeamsBot) {
+          // web app for bot
           envStateV2["fx-resource-bot"] = envStateV2["fx-resource-bot"] || {};
           envStateV2["fx-resource-bot"].botId = envStateV3[ComponentNames.BotService].botId;
           envStateV2["fx-resource-bot"].botPassword =
@@ -115,16 +128,29 @@ export function migrateEnvStateToV2(settings: ProjectSettingsV3, envStateV3: Jso
             envStateV3[ComponentNames.AzureWebApp].resourceId;
           envStateV2["fx-resource-bot"].siteEndpoint =
             envStateV3[ComponentNames.AzureWebApp].endpoint;
+        } else {
+          //  web app for tab
         }
       }
-    } else if (key === ComponentNames.AadApp) {
-      envStateV2["fx-resource-aad-app-for-teams"] = envStateV3[key];
-    } else if (key === ComponentNames.Function) {
-      envStateV2["fx-resource-function"] = envStateV2["fx-resource-function"] || {};
-      envStateV2["fx-resource-function"].functionAppResourceId =
-        envStateV3[ComponentNames.AzureWebApp].resourceId;
-      envStateV2["fx-resource-function"].functionEndpoint =
-        envStateV3[ComponentNames.AzureWebApp].endpoint;
+    } else if (key === ComponentNames.AzureStorage) {
+      const components = settings.components.filter(
+        (c) => c.hosting === ComponentNames.AzureStorage
+      );
+      if (components.length > 0) {
+        const component = components[0];
+        if (component.name === ComponentNames.TeamsTab) {
+          envStateV2["fx-resource-frontend-hosting"] =
+            envStateV2["fx-resource-frontend-hosting"] || {};
+          envStateV2["fx-resource-frontend-hosting"].domain =
+            envStateV3[ComponentNames.AzureStorage].domain;
+          envStateV2["fx-resource-frontend-hosting"].endpoint =
+            envStateV3[ComponentNames.AzureStorage].endpoint;
+          envStateV2["fx-resource-frontend-hosting"].indexPath =
+            envStateV3[ComponentNames.AzureStorage].indexPath;
+          envStateV2["fx-resource-frontend-hosting"].storageResourceId =
+            envStateV3[ComponentNames.AzureStorage].resourceId;
+        }
+      }
     }
   }
 }

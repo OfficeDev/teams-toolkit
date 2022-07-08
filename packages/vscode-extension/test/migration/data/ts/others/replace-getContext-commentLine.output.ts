dbe30ca1f72c6bae8269071a1601673af3494860
HuihuiWu-Microsoft <73154171+HuihuiWu-Microsoft@users.noreply.github.com>
2022-07-08 10:04:59 +0800

    chore: setup vsc extension unit test framework to allow test run without extension host (#5394)
    * chore: refactor to use nyc for extension code coverage report
    
    * chore: set coverage threshold for vscode extension
    
    * chore: block when coverage is low
    
    * chore: test block ut if coverage failed
    
    * chore: print coverage log
    
    * chore: try to print coverage log
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * test: setup unit test framework without extension host
    
    * fix: fix all unit tests fail
    
    * test: unify all unit tests
    
    * chore: ignore mocha and nyc files
    
    * chore: update codeowners file

---
+++ packages/vscode-extension/test/migration/data/ts/others/replace-getContext-commentLine.output.ts
@@ -0,0 +1,18 @@
+import * as microsoftTeams from "@microsoft/teams-js";
+function myFunc() {
+  //TODO: Convert callback to promise, for more info, please refer to https://aka.ms/teamsfx-callback-to-promise.
+  //TODO: Change the context interface, for more info, please refer to https://aka.ms/teamsfx-context-mapping.
+  const result = microsoftTeams.app.getContext(123);
+  if (1 === 1) {
+    let x = undefined;
+    //TODO: Convert callback to promise, for more info, please refer to https://aka.ms/teamsfx-callback-to-promise.
+    //TODO: Change the context interface, for more info, please refer to https://aka.ms/teamsfx-context-mapping.
+    x = microsoftTeams.app.getContext(123);
+    //TODO: Convert callback to promise, for more info, please refer to https://aka.ms/teamsfx-callback-to-promise.
+    //TODO: Change the context interface, for more info, please refer to https://aka.ms/teamsfx-context-mapping.
+    microsoftTeams.app.getContext(123);
+  }
+}
+//TODO: Convert callback to promise, for more info, please refer to https://aka.ms/teamsfx-callback-to-promise.
+//TODO: Change the context interface, for more info, please refer to https://aka.ms/teamsfx-context-mapping.
+microsoftTeams.app.getContext(123);

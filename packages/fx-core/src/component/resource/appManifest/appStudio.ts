f41b9edfe71ab98c0414f586a617ecf48bcfef71
Huajie Zhang <huajiezhang@microsoft.com>
2022-07-14 09:52:08 +0800

    chore(v3): migration aad manifest (#5516)
    * feat: improve migration
    
    * feat: up
    
    * fix: ut
    
    * feat: remove triggers in project settings
    
    * fix: bug
    
    * feat: up
    
    * feat: add feature question
    
    * feat: add feature question
    
    * feat: up
    
    * feat: parity with v1 for create project question
    
    * feat: up
    
    * fix: ut
    
    * feat: aad manifest migration
    
    * fix: app studio

--- packages/fx-core/src/component/resource/appManifest/appStudio.ts
+++ packages/fx-core/src/component/resource/appManifest/appStudio.ts
@@ -36,7 +36,8 @@ export async function createOrUpdateTeamsApp(
   ctx: v2.Context,
   inputs: InputsWithProjectPath,
   envInfo: v3.EnvInfoV3,
-  tokenProvider: TokenProvider
+  tokenProvider: TokenProvider,
+  withEmptyCapabilities = false
 ): Promise<Result<string, FxError>> {
   const appStudioTokenRes = await tokenProvider.m365TokenProvider.getAccessToken({
     scopes: AppStudioScopes,
@@ -59,7 +60,11 @@ export async function createOrUpdateTeamsApp(
     }
     archivedFile = await fs.readFile(inputs.appPackagePath);
   } else {
-    const buildPackage = await buildTeamsAppPackage(inputs.projectPath, envInfo!, true);
+    const buildPackage = await buildTeamsAppPackage(
+      inputs.projectPath,
+      envInfo!,
+      withEmptyCapabilities
+    );
     if (buildPackage.isErr()) {
       return err(buildPackage.error);
     }

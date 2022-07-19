86d2482e2afed2fb3907482e23aa1c0002705cf3
huajiezhang <huajiezhang@microsoft.com>
2022-07-19 23:16:27 +0800

    test: ut

--- packages/fx-core/tests/component/questionv3.test.ts
+++ packages/fx-core/tests/component/questionv3.test.ts
@@ -6,6 +6,7 @@ import sinon from "sinon";
 import {
   getQuestionsForAddFeatureSubCommand,
   getQuestionsForAddFeatureV3,
+  getQuestionsForAddResourceV3,
   getQuestionsForDeployV3,
 } from "../../src/component/questionV3";
 import * as manifestUtils from "../../src/component/resource/appManifest/utils";
@@ -18,7 +19,9 @@ import {
 } from "@microsoft/teamsfx-api";
 import { createContextV3 } from "../../src/component/utils";
 import { newEnvInfoV3 } from "../../src";
-import { FeatureId } from "../../build/component/questionV3";
+import { FeatureId } from "../../src/component/questionV3";
+import "../../src/component/core";
+
 describe("question for v3", () => {
   const sandbox = sinon.createSandbox();
   afterEach(() => {
@@ -120,6 +123,48 @@ describe("question for v3", () => {
     };
     const context = createContextV3(projectSettings);
     const res = await getQuestionsForAddFeatureV3(context, inputs);
+    assert.isTrue(res.isOk());
+  });
+  it("getQuestionsForAddResourceV3 - CLI_HELP", async () => {
+    const context = createContextV3();
+    const inputs: InputsWithProjectPath = {
+      platform: Platform.CLI_HELP,
+      projectPath: ".",
+    };
+    const res = await getQuestionsForAddResourceV3(context, inputs);
+    assert.isTrue(res.isOk());
+  });
+  it("getQuestionsForAddResourceV3 - VS Code", async () => {
+    const projectSettings = {
+      appName: "hj070701",
+      projectId: "112233",
+      version: "2.1.0",
+      isFromSample: false,
+      components: [
+        {
+          name: "teams-bot",
+          hosting: "azure-function",
+          capabilities: ["notification"],
+          build: true,
+          folder: "bot",
+        },
+        {
+          name: "azure-function",
+          connections: ["teams-bot"],
+        },
+        {
+          name: "bot-service",
+          provision: true,
+        },
+      ],
+      programmingLanguage: "javascript",
+    };
+    const inputs: InputsWithProjectPath = {
+      platform: Platform.VSCode,
+      projectPath: ".",
+    };
+    const context = createContextV3(projectSettings);
+    const res = await getQuestionsForAddResourceV3(context, inputs);
     assert.isTrue(res.isOk());
   });
   it("getQuestionsForAddFeatureSubCommand", async () => {

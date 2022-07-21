6e2fddc8db85afb0fb0d7c23d4cbfac065dc0488
Bowen Song <bowsong@microsoft.com>
2022-07-21 16:46:17 +0800

    perf: enable non sso tab template for dotnet (#5585)

--- packages/fx-core/src/plugins/resource/frontend/dotnet/resources/templateInfo.ts
+++ packages/fx-core/src/plugins/resource/frontend/dotnet/resources/templateInfo.ts
@@ -6,14 +6,15 @@ export class TemplateInfo {
   scenario: string;
   variables: TemplateVariable;
 
-  constructor(templateVariable: TemplateVariable) {
+  constructor(templateVariable: TemplateVariable, scenario?: string) {
     this.group = TemplateInfo.DefaultGroup;
     this.language = TemplateInfo.DonetLanguage;
-    this.scenario = TemplateInfo.DefaultScenario;
+    this.scenario = scenario ?? TemplateInfo.DefaultScenario;
     this.variables = templateVariable;
   }
 
   static readonly DefaultGroup = "tab";
   static readonly DonetLanguage = "csharp";
   static readonly DefaultScenario = "default";
+  static readonly NoneSsoScenario = "non-sso";
 }

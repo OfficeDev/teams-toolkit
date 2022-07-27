0caa6f63688af659b975447419422acdb0832cf3
Yuqi Zhou <yuqzho@microsoft.com>
2022-07-27 14:14:17 +0800

    chore: test

---
+++ packages/cli/tests/e2e/solution/ProvisionWithInvalidSubscriptionInfoJsonContent.tests.ts
@@ -0,0 +1,68 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+/**
+ * @author Yuqi Zhou <yuqzho@microsoft.com>
+ */
+
+import path from "path";
+import { environmentManager, getUuid } from "@microsoft/teamsfx-core";
+import {
+  getSubscriptionId,
+  getTestFolder,
+  getUniqueAppName,
+  cleanUp,
+  setBotSkuNameToB1Bicep,
+  setSimpleAuthSkuNameToB1Bicep,
+  validateTabAndBotProjectProvision,
+  execAsync,
+  getActivePluginsFromProjectSetting,
+  getCapabilitiesFromProjectSetting,
+} from "../commonUtils";
+import { CliHelper } from "../../commonlib/cliHelper";
+import { Capability, PluginId, ProjectSettingKey, TestFilePath } from "../../commonlib/constants";
+import fs from "fs-extra";
+import { expect } from "chai";
+import { SubscriptionInfo } from "@microsoft/teamsfx-api";
+import { FrontendValidator } from "../../commonlib";
+
+describe("Provision with invalid subscription info", () => {
+  const testFolder = getTestFolder();
+  const appName = getUniqueAppName();
+  const projectPath = path.resolve(testFolder, appName);
+
+  const env = Object.assign({}, process.env);
+
+  after(async () => {
+    await cleanUp(appName, projectPath, true, true, false);
+  });
+
+  it("Provision non SSO Tab project", async () => {
+    // Arrange
+    await CliHelper.createProjectWithCapability(appName, testFolder, Capability.TabNonSso, env);
+
+    // Assert
+    const capabilities = await getCapabilitiesFromProjectSetting(projectPath);
+    expect(capabilities.includes(Capability.TabNonSso)).to.be.true;
+
+    const subscriptionInfoJsonFilePath = path.join(projectPath, ".fx/subscriptionInfo.json");
+    expect(await fs.pathExists(subscriptionInfoJsonFilePath)).to.be.true;
+
+    // Arrange
+    const subscriptionInfo: SubscriptionInfo = {
+      subscriptionName: "test",
+      subscriptionId: "b91424c7-bd0f-45a1-91e7-d8916efbbcdc",
+      tenantId: "b91424c7-bd0f-45a1-91e7-d8916efbbcdc",
+    };
+    fs.writeJSON(subscriptionInfoJsonFilePath, JSON.stringify(subscriptionInfo, null, 4));
+
+    await CliHelper.provisionProject(projectPath, "", env);
+
+    // Assert
+    const context = await fs.readJSON(`${projectPath}/.fx/states/state.dev.json`);
+
+    // Validate Tab Frontend
+    const frontend = FrontendValidator.init(context);
+    await FrontendValidator.validateProvision(frontend);
+  });
+});

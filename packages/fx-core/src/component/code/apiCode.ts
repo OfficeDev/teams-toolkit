d04075f0efb0ea5c326912d8068a5fdcc31c6716
zhijie <zhihuan@microsoft.com>
2022-08-01 17:24:43 +0800

    chore: pre check for function deploy

--- packages/fx-core/src/component/code/apiCode.ts
+++ packages/fx-core/src/component/code/apiCode.ts
@@ -14,16 +14,25 @@ import {
 import * as path from "path";
 import "reflect-metadata";
 import { Service } from "typedi";
+import { DepsChecker, DepsType } from "../../common/deps-checker/depsChecker";
+import { CheckerFactory } from "../../common/deps-checker/checkerFactory";
 import { CoreQuestionNames } from "../../core/question";
 import { DefaultValues, FunctionPluginPathInfo } from "../../plugins/resource/function/constants";
 import { FunctionLanguage, QuestionKey } from "../../plugins/resource/function/enums";
 import { FunctionDeploy } from "../../plugins/resource/function/ops/deploy";
 import { FunctionScaffold } from "../../plugins/resource/function/ops/scaffold";
+import { funcDepsHelper } from "../../plugins/resource/function/utils/depsChecker/funcHelper";
 import { ComponentNames } from "../constants";
 import { BadComponent, invalidProjectSettings } from "../error";
 import { ErrorMessage, ProgressMessages, ProgressTitles } from "../messages";
 import { ActionExecutionMW } from "../middleware/actionExecutionMW";
 import { getComponent } from "../workflow";
+import { TelemetryHelper } from "../../plugins/resource/function/utils/telemetry-helper";
+import { Logger } from "../../plugins/resource/function/utils/logger";
+import { funcDepsLogger } from "../../plugins/resource/function/utils/depsChecker/funcPluginLogger";
+import { funcDepsTelemetry } from "../../plugins/resource/function/utils/depsChecker/funcPluginTelemetry";
+import { LinuxNotSupportedError } from "../../common";
+import { InfoMessages } from "../../plugins/resource/function/resources/message";
 /**
  * api scaffold
  */
@@ -87,9 +96,51 @@ export class ApiCodeProvider {
     const language = context.projectSetting.programmingLanguage;
     if (!language || !Object.values(FunctionLanguage).includes(language as FunctionLanguage))
       throw new invalidProjectSettings(ErrorMessage.programmingLanguageInvalid);
-    actionContext?.progressBar?.next(ProgressMessages.buildingApi);
     const buildPath = path.resolve(inputs.projectPath, teamsApi.folder);
+
+    await this.handleDotnetChecker(context, inputs);
+    try {
+      await FunctionDeploy.installFuncExtensions(buildPath, language as FunctionLanguage);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        // wrap the original error to UserError so the extensibility model will pop-up a dialog correctly
+        throw funcDepsHelper.transferError(error);
+      } else {
+        throw error;
+      }
+    }
+
+    actionContext?.progressBar?.next(ProgressMessages.buildingApi);
     await FunctionDeploy.build(buildPath, language as FunctionLanguage);
     return ok(undefined);
+  }
+
+  private async handleDotnetChecker(ctx: ContextV3, inputs: InputsWithProjectPath): Promise<void> {
+    Logger.setLogger(ctx.logProvider);
+    TelemetryHelper.setContext(ctx);
+    const dotnetChecker: DepsChecker = CheckerFactory.createChecker(
+      DepsType.Dotnet,
+      funcDepsLogger,
+      funcDepsTelemetry
+    );
+    try {
+      if (!(await funcDepsHelper.dotnetCheckerEnabled(inputs))) {
+        return;
+      }
+      await dotnetChecker.resolve();
+    } catch (error) {
+      if (error instanceof LinuxNotSupportedError) {
+        return;
+      }
+      if (error instanceof Error) {
+        funcDepsLogger.error(InfoMessages.failedToInstallDotnet(error));
+        await funcDepsLogger.printDetailLog();
+        throw funcDepsHelper.transferError(error);
+      } else {
+        throw error;
+      }
+    } finally {
+      funcDepsLogger.cleanup();
+    }
   }
 }

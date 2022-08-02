75481b27d6928c5314415b5419ef767ce7d4c371
huajiezhang <huajiezhang@microsoft.com>
2022-08-02 17:05:06 +0800

    feat: function e2e

---
+++ packages/cli/tests/e2e/function/TabAddFunctionKeyvault_v3.tests.ts
@@ -0,0 +1,60 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+/**
+ * @author Di Lin <dilin@microsoft.com>
+ */
+
+import path from "path";
+import "mocha";
+
+import {
+  getSubscriptionId,
+  getTestFolder,
+  getUniqueAppName,
+  cleanUp,
+  setSimpleAuthSkuNameToB1Bicep,
+  readContextMultiEnv,
+} from "../commonUtils";
+import { environmentManager } from "@microsoft/teamsfx-core";
+import { CliHelper } from "../../commonlib/cliHelper";
+import { Capability, Resource } from "../../commonlib/constants";
+import { FunctionValidator } from "../../commonlib";
+import mockedEnv from "mocked-env";
+describe("Configuration successfully changed when with different plugins", function () {
+  const testFolder = getTestFolder();
+  const subscription = getSubscriptionId();
+  const appName = getUniqueAppName();
+  const projectPath = path.resolve(testFolder, appName);
+  const env = environmentManager.getDefaultEnvName();
+  let mockedEnvRestore: () => void;
+  before(async () => {
+    mockedEnvRestore = mockedEnv({
+      TEAMSFX_APIV3: "true",
+    });
+  });
+  after(async () => {
+    await cleanUp(appName, projectPath, true, false, false);
+    mockedEnvRestore();
+  });
+
+  it(`tab + function + key vault`, async function () {
+    await CliHelper.createProjectWithCapability(appName, testFolder, Capability.Tab);
+    await CliHelper.addResourceToProject(projectPath, Resource.AzureFunction);
+    await CliHelper.addResourceToProject(projectPath, Resource.AzureKeyVault);
+
+    // Provision
+    await setSimpleAuthSkuNameToB1Bicep(projectPath, env);
+    await CliHelper.setSubscription(subscription, projectPath);
+    await CliHelper.provisionProject(projectPath);
+
+    // Assert
+    {
+      const context = await readContextMultiEnv(projectPath, env);
+
+      // Validate Function App
+      const functionValidator = new FunctionValidator(context, projectPath, env);
+      await functionValidator.validateProvision();
+    }
+  });
+});

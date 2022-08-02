01bdf507d0e429824198784390106376dc093792
Huajie Zhang <huajiezhang@microsoft.com>
2022-08-02 12:38:26 +0800

    feat: fix apim provision (#5673)

--- packages/fx-core/src/component/resource/apim.ts
+++ packages/fx-core/src/component/resource/apim.ts
@@ -35,20 +35,24 @@ export class APIMResource extends AzureResource {
     context: ResourceContextV3,
     inputs: InputsWithProjectPath
   ): Promise<Result<Action | undefined, FxError>> {
-    const ctx = context as ResourceContextV3;
-    const apimV3 = Container.get<ApimPluginV3>(BuiltInFeaturePluginNames.apim);
-    const res = await apimV3.provisionResource(ctx, inputs, ctx.envInfo, ctx.tokenProvider);
-    if (res.isErr()) return err(res.error);
+    if (context.envInfo.envName !== "local") {
+      const ctx = context as ResourceContextV3;
+      const apimV3 = Container.get<ApimPluginV3>(BuiltInFeaturePluginNames.apim);
+      const res = await apimV3.provisionResource(ctx, inputs, ctx.envInfo, ctx.tokenProvider);
+      if (res.isErr()) return err(res.error);
+    }
     return ok(undefined);
   }
   async configure(
     context: ResourceContextV3,
     inputs: InputsWithProjectPath
   ): Promise<Result<undefined, FxError>> {
-    const ctx = context as ResourceContextV3;
-    const apimV3 = Container.get<ApimPluginV3>(BuiltInFeaturePluginNames.apim);
-    const res = await apimV3.configureResource(ctx, inputs, ctx.envInfo, ctx.tokenProvider);
-    if (res.isErr()) return err(res.error);
+    if (context.envInfo.envName !== "local") {
+      const ctx = context as ResourceContextV3;
+      const apimV3 = Container.get<ApimPluginV3>(BuiltInFeaturePluginNames.apim);
+      const res = await apimV3.configureResource(ctx, inputs, ctx.envInfo, ctx.tokenProvider);
+      if (res.isErr()) return err(res.error);
+    }
     return ok(undefined);
   }
   async deploy(

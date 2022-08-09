20ba948694f81a3cf322594d274f21dbc2b70d9e
Ning Tang <nintan@microsoft.com>
2022-08-09 16:41:20 +0800

    chore(vsc): add ut for account treeview nodes

---
+++ packages/vscode-extension/test/extension/treeview/account/m365Node.test.ts
@@ -0,0 +1,49 @@
+import * as chai from "chai";
+import * as sinon from "sinon";
+import * as vscode from "vscode";
+
+import { M365AccountNode } from "../../../../src/treeview/account/m365Node";
+import { AccountItemStatus, loadingIcon, m365Icon } from "../../../../src/treeview/account/common";
+import { DynamicNode } from "../../../../src/treeview/dynamicNode";
+
+describe("m365Node", () => {
+  const sandbox = sinon.createSandbox();
+  const eventEmitter = new vscode.EventEmitter<DynamicNode | undefined | void>();
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it("setSignedIn", async () => {
+    const m365Node = new M365AccountNode(eventEmitter);
+    await m365Node.setSignedIn("test upn");
+    const treeItem = await m365Node.getTreeItem();
+
+    chai.assert.equal(treeItem.iconPath, m365Icon);
+    chai.assert.equal(treeItem.collapsibleState, vscode.TreeItemCollapsibleState.None);
+    chai.assert.equal(treeItem.label, "test upn");
+    chai.assert.equal(treeItem.contextValue, "signedinM365");
+    chai.assert.equal(treeItem.command, undefined);
+  });
+
+  it("setSigningIn", async () => {
+    const m365Node = new M365AccountNode(eventEmitter);
+    m365Node.setSigningIn();
+    const treeItem = await m365Node.getTreeItem();
+
+    chai.assert.equal(treeItem.iconPath, loadingIcon);
+    chai.assert.equal(treeItem.collapsibleState, vscode.TreeItemCollapsibleState.None);
+    chai.assert.equal(treeItem.contextValue, "");
+  });
+
+  it("setSignedOut", async () => {
+    const m365Node = new M365AccountNode(eventEmitter);
+    m365Node.status = AccountItemStatus.SignedIn;
+    await m365Node.setSignedOut();
+    const treeItem = await m365Node.getTreeItem();
+
+    chai.assert.equal(treeItem.iconPath, m365Icon);
+    chai.assert.equal(treeItem.collapsibleState, vscode.TreeItemCollapsibleState.None);
+    chai.assert.equal(treeItem.contextValue, "signinM365");
+  });
+});
---
+++ packages/vscode-extension/test/extension/treeview/account/sideloadingNode.test.ts
@@ -0,0 +1,43 @@
+import * as chai from "chai";
+import * as sinon from "sinon";
+import * as vscode from "vscode";
+
+import * as tools from "@microsoft/teamsfx-core/build/common/tools";
+
+import { errorIcon, passIcon, warningIcon } from "../../../../src/treeview/account/common";
+import { SideloadingNode } from "../../../../src/treeview/account/sideloadingNode";
+import { DynamicNode } from "../../../../src/treeview/dynamicNode";
+import * as handlers from "../../../../src/handlers";
+
+describe("sideloadingNode", () => {
+  const sandbox = sinon.createSandbox();
+  const eventEmitter = new vscode.EventEmitter<DynamicNode | undefined | void>();
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it("getTreeItem with empty string", async () => {
+    const sideloadingNode = new SideloadingNode(eventEmitter, "");
+    const treeItem = await sideloadingNode.getTreeItem();
+
+    chai.assert.equal(treeItem.iconPath, warningIcon);
+  });
+
+  it("getTreeItem with invalid token", async () => {
+    sandbox.stub(tools, "getSideloadingStatus").returns(Promise.resolve(false));
+    sandbox.stub(handlers, "checkSideloadingCallback");
+    const sideloadingNode = new SideloadingNode(eventEmitter, "token");
+    const treeItem = await sideloadingNode.getTreeItem();
+
+    chai.assert.equal(treeItem.iconPath, errorIcon);
+  });
+
+  it("getTreeItem with valid token", async () => {
+    sandbox.stub(tools, "getSideloadingStatus").returns(Promise.resolve(true));
+    const sideloadingNode = new SideloadingNode(eventEmitter, "token");
+    const treeItem = await sideloadingNode.getTreeItem();
+
+    chai.assert.equal(treeItem.iconPath, passIcon);
+  });
+});

7579d9f5daab5b3516d2497d0f52457e5dd8b297
Qianhao Dong <qidon@microsoft.com>
2022-08-17 14:53:13 +0800

    chore: fix action

--- .github/scripts/sideloading-replace.sh
+++ .github/scripts/sideloading-replace.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-set -x
 filePath=packages/cli/src/cmds/m365/serviceConstant.ts
 echo "Replace placeholders in $filePath"
-echo "$SERVICE_ENDPOINT_PLACEHOLDER"
-echo "$SERVICE_ENDPOINT_PLACEHOLDER"
-sed -i -e "s@{{SERVICE_ENDPOINT_PLACEHOLDER}}@$SERVICE_ENDPOINT_PLACEHOLDER@g" $filePath
-sed -i -e "s@{{SERVICE_SCOPE_PLACEHOLDER}}@$SERVICE_SCOPE_PLACEHOLDER@g" $filePath
+sed -i -e "s@{{SERVICE_ENDPOINT_PLACEHOLDER}}@$SIDELOADING_SERVICE_ENDPOINT@g" $filePath
+sed -i -e "s@{{SERVICE_SCOPE_PLACEHOLDER}}@$SIDELOADING_SERVICE_SCOPE@g" $filePath
 echo "Replace Done."

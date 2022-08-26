12753d7cbc50917fd02165cc8777ab7a405ca409
Zhaofeng Xu <zhaofengxu@microsoft.com>
2022-08-26 16:42:49 +0800

    chore(componentize): update sso question option

--- packages/fx-core/src/component/feature/sso.ts
+++ packages/fx-core/src/component/feature/sso.ts
@@ -2,30 +2,47 @@
 // Licensed under the MIT license.
 
 import {
+  AzureSolutionSettings,
   ContextV3,
   err,
   FxError,
   InputsWithProjectPath,
   ok,
   Platform,
+  ProjectSettingsV3,
   Result,
   Stage,
+  SystemError,
+  UserError,
   v3,
+  Void,
 } from "@microsoft/teamsfx-api";
 import "reflect-metadata";
 import { Container, Service } from "typedi";
+import { isExistingTabApp, BotHostTypeName, BotHostTypes } from "../../common";
+import { ResourcePlugins } from "../../common/constants";
 import { getLocalizedString } from "../../common/localizeUtils";
+import { hasBot } from "../../common/projectSettingsHelperV3";
 import { convertToAlphanumericOnly } from "../../common/utils";
+import { sendErrorTelemetryThenReturnError } from "../../core/telemetry";
 import {
   AddSsoParameters,
   AzureSolutionQuestionNames,
+  BotOptionItem,
+  BotSsoItem,
+  HostTypeOptionAzure,
+  MessageExtensionItem,
+  PluginNames,
+  SolutionError,
+  SolutionSource,
   SolutionTelemetryComponentName,
   SolutionTelemetryEvent,
   SolutionTelemetryProperty,
   TabOptionItem,
+  TabSsoItem,
 } from "../../plugins";
 import "../connection/azureWebAppConfig";
-import { ComponentNames } from "../constants";
+import { ComponentNames, TelemetryConstants } from "../constants";
 import { generateLocalDebugSettings } from "../debug";
 import { AadApp } from "../resource/aadApp/aadApp";
 import { AppManifest } from "../resource/appManifest/appManifest";
@@ -39,12 +56,24 @@ export class SSO {
   name = "sso";
 
   async add(context: ContextV3, inputs: InputsWithProjectPath): Promise<Result<any, FxError>> {
-    const updates = getUpdateComponents(context, inputs);
+    context.telemetryReporter.sendTelemetryEvent(SolutionTelemetryEvent.AddSsoStart, {
+      [SolutionTelemetryProperty.Component]: SolutionTelemetryComponentName,
+    });
+
+    const updates = getUpdateComponents(context.projectSetting, inputs.stage === Stage.create);
     // generate manifest
     const aadApp = Container.get<AadApp>(ComponentNames.AadApp);
     {
       const res = await aadApp.generateManifest(context, inputs);
-      if (res.isErr()) return err(res.error);
+      if (res.isErr()) {
+        return err(
+          sendErrorTelemetryThenReturnError(
+            SolutionTelemetryEvent.AddSso,
+            res.error,
+            context.telemetryReporter
+          )
+        );
+      }
     }
 
     // config sso
@@ -73,7 +102,15 @@ export class SSO {
         convertToAlphanumericOnly(context.projectSetting.appName),
         res.value
       );
-      if (bicepRes.isErr()) return bicepRes;
+      if (bicepRes.isErr()) {
+        return err(
+          sendErrorTelemetryThenReturnError(
+            SolutionTelemetryEvent.AddSso,
+            bicepRes.error,
+            context.telemetryReporter
+          )
+        );
+      }
     }
 
     // generate auth files
@@ -82,7 +119,15 @@ export class SSO {
       inputs[AzureSolutionQuestionNames.Features] !== TabOptionItem.id
     ) {
       const res = await aadApp.generateAuthFiles(context, inputs, updates.tab!, updates.bot!);
-      if (res.isErr()) return err(res.error);
+      if (res.isErr()) {
+        return err(
+          sendErrorTelemetryThenReturnError(
+            SolutionTelemetryEvent.AddSso,
+            res.error,
+            context.telemetryReporter
+          )
+        );
+      }
     }
 
     // update app manifest
@@ -90,19 +135,43 @@ export class SSO {
       const capabilities: v3.ManifestCapability[] = [{ name: "WebApplicationInfo" }];
       const appManifest = Container.get<AppManifest>(ComponentNames.AppManifest);
       const res = await appManifest.addCapability(inputs, capabilities);
-      if (res.isErr()) return err(res.error);
+      if (res.isErr()) {
+        return err(
+          sendErrorTelemetryThenReturnError(
+            SolutionTelemetryEvent.AddSso,
+            res.error,
+            context.telemetryReporter
+          )
+        );
+      }
     }
 
     // local debug settings
     {
       const res = await generateLocalDebugSettings(context, inputs);
-      if (res.isErr()) return err(res.error);
+      if (res.isErr()) {
+        return err(
+          sendErrorTelemetryThenReturnError(
+            SolutionTelemetryEvent.AddSso,
+            res.error,
+            context.telemetryReporter
+          )
+        );
+      }
     }
 
     // generate config bicep
     {
       const res = await generateConfigBiceps(context, inputs);
-      if (res.isErr()) return err(res.error);
+      if (res.isErr()) {
+        return err(
+          sendErrorTelemetryThenReturnError(
+            SolutionTelemetryEvent.AddSso,
+            res.error,
+            context.telemetryReporter
+          )
+        );
+      }
     }
 
     // show notification
@@ -131,6 +200,17 @@ export class SSO {
       );
     }
 
+    context.telemetryReporter.sendTelemetryEvent(SolutionTelemetryEvent.AddSso, {
+      [SolutionTelemetryProperty.Component]: SolutionTelemetryComponentName,
+      [SolutionTelemetryProperty.Success]: TelemetryConstants.values.yes,
+      [SolutionTelemetryProperty.AddTabSso]: updates.tab
+        ? TelemetryConstants.values.yes
+        : TelemetryConstants.values.no,
+      [SolutionTelemetryProperty.AddBotSso]: updates.bot
+        ? TelemetryConstants.values.yes
+        : TelemetryConstants.values.no,
+    });
+
     return ok({
       func: AddSsoParameters.AddSso,
       capabilities: [
@@ -146,8 +226,11 @@ export interface updateComponents {
   tab?: boolean;
   aad?: boolean;
 }
-function getUpdateComponents(context: ContextV3, inputs: InputsWithProjectPath): updateComponents {
-  if (inputs.stage === Stage.create) {
+function getUpdateComponents(
+  projectSetting: ProjectSettingsV3,
+  isCreateStage: boolean
+): updateComponents {
+  if (isCreateStage) {
     return {
       tab: true,
       aad: true,
@@ -155,12 +238,20 @@ function getUpdateComponents(context: ContextV3, inputs: InputsWithProjectPath):
   }
   let needsBot = false;
   let needsTab = false;
-  const aadComponent = getComponent(context.projectSetting, ComponentNames.AadApp);
-  const teamsBotComponent = getComponent(context.projectSetting, ComponentNames.TeamsBot);
+  const aadComponent = getComponent(projectSetting, ComponentNames.AadApp);
+  const teamsBotComponent = getComponent(projectSetting, ComponentNames.TeamsBot);
   if (teamsBotComponent && !teamsBotComponent.sso) {
-    needsBot = teamsBotComponent.hosting !== ComponentNames.Function;
+    if (
+      teamsBotComponent.capabilities &&
+      teamsBotComponent.capabilities.length === 1 &&
+      teamsBotComponent.capabilities.includes("message-extension")
+    ) {
+      needsBot = false;
+    } else {
+      needsBot = teamsBotComponent.hosting !== ComponentNames.Function;
+    }
   }
-  const teamsTabComponent = getComponent(context.projectSetting, ComponentNames.TeamsTab);
+  const teamsTabComponent = getComponent(projectSetting, ComponentNames.TeamsTab);
   if (teamsTabComponent && !teamsTabComponent.sso) {
     needsTab = true;
   }
@@ -169,4 +260,61 @@ function getUpdateComponents(context: ContextV3, inputs: InputsWithProjectPath):
     tab: needsTab,
     aad: !aadComponent,
   };
+}
+
+export function canAddSso(
+  projectSettings: ProjectSettingsV3,
+  returnError = false
+): boolean | Result<Void, FxError> {
+  // TODO: support existing tab app with sso in v3
+
+  const update = getUpdateComponents(projectSettings, false);
+  if (update.tab || update.bot || update.aad) {
+    return true;
+  } else {
+    const aadComponent = getComponent(projectSettings, ComponentNames.AadApp);
+    const teamsBotComponent = getComponent(projectSettings, ComponentNames.TeamsBot);
+
+    if (teamsBotComponent) {
+      if (
+        teamsBotComponent.capabilities &&
+        teamsBotComponent.capabilities.length === 1 &&
+        teamsBotComponent.capabilities.includes("message-extension")
+      ) {
+        return returnError
+          ? err(
+              new SystemError(
+                SolutionSource,
+                SolutionError.AddSsoNotSupported,
+                getLocalizedString("core.addSso.onlyMeNotSupport")
+              )
+            )
+          : false;
+      }
+      if (teamsBotComponent.hosting === ComponentNames.Function) {
+        return returnError
+          ? err(
+              new SystemError(
+                SolutionSource,
+                SolutionError.AddSsoNotSupported,
+                getLocalizedString("core.addSso.functionNotSupport")
+              )
+            )
+          : false;
+      }
+    }
+
+    if (aadComponent) {
+      return returnError
+        ? err(
+            new SystemError(
+              SolutionSource,
+              SolutionError.SsoEnabled,
+              getLocalizedString("core.addSso.ssoEnabled")
+            )
+          )
+        : false;
+    }
+    return false;
+  }
 }

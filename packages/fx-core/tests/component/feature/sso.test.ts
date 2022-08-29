47adde45bff9e487fb26cb269f579dec993c8c99
Zhaofeng Xu <zhaofengxu@microsoft.com>
2022-08-29 13:08:44 +0800

    chore(componentize): update

--- packages/fx-core/tests/component/feature/sso.test.ts
+++ packages/fx-core/tests/component/feature/sso.test.ts
@@ -3,17 +3,15 @@
 
 import {
   InputsWithProjectPath,
-  ok,
   err,
   Platform,
   ProjectSettingsV3,
   TeamsAppManifest,
-  FxError,
   UserError,
 } from "@microsoft/teamsfx-api";
 import { assert } from "chai";
 import "mocha";
-import { createSandbox, SinonStub } from "sinon";
+import { createSandbox } from "sinon";
 import * as utils from "../../../src/component/utils";
 import { setTools } from "../../../src/core/globalVars";
 import { MockTools, randomAppName } from "../../core/utils";
@@ -21,14 +19,8 @@ import "../../../src/component/core";
 import { canAddSso } from "../../../src/component/feature/sso";
 import path from "path";
 import Container from "typedi";
-import { environmentManager } from "../../../src";
 import { ComponentNames } from "../../../src/component/constants";
-import { manifestUtils } from "../../../src/component/resource/appManifest/utils";
-import fs from "fs-extra";
 import * as os from "os";
-import * as projectSettingsLoader from "../../../src/core/middleware/projectSettingsLoader";
-import * as aadManifest from "../../../src/core/generateAadManifestTemplate";
-import * as templatesAction from "../../../src/common/template-utils/templatesActions";
 import * as telemetry from "../../../src/core/telemetry";
 
 describe("SSO can add in project", () => {
@@ -151,31 +143,20 @@ describe("SSO feature", () => {
     appName: "",
     projectId: "",
     programmingLanguage: "typescript",
-    components: [],
+    components: [
+      {
+        name: "teams-tab",
+        hosting: "azure-storage",
+        deploy: true,
+        provision: true,
+        build: true,
+        folder: "tabs",
+      },
+    ],
   };
   context.projectSetting = projectSetting;
   const manifest = {} as TeamsAppManifest;
-
-  let writeFileStub: SinonStub;
   beforeEach(() => {
-    sandbox.stub(tools.ui, "showMessage").resolves(ok("Confirm"));
-    sandbox.stub(manifestUtils, "readAppManifest").resolves(ok(manifest));
-    sandbox.stub(manifestUtils, "writeAppManifest").resolves(ok(undefined));
-    sandbox.stub(projectSettingsLoader, "loadProjectSettings").resolves(ok(projectSetting));
-    sandbox.stub(fs, "readJson").resolves({});
-    sandbox.stub(fs, "writeJSON").resolves();
-    sandbox.stub(fs, "writeJson").resolves();
-    sandbox.stub(fs, "pathExists").resolves(true);
-    sandbox.stub(fs, "copyFile").resolves();
-    sandbox.stub(fs, "ensureDir").resolves();
-    sandbox.stub(fs, "appendFile").resolves();
-    writeFileStub = sandbox.stub(fs, "writeFile").resolves();
-    sandbox.stub(fs, "ensureDirSync").returns();
-    sandbox.stub(fs, "readdirSync").returns([]);
-    sandbox.stub(fs, "appendFileSync").returns();
-    sandbox.stub(fs, "writeFileSync").returns();
-    sandbox.stub(environmentManager, "listRemoteEnvConfigs").resolves(ok(["dev"]));
-    sandbox.stub(aadManifest, "generateAadManifestTemplate").resolves();
     sandbox.stub(telemetry, "sendErrorTelemetryThenReturnError").returns(
       new UserError({
         name: "mock error",
@@ -191,8 +172,6 @@ describe("SSO feature", () => {
   });
 
   it("add sso", async () => {
-    sandbox.stub(templatesAction, "scaffoldFromTemplates").resolves();
-    sandbox.stub(utils.bicepUtils, "persistBiceps").resolves(ok(undefined));
     const aadComponent = Container.get(ComponentNames.AadApp) as any;
     sandbox.stub(aadComponent, "generateManifest").resolves(err(undefined));
 
@@ -202,12 +181,6 @@ describe("SSO feature", () => {
       language: "typescript",
       "app-name": appName,
     };
-    const tabComponent = Container.get("teams-tab") as any;
-    const addTabRes = await tabComponent.add(context, inputs);
-    if (addTabRes.isErr()) {
-      console.log(addTabRes.error);
-    }
-    assert.isTrue(addTabRes.isOk());
 
     const component = Container.get(ComponentNames.SSO) as any;
     const ssoRes = await component.add(context, inputs);

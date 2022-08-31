4f1c691efaa8c3a95f237f64a54df9fea604e32a
LongOddCode <haolong@microsoft.com>
2022-08-31 14:15:29 +0800

    chore: remove unused pvm code

--- packages/fx-core/tests/plugins/solution/debug/util/ngrok.test.ts
+++ packages/fx-core/tests/plugins/solution/debug/util/ngrok.test.ts
@@ -7,7 +7,7 @@ import axios from "axios";
 import { stub, restore } from "sinon";
 import rewire from "rewire";
 
-const ngrok = rewire("../../../../../src/plugins/solution/fx-solution/debug/util/ngrok.ts");
+const ngrok = rewire("../../../../../src/plugins/solution/fx-solution/debug/util/ngrok");
 
 describe("ngrok", () => {
   before(() => {
@@ -18,12 +18,25 @@ describe("ngrok", () => {
     restore();
   });
 
-  it("only support port in [4040, 4045)", async () => {
+  it("don't support port out of [4040, 4045)", async () => {
     stub(axios, "get").callsFake(async () => {
-      console.log("running!");
       return undefined;
     });
     const result = await ngrok.getNgrokHttpUrl("4039");
     expect(result).to.be.undefined;
+  });
+
+  it("could get ngrok url if port in [4040, 4045)", async () => {
+    stub(axios, "get").callsFake(async () => {
+      return {
+        data: {
+          tunnels: [
+            { public_url: "xxx", proto: "https", config: { addr: "http://localhost:4041" } },
+          ],
+        },
+      };
+    });
+    const result = await ngrok.getNgrokHttpUrl("4041");
+    expect(result).equals("xxx");
   });
 });

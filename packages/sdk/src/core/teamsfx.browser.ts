b2a4f0fdf32f8b773c2df76a504997bf86701864
frankqianms <frankqian@microsoft.com>
2022-09-01 10:14:03 +0800

    fix: .

--- packages/sdk/src/core/teamsfx.browser.ts
+++ packages/sdk/src/core/teamsfx.browser.ts
@@ -9,7 +9,7 @@ import { formatString } from "../util/utils";
 import { ErrorWithCode, ErrorCode, ErrorMessage } from "../core/errors";
 import { internalLogger } from "../util/logger";
 import { TeamsFxConfiguration } from "../models/teamsfxConfiguration";
-import { AuthenticationConfiguration } from "../../types/teamsfx-public";
+import { AuthenticationConfiguration } from "../models/configuration";
 
 /**
  * A class providing credential and configuration.

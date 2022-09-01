1342662d195479cf556fe25e8a557feccd44c33e
Xiaofu Huang <xiaofhua@microsoft.com>
2022-09-01 18:41:45 +0800

    fix: remove useless import

--- packages/vscode-extension/src/debug/taskTerminal/baseTaskTerminal.ts
+++ packages/vscode-extension/src/debug/taskTerminal/baseTaskTerminal.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import * as vscode from "vscode";
-import { assembleError, FxError, Result, UserError } from "@microsoft/teamsfx-api";
+import { assembleError, FxError, Result } from "@microsoft/teamsfx-api";
 import * as globalVariables from "../../globalVariables";
 import { showError } from "../../handlers";
 

de4e74b0ca24531a14f37a766b08bce0033b7c43
Kuojian Lu <kuojianlu@gmail.com>
2022-09-07 15:52:31 +0800

    chore(debug): add bot/sso/manifest debug output (#6036)
    * chore(debug): add bot/sso/manifest debug output
    
    - refactor debug handler, splitting setUp into several steps for better
      output transparency

--- packages/fx-core/src/component/debugHandler/bot.ts
+++ packages/fx-core/src/component/debugHandler/bot.ts
@@ -3,9 +3,12 @@
 "use strict";
 
 import { cloneDeep } from "lodash";
+import * as path from "path";
+import * as util from "util";
 
 import {
   assembleError,
+  CryptoProvider,
   err,
   FxError,
   LogProvider,
@@ -16,7 +19,6 @@ import {
   TelemetryReporter,
   UserInteraction,
   v3,
-  Void,
 } from "@microsoft/teamsfx-api";
 
 import { AppStudioScopes, GraphScopes } from "../../common/tools";
@@ -32,13 +34,32 @@ import { PluginLocalDebug } from "../../plugins/resource/bot/resources/strings";
 import { genUUID } from "../../plugins/resource/bot/utils/common";
 import { ResourceNameFactory } from "../../plugins/resource/bot/utils/resourceNameFactory";
 import { ComponentNames } from "../constants";
-import { botTunnelEndpointPlaceholder } from "./constants";
+import { DebugAction } from "./common";
 import {
   BotMessagingEndpointMissingError,
   errorSource,
   InvalidExistingBotArgsError,
 } from "./error";
 import { LocalEnvKeys, LocalEnvProvider } from "./localEnvProvider";
+
+const botTunnelEndpointPlaceholder = "${teamsfx:botTunnelEndpoint}";
+
+const botDebugMessages = {
+  validatingArgs: "Validating the arguments ...",
+  registeringAAD: "Registering an AAD app for bot ...",
+  registeringBot: "Registering a bot in bot framework developer portal ...",
+  updatingBotMessagingEndpoint: "Updating the bot messaging endpoint ...",
+  savingStates: "Saving the states for bot ...",
+  settingEnvs: "Setting the environment variables for bot ...",
+  AADRegistered: "AAD app is registered",
+  useExistingAAD: "Skip registering AAD app but use the existing AAD app from args",
+  AADAlreadyRegistered: "Skip registering AAD app as it has already been registered before",
+  botRegistered: "Bot is registered",
+  botAlreadyRegistered: "Skip registering bot as it has already been registered before",
+  botMessagingEndpointUpdated: "Bot messaging endpoint is updated to %s",
+  statesSaved: "The states for bot are saved in %s",
+  envsSet: "The environment variables for bot are set in %s",
+};
 
 export interface BotDebugArgs {
   botId?: string;
@@ -53,6 +74,12 @@ export class BotDebugHandler {
   private readonly logger?: LogProvider;
   private readonly telemetry?: TelemetryReporter;
   private readonly ui?: UserInteraction;
+
+  private existing = false;
+
+  private projectSettingsV3?: ProjectSettingsV3;
+  private cryptoProvider?: CryptoProvider;
+  private envInfoV3?: v3.EnvInfoV3;
 
   constructor(
     projectPath: string,
@@ -70,131 +97,39 @@ export class BotDebugHandler {
     this.ui = ui;
   }
 
-  // TODO: output message
-  public async setUp(): Promise<Result<Void, FxError>> {
-    try {
-      const checkArgsResult = await this.checkArgs();
-      if (checkArgsResult.isErr()) {
-        return err(checkArgsResult.error);
-      }
-
-      const projectSettingsResult = await loadProjectSettingsByProjectPath(this.projectPath, true);
-      if (projectSettingsResult.isErr()) {
-        return err(projectSettingsResult.error);
-      }
-      const projectSettingsV3: ProjectSettingsV3 = projectSettingsResult.value as ProjectSettingsV3;
-
-      const cryptoProvider = new LocalCrypto(projectSettingsV3.projectId);
-
-      const envInfoResult = await environmentManager.loadEnvInfo(
-        this.projectPath,
-        cryptoProvider,
-        environmentManager.getLocalEnvName(),
-        true
-      );
-      if (envInfoResult.isErr()) {
-        return err(envInfoResult.error);
-      }
-      const envInfoV3: v3.EnvInfoV3 = envInfoResult.value;
-      envInfoV3.state[ComponentNames.TeamsBot] = envInfoV3.state[ComponentNames.TeamsBot] || {};
-
-      // set botId, botPassword from args to state
-      if (checkArgsResult.value) {
-        envInfoV3.state[ComponentNames.TeamsBot].botId = this.args.botId;
-        envInfoV3.state[ComponentNames.TeamsBot].botPassword = this.args.botPassword;
-      }
-
-      // set validDomain, domain, siteEndpoint from args to state
-      const url = new URL(this.args.botMessagingEndpoint!);
-      envInfoV3.state[ComponentNames.TeamsBot].validDomain = url.hostname;
-      envInfoV3.state[ComponentNames.TeamsBot].domain = url.hostname;
-      envInfoV3.state[ComponentNames.TeamsBot].siteEndpoint = url.origin;
-
-      // not using existing bot and not yet created
-      if (!envInfoV3.state[ComponentNames.TeamsBot].botId) {
-        const tokenResult = await this.m365TokenProvider.getAccessToken({
-          scopes: GraphScopes,
-        });
-        if (tokenResult.isErr()) {
-          return err(tokenResult.error);
-        }
-
-        const displayName = ResourceNameFactory.createCommonName(
-          genUUID(),
-          projectSettingsV3.appName,
-          MaxLengths.AAD_DISPLAY_NAME
-        );
-        const botAuthCredential = await AADRegistration.registerAADAppAndGetSecretByGraph(
-          tokenResult.value,
-          displayName
-        );
-
-        // set objectId, botId, botPassword to state
-        envInfoV3.state[ComponentNames.TeamsBot].objectId = botAuthCredential.objectId;
-        envInfoV3.state[ComponentNames.TeamsBot].botId = botAuthCredential.clientId;
-        envInfoV3.state[ComponentNames.TeamsBot].botPassword = botAuthCredential.clientSecret;
-      }
-
-      const tokenResult = await this.m365TokenProvider.getAccessToken({
-        scopes: AppStudioScopes,
-      });
-      if (tokenResult.isErr()) {
-        return err(tokenResult.error);
-      }
-
-      const botReg: IBotRegistration = {
-        botId: envInfoV3.state[ComponentNames.TeamsBot].botId,
-        name:
-          convertToAlphanumericOnly(projectSettingsV3.appName) +
-          PluginLocalDebug.LOCAL_DEBUG_SUFFIX,
-        description: "",
-        iconUrl: "",
-        messagingEndpoint: this.args.botMessagingEndpoint!,
-        callingEndpoint: "",
-      };
-
-      await AppStudio.createBotRegistration(tokenResult.value, botReg);
-
-      await AppStudio.updateMessageEndpoint(
-        tokenResult.value,
-        envInfoV3.state[ComponentNames.TeamsBot].botId,
-        this.args.botMessagingEndpoint!
-      );
-
-      await environmentManager.writeEnvState(
-        cloneDeep(envInfoV3.state),
-        this.projectPath,
-        cryptoProvider,
-        environmentManager.getLocalEnvName(),
-        true
-      );
-
-      await this.setEnvs(envInfoV3);
-
-      return ok(Void);
-    } catch (error: any) {
-      return err(assembleError(error, errorSource));
-    }
+  public getActions(): DebugAction[] {
+    const actions: DebugAction[] = [];
+    actions.push({
+      startMessage: botDebugMessages.validatingArgs,
+      run: this.checkArgs.bind(this),
+    });
+    actions.push({
+      startMessage: botDebugMessages.registeringAAD,
+      run: this.registerAAD.bind(this),
+    });
+    actions.push({
+      startMessage: botDebugMessages.registeringBot,
+      run: this.registerBot.bind(this),
+    });
+    actions.push({
+      startMessage: botDebugMessages.updatingBotMessagingEndpoint,
+      run: this.updateBotMessagingEndpoint.bind(this),
+    });
+    actions.push({
+      startMessage: botDebugMessages.savingStates,
+      run: this.saveStates.bind(this),
+    });
+    actions.push({
+      startMessage: botDebugMessages.settingEnvs,
+      run: this.setEnvs.bind(this),
+    });
+    return actions;
   }
 
-  private async setEnvs(envInfoV3: v3.EnvInfoV3): Promise<void> {
-    const localEnvProvider = new LocalEnvProvider(this.projectPath);
-    const botEnvs = await localEnvProvider.loadBotLocalEnvs();
-
-    botEnvs.template[LocalEnvKeys.bot.template.BotId] =
-      envInfoV3.state[ComponentNames.TeamsBot].botId;
-    botEnvs.template[LocalEnvKeys.bot.template.BotPassword] =
-      envInfoV3.state[ComponentNames.TeamsBot].botPassword;
-
-    localEnvProvider.saveBotLocalEnvs(botEnvs);
-  }
-
-  // return true if using existing bot
-  private async checkArgs(): Promise<Result<boolean, FxError>> {
+  private async checkArgs(): Promise<Result<string[], FxError>> {
     // TODO: allow botPassword to be set in other places (like env) instead of tasks.json
-    let flag = false;
     if (this.args.botId && this.args.botPassword) {
-      flag = true;
+      this.existing = true;
     } else if (this.args.botId || this.args.botPassword) {
       return err(InvalidExistingBotArgsError());
     }
@@ -202,6 +137,8 @@ export class BotDebugHandler {
     if (!this.args.botMessagingEndpoint || this.args.botMessagingEndpoint.trim().length === 0) {
       return err(BotMessagingEndpointMissingError());
     }
+
+    this.args.botMessagingEndpoint = this.args.botMessagingEndpoint.trim();
 
     if (this.args.botMessagingEndpoint.includes(botTunnelEndpointPlaceholder)) {
       // TODO: get bot endpoint from tunnel manager
@@ -212,6 +149,175 @@ export class BotDebugHandler {
       );
     }
 
-    return ok(flag);
+    return ok([]);
+  }
+
+  private async registerAAD(): Promise<Result<string[], FxError>> {
+    try {
+      const projectSettingsResult = await loadProjectSettingsByProjectPath(this.projectPath, true);
+      if (projectSettingsResult.isErr()) {
+        return err(projectSettingsResult.error);
+      }
+      this.projectSettingsV3 = projectSettingsResult.value as ProjectSettingsV3;
+
+      this.cryptoProvider = new LocalCrypto(this.projectSettingsV3.projectId);
+
+      const envInfoResult = await environmentManager.loadEnvInfo(
+        this.projectPath,
+        this.cryptoProvider,
+        environmentManager.getLocalEnvName(),
+        true
+      );
+      if (envInfoResult.isErr()) {
+        return err(envInfoResult.error);
+      }
+      this.envInfoV3 = envInfoResult.value;
+      this.envInfoV3.state[ComponentNames.TeamsBot] =
+        this.envInfoV3.state[ComponentNames.TeamsBot] || {};
+
+      if (this.existing) {
+        // use existing bot
+        // set botId, botPassword from args to state
+        this.envInfoV3.state[ComponentNames.TeamsBot].botId = this.args.botId;
+        this.envInfoV3.state[ComponentNames.TeamsBot].botPassword = this.args.botPassword;
+
+        return ok([botDebugMessages.useExistingAAD]);
+      } else if (
+        this.envInfoV3.state[ComponentNames.TeamsBot].botId &&
+        this.envInfoV3.state[ComponentNames.TeamsBot].botPassword
+      ) {
+        // AAD already registered
+        return ok([botDebugMessages.AADAlreadyRegistered]);
+      } else {
+        // not using existing bot and AAD not yet registered
+        const tokenResult = await this.m365TokenProvider.getAccessToken({
+          scopes: GraphScopes,
+        });
+        if (tokenResult.isErr()) {
+          return err(tokenResult.error);
+        }
+
+        const displayName = ResourceNameFactory.createCommonName(
+          genUUID(),
+          this.projectSettingsV3.appName,
+          MaxLengths.AAD_DISPLAY_NAME
+        );
+        const botAuthCredential = await AADRegistration.registerAADAppAndGetSecretByGraph(
+          tokenResult.value,
+          displayName
+        );
+
+        // set objectId, botId, botPassword to state
+        this.envInfoV3.state[ComponentNames.TeamsBot].objectId = botAuthCredential.objectId;
+        this.envInfoV3.state[ComponentNames.TeamsBot].botId = botAuthCredential.clientId;
+        this.envInfoV3.state[ComponentNames.TeamsBot].botPassword = botAuthCredential.clientSecret;
+
+        return ok([botDebugMessages.AADRegistered]);
+      }
+    } catch (error: unknown) {
+      return err(assembleError(error, errorSource));
+    }
+  }
+
+  private async registerBot(): Promise<Result<string[], FxError>> {
+    try {
+      const tokenResult = await this.m365TokenProvider.getAccessToken({
+        scopes: AppStudioScopes,
+      });
+      if (tokenResult.isErr()) {
+        return err(tokenResult.error);
+      }
+
+      const result = await AppStudio.getBotRegistration(
+        tokenResult.value,
+        this.envInfoV3!.state[ComponentNames.TeamsBot].botId
+      );
+      if (result) {
+        return ok([botDebugMessages.botAlreadyRegistered]);
+      }
+
+      const botReg: IBotRegistration = {
+        botId: this.envInfoV3!.state[ComponentNames.TeamsBot].botId,
+        name:
+          convertToAlphanumericOnly(this.projectSettingsV3!.appName) +
+          PluginLocalDebug.LOCAL_DEBUG_SUFFIX,
+        description: "",
+        iconUrl: "",
+        messagingEndpoint: "",
+        callingEndpoint: "",
+      };
+
+      await AppStudio.createBotRegistration(tokenResult.value, botReg);
+
+      return ok([botDebugMessages.botRegistered]);
+    } catch (error: unknown) {
+      return err(assembleError(error, errorSource));
+    }
+  }
+
+  private async updateBotMessagingEndpoint(): Promise<Result<string[], FxError>> {
+    try {
+      // set validDomain, domain, siteEndpoint from args to state
+      const url = new URL(this.args.botMessagingEndpoint!);
+      this.envInfoV3!.state[ComponentNames.TeamsBot].validDomain = url.hostname;
+      this.envInfoV3!.state[ComponentNames.TeamsBot].domain = url.hostname;
+      this.envInfoV3!.state[ComponentNames.TeamsBot].siteEndpoint = url.origin;
+
+      const tokenResult = await this.m365TokenProvider.getAccessToken({
+        scopes: AppStudioScopes,
+      });
+      if (tokenResult.isErr()) {
+        return err(tokenResult.error);
+      }
+
+      await AppStudio.updateMessageEndpoint(
+        tokenResult.value,
+        this.envInfoV3!.state[ComponentNames.TeamsBot].botId,
+        this.args.botMessagingEndpoint!
+      );
+
+      return ok([
+        util.format(botDebugMessages.botMessagingEndpointUpdated, this.args.botMessagingEndpoint),
+      ]);
+    } catch (error: unknown) {
+      return err(assembleError(error, errorSource));
+    }
+  }
+
+  private async saveStates(): Promise<Result<string[], FxError>> {
+    try {
+      const statePath = await environmentManager.writeEnvState(
+        cloneDeep(this.envInfoV3!.state),
+        this.projectPath,
+        this.cryptoProvider!,
+        environmentManager.getLocalEnvName(),
+        true
+      );
+      if (statePath.isErr()) {
+        return err(statePath.error);
+      }
+
+      return ok([util.format(botDebugMessages.statesSaved, path.normalize(statePath.value))]);
+    } catch (error: unknown) {
+      return err(assembleError(error, errorSource));
+    }
+  }
+
+  private async setEnvs(): Promise<Result<string[], FxError>> {
+    try {
+      const localEnvProvider = new LocalEnvProvider(this.projectPath);
+      const botEnvs = await localEnvProvider.loadBotLocalEnvs();
+
+      botEnvs.template[LocalEnvKeys.bot.template.BotId] =
+        this.envInfoV3!.state[ComponentNames.TeamsBot].botId;
+      botEnvs.template[LocalEnvKeys.bot.template.BotPassword] =
+        this.envInfoV3!.state[ComponentNames.TeamsBot].botPassword;
+
+      const envPath = await localEnvProvider.saveBotLocalEnvs(botEnvs);
+
+      return ok([util.format(botDebugMessages.envsSet, path.normalize(envPath))]);
+    } catch (error: unknown) {
+      return err(assembleError(error, errorSource));
+    }
   }
 }

2bee4d842e08cd81c11d17c54bf7e2058f72858a
Wang Yefu <yefuwang@microsoft.com>
2022-09-07 13:42:25 +0800

    chore: add more ut

--- packages/fx-core/tests/plugins/resource/appstudio/unit/appstudio.test.ts
+++ packages/fx-core/tests/plugins/resource/appstudio/unit/appstudio.test.ts
@@ -11,6 +11,7 @@ import { TelemetryUtils } from "../../../../../src/plugins/resource/appstudio/ut
 import { RetryHandler } from "../../../../../src/plugins/resource/appstudio/utils/utils";
 import { newEnvInfo } from "../../../../../src/core/environment";
 import { PluginContext } from "@microsoft/teamsfx-api";
+import { PublishingState } from "../../../../../src/plugins/resource/appstudio/interfaces/IPublishingAppDefinition";
 
 describe("App Studio API Test", () => {
   const appStudioToken = "appStudioToken";
@@ -47,32 +48,67 @@ describe("App Studio API Test", () => {
         chai.assert.equal(error.name, AppStudioError.DeveloperPortalAPIFailedError.name);
       }
     });
+
+    it("should throw error with response on BadeRequest with 2xx status code", async () => {
+      const fakeAxiosInstance = axios.create();
+      sinon.stub(axios, "create").returns(fakeAxiosInstance);
+
+      const response = {
+        data: {
+          error: "BadRequest",
+        },
+        message: "fake message",
+      };
+      sinon.stub(fakeAxiosInstance, "post").resolves(response);
+
+      const ctx = {
+        envInfo: newEnvInfo(),
+        root: "fakeRoot",
+      } as any as PluginContext;
+      TelemetryUtils.init(ctx);
+      sinon.stub(TelemetryUtils, "sendErrorEvent").callsFake(() => {});
+
+      try {
+        await AppStudioClient.publishTeamsApp(appStudioToken, Buffer.from(""), appStudioToken);
+      } catch (error) {
+        chai.assert.equal(error.name, AppStudioError.DeveloperPortalAPIFailedError.name);
+        chai.assert.isNotNull(error.response);
+      }
+    });
   });
 
-  it("BadeRequest with 2xx status code", async () => {
-    const fakeAxiosInstance = axios.create();
-    sinon.stub(axios, "create").returns(fakeAxiosInstance);
+  describe("publishTeamsAppUpdate", () => {
+    it("should throw error with response on BadeRequest with 2xx status code", async () => {
+      const fakeAxiosInstance = axios.create();
+      sinon.stub(axios, "create").returns(fakeAxiosInstance);
 
-    const response = {
-      data: {
-        error: "BadRequest",
-      },
-      message: "fake message",
-    };
-    sinon.stub(fakeAxiosInstance, "post").resolves(response);
+      const response = {
+        data: {
+          error: "BadRequest",
+        },
+        message: "fake message",
+      };
 
-    const ctx = {
-      envInfo: newEnvInfo(),
-      root: "fakeRoot",
-    } as any as PluginContext;
-    TelemetryUtils.init(ctx);
-    sinon.stub(TelemetryUtils, "sendErrorEvent").callsFake(() => {});
+      sinon.stub(fakeAxiosInstance, "post").resolves(response);
+      sinon.stub(AppStudioClient, "getAppByTeamsAppId").resolves({
+        publishingState: PublishingState.submitted,
+        teamsAppId: "xx",
+        displayName: "xx",
+        lastModifiedDateTime: null,
+      });
 
-    try {
-      await AppStudioClient.publishTeamsApp(appStudioToken, Buffer.from(""), appStudioToken);
-    } catch (error) {
-      chai.assert.equal(error.name, AppStudioError.DeveloperPortalAPIFailedError.name);
-      chai.assert.isNotNull(error.response);
-    }
+      const ctx = {
+        envInfo: newEnvInfo(),
+        root: "fakeRoot",
+      } as any as PluginContext;
+      TelemetryUtils.init(ctx);
+      sinon.stub(TelemetryUtils, "sendErrorEvent").callsFake(() => {});
+
+      try {
+        await AppStudioClient.publishTeamsAppUpdate("", Buffer.from(""), appStudioToken);
+      } catch (error) {
+        chai.assert.isNotNull(error.response);
+      }
+    });
   });
 });

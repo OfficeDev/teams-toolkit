37bddec1d1dec8168d69b60c07625a71b6c9d6b3
xzf0587 <zhaofengxu@microsoft.com>
2022-09-08 19:07:27 +0800

    chore(componentize): update aad (#6062)
    * chore(componentize): update aad

--- packages/fx-core/src/component/resource/aadApp/aadApp.ts
+++ packages/fx-core/src/component/resource/aadApp/aadApp.ts
@@ -9,8 +9,11 @@ import {
   FxError,
   InputsWithProjectPath,
   ok,
+  PluginContext,
   ResourceContextV3,
   Result,
+  SystemError,
+  UserError,
 } from "@microsoft/teamsfx-api";
 import "reflect-metadata";
 import { Service } from "typedi";
@@ -24,6 +27,11 @@ import { convertProjectSettingsV3ToV2 } from "../../migrate";
 import { generateAadManifestTemplate } from "../../../core/generateAadManifestTemplate";
 import { createAuthFiles } from "../../../plugins/solution/fx-solution/v2/executeUserTask";
 import { isVSProject } from "../../../common";
+import { DialogUtils } from "../../../plugins/resource/aad/utils/dialog";
+import { TelemetryUtils } from "../../../plugins/resource/aad/utils/telemetry";
+import { Messages, Telemetry } from "../../../plugins/resource/aad/constants";
+import { UnhandledError } from "../../../plugins/resource/aad/errors";
+import { AadResult, ResultFactory } from "../../../plugins/resource/aad/results";
 @Service(ComponentNames.AadApp)
 export class AadApp implements CloudResource {
   readonly type = "cloud";
@@ -89,10 +97,7 @@ export class AadApp implements CloudResource {
     const aadAppImplement = new AadAppForTeamsImpl();
     const convertCtx = convertContext(context, inputs);
     await aadAppImplement.provisionUsingManifest(convertCtx);
-    const convertState = convertCtx.envInfo.state.get("fx-resource-aad-app-for-teams");
-    convertState.forEach((v: any, k: string) => {
-      context.envInfo.state[ComponentNames.AadApp][k] = v;
-    });
+    this.setState(convertCtx, context);
     return ok(undefined);
   }
   async configure(
@@ -114,24 +119,100 @@ export class AadApp implements CloudResource {
   ): Promise<Result<undefined, FxError>> {
     const aadAppImplement = new AadAppForTeamsImpl();
     const convertCtx = convertContext(context, inputs);
-    await aadAppImplement.setApplicationInContext(convertCtx);
-    const convertState = convertCtx.envInfo.state.get("fx-resource-aad-app-for-teams");
-    convertState.forEach((v: any, k: string) => {
-      context.envInfo.state[ComponentNames.AadApp][k] = v;
-    });
-    return ok(undefined);
+    const res = await this.runWithExceptionCatchingAsync(
+      async () => aadAppImplement.setApplicationInContext(convertCtx),
+      convertCtx,
+      Messages.EndDeploy.telemetry
+    );
+    if (res.isErr()) {
+      return res;
+    }
+    this.setState(convertCtx, context);
+    return res;
   }
+
   async deploy(
     context: ResourceContextV3,
     inputs: InputsWithProjectPath
   ): Promise<Result<undefined, FxError>> {
     const aadAppImplement = new AadAppForTeamsImpl();
     const convertCtx = convertContext(context, inputs);
-    await aadAppImplement.deploy(convertCtx);
+    const res = await this.runWithExceptionCatchingAsync(
+      () => aadAppImplement.deploy(convertCtx),
+      convertCtx,
+      Messages.EndDeploy.telemetry
+    );
+    if (res.isErr()) {
+      return res;
+    }
+    this.setState(convertCtx, context);
+    return res;
+  }
+
+  private setState(convertCtx: PluginContext, context: ResourceContextV3) {
     const convertState = convertCtx.envInfo.state.get("fx-resource-aad-app-for-teams");
     convertState.forEach((v: any, k: string) => {
       context.envInfo.state[ComponentNames.AadApp][k] = v;
     });
-    return ok(undefined);
+  }
+
+  private async runWithExceptionCatchingAsync(
+    fn: () => Promise<AadResult>,
+    ctx: PluginContext,
+    stage: string
+  ): Promise<AadResult> {
+    try {
+      return await fn();
+    } catch (e) {
+      return this.returnError(e, ctx, stage);
+    }
+  }
+
+  private returnError(e: any, ctx: PluginContext, stage: string): AadResult {
+    if (e instanceof SystemError || e instanceof UserError) {
+      let errorMessage = e.message;
+      // For errors contains innerError, e.g. failures when calling Graph API
+      if (e.innerError) {
+        errorMessage += ` Detailed error: ${e.innerError.message}.`;
+        if (e.innerError.response?.data?.errorMessage) {
+          // For errors return from App Studio API
+          errorMessage += ` Reason: ${e.innerError.response?.data?.errorMessage}`;
+        } else if (e.innerError.response?.data?.error?.message) {
+          // For errors return from Graph API
+          errorMessage += ` Reason: ${e.innerError.response?.data?.error?.message}`;
+        }
+        e.message = errorMessage;
+      }
+      ctx.logProvider?.error(errorMessage);
+      TelemetryUtils.sendErrorEvent(
+        stage,
+        e.name,
+        e instanceof UserError ? Telemetry.userError : Telemetry.systemError,
+        errorMessage
+      );
+      DialogUtils.progress?.end(false);
+      return err(e);
+    } else {
+      if (!(e instanceof Error)) {
+        e = new Error(e.toString());
+      }
+
+      ctx.logProvider?.error(e.message);
+      TelemetryUtils.sendErrorEvent(
+        stage,
+        UnhandledError.name,
+        Telemetry.systemError,
+        UnhandledError.message() + " " + e.message
+      );
+      return err(
+        ResultFactory.SystemError(
+          UnhandledError.name,
+          UnhandledError.message(),
+          e,
+          undefined,
+          undefined
+        )
+      );
+    }
   }
 }
--- packages/fx-core/tests/component/resource/aadApp/index.test.ts
+++ packages/fx-core/tests/component/resource/aadApp/index.test.ts
@@ -1,7 +1,13 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { InputsWithProjectPath, Platform, ProjectSettingsV3 } from "@microsoft/teamsfx-api";
+import {
+  InputsWithProjectPath,
+  ok,
+  Platform,
+  ProjectSettingsV3,
+  ResourceContextV3,
+} from "@microsoft/teamsfx-api";
 import { assert } from "chai";
 import "mocha";
 import { createSandbox } from "sinon";
@@ -13,6 +19,10 @@ import * as os from "os";
 import { convertContext } from "../../../../src/component/resource/aadApp/utils";
 import { setTools } from "../../../../src/core/globalVars";
 import { newEnvInfoV3 } from "../../../../src/core/environment";
+import Container from "typedi";
+import { ComponentNames } from "../../../../src/component/constants";
+import { AadApp } from "../../../../src/component/resource/aadApp/aadApp";
+import { AadAppForTeamsImpl } from "../../../../src/plugins/resource/aad/plugin";
 
 describe("aadApp utils", () => {
   const sandbox = createSandbox();
@@ -62,5 +72,56 @@ describe("aadApp utils", () => {
     assert.isDefined(res.envInfo.config);
     assert.isDefined(res.envInfo.config.auth);
     assert.isDefined(res.root);
+  });
+});
+
+describe("aadApp component", () => {
+  const sandbox = createSandbox();
+  const tools = new MockTools();
+  setTools(tools);
+  const appName = `unittest${randomAppName()}`;
+  const projectPath = path.join(os.homedir(), "TeamsApps", appName);
+  const context = utils.createContextV3();
+  const projectSetting: ProjectSettingsV3 = {
+    appName: "",
+    projectId: "",
+    programmingLanguage: "typescript",
+    components: [
+      {
+        name: "teams-tab",
+        hosting: "azure-storage",
+        deploy: true,
+        provision: true,
+        build: true,
+        folder: "tabs",
+        sso: true,
+      },
+      {
+        name: "aad-app",
+        provision: true,
+        deploy: true,
+      },
+    ],
+  };
+  context.projectSetting = projectSetting;
+  beforeEach(() => {
+    context.envInfo = newEnvInfoV3();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it("execution happy path", async () => {
+    sandbox.stub(AadAppForTeamsImpl.prototype, "provisionUsingManifest").resolves(ok(undefined));
+    const inputs: InputsWithProjectPath = {
+      projectPath: projectPath,
+      platform: Platform.VSCode,
+      language: "typescript",
+      "app-name": appName,
+    };
+    const component = new AadApp();
+    const res = await component.provision(context as ResourceContextV3, inputs);
+    assert.isTrue(res.isOk());
   });
 });

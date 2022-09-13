737200429c0d296c5743e58071a2c8bd98af8b81
Yiqing Zhao <cissyzhao99@gmail.com>
2022-09-13 09:32:44 +0800

    chore: remove unused type

--- packages/sdk-react/src/useTeamsFx.ts
+++ packages/sdk-react/src/useTeamsFx.ts
@@ -2,51 +2,19 @@
 // Licensed under the MIT license.
 
 import { LogLevel, setLogLevel, setLogFunction, TeamsFx, IdentityType } from "@microsoft/teamsfx";
+import { TeamsFxContextConfig } from "./TeamsFxContext";
 import { useTeams } from "msteams-react-base-component";
-import { ThemePrepared } from "@fluentui/react-northstar";
 import { useData } from "./useData";
-
-export type TeamsFxContext = {
-  /**
-   * Instance of TeamsFx.
-   */
-  teamsfx?: TeamsFx;
-  /**
-   * Status of data loading.
-   */
-  loading: boolean;
-  /**
-   * Error information.
-   */
-  error: unknown;
-  /**
-   * Indicates that current environment is in Teams
-   */
-  inTeams?: boolean;
-  /**
-   * Teams theme.
-   */
-  theme: ThemePrepared;
-  /**
-   * Teams theme string.
-   */
-  themeString: string;
-  /**
-   * Teams context object.
-   */
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  context?: any;
-};
 
 /**
  * Initialize TeamsFx SDK with customized configuration.
  *
  * @param teamsfxConfig - custom configuration to override default ones.
- * @returns TeamsFxContext object
+ * @returns TeamsFxContextConfig object
  *
  * @beta
  */
-export function useTeamsFx(teamsfxConfig?: Record<string, string>): TeamsFxContext {
+export function useTeamsFx(teamsfxConfig?: Record<string, string>): TeamsFxContextConfig {
   const [result] = useTeams({});
   const { data, error, loading } = useData(async () => {
     if (process.env.NODE_ENV === "development") {

55452c7eaefa0100715a2a26d4963ca7c0b464ee
Huihui Wu <huihuiwu@microsoft.com>
2022-09-14 13:29:35 +0800

    chore: add setting for spfx version

--- packages/fx-core/src/common/constants.ts
+++ packages/fx-core/src/common/constants.ts
@@ -79,6 +79,7 @@ export class FeatureFlagName {
   static readonly M365App = "TEAMSFX_M365_APP";
   static readonly YoCheckerEnable = "TEAMSFX_YO_ENV_CHECKER_ENABLE";
   static readonly GeneratorCheckerEnable = "TEAMSFX_GENERATOR_ENV_CHECKER_ENABLE";
+  static readonly SPFxVersion = "TEAMSFX_SPFX_VERSIOIN";
   static readonly SPFxMultiTab = "TEAMSFX_SPFX_MULTI_TAB";
   static readonly ApiConnect = "TEAMSFX_API_CONNECT_ENABLE";
   static readonly DeployManifest = "TEAMSFX_DEPLOY_MANIFEST";

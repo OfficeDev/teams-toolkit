ff8539a6146b4dd81989f636c5df1f796db21f94
huajiezhang <huajiezhang@microsoft.com>
2022-09-21 13:21:34 +0800

    feat: up

--- packages/cli/src/cmds/preview/preview.ts
+++ packages/cli/src/cmds/preview/preview.ts
@@ -782,7 +782,9 @@ export default class Preview extends YargsCommand {
       ?.get(constants.solutionPluginName)
       ?.get(constants.teamsAppTenantIdConfigKey) as string;
 
-    const remoteTeamsAppId: string = config?.config?.get(constants.remoteTeamsAppIdConfigKey);
+    const remoteTeamsAppId: string = config?.config
+      ?.get(constants.appstudioPluginName)
+      ?.get(constants.remoteTeamsAppIdConfigKey);
     if (remoteTeamsAppId === undefined || remoteTeamsAppId.length === 0) {
       return err(errors.PreviewWithoutProvision());
     }

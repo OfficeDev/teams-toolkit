ef1c05f3618c1e4cb4608b73e03d3480dfbcd011
= <=>
2022-09-21 16:24:00 +0800

    move to prviate repo

--- packages/portal/routes/api/todayreportstatus.ts
+++
@@ -1,8 +0,0 @@
-export const GET = async () => {
-  const data = await Deno.readTextFile('test-web-server/fakeData.json');
-  return new Response(data, {
-    headers: {
-      'content-type': 'application/json',
-    },
-  });
-};

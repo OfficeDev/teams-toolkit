d32a04a4dc6abd1782bded08c0a9eb6b8edc3316
yukun-dong <dongyukun1995@163.com>
2022-09-26 16:27:55 +0800

    test: add test for external sample

--- packages/fx-core/tests/component/generator/generator.test.ts
+++ packages/fx-core/tests/component/generator/generator.test.ts
@@ -16,6 +16,10 @@ import {
 import { assert } from "chai";
 import { compareDirs } from "./utils";
 import { templateDownloadBaseUrl } from "../../../src/component/generator/constant";
+import { Generator } from "../../../src/component/generator/generator";
+import { createContextV3 } from "../../../src/component/utils";
+import { setTools } from "../../../src/core/globalVars";
+import { MockTools } from "../../core/utils";
 describe("Generator utils", () => {
   const tmpDir = path.join(__dirname, "tmp");
 
@@ -66,5 +70,25 @@ describe("Generator utils", () => {
       await getValidSampleDestination(sampleName, dstPath),
       path.join(dstPath, "generator_1")
     );
+  });
+});
+
+describe("Generator happy path", () => {
+  const tools = new MockTools();
+  setTools(tools);
+  const context = createContextV3();
+  const tmpDir = path.join(__dirname, "tmp");
+
+  afterEach(async () => {
+    if (await fs.pathExists(tmpDir)) {
+      await fs.rm(tmpDir, { recursive: true });
+    }
+  });
+  it("external sample", async () => {
+    const sampleName = "bot-proactive-messaging-teamsfx";
+    await Generator.generateSample(sampleName, tmpDir, context);
+    const files = await fs.readdir(path.join(tmpDir, sampleName));
+    assert.isTrue(files.length > 0);
+    assert.isTrue(files.includes(".fx"));
   });
 });

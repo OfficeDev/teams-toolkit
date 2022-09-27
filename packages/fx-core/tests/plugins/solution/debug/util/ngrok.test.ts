b416ce7efeb15595f4c93baab16b2a6f3aef3da2
Xiaofu Huang <xiaofhua@microsoft.com>
2022-09-27 15:19:57 +0800

    test(localdebug): update test cases

--- packages/fx-core/tests/plugins/solution/debug/util/ngrok.test.ts
+++ packages/fx-core/tests/plugins/solution/debug/util/ngrok.test.ts
@@ -8,77 +8,122 @@ import { stub, restore } from "sinon";
 import rewire from "rewire";
 
 const ngrok = rewire("../../../../../src/plugins/solution/fx-solution/debug/util/ngrok");
-
 describe("ngrok", () => {
-  before(() => {
-    ngrok.__set__("delay", () => {});
-  });
-
-  afterEach(() => {
-    restore();
-  });
-
-  it("don't support port out of [4040, 4045)", async () => {
-    stub(axios, "get").callsFake(async () => {
-      return undefined;
+  describe("getNgrokHttpUrl", () => {
+    before(() => {
+      ngrok.__set__("delay", () => {});
     });
-    const result = await ngrok.getNgrokHttpUrl("4039");
-    expect(result).to.be.undefined;
+
+    afterEach(() => {
+      restore();
+    });
+
+    it("don't support port out of [4040, 4045)", async () => {
+      stub(axios, "get").callsFake(async () => {
+        return undefined;
+      });
+      const result = await ngrok.getNgrokHttpUrl("4039");
+      expect(result).to.be.undefined;
+    });
+
+    it("could get ngrok url if port in [4040, 4045)", async () => {
+      stub(axios, "get").callsFake(async () => {
+        return {
+          data: {
+            tunnels: [
+              { public_url: "xxx", proto: "https", config: { addr: "http://localhost:4041" } },
+            ],
+          },
+        };
+      });
+      const result = await ngrok.getNgrokHttpUrl("4041");
+      expect(result).equals("xxx");
+    });
+
+    it("could get ngrok url by addr string", async () => {
+      stub(axios, "get").callsFake(async () => {
+        return {
+          data: {
+            tunnels: [
+              { public_url: "test_url", proto: "https", config: { addr: "http://localhost:4041" } },
+            ],
+          },
+        };
+      });
+      const result = await ngrok.getNgrokHttpUrl("http://localhost:4041");
+      expect(result).equals("test_url");
+    });
+
+    it("could get ngrok url by addr string and trailing slash", async () => {
+      stub(axios, "get").callsFake(async () => {
+        return {
+          data: {
+            tunnels: [
+              { public_url: "test_url", proto: "https", config: { addr: "http://localhost:4041" } },
+            ],
+          },
+        };
+      });
+      const result = await ngrok.getNgrokHttpUrl("http://localhost:4041/");
+      expect(result).equals("test_url");
+    });
+
+    it("could get ngrok url by addr number and trailing slash", async () => {
+      stub(axios, "get").callsFake(async () => {
+        return {
+          data: {
+            tunnels: [
+              {
+                public_url: "test_url",
+                proto: "https",
+                config: { addr: "http://localhost:4041/" },
+              },
+            ],
+          },
+        };
+      });
+      const result = await ngrok.getNgrokHttpUrl(4041);
+      expect(result).equals("test_url");
+    });
   });
 
-  it("could get ngrok url if port in [4040, 4045)", async () => {
-    stub(axios, "get").callsFake(async () => {
-      return {
-        data: {
-          tunnels: [
-            { public_url: "xxx", proto: "https", config: { addr: "http://localhost:4041" } },
-          ],
-        },
-      };
+  describe("getNgrokTunnelFromApi", () => {
+    before(() => {
+      ngrok.__set__("delay", () => {});
     });
-    const result = await ngrok.getNgrokHttpUrl("4041");
-    expect(result).equals("xxx");
-  });
 
-  it("could get ngrok url by addr string", async () => {
-    stub(axios, "get").callsFake(async () => {
-      return {
-        data: {
-          tunnels: [
-            { public_url: "test_url", proto: "https", config: { addr: "http://localhost:4041" } },
-          ],
-        },
-      };
+    afterEach(() => {
+      restore();
     });
-    const result = await ngrok.getNgrokHttpUrl("http://localhost:4041");
-    expect(result).equals("test_url");
-  });
 
-  it("could get ngrok url by addr string and trailing slash", async () => {
-    stub(axios, "get").callsFake(async () => {
-      return {
-        data: {
-          tunnels: [
-            { public_url: "test_url", proto: "https", config: { addr: "http://localhost:4041" } },
-          ],
-        },
-      };
+    it("failed to get result from api", async () => {
+      stub(axios, "get").callsFake(async () => {
+        return undefined;
+      });
+      const result = await ngrok.getNgrokTunnelFromApi("http://127.0.0.1:4040/api/tunnels");
+      expect(result).to.be.undefined;
     });
-    const result = await ngrok.getNgrokHttpUrl("http://localhost:4041/");
-    expect(result).equals("test_url");
-  });
 
-  it("could get ngrok url by addr number and trailing slash", async () => {
-    stub(axios, "get").callsFake(async () => {
-      return {
-        data: {
-          tunnels: [
-            { public_url: "test_url", proto: "https", config: { addr: "http://localhost:4041/" } },
-          ],
-        },
-      };
+    it("failed to get result from api", async () => {
+      stub(axios, "get").callsFake(async () => {
+        throw new Error("Failed to call api");
+      });
+      const result = await ngrok.getNgrokTunnelFromApi("http://127.0.0.1:4040/api/tunnels");
+      expect(result).to.be.undefined;
     });
-    const result = await ngrok.getNgrokHttpUrl(4041);
-    expect(result).equals("test_url");
+
+    it("could get ngrok https url", async () => {
+      stub(axios, "get").callsFake(async () => {
+        return {
+          data: {
+            tunnels: [
+              { public_url: "xxx", proto: "http", config: { addr: "http://localhost:4041" } },
+            ],
+          },
+        };
+      });
+      const result = await ngrok.getNgrokTunnelFromApi("http://127.0.0.1:4040/api/tunnels");
+      expect(result).equals("xxx");
+    });
   });
 });

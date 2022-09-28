fb9c537f7436ecaa5965759516847292587c8c29
Kuojian Lu <kuojianlu@gmail.com>
2022-09-28 11:41:37 +0800

    chore: update ut

--- packages/vscode-extension/test/extension/progressHandler.test.ts
+++ packages/vscode-extension/test/extension/progressHandler.test.ts
@@ -3,6 +3,7 @@
 "use strict";
 
 import * as sinon from "sinon";
+import * as chai from "chai";
 import { window } from "vscode";
 
 import { ProgressHandler } from "../../src/progressHandler";
@@ -15,24 +16,52 @@ describe("ProgressHandler", () => {
   });
 
   it("terminal", async () => {
-    const progressHandler = new ProgressHandler("test title", 2, "terminal");
+    const progressHandler = new ProgressHandler("test title", 1, "terminal");
+    let message: string | undefined = undefined;
     sinon.stub(window, "withProgress").callsFake(async (options, task) => {
-      return await task({ report: () => {} }, new vscodeMocks.CancellationToken());
+      return await task(
+        {
+          report: (value) => {
+            message = value.message;
+          },
+        },
+        new vscodeMocks.CancellationToken()
+      );
     });
     sinon.stub(commonUtils, "sleep").callsFake(async () => {});
+    let expected =
+      "test title: [0/1] Check [terminal window](command:workbench.action.terminal.focus) for details. Prepare task. (Notice: You can reload the window and retry if task spends too long time.)";
     await progressHandler.start();
-    await progressHandler.next();
+    chai.assert.equal(message, expected);
+    await progressHandler.next("test message.");
+    expected =
+      "test title: [1/1] Check [terminal window](command:workbench.action.terminal.focus) for details. test message. (Notice: You can reload the window and retry if task spends too long time.)";
+    chai.assert.equal(message, expected);
     sinon.restore();
   });
 
   it("output", async () => {
-    const progressHandler = new ProgressHandler("test title", 2, "terminal");
+    const progressHandler = new ProgressHandler("test title", 1, "output");
+    let message: string | undefined = undefined;
     sinon.stub(window, "withProgress").callsFake(async (options, task) => {
-      return await task({ report: () => {} }, new vscodeMocks.CancellationToken());
+      return await task(
+        {
+          report: (value) => {
+            message = value.message;
+          },
+        },
+        new vscodeMocks.CancellationToken()
+      );
     });
     sinon.stub(commonUtils, "sleep").callsFake(async () => {});
+    let expected =
+      "test title: [0/1] Check [output window](command:fx-extension.showOutputChannel) for details. Prepare task. (Notice: You can reload the window and retry if task spends too long time.)";
     await progressHandler.start();
-    await progressHandler.next();
+    chai.assert.equal(message, expected);
+    await progressHandler.next("test message.");
+    expected =
+      "test title: [1/1] Check [output window](command:fx-extension.showOutputChannel) for details. test message. (Notice: You can reload the window and retry if task spends too long time.)";
+    chai.assert.equal(message, expected);
     sinon.restore();
   });
 });

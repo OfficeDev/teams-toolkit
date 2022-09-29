997ed2ed3d71b93f0ec01bd48964791bc4749205
Ning Tang <nintan@microsoft.com>
2022-09-29 10:17:51 +0800

    chore(vsc): remove choose folder A/B test codes (#6240)

--- packages/vscode-extension/src/exp/treatmentVariables.ts
+++ packages/vscode-extension/src/exp/treatmentVariables.ts
@@ -2,14 +2,11 @@ export class TreatmentVariables {
   public static readonly VSCodeConfig = "vscode";
   public static readonly EmbeddedSurvey = "embeddedsurvey";
   public static readonly CustomizeTreeview = "customizetreeview";
-  public static readonly UseFolderSelection = "usefolderselection";
   public static readonly PreviewTreeViewCommand = "previewtreeviewcommand";
 }
 
 export class TreatmentVariableValue {
   public static isEmbeddedSurvey: boolean | undefined = undefined;
-  // If this is true, users can choose default folder or a customized folder when creating a new project.
-  public static useFolderSelection: boolean | undefined = undefined;
   // If this is true, users will see a new Tree View command to preview the Teams app.
   public static previewTreeViewCommand: boolean | undefined = undefined;
 }

5ce15e93867e825557f282863b24d839e69ddeae
Jun Han <junhan@microsoft.com>
2022-10-09 16:08:51 +0800

    chore(app-setting): remove unused environment variables

--- templates/bot/ts/default/config.ts
+++ templates/bot/ts/default/config.ts
@@ -1,13 +1,6 @@
 const config = {
   botId: process.env.BOT_ID,
   botPassword: process.env.BOT_PASSWORD,
-  clientId: process.env.M365_CLIENT_ID,
-  clientSecret: process.env.M365_CLIENT_SECRET,
-  tenantId: process.env.M365_TENANT_ID,
-  authorityHost: process.env.M365_AUTHORITY_HOST,
-  initiateLoginEndpoint: process.env.INITIATE_LOGIN_ENDPOINT,
-  apiEndpoint: process.env.API_ENDPOINT,
-  applicationIdUri: process.env.M365_APPLICATION_ID_URI,
 };
 
 export default config;

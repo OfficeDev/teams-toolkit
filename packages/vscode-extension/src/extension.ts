98ff932e1aae7b4fecd37faff5cc3e6bf302cbce
wenyt <75360946+wenytang-ms@users.noreply.github.com>
2022-10-12 14:20:35 +0800

    fix: deploy aad manifest condition update (#6333)
    * fix: deploy aad manifest condition update
    
    * chore: update
    
    * test: add test
    
    * test: add test
    
    * test: add test
    
    * test: add test
    
    * test: update

--- packages/vscode-extension/src/extension.ts
+++ packages/vscode-extension/src/extension.ts
@@ -12,6 +12,7 @@ import {
   ConfigFolderName,
   FxError,
   InputConfigsFolderName,
+  ProjectSettingsV3,
   ProjectSettingsFileName,
   Result,
   TemplateFolderName,
@@ -57,7 +58,7 @@ import {
 import { loadLocalizedStrings } from "./utils/localizeUtils";
 import { ExtensionSurvey } from "./utils/survey";
 import { ExtensionUpgrade } from "./utils/upgrade";
-import { isAADEnabled } from "@microsoft/teamsfx-core/build/common/tools";
+import { hasAAD } from "@microsoft/teamsfx-core/build/common/projectSettingsHelperV3";
 import { UriHandler } from "./uriHandler";
 
 export let VS_CODE_UI: VsCodeUI;
@@ -669,10 +670,13 @@ async function initializeContextKey(isTeamsFxProject: boolean) {
 }
 
 async function setAadManifestEnabledContext() {
+  const projectSettingsConfig = await handlers.getAzureProjectConfigV3();
   vscode.commands.executeCommand(
     "setContext",
     "fx-extension.isAadManifestEnabled",
-    isAADEnabled(await handlers.getAzureSolutionSettings())
+    projectSettingsConfig
+      ? hasAAD(projectSettingsConfig.projectSettings as ProjectSettingsV3)
+      : false
   );
 }
 

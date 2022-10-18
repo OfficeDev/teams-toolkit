7307cee198c0fa438b82d30df381f73571ad10db
Zhijie Huang <zhihuan@microsoft.com>
2022-10-18 09:41:04 +0800

    chore: add notification bot project template (#6381)
    * chore: notification template
    
    * chore: add notification bot templates
    
    * chore: add notification http+timer bot
    
    * chore: add tpl ext for template file

---
+++ templates/scenarios/notification-http-trigger-js/bot/src/httpTrigger.js
@@ -0,0 +1,56 @@
+const notificationTemplate = require("./adaptiveCards/notification-default.json");
+const { AdaptiveCards } = require("@microsoft/adaptivecards-tools");
+const { bot } = require("./internal/initialize");
+
+// HTTP trigger to send notification. You need to add authentication / authorization for this API. Refer https://aka.ms/teamsfx-notification for more details.
+module.exports = async function (context, req) {
+  for (const target of await bot.notification.installations()) {
+    await target.sendAdaptiveCard(
+      AdaptiveCards.declare(notificationTemplate).render({
+        title: "New Event Occurred!",
+        appName: "Contoso App Notification",
+        description: `This is a sample http-triggered notification to ${target.type}`,
+        notificationUrl: "https://www.adaptivecards.io/",
+      })
+    );
+  }
+
+  /****** To distinguish different target types ******/
+  /** "Channel" means this bot is installed to a Team (default to notify General channel)
+  if (target.type === NotificationTargetType.Channel) {
+    // Directly notify the Team (to the default General channel)
+    await target.sendAdaptiveCard(...);
+    // List all channels in the Team then notify each channel
+    const channels = await target.channels();
+    for (const channel of channels) {
+      await channel.sendAdaptiveCard(...);
+    }
+    // List all members in the Team then notify each member
+    const members = await target.members();
+    for (const member of members) {
+      await member.sendAdaptiveCard(...);
+    }
+  }
+  **/
+
+  /** "Group" means this bot is installed to a Group Chat
+  if (target.type === NotificationTargetType.Group) {
+    // Directly notify the Group Chat
+    await target.sendAdaptiveCard(...);
+    // List all members in the Group Chat then notify each member
+    const members = await target.members();
+    for (const member of members) {
+      await member.sendAdaptiveCard(...);
+    }
+  }
+  **/
+
+  /** "Person" means this bot is installed as a Personal app
+  if (target.type === NotificationTargetType.Person) {
+    // Directly notify the individual person
+    await target.sendAdaptiveCard(...);
+  }
+  **/
+
+  context.res = {};
+};

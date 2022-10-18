7ae2a76f0cce92b040e8c7e6ecd523a8b7fba053
zhijie <zhihuan@microsoft.com>
2022-10-18 20:19:52 +0800

    chore: add sso tab templates

---
+++ templates/scenarios/sso-tab-ts/tabs/src/components/App.tsx
@@ -0,0 +1,43 @@
+// https://fluentsite.z22.web.core.windows.net/quick-start
+import { Provider, teamsTheme, Loader } from "@fluentui/react-northstar";
+import { HashRouter as Router, Redirect, Route } from "react-router-dom";
+import { useTeamsFx } from "@microsoft/teamsfx-react";
+import Privacy from "./Privacy";
+import TermsOfUse from "./TermsOfUse";
+import Tab from "./Tab";
+import "./App.css";
+import TabConfig from "./TabConfig";
+import { TeamsFxContext } from "./Context";
+import config from "./sample/lib/config";
+
+/**
+ * The main app which handles the initialization and routing
+ * of the app.
+ */
+export default function App() {
+  const { loading, theme, themeString, teamsfx } = useTeamsFx({
+    initiateLoginEndpoint: config.initiateLoginEndpoint!,
+    clientId: config.clientId!,
+  });
+  return (
+    <TeamsFxContext.Provider value={{theme, themeString, teamsfx}}>
+      <Provider theme={theme || teamsTheme} styles={{ backgroundColor: "#eeeeee" }}>
+        <Router>
+          <Route exact path="/">
+            <Redirect to="/tab" />
+          </Route>
+          {loading ? (
+            <Loader style={{ margin: 100 }} />
+          ) : (
+            <>
+              <Route exact path="/privacy" component={Privacy} />
+              <Route exact path="/termsofuse" component={TermsOfUse} />
+              <Route exact path="/tab" component={Tab} />
+              <Route exact path="/config" component={TabConfig} />
+            </>
+          )}
+        </Router>
+      </Provider>
+      </TeamsFxContext.Provider>
+  );
+}
---
+++ templates/scenarios/sso-tab-ts/tabs/src/components/sample/AzureFunctions.tsx
@@ -0,0 +1,90 @@
+import { useContext } from "react";
+import { Button, Loader } from "@fluentui/react-northstar";
+import { useData } from "@microsoft/teamsfx-react";
+import * as axios from "axios";
+import { BearerTokenAuthProvider, createApiClient, TeamsFx } from "@microsoft/teamsfx";
+import { TeamsFxContext } from "../Context";
+import config from "./lib/config";
+
+const functionName = config.apiName || "myFunc";
+
+async function callFunction(teamsfx?: TeamsFx) {
+  if (!teamsfx) {
+    throw new Error("TeamsFx SDK is not initialized.");
+  }
+  try {
+    const credential = teamsfx.getCredential();
+    const apiBaseUrl = config.apiEndpoint + "/api/";
+    // createApiClient(...) creates an Axios instance which uses BearerTokenAuthProvider to inject token to request header
+    const apiClient = createApiClient(
+      apiBaseUrl,
+      new BearerTokenAuthProvider(async () => (await credential.getToken(""))!.token));
+    const response = await apiClient.get(functionName);
+    return response.data;
+  } catch (err: unknown) {
+    if (axios.default.isAxiosError(err)) {
+      let funcErrorMsg = "";
+
+      if (err?.response?.status === 404) {
+        funcErrorMsg = `There may be a problem with the deployment of Azure Function App, please deploy Azure Function (Run command palette "Teams: Deploy to the cloud") first before running this App`;
+      } else if (err.message === "Network Error") {
+        funcErrorMsg =
+          "Cannot call Azure Function due to network error, please check your network connection status and ";
+        if (err.config?.url && err.config.url.indexOf("localhost") >= 0) {
+          funcErrorMsg += `make sure to start Azure Function locally (Run "npm run start" command inside api folder from terminal) first before running this App`;
+        } else {
+          funcErrorMsg += `make sure to provision and deploy Azure Function (Run command palette "Teams: Provision in the cloud" and "Teams: Deploy to the cloud) first before running this App`;
+        }
+      } else {
+        funcErrorMsg = err.message;
+        if (err.response?.data?.error) {
+          funcErrorMsg += ": " + err.response.data.error;
+        }
+      }
+
+      throw new Error(funcErrorMsg);
+    }
+    throw err;
+  }
+}
+
+export function AzureFunctions(props: { codePath?: string; docsUrl?: string }) {
+  const { codePath, docsUrl } = {
+    codePath: `api/${functionName}/index.ts`,
+    docsUrl: "https://aka.ms/teamsfx-azure-functions",
+    ...props,
+  };
+  const teamsfx = useContext(TeamsFxContext).teamsfx;
+  const { loading, data, error, reload } = useData(() => callFunction(teamsfx), {
+    autoLoad: false,
+  });
+  return (
+    <div>
+      <h2>Call your Azure Function</h2>
+      <p>An Azure Functions app is running. Authorize this app and click below to call it for a response:</p>
+      <Button primary content="Call Azure Function" disabled={loading} onClick={reload} />
+      {loading && (
+        <pre className="fixed">
+          {" "}
+          <Loader />{" "}
+        </pre>
+      )}
+      {!loading && !!data && !error && <pre className="fixed">{JSON.stringify(data, null, 2)}</pre>}
+      {!loading && !data && !error && <pre className="fixed"></pre>}
+      {!loading && !!error && <div className="error fixed">{(error as any).toString()}</div>}
+      <h4>How to edit the Azure Function</h4>
+      <p>
+        See the code in <code>{codePath}</code> to add your business logic.
+      </p>
+      {!!docsUrl && (
+        <p>
+          For more information, see the{" "}
+          <a href={docsUrl} target="_blank" rel="noreferrer">
+            docs
+          </a>
+          .
+        </p>
+      )}
+    </div>
+  );
+}

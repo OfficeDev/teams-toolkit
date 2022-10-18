9b47236266b88401959121da534607c4629d0d41
huajiezhang <huajiezhang@microsoft.com>
2022-10-18 12:33:04 +0800

    feat: revert

--- packages/fx-core/src/component/coordinator/index.ts
+++ packages/fx-core/src/component/coordinator/index.ts
@@ -6,7 +6,6 @@ import {
   FxError,
   InputsWithProjectPath,
   ok,
-  Platform,
   ResourceContextV3,
   Result,
 } from "@microsoft/teamsfx-api";
@@ -14,14 +13,7 @@ import { merge } from "lodash";
 import { Container } from "typedi";
 import { TelemetryEvent, TelemetryProperty } from "../../common/telemetry";
 import { environmentManager } from "../../core/environment";
-import { InvalidInputError } from "../../core/error";
 import { getQuestionsForCreateProjectV2 } from "../../core/middleware/questionModel";
-import {
-  CoreQuestionNames,
-  ProjectNamePattern,
-  QuestionRootFolder,
-  ScratchOptionNo,
-} from "../../core/question";
 import {
   ApiConnectionOptionItem,
   AzureResourceApim,
@@ -31,25 +23,17 @@ import {
   AzureSolutionQuestionNames,
   BotFeatureIds,
   CicdOptionItem,
-  M365SearchAppOptionItem,
-  M365SsoLaunchPageOptionItem,
   SingleSignOnOptionItem,
   TabFeatureIds,
   TabSPFxNewUIItem,
-  ComponentNames,
-  TabSPFxItem,
 } from "../constants";
+import { ComponentNames } from "../constants";
 import { ActionExecutionMW } from "../middleware/actionExecutionMW";
 import {
   getQuestionsForAddFeatureV3,
   getQuestionsForDeployV3,
   getQuestionsForProvisionV3,
 } from "../question";
-import * as jsonschema from "jsonschema";
-import * as path from "path";
-import { globalVars } from "../../core/globalVars";
-import fs from "fs-extra";
-import { globalStateUpdate } from "../../common/globalState";
 
 export class Coordinator {
   @hooks([
@@ -66,68 +50,9 @@ export class Coordinator {
     context: ContextV3,
     inputs: InputsWithProjectPath,
     actionContext?: ActionContext
-  ): Promise<Result<string, FxError>> {
-    const folder = inputs[QuestionRootFolder.name] as string;
-    if (!folder) {
-      return err(InvalidInputError("folder is undefined"));
-    }
-    inputs.folder = folder;
-    const scratch = inputs[CoreQuestionNames.CreateFromScratch] as string;
-    let projectPath = "";
-    const automaticNpmInstall = "automaticNpmInstall";
-    if (scratch === ScratchOptionNo.id) {
-      // create from sample
-    } else {
-      // create from new
-      const appName = inputs[CoreQuestionNames.AppName] as string;
-      if (undefined === appName) return err(InvalidInputError(`App Name is empty`, inputs));
-      const validateResult = jsonschema.validate(appName, {
-        pattern: ProjectNamePattern,
-      });
-      if (validateResult.errors && validateResult.errors.length > 0) {
-        return err(InvalidInputError(`${validateResult.errors[0].message}`, inputs));
-      }
-      projectPath = path.join(folder, appName);
-      inputs.projectPath = projectPath;
-
-      await fs.ensureDir(projectPath);
-
-      // set isVS global var when creating project
-      globalVars.isVS = inputs[CoreQuestionNames.ProgrammingLanguage] === "csharp";
-      const features = inputs.capabilities as string;
-
-      delete inputs.folder;
-
-      if (features === M365SsoLaunchPageOptionItem.id || features === M365SearchAppOptionItem.id) {
-        context.projectSetting.isM365 = true;
-        inputs.isM365 = true;
-      }
-
-      let group = "";
-
-      if (BotFeatureIds.includes(features)) {
-        // bot
-        group = "bot";
-      } else if (TabFeatureIds.includes(features)) {
-        // tab
-        group = "tab";
-      } else if (features === TabSPFxItem.id) {
-        // spfx tab
-        group = "tab";
-      }
-
-      //TODO
-
-      merge(actionContext?.telemetryProps, {
-        [TelemetryProperty.Feature]: features,
-      });
-    }
-    if (inputs.platform === Platform.VSCode) {
-      await globalStateUpdate(automaticNpmInstall, true);
-    }
-    context.projectPath = projectPath;
-
-    return ok(projectPath);
+  ): Promise<Result<undefined, FxError>> {
+    //TODO  convert inputs into templates name, call generator
+    return ok(undefined);
   }
 
   /**

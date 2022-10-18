4108110cc07a56d6473df751d4329cdb5ec6973d
zhijie <zhihuan@microsoft.com>
2022-10-18 09:40:02 +0800

    chore: fetch template from source for debugging

--- packages/fx-core/src/component/generator/generatorAction.ts
+++ packages/fx-core/src/component/generator/generatorAction.ts
@@ -9,6 +9,7 @@ import fs from "fs-extra";
 import { defaultTimeoutInMs, defaultTryLimits } from "./constant";
 import { getTemplatesFolder } from "../../folder";
 import { MissKeyError } from "./error";
+import { FeatureFlagName } from "../../common/constants";
 
 export interface GeneratorAction {
   name: string;
@@ -16,6 +17,7 @@ export interface GeneratorAction {
 }
 
 export enum GeneratorActionName {
+  FetchTemplateZipFromSourceCode = "FetchTemplateZipFromSourceCodeAction",
   FetchTemplateUrlWithTag = "FetchTemplatesUrlWithTag",
   FetchSampleUrlWithTag = "FetchSamplesUrlWithTag",
   FetchZipFromUrl = "FetchZipFromUrl",
@@ -23,9 +25,46 @@ export enum GeneratorActionName {
   Unzip = "Unzip",
 }
 
+// * This action is only for debug purpose
+export const fetchTemplateZipFromSourceCodeAction: GeneratorAction = {
+  name: GeneratorActionName.FetchTemplateZipFromSourceCode,
+  run: async (context: GeneratorContext) => {
+    const isDebugMode = () => {
+      const DebugTemplateFlag = process.env[FeatureFlagName.DebugTemplate];
+      return DebugTemplateFlag?.toLowerCase() === "true" && process.env.NODE_ENV === "development";
+    };
+
+    if (!isDebugMode()) {
+      return;
+    }
+
+    if (context.zip) {
+      return;
+    }
+
+    //! This path only works in debug mode
+    const templateSourceCodePath = path.resolve(
+      __dirname,
+      "../../../../../",
+      "templates",
+      "scenarios",
+      context.name
+    );
+
+    const zip = new AdmZip();
+    zip.addLocalFolder(templateSourceCodePath);
+
+    context.zip = zip;
+  },
+};
+
 export const fetchTemplateUrlWithTagAction: GeneratorAction = {
   name: GeneratorActionName.FetchTemplateUrlWithTag,
   run: async (context: GeneratorContext) => {
+    if (context.zip || context.zipUrl) {
+      return;
+    }
+
     context.zipUrl = await fetchTemplateZipUrl(context.name);
   },
 };
@@ -44,6 +83,10 @@ export const fetchSampleUrlWithTagAction: GeneratorAction = {
 export const fetchZipFromUrlAction: GeneratorAction = {
   name: GeneratorActionName.FetchZipFromUrl,
   run: async (context: GeneratorContext) => {
+    if (context.zip) {
+      return;
+    }
+
     if (!context.zipUrl) {
       throw new MissKeyError("zipUrl");
     }
@@ -87,6 +130,7 @@ export const unzipAction: GeneratorAction = {
 };
 
 export const TemplateActionSeq: GeneratorAction[] = [
+  fetchTemplateZipFromSourceCodeAction,
   fetchTemplateUrlWithTagAction,
   fetchZipFromUrlAction,
   fetchTemplateZipFromLocalAction,

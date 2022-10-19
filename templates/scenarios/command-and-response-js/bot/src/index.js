a5cac1e2df934cade56b72623e32bccc5f809e23
Zhijie Huang <zhihuan@microsoft.com>
2022-10-19 11:20:39 +0800

    chore: add command response bot template (#6402)

---
+++ templates/scenarios/command-and-response-js/bot/src/index.js
@@ -0,0 +1,20 @@
+// Create HTTP server.
+const restify = require("restify");
+const { commandBot } = require("./internal/initialize");
+
+// This template uses `restify` to serve HTTP responses.
+// Create a restify server.
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+  console.log(`\nBot Started, ${server.name} listening to ${server.url}`);
+});
+
+// Register an API endpoint with `restify`. Teams sends messages to your application
+// through this endpoint.
+//
+// The Teams Toolkit bot registration configures the bot with `/api/messages` as the
+// Bot Framework endpoint. If you customize this route, update the Bot registration
+// in `templates/azure/provision/botservice.bicep`.
+server.post("/api/messages", async (req, res) => {
+  await commandBot.requestHandler(req, res);
+});

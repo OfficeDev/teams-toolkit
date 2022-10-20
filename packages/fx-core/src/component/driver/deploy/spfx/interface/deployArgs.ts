c10b77e4ba7e5d446c1dec188aa539ff3bc9e881
Alive-Fish <zhiyou@microsoft.com>
2022-10-20 16:00:39 +0800

    chore: add telemetry and progress in spfx deploy driver (#6425)
    * chore: add telemetry and progress in spfx deploy driver
    
    * fix: unit test

--- packages/fx-core/src/component/driver/deploy/spfx/interface/deployArgs.ts
+++ packages/fx-core/src/component/driver/deploy/spfx/interface/deployArgs.ts
@@ -2,6 +2,15 @@
 // Licensed under the MIT license.
 
 export interface DeploySPFxArgs {
+  /**
+   * create app catalog if there is no valid one in the
+   * current Microsoft 365 tenant, default to 'false'
+   */
   createAppCatalogIfNotExist: boolean;
+
+  /**
+   * the 'package-solution.json' path of the SPFx project,
+   * will use it to get the bundled zipped file path.
+   */
   packageSolutionPath: string;
 }

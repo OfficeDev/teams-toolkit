c2b120226c216558d412f9f769d239bfa3d6c690
IvanHe <ivan1377@163.com>
2022-10-20 14:53:25 +0800

    chore: add more cases

---
+++ packages/fx-core/tests/component/resource/botService/botRegistration/factory.test.ts
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+import "mocha";
+import * as chai from "chai";
+import * as sinon from "sinon";
+import { RemoteBotRegistration } from "../../../../../src/component/resource/botService/botRegistration/remoteBotRegistration";
+import {
+  BotRegistrationFactory,
+  BotRegistrationKind,
+} from "../../../../../src/component/resource/botService/botRegistration/factory";
+import { expect } from "chai";
+import { LocalBotRegistration } from "../../../../../src/component/resource/botService/botRegistration/localBotRegistration";
+
+describe("Test Factory", () => {
+  it("should return an instance of LocalBotRegistration", async () => {
+    const botReg = BotRegistrationFactory.create(BotRegistrationKind.Local);
+    expect(botReg).to.be.instanceOf(LocalBotRegistration);
+  });
+  it("should return an instance of RemoteBotRegistration", async () => {
+    const botReg = BotRegistrationFactory.create(BotRegistrationKind.Remote);
+    expect(botReg).to.be.instanceOf(RemoteBotRegistration);
+  });
+});

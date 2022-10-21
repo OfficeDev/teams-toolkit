570f256a8ddc4272b42819dd03ca67debfce45e9
Ivan He <ruhe@microsoft.com>
2022-10-21 08:41:45 +0800

    chore: add cases

---
+++ packages/fx-core/tests/component/resource/botService/messages.test.ts
@@ -0,0 +1,43 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { expect } from "chai";
+import "mocha";
+import { getDefaultString, getLocalizedString } from "../../../../src/common/localizeUtils";
+import { Messages } from "../../../../src/component/resource/botService/messages";
+describe("Test Messages", () => {
+  it("SomethingIsMissing", () => {
+    const sth: string = "sth";
+    const messages = Messages.SomethingIsMissing(sth);
+    expect(messages[0]).to.be.equals(getDefaultString("plugins.bot.SomethingIsMissing", sth))
+    expect(messages[1]).to.be.equals(getLocalizedString("plugins.bot.SomethingIsMissing", sth));
+  });
+
+  it("FailToProvisionSomeResource", () => {
+    const resource: string = "webapp";
+    const messages = Messages.FailToProvisionSomeResource(resource);
+    expect(messages[0]).to.be.equals(getDefaultString("plugins.bot.FailedToProvision", resource))
+    expect(messages[1]).to.be.equals(getLocalizedString("plugins.bot.FailedToProvision", resource));
+  });
+
+  it("FailToUpdateConfigs", () => {
+    const sth: string = "sth";
+    const messages = Messages.FailToUpdateConfigs(sth);
+    expect(messages[0]).to.be.equals(getDefaultString("plugins.bot.FailedToUpdateConfigs", sth))
+    expect(messages[1]).to.be.equals(getLocalizedString("plugins.bot.FailedToUpdateConfigs", sth));
+  });
+
+  it("SomethingIsMissing", () => {
+    const sth: string = "sth";
+    const messages = Messages.SomethingIsMissing(sth);
+    expect(messages[0]).to.be.equals(getDefaultString("plugins.bot.SomethingIsMissing", sth))
+    expect(messages[1]).to.be.equals(getLocalizedString("plugins.bot.SomethingIsMissing", sth));
+  });
+
+  it("SomethingIsMissing", () => {
+    const sth: string = "sth";
+    const messages = Messages.SomethingIsMissing(sth);
+    expect(messages[0]).to.be.equals(getDefaultString("plugins.bot.SomethingIsMissing", sth))
+    expect(messages[1]).to.be.equals(getLocalizedString("plugins.bot.SomethingIsMissing", sth));
+  });
+});

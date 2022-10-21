ab62ced120a14f8c820a79b2ac463195e48d28cf
zhijie <zhihuan@microsoft.com>
2022-10-21 15:07:09 +0800

    chore: workflow bot

---
+++ templates/scenarios/workflow-bot-js/bot/src/internal/initialize.js
@@ -0,0 +1,26 @@
+const { ConversationBot } = require("@microsoft/teamsfx");
+const { DoStuffActionHandler } = require("../cardActions/doStuffActionHandler");
+const { HelloWorldCommandHandler } = require("../commands/helloworldCommandHandler");
+const { default: config } = require("./config");
+
+// Create the conversation bot and register the command and card action handlers for your app.
+const conversationBot = new ConversationBot({
+  // The bot id and password to create BotFrameworkAdapter.
+  // See https://aka.ms/about-bot-adapter to learn more about adapters.
+  adapterConfig: {
+    appId: config.botId,
+    appPassword: config.botPassword,
+  },
+  command: {
+    enabled: true,
+    commands: [new HelloWorldCommandHandler()],
+  },
+  cardAction: {
+    enabled: true,
+    actions: [new DoStuffActionHandler()],
+  },
+});
+
+module.exports = {
+  conversationBot,
+};

e5a33841332fb498e77953799c67a9c465b5a432
Yiqing Zhao <cissyzhao99@gmail.com>
2022-10-24 17:06:34 +0800

    chore: update .net sdk

--- packages/dotnet-sdk/src/TeamsFx/wwwroot/jsInterop.js
+++ packages/dotnet-sdk/src/TeamsFx/wwwroot/jsInterop.js
@@ -1,58 +1,18 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-
 export function initialize() {
-    const initializeTeamsSdkTimeoutInMillisecond = 5000;
-    let initialized = false;
-    return new Promise((resolve, reject) => {
-        try {
-            microsoftTeams.initialize(() => {
-                initialized = true;
-                resolve();
-            });
-        } catch (e) {
-            reject(e);
-        }
-        // If the code not running in Teams, the initialize callback function would never trigger
-        setTimeout(() => {
-            if (!initialized) {
-                reject("timeout");
-            }
-        }, initializeTeamsSdkTimeoutInMillisecond);
-    });
+    microsoftTeams.app.initialize();
 }
 
 export function authenticate(url) {
-    return new Promise((resolve, reject) => {
-        microsoftTeams.authentication.authenticate({
-            url: url,
-            width: 600,
-            height: 535,
-            successCallback: function (token) {
-                resolve(token);
-            },
-            failureCallback: function (reason) {
-                reject(reason);
-            },
-        });
+    microsoftTeams.authentication.authenticate({
+        url: url,
+        width: 600,
+        height: 535,
     });
 }
 
-export function getAuthToken() {
-    return new Promise((resolve, reject) => {
-        try {
-            microsoftTeams.authentication.getAuthToken({
-                successCallback: function (token) {
-                    resolve(token);
-                },
-                failureCallback: function (reason) {
-                    reject(reason);
-                },
-                resources: [],
-            });
-        } catch (e) {
-            reject(e);
-        }
-    });
+export function getAuthToken(...resources) {
+    microsoftTeams.authentication.getAuthToken(...resources);
 }

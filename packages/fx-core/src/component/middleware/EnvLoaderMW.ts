d85728c8e34dee73827e72aa60c5850cf318de76
huajiezhang <huajiezhang@microsoft.com>
2022-10-24 15:46:58 +0800

    feat: decript

--- packages/fx-core/src/component/middleware/EnvLoaderMW.ts
+++ packages/fx-core/src/component/middleware/EnvLoaderMW.ts
@@ -4,16 +4,13 @@ import { HookContext, Middleware, NextFunction } from "@feathersjs/hooks";
 import { Inputs } from "@microsoft/teamsfx-api";
 import { envUtil } from "../utils/envUtil";
 
-/**
- * in case there're some uncatched exceptions, this middleware will act as a guard
- * to catch exceptions and return specific error.
- */
 export const EnvLoaderMW: Middleware = async (ctx: HookContext, next: NextFunction) => {
   const inputs = ctx.arguments[ctx.arguments.length - 1] as Inputs;
   const env = inputs.env;
   const projectPath = inputs.projectPath;
   if (projectPath && env) {
     await envUtil.readEnv(projectPath, env);
+    //TODO decrypt
   }
   await next();
 };

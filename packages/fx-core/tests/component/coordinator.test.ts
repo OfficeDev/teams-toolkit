21ac696f6dea6c1f6c81974630ad73ce0dbf5efe
huajiezhang <huajiezhang@microsoft.com>
2022-10-27 11:08:59 +0800

    feat: up

--- packages/fx-core/tests/component/coordinator.test.ts
+++ packages/fx-core/tests/component/coordinator.test.ts
@@ -157,4 +157,69 @@ describe("component coordinator test", () => {
     const res = await fxCore.provisionResources(inputs);
     assert.isTrue(res.isOk());
   });
+
+  it("provision happy path (with inputs)", async () => {
+    const mockProjectModel: ProjectModel = {
+      registerApp: {
+        name: "configureApp",
+        run: async (ctx: DriverContext) => {
+          return ok({
+            env: new Map(),
+            unresolvedPlaceHolders: ["AZURE_SUBSCRIPTION_ID", "AZURE_RESOURCE_GROUP_NAME"],
+          });
+        },
+      },
+    };
+    sandbox.stub(YamlParser.prototype, "parse").resolves(ok(mockProjectModel));
+    sandbox
+      .stub(mockProjectModel.registerApp!, "run")
+      .onFirstCall()
+      .resolves(
+        ok({
+          env: new Map(),
+          unresolvedPlaceHolders: ["AZURE_SUBSCRIPTION_ID", "AZURE_RESOURCE_GROUP_NAME"],
+        })
+      )
+      .onSecondCall()
+      .resolves(
+        ok({
+          env: new Map(),
+          unresolvedPlaceHolders: [],
+        })
+      );
+    sandbox.stub(envUtil, "listEnv").resolves(ok(["dev", "prod"]));
+    sandbox.stub(envUtil, "readEnv").resolves(ok({}));
+    sandbox.stub(envUtil, "writeEnv").resolves(ok(undefined));
+    sandbox.stub(provisionUtils, "ensureSubscription").resolves(
+      ok({
+        subscriptionId: "mockSubId",
+        tenantId: "mockTenantId",
+        subscriptionName: "mockSubName",
+      })
+    );
+
+    sandbox.stub(provisionUtils, "ensureResourceGroup").resolves(
+      ok({
+        createNewResourceGroup: true,
+        name: "test-rg",
+        location: "East US",
+      })
+    );
+    sandbox.stub(tools.ui, "selectOption").callsFake(async (config) => {
+      if (config.name === "env") {
+        return ok({ type: "success", result: "dev" });
+      } else {
+        return ok({ type: "success", result: "" });
+      }
+    });
+    const inputs: Inputs = {
+      platform: Platform.VSCode,
+      projectPath: ".",
+      subscription: "mockSubId",
+      "resource-group": "test-rg",
+    };
+    const fxCore = new FxCore(tools);
+    const res = await fxCore.provisionResources(inputs);
+    assert.isTrue(res.isOk());
+  });
 });
--- packages/fx-core/tests/component/envUtil.test.ts
+++ packages/fx-core/tests/component/envUtil.test.ts
@@ -1,5 +1,15 @@
 import { hooks } from "@feathersjs/hooks/lib";
-import { FxError, Inputs, ok, Platform, Result, Settings } from "@microsoft/teamsfx-api";
+import {
+  err,
+  FxError,
+  Inputs,
+  ok,
+  Platform,
+  Result,
+  Settings,
+  UserCancelError,
+  UserError,
+} from "@microsoft/teamsfx-api";
 import { assert } from "chai";
 import fs from "fs-extra";
 import "mocha";
@@ -10,7 +20,11 @@ import { LocalCrypto } from "../../src/core/crypto";
 import { EnvLoaderMW, EnvWriterMW } from "../../src/component/middleware/envMW";
 import { ContextInjectorMW } from "../../src/core/middleware/contextInjector";
 import { CoreHookContext } from "../../src/core/types";
+import { MockTools } from "../core/utils";
+import { setTools } from "../../src/core/globalVars";
 describe("env utils", () => {
+  const tools = new MockTools();
+  setTools(tools);
   const sandbox = sinon.createSandbox();
   const cryptoProvider = new LocalCrypto("mockProjectId");
   const decrypted = "123";
@@ -33,6 +47,13 @@ describe("env utils", () => {
     assert.isTrue(res.isOk());
     assert.equal(process.env.SECRET_ABC, decrypted);
   });
+  it("envUtil.readEnv fail", async () => {
+    sandbox
+      .stub(settingsUtil, "readSettings")
+      .resolves(err(new UserError({ source: "test", name: "TestError", message: "message" })));
+    const res = await envUtil.readEnv(".", "dev");
+    assert.isTrue(res.isErr());
+  });
   it("envUtil.writeEnv", async () => {
     let value = "";
     sandbox.stub(fs, "writeFile").callsFake(async (file: fs.PathLike | number, data: any) => {
@@ -49,8 +70,22 @@ describe("env utils", () => {
     assert.isTrue(decRes.isOk());
     assert.equal(decRes.value, decrypted);
   });
-
-  it("EnvLoaderMW", async () => {
+  it("envUtil.writeEnv failed", async () => {
+    sandbox
+      .stub(settingsUtil, "readSettings")
+      .resolves(err(new UserError({ source: "test", name: "TestError", message: "message" })));
+    const res = await envUtil.writeEnv(".", "dev", { SECRET_ABC: decrypted });
+    assert.isTrue(res.isErr());
+  });
+  it("envUtil.listEnv", async () => {
+    sandbox.stub(fs, "readdir").resolves([".env.dev", ".env.prod"] as any);
+    const res = await envUtil.listEnv(".");
+    assert.isTrue(res.isOk());
+    if (res.isOk()) {
+      assert.deepEqual(res.value, ["dev", "prod"]);
+    }
+  });
+  it("EnvLoaderMW success", async () => {
     const encRes = await cryptoProvider.encrypt(decrypted);
     if (encRes.isErr()) throw encRes.error;
     const encrypted = encRes.value;
@@ -75,7 +110,71 @@ describe("env utils", () => {
     assert.isTrue(res.isOk());
     assert.equal(process.env.SECRET_ABC, decrypted);
   });
-
+  it("EnvLoaderMW fail without projectPath", async () => {
+    class MyClass {
+      async myMethod(inputs: Inputs): Promise<Result<any, FxError>> {
+        return ok(undefined);
+      }
+    }
+    hooks(MyClass, {
+      myMethod: [EnvLoaderMW],
+    });
+    const my = new MyClass();
+    const inputs = {
+      platform: Platform.VSCode,
+      env: "dev",
+    };
+    const res = await my.myMethod(inputs);
+    assert.isTrue(res.isErr());
+    if (res.isErr()) {
+      assert.equal(res.error.name, "NoProjectOpenedError");
+    }
+  });
+  it("EnvLoaderMW fail with listEnv Error", async () => {
+    sandbox
+      .stub(envUtil, "listEnv")
+      .resolves(err(new UserError({ source: "test", name: "TestError", message: "message" })));
+    class MyClass {
+      async myMethod(inputs: Inputs): Promise<Result<any, FxError>> {
+        return ok(undefined);
+      }
+    }
+    hooks(MyClass, {
+      myMethod: [EnvLoaderMW],
+    });
+    const my = new MyClass();
+    const inputs = {
+      platform: Platform.VSCode,
+      projectPath: ".",
+    };
+    const res = await my.myMethod(inputs);
+    assert.isTrue(res.isErr());
+    if (res.isErr()) {
+      assert.equal(res.error.name, "TestError");
+    }
+  });
+  it("EnvLoaderMW cancel", async () => {
+    sandbox.stub(envUtil, "listEnv").resolves(ok(["dev", "prod"]));
+    sandbox.stub(tools.ui, "selectOption").resolves(err(UserCancelError));
+    class MyClass {
+      async myMethod(inputs: Inputs): Promise<Result<any, FxError>> {
+        return ok(undefined);
+      }
+    }
+    hooks(MyClass, {
+      myMethod: [EnvLoaderMW],
+    });
+    const my = new MyClass();
+    const inputs = {
+      platform: Platform.VSCode,
+      projectPath: ".",
+    };
+    const res = await my.myMethod(inputs);
+    assert.isTrue(res.isErr());
+    if (res.isErr()) {
+      assert.equal(res.error.name, "UserCancel");
+    }
+  });
   it("EnvWriterMW", async () => {
     let value = "";
     sandbox.stub(fs, "writeFile").callsFake(async (file: fs.PathLike | number, data: any) => {

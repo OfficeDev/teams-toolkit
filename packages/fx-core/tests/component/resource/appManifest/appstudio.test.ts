a3d5930e1ab916f80438c9aba86d5a14067f0d2c
yuqizhou77 <86260893+yuqizhou77@users.noreply.github.com>
2022-11-02 14:17:25 +0800

    chore: get manifest, icon and languages  (#6587)
    * chore: draft
    
    * chore: minor
    
    * chore: draft
    
    * chore: first
    
    * chore: add ut

--- packages/fx-core/tests/component/resource/appManifest/appstudio.test.ts
+++ packages/fx-core/tests/component/resource/appManifest/appstudio.test.ts
@@ -6,8 +6,13 @@ import * as chai from "chai";
 import sinon from "sinon";
 import { MockLogProvider, MockM365TokenProvider } from "../../../core/utils";
 import { err, ok, UserError } from "@microsoft/teamsfx-api";
-import { checkIfAppInDifferentAcountSameTenant } from "../../../../src/component/resource/appManifest/appStudio";
+import {
+  checkIfAppInDifferentAcountSameTenant,
+  getAppPackage,
+} from "../../../../src/component/resource/appManifest/appStudio";
 import { AppStudioClient } from "../../../../src/component/resource/appManifest/appStudioClient";
+import AdmZip from "adm-zip";
+import { RetryHandler } from "../../../../src/component/resource/appManifest/utils/utils";
 
 describe("appStudio", () => {
   const sandbox = sinon.createSandbox();
@@ -81,6 +86,92 @@ describe("appStudio", () => {
       if (res.isOk()) {
         chai.assert.isFalse(res.value);
       }
+    });
+  });
+
+  describe("getAppPackage", () => {
+    const logger = new MockLogProvider();
+    const teamsAppId = "teams";
+    const m365TokenProvider = new MockM365TokenProvider();
+
+    afterEach(() => {
+      sandbox.restore();
+    });
+
+    it("get package successfully", async () => {
+      m365TokenProvider.getAccessToken = sandbox.stub().returns(ok("token"));
+      const zip = new AdmZip();
+      zip.addFile("manifest.json", new Buffer(""));
+      zip.addFile("color.png", new Buffer(""));
+      zip.addFile("outline.png", new Buffer(""));
+      zip.addFile("zh-cn.json", new Buffer(""));
+      const archivedFile = zip.toBuffer();
+      sandbox.stub(RetryHandler, "Retry").resolves({
+        data: archivedFile,
+      });
+
+      const res = await getAppPackage(teamsAppId, m365TokenProvider, logger);
+      chai.assert.isTrue(res.isOk());
+
+      if (res.isOk()) {
+        chai.assert.isTrue(res.value.manifest !== undefined);
+        chai.assert.isTrue(res.value.icons !== undefined);
+        chai.assert.isTrue(res.value.icons?.color !== undefined);
+        chai.assert.isTrue(res.value.icons?.outline !== undefined);
+        chai.assert.isTrue(res.value.languages !== undefined);
+      }
+    });
+
+    it("get package successfully with unsupported file", async () => {
+      m365TokenProvider.getAccessToken = sandbox.stub().returns(ok("token"));
+      const zip = new AdmZip();
+      zip.addFile("manifest.json", new Buffer(""));
+      zip.addFile("color.png", new Buffer(""));
+      zip.addFile("outline.png", new Buffer(""));
+      zip.addFile("idk.json", new Buffer(""));
+      const archivedFile = zip.toBuffer();
+      sandbox.stub(RetryHandler, "Retry").resolves({
+        data: archivedFile,
+      });
+      const loggerSpy = sandbox.stub(logger, "warning").resolves();
+
+      const res = await getAppPackage(teamsAppId, m365TokenProvider, logger);
+      chai.assert.isTrue(res.isOk());
+
+      if (res.isOk()) {
+        chai.assert.isTrue(loggerSpy.called);
+        chai.assert.isUndefined(res.value.languages);
+      }
+    });
+
+    it("get token error: returns error", async () => {
+      m365TokenProvider.getAccessToken = sandbox
+        .stub()
+        .returns(err(new UserError("token", "token", "", "")));
+
+      const res = await getAppPackage(teamsAppId, m365TokenProvider, logger);
+      chai.assert.isTrue(res.isErr());
+      if (res.isErr()) {
+        chai.assert.equal(res.error.name, "token");
+      }
+    });
+
+    it("get package failed due to api", async () => {
+      m365TokenProvider.getAccessToken = sandbox.stub().returns(ok("token"));
+
+      sandbox.stub(RetryHandler, "Retry").throws();
+
+      const res = await getAppPackage(teamsAppId, m365TokenProvider, logger);
+      chai.assert.isTrue(res.isErr());
+    });
+
+    it("get package empty response", async () => {
+      m365TokenProvider.getAccessToken = sandbox.stub().returns(ok("token"));
+
+      sandbox.stub(RetryHandler, "Retry").resolves({});
+
+      const res = await getAppPackage(teamsAppId, m365TokenProvider, logger);
+      chai.assert.isTrue(res.isErr());
     });
   });
 });

a0c78a49f2cb5b0453c714303491323d3efa14ae
zhijie <zhihuan@microsoft.com>
2022-11-03 13:38:11 +0800

    chore: template zip script

--- .github/scripts/template-zip-autogen-v3.sh
+++ .github/scripts/template-zip-autogen-v3.sh
@@ -14,15 +14,18 @@ TEMPLATE_OUTPUT_DIR=$1
 mkdir -p ${TEMPLATE_OUTPUT_DIR}
 
 TEMPLATE_LIST=$(jq -r '.templatesV3[]' ./templates/package.json)
+TEMPLATE_PATHS=$(ls -d ./templates/scenarios/*/)
 
-for TEMPLATE in ${TEMPLATE_LIST[@]}; do
+for TEMPLATE_PATH in ${TEMPLATE_PATHS[@]}; do
 
-    if [ ! -d ./templates/scenarios/${TEMPLATE} ]; then
-        echo "The folder ./templates/scenarios/${TEMPLATE}  does not exist."
+    if [ ! -d ${TEMPLATE_PATH} ]; then
+        echo "The folder ${TEMPLATE_PATH} does not exist."
         exit -1
     fi
 
-    cd ./templates/scenarios/${TEMPLATE} 
+    ARR=(${TEMPLATE_PATH//// }) # split template path by '/'
+    TEMPLATE=(${ARR[3]})
+    cd ${TEMPLATE_PATH}
     zip -rq ${TEMPLATE_OUTPUT_DIR}/${TEMPLATE}.zip .
     cd -
 done

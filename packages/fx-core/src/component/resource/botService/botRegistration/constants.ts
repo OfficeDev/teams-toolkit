17ce71b59935b307f8404cc97e1b26a02cdea032
Ivan Jobs <ruhe@microsoft.com>
2022-11-03 10:45:21 +0800

    chore(v3): add logging telemetry botaad driver (#6551)
    chore(v3): add logging, telemetry, progressBar.

---
+++ packages/fx-core/src/component/resource/botService/botRegistration/constants.ts
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export const logMessageKeys = {
+  startCreateBotAadApp: "botRegistration.log.startCreateBotAadApp",
+  successCreateBotAadApp: "botRegistration.log.successCreateBotAadApp",
+  skipCreateBotAadApp: "botRegistration.log.skipCreateBotAadApp",
+  failCreateBotAadApp: "botRegistration.log.failCreateBotAadApp",
+};
+
+export const progressBarKeys = {
+  creatingBotAadApp: "botRegistration.ProgressBar.creatingBotAadApp",
+};

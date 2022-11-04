ac02937839877d980b98989e84b48ad962feb743
Yuqi Zhou <yuqzho@microsoft.com>
2022-11-04 13:11:32 +0800

    chore: fix ut

--- packages/fx-core/tests/component/coordinator.test.ts
+++ packages/fx-core/tests/component/coordinator.test.ts
@@ -331,7 +331,7 @@ describe("component coordinator test", () => {
         tenantUserName: "mockM365UserName",
       })
     );
-    // sandbox.stub(provisionUtils, "askForProvisionConsentV3").resolves(ok(undefined));
+    sandbox.stub(provisionUtils, "askForProvisionConsentV3").resolves(ok(undefined));
     sandbox.stub(tools.tokenProvider.azureAccountProvider, "getSelectedSubscription").resolves({
       subscriptionId: "mockSubId",
       tenantId: "mockTenantId",

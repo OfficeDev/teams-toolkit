cd882b9b4369a47419a0adbe07025eaa29cbfd88
Ning Liu <nliu@microsoft.com>
2022-11-07 15:43:05 +0800

    fix: ut coverage

--- packages/vscode-extension/test/extension/hoverProvider.test.ts
+++ packages/vscode-extension/test/extension/hoverProvider.test.ts
@@ -4,7 +4,10 @@
 import * as chai from "chai";
 import * as sinon from "sinon";
 import * as vscode from "vscode";
+import { v4 } from "uuid";
 import { ProjectConfigV3, ok } from "@microsoft/teamsfx-api";
+import { envUtil } from "@microsoft/teamsfx-core";
+import * as commonTools from "@microsoft/teamsfx-core/build/common/tools";
 import { ManifestTemplateHoverProvider } from "../../src/hoverProvider";
 import { vscodeHelper } from "../../src/debug/depsChecker/vscodeHelper";
 import * as handlers from "../../src/handlers";
@@ -57,6 +60,68 @@ describe("Manifest template hover", async () => {
   beforeEach(() => {
     sinon.stub(handlers, "core").value(new MockCore());
     sinon.stub(MockCore.prototype, "getProjectConfigV3").resolves(ok(config));
+    sinon.stub(vscodeHelper, "isDotnetCheckerEnabled").returns(false);
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it("hover - match", async () => {
+    const hoverProvider = new ManifestTemplateHoverProvider();
+    const position = new vscode.Position(5, 15);
+    const cts = new vscode.CancellationTokenSource();
+    const hover = await hoverProvider.provideHover(document, position, cts.token);
+
+    chai.assert.isTrue(hover !== undefined);
+    if (hover !== undefined) {
+      chai.assert.isTrue(hover.contents.length > 0);
+    }
+  });
+
+  it("hover-undefined", async () => {
+    const hoverProvider = new ManifestTemplateHoverProvider();
+    const position = new vscode.Position(1, 1);
+    const cts = new vscode.CancellationTokenSource();
+    const hover = await hoverProvider.provideHover(document, position, cts.token);
+
+    chai.assert.isTrue(hover === undefined);
+  });
+});
+
+describe("Manifest template hover - V3", async () => {
+  const text = `{
+    "$schema": "https://developer.microsoft.com/en-us/json-schemas/teams/v1.14/MicrosoftTeams.schema.json",
+    "manifestVersion": "1.14",
+    "version": "1.0.0",
+    "id": "\${{TEAMS_APP_ID}}"
+  }`;
+  const document: vscode.TextDocument = {
+    fileName: "manifest.template.json",
+    getText: () => {
+      return text;
+    },
+    lineAt: (line: number) => {
+      const lines = text.split("\n");
+      return {
+        lineNumber: line,
+        text: lines[line - 1],
+      };
+    },
+    getWordRangeAtPosition: (position: vscode.Position, regex?: RegExp) => {
+      return undefined;
+    },
+  } as any;
+
+  beforeEach(() => {
+    sinon.stub(commonTools, "isV3Enabled").returns(true);
+    sinon.stub(handlers, "core").value(new MockCore());
+    sinon.stub(envUtil, "listEnv").resolves(ok(["local", "dev"]));
+    sinon.stub(envUtil, "readEnv").resolves(
+      ok({
+        ["TEAMST_APP_ID"]: v4(),
+      })
+    );
     sinon.stub(vscodeHelper, "isDotnetCheckerEnabled").returns(false);
   });
 

67faa93e3b75d41db142da0656399130ef850515
Zhijie Huang <zhihuan@microsoft.com>
2022-11-07 14:01:17 +0800

    chore(template v3): add nodejs bot templates (#6650)
    * chore: add default bot
    
    * chore: add message extension
    
    * chore: workflow bot
    
    * chore: .env.teamsfx.local
    
    * chore: sync changes

---
+++ templates/scenarios/workflow-bot-ts/bot/src/index.ts
@@ -0,0 +1,11 @@
+import * as restify from "restify";
+import { conversationBot } from "./internal/initialize";
+
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+  console.log(`\nBot Started, ${server.name} listening to ${server.url}`);
+});
+
+server.post("/api/messages", async (req, res) => {
+  await conversationBot.requestHandler(req, res);
+});

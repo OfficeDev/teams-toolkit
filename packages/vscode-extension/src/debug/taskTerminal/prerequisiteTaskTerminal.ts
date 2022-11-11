80e95c622f00e5c5e27016dd43e45171d2164beb
Alex Wang <ice_b0und@hotmail.com>
2022-11-11 12:09:31 +0800

    feat(video-filter): add VxTestAppChecker boilerplate code

--- packages/vscode-extension/src/debug/taskTerminal/prerequisiteTaskTerminal.ts
+++ packages/vscode-extension/src/debug/taskTerminal/prerequisiteTaskTerminal.ts
@@ -19,9 +19,19 @@ import { checkAndInstallForTask } from "../prerequisitesHandler";
 import { BaseTaskTerminal } from "./baseTaskTerminal";
 import { Prerequisite, TaskDefaultValue } from "@microsoft/teamsfx-core/build/common/local";
 
+export interface PrerequisiteInstallOptionsVxTestApp {
+  version: string;
+}
+
+export interface PrerequisiteInstallOptions {
+  // TODO(aochengwang): Maybe change test app name
+  vxTestApp: PrerequisiteInstallOptionsVxTestApp;
+}
+
 export interface PrerequisiteArgs {
   prerequisites?: string[];
   portOccupancy?: number[];
+  installOptions?: PrerequisiteInstallOptions;
 }
 
 export class PrerequisiteTaskTerminal extends BaseTaskTerminal {
@@ -77,6 +87,7 @@ export class PrerequisiteTaskTerminal extends BaseTaskTerminal {
     const res = await checkAndInstallForTask(
       this.args.prerequisites ?? [],
       this.args.portOccupancy,
+      this.args.installOptions,
       telemetryProperties
     );
     const duration = this.getDurationInSeconds();

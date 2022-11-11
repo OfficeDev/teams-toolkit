2c7616f7f547b4a6e1742b58f910db6e538306dd
Huajie Zhang <huajiezhang@microsoft.com>
2022-11-11 09:58:31 +0800

    chore(core): dotenv serialize and deserialize to keep comments (#6753)
    * feat: dotenv serialize/deserialize
    
    * feat: up
    
    * feat: append new keys
    
    * feat: append new keys
    
    * feat: append new keys
    
    * feat: append new keys
    
    * feat: append new keys

--- packages/fx-core/tests/component/envUtil.test.ts
+++ packages/fx-core/tests/component/envUtil.test.ts
@@ -14,7 +14,7 @@ import { assert } from "chai";
 import fs from "fs-extra";
 import "mocha";
 import * as sinon from "sinon";
-import { envUtil } from "../../src/component/utils/envUtil";
+import { dotenvUtil, envUtil } from "../../src/component/utils/envUtil";
 import { settingsUtil } from "../../src/component/utils/settingsUtil";
 import { LocalCrypto } from "../../src/core/crypto";
 import { EnvLoaderMW, EnvWriterMW } from "../../src/component/middleware/envMW";
@@ -61,7 +61,15 @@ describe("env utils", () => {
     assert.isTrue(res.isOk());
     assert.equal(process.env.SECRET_ABC, decrypted);
   });
-
+  it("envUtil.readEnv silent", async () => {
+    sandbox.stub(fs, "pathExists").resolves(false);
+    sandbox.stub(settingsUtil, "readSettings").resolves(ok(mockSettings));
+    const res = await envUtil.readEnv(".", "dev", false, true);
+    assert.isTrue(res.isOk());
+    if (res.isOk()) {
+      assert.deepEqual(res.value, {});
+    }
+  });
   it("envUtil.readEnv - loadToProcessEnv false", async () => {
     const encRes = await cryptoProvider.encrypt(decrypted);
     if (encRes.isErr()) throw encRes.error;
@@ -358,5 +366,36 @@ describe("env utils", () => {
     };
     const res = await my.myMethod(inputs);
     assert.isTrue(res.isErr());
+  });
+
+  it("dotenvUtil deserialize", async () => {
+    const res = dotenvUtil.deserialize("#COMMENT\n\r\nKEY=VALUE");
+    assert.deepEqual(res, {
+      lines: ["#COMMENT", "", "", { key: "KEY", value: "VALUE" }],
+      obj: { KEY: "VALUE" },
+    });
+  });
+  it("dotenvUtil deserialize empty", async () => {
+    const res = dotenvUtil.deserialize("");
+    assert.deepEqual(res, {
+      lines: [""],
+      obj: {},
+    });
+  });
+  it("dotenvUtil serialize with lines", async () => {
+    const parsed = {
+      lines: ["#COMMENT", "", "", { key: "KEY2", value: "VALUE2" }],
+      obj: { KEY: "VALUE", KEY2: "VALUE2" },
+    };
+    const str = dotenvUtil.serialize(parsed);
+    assert.equal(str, "#COMMENT\n\n\nKEY2=VALUE2\nKEY=VALUE");
+  });
+
+  it("dotenvUtil serialize without lines", async () => {
+    const parsed = {
+      obj: { KEY: "VALUE", KEY2: "VALUE2" },
+    };
+    const str = dotenvUtil.serialize(parsed);
+    assert.equal(str, "KEY=VALUE\nKEY2=VALUE2");
   });
 });

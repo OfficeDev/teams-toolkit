72caeb7eef4900012d1e077d19be4677fe506ab0
Zhiyu You <zhiyou@microsoft.com>
2022-11-14 11:23:54 +0800

    test(server): fix unit test

--- packages/server/tests/serverConnections.test.ts
+++ packages/server/tests/serverConnections.test.ts
@@ -85,15 +85,31 @@ describe("serverConnections", () => {
 
   it("preProvisionResourcesRequest", () => {
     const connection = new ServerConnection(msgConn);
-    const fake = sandbox.fake.returns("test");
+    const fake = sandbox.fake.returns({
+      needAzureLogin: true,
+      needM365Login: true,
+      resolvedAzureSubscriptionId: undefined,
+      resolvedAzureResourceGroupName: undefined,
+    });
     sandbox.replace(connection["core"], "preProvisionForVS", fake);
     const inputs = {
       platform: "vs",
     };
     const token = {};
-    const res = connection.provisionResourcesRequest(inputs as Inputs, token as CancellationToken);
+    const res = connection.preProvisionResourcesRequest(
+      inputs as Inputs,
+      token as CancellationToken
+    );
     res.then((data) => {
-      assert.equal(data, ok("test"));
+      assert.equal(
+        data,
+        ok({
+          needAzureLogin: true,
+          needM365Login: true,
+          resolvedAzureSubscriptionId: undefined,
+          resolvedAzureResourceGroupName: undefined,
+        })
+      );
     });
   });
 

10486bb1e639e1704c9212941ef77d3761fa3561
Alex Wang <a1exwang@users.noreply.github.com>
2022-11-21 15:30:19 +0800

    chore: add VxTestApp check & install happy path (#6823)
    * chore: add vxTestapp checker happy path
    
    * test: add unit test
    
    * chore: extract strings & unit test
    
    * chore: fix test
    
    * chore: fix app name
    
    * chore: fix string
    
    * Update packages/fx-core/resource/package.nls.json
    
    Co-authored-by: qinezh <qinezh@users.noreply.github.com>
    
    Co-authored-by: qinezh <qinezh@users.noreply.github.com>

--- packages/fx-core/src/common/deps-checker/depsError.ts
+++ packages/fx-core/src/common/deps-checker/depsError.ts
@@ -67,3 +67,11 @@ export class BackendExtensionsInstallError extends DepsCheckerError {
     Object.setPrototypeOf(this, BackendExtensionsInstallError.prototype);
   }
 }
+
+export class VxTestAppCheckError extends DepsCheckerError {
+  constructor(message: string, helpLink: string) {
+    super(message, helpLink);
+
+    Object.setPrototypeOf(this, VxTestAppCheckError.prototype);
+  }
+}

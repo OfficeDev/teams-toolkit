7e575f184a54223d3875e05485c9a972f10a930c
Wang Yefu <yefuwang@microsoft.com>
2022-11-23 16:06:05 +0800

    chore: add interface to support summary

--- packages/fx-core/src/component/driver/interface/stepDriver.ts
+++ packages/fx-core/src/component/driver/interface/stepDriver.ts
@@ -4,11 +4,17 @@
 import { DriverContext } from "./commonArgs";
 import { FxError, Result } from "@microsoft/teamsfx-api";
 
+export type ExecutionResult = { result: Result<Map<string, string>, FxError>; summary: string };
+
 export interface StepDriver {
+  readonly description?: string;
+
   /**
    * Run the driver.
    * @param args Arguments from the `with` section in the yaml file.
    * @param context logger, telemetry, progress bar, etc.
    */
   run(args: unknown, context: DriverContext): Promise<Result<Map<string, string>, FxError>>;
+
+  execute?(args: unknown, ctx: DriverContext): Promise<ExecutionResult>;
 }

5d6420e3d7dbcb0e667f7f2031d1aed95e83e9c3
Alive-Fish <zhiyou@microsoft.com>
2022-11-23 15:28:54 +0800

    perf(core): fix the provision successful message (#6952)

--- packages/fx-core/tests/component/coordinator.test.ts
+++ packages/fx-core/tests/component/coordinator.test.ts
@@ -690,7 +690,7 @@ describe("component coordinator test", () => {
     });
     sandbox.stub(tools.tokenProvider.azureAccountProvider, "setSubscription").resolves();
     const inputs: Inputs = {
-      platform: Platform.VSCode,
+      platform: Platform.CLI,
       projectPath: ".",
       env: "dev",
       targetSubscriptionId: "mockSubId",

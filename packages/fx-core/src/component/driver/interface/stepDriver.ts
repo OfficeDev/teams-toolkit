9ac7a7bba737eab126cb996020122141b6a61f83
WANG YEFU <yefuwang@microsoft.com>
2022-11-24 16:35:14 +0800

    chore: add interface method to support actions returning summary  (#6954)
    * chore: add interface to support summary
    
    * chore: add comments
    
    * chore: add a simple implementation of aad/create
    
    * chore: add more comments

--- packages/fx-core/src/component/driver/interface/stepDriver.ts
+++ packages/fx-core/src/component/driver/interface/stepDriver.ts
@@ -4,11 +4,28 @@
 import { DriverContext } from "./commonArgs";
 import { FxError, Result } from "@microsoft/teamsfx-api";
 
+export type ExecutionResult = {
+  result: Result<Map<string, string>, FxError>;
+  // summaries hold the successful logs even if the result is a FxError.
+  summaries: string[];
+};
+
 export interface StepDriver {
+  readonly description?: string;
+
   /**
    * Run the driver.
    * @param args Arguments from the `with` section in the yaml file.
    * @param context logger, telemetry, progress bar, etc.
    */
   run(args: unknown, context: DriverContext): Promise<Result<Map<string, string>, FxError>>;
+
+  /**
+   * Run the driver and return success summary entries along with the result, no matter the result is success or failure.
+   * Because a failed action may still emit some succuessful summaires.
+   * The summary is expected to contain human readable information that will be presented to users.
+   * @param args Arguments from the `with` section in the yaml file.
+   * @param ctx logger, telemetry, progress bar, etc.
+   */
+  execute?(args: unknown, ctx: DriverContext): Promise<ExecutionResult>;
 }

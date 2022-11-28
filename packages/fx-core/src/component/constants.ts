d61be28f3e1bff3431d4f1c789ef404bca0cf38d
Ning Tang <nintan@microsoft.com>
2022-11-28 10:16:48 +0800

    feat(vsc): change "Tutorials" to "How-to guides" (#6965)
    * chore(vsc): move in product doc out of A/B test
    
    * chore: fix UT
    
    * chore: remove unused strings

--- packages/fx-core/src/component/constants.ts
+++ packages/fx-core/src/component/constants.ts
@@ -715,8 +715,8 @@ export const TabNewUIOptionItem: OptionItem = {
   data: "https://aka.ms/teamsfx-tab-with-sso",
   buttons: [
     {
-      iconPath: "tasklist",
-      tooltip: getLocalizedString("core.option.tutorial"),
+      iconPath: "file-symlink-file",
+      tooltip: getLocalizedString("core.option.github"),
       command: "fx-extension.openTutorial",
     },
   ],
@@ -749,8 +749,8 @@ export const NotificationOptionItem: OptionItem = {
   data: "https://aka.ms/teamsfx-send-notification",
   buttons: [
     {
-      iconPath: "tasklist",
-      tooltip: getLocalizedString("core.option.tutorial"),
+      iconPath: "file-symlink-file",
+      tooltip: getLocalizedString("core.option.github"),
       command: "fx-extension.openTutorial",
     },
   ],
@@ -767,8 +767,8 @@ export const CommandAndResponseOptionItem: OptionItem = {
   data: "https://aka.ms/teamsfx-create-command",
   buttons: [
     {
-      iconPath: "tasklist",
-      tooltip: getLocalizedString("core.option.tutorial"),
+      iconPath: "file-symlink-file",
+      tooltip: getLocalizedString("core.option.github"),
       command: "fx-extension.openTutorial",
     },
   ],
@@ -785,8 +785,8 @@ export const WorkflowOptionItem: OptionItem = {
   data: "https://aka.ms/teamsfx-create-workflow",
   buttons: [
     {
-      iconPath: "tasklist",
-      tooltip: getLocalizedString("core.option.tutorial"),
+      iconPath: "file-symlink-file",
+      tooltip: getLocalizedString("core.option.github"),
       command: "fx-extension.openTutorial",
     },
   ],

24199ceada9e1ea90143c2b183a1f6b710f193d7
yuqizhou77 <86260893+yuqizhou77@users.noreply.github.com>
2022-11-30 10:49:38 +0800

    chore: tdp v3 feature flag check (#7009)
    * chore: v3 flag check
    
    chore: minor
    
    chore: add item
    
    chore: test test
    
    chore: test test
    
    chore: remove not used
    
    * chore: cleanup
    
    * chore: minor

--- packages/vscode-extension/src/uriHandler.ts
+++ packages/vscode-extension/src/uriHandler.ts
@@ -1,7 +1,9 @@
-import { isTDPIntegrationEnabled } from "@microsoft/teamsfx-core/build/common/featureFlags";
+import { isV3Enabled } from "@microsoft/teamsfx-core";
 import * as vscode from "vscode";
 import * as queryString from "query-string";
 import { localize } from "./utils/localizeUtils";
+import * as util from "util";
+import { SwitchToPreReleaseVersionLink } from "./constants";
 
 enum Referrer {
   DeveloperPortal = "developerportal",
@@ -15,37 +17,45 @@ interface QueryParams {
 let isRunning = false;
 export class UriHandler implements vscode.UriHandler {
   handleUri(uri: vscode.Uri): vscode.ProviderResult<void> {
-    if (isTDPIntegrationEnabled()) {
-      if (isRunning) {
-        vscode.window.showWarningMessage(
-          localize("teamstoolkit.devPortalIntegration.blockingMessage")
+    if (isRunning) {
+      vscode.window.showWarningMessage(
+        localize("teamstoolkit.devPortalIntegration.blockingMessage")
+      );
+      return;
+    }
+
+    if (!uri.query) {
+      vscode.window.showErrorMessage(localize("teamstoolkit.devPortalIntegration.invalidLink"));
+      return;
+    }
+    const queryParamas = queryString.parse(uri.query) as QueryParams;
+    if (!queryParamas.referrer) {
+      vscode.window.showErrorMessage(localize("teamstoolkit.devPortalIntegration.invalidLink"));
+      return;
+    }
+
+    if (queryParamas.referrer === Referrer.DeveloperPortal) {
+      if (!isV3Enabled()) {
+        vscode.window.showErrorMessage(
+          util.format(
+            localize("teamstoolkit.devPortalIntegration.installPreReleaseWarning"),
+            SwitchToPreReleaseVersionLink
+          )
         );
         return;
       }
 
-      if (!uri.query) {
-        vscode.window.showErrorMessage(localize("teamstoolkit.devPortalIntegration.invalidLink"));
-        return;
-      }
-      const queryParamas = queryString.parse(uri.query) as QueryParams;
-      if (!queryParamas.referrer) {
+      if (!queryParamas.appId) {
         vscode.window.showErrorMessage(localize("teamstoolkit.devPortalIntegration.invalidLink"));
         return;
       }
 
-      if (queryParamas.referrer === Referrer.DeveloperPortal) {
-        if (!queryParamas.appId) {
-          vscode.window.showErrorMessage(localize("teamstoolkit.devPortalIntegration.invalidLink"));
-          return;
-        }
-
-        isRunning = true;
-        vscode.commands
-          .executeCommand("fx-extension.openFromTdp", queryParamas.appId, queryParamas.login_hint)
-          .then(() => {
-            isRunning = false;
-          });
-      }
+      isRunning = true;
+      vscode.commands
+        .executeCommand("fx-extension.openFromTdp", queryParamas.appId, queryParamas.login_hint)
+        .then(() => {
+          isRunning = false;
+        });
     }
   }
 }

e779e0ccffc1b9f2221c64366caccf976cab4660
Ivan Jobs <ruhe@microsoft.com>
2022-12-05 11:32:43 +0800

    chore: v3 summary botaad (#7079)
    * chore: v3 summary botaad

--- packages/fx-core/src/component/driver/botAadApp/utility/constants.ts
+++ packages/fx-core/src/component/driver/botAadApp/utility/constants.ts
@@ -5,4 +5,6 @@ export const logMessageKeys = {
   startExecuteDriver: "driver.botAadApp.log.startExecuteDriver",
   successExecuteDriver: "driver.botAadApp.log.successExecuteDriver",
   failExecuteDriver: "driver.botAadApp.log.failExecuteDriver",
+  successCreateBotAad: "driver.botAadApp.log.successCreateBotAad",
+  useExistingBotAad: "driver.botAadApp.log.useExistingBotAad",
 };

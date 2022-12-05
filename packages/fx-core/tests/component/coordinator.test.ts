6d0ad7232ae91988e4c4b67565b4654d1b3155c9
huajiezhang <huajiezhang@microsoft.com>
2022-12-05 17:02:30 +0800

    feat: ut

--- packages/fx-core/tests/component/coordinator.test.ts
+++ packages/fx-core/tests/component/coordinator.test.ts
@@ -52,6 +52,10 @@ import { developerPortalScaffoldUtils } from "../../src/component/developerPorta
 import { createContextV3 } from "../../src/component/utils";
 import * as appStudio from "../../src/component/resource/appManifest/appStudio";
 import * as v3MigrationUtils from "../../src/core/middleware/utils/v3MigrationUtils";
+import { manifestUtils } from "../../src/component/resource/appManifest/utils/ManifestUtils";
+import { ValidateTeamsAppDriver } from "../../src/component/driver/teamsApp/validate";
+import Container from "typedi";
+import { CreateAppPackageDriver } from "../../src/component/driver/teamsApp/createAppPackage";
 
 function mockedResolveDriverInstances(log: LogProvider): Result<DriverInstance[], FxError> {
   return ok([
@@ -1891,6 +1895,31 @@ describe("component coordinator test", () => {
     const res2 = await fxCore.getQuestions(Stage.initInfra, inputs);
     assert.isTrue(res2.isOk());
   });
+
+  it("executeUserTaskNew", async () => {
+    sandbox.stub(manifestUtils, "getTeamsAppManifestPath").resolves("");
+    const driver1: ValidateTeamsAppDriver = Container.get("teamsApp/validate");
+    const driver2: CreateAppPackageDriver = Container.get("teamsApp/createAppPackage");
+    sandbox.stub(driver1, "run").resolves(ok(new Map()));
+    sandbox.stub(driver2, "run").resolves(ok(new Map()));
+    const inputs: Inputs = {
+      platform: Platform.VSCode,
+    };
+    const fxCore = new FxCore(tools);
+    const res1 = await fxCore.executeUserTask(
+      { namespace: "", method: "getManifestTemplatePath" },
+      inputs
+    );
+    assert.isTrue(res1.isOk());
+    const res2 = await fxCore.executeUserTask(
+      { namespace: "", method: "validateManifest" },
+      inputs
+    );
+    assert.isTrue(res2.isOk());
+    const res3 = await fxCore.executeUserTask({ namespace: "", method: "buildPackage" }, inputs);
+    assert.isTrue(res3.isOk());
+  });
+
   describe("publishInDeveloperPortal", () => {
     afterEach(() => {
       sandbox.restore();

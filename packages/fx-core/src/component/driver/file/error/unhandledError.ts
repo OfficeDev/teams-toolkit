c6c08840f8df94783f8131a579ad555a6692f708
Kuojian Lu <kuojianlu@gmail.com>
2022-12-06 21:16:53 +0800

    chore: rename env/generate action name
    - env/generate -> file/updateEnv

--- packages/fx-core/src/component/driver/file/error/unhandledError.ts
+++ packages/fx-core/src/component/driver/file/error/unhandledError.ts
@@ -6,7 +6,7 @@ import { SystemError, SystemErrorOptions, UserErrorOptions } from "@microsoft/te
 import { getDefaultString, getLocalizedString } from "../../../../common/localizeUtils";
 
 const errorCode = "UnhandledError";
-const messageKey = "driver.env.error.unhandledError";
+const messageKey = "file/updateEnv.error.unhandledError";
 
 export class UnhandledSystemError extends SystemError {
   constructor(actionName: string, detail: string) {

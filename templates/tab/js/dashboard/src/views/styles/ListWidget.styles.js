e784bafe0b1f951e38f07643f3f91dfd8117768e
huimiu <107838226+huimiu@users.noreply.github.com>
2022-12-14 10:47:06 +0800

    chore: extract dashboard js style definitions (#7226)

--- templates/tab/js/dashboard/src/views/styles/ListWidget.styles.js
+++ templates/tab/js/dashboard/src/views/styles/ListWidget.styles.js
@@ -1,3 +1,22 @@
+import { tokens } from "@fluentui/react-components";
+
+export const bodyContentStyle = () => ({
+  display: "grid",
+  gap: "0.5rem",
+});
+
+export const itemLayoutStyle = () => ({
+  display: "grid",
+});
+
+export const dividerStyle = () => ({
+  marginBottom: "0.5rem",
+  marginLeft: "-2.25rem",
+  marginRight: "-2.3rem",
+  height: "1px",
+  background: tokens.colorNeutralStroke2,
+});
+
 export const itemTitleStyle = () => ({
   fontFamily: "Segoe UI",
   fontSize: "0.875rem",

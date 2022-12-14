800a6e34d584dd2993c0160e3e226ce8fad0214b
Yiqing Zhao <cissyzhao99@gmail.com>
2022-12-14 13:33:01 +0800

    chore(templates): use cloudAdapter for command-and-response

--- templates/scenarios/js/command-and-response/src/internal/initialize.js
+++ templates/scenarios/js/command-and-response/src/internal/initialize.js
@@ -1,4 +1,5 @@
-const { ConversationBot } = require("@microsoft/teamsfx");
+const { BotBuilderCloudAdapter } = require("@microsoft/teamsfx");
+const ConversationBot = BotBuilderCloudAdapter.ConversationBot;
 const { HelloWorldCommandHandler } = require("../helloworldCommandHandler");
 const config = require("./config");
 
@@ -6,11 +7,12 @@ const config = require("./config");
 // You can also use the commandApp.command.registerCommands to register other commands
 // if you don't want to register all of them in the constructor
 const commandApp = new ConversationBot({
-  // The bot id and password to create BotFrameworkAdapter.
+  // The bot id and password to create CloudAdapter.
   // See https://aka.ms/about-bot-adapter to learn more about adapters.
   adapterConfig: {
-    appId: config.botId,
-    appPassword: config.botPassword,
+    MicrosoftAppId: config.botId,
+    MicrosoftAppPassword: config.botPassword,
+    MicrosoftAppType: "MultiTenant",
   },
   command: {
     enabled: true,

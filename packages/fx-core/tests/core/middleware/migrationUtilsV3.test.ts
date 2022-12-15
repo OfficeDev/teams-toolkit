20aeb803c6fb15c7fda0b5248556611bb443b74f
Bowen Song <bowsong@microsoft.com>
2022-12-15 08:29:31 +0800

    perf(migrationV3): use default output instead of throwing error when … (#7236)
    * perf(migrationV3): use default output instead of throwing error when cannot find key in bicep
    
    * test: add ut

--- packages/fx-core/tests/core/middleware/migrationUtilsV3.test.ts
+++ packages/fx-core/tests/core/middleware/migrationUtilsV3.test.ts
@@ -14,10 +14,13 @@ import {
 import { randomAppName } from "../utils";
 import * as os from "os";
 import * as path from "path";
-import { Inputs, Platform } from "@microsoft/teamsfx-api";
+import * as v3MigrationUtils from "../../../src/core/middleware/utils/v3MigrationUtils";
+import * as migrationUtils from "../../../src/core/middleware/utils/MigrationUtils";
+import { err, Inputs, Platform, SystemError } from "@microsoft/teamsfx-api";
 import { MigrationContext } from "../../../src/core/middleware/utils/migrationContext";
 import { mockMigrationContext } from "./projectMigrationV3.test";
 import sinon from "sinon";
+import { getPlaceholderMappings } from "../../../src/core/middleware/utils/debug/debugV3MigrationUtils";
 
 describe("MigrationUtilsV3", () => {
   it("happy path for fixed namings", () => {
@@ -129,12 +132,7 @@ describe("MigrationUtilsV3", () => {
       FileType.STATE,
       bicepContent
     );
-    assert.isTrue(
-      res.isErr() &&
-        res.error.message ===
-          "Failed to find matching output in provision.bicep for key state.fx-resource-azure-sql.databaseName_test3" &&
-        res.error.name == "FailedToConvertV2ConfigNameToV3"
-    );
+    assert.isTrue(res.isOk() && res.value === "STATE__FX_RESOURCE_AZURE_SQL__DATABASENAME_TEST3");
   });
 });
 
@@ -315,5 +313,29 @@ describe("MigrationUtils: needMigrateToAadManifest", async () => {
       .withArgs(path.join(projectPath, "permissions.json"), () => {})
       .resolves(false);
     assert.isTrue(!(await needMigrateToAadManifest(migrationContext)));
+  });
+});
+
+describe("Migration: getPlaceholderMappings", () => {
+  const sandbox = sinon.createSandbox();
+  const appName = randomAppName();
+  const projectPath = path.join(os.tmpdir(), appName);
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it("failed due to naming converter throws error", async () => {
+    sandbox.stub(v3MigrationUtils, "readBicepContent").resolves("");
+    sandbox
+      .stub(migrationUtils, "namingConverterV3")
+      .returns(err(new SystemError("source", "name", "message")));
+    const migrationContext = await mockMigrationContext(projectPath);
+    const res = await getPlaceholderMappings(migrationContext);
+    assert.equal(res.botDomain, undefined);
+    assert.equal(res.tabIndexPath, undefined);
+    assert.equal(res.tabEndpoint, undefined);
+    assert.equal(res.tabDomain, undefined);
+    assert.equal(res.botEndpoint, undefined);
   });
 });

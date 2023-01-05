a1395cf168df499f2680a5c7e73f5d849f614987
Kuojian Lu <kuojianlu@gmail.com>
2023-01-05 17:42:21 +0800

    chore(debug-migration): add channels to botFramework/create (#7406)

--- packages/fx-core/src/core/middleware/utils/debug/appLocalYmlGenerator.ts
+++ packages/fx-core/src/core/middleware/utils/debug/appLocalYmlGenerator.ts
@@ -15,6 +15,7 @@ export class AppLocalYmlConfig {
   provision?: {
     bot?: {
       messagingEndpoint: string;
+      isM365?: boolean;
     };
   };
   configureApp?: {
@@ -79,6 +80,10 @@ export class AppLocalYmlGenerator extends BaseAppYmlGenerator {
     if (OldProjectSettingsHelper.includeFunction(this.oldProjectSettings)) {
       functionName =
         OldProjectSettingsHelper.getFunctionName(this.oldProjectSettings) || "getUserProfile";
+    }
+
+    if (this.handlebarsContext.config.provision?.bot) {
+      this.handlebarsContext.config.provision.bot.isM365 = this.oldProjectSettings.isM365;
     }
 
     if (this.handlebarsContext.config.deploy?.sso) {

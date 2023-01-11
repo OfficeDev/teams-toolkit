f3ec34e1b6a7911fdd9094ae5315afc6c613ecf7
turenlong <rentu@microsoft.com>
2023-01-11 17:17:48 +0800

    perf: remove unused parameter

--- packages/fx-core/src/component/driver/aad/interface/updateAadAppArgs.ts
+++ packages/fx-core/src/component/driver/aad/interface/updateAadAppArgs.ts
@@ -4,5 +4,4 @@
 export interface UpdateAadAppArgs {
   manifestTemplatePath: string; // Relative path to project root
   outputFilePath: string; // Relative path to project root
-  onlyBuild?: boolean; // Only build aad manifest for preview purpose
 }

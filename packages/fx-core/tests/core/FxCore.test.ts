36d0b898bdf18b032d8b32e7128d90c9793ee76b
huajiezhang <huajiezhang@microsoft.com>
2023-01-12 17:04:27 +0800

    feat: up

--- packages/fx-core/tests/core/FxCore.test.ts
+++ packages/fx-core/tests/core/FxCore.test.ts
@@ -172,6 +172,9 @@ describe("Core basic APIs", () => {
     assert.isTrue(envListResult.value[0] === environmentManager.getDefaultEnvName());
     inputs[CoreQuestionNames.NewTargetEnvName] = newEnvName;
     const createEnvRes = await core.createEnv(inputs);
+    if (createEnvRes.isErr()) {
+      console.error(createEnvRes.error);
+    }
     assert.isTrue(createEnvRes.isOk());
 
     const newEnvListResult = await environmentManager.listRemoteEnvConfigs(projectPath);

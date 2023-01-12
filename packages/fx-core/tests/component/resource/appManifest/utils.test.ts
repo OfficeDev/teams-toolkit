efd09810d4f801c311052b44e23c82cf5069df5a
Yuqi Zhou <yuqzho@microsoft.com>
2023-01-12 17:06:13 +0800

    chore: ut

--- packages/fx-core/tests/component/resource/appManifest/utils.test.ts
+++ packages/fx-core/tests/component/resource/appManifest/utils.test.ts
@@ -168,6 +168,25 @@ describe("utils", () => {
 
       chai.assert.isTrue(needTab);
     });
+
+    it("private chat tab, team scope: returns true", () => {
+      const appDefinition: AppDefinition = {
+        teamsAppId: "mockAppId",
+        tenantId: "mockTenantId",
+        configurableTabs: [
+          {
+            ...validConfigurableTabForTabCode,
+            context: [MeetingsContext.PrivateChatTab],
+            scopes: [CommandScope.Team],
+          },
+        ],
+        bots: [validBot],
+      };
+
+      const needTab = needTabCode(appDefinition);
+
+      chai.assert.isTrue(needTab);
+    });
   });
 
   describe("needBot", () => {
@@ -287,6 +306,26 @@ describe("utils", () => {
       const res = hasMeetingExtension(appDefinition);
 
       chai.assert.isTrue(res);
+    });
+
+    it("missing scope: returns false", () => {
+      const appDefinition: AppDefinition = {
+        teamsAppId: "mockAppId",
+        tenantId: "mockTenantId",
+        configurableTabs: [
+          {
+            ...validConfigurableTabForMeetingExtension,
+            context: [MeetingsContext.DetailsTab],
+            scopes: [],
+          },
+        ],
+        staticTabs: [],
+        bots: [validBot],
+      };
+
+      const res = hasMeetingExtension(appDefinition);
+
+      chai.assert.isFalse(res);
     });
 
     it("chat tab: returns true", () => {

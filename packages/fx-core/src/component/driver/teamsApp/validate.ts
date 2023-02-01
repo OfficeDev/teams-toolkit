ca2af177f2be38488a07bd68da3f8d840d91d13f
Ning Liu <nliu@microsoft.com>
2023-02-01 10:42:18 +0800

    feat: validate against app packgee

--- packages/fx-core/src/component/driver/teamsApp/validate.ts
+++ packages/fx-core/src/component/driver/teamsApp/validate.ts
@@ -1,9 +1,12 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { Result, FxError, ok, err, ManifestUtil, Platform } from "@microsoft/teamsfx-api";
+import { Result, FxError, ok, err, ManifestUtil, Platform, Colors } from "@microsoft/teamsfx-api";
 import { hooks } from "@feathersjs/hooks/lib";
 import { Service } from "typedi";
+import fs from "fs-extra";
+import * as path from "path";
+import { EOL } from "os";
 import { StepDriver, ExecutionResult } from "../interface/stepDriver";
 import { DriverContext } from "../interface/commonArgs";
 import { WrapDriverContext } from "../util/wrapUtil";
@@ -12,8 +15,10 @@ import { addStartAndEndTelemetry } from "../middleware/addStartAndEndTelemetry";
 import { manifestUtils } from "../../resource/appManifest/utils/ManifestUtils";
 import { AppStudioResultFactory } from "../../resource/appManifest/results";
 import { AppStudioError } from "../../resource/appManifest/errors";
-import { getLocalizedString } from "../../../common/localizeUtils";
+import { AppStudioClient } from "../../resource/appManifest/appStudioClient";
+import { getDefaultString, getLocalizedString } from "../../../common/localizeUtils";
 import { HelpLinks } from "../../../common/constants";
+import { AppStudioScopes } from "../../../common/tools";
 import { getAbsolutePath } from "../../utils/common";
 
 const actionName = "teamsApp/validate";
@@ -54,9 +59,68 @@ export class ValidateTeamsAppDriver implements StepDriver {
       return err(result.error);
     }
 
+    if (args.appPackagePath) {
+      let appPackagePath = args.appPackagePath;
+      if (!path.isAbsolute(appPackagePath)) {
+        appPackagePath = path.join(context.projectPath, appPackagePath);
+      }
+      if (!(await fs.pathExists(appPackagePath))) {
+        return err(
+          AppStudioResultFactory.UserError(
+            AppStudioError.FileNotFoundError.name,
+            AppStudioError.FileNotFoundError.message(appPackagePath)
+          )
+        );
+      }
+      const archivedFile = await fs.readFile(appPackagePath);
+
+      const appStudioTokenRes = await context.m365TokenProvider.getAccessToken({
+        scopes: AppStudioScopes,
+      });
+      if (appStudioTokenRes.isErr()) {
+        return err(appStudioTokenRes.error);
+      }
+      const appStudioToken = appStudioTokenRes.value;
+
+      const validationResult = await AppStudioClient.partnerCenterAppPackageValidation(
+        archivedFile,
+        appStudioToken
+      );
+
+      // logs in output window
+      const errors = validationResult.errors
+        .map((error) => {
+          return `(x)Error: ${error.content} \n${getLocalizedString("core.option.learnMore")}: ${
+            error.helpUrl
+          }`;
+        })
+        .join(EOL);
+      const warnings = validationResult.warnings
+        .map((warning) => {
+          return `(!)Warning: ${warning.content} \n${getLocalizedString(
+            "core.option.learnMore"
+          )}: ${warning.helpUrl}`;
+        })
+        .join(EOL);
+      const outputMessage = `${EOL}Teams Toolkit is validating your app package:${EOL}${errors}${EOL}${warnings}${EOL}Validation completed.${EOL}Summary:${EOL}${
+        validationResult.errors.length + validationResult.warnings.length
+      } failed, ${
+        validationResult.notes.length
+      }passed ${EOL}A completed log of validations can be found in %s`;
+      context.logProvider?.info(outputMessage);
+
+      const message = getLocalizedString(
+        "driver.teamsApp.validate.result",
+        validationResult.errors.length + validationResult.warnings.length,
+        validationResult.notes.length
+      );
+      context.ui?.showMessage("info", message, false);
+      return ok(new Map());
+    }
+
     const state = this.loadCurrentState();
     const manifestRes = await manifestUtils.getManifestV3(
-      getAbsolutePath(args.manifestPath, context.projectPath),
+      getAbsolutePath(args.manifestPath!, context.projectPath),
       state,
       withEmptyCapabilities
     );
@@ -126,20 +190,18 @@ export class ValidateTeamsAppDriver implements StepDriver {
   }
 
   private validateArgs(args: ValidateTeamsAppArgs): Result<any, FxError> {
-    const invalidParams: string[] = [];
-    if (!args || !args.manifestPath) {
-      invalidParams.push("manifestPath");
-    }
-    if (invalidParams.length > 0) {
+    if (!args || (!args.manifestPath && !args.appPackagePath)) {
       return err(
         AppStudioResultFactory.UserError(
           AppStudioError.InvalidParameterError.name,
-          AppStudioError.InvalidParameterError.message(actionName, invalidParams),
+          [
+            getDefaultString("driver.teamsApp.validate.invalidParameter", actionName),
+            getLocalizedString("driver.teamsApp.validate.invalidParameter", actionName),
+          ],
           "https://aka.ms/teamsfx-actions/teamsapp-validate"
         )
       );
-    } else {
-      return ok(undefined);
     }
+    return ok(undefined);
   }
 }

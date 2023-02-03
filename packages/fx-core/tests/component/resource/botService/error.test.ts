19cffba4a69308302394b1bf4763e115dd1fc81a
Ivan Jobs <ivan1377@163.com>
2023-02-03 12:09:51 +0800

    chore: increase ut

--- packages/fx-core/tests/component/resource/botService/error.test.ts
+++ packages/fx-core/tests/component/resource/botService/error.test.ts
@@ -1,10 +1,19 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+/**
+ * @author zhijie <zhihuan@microsoft.com>
+ */
 import { SystemError, UserError } from "@microsoft/teamsfx-api";
 import { assert } from "chai";
 import "mocha";
-import { wrapError } from "../../../../src/component/resource/botService/errors";
+import { ErrorNames } from "../../../../src/component/resource/botService/constants";
+import {
+  BotFrameworkConflictResultError,
+  BotFrameworkForbiddenResultError,
+  BotFrameworkNotAllowedToAcquireTokenError,
+  wrapError,
+} from "../../../../src/component/resource/botService/errors";
 
 describe("wrap error", () => {
   it("wrap empty error", () => {
@@ -32,5 +41,20 @@ describe("wrap error", () => {
       assert.isTrue(res.error instanceof SystemError);
       assert.equal(res.error.name, "utError");
     }
+  });
+
+  it("Increase UT - BotFrameworkNotAllowedToAcquireTokenError", () => {
+    const e = new BotFrameworkNotAllowedToAcquireTokenError();
+    assert.isTrue(e.name === ErrorNames.ACQUIRE_BOT_FRAMEWORK_TOKEN_ERROR);
+  });
+
+  it("Increase UT - BotFrameworkForbiddenResultError", () => {
+    const e = new BotFrameworkForbiddenResultError();
+    assert.isTrue(e.name === ErrorNames.FORBIDDEN_RESULT_BOT_FRAMEWORK_ERROR);
+  });
+
+  it("Increase UT - BotFrameworkConflictResultError", () => {
+    const e = new BotFrameworkConflictResultError();
+    assert.isTrue(e.name === ErrorNames.CONFLICT_RESULT_BOT_FRAMEWORK_ERROR);
   });
 });

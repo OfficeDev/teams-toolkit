ef78874daba436aea5a6507d852712347ae6d74e
huajiezhang <huajiezhang@microsoft.com>
2023-02-05 12:02:16 +0800

    feat: ut

--- packages/fx-core/tests/component/driver/script/scriptDriver.test.ts
+++ packages/fx-core/tests/component/driver/script/scriptDriver.test.ts
@@ -1,11 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
 import "mocha";
-import * as chai from "chai";
 import * as sinon from "sinon";
 import * as tools from "../../../../src/common/tools";
 import { TestAzureAccountProvider } from "../../util/azureAccountMock";
@@ -14,6 +10,7 @@ import { DriverContext } from "../../../../src/component/driver/interface/common
 import { scriptDriver } from "../../../../src/component/driver/script/scriptDriver";
 import { assert } from "chai";
 import { MockUserInteraction } from "../../../core/utils";
+import { err, UserError } from "@microsoft/teamsfx-api";
 
 describe("Script Driver test", () => {
   const sandbox = sinon.createSandbox();
@@ -25,8 +22,40 @@ describe("Script Driver test", () => {
   afterEach(() => {
     sandbox.restore();
   });
-
-  it("set output", async () => {
+  it("execute success", async () => {
+    const args = {
+      workingDirectory: "./",
+      run: "::set-output KEY=VALUE",
+    };
+    const context = {
+      azureAccountProvider: new TestAzureAccountProvider(),
+      logProvider: new TestLogProvider(),
+      ui: new MockUserInteraction(),
+      projectPath: "./",
+    } as DriverContext;
+    const res = await scriptDriver.execute(args, context);
+    assert.isTrue(res.result.isOk());
+    if (res.result.isOk()) {
+      const output = res.result.value;
+      assert.equal(output.get("KEY"), "VALUE");
+    }
+  });
+  it("execute fail", async () => {
+    sandbox.stub(scriptDriver, "executeCommand").resolves(err(new UserError({})));
+    const args = {
+      workingDirectory: "./",
+      run: "::set-output KEY=VALUE",
+    };
+    const context = {
+      azureAccountProvider: new TestAzureAccountProvider(),
+      logProvider: new TestLogProvider(),
+      ui: new MockUserInteraction(),
+      projectPath: "./",
+    } as DriverContext;
+    const res = await scriptDriver.execute(args, context);
+    assert.isTrue(res.result.isErr());
+  });
+  it("executeCommand: set output", async () => {
     const args = {
       workingDirectory: "./",
       run: "::set-output KEY=VALUE",
@@ -38,7 +67,7 @@ describe("Script Driver test", () => {
       projectPath: "./",
     } as DriverContext;
     const res = await scriptDriver.executeCommand(args, context);
-    chai.assert.equal(res.isOk(), true);
+    assert.isTrue(res.isOk());
     if (res.isOk()) {
       const output = res.value[1];
       assert.deepEqual(output, { KEY: "VALUE" });

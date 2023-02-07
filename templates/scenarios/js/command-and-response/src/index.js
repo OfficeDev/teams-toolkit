076c9d07638ffcb796ef96331eb1692113b7df68
Yiqing Zhao <cissyzhao99@gmail.com>
2023-02-07 17:17:45 +0800

    chore(templates): update templates to use SDK implemented with cloudAdapter (#7274)
    * chore(templates): use cloudAdapter for command-and-response
    
    * chore(templates): update notification resitfy bot to use CloudAdapter
    
    * chore(templates): update http notification bot
    
    * chore(templates): update notification timer app
    
    * chore(templates): update http timer notification bot
    
    * chore: update workflow bot templates
    
    * chore: update v2 default bot template
    
    * chore: update m365 message extension
    
    * chore: update bot templates to remove type error
    
    * docs: update README
    
    * Update package.json.tpl
    
    * fix: update indences
    
    * chore: update package.json version

--- templates/scenarios/js/command-and-response/src/index.js
+++ templates/scenarios/js/command-and-response/src/index.js
@@ -5,6 +5,7 @@ const { commandApp } = require("./internal/initialize");
 // This template uses `restify` to serve HTTP responses.
 // Create a restify server.
 const server = restify.createServer();
+server.use(restify.plugins.bodyParser());
 server.listen(process.env.port || process.env.PORT || 3978, () => {
   console.log(`\nApp Started, ${server.name} listening to ${server.url}`);
 });
--- templates/scenarios/ts/command-and-response/src/internal/initialize.ts
+++ templates/scenarios/ts/command-and-response/src/internal/initialize.ts
@@ -1,16 +1,18 @@
 import { HelloWorldCommandHandler } from "../helloworldCommandHandler";
-import { ConversationBot } from "@microsoft/teamsfx";
+import { BotBuilderCloudAdapter } from "@microsoft/teamsfx";
+import ConversationBot = BotBuilderCloudAdapter.ConversationBot;
 import config from "./config";
 
 // Create the command bot and register the command handlers for your app.
 // You can also use the commandApp.command.registerCommands to register other commands
 // if you don't want to register all of them in the constructor
 export const commandApp = new ConversationBot({
-  // The bot id and password to create BotFrameworkAdapter.
+  // The bot id and password to create CloudAdapter.
   // See https://aka.ms/about-bot-adapter to learn more about adapters.
   adapterConfig: {
-    appId: config.botId,
-    appPassword: config.botPassword,
+    MicrosoftAppId: config.botId,
+    MicrosoftAppPassword: config.botPassword,
+    MicrosoftAppType: "MultiTenant",
   },
   command: {
     enabled: true,

e2d0056038d21a78043a4db7b4f5dfde0976ef11
FanH <Siglud@gmail.com>
2023-02-22 15:10:41 +0800

    chore: add more test check progress bar steps (#7836)
    * chore: add more test for progress bar step check

--- packages/fx-core/tests/component/driver/deploy/azure/azureAppServiceDeployDriver.test.ts
+++ packages/fx-core/tests/component/driver/deploy/azure/azureAppServiceDeployDriver.test.ts
@@ -25,6 +25,7 @@ import * as os from "os";
 import * as path from "path";
 import * as uuid from "uuid";
 import { ProgressMessages } from "../../../../../src/component/messages";
+import { IProgressHandler } from "@microsoft/teamsfx-api";
 
 describe("Azure App Service Deploy Driver test", () => {
   const sandbox = sinon.createSandbox();
@@ -63,10 +64,20 @@ describe("Azure App Service Deploy Driver test", () => {
       resourceId:
         "/subscriptions/e24d88be-bbbb-1234-ba25-aa11aaaa1aa1/resourceGroups/hoho-rg/providers/Microsoft.Web/sites/some-server-farm",
     } as DeployArgs;
+    const progressHandler: IProgressHandler = {
+      start: async (detail?: string): Promise<void> => {},
+      next: async (detail?: string): Promise<void> => {},
+      end: async (): Promise<void> => {},
+    };
+    const ui = new MockUserInteraction();
+    sandbox.stub(ui, "createProgressBar").returns(progressHandler);
+    const progressNextCaller = sandbox.stub(progressHandler, "next").resolves();
+    const progressEndCaller = sandbox.stub(progressHandler, "end").resolves();
+
     const context = {
       azureAccountProvider: new TestAzureAccountProvider(),
       logProvider: new TestLogProvider(),
-      ui: new MockUserInteraction(),
+      ui: ui,
     } as DriverContext;
     sandbox
       .stub(context.azureAccountProvider, "getIdentityCredentialAsync")
@@ -101,6 +112,9 @@ describe("Azure App Service Deploy Driver test", () => {
     sandbox.stub(client.webApps, "restart").resolves();
     const res = await deploy.run(args, context);
     expect(res.unwrapOr(new Map([["a", "a"]])).size).to.equal(0);
+    // progress bar have 6 steps
+    expect(progressNextCaller.callCount).to.equal(6);
+    expect(progressEndCaller.callCount).to.equal(1);
     const rex = await deploy.execute(args, context);
     expect(rex.result.unwrapOr(new Map([["a", "a"]])).size).to.equal(0);
   });
@@ -353,10 +367,21 @@ describe("Azure App Service Deploy Driver test", () => {
         "/subscriptions/e24d88be-bbbb-1234-ba25-aa11aaaa1aa1/resourceGroups/hoho-rg/providers/Microsoft.Web/sites/some-server-farm",
       dryRun: true,
     } as DeployArgs;
+
+    const progressHandler: IProgressHandler = {
+      start: async (detail?: string): Promise<void> => {},
+      next: async (detail?: string): Promise<void> => {},
+      end: async (): Promise<void> => {},
+    };
+    const ui = new MockUserInteraction();
+    sandbox.stub(ui, "createProgressBar").returns(progressHandler);
+    const progressNextCaller = sandbox.stub(progressHandler, "next").resolves();
+    const progressEndCaller = sandbox.stub(progressHandler, "end").resolves();
+
     const context = {
       azureAccountProvider: new TestAzureAccountProvider(),
       logProvider: new TestLogProvider(),
-      ui: new MockUserInteraction(),
+      ui: ui,
     } as DriverContext;
     sandbox
       .stub(context.azureAccountProvider, "getIdentityCredentialAsync")
@@ -392,6 +417,9 @@ describe("Azure App Service Deploy Driver test", () => {
     const res = await deploy.execute(args, context);
     assert.equal(res.result.isOk(), true);
     assert.equal(res.summaries[0], "Preparations of deployment are complete. ");
+    // dry run will have only one progress step
+    assert.equal(progressNextCaller.callCount, 1);
+    expect(progressEndCaller.callCount).to.equal(1);
   });
 
   it("list credential error", async () => {

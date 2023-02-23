5ff0002c96f61e35e1f1e782a03b67de3f595a03
Kuojian Lu <kuojianlu@gmail.com>
2023-02-23 13:29:30 +0800

    chore(m365): rename "Office.com" to "Microsoft 365 app"

--- packages/cli/src/cmds/preview/errors.ts
+++ packages/cli/src/cmds/preview/errors.ts
@@ -166,7 +166,7 @@ export function OnlyLaunchPageSupportedInOffice(): UserError {
   return new UserError(
     constants.cliSource,
     "OnlyLaunchPageSupportedByOffice",
-    "Only launch page is supported in Office. Message extension is not supported in Office."
+    "Only launch page is supported in Microsoft 365 app. Message extension is not supported in Microsoft 365 app."
   );
 }
 

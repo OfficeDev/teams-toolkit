ad2e2775421b65eea92e5bd3623345e4b308a3b5
Ning Liu <nliu@microsoft.com>
2023-03-07 19:50:04 +0800

    fix: ut coverage

--- packages/vscode-extension/test/extension/handlers.test.ts
+++ packages/vscode-extension/test/extension/handlers.test.ts
@@ -406,6 +406,58 @@ describe("handlers", () => {
       sinon.restore();
     });
 
+    it("validateManifestHandler() - user cancel", async () => {
+      sinon.stub(commonTools, "isV3Enabled").returns(true);
+      sinon.stub(commonTools, "isValidationEnabled").returns(true);
+      sinon.stub(handlers, "core").value(new MockCore());
+      sinon.stub(ExtTelemetry, "sendTelemetryEvent");
+      sinon.stub(ExtTelemetry, "sendTelemetryErrorEvent");
+      sinon.stub(localizeUtils, "localize").returns("");
+
+      sinon.stub(extension, "VS_CODE_UI").value({
+        selectOption: (options: any) => {
+          return Promise.resolve(err(new Error("User cancel")));
+        },
+      });
+
+      const res = await handlers.validateManifestHandler();
+
+      chai.assert(res.isErr());
+      if (res.isErr()) {
+        chai.assert.equal(res.error.message, "User cancel");
+      }
+      sinon.restore();
+    });
+
+    it("validateManifestHandler() - user cancel env", async () => {
+      sinon.stub(commonTools, "isV3Enabled").returns(true);
+      sinon.stub(commonTools, "isValidationEnabled").returns(true);
+      sinon.stub(handlers, "core").value(new MockCore());
+      sinon.stub(ExtTelemetry, "sendTelemetryEvent");
+      sinon.stub(ExtTelemetry, "sendTelemetryErrorEvent");
+      sinon.stub(localizeUtils, "localize").returns("");
+      sinon.stub(projectSettingsHelper, "isValidProject").returns(true);
+      sinon.stub(environmentManager, "listAllEnvConfigs").resolves(ok(["dev", "local"]));
+
+      sinon.stub(extension, "VS_CODE_UI").value({
+        selectOption: (options: any) => {
+          if (options.name === "targetEnvName") {
+            return Promise.resolve(err(new Error("User cancel")));
+          } else {
+            return Promise.resolve(ok({ type: "success", result: "validateAgainstSchema" }));
+          }
+        },
+      });
+
+      const res = await handlers.validateManifestHandler();
+
+      chai.assert(res.isErr());
+      if (res.isErr()) {
+        chai.assert.equal(res.error.message, "User cancel");
+      }
+      sinon.restore();
+    });
+
     it("debugHandler()", async () => {
       const sendTelemetryEventStub = sinon.stub(ExtTelemetry, "sendTelemetryEvent");
       const executeCommandStub = sinon.stub(vscode.commands, "executeCommand");

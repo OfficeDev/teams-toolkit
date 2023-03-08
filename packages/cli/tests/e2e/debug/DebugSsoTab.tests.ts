a8894c552101edd05248121e2d5ae7d86878c1d0
Kuojian Lu <kuojianlu@gmail.com>
2023-03-08 11:32:00 +0800

    test(e2e): add e2e test for debug (#7978)
    - add e2e test for sso-tab, notification-http-trigger debug
    - add e2e test for m365-tab m365-message-extension debug
    - update code owners
    - add azure functions installation in e2e-test.yml
    - add env for sideloading service in e2e-test.yml

---
+++ packages/cli/tests/e2e/debug/DebugSsoTab.tests.ts
@@ -0,0 +1,63 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import * as chai from "chai";
+import { describe } from "mocha";
+import * as path from "path";
+
+import { it } from "@microsoft/extra-shot-mocha";
+import { isV3Enabled } from "@microsoft/teamsfx-core/build/common/tools";
+import { envUtil } from "@microsoft/teamsfx-core/build/component/utils/envUtil";
+import { setTools } from "@microsoft/teamsfx-core/build/core/globalVars";
+
+import { CliHelper } from "../../commonlib/cliHelper";
+import { Capability } from "../../commonlib/constants";
+import { cleanUpLocalProject, getTestFolder, getUniqueAppName } from "../commonUtils";
+import { deleteAadAppByObjectId, deleteTeamsApp } from "./clean";
+
+describe("Debug V3 sso-tab template", () => {
+  const testFolder = getTestFolder();
+  const appName = getUniqueAppName();
+  const projectPath = path.resolve(testFolder, appName);
+
+  setTools({} as any);
+
+  afterEach(async function () {
+    if (!isV3Enabled()) {
+      this.skip();
+    }
+
+    const envRes = await envUtil.readEnv(projectPath, "local", false);
+    chai.assert.isTrue(envRes.isOk());
+    if (envRes.isOk()) {
+      await deleteTeamsApp(envRes.value.TEAMS_APP_ID);
+      await deleteAadAppByObjectId(envRes.value.AAD_APP_OBJECT_ID);
+    }
+    await cleanUpLocalProject(projectPath);
+  });
+
+  it("happy path: provision and deploy", { testPlanCaseId: 17449525 }, async function () {
+    if (!isV3Enabled()) {
+      this.skip();
+    }
+
+    await CliHelper.createProjectWithCapability(appName, testFolder, Capability.Tab);
+    console.log(`[Successfully] scaffold to ${projectPath}`);
+
+    await CliHelper.provisionProject(projectPath, "--env local");
+    const envRes = await envUtil.readEnv(projectPath, "local", false);
+    chai.assert.isTrue(envRes.isOk());
+    if (envRes.isOk()) {
+      chai.assert.isTrue(
+        envRes.value.TEAMS_APP_ID !== undefined && envRes.value.TEAMS_APP_ID !== ""
+      );
+      chai.assert.isTrue(
+        envRes.value.AAD_APP_OBJECT_ID !== undefined && envRes.value.AAD_APP_OBJECT_ID !== ""
+      );
+    }
+    console.log(`[Successfully] provision for ${projectPath}`);
+
+    await CliHelper.deployAll(projectPath, "--env local");
+    console.log(`[Successfully] deploy for ${projectPath}`);
+  });
+});

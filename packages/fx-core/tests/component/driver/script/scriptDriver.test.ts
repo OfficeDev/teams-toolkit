27f3f4ed7f7ae9d8e3ffba544a94cd53350ca096
Siglud <siglud@gmail.com>
2023-03-10 15:02:30 +0800

    chore: use new ui in npm & dotnet & npx driver

--- packages/fx-core/tests/component/driver/script/scriptDriver.test.ts
+++ packages/fx-core/tests/component/driver/script/scriptDriver.test.ts
@@ -11,8 +11,11 @@ import { scriptDriver } from "../../../../src/component/driver/script/scriptDriv
 import { assert } from "chai";
 import { MockUserInteraction } from "../../../core/utils";
 import { err, ok, UserError } from "@microsoft/teamsfx-api";
-import fs from "fs-extra";
+import * as fs from "fs-extra";
 import * as child_process from "child_process";
+import { execCallback } from "../../../../src/component/code/utils";
+import * as utils from "../../../../src/component/code/utils";
+
 describe("Script Driver test", () => {
   const sandbox = sinon.createSandbox();
   beforeEach(() => {
@@ -64,46 +67,34 @@ describe("Script Driver test", () => {
   });
   it("execCallback with Error", async () => {
     sandbox.stub(fs, "appendFile").resolves();
-    const context = {
-      azureAccountProvider: new TestAzureAccountProvider(),
-      logProvider: new TestLogProvider(),
-      ui: new MockUserInteraction(),
-      projectPath: "./",
-    } as DriverContext;
     process.env.SECRET_MY = "VAL";
-    await scriptDriver.execCallback(
+    await execCallback(
       (a: any) => {},
       new Error("error"),
       "SECRET_MY=VAL",
       "SECRET_MY=VAL",
       "",
-      context,
+      new TestLogProvider(),
       "",
       "./log"
     );
   });
   it("execCallback without Error", async () => {
     sandbox.stub(fs, "appendFile").resolves();
-    const context = {
-      azureAccountProvider: new TestAzureAccountProvider(),
-      logProvider: new TestLogProvider(),
-      ui: new MockUserInteraction(),
-      projectPath: "./",
-    } as DriverContext;
     process.env.SECRET_MY = "VAL";
-    await scriptDriver.execCallback(
+    await execCallback(
       (a: any) => {},
       null,
       "SECRET_MY=VAL",
       "SECRET_MY=VAL",
       "",
-      context,
+      new TestLogProvider(),
       "",
       "./log"
     );
   });
   it("execute failed, mock executeCommand fail", async () => {
-    sandbox.stub(scriptDriver, "executeCommand").resolves(err(new UserError({})));
+    sandbox.stub(utils, "executeCommand").resolves(err(new UserError({})));
     const args = {
       workingDirectory: "./",
       run: "::set-output KEY=VALUE",
@@ -128,7 +119,13 @@ describe("Script Driver test", () => {
       ui: new MockUserInteraction(),
       projectPath: "./",
     } as DriverContext;
-    const res = await scriptDriver.executeCommand(args, context);
+    const res = await utils.executeCommand(
+      args.run,
+      context.projectPath,
+      context.logProvider,
+      context.ui,
+      args.workingDirectory
+    );
     assert.isTrue(res.isOk());
     if (res.isOk()) {
       const output = res.value[1];

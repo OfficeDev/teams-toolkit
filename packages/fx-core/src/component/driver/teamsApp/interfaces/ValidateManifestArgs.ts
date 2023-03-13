21d53906c6716e968e5d8671682b13ea5de294dd
Ning Liu <nliu@microsoft.com>
2023-03-13 14:34:12 +0800

    feat: provide teamsApp/validateManifest and teamsApp/validateAppPackage actions

---
+++ packages/fx-core/src/component/driver/teamsApp/interfaces/ValidateManifestArgs.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export interface ValidateManifestArgs {
+  /**
+   * Teams app manifest path
+   */
+  manifestPath: string;
+}

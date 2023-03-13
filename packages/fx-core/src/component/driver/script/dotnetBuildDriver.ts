40f5b653e7c2eecf0de567caa22cd8b8a0b72195
FanH <fanhu@microsoft.com>
2023-03-13 14:18:10 +0800

    chore: npm & dotnet & npx driver using new UX (#8011)
    * chore: use new ui in npm & dotnet & npx driver

--- packages/fx-core/src/component/driver/script/dotnetBuildDriver.ts
+++ packages/fx-core/src/component/driver/script/dotnetBuildDriver.ts
@@ -1,5 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
+/**
+ * @owner fanhu <fanhu@microsoft.com>
+ */
 
 import { BaseBuildDriver } from "./baseBuildDriver";
 import { Service } from "typedi";

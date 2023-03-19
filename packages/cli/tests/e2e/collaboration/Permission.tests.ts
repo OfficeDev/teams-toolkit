31dd7138f278583a320a1c7fde8fcf9ec0820819
KennethBWSong <63089166+KennethBWSong@users.noreply.github.com>
2023-03-19 15:19:01 +0800

    test: update e2e test

--- packages/cli/tests/e2e/collaboration/Permission.tests.ts
+++ packages/cli/tests/e2e/collaboration/Permission.tests.ts
@@ -66,7 +66,7 @@ describe("Collaboration", function () {
       let checkPermissionResult;
       if (isV3Enabled()) {
         checkPermissionResult = await execAsyncWithRetry(
-          `teamsfx permission status --env dev --interactive false --env dev --teams-app-manifest ${projectPath}/appPackage/manifest.json --aad-app-manifest ${projectPath}/aad.manifest.json`,
+          `teamsfx permission status --env dev --interactive false --teams-app-manifest ${projectPath}/appPackage/manifest.json --aad-app-manifest ${projectPath}/aad.manifest.json`,
           {
             cwd: projectPath,
             env: process.env,

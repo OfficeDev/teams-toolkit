a39ec6face96c91d0b18fad2e7a2569355f2d974
Ning Liu <nliu@microsoft.com>
2023-03-20 15:10:45 +0800

    fix: ut coverage

--- packages/fx-core/tests/core/FxCore.test.ts
+++ packages/fx-core/tests/core/FxCore.test.ts
@@ -64,6 +64,7 @@ import { MissingEnvInFileUserError } from "../../src/component/driver/aad/error/
 import { pathUtils } from "../../src/component/utils/pathUtils";
 import { AddWebPartDriver } from "../../src/component/driver/add/addWebPart";
 import { ValidateAppPackageDriver } from "../../src/component/driver/teamsApp/validateAppPackage";
+import { CreateAppPackageDriver } from "../../src/component/driver/teamsApp/createAppPackage";
 
 describe("Core basic APIs", () => {
   const sandbox = sinon.createSandbox();
@@ -895,6 +896,20 @@ describe("Teams app APIs", () => {
 
     const runSpy = sinon.spy(ValidateAppPackageDriver.prototype, "run");
     await core.validateApplication(inputs);
+    sinon.assert.calledOnce(runSpy);
+  });
+
+  it("create app package", async () => {
+    const appName = await mockV3Project();
+    const inputs: Inputs = {
+      platform: Platform.VSCode,
+      [CoreQuestionNames.Folder]: os.tmpdir(),
+      [CoreQuestionNames.TeamsAppManifestFilePath]: ".\\appPackage\\appPackage.dev.zip",
+      projectPath: path.join(os.tmpdir(), appName),
+    };
+
+    const runSpy = sinon.spy(CreateAppPackageDriver.prototype, "run");
+    await core.createAppPackage(inputs);
     sinon.assert.calledOnce(runSpy);
   });
 });

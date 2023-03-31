eb2bbf6a3e65a0da96cce5c84a00ef71b86eef24
huimiao <huimiao@microsoft.com>
2023-03-31 11:22:49 +0800

    chore: add loading

--- templates/tab/js/dashboard/src/App.jsx
+++ templates/tab/js/dashboard/src/App.jsx
@@ -5,11 +5,12 @@ import { HashRouter as Router, Redirect, Route } from "react-router-dom";
 // https://fluentsite.z22.web.core.windows.net/quick-start
 import {
   FluentProvider,
+  Spinner,
   teamsDarkTheme,
   teamsHighContrastTheme,
   teamsLightTheme,
 } from "@fluentui/react-components";
-import { useTeams } from "@microsoft/teamsfx-react";
+import { useTeamsUserCredential } from "@microsoft/teamsfx-react";
 
 import { TeamsFxContext } from "./internal/context";
 import SampleDashboard from "./views/dashboards/SampleDashboard";
@@ -22,9 +23,12 @@ import TermsOfUse from "./views/TermsOfUse";
  * of the app.
  */
 export default function App() {
-  const { themeString } = useTeams({})[0];
+  const { loading, themeString, teamsUserCredential } = useTeamsUserCredential({
+    initiateLoginEndpoint: process.env.REACT_APP_START_LOGIN_PAGE_URL,
+    clientId: process.env.REACT_APP_CLIENT_ID,
+  });
   return (
-    <TeamsFxContext.Provider value={{ themeString }}>
+    <TeamsFxContext.Provider value={{ themeString, teamsUserCredential }}>
       <FluentProvider
         id="fluent-provider"
         theme={
@@ -39,14 +43,16 @@ export default function App() {
           <Route exact path="/">
             <Redirect to="/tab" />
           </Route>
-          {
+          {loading ? (
+            <Spinner id="spinner" />
+          ) : (
             <>
               <Route exact path="/privacy" component={Privacy} />
               <Route exact path="/termsofuse" component={TermsOfUse} />
               <Route exact path="/tab" component={SampleDashboard} />
               <Route exact path="/config" component={TabConfig} />
             </>
-          }
+          )}
         </Router>
       </FluentProvider>
     </TeamsFxContext.Provider>

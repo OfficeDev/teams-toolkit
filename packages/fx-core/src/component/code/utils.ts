9a313b79920772c1b012321a98882b0a702a5996
huajiezhang <huajiezhang@microsoft.com>
2023-04-10 12:50:21 +0800

    chore(core): refine script driver error messasge

--- packages/fx-core/src/component/code/utils.ts
+++ packages/fx-core/src/component/code/utils.ts
@@ -8,6 +8,7 @@ import { exec } from "child_process";
 import { DriverContext } from "../driver/interface/commonArgs";
 import fs from "fs-extra";
 import { DotenvOutput } from "../utils/envUtil";
+import { ScriptExecutionError, ScriptTimeoutError } from "../../error/script";
 
 export function convertToLangKey(programmingLanguage: string): string {
   switch (programmingLanguage) {
@@ -109,12 +110,13 @@ export async function executeCommand(
       },
       async (error, stdout, stderr) => {
         if (error) {
-          await logProvider.error(
-            `Failed to run command: "${maskSecretValues(command)}" on path: "${workingDir}".`
-          );
-          resolve(err(assembleError(error)));
+          if (error.killed) {
+            resolve(err(new ScriptTimeoutError(run)));
+          } else {
+            resolve(err(new ScriptExecutionError(error.message)));
+          }
         } else {
-          // parse '::set-output' patterns
+          // handle '::set-output' or '::set-teamsfx-env' pattern
           const outputString = outputStrings.join("");
           const outputObject = parseSetOutputCommand(outputString);
           resolve(ok([outputString, outputObject]));
@@ -139,14 +141,17 @@ export async function executeCommand(
   });
 }
 
+const SET_ENV_CMD1 = "::set-output ";
+const SET_ENV_CMD2 = "::set-teamsfx-env ";
+
 function parseSetOutputCommand(stdout: string): DotenvOutput {
   const lines = stdout.toString().replace(/\r\n?/gm, "\n").split(/\r?\n/);
   const output: DotenvOutput = {};
   for (const line of lines) {
-    if (line.startsWith("::set-output ") || line.startsWith("set-teamsfx-env ")) {
-      const str = line.startsWith("::set-output ")
-        ? line.substring(12).trim()
-        : line.substring(15).trim();
+    if (line.startsWith(SET_ENV_CMD1) || line.startsWith(SET_ENV_CMD2)) {
+      const str = line.startsWith(SET_ENV_CMD1)
+        ? line.substring(SET_ENV_CMD1.length).trim()
+        : line.substring(SET_ENV_CMD2.length).trim();
       const arr = str.split("=");
       if (arr.length === 2) {
         const key = arr[0].trim();
---
+++ packages/fx-core/src/error/script.ts
@@ -0,0 +1,39 @@
+import {
+  SystemError,
+  SystemErrorOptions,
+  UserError,
+  UserErrorOptions,
+} from "@microsoft/teamsfx-api";
+import { getDefaultString, getLocalizedString } from "../common/localizeUtils";
+
+/**
+ * Script execution timeout
+ */
+export class ScriptTimeoutError extends UserError {
+  constructor(cmd: string) {
+    const key = "error.script.ScriptTimeoutError";
+    const errorOptions: UserErrorOptions = {
+      source: "script",
+      name: "ScriptTimeoutError",
+      message: getDefaultString(key, cmd),
+      displayMessage: getLocalizedString(key, cmd),
+    };
+    super(errorOptions);
+  }
+}
+
+/**
+ * Script execution error
+ */
+export class ScriptExecutionError extends UserError {
+  constructor(message: string) {
+    const key = "error.script.ScriptExecutionError";
+    const errorOptions: UserErrorOptions = {
+      source: "script",
+      name: "ScriptExecutionError",
+      message: getDefaultString(key, message),
+      displayMessage: getLocalizedString(key, message),
+    };
+    super(errorOptions);
+  }
+}

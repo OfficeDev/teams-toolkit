90e9627c064c506e8e607e363fdd709102a56f12
huajiezhang <huajiezhang@microsoft.com>
2023-04-10 14:14:02 +0800

    chore: ut

--- packages/fx-core/tests/component/driver/script/scriptDriver.test.ts
+++ packages/fx-core/tests/component/driver/script/scriptDriver.test.ts
@@ -15,7 +15,8 @@ import * as fs from "fs-extra";
 import mockedEnv, { RestoreFn } from "mocked-env";
 import * as child_process from "child_process";
 import * as utils from "../../../../src/component/code/utils";
-import { ScriptTimeoutError } from "../../../../src/error/script";
+import { ScriptExecutionError, ScriptTimeoutError } from "../../../../src/error/script";
+import { convertScriptErrorToFxError } from "../../../../src/component/code/utils";
 
 describe("Script Driver test", () => {
   const sandbox = sinon.createSandbox();
@@ -159,24 +160,14 @@ describe("Script Driver test", () => {
     );
     assert.isTrue(res.isErr());
   });
-  it("execute command ScriptTimeoutError", async () => {
-    const args = {
-      workingDirectory: "./",
-      shell: "cmd",
-      timeout: 1,
-      run: "ping www.abc.com",
-    };
-    const context = {
-      azureAccountProvider: new TestAzureAccountProvider(),
-      logProvider: new TestLogProvider(),
-      ui: new MockUserInteraction(),
-      projectPath: "./",
-    } as DriverContext;
-    sandbox.stub(context.ui!, "runCommand").resolves(ok(""));
-    const res = await scriptDriver.execute(args, context);
-    assert.isTrue(res.result.isErr());
-    if (res.result.isErr()) {
-      assert.isTrue(res.result.error instanceof ScriptTimeoutError);
-    }
+  it("convertScriptErrorToFxError ScriptTimeoutError", async () => {
+    const error = { killed: true } as child_process.ExecException;
+    const res = convertScriptErrorToFxError(error, () => {}, "test");
+    assert.isTrue(res instanceof ScriptTimeoutError);
+  });
+  it("convertScriptErrorToFxError ScriptExecutionError", async () => {
+    const error = { killed: false, message: "command failed" } as child_process.ExecException;
+    const res = convertScriptErrorToFxError(error, () => {}, "test");
+    assert.isTrue(res instanceof ScriptExecutionError);
   });
 });

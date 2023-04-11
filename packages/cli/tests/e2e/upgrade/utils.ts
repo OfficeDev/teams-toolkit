46ecc0fd9740948e5f1512deb884e5012f8d492c
xzf0587 <zhaofengxu@microsoft.com>
2023-04-11 13:32:27 +0800

    chore(e2e): update

--- packages/cli/tests/e2e/upgrade/utils.ts
+++ packages/cli/tests/e2e/upgrade/utils.ts
@@ -4,11 +4,9 @@
 /**
  * @author xzf0587 <zhaofengxu@microsoft.com>
  */
-import { Inputs, Platform } from "@microsoft/teamsfx-api";
 import { assert } from "chai";
 import fs from "fs-extra";
 import "mocha";
-import * as path from "path";
 
 const ymlHeader =
   "# yaml-language-server: $schema=https://developer.microsoft.com/json-schemas/teams-toolkit/teamsapp-yaml/1.0.0/yaml.schema.json";

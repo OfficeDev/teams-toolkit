e103d19b165752fc52e5f0bcc1bf11bbf6866065
huimiao <huimiao@microsoft.com>
2023-04-11 17:19:55 +0800

    chore: format a file

--- templates/scenarios/js/dashboard-tab/src/dashboards/SampleDashboard.jsx
+++ templates/scenarios/js/dashboard-tab/src/dashboards/SampleDashboard.jsx
@@ -4,7 +4,6 @@ import ChartWidget from "../widgets/ChartWidget";
 import ListWidget from "../widgets/ListWidget";
 
 export default class SampleDashboard extends BaseDashboard {
-  
   layout() {
     return (
       <>

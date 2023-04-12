c397f000831e2503dfedf27d12046505e5cb90d4
Ning Tang <nintan@microsoft.com>
2023-04-12 18:10:45 +0800

    chore: fix ut

--- packages/vscode-extension/test/localdebug/commonUtils.test.ts
+++ packages/vscode-extension/test/localdebug/commonUtils.test.ts
@@ -8,9 +8,11 @@ import * as sinon from "sinon";
 import { ok } from "@microsoft/teamsfx-api";
 
 import * as commonUtils from "../../src/debug/commonUtils";
+import * as globalVariables from "../../src/globalVariables";
 import { metadataUtil } from "@microsoft/teamsfx-core/build/component/utils/metadataUtil";
 import { pathUtils } from "@microsoft/teamsfx-core/build/component/utils/pathUtils";
 import { envUtil } from "@microsoft/teamsfx-core/build/component/utils/envUtil";
+import { Uri } from "vscode";
 
 const testDataFolder = path.resolve(__dirname, "test-data");
 
@@ -69,6 +71,7 @@ describe("[debug > commonUtils]", () => {
     });
 
     it("returns teamsAppId successfully", async () => {
+      sandbox.stub(globalVariables, "workspaceUri").value(Uri.file("test"));
       sandbox.stub(pathUtils, "getYmlFilePath");
       sandbox.stub(metadataUtil, "parse").resolves(
         ok({

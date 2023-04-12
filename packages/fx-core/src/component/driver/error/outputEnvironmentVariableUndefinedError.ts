4b62a050238d01c8f94faa2ecb921c38e026f5c8
blackchoey <blackchoey@gmail.com>
2023-04-12 10:09:24 +0800

    chore: resolve comments

--- packages/fx-core/src/component/driver/error/outputEnvironmentVariableUndefinedError.ts
+++ packages/fx-core/src/component/driver/error/outputEnvironmentVariableUndefinedError.ts
@@ -5,7 +5,7 @@ import { SystemError } from "@microsoft/teamsfx-api";
 import { getDefaultString, getLocalizedString } from "../../../common/localizeUtils";
 
 const errorCode = "OutputEnvironmentVariableUndefined";
-const messageKey = "driver.error.outputEnvironmentVariableUndefined"; // The output environment variable name(s) are not defined.
+const messageKey = "error.driver.outputEnvironmentVariableUndefined"; // The output environment variable name(s) are not defined.
 
 // This error should only be thrown when an internal logic calls the action directly. If users defines wrong output env vars in the yml file, the schema validation is expected to find the error and instruct users to fix them.
 export class OutputEnvironmentVariableUndefinedError extends SystemError {

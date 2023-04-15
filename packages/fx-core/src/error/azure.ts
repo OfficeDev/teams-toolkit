ce91c224cbf9d0f6f7977deadcb3baf210dfadb6
huajiezhang <huajiezhang@microsoft.com>
2023-04-15 11:02:12 +0800

    chore(core): refine resource group errors

--- packages/fx-core/src/error/azure.ts
+++ packages/fx-core/src/error/azure.ts
@@ -86,3 +86,83 @@ export class ResourceGroupNotExistError extends UserError {
     super(errorOptions);
   }
 }
+
+/**
+ * Create resource group error
+ */
+export class CreateResourceGroupError extends UserError {
+  constructor(resourceGroupName: string, subscriptionId: string, message: string) {
+    const key = "error.azure.CreateResourceGroupError";
+    const errorOptions: UserErrorOptions = {
+      source: "coordinator",
+      name: "CreateResourceGroupError",
+      message: getDefaultString(key, resourceGroupName, subscriptionId, message),
+      displayMessage: getLocalizedString(key, resourceGroupName, subscriptionId, message),
+    };
+    super(errorOptions);
+  }
+}
+
+/**
+ * Check resource group existence error
+ */
+export class CheckResourceGroupExistenceError extends UserError {
+  constructor(resourceGroupName: string, subscriptionId: string, message: string) {
+    const key = "error.azure.CheckResourceGroupExistenceError";
+    const errorOptions: UserErrorOptions = {
+      source: "coordinator",
+      name: "CheckResourceGroupExistenceError",
+      message: getDefaultString(key, resourceGroupName, subscriptionId, message),
+      displayMessage: getLocalizedString(key, resourceGroupName, subscriptionId, message),
+    };
+    super(errorOptions);
+  }
+}
+
+/**
+ * List resource groups error
+ */
+export class ListResourceGroupsError extends UserError {
+  constructor(subscriptionId: string, message: string) {
+    const key = "error.azure.ListResourceGroupsError";
+    const errorOptions: UserErrorOptions = {
+      source: "coordinator",
+      name: "ListResourceGroupsError",
+      message: getDefaultString(key, subscriptionId, message),
+      displayMessage: getLocalizedString(key, subscriptionId, message),
+    };
+    super(errorOptions);
+  }
+}
+
+/**
+ * Get resource group error
+ */
+export class GetResourceGroupError extends UserError {
+  constructor(resourceGroupName: string, subscriptionId: string, message: string) {
+    const key = "error.azure.GetResourceGroupError";
+    const errorOptions: UserErrorOptions = {
+      source: "coordinator",
+      name: "GetResourceGroupError",
+      message: getDefaultString(key, resourceGroupName, subscriptionId, message),
+      displayMessage: getLocalizedString(key, resourceGroupName, subscriptionId, message),
+    };
+    super(errorOptions);
+  }
+}
+
+/**
+ * List resource group locations error
+ */
+export class ListResourceGroupLocationsError extends UserError {
+  constructor(subscriptionId: string, message: string) {
+    const key = "error.azure.ListResourceGroupLocationsError";
+    const errorOptions: UserErrorOptions = {
+      source: "coordinator",
+      name: "ListResourceGroupLocationsError",
+      message: getDefaultString(key, subscriptionId, message),
+      displayMessage: getLocalizedString(key, subscriptionId, message),
+    };
+    super(errorOptions);
+  }
+}

1dbafee062ba0d3faad65952bdda9e47ef726d97
Xiaofu Huang <xiaofhua@microsoft.com>
2023-04-20 13:36:20 +0800

    fix: remove mkdir for symlink

--- packages/fx-core/src/common/deps-checker/util/fileHelper.ts
+++ packages/fx-core/src/common/deps-checker/util/fileHelper.ts
@@ -27,7 +27,7 @@ export async function createSymlink(target: string, linkFilePath: string): Promi
   if (await fs.pathExists(linkFilePath)) {
     throw new DepsCheckerError(Messages.symlinkDirAlreadyExist(), defaultHelpLink);
   }
-  await fs.mkdir(path.dirname(linkFilePath), { recursive: true });
+
   return await fs.ensureSymlink(
     target,
     linkFilePath,

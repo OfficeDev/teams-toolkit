ae6569cfae06df917e3d22b23b47f818ffbed3d1
Alive-Fish <zhiyou@microsoft.com>
2023-04-25 10:49:10 +0800

    test: remove unused e2e tests (#8529)
    * test: remove unused e2e tests
    
    * test: fix account command e2e test

--- packages/cli/tests/e2e/command/helpInfo.tests.ts
+++ packages/cli/tests/e2e/command/helpInfo.tests.ts
@@ -5,11 +5,9 @@
  * @author Zhiyu You <zhiyou@microsoft.com>
  */
 
-import { isPreviewFeaturesEnabled } from "@microsoft/teamsfx-core/build/common/featureFlags";
-import { expect } from "chai";
 import { it } from "@microsoft/extra-shot-mocha";
+import { expect } from "chai";
 import { execAsync } from "../commonUtils";
-import { isV3Enabled } from "@microsoft/teamsfx-core";
 
 describe("teamsfx command help", function () {
   it(`teamsfx account show -h`, { testPlanCaseId: 15685961 }, async function () {
@@ -18,21 +16,5 @@ describe("teamsfx command help", function () {
       timeout: 0,
     });
     expect(result.stdout).not.includes("--action");
-  });
-
-  it(`teamsfx add azure-apim -h`, { testPlanCaseId: 15685963 }, async function () {
-    /// TODO: will be deleted when not support V2
-    if (isV3Enabled()) {
-      this.skip();
-    }
-    const command = isPreviewFeaturesEnabled()
-      ? `teamsfx add azure-apim -h`
-      : `teamsfx resource add azure-apim -h`;
-    const result = await execAsync(command, {
-      env: process.env,
-      timeout: 0,
-    });
-    expect(result.stdout).includes("--function-name");
-    expect(result.stdout).not.includes("--resource-group");
   });
 });

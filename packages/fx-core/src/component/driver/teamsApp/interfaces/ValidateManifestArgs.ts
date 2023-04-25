a00028ab0a1dc8f61dc08f10349176addbc29998
Ning Liu <nliu@microsoft.com>
2023-04-25 14:40:56 +0800

    fix: unify /validateManifest output

--- packages/fx-core/src/component/driver/teamsApp/interfaces/ValidateManifestArgs.ts
+++ packages/fx-core/src/component/driver/teamsApp/interfaces/ValidateManifestArgs.ts
@@ -6,4 +6,9 @@ export interface ValidateManifestArgs {
    * Teams app manifest path
    */
   manifestPath: string;
+  /**
+   * Internal arguments
+   * Show message for non-life cycle command
+   */
+  showMessage?: boolean;
 }

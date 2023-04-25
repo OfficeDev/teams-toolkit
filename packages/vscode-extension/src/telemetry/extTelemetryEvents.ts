7d0c5139806d66481c392ecd5c09a9eb6c487122
wenyutang-ms <wenyutang@microsoft.com>
2023-04-25 09:42:41 +0800

    docs: remove prerelease readme for stable version

--- packages/vscode-extension/src/telemetry/extTelemetryEvents.ts
+++ packages/vscode-extension/src/telemetry/extTelemetryEvents.ts
@@ -192,8 +192,6 @@ export enum TelemetryEvent {
   ShowWhatIsNewNotification = "show-what-is-new-notification",
   ShowWhatIsNewContext = "show-what-is-new-context",
 
-  ShowPreivewNotification = "show-preview-notification",
-
   ShowLocalDebugNotification = "show-local-debug-notification",
   ShowLocalPreviewNotification = "show-local-preview-notification",
   ClickLocalDebug = "click-local-debug",

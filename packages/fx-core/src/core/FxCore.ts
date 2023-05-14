b325c8d7023779dfd79f90202433dc1164d6d85f
huajiezhang <huajiezhang@microsoft.com>
2023-05-14 13:46:42 +0800

    refactor: clean up middlewares

--- packages/fx-core/src/core/FxCore.ts
+++ packages/fx-core/src/core/FxCore.ts
@@ -17,7 +17,6 @@ import {
   InputsWithProjectPath,
   ok,
   Platform,
-  ProjectConfig,
   ProjectConfigV3,
   ProjectSettings,
   ProjectSettingsV3,
@@ -47,6 +46,7 @@ import { DriverContext } from "../component/driver/interface/commonArgs";
 import "../component/driver/script/scriptDriver";
 import { ApiConnectorImpl } from "../component/feature/apiconnector/ApiConnectorImpl";
 import { addCicdQuestion } from "../component/feature/cicd/cicd";
+import { EnvLoaderMW } from "../component/middleware/envMW";
 import { QuestionMW } from "../component/middleware/questionMW";
 import {
   FeatureId,
@@ -61,6 +61,7 @@ import {
 import { AppManifest, publishQuestion } from "../component/resource/appManifest/appManifest";
 import { createContextV3 } from "../component/utils";
 import { envUtil } from "../component/utils/envUtil";
+import { settingsUtil } from "../component/utils/settingsUtil";
 import { CallbackRegistry } from "./callback";
 import { checkPermission, grantPermission, listCollaborator } from "./collaborator";
 import { LocalCrypto } from "./crypto";
@@ -72,17 +73,14 @@ import { AadManifestMigrationMW } from "./middleware/aadManifestMigration";
 import { ConcurrentLockerMW } from "./middleware/concurrentLocker";
 import { ProjectConsolidateMW } from "./middleware/consolidateLocalRemote";
 import { ContextInjectorMW } from "./middleware/contextInjector";
-import { EnvInfoLoaderMW_V3, loadEnvInfoV3 } from "./middleware/envInfoLoaderV3";
-import { EnvInfoWriterMW_V3 } from "./middleware/envInfoWriterV3";
+import { loadEnvInfoV3 } from "./middleware/envInfoLoaderV3";
 import { ErrorHandlerMW } from "./middleware/errorHandler";
 import { ProjectMigratorMW } from "./middleware/projectMigrator";
 import { ProjectSettingsLoaderMW } from "./middleware/projectSettingsLoader";
-import { ProjectSettingsWriterMW } from "./middleware/projectSettingsWriter";
 import { ProjectVersionCheckerMW } from "./middleware/projectVersionChecker";
 import { getQuestionsForCreateProjectV2 } from "./middleware/questionModel";
 import { CoreQuestionNames } from "./question";
 import { CoreHookContext, PreProvisionResForVS, VersionCheckRes } from "./types";
-import { settingsUtil } from "../component/utils/settingsUtil";
 
 export class FxCore implements v3.ICore {
   tools: Tools;
@@ -343,27 +341,13 @@ export class FxCore implements v3.ICore {
   /**
    * @deprecated in V3
    */
-  @hooks([
-    ErrorHandlerMW,
-    ConcurrentLockerMW,
-    ProjectMigratorMW,
-    ProjectConsolidateMW,
-    AadManifestMigrationMW,
-    ProjectVersionCheckerMW,
-    ProjectSettingsLoaderMW,
-    EnvInfoLoaderMW_V3(false),
-    ContextInjectorMW,
-  ])
   async getProjectConfig(
     inputs: Inputs,
     ctx?: CoreHookContext
-  ): Promise<Result<ProjectConfig | undefined, FxError>> {
-    if (!ctx) return err(new ObjectIsUndefinedError("getProjectConfig input stuff"));
-    inputs.stage = Stage.getProjectConfig;
-    setCurrentStage(Stage.getProjectConfig);
+  ): Promise<Result<any | undefined, FxError>> {
     return ok({
-      settings: ctx.projectSettings,
-      config: ctx.envInfoV3?.state,
+      settings: {},
+      config: {},
     });
   }
 
@@ -434,7 +418,7 @@ export class FxCore implements v3.ICore {
     ErrorHandlerMW,
     ConcurrentLockerMW,
     ProjectSettingsLoaderMW,
-    EnvInfoLoaderMW_V3(false),
+    EnvLoaderMW(false),
     ContextInjectorMW,
   ])
   async getSelectedEnv(
--- packages/fx-core/src/core/middleware/envInfoLoaderV3.ts
+++ packages/fx-core/src/core/middleware/envInfoLoaderV3.ts
@@ -1,41 +1,25 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { Middleware, NextFunction } from "@feathersjs/hooks/lib";
-import _ from "lodash";
 import {
-  err,
   FxError,
   Inputs,
-  Json,
-  ok,
   ProjectSettings,
   QTreeNode,
   Result,
-  Stage,
-  Tools,
+  err,
+  ok,
   traverse,
-  UserCancelError,
   v2,
   v3,
 } from "@microsoft/teamsfx-api";
-import { isV3Enabled } from "../../common/tools";
-import { ComponentNames } from "../../component/constants";
-import { envLoaderMWImpl } from "../../component/middleware/envMW";
 import { LocalCrypto } from "../crypto";
 import { environmentManager, newEnvInfoV3 } from "../environment";
-import { NoProjectOpenedError, ProjectSettingsUndefinedError } from "../error";
-import { globalVars, TOOLS } from "../globalVars";
-import {
-  getQuestionNewTargetEnvironmentName,
-  QuestionSelectSourceEnvironment,
-  QuestionSelectTargetEnvironment,
-} from "../question";
+import { NoProjectOpenedError } from "../error";
+import { TOOLS } from "../globalVars";
+import { QuestionSelectSourceEnvironment, getQuestionNewTargetEnvironmentName } from "../question";
 import { CoreHookContext } from "../types";
-import { shouldIgnored } from "./projectSettingsLoader";
-import { FileNotFoundError } from "../../error/common";
 
-const newTargetEnvNameOption = "+ new environment";
 const lastUsedMark = " (last used)";
 export let lastUsedEnv: string | undefined;
 
@@ -44,93 +28,6 @@ export type CreateEnvCopyInput = {
   sourceEnvName: string;
 };
 
-export function EnvInfoLoaderMW_V3(skip: boolean, ignoreLocalEnv = false): Middleware {
-  return async (ctx: CoreHookContext, next: NextFunction) => {
-    if (shouldIgnored(ctx)) {
-      await next();
-      return;
-    }
-    const inputs = ctx.arguments[ctx.arguments.length - 1] as Inputs;
-    if (isV3Enabled()) {
-      const envBefore = _.cloneDeep(process.env);
-      try {
-        await envLoaderMWImpl(inputs.ignoreLocalEnv || ignoreLocalEnv ? false : true, ctx, next);
-        return;
-      } finally {
-        const keys = Object.keys(process.env);
-        for (const k of keys) {
-          if (!(k in envBefore)) {
-            delete process.env[k];
-          } else {
-            process.env[k] = envBefore[k];
-          }
-        }
-      }
-    }
-
-    if (!ctx.projectSettings) {
-      ctx.result = err(ProjectSettingsUndefinedError());
-      return;
-    }
-
-    if (!inputs.projectPath) {
-      ctx.result = err(new NoProjectOpenedError());
-      return;
-    }
-
-    // make sure inputs.env always has value so telemetry can use it.
-    if (inputs.stage === Stage.debug) inputs.ignoreEnvInfo = false; // for local debug v3, envInfo should not be ignored
-    const envRes = await getTargetEnvName(skip, inputs, ctx);
-    if (envRes.isErr()) {
-      ctx.result = err(envRes.error);
-      return;
-    }
-    inputs.env = envRes.value;
-
-    const result = await loadEnvInfoV3(
-      inputs as v2.InputsWithProjectPath,
-      ctx.projectSettings,
-      inputs.env,
-      skip || inputs.ignoreEnvInfo
-    );
-    if (result.isErr()) {
-      ctx.result = err(result.error);
-      return;
-    }
-
-    ctx.envInfoV3 = result.value;
-
-    upgradeProgrammingLanguage(ctx.envInfoV3.state, ctx.projectSettings);
-    upgradeDefaultFunctionName(ctx.envInfoV3.state, ctx.projectSettings);
-
-    // set globalVars for teamsAppId and m365TenantId
-    const appManifestKey = ComponentNames.AppManifest;
-    globalVars.teamsAppId = ctx.envInfoV3.state?.[appManifestKey]?.teamsAppId;
-    globalVars.m365TenantId = ctx.envInfoV3.state?.[appManifestKey]?.m365TenantId;
-    await next();
-  };
-}
-export function upgradeProgrammingLanguage(solutionConfig: Json, projectSettings: ProjectSettings) {
-  const programmingLanguage = solutionConfig.solution?.programmingLanguage;
-  if (programmingLanguage) {
-    // add programmingLanguage in project settings
-    projectSettings.programmingLanguage = programmingLanguage;
-
-    // remove programmingLanguage in solution config
-    solutionConfig.solution.programmingLanguage = undefined;
-  }
-}
-
-export function upgradeDefaultFunctionName(solutionConfig: Json, projectSettings: ProjectSettings) {
-  // upgrade defaultFunctionName if exists.
-  const defaultFunctionName = solutionConfig.solution?.defaultFunctionName;
-  if (defaultFunctionName) {
-    // add defaultFunctionName in project settings
-    projectSettings.defaultFunctionName = defaultFunctionName;
-    // remove defaultFunctionName in function plugin's config
-    solutionConfig.solution.defaultFunctionName = undefined;
-  }
-}
 export async function loadEnvInfoV3(
   inputs: v2.InputsWithProjectPath,
   projectSettings: ProjectSettings,
@@ -161,75 +58,6 @@ export async function loadEnvInfoV3(
     envInfo = envDataResult.value as v3.EnvInfoV3;
   }
   return ok(envInfo);
-}
-
-export async function getTargetEnvName(
-  skip: boolean,
-  inputs: Inputs,
-  ctx: CoreHookContext
-): Promise<Result<string, FxError>> {
-  let targetEnvName: string;
-  if (!skip && !inputs.ignoreEnvInfo) {
-    // TODO: This is a workaround for collabrator & manifest preview feature to programmatically load an env in extension.
-    if (inputs.env) {
-      const result = await useUserSetEnv(inputs.projectPath!, inputs.env);
-      if (result.isErr()) {
-        ctx.result = result;
-        return err(result.error);
-      }
-      targetEnvName = result.value;
-    } else {
-      const result = await askTargetEnvironment(TOOLS, inputs);
-      if (result.isErr()) {
-        ctx.result = err(result.error);
-        return err(result.error);
-      }
-      targetEnvName = result.value;
-      TOOLS.logProvider.info(
-        `[${targetEnvName}] is selected as the target environment to ${ctx.method}`
-      );
-
-      lastUsedEnv = targetEnvName;
-    }
-  } else {
-    targetEnvName = environmentManager.getDefaultEnvName();
-  }
-  return ok(targetEnvName);
-}
-
-export async function askTargetEnvironment(
-  tools: Tools,
-  inputs: Inputs
-): Promise<Result<string, FxError>> {
-  const getQuestionRes = await getQuestionsForTargetEnv(inputs, lastUsedEnv);
-  if (getQuestionRes.isErr()) {
-    tools.logProvider.error(
-      `[core:env] failed to get questions for target environment: ${getQuestionRes.error.message}`
-    );
-    return err(getQuestionRes.error);
-  }
-
-  tools.logProvider.debug(`[core:env] success to get questions for target environment.`);
-
-  const node = getQuestionRes.value;
-  if (node) {
-    const res = await traverse(node, inputs, tools.ui);
-    if (res.isErr()) {
-      tools.logProvider.debug(`[core:env] failed to run question model for target environment.`);
-      return err(res.error);
-    }
-  }
-
-  if (!inputs.targetEnvName) {
-    return err(UserCancelError);
-  }
-
-  let targetEnvName = inputs.targetEnvName;
-  if (targetEnvName.endsWith(lastUsedMark)) {
-    targetEnvName = targetEnvName.slice(0, targetEnvName.indexOf(lastUsedMark));
-  }
-
-  return ok(targetEnvName);
 }
 
 export async function askNewEnvironment(
@@ -269,60 +97,6 @@ export async function askNewEnvironment(
     targetEnvName: inputs.newTargetEnvName,
     sourceEnvName: selectedEnvName,
   };
-}
-
-export async function useUserSetEnv(
-  projectPath: string,
-  env: string
-): Promise<Result<string, FxError>> {
-  let checkEnv = await environmentManager.checkEnvExist(projectPath, env);
-  if (checkEnv.isErr()) {
-    return err(checkEnv.error);
-  }
-
-  let envExists = checkEnv.value;
-  if (!envExists) {
-    if (env === environmentManager.getLocalEnvName()) {
-      await environmentManager.createLocalEnv(projectPath);
-      checkEnv = await environmentManager.checkEnvExist(projectPath, env);
-      if (checkEnv.isErr()) {
-        return err(checkEnv.error);
-      }
-      envExists = checkEnv.value;
-    }
-    if (!envExists) {
-      return err(new FileNotFoundError("EnvInfoLoaderMW_V3", `config.${env}.json`));
-    }
-  }
-
-  return ok(env);
-}
-
-export async function getQuestionsForTargetEnv(
-  inputs: Inputs,
-  lastUsed?: string
-): Promise<Result<QTreeNode | undefined, FxError>> {
-  if (!inputs.projectPath) {
-    return err(new NoProjectOpenedError());
-  }
-
-  const envProfilesResult = await environmentManager.listRemoteEnvConfigs(inputs.projectPath, true);
-  if (envProfilesResult.isErr()) {
-    return err(envProfilesResult.error);
-  }
-
-  const envList = reOrderEnvironments(envProfilesResult.value, lastUsed);
-  const selectEnv = QuestionSelectTargetEnvironment();
-  selectEnv.staticOptions = envList;
-
-  const node = new QTreeNode(selectEnv);
-
-  const childNode = new QTreeNode(getQuestionNewTargetEnvironmentName(inputs.projectPath));
-  childNode.condition = { equals: newTargetEnvNameOption };
-
-  node.addChild(childNode);
-
-  return ok(node.trim());
 }
 
 async function getQuestionsForNewEnv(

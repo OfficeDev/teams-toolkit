f6b09397960b318c34a0a37f31e5b6bf6b130e15
qinezh <qinezh@users.noreply.github.com>
2023-05-18 14:29:49 +0800

    feat: update js/ts template with latest SDK (#8736)

--- templates/scenarios/js/notification-timer-trigger/src/timerTrigger.js
+++ templates/scenarios/js/notification-timer-trigger/src/timerTrigger.js
@@ -5,53 +5,78 @@ const { notificationApp } = require("./internal/initialize");
 // Time trigger to send notification. You can change the schedule in ../timerNotifyTrigger/function.json
 module.exports = async function (context, myTimer) {
   const timeStamp = new Date().toISOString();
-  for (const target of await notificationApp.notification.installations()) {
-    await target.sendAdaptiveCard(
-      AdaptiveCards.declare(notificationTemplate).render({
-        title: "New Event Occurred!",
-        appName: "Contoso App Notification",
-        description: `This is a sample time-triggered notification (${timeStamp}).`,
-        notificationUrl: "https://aka.ms/teamsfx-notification-new",
-      })
+  const pageSize = 100;
+  let continuationToken = undefined;
+  do {
+    const pagedData = await notificationApp.notification.getPagedInstallations(
+      pageSize,
+      continuationToken
     );
-  }
+    const installations = pagedData.data;
+    continuationToken = pagedData.continuationToken;
 
-  /****** To distinguish different target types ******/
-  /** "Channel" means this bot is installed to a Team (default to notify General channel)
-  if (target.type === NotificationTargetType.Channel) {
-    // Directly notify the Team (to the default General channel)
-    await target.sendAdaptiveCard(...);
-    // List all channels in the Team then notify each channel
-    const channels = await target.channels();
-    for (const channel of channels) {
-      await channel.sendAdaptiveCard(...);
-    }
-    // List all members in the Team then notify each member
-    const members = await target.members();
-    for (const member of members) {
-      await member.sendAdaptiveCard(...);
-    }
-  }
-  **/
+    for (const target of installations) {
+      await target.sendAdaptiveCard(
+        AdaptiveCards.declare(notificationTemplate).render({
+          title: "New Event Occurred!",
+          appName: "Contoso App Notification",
+          description: `This is a sample time-triggered notification (${timeStamp}).`,
+          notificationUrl: "https://aka.ms/teamsfx-notification-new",
+        })
+      );
 
-  /** "Group" means this bot is installed to a Group Chat
-  if (target.type === NotificationTargetType.Group) {
-    // Directly notify the Group Chat
-    await target.sendAdaptiveCard(...);
-    // List all members in the Group Chat then notify each member
-    const members = await target.members();
-    for (const member of members) {
-      await member.sendAdaptiveCard(...);
-    }
-  }
-  **/
+      /****** To distinguish different target types ******/
+      /** "Channel" means this bot is installed to a Team (default to notify General channel)
+      if (target.type === NotificationTargetType.Channel) {
+        // Directly notify the Team (to the default General channel)
+        await target.sendAdaptiveCard(...);
+        // List all channels in the Team then notify each channel
+        const channels = await target.channels();
+        for (const channel of channels) {
+          await channel.sendAdaptiveCard(...);
+        }
+        // List all members in the Team then notify each member
+        const pageSize = 100;
+        let continuationToken: string | undefined = undefined;
+        do {
+          const pagedData = await target.getPagedMembers(pageSize, continuationToken);
+          const members = pagedData.data;
+          continuationToken = pagedData.continuationToken;
 
-  /** "Person" means this bot is installed as a Personal app
-  if (target.type === NotificationTargetType.Person) {
-    // Directly notify the individual person
-    await target.sendAdaptiveCard(...);
-  }
-  **/
+          for (const member of members) {
+            await member.sendAdaptiveCard(...);
+          }
+        } while (continuationToken);
+      }
+      **/
+
+      /** "Group" means this bot is installed to a Group Chat
+      if (target.type === NotificationTargetType.Group) {
+        // Directly notify the Group Chat
+        await target.sendAdaptiveCard(...);
+        // List all members in the Group Chat then notify each member
+        const pageSize = 100;
+        let continuationToken: string | undefined = undefined;
+        do {
+          const pagedData = await target.getPagedMembers(pageSize, continuationToken);
+          const members = pagedData.data;
+          continuationToken = pagedData.continuationToken;
+
+          for (const member of members) {
+            await member.sendAdaptiveCard(...);
+          }
+        } while (continuationToken);
+      }
+      **/
+
+      /** "Person" means this bot is installed as a Personal app
+      if (target.type === NotificationTargetType.Person) {
+        // Directly notify the individual person
+        await target.sendAdaptiveCard(...);
+      }
+      **/
+    }
+  } while (continuationToken);
 
   /** You can also find someone and notify the individual person
   const member = await notificationApp.notification.findMember(

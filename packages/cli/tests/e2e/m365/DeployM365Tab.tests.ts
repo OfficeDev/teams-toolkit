4e46f80bc8656641585d1a4fc0cafc3fcbe931d7
Kuojian Lu <kuojianlu@gmail.com>
2023-05-23 10:05:31 +0800

    test(e2e): fix m365 deployment cases (#8758)

--- packages/cli/tests/e2e/m365/DeployM365Tab.tests.ts
+++ packages/cli/tests/e2e/m365/DeployM365Tab.tests.ts
@@ -39,7 +39,7 @@ describe("Deploy V3 m365-tab template", () => {
     }
 
     // clean up
-    const context = await readContextMultiEnvV3(projectPath, "local");
+    const context = await readContextMultiEnvV3(projectPath, "dev");
     if (context?.TEAMS_APP_ID) {
       await deleteTeamsApp(context.TEAMS_APP_ID);
     }
@@ -94,7 +94,7 @@ describe("Deploy V3 m365-tab template", () => {
     chai.assert.isNotEmpty(context.M365_APP_ID);
 
     // deploy
-    await CliHelper.deployAll(projectPath, "", "local");
+    await CliHelper.deployAll(projectPath, "", "dev");
     console.log(`[Successfully] deploy for ${projectPath}`);
 
     context = await readContextMultiEnvV3(projectPath, "dev");

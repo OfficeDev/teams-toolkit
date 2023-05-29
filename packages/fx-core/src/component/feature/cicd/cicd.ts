190670e4852f645c397584e66c86b12701181e28
Huajie Zhang <zhjay23@qq.com>
2023-05-29 11:06:11 +0800

    fix: error

--- packages/fx-core/src/component/feature/cicd/cicd.ts
+++ packages/fx-core/src/component/feature/cicd/cicd.ts
@@ -1,24 +1,16 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { hooks } from "@feathersjs/hooks/lib";
 import { ContextV3, err, FxError, InputsWithProjectPath, ok, Result } from "@microsoft/teamsfx-api";
 import "reflect-metadata";
 import { Service } from "typedi";
 import { ComponentNames } from "../../constants";
-import { ActionExecutionMW } from "../../middleware/actionExecutionMW";
 import { CICDImpl } from "./CICDImpl";
 import { questionNames } from "./questions";
 @Service(ComponentNames.CICD)
 export class CICD {
   name = ComponentNames.CICD;
-  @hooks([
-    ActionExecutionMW({
-      question: async (context: ContextV3, inputs: InputsWithProjectPath) => {
-        return await addCicdQuestion(context, inputs);
-      },
-    }),
-  ])
+
   async add(
     context: ContextV3,
     inputs: InputsWithProjectPath

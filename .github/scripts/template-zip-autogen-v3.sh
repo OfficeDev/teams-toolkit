6874d920ece00f3d10965b1623d15fb283b0fa9e
zhijie <zhihuan@microsoft.com>
2023-05-30 11:15:43 +0800

    ci: unnecessary quotes

--- .github/scripts/template-zip-autogen-v3.sh
+++ .github/scripts/template-zip-autogen-v3.sh
@@ -18,12 +18,12 @@ TEMPLATE_NAMES=(common csharp js ts)
 
 for TEMPLATE_NAME in ${TEMPLATE_NAMES[@]}; do
     TEMPLATE_PATH=${TEMPLATE_BASE_DIR}/${TEMPLATE_NAME}
-    if [ ! -d "${TEMPLATE_PATH}" ]; then
+    if [ ! -d ${TEMPLATE_PATH} ]; then
         echo "The folder ${TEMPLATE_PATH} does not exist."
         exit -1
     fi
 
-    cd "${TEMPLATE_PATH}"
+    cd ${TEMPLATE_PATH}
     zip -rq ${TEMPLATE_OUTPUT_DIR}/${TEMPLATE_NAME}.zip .
     cd -
 done

29eca47aece195bd12cc564f9b8d3242f343ecd3
wenyutang-ms <wenyutang@microsoft.com>
2023-05-31 17:00:48 +0800

    build: update

--- .github/scripts/vsc-version.sh
+++ .github/scripts/vsc-version.sh
@@ -8,10 +8,15 @@ echo '-----------------' $VERSION
 # get minor version
 MINOR_VER=$(echo $VERSION | awk -F. '{print $2}')
 DATE_WITH_TIME=`date "+%Y%m%d%H"`
-#otherwise, bump up minor version, and set patch version start from 0.
-if [ "$PREID" == "beta" ] && [ $((MINOR_VER%2)) -eq 0 ]; then
-    echo "Need to bump up version with even minor version for beta"
-    VERSION=$(echo ${VERSION%-*} | awk -v val=$DATE_WITH_TIME -F. '/[0-9]+\./{$2++;$3=val;print}' OFS=.)
+# prerelease version should set minor version to odd number, and set patch version as timestamp.
+if [ "$PREID" == "beta" ]; then
+    if [ $((MINOR_VER%2)) -eq 0 ]; then
+        echo "Need to bump up version with even minor version for beta"
+        VERSION=$(echo ${VERSION%-*} | awk -v val=$DATE_WITH_TIME -F. '/[0-9]+\./{$2++;$3=val;print}' OFS=.)
+    else
+        echo "Need to set patch version as timestamp for beta"
+        VERSION=$(echo ${VERSION%-*} | awk -v val=$DATE_WITH_TIME -F. '/[0-9]+\./{$3=val;print}' OFS=.)
+    fi
     echo '=====================' $VERSION
     # update the package.json file
     jq --arg VERSION "$VERSION" '.version=$VERSION' package.json > tmp.$$.json

0185505c64263fc7ff235af21df3b0cfb74a341d
Yiqing Zhao <cissyzhao99@gmail.com>
2023-06-06 15:12:43 +0800

    test: add unit test

--- packages/sdk/test/unit/node/core/onBehalfOfUserCredential.spec.ts
+++ packages/sdk/test/unit/node/core/onBehalfOfUserCredential.spec.ts
@@ -11,7 +11,7 @@ import {
   UserInfo,
 } from "../../../../src";
 import * as sinon from "sinon";
-import { AuthenticationResult, ConfidentialClientApplication } from "@azure/msal-node";
+import { AuthenticationResult, ConfidentialClientApplication, AuthError } from "@azure/msal-node";
 
 chaiUse(chaiPromises);
 const jwtBuilder = require("jwt-builder");
@@ -270,6 +270,18 @@ fakeCert
   });
 
   it("getToken should throw TokenExpiredError when get SSO token with sso token expired", async function () {
+    sandbox.restore();
+    sandbox
+      .stub(ConfidentialClientApplication.prototype, "acquireTokenOnBehalfOf")
+      .callsFake((): Promise<AuthenticationResult | null> => {
+        return new Promise<AuthenticationResult>(() => {
+          throw new AuthError(
+            "ServerError: invalid_grant: 50013",
+            "AADSTS50013: Assertionfailed signature validation"
+          );
+        });
+      });
+
     const expiredSsoToken =
       "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Im5PbzNaRHJPRFhFSzFqS1doWHNsSFJfS1hFZyJ9.eyJhdWQiOiJjZWVkYTJjNi00MDBmLTQyYjMtYjE4ZC1jY2NmYzk5NjM4NmYiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vNzJmOTg4YmYtODZmMS00MWFmLTkxYWItMmQ3Y2QwMTFkYjQ3L3YyLjAiLCJpYXQiOjE2MTk0OTI3MzEsIm5iZiI6MTYxOTQ5MjczMSwiZXhwIjoxNjE5NDk2NjMxLCJhaW8iOiJBVFFBeS84VEFBQUFFWDZLU0prRjlOaEFDL1NXV1hWTXFPVDNnNGZXR2dqS0ZEWjRramlEb25OVlY2cDlZTVFMaTFqVXdHWEZaclpaIiwiYXpwIjoiYjBjNDdmMjktM2M1Ny00MDQyLTkzM2YtYTdkNTQ2YmFlMzg3IiwiYXpwYWNyIjoiMCIsIm5hbWUiOiJNZXRhIE9TIHNlcnZpY2UgYWNjb3VudCBmb3IgZGV2ZWxvcG1lbnQiLCJvaWQiOiIyYTYxYzRjMy1lY2Y5LTQ5ZWItYjcxNy02NjczZmZmZDg5MmQiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJtZXRhZGV2QG1pY3Jvc29mdC5jb20iLCJyaCI6IjAuQVFFQXY0ajVjdkdHcjBHUnF5MTgwQkhiUnlsX3hMQlhQRUpBa3otbjFVYTY0NGNhQUpRLiIsInNjcCI6ImFjY2Vzc19hc191c2VyIiwic3ViIjoiNEhUVXFCbWVBQVFWa2ZrbU0wcFRtVHh3QjRkcDdITGtxSjRSYXFvb3dUTSIsInRpZCI6IjcyZjk4OGJmLTg2ZjEtNDFhZi05MWFiLTJkN2NkMDExZGI0NyIsInV0aSI6ImFVQkxZSENBWmsyZE9LNW1wR2ctQUEiLCJ2ZXIiOiIyLjAifQ.QCkyqat72TS85vQ6h-jqAj-pnAOOkeOy3-WxgEQ1DJbW6fsoXmVGgso-ncMmeiYIoA1r9jy1cBfnEMBI1tBKcq4TOHseyde2uM-pxCGHNhFC_WiWy9KXKiou5bvgXdVqqCT7CQejpiNdm3wL-EFhXWBRj6OlLMLcUtnlcnKfOSmx8IIOuQrCjWtuE_wjpfo2AwkguuJ5defyOkYqlCfcJ9FyUrqhqsONMdh0lJiVY94PZ00UTjH3zPaC2tnKrGeXn-qrr9dccEUx2HqyAfdzPwymBLWMCrirVRKCZV3DtfKuozKkIxIPZz0891QZcFO8VgfBJaLmr6J7EL8lPtFKnw";
     const credential = new OnBehalfOfUserCredential(expiredSsoToken, authConfig);
@@ -280,6 +292,14 @@ fakeCert
     err = await expect(credential.getToken("")).to.eventually.be.rejectedWith(ErrorWithCode);
     assert.strictEqual(err.code, ErrorCode.TokenExpiredError);
     assert.strictEqual(err.message!, "Sso token has already expired.");
+
+    err = await expect(credential.getToken(scope)).to.eventually.be.rejectedWith(ErrorWithCode);
+    assert.strictEqual(err.code, ErrorCode.TokenExpiredError);
+    assert.isTrue(
+      err.message!.indexOf(
+        "Failed to get access token from AAD server, assertion is invalid because of various reasons: "
+      ) >= 0
+    );
   });
 
   it("getToken should throw ServiceError when fail to get access token due to AAD outage", async function () {
@@ -289,7 +309,7 @@ fakeCert
       .stub(ConfidentialClientApplication.prototype, "acquireTokenOnBehalfOf")
       .callsFake((): Promise<AuthenticationResult | null> => {
         return new Promise<AuthenticationResult>(() => {
-          throw new Error("AAD outage");
+          throw new AuthError("AAD Outage", "AAD Outage");
         });
       });
     const oboCredential = new OnBehalfOfUserCredential(ssoToken, authConfig);

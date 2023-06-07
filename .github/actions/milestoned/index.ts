0a7420ce4369f60ac4a282507e4c9ba3e2998b88
xzf0587 <zhaofengxu@microsoft.com>
2023-06-07 14:57:17 +0800

    feat: github engineering actions

---
+++ .github/actions/milestoned/index.ts
@@ -0,0 +1,71 @@
+import { OctoKitIssue } from '../api/octokit';
+import { Action } from '../common/Action';
+import { DevopsClient } from '../common/azdo';
+import { getAccounts, getRequiredInput, safeLog } from '../common/utils';
+import { context } from '@actions/github';
+import { getInput } from '@actions/core';
+
+const milestonePrefix = getRequiredInput('milestone-prefix');
+const devopsToken = getRequiredInput('devops-token');
+const org = getRequiredInput('devops-org');
+const projectId = getRequiredInput('devops-projectId');
+const titlePreix = getRequiredInput('title-prefix');
+const bugLabel = getRequiredInput('bug-label');
+const bugArea = getRequiredInput('bug-area-path');
+const bugIteration = getRequiredInput('bug-iteration-path');
+const featureLabel = getInput('feature-label');
+const featureArea = getInput('feature-area-path');
+const featureIteration = getInput('feature-iteration-path');
+
+class Milestoned extends Action {
+	id = 'Milestoned';
+
+	async onMilestoned(issue: OctoKitIssue) {
+		const content = await issue.getIssue();
+		if (content.milestone?.startsWith(milestonePrefix)) {
+			safeLog(`got a milestoned ${content.milestone} issue`);
+			let client = await this.createClient();
+			const users = getAccounts;
+			let asignee = undefined;
+			if (content.assignee && users[content.assignee]) {
+				asignee = users[content.assignee];
+				asignee += '@microsoft.com';
+			}
+			const url = this.issueUrl(content.number);
+			const title = titlePreix + `[${content.milestone}]` + content.title;
+			if (featureLabel && content.labels.includes(featureLabel)) {
+				safeLog(`issue labeled with ${featureLabel}. Feature work item will be created.`);
+				await client.createFeatureItem(title, asignee, undefined, url);
+			} else if (content.labels.includes(bugLabel)) {
+				safeLog(`issue labeled with ${bugLabel}. Bug work item will be created.`);
+				await client.createBugItem(title, asignee, undefined, url);
+			} else {
+				safeLog(
+					`issue labeled without feature label(${featureLabel}) and bug label(${bugLabel}). Default bug work item will be created.`,
+				);
+				await client.createBugItem(title, asignee, undefined, url);
+			}
+			safeLog(`finished to create work item.`);
+		}
+	}
+
+	private async createClient() {
+		let client = new DevopsClient(
+			devopsToken,
+			org,
+			projectId,
+			bugArea,
+			bugIteration,
+			featureArea,
+			featureIteration,
+		);
+		await client.init();
+		return client;
+	}
+
+	private issueUrl(id: number) {
+		return `https://github.com/${context.repo.owner}/${context.repo.repo}/issues/${id}`;
+	}
+}
+
+new Milestoned().run() // eslint-disable-line

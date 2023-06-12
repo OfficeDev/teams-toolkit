785b95e14cbfa43f9e75b2d93e4a0dd7f6216d43
xzf0587 <zhaofengxu@microsoft.com>
2023-06-13 00:43:12 +0800

    refactor: update

--- .github/actions/issue-milestoned/index.ts
+++ .github/actions/issue-milestoned/index.ts
@@ -56,7 +56,7 @@ class Milestoned extends Action {
 	async onTriggered(_: OctoKit) {
 		const issueNumber = process.env.ISSUE_NUMBER;
 		safeLog(`start manually create work item for issue ${issueNumber}`);
-		const issue = await new OctoKitIssue(githubToken, context.repo, { number: issueNumber });
+		const issue = new OctoKitIssue(githubToken, context.repo, { number: issueNumber });
 		await this.onMilestoned(issue);
 	}
 

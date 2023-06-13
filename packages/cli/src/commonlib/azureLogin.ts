c5032ddbf30481bdb3993e0da62c3827fd42c650
Zhiyu You <zhiyou@microsoft.com>
2023-06-13 10:31:24 +0800

    refactor(cli): clean up

--- packages/cli/src/commonlib/azureLogin.ts
+++ packages/cli/src/commonlib/azureLogin.ts
@@ -41,7 +41,7 @@ import CLIUIInstance from "../userInteraction";
 import * as path from "path";
 import * as fs from "fs-extra";
 import { isWorkspaceSupported } from "../utils";
-import { AzureScopes, isV3Enabled } from "@microsoft/teamsfx-core/build/common/tools";
+import { AzureScopes } from "@microsoft/teamsfx-core/build/common/tools";
 
 const accountName = "azure";
 const scopes = ["https://management.core.windows.net/user_impersonation"];

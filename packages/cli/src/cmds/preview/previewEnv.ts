37aa1ba8b5f6e3cd2a991deb163446269111dd62
Huajie Zhang <zhjay23@qq.com>
2023-06-14 14:32:58 +0800

    refactor: clean up code

--- packages/cli/src/cmds/preview/previewEnv.ts
+++ packages/cli/src/cmds/preview/previewEnv.ts
@@ -3,23 +3,24 @@
 
 "use strict";
 
+import { Colors, err, FxError, LogLevel, ok, Result } from "@microsoft/teamsfx-api";
+import {
+  AppStudioScopes,
+  assembleError,
+  CoreQuestionNames,
+  environmentManager,
+  envUtil,
+  FxCore,
+  getSideloadingStatus,
+  Hub,
+  loadTeamsFxDevScript,
+  TelemetryContext,
+} from "@microsoft/teamsfx-core";
 import * as fs from "fs-extra";
 import * as path from "path";
 import * as util from "util";
 import { Argv } from "yargs";
-import { Colors, err, FxError, LogLevel, ok, Result } from "@microsoft/teamsfx-api";
-import { TelemetryContext } from "@microsoft/teamsfx-core/localTelemetryReporter";
-import { loadTeamsFxDevScript } from "@microsoft/teamsfx-core/packageJsonHelper";
-import { AppStudioScopes, getSideloadingStatus } from "@microsoft/teamsfx-core";
-import { envUtil } from "@microsoft/teamsfx-core";
-import { environmentManager } from "@microsoft/teamsfx-core";
-import * as commonUtils from "./commonUtils";
-import * as constants from "./constants";
-import * as errors from "./errors";
-import { openHubWebClientNew } from "./launch";
-import { localTelemetryReporter } from "./localTelemetryReporter";
-import { ServiceLogWriter } from "./serviceLogWriter";
-import { Task } from "./task";
+import activate from "../../activate";
 import { signedOut } from "../../commonlib/common/constant";
 import cliLogger from "../../commonlib/log";
 import M365TokenInstance from "../../commonlib/m365Login";
@@ -29,10 +30,13 @@ import { TelemetryEvent, TelemetryProperty } from "../../telemetry/cliTelemetryE
 import CLIUIInstance from "../../userInteraction";
 import { getColorizedString, getSystemInputs, isWorkspaceSupported } from "../../utils";
 import { YargsCommand } from "../../yargsCommand";
-import activate from "../../activate";
-import { CoreQuestionNames } from "@microsoft/teamsfx-core";
-import { Hub } from "@microsoft/teamsfx-core";
-import { FxCore, assembleError } from "@microsoft/teamsfx-core";
+import * as commonUtils from "./commonUtils";
+import * as constants from "./constants";
+import * as errors from "./errors";
+import { openHubWebClientNew } from "./launch";
+import { localTelemetryReporter } from "./localTelemetryReporter";
+import { ServiceLogWriter } from "./serviceLogWriter";
+import { Task } from "./task";
 enum Progress {
   M365Account = "Microsoft 365 Account",
 }

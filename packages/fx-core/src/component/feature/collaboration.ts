a3da326dcb13f859c123cff738826093d8536a7e
KennethBWSong <63089166+KennethBWSong@users.noreply.github.com>
2023-06-29 10:28:37 +0800

    docs: add copyright

--- packages/fx-core/src/component/feature/collaboration.ts
+++ packages/fx-core/src/component/feature/collaboration.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import { Context, FxError, M365TokenProvider, Result, err, ok } from "@microsoft/teamsfx-api";
 import { Service } from "typedi";
 import { hooks } from "@feathersjs/hooks/lib";

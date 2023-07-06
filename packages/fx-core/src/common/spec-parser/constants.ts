c29999659ae3f65923fcdb09fe2db06eb44fbd02
turenlong <rentu@microsoft.com>
2023-07-06 16:34:29 +0800

    test: add test cases to make lint happy

--- packages/fx-core/src/common/spec-parser/constants.ts
+++ packages/fx-core/src/common/spec-parser/constants.ts
@@ -3,7 +3,7 @@
 "use strict";
 
 export class ConstantString {
-  static readonly CancelledMessage = "Operation canceled";
+  static readonly CancelledMessage = "Operation cancelled";
   static readonly GetMethod = "get";
   static readonly PostMethod = "post";
 }
--- packages/fx-core/tests/common/spec-parser/specParser.test.ts
+++ packages/fx-core/tests/common/spec-parser/specParser.test.ts
@@ -5,13 +5,61 @@ import "mocha";
 import { expect } from "chai";
 import sinon from "sinon";
 import { SpecParser } from "../../../src/common/spec-parser/specParser";
-import { ErrorType } from "../../../src/common/spec-parser/interfaces";
+import { ErrorType, ValidationStatus } from "../../../src/common/spec-parser/interfaces";
 import SwaggerParser from "@apidevtools/swagger-parser";
 import { SpecParserError } from "../../../src/common/spec-parser/specParserError";
+import { ConstantString } from "../../../src/common/spec-parser/constants";
 
 describe("SpecParser", () => {
   afterEach(() => {
     sinon.restore();
+  });
+
+  describe("validate", () => {
+    it("should return a valid result when the spec is valid", async () => {
+      const parser = new SpecParser("/path/to/spec.yaml");
+      const validateStub = sinon.stub(SwaggerParser, "validate").resolves({} as any);
+
+      const result = await parser.validate();
+
+      expect(result.status).to.equal(ValidationStatus.Valid);
+      expect(result.warnings).to.be.an("array").that.is.empty;
+      expect(result.errors).to.be.an("array").that.is.empty;
+      sinon.assert.calledOnce(validateStub);
+    });
+
+    it("should return an error result when the spec is not valid", async () => {
+      const parser = new SpecParser("/path/to/spec.yaml");
+      const validateStub = sinon.stub(SwaggerParser, "validate").rejects(new Error("Invalid spec"));
+
+      const result = await parser.validate();
+
+      expect(result.status).to.equal(ValidationStatus.Error);
+      expect(result.warnings).to.be.an("array").that.is.empty;
+      expect(result.errors).to.be.an("array").that.has.lengthOf(1);
+      expect(result.errors[0].type).to.equal(ErrorType.SpecNotValid);
+      expect(result.errors[0].content).to.equal("Error: Invalid spec");
+      sinon.assert.calledOnce(validateStub);
+    });
+  });
+
+  describe("generate", () => {
+    it("should throw an error if the signal is aborted", async () => {
+      const manifestPath = "path/to/manifest";
+      const filter = ["GET /pet/{petId}"];
+      const specPath = "path/to/spec";
+      const adaptiveCardFolder = "path/to/adaptiveCardFolder";
+      const signal = { aborted: true } as AbortSignal;
+      const parser = new SpecParser("/path/to/spec.yaml");
+
+      try {
+        await parser.generate(manifestPath, filter, specPath, adaptiveCardFolder, signal);
+        expect.fail("Expected an error to be thrown");
+      } catch (err) {
+        expect((err as SpecParserError).message).contain(ConstantString.CancelledMessage);
+        expect((err as SpecParserError).errorType).to.equal(ErrorType.Cancelled);
+      }
+    });
   });
 
   describe("list", () => {

ee25f58d6e8a265607679934afe36436464fd175
xzf0587 <zhaofengxu@microsoft.com>
2023-07-06 13:42:51 +0800

    refactor: update

--- packages/eslint-plugin-teamsfx/lib/calcHeader.js
+++ packages/eslint-plugin-teamsfx/lib/calcHeader.js
@@ -2,18 +2,29 @@ var fs = require("fs");
 var content = fs.readFileSync("header-check.json", "utf8");
 var data = JSON.parse(content);
 var files = [];
-data.forEach((result) => {
-  result.messages.forEach((message) => {
-    const ruleId = message.ruleId;
-    if (ruleId === "header/header") {
-      files.push(result.filePath);
+for (const result of data) {
+    for (const message of result.messages) {
+        const ruleId = message.ruleId;
+        if (ruleId === "header/header") {
+            files.push(result.filePath);
+        }
     }
-  });
-});
+}
+// data.forEach((result) => {
+//   result.messages.forEach((message) => {
+//     const ruleId = message.ruleId;
+//     if (ruleId === "header/header") {
+//       files.push(result.filePath);
+//     }
+//   });
+// });
 console.log(`Files with header issues: ${files.length}`);
-files.forEach((file) => {
-  console.log(file);
-});
+for (const file of files) {
+    console.log(file);
+}
+// files.forEach((file) => {
+//   console.log(file);
+// });
 if (files.length > 0) {
   process.exit(1);
 }

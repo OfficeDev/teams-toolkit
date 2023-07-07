fba99bb6ec6f6e31ec79b0c8af55630f46c899e7
xzf0587 <zhaofengxu@microsoft.com>
2023-07-07 10:12:10 +0800

    refactor: update

--- packages/eslint-plugin-teamsfx/config/header.js
+++ packages/eslint-plugin-teamsfx/config/header.js
@@ -1,2 +1,32 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
+"use strict";
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: [
+    "@typescript-eslint/eslint-plugin",
+    "prettier",
+    "no-secrets",
+    "header",
+    "@microsoft/eslint-plugin-teamsfx",
+  ],
+  overrides: [
+    {
+      files: ["src/**/*.ts"],
+      rules: {
+        "header/header": [
+          "error",
+          "../eslint-plugin-teamsfx/config/header"
+        ],
+      },
+    },
+  ],
+};

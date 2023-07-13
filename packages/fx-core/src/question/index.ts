a688252e88a06b39aa431786ff0d106d341b3eb2
Ning Liu <nliu@microsoft.com>
2023-07-13 10:45:43 +0800

    refactor: add copyright

--- packages/fx-core/src/question/index.ts
+++ packages/fx-core/src/question/index.ts
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
 import { FxError, IQTreeNode, Result, ok } from "@microsoft/teamsfx-api";
 import { createProjectCliHelpNode, createProjectQuestionNode } from "./create";
 import {

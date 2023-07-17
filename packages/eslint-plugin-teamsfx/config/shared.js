5da3c6eb3e431551f8bb1731be2371f07716c99b
Zhaofeng Xu <zhaofengxu@microsoft.com>
2023-07-17 16:38:59 +0800

    refactor: update shared.js (#9311)

--- packages/eslint-plugin-teamsfx/config/shared.js
+++ packages/eslint-plugin-teamsfx/config/shared.js
@@ -9,7 +9,6 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: "module",
-    project: ['./tsconfig.eslint.json']
   },
   extends: [
     "plugin:@typescript-eslint/recommended",

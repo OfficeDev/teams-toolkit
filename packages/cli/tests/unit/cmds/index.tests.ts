8e74881d3fd9a7ad986aafd9a391eba1ab13a347
huajiezhang <huajiezhang@microsoft.com>
2023-07-20 21:07:13 +0800

    test: unit

--- packages/cli/tests/unit/cmds/index.tests.ts
+++ packages/cli/tests/unit/cmds/index.tests.ts
@@ -86,12 +86,26 @@ describe("sendCommandUsageTelemetry", function () {
     sendCommandUsageTelemetry(["node", "cli.js", "new", "-h"]);
     assert.isTrue(
       sendTelemetryStub.calledWith(TelemetryEvent.Command, {
-        [TelemetryProperty.CommandOptions]: "h,debug",
+        [TelemetryProperty.CommandOptions]: "h",
         [TelemetryProperty.CommandHead]: "new",
         [TelemetryProperty.CommandBody]: "new",
         [TelemetryProperty.CommandHelp]: "true",
         [TelemetryProperty.Interactive]: "",
         [TelemetryProperty.CommandLogLevel]: "info",
+      })
+    );
+  });
+  it("happy path --debug", () => {
+    const sendTelemetryStub = sandbox.stub(cliTelemetry, "sendTelemetryEvent");
+    sendCommandUsageTelemetry(["node", "cli.js", "new", "-h", "--debug"]);
+    assert.isTrue(
+      sendTelemetryStub.calledWith(TelemetryEvent.Command, {
+        [TelemetryProperty.CommandOptions]: "h,debug",
+        [TelemetryProperty.CommandHead]: "new",
+        [TelemetryProperty.CommandBody]: "new",
+        [TelemetryProperty.CommandHelp]: "true",
+        [TelemetryProperty.Interactive]: "",
+        [TelemetryProperty.CommandLogLevel]: "debug",
       })
     );
   });

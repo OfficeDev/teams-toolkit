a354d4764a6607533e8f453a5c6b95aa7cee243f
ayachensiyuan <ayachensiyuan@163.com>
2023-07-25 10:51:51 +0800

    test: sample template

--- packages/tests/src/e2e/samples/ProvisionAdaptiveCard.tests.ts
+++ packages/tests/src/e2e/samples/ProvisionAdaptiveCard.tests.ts
@@ -5,17 +5,12 @@
  * @author Ivan Chen <v-ivanchen@microsoft.com>
  */
 
-import { it } from "@microsoft/extra-shot-mocha";
 import { TemplateProjectFolder } from "../../utils/constants";
 import sampleCaseFactory from "./sampleCaseFactory";
 
-describe("teamsfx new template", function () {
-  it(
-    `${TemplateProjectFolder.AdaptiveCard}`,
-    { testPlanCaseId: 15277474, author: "v-ivanchen@microsoft.com" },
-    async function () {
-      const sampleCase = sampleCaseFactory(TemplateProjectFolder.AdaptiveCard)
-      sampleCase.test();
-    }
-  );
-});
+const sampleCase = sampleCaseFactory(
+  TemplateProjectFolder.AdaptiveCard,
+  15277474,
+  "v-ivanchen@microsoft.com"
+);
+sampleCase.test();

ff2fc7133d36fd9acc76999781d05b5f9d4466d5
huajiezhang <huajiezhang@microsoft.com>
2023-07-29 18:42:28 +0800

    test: ut

--- packages/cli/tests/unit/commands.tests.ts
+++ packages/cli/tests/unit/commands.tests.ts
@@ -6,9 +6,17 @@ import * as activate from "../../src/activate";
 import { FxCore, UserCancelError } from "@microsoft/teamsfx-core";
 import { err, ok } from "@microsoft/teamsfx-api";
 import { CLIContext } from "../../src/commands/types";
+import { createSampleCommand } from "../../src/commands/models/createSample";
+import * as utils from "../../src/utils";
+import { listSampleCommand } from "../../src/commands/models/listSamples";
+import { logger } from "../../src/commonlib/logger";
 
 describe("CLI commands", () => {
   const sandbox = sinon.createSandbox();
+
+  beforeEach(() => {
+    sandbox.stub(logger, "info").resolves(true);
+  });
 
   afterEach(() => {
     sandbox.restore();
@@ -40,6 +48,49 @@ describe("CLI commands", () => {
       };
       const res = await createCommand.handler(ctx);
       assert.isTrue(res.isErr());
+    });
+  });
+
+  describe("createSampleCommand", async () => {
+    it("happy path", async () => {
+      sandbox.stub(activate, "createFxCore").returns(new FxCore({} as any));
+      sandbox.stub(FxCore.prototype, "createProject").resolves(ok("..."));
+      const ctx: CLIContext = {
+        command: createSampleCommand,
+        optionValues: {},
+        globalOptionValues: {},
+        argumentValues: [],
+        telemetryProperties: {},
+      };
+      const res = await createSampleCommand.handler(ctx);
+      assert.isTrue(res.isOk());
+    });
+    it("core return error", async () => {
+      sandbox.stub(activate, "createFxCore").returns(new FxCore({} as any));
+      sandbox.stub(FxCore.prototype, "createProject").resolves(err(new UserCancelError()));
+      const ctx: CLIContext = {
+        command: createSampleCommand,
+        optionValues: {},
+        globalOptionValues: {},
+        argumentValues: [],
+        telemetryProperties: {},
+      };
+      const res = await createSampleCommand.handler(ctx);
+      assert.isTrue(res.isErr());
+    });
+  });
+  describe("listSampleCommand", async () => {
+    it("happy path", async () => {
+      sandbox.stub(utils, "getTemplates").resolves([]);
+      const ctx: CLIContext = {
+        command: listSampleCommand,
+        optionValues: {},
+        globalOptionValues: {},
+        argumentValues: [],
+        telemetryProperties: {},
+      };
+      const res = await listSampleCommand.handler(ctx);
+      assert.isTrue(res.isOk());
     });
   });
 });

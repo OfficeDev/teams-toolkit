78f1ceaad558792d82559ea16cd0c6d252cd2732
Ning Liu <nliu@microsoft.com>
2023-08-01 10:55:56 +0800

    test: update ut

--- packages/fx-core/tests/core/FxCore.test.ts
+++ packages/fx-core/tests/core/FxCore.test.ts
@@ -72,6 +72,7 @@ import { MockTools, deleteFolder, randomAppName } from "./utils";
 import { FeatureFlagName } from "../../src/common/constants";
 import { OpenAIPluginManifestHelper } from "../../src/component/generator/copilotPlugin/helper";
 import * as CopilotPluginHelper from "../../src/component/generator/copilotPlugin/helper";
+import { SpecParser } from "../../src/common/spec-parser/specParser";
 
 const tools = new MockTools();
 
@@ -1369,6 +1370,8 @@ describe("copilotPlugin", async () => {
       projectPath: path.join(os.tmpdir(), appName),
     };
     const core = new FxCore(tools);
+    sinon.stub(SpecParser.prototype, "generate").resolves();
+
     const result = await core.copilotPluginAddAPI(inputs);
     assert.isTrue(result.isOk());
   });

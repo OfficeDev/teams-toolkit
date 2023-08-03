e13843da0b044778deee88750916e589a3baadb5
Ning Tang <nintan@microsoft.com>
2023-08-03 13:47:56 +0800

    feat: replace message extension with message extension action template (#9482)
    * feat: replace message extension with message extension action template
    
    * fix: template name
    
    * fix: duplicated option id

--- packages/fx-core/src/question/create.ts
+++ packages/fx-core/src/question/create.ts
@@ -344,7 +344,7 @@ export class CapabilityOptions {
 
   static collectFormMe(): OptionItem {
     return {
-      id: "MessagingExtension",
+      id: "CollectFormMessagingExtension",
       label: `${getLocalizedString("core.MessageExtensionOption.labelNew")}`,
       cliName: "message-extension",
       detail: getLocalizedString("core.MessageExtensionOption.detail"),

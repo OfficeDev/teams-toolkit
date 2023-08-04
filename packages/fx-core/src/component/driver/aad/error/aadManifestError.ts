6024d5245aa956fdb6174cb483fa2bae38e07bd5
wenyt <75360946+wenytang-ms@users.noreply.github.com>
2023-08-04 14:24:23 +0800

    refactor: add CannotDeleteOrUpdateEnabledEntitlement as httpclienterror (#9420)
    * refactor: add CannotDeleteOrUpdateEnabledEntitlement as httpclienterror
    
    * docs: update prompt string
    
    * test: update test case
    
    * test: update
    
    * refactor: update code according to comments
    
    * chore: update
    
    * refactor: update
    
    * ci: revert
    
    * perf: udpate
    
    * test: update
    
    * perf: update
    
    * test: udpate
    
    * docs: update
    
    * test: update

--- packages/fx-core/src/component/driver/aad/error/aadManifestError.ts
+++ packages/fx-core/src/component/driver/aad/error/aadManifestError.ts
@@ -68,3 +68,15 @@ export class UnknownResourceAccessTypeUserError extends UserError {
     });
   }
 }
+
+export class DeleteOrUpdatePermissionFailedError extends UserError {
+  constructor(actionName: string) {
+    super({
+      source: actionName,
+      name: "DeleteOrUpdatePermissionFailed",
+      message: getDefaultString("error.aad.manifest.DeleteOrUpdatePermissionFailed"),
+      displayMessage: getLocalizedString("error.aad.manifest.DeleteOrUpdatePermissionFailed"),
+      helpLink: "https://aka.ms/teamsfx-aad-manifest",
+    });
+  }
+}

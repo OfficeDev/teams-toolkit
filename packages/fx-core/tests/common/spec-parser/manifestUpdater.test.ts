a8038208e17aad9a3d7bf910db9001b0bc7d1213
Yuqi Zhou <yuqzho@microsoft.com>
2023-08-08 20:17:57 +0800

    test: ut

--- packages/fx-core/tests/common/spec-parser/manifestUpdater.test.ts
+++ packages/fx-core/tests/common/spec-parser/manifestUpdater.test.ts
@@ -75,6 +75,50 @@ describe("manifestUpdater", () => {
     expect(result).to.deep.equal(expectedManifest);
     readJSONStub.restore();
   });
+
+  it("should skip updating full/description if missing info/description", async () => {
+    const manifestPath = "/path/to/your/manifest.json";
+    const outputSpecPath = "/path/to/your/spec/outputSpec.yaml";
+    const adaptiveCardFolder = "/path/to/your/adaptiveCards";
+
+    const originalManifest = {
+      name: { short: "Original Name", full: "Original Full Name" },
+      description: { short: "Original Short Description", full: "Original Full Description" },
+      composeExtensions: [],
+    };
+    const expectedManifest = {
+      name: { short: "Original Name", full: "Original Full Name" },
+      description: { short: spec.info.title, full: "Original Full Description" },
+      composeExtensions: [
+        {
+          type: "apiBased",
+          supportsConversationalAI: true,
+          apiSpecFile: "spec/outputSpec.yaml",
+          commands: [
+            {
+              context: ["compose"],
+              type: "query",
+              title: "Get all pets",
+              id: "getPets",
+              parameters: [
+                { name: "limit", title: "Limit", description: "Maximum number of pets to return" },
+              ],
+              apiResponseRenderingTemplate: "adaptiveCards/getPets.json",
+            },
+          ],
+        },
+      ],
+    };
+    const readJSONStub = sinon.stub(fs, "readJSON").resolves(originalManifest);
+
+    const result = await updateManifest(manifestPath, outputSpecPath, adaptiveCardFolder, {
+      ...spec,
+      info: { title: "My API" },
+    });
+
+    expect(result).to.deep.equal(expectedManifest);
+    readJSONStub.restore();
+  });
 });
 
 describe("generateCommands", () => {

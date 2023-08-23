494f2a5ef05601a60306a9c7530b2c35da52b6df
wenyutang-ms <wenyutang@microsoft.com>
2023-08-23 16:40:20 +0800

    Revert "test: remove useless code"
    This reverts commit dd15b56bb595efc27d282a2066ff87ef701c20af.

---
+++ packages/fx-core/tests/core/middleware/VideoFilterAppBlockerMW.test.ts
@@ -0,0 +1,166 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { hooks, NextFunction } from "@feathersjs/hooks/lib";
+import { Func, FxError, Inputs, ok, Platform, Result } from "@microsoft/teamsfx-api";
+import { assert } from "chai";
+import "mocha";
+import mockFs from "mock-fs";
+import * as path from "path";
+import { VideoFilterAppRemoteNotSupportedError } from "../../../src/core/error";
+import { setTools } from "../../../src/core/globalVars";
+import { VideoFilterAppBlockerMW } from "../../../src/core/middleware/videoFilterAppBlocker";
+import { CoreHookContext } from "../../../src/core/types";
+import { MockTools } from "../utils";
+
+describe("Middleware - VideoFilterAppBlockerMW", () => {
+  function createMock(): {
+    nextMiddlewareCalled: boolean;
+    inputs: Inputs;
+    instance: { myMethod: (inputs: Inputs) => Promise<Result<boolean, FxError>> };
+  } {
+    setTools(new MockTools());
+    class MyClass {
+      async myMethod(inputs: Inputs): Promise<Result<boolean, FxError>> {
+        return ok(true);
+      }
+    }
+    const instance = new MyClass();
+
+    const result = {
+      nextMiddlewareCalled: false,
+      inputs: { platform: Platform.VSCode, projectPath: mockProjectRoot },
+      instance,
+    };
+    const TestMW = async (ctx: CoreHookContext, next: NextFunction) => {
+      result.nextMiddlewareCalled = true;
+      await next();
+    };
+    hooks(MyClass, {
+      myMethod: [VideoFilterAppBlockerMW, TestMW],
+    });
+    return result;
+  }
+
+  const mockProjectRoot = "video-filter";
+
+  afterEach(function () {
+    mockFs.restore();
+  });
+
+  it("blocks video filter project", async () => {
+    const mock = createMock();
+    mockFs({
+      [path.join(mockProjectRoot, "appPackage", "manifest.json")]: JSON.stringify({
+        meetingExtensionDefinition: { videoFiltersConfigurationUrl: "https://a.b.c" },
+      }),
+    });
+
+    const result = await mock.instance.myMethod(mock.inputs);
+
+    assert.isTrue(result.isErr());
+    assert.equal(result._unsafeUnwrapErr().name, VideoFilterAppRemoteNotSupportedError.name);
+    assert.isFalse(mock.nextMiddlewareCalled);
+  });
+
+  it("ignores non-video project", async () => {
+    const mock = createMock();
+    mockFs({
+      [path.join(mockProjectRoot, "appPackage", "manifest.json")]: JSON.stringify({
+        meetingExtensionDefinition: {},
+      }),
+    });
+
+    const result = await mock.instance.myMethod(mock.inputs);
+
+    assert.isTrue(result.isOk());
+    assert.isTrue(result._unsafeUnwrap());
+    assert.isTrue(mock.nextMiddlewareCalled);
+  });
+
+  it("ignores project with incorrect manifest", async () => {
+    const mock = createMock();
+    mockFs({
+      [path.join(mockProjectRoot, "appPackage", "manifest.json")]: "invalid json",
+    });
+
+    const result = await mock.instance.myMethod(mock.inputs);
+
+    assert.isTrue(result.isOk());
+    assert.isTrue(result._unsafeUnwrap());
+    assert.isTrue(mock.nextMiddlewareCalled);
+  });
+
+  describe("user task", () => {
+    it("blocks build package", async () => {
+      setTools(new MockTools());
+      class MyClass {
+        async executeUserTask(
+          func: Func,
+          inputs: Inputs,
+          ctx?: CoreHookContext
+        ): Promise<Result<any, FxError>> {
+          return ok(true);
+        }
+      }
+      let nextMiddlewareCalled = false;
+      const instance = new MyClass();
+      const inputs = { platform: Platform.VSCode, projectPath: mockProjectRoot };
+      const TestMW = async (ctx: CoreHookContext, next: NextFunction) => {
+        nextMiddlewareCalled = true;
+        await next();
+      };
+      hooks(MyClass, {
+        executeUserTask: [VideoFilterAppBlockerMW, TestMW],
+      });
+      mockFs({
+        [path.join(mockProjectRoot, "appPackage", "manifest.json")]: "invalid json",
+      });
+
+      const func = {
+        namespace: "fx-solution-azure",
+        method: "validateManifest",
+      };
+      const result = await instance.executeUserTask(func, inputs);
+
+      assert.isTrue(result.isOk());
+      assert.isTrue(result._unsafeUnwrap());
+      assert.isTrue(nextMiddlewareCalled);
+    });
+    it("doesn't block edit manifest", async () => {
+      setTools(new MockTools());
+      class MyClass {
+        async executeUserTask(
+          func: Func,
+          inputs: Inputs,
+          ctx?: CoreHookContext
+        ): Promise<Result<any, FxError>> {
+          return ok(true);
+        }
+      }
+      let nextMiddlewareCalled = false;
+      const instance = new MyClass();
+      const inputs = { platform: Platform.VSCode, projectPath: mockProjectRoot };
+      const TestMW = async (ctx: CoreHookContext, next: NextFunction) => {
+        nextMiddlewareCalled = true;
+        await next();
+      };
+      hooks(MyClass, {
+        executeUserTask: [VideoFilterAppBlockerMW, TestMW],
+      });
+      mockFs({
+        [path.join(mockProjectRoot, "appPackage", "manifest.json")]: "invalid json",
+      });
+
+      const func = {
+        namespace: "fx-solution-azure/fx-resource-appstudio",
+        method: "getManifestTemplatePath",
+      };
+      const result = await instance.executeUserTask(func, inputs);
+
+      assert.isTrue(result.isOk());
+      assert.isTrue(result._unsafeUnwrap());
+      assert.isTrue(nextMiddlewareCalled);
+    });
+  });
+});

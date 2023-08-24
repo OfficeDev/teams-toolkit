707497cf0162eb2d28db08c541b4028b1a6596ea
Ning Liu <nliu@microsoft.com>
2023-08-24 14:39:43 +0800

    test: update ut

--- packages/fx-core/tests/component/driver/teamsApp/appstudioclient.test.ts
+++ packages/fx-core/tests/component/driver/teamsApp/appstudioclient.test.ts
@@ -17,6 +17,7 @@ import { PublishingState } from "../../../../src/component/driver/teamsApp/inter
 import { manifestUtils } from "../../../../src/component/driver/teamsApp/utils/ManifestUtils";
 import { AppStudioResultFactory } from "../../../../src/component/driver/teamsApp/results";
 import { Constants } from "../../../../src/component/driver/teamsApp/constants";
+import { MockedLogProvider } from "../../../plugins/solution/util";
 
 function newEnvInfo() {
   return {
@@ -28,6 +29,7 @@ function newEnvInfo() {
 
 describe("App Studio API Test", () => {
   const appStudioToken = "appStudioToken";
+  const logProvider = new MockedLogProvider();
 
   const appDef: AppDefinition = {
     appName: "fake",
@@ -152,7 +154,7 @@ describe("App Studio API Test", () => {
 
       AppStudioClient.setRegion("https://dev.teams.microsoft.com/amer");
 
-      const res = await AppStudioClient.importApp(Buffer.from(""), appStudioToken);
+      const res = await AppStudioClient.importApp(Buffer.from(""), appStudioToken, logProvider);
       chai.assert.equal(res, appDef);
     });
 
@@ -166,7 +168,7 @@ describe("App Studio API Test", () => {
       sinon.stub(fakeAxiosInstance, "post").resolves(response);
       AppStudioClient.setRegion("https://dev.teams.microsoft.com");
 
-      const res = await AppStudioClient.importApp(Buffer.from(""), appStudioToken);
+      const res = await AppStudioClient.importApp(Buffer.from(""), appStudioToken, logProvider);
       chai.assert.equal(res, appDef);
     });
 
@@ -189,7 +191,7 @@ describe("App Studio API Test", () => {
       sinon.stub(TelemetryUtils, "sendErrorEvent").callsFake(() => {});
 
       try {
-        await AppStudioClient.importApp(Buffer.from(""), appStudioToken);
+        await AppStudioClient.importApp(Buffer.from(""), appStudioToken, logProvider);
       } catch (error) {
         chai.assert.equal(error.name, AppStudioError.TeamsAppCreateConflictError.name);
       }
@@ -214,7 +216,7 @@ describe("App Studio API Test", () => {
       sinon.stub(TelemetryUtils, "sendErrorEvent").callsFake(() => {});
 
       try {
-        await AppStudioClient.importApp(Buffer.from(""), appStudioToken);
+        await AppStudioClient.importApp(Buffer.from(""), appStudioToken, logProvider);
       } catch (error) {
         chai.assert.equal(
           error.name,
@@ -245,7 +247,7 @@ describe("App Studio API Test", () => {
       sinon.stub(TelemetryUtils, "sendErrorEvent").callsFake(() => {});
 
       try {
-        await AppStudioClient.importApp(Buffer.from(""), appStudioToken);
+        await AppStudioClient.importApp(Buffer.from(""), appStudioToken, logProvider);
       } catch (error) {
         chai.assert.equal(error.name, AppStudioError.DeveloperPortalAPIFailedError.name);
       }
@@ -274,7 +276,7 @@ describe("App Studio API Test", () => {
       sinon.stub(TelemetryUtils, "sendErrorEvent").callsFake(() => {});
 
       try {
-        await AppStudioClient.importApp(Buffer.from(""), appStudioToken);
+        await AppStudioClient.importApp(Buffer.from(""), appStudioToken, logProvider);
       } catch (error) {
         chai.assert.equal(error.name, AppStudioError.InvalidTeamsAppIdError.name);
       }
@@ -305,7 +307,7 @@ describe("App Studio API Test", () => {
       sinon.stub(TelemetryUtils, "sendErrorEvent").callsFake(() => {});
 
       try {
-        await AppStudioClient.importApp(Buffer.from(""), appStudioToken);
+        await AppStudioClient.importApp(Buffer.from(""), appStudioToken, logProvider);
       } catch (error) {
         chai.assert.equal(error.name, AppStudioError.FileNotFoundError.name);
       }
@@ -334,7 +336,7 @@ describe("App Studio API Test", () => {
       sinon.stub(TelemetryUtils, "sendErrorEvent").callsFake(() => {});
 
       try {
-        await AppStudioClient.importApp(Buffer.from(""), appStudioToken);
+        await AppStudioClient.importApp(Buffer.from(""), appStudioToken, logProvider);
       } catch (error) {
         chai.assert.equal(error.name, AppStudioError.DeveloperPortalAPIFailedError.name);
       }

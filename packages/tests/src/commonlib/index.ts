a82aeff697748199071275272b7585160f693cba
wenyutang-ms <wenyutang@microsoft.com>
2023-08-28 16:11:14 +0800

    ci: update

---
+++ packages/tests/src/commonlib/index.ts
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+"use strict";
+
+export * as cfg from "@microsoft/teamsfx-cli/src/commonlib/common/userPasswordConfig"; // eslint-disable-line
+export * from "./aadValidate";
+export * from "./simpleAuthValidator";
+export * from "./aadManager";
+export * from "./resourceGroupManager";
+export * from "./functionValidator";
+export * from "./botValidator";
+export * from "./frontendValidator";
+export * from "./appStudioValidator";
+export * from "./sharepointValidator";

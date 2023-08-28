1570f44e324c795bb6c8112d7b74e08c066a449e
wenyutang-ms <wenyutang@microsoft.com>
2023-08-28 16:35:17 +0800

    test: update

--- packages/tests/.eslintrc.js
+++ packages/tests/.eslintrc.js
@@ -3,17 +3,17 @@ module.exports = {
     tsconfigRootDir: __dirname,
   },
   // plugins: ["eslint-plugin-local-rules"],
-  // extends: ["../eslint-plugin-teamsfx/config/shared.js"],
-  // overrides: [
-  //   {
-  //     files: ["src/**/*.ts"],
-  //     extends: ["../eslint-plugin-teamsfx/config/header.js"],
-  //   },
-  //   {
-  //     files: ["src/e2e/**/*.ts", "src/ui-test/**/*.ts"],
-  //     rules: {
-  //       "local-rules/jsdoc-author": 2,
-  //     },
-  //   },
-  // ],
+  extends: ["../eslint-plugin-teamsfx/config/shared.js"],
+  overrides: [
+    {
+      files: ["src/**/*.ts"],
+      extends: ["../eslint-plugin-teamsfx/config/header.js"],
+    },
+    //   {
+    //     files: ["src/e2e/**/*.ts", "src/ui-test/**/*.ts"],
+    //     rules: {
+    //       "local-rules/jsdoc-author": 2,
+    //     },
+    //   },
+  ],
 };

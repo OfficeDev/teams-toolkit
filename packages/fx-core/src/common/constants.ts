3e308ae859dacbe1a2da9979a0165c1c0261243d
Ning Tang <nintan@microsoft.com>
2023-08-28 15:57:56 +0800

    feat(core): enable feature flag when downloading samples (#9743)
    * feat(core): enable feature flag in sample downloading
    
    * fix: add beta version

--- packages/fx-core/src/common/constants.ts
+++ packages/fx-core/src/common/constants.ts
@@ -57,4 +57,5 @@ export class FeatureFlagName {
   static readonly OfficeAddin = "TEAMSFX_OFFICE_ADDIN";
   static readonly ImportSPFx = "IMPORT_SPFX";
   static readonly CopilotPlugin = "COPILOT_PLUGIN";
+  static readonly SampleConfigBranch = "TEAMSFX_SAMPLE_CONFIG_BRANCH";
 }

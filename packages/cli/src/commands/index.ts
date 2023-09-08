481a0dec608f8c745519bfa105324a376e4f20d9
huajiezhang <huajiezhang@microsoft.com>
2023-09-08 09:39:54 +0800

    test: ut

--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -3,7 +3,7 @@
 import { engine } from "./engine";
 import { rootCommand } from "./models/root";
 
-export async function start(binName: "teamsfx" | "teamsapp"): Promise<void> {
+export async function start(binName: "teamsfx" | "teamsapp" = "teamsapp"): Promise<void> {
   rootCommand.name = binName;
   rootCommand.fullName = binName;
   await engine.start(rootCommand);

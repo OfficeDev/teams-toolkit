bced9aa174c98c7195c24310a87fe7a154469953
Yuqi Zhou <86260893+yuqizhou77@users.noreply.github.com>
2023-09-19 10:40:18 +0800

    refactor: ff (#9971)

--- packages/fx-core/src/common/featureFlags.ts
+++ packages/fx-core/src/common/featureFlags.ts
@@ -45,8 +45,8 @@ export function isCopilotPluginEnabled(): boolean {
 }
 
 export function isApiCopilotPluginEnabled(): boolean {
-  return isFeatureFlagEnabled(FeatureFlagName.ApiCopilotPlugin, true) && isCopilotPluginEnabled();
-  // return isFeatureFlagEnabled(FeatureFlagName.ApiCopilotPlugin, false) && isCopilotPluginEnabled(); // TODO: update default value to false for September release
+  // return isFeatureFlagEnabled(FeatureFlagName.ApiCopilotPlugin, true) && isCopilotPluginEnabled();
+  return isFeatureFlagEnabled(FeatureFlagName.ApiCopilotPlugin, false) && isCopilotPluginEnabled();
 }
 
 export function isCliNewUxEnabled(): boolean {

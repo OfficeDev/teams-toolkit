c50827c2d8216ce45db31f6a007b509855ec6a9a
ayachensiyuan <ayachensiyuan@163.com>
2023-09-20 10:17:51 +0800

    test: add aibot test case

--- packages/tests/src/e2e/bot/ProvisionAiBot.tests.ts
+++ packages/tests/src/e2e/bot/ProvisionAiBot.tests.ts
@@ -7,8 +7,22 @@
 
 import { Capability } from "../../utils/constants";
 import { CaseFactory } from "../caseFactory";
+import * as fs from "fs-extra";
+import * as path from "path";
+import { expect } from "chai";
 
-class AiBotTestCase extends CaseFactory {}
+class AiBotTestCase extends CaseFactory {
+    public override async onAfterCreate(projectPath: string): Promise<void> {
+        expect(fs.pathExistsSync(path.resolve(projectPath, "infra"))).to.be.true;
+        const userFile = path.resolve(projectPath, "env", `.env.dev.user`);
+        const SECRET_OPENAI_API_KEY = "SECRET_OPENAI_API_KEY=MY_OPENAI_API_KEY";
+        const SECRET_AZURE_OPENAI_API_KEY = "SECRET_AZURE_OPENAI_API_KEY=MY_SECRET_AZURE_OPENAI_API_KEY";
+        const SECRET_AZURE_OPENAI_ENDPOINT = "SECRET_AZURE_OPENAI_ENDPOINT=MY_SECRET_AZURE_OPENAI_ENDPOINT";
+        const KEY = SECRET_AZURE_OPENAI_API_KEY + "\n" + SECRET_AZURE_OPENAI_ENDPOINT + "\n" + SECRET_OPENAI_API_KEY;
+        fs.writeFileSync(userFile, KEY);
+        console.log(`add key ${KEY} to .env.dev.user file`);
+      }
+}
 
 new AiBotTestCase(
     Capability.AiBot,

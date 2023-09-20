d871f83798f8f3795d2e6c254fef1b045875ee49
huajiezhang <huajiezhang@microsoft.com>
2023-09-20 12:18:09 +0800

    fix: teamsfx update aad-app

--- packages/cli/src/commands/models/updateAadApp.ts
+++ packages/cli/src/commands/models/updateAadApp.ts
@@ -5,6 +5,7 @@ import { DeployAadManifestInputs, DeployAadManifestOptions } from "@microsoft/te
 import { getFxCore } from "../../activate";
 import { TelemetryEvent } from "../../telemetry/cliTelemetryEvents";
 import { ProjectFolderOption } from "../common";
+import * as path from "path";
 
 export const updateAadAppCommand: CLICommand = {
   name: "aad-app",
@@ -17,6 +18,11 @@ export const updateAadAppCommand: CLICommand = {
   handler: async (ctx) => {
     const inputs = ctx.optionValues as DeployAadManifestInputs & InputsWithProjectPath;
     inputs.ignoreEnvInfo = false;
+    if (inputs["manifest-file-path"]) {
+      if (!path.isAbsolute(inputs["manifest-file-path"])) {
+        inputs["manifest-file-path"] = path.join(inputs.projectPath!, inputs["manifest-file-path"]);
+      }
+    }
     const core = getFxCore();
     const res = await core.deployAadManifest(inputs);
     return res;

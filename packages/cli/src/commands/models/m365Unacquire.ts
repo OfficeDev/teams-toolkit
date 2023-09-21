d3bbb5efd00de237b2b2e1d60d9c720b08ee4f3f
huajiezhang <huajiezhang@microsoft.com>
2023-09-21 21:19:13 +0800

    fix: rename m365 command

--- packages/cli/src/commands/models/m365Unacquire.ts
+++ packages/cli/src/commands/models/m365Unacquire.ts
@@ -9,7 +9,7 @@ import { TelemetryEvent } from "../../telemetry/cliTelemetryEvents";
 import { sideloadingServiceEndpoint } from "./m365Sideloading";
 
 export const m365UnacquireCommand: CLICommand = {
-  name: "unacquire",
+  name: "uninstall",
   description: "Remove an acquired M365 App.",
   options: [
     {

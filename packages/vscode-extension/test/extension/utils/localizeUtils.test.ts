6591c18567c8ef87ebb78921d509a50cdb4d20c8
Huihui Wu <huihuiwu@microsoft.com>
2023-09-21 13:38:41 +0800

    test: add ut to increase coverage

--- packages/vscode-extension/test/extension/utils/localizeUtils.test.ts
+++ packages/vscode-extension/test/extension/utils/localizeUtils.test.ts
@@ -3,15 +3,18 @@ import * as fs from "fs-extra";
 import sinon from "ts-sinon";
 import VsCodeLogInstance from "../../../src/commonlib/log";
 import * as globalVariables from "../../../src/globalVariables";
-import { _resetCollections, loadLocalizedStrings } from "../../../src/utils/localizeUtils";
+import {
+  _resetCollections,
+  loadLocalizedStrings,
+  parseLocale,
+} from "../../../src/utils/localizeUtils";
 
 describe("localizeUtils", () => {
+  afterEach(() => {
+    _resetCollections();
+    sinon.restore();
+  });
   describe("loadLocalizedStrings", () => {
-    afterEach(() => {
-      _resetCollections();
-      sinon.restore();
-    });
-
     it("should log error if no default string collection", () => {
       sinon.stub(fs, "pathExistsSync").callsFake((directory: string) => {
         if (directory.includes("package.nls.json")) {
@@ -27,6 +30,16 @@ describe("localizeUtils", () => {
       loadLocalizedStrings();
 
       chai.expect(vscodeLogStub.calledOnce).to.be.true;
+    });
+  });
+
+  describe("parseLocale", () => {
+    it("should return current locale", () => {
+      sinon.stub(process, "env").value({ VSCODE_NLS_CONFIG: '{ "locale": "zh-cn" }' });
+
+      const locale = parseLocale();
+
+      chai.expect(locale).to.equal("zh-cn");
     });
   });
 });

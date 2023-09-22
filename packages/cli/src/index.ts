bae10b87aced41f3a3f25624c4d965013371f87e
huajiezhang <huajiezhang@microsoft.com>
2023-09-22 13:14:00 +0800

    fix: update warning message for old cli name

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -41,11 +41,7 @@ export async function start(binName: "teamsfx" | "teamsapp"): Promise<void> {
   initTelemetryReporter();
   if (binName === "teamsfx") {
     logger.warning(
-      `
-  **********************************************************************************
-  * Warning: command 'teamsfx' is deprecated and will be replaced with 'teamsapp'. *
-  **********************************************************************************/
-  `
+      "Warning: We are planning to depreate 'teamsfx' as command signagure and move to 'teamsapp' instead in the next major version of Teams Toolkit CLI."
     );
   }
   cliTelemetry.reporter?.addSharedProperty(TelemetryProperty.BinName, binName); // trigger binary name for telemetry

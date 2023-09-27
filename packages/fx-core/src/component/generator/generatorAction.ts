ca3d7751380cfdaa6a2e9edfa748c544365982ce
Yimin-Jin <yiminjin@microsoft.com>
2023-09-27 16:42:04 +0800

    fefactor: add judgment conditions

--- packages/fx-core/src/component/generator/generatorAction.ts
+++ packages/fx-core/src/component/generator/generatorAction.ts
@@ -122,7 +122,7 @@ export const fetchZipFromUrlAction: GeneratorAction = {
 export const fetchTemplateFromLocalAction: GeneratorAction = {
   name: GeneratorActionName.FetchTemplateZipFromLocal,
   run: async (context: GeneratorContext) => {
-    if (context.outputs?.length !== 0) {
+    if (context.zip && context.outputs && context.outputs.length > 0) {
       return;
     }
     context.logProvider.debug(`Fetching zip from local: ${JSON.stringify(context)}`);

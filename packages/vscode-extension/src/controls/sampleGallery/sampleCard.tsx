d20951e29778f0da94b5e2258a675ac63e1c6008
Ning Tang <nintan@microsoft.com>
2023-10-08 10:08:15 +0800

    feat: enable resize for sample card thumbnail

--- packages/vscode-extension/src/controls/sampleGallery/sampleCard.tsx
+++ packages/vscode-extension/src/controls/sampleGallery/sampleCard.tsx
@@ -33,7 +33,7 @@ export default class SampleCard extends React.Component<SampleProps, unknown> {
             <FontIcon iconName="FavoriteStar" className="star"></FontIcon>
           </div>
         )}
-        <Image src={sample.thumbnailUrl} />
+        <Image className="thumbnail" src={sample.thumbnailUrl} />
       </>
     );
     const legacySampleImage = (

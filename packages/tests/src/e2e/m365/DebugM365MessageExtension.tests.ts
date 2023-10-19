7a8fd107c870f4a8c1722d23c2b34e122e6a021e
Kuojian Lu <kuojianlu@gmail.com>
2023-10-19 14:46:47 +0800

    test(e2e): fix m365 me e2e

--- packages/tests/src/e2e/m365/DebugM365MessageExtension.tests.ts
+++ packages/tests/src/e2e/m365/DebugM365MessageExtension.tests.ts
@@ -28,11 +28,22 @@ import {
   getBot,
   getTeamsApp,
 } from "../debug/utility";
+import { FeatureFlagName } from "@microsoft/teamsfx-core/src/common/constants";
 
 describe("Debug V3 m365-message-extension template", () => {
   const testFolder = getTestFolder();
   const appName = getUniqueAppName();
   const projectPath = path.resolve(testFolder, appName);
+  let copilotPluginEnv: string;
+
+  before(function () {
+    copilotPluginEnv = process.env[FeatureFlagName.CopilotPlugin] as string;
+    process.env[FeatureFlagName.CopilotPlugin] = "false";
+  });
+
+  after(function () {
+    process.env[FeatureFlagName.CopilotPlugin] = copilotPluginEnv;
+  });
 
   afterEach(async function () {
     const context = await readContextMultiEnvV3(projectPath, "local");

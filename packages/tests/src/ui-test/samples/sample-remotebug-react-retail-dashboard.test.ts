e3724c61ac32162a20c42c1f6cd43f5eb965b8d2
v-ivanchen_microsoft <v-ivanchen@microsoft.com>
2023-10-19 15:25:39 +0800

    test: add ui test cases

---
+++ packages/tests/src/ui-test/samples/sample-remotebug-react-retail-dashboard.test.ts
@@ -0,0 +1,48 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+/**
+ * @author Ivan Chen <v-ivanchen@microsoft.com>
+ */
+
+import { TemplateProject } from "../../utils/constants";
+import { CaseFactory } from "./sampleCaseFactory";
+import { Page } from "playwright";
+import { initTeamsPage } from "../../utils/playwrightOperation";
+import { SampledebugContext } from "./sampledebugContext";
+import { Env } from "../../utils/env";
+
+class RetailDashboardTestCase extends CaseFactory {
+  public override async onInitPage(
+    sampledebugContext: SampledebugContext,
+    teamsAppId: string,
+    options?: {
+      teamsAppName: string;
+      type: string;
+    }
+  ): Promise<Page> {
+    return await initTeamsPage(
+      sampledebugContext.context!,
+      teamsAppId,
+      Env.username,
+      Env.password,
+      {
+        teamsAppName: options?.teamsAppName,
+        type: options?.type,
+      }
+    );
+  }
+}
+
+new RetailDashboardTestCase(
+  TemplateProject.RetailDashboard,
+  25051150,
+  "v-ivanchen@microsoft.com",
+  "dev",
+  [],
+  {
+    teamsAppName: "react-retail-dashboard-dev",
+    type: "spfx",
+    skipValidation: true,
+  }
+).test();
---
+++ packages/tests/src/ui-test/samples/sample-remotedebug-sso-tab-via-apim-proxy.test.ts
@@ -0,0 +1,31 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+/**
+ * @author Ivan Chen <v-ivanchen@microsoft.com>
+ */
+
+import { Page } from "playwright";
+import { TemplateProject } from "../../utils/constants";
+import { validateTab } from "../../utils/playwrightOperation";
+import { CaseFactory } from "./sampleCaseFactory";
+import { Env } from "../../utils/env";
+
+class SsotabApimTestCase extends CaseFactory {
+  override async onValidate(
+    page: Page,
+    options?: { includeFunction: boolean }
+  ): Promise<void> {
+    return await validateTab(page, {
+      displayName: Env.displayName,
+      includeFunction: options?.includeFunction,
+    });
+  }
+}
+
+new SsotabApimTestCase(
+  TemplateProject.TabSSOApimProxy,
+  25191534,
+  "v-ivanchen@microsoft.com",
+  "dev"
+).test();

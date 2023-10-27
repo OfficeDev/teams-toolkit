eb4b9e5503f44ec57f21871a1bbaa64d5bcd6d38
Alex Wang <ice_b0und@hotmail.com>
2023-10-27 14:20:15 +0800

    feat: remove feature flag for 1.3 schema

--- packages/fx-core/tests/component/configManager/validator.test.ts
+++ packages/fx-core/tests/component/configManager/validator.test.ts
@@ -9,8 +9,6 @@ import chai from "chai";
 import { describe, it } from "mocha";
 import { Validator } from "../../../src/component/configManager/validator";
 import * as sinon from "sinon";
-import * as featureFlags from "../../../src/common/featureFlags";
-
 describe("yaml validator", () => {
   const validator = new Validator();
   afterEach(() => {
@@ -26,11 +24,7 @@ describe("yaml validator", () => {
       .expect(validator.supportedVersions())
       .contains("1.0.0")
       .and.contains("1.1.0")
-      .and.contains("v1.2");
-  });
-
-  it("should support v1.3 for test tool", () => {
-    sinon.stub(featureFlags, "isTestToolEnabled").resolves(true);
-    chai.expect(validator.supportedVersions()).contain("v1.3");
+      .and.contains("v1.2")
+      .and.contains("v1.3");
   });
 });

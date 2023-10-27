eb9f054dbcdd25a16d6be0cbc800c0688f0aea5d
turenlong <rentu@microsoft.com>
2023-10-27 13:14:11 +0800

    perf: add missing export type

--- packages/fx-core/src/common/spec-parser/index.browser.ts
+++ packages/fx-core/src/common/spec-parser/index.browser.ts
@@ -4,6 +4,13 @@
 
 export { SpecParser } from "./specParser.browser";
 export { SpecParserError } from "./specParserError";
-export { ValidationStatus, WarningType, ErrorType, APIInfo, ValidateResult } from "./interfaces";
+export {
+  ValidationStatus,
+  WarningType,
+  ErrorType,
+  ErrorResult,
+  APIInfo,
+  ValidateResult,
+} from "./interfaces";
 export type { WarningResult } from "./interfaces";
 export { ConstantString } from "./constants";

11fdc6859fa4b93069019873fab4d90761994b01
Bowen Song <bowsong@microsoft.com>
2023-10-30 14:04:50 +0800

    perf(apikey): add apiKey/create action (#10232)
    * perf(apikey): add apiKey/create action
    
    * test: add ut
    
    * style: add code owner
    
    * test: update ut
    
    * docs: update messages

---
+++ packages/fx-core/tests/component/driver/apiKey/create.test.ts
@@ -0,0 +1,267 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import "mocha";
+import * as sinon from "sinon";
+import * as chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+import mockedEnv, { RestoreFn } from "mocked-env";
+import {
+  MockedLogProvider,
+  MockedM365Provider,
+  MockedUserInteraction,
+} from "../../../plugins/solution/util";
+import { CreateApiKeyDriver } from "../../../../src/component/driver/apiKey/create";
+import { AppStudioClient } from "../../../../src/component/driver/teamsApp/clients/appStudioClient";
+import { ApiSecretRegistrationAppType } from "../../../../src/component/driver/teamsApp/interfaces/ApiSecretRegistration";
+import { SystemError, err } from "@microsoft/teamsfx-api";
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+const outputKeys = {
+  registrationId: "REGISTRATION_ID",
+};
+
+const outputEnvVarNames = new Map<string, string>(Object.entries(outputKeys));
+
+describe("CreateApiKeyDriver", () => {
+  const mockedDriverContext: any = {
+    m365TokenProvider: new MockedM365Provider(),
+    ui: new MockedUserInteraction(),
+  };
+  const createApiKeyDriver = new CreateApiKeyDriver();
+
+  let envRestore: RestoreFn | undefined;
+
+  afterEach(() => {
+    sinon.restore();
+    if (envRestore) {
+      envRestore();
+      envRestore = undefined;
+    }
+  });
+
+  it("happy path: create registraionid and read domain, clientSecret from env", async () => {
+    sinon.stub(AppStudioClient, "createApiKeyRegistration").resolves({
+      id: "mockedRegistrationId",
+      clientSecrets: [],
+      targetUrlsShouldStartWith: [],
+      applicableToApps: ApiSecretRegistrationAppType.SpecificApp,
+    });
+
+    const args: any = {
+      name: "test",
+      domain: "https://test",
+      appId: "mockedAppId",
+      clientSecret: "mockedClientSecret, mockedClientSecret2",
+    };
+    const result = await createApiKeyDriver.execute(args, mockedDriverContext, outputEnvVarNames);
+    expect(result.result.isOk()).to.be.true;
+    if (result.result.isOk()) {
+      expect(result.result.value.get(outputKeys.registrationId)).to.equal("mockedRegistrationId");
+      expect(result.summaries.length).to.equal(1);
+    }
+  });
+
+  it("happy path: registration id exists in env", async () => {
+    sinon.stub(AppStudioClient, "getApiKeyRegistrationById").resolves({
+      id: "mockedRegistrationId",
+      clientSecrets: [],
+      targetUrlsShouldStartWith: [],
+      applicableToApps: ApiSecretRegistrationAppType.SpecificApp,
+    });
+
+    const args: any = {
+      name: "test",
+      domain: "https://test",
+      appId: "mockedAppId",
+      clientSecret: "mockedClientSecret",
+    };
+    envRestore = mockedEnv({
+      [outputKeys.registrationId]: "existing value",
+    });
+    const result = await createApiKeyDriver.execute(args, mockedDriverContext, outputEnvVarNames);
+    expect(result.result.isOk()).to.be.true;
+    if (result.result.isOk()) {
+      expect(result.result.value.size).to.equal(0);
+      expect(result.summaries.length).to.equal(0);
+    }
+  });
+
+  it("should throw error when empty outputEnvVarNames", async () => {
+    const args: any = {
+      name: "test",
+      domain: "https://test",
+      appId: "mockedAppId",
+      clientSecret: "mockedClientSecret",
+    };
+    const result = await createApiKeyDriver.execute(args, mockedDriverContext, undefined);
+    expect(result.result.isErr()).to.be.true;
+    if (result.result.isErr()) {
+      expect(result.result.error.name).to.equal("OutputEnvironmentVariableUndefined");
+    }
+  });
+
+  it("should throw error when failed to get app studio token", async () => {
+    sinon
+      .stub(MockedM365Provider.prototype, "getAccessToken")
+      .resolves(err(new SystemError("source", "name", "message")));
+    const args: any = {
+      name: "test",
+      domain: "https://test",
+      appId: "mockedAppId",
+      clientSecret: "mockedClientSecret",
+    };
+    const result = await createApiKeyDriver.execute(args, mockedDriverContext, outputEnvVarNames);
+    expect(result.result.isErr()).to.be.true;
+    if (result.result.isErr()) {
+      expect(result.result.error.name).to.equal("name");
+    }
+  });
+
+  it("should show warning if registration id exists and failed to get API key", async () => {
+    sinon
+      .stub(AppStudioClient, "getApiKeyRegistrationById")
+      .throws(new SystemError("source", "name", "message"));
+
+    const args: any = {
+      name: "test",
+      domain: "https://test",
+      appId: "mockedAppId",
+      clientSecret: "mockedClientSecret",
+    };
+    envRestore = mockedEnv({
+      [outputKeys.registrationId]: "existing value",
+    });
+    const result = await createApiKeyDriver.execute(args, mockedDriverContext, outputEnvVarNames);
+    expect(result.result.isOk()).to.be.true;
+  });
+
+  it("should throw error if missing name", async () => {
+    const args: any = {
+      name: "",
+      domain: "https://test",
+      appId: "mockedAppId",
+      clientSecret: "mockedClientSecret",
+    };
+    const result = await createApiKeyDriver.execute(args, mockedDriverContext, outputEnvVarNames);
+    expect(result.result.isErr()).to.be.true;
+    if (result.result.isErr()) {
+      expect(result.result.error.name).to.equal("InvalidActionInputError");
+    }
+  });
+
+  it("should throw error if name is too long", async () => {
+    const args: any = {
+      name: "a".repeat(129),
+      domain: "https://test",
+      appId: "mockedAppId",
+      clientSecret: "mockedClientSecret",
+    };
+    const result = await createApiKeyDriver.execute(args, mockedDriverContext, outputEnvVarNames);
+    expect(result.result.isErr()).to.be.true;
+    if (result.result.isErr()) {
+      expect(result.result.error.name).to.equal("ApiKeyNameTooLong");
+    }
+  });
+
+  it("should throw error if missing appId", async () => {
+    const args: any = {
+      name: "test",
+      domain: "https://test",
+      appId: "",
+      clientSecret: "mockedClientSecret",
+    };
+    const result = await createApiKeyDriver.execute(args, mockedDriverContext, outputEnvVarNames);
+    expect(result.result.isErr()).to.be.true;
+    if (result.result.isErr()) {
+      expect(result.result.error.name).to.equal("InvalidActionInputError");
+    }
+  });
+
+  it("should throw error if invalid clientSecret", async () => {
+    let args: any = {
+      name: "test",
+      domain: "https://test",
+      appId: "",
+      clientSecret: "secret",
+    };
+    let result = await createApiKeyDriver.execute(args, mockedDriverContext, outputEnvVarNames);
+    expect(result.result.isErr()).to.be.true;
+    if (result.result.isErr()) {
+      expect(result.result.error.name).to.equal("ApiKeyClientSecretInvalid");
+    }
+
+    args = {
+      name: "test",
+      domain: "https://test",
+      appId: "",
+      clientSecret: "mockedSecret, mockedSecret2, mockedSecret3",
+    };
+    result = await createApiKeyDriver.execute(args, mockedDriverContext, outputEnvVarNames);
+    expect(result.result.isErr()).to.be.true;
+    if (result.result.isErr()) {
+      expect(result.result.error.name).to.equal("ApiKeyClientSecretInvalid");
+    }
+  });
+
+  it("should throw error if invalid domain", async () => {
+    let args: any = {
+      name: "test",
+      domain: "https://test, https://test2",
+      appId: "",
+      clientSecret: "mockedSecret",
+    };
+    let result = await createApiKeyDriver.execute(args, mockedDriverContext, outputEnvVarNames);
+    expect(result.result.isErr()).to.be.true;
+    if (result.result.isErr()) {
+      expect(result.result.error.name).to.equal("ApiKeyDomainInvalid");
+    }
+
+    args = {
+      name: "test",
+      domain: ", https://test",
+      appId: "",
+      clientSecret: "mockedSecret, mockedSecret2, mockedSecret3",
+    };
+    result = await createApiKeyDriver.execute(args, mockedDriverContext, outputEnvVarNames);
+    expect(result.result.isErr()).to.be.true;
+    if (result.result.isErr()) {
+      expect(result.result.error.name).to.equal("ApiKeyClientSecretInvalid");
+    }
+  });
+
+  it("should throw error if failed to create API key", async () => {
+    sinon
+      .stub(AppStudioClient, "createApiKeyRegistration")
+      .throws(new SystemError("source", "name", "message"));
+
+    const args: any = {
+      name: "test",
+      domain: "https://test",
+      appId: "mockedAppId",
+      clientSecret: "mockedClientSecret, mockedClientSecret2",
+    };
+    const result = await createApiKeyDriver.execute(args, mockedDriverContext, outputEnvVarNames);
+    expect(result.result.isErr()).to.be.true;
+    if (result.result.isErr()) {
+      expect(result.result.error.name).to.equal("name");
+    }
+  });
+
+  it("should throw unhandled error if error is not SystemError or UserError", async () => {
+    sinon.stub(MockedM365Provider.prototype, "getAccessToken").throws(new Error("unhandled error"));
+    const args: any = {
+      name: "test",
+      domain: "https://test",
+      appId: "mockedAppId",
+      clientSecret: "mockedClientSecret, mockedClientSecret2",
+    };
+    const result = await createApiKeyDriver.execute(args, mockedDriverContext, outputEnvVarNames);
+    expect(result.result.isErr()).to.be.true;
+    if (result.result.isErr()) {
+      expect(result.result.error.source).to.equal("apiKeyCreate");
+    }
+  });
+});

11a947cd260a158f6d473371d2adca3dd68852ca
Yuqi Zhou <yuqzho@microsoft.com>
2023-11-15 12:57:28 +0800

    refactor: rename ff
    refactor: some revert

--- packages/fx-core/src/common/constants.ts
+++ packages/fx-core/src/common/constants.ts
@@ -55,7 +55,7 @@ export class FeatureFlagName {
   static readonly V3Migration = "TEAMSFX_V3_MIGRATION";
   static readonly VideoFilter = "TEAMSFX_VIDEO_FILTER";
   static readonly OfficeAddin = "TEAMSFX_OFFICE_ADDIN";
-  static readonly CopilotPlugin = "COPILOT_PLUGIN";
+  static readonly CopilotPlugin = "DEVELOP_COPILOT_PLUGIN";
   static readonly ApiCopilotPlugin = "API_COPILOT_PLUGIN";
   static readonly SampleConfigBranch = "TEAMSFX_SAMPLE_CONFIG_BRANCH";
   static readonly TestTool = "TEAMSFX_TEST_TOOL";

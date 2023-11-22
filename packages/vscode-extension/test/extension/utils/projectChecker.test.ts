48b338b9c4ff98eb7389321a12224be6159b5c13
huajiezhang <huajiezhang@microsoft.com>
2023-11-22 15:18:56 +0800

    test: ut

--- packages/vscode-extension/test/extension/utils/projectChecker.test.ts
+++ packages/vscode-extension/test/extension/utils/projectChecker.test.ts
@@ -1,4 +1,4 @@
-import { UserError, err } from "@microsoft/teamsfx-api";
+import { UserError, err, ok } from "@microsoft/teamsfx-api";
 import "mocha";
 import * as sinon from "sinon";
 import * as global from "../../../src/globalVariables";
@@ -8,18 +8,25 @@ import { MockCore } from "../../mocks/mockCore";
 
 describe("checkProjectTypeAndSendTelemetry", () => {
   const sandbox = sinon.createSandbox();
+  const core = new MockCore();
   afterEach(() => {
     sandbox.restore();
   });
   it("happy", async () => {
-    sandbox.stub(global, "workspaceUri").value("./");
-    const core = new MockCore();
+    sandbox.stub(global, "workspaceUri").value(vscode.Uri.file("./"));
     sandbox.stub(handler, "core").value(core);
+    sandbox.stub(core, "checkProjectType").resolves(
+      ok({
+        isTeamsFx: true,
+        hasTeamsManifest: true,
+        dependsOnTeamsJs: false,
+        lauguages: ["ts"],
+      })
+    );
     await checkProjectTypeAndSendTelemetry();
   });
   it("error", async () => {
-    sandbox.stub(global, "workspaceUri").value("./");
-    const core = new MockCore();
+    sandbox.stub(global, "workspaceUri").value(vscode.Uri.file("./"));
     sandbox.stub(handler, "core").value(core);
     sandbox.stub(core, "checkProjectType").resolves(err(new UserError({})));
     await checkProjectTypeAndSendTelemetry();

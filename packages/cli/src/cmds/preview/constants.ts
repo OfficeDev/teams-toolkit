a81d549b2d8687e41f88f179685b52c0604e4a1f
Ning Tang <nintan@microsoft.com>
2023-12-01 10:13:43 +0800

    fix: comment

--- packages/cli/src/cmds/preview/constants.ts
+++ packages/cli/src/cmds/preview/constants.ts
@@ -104,7 +104,7 @@ export const doctorResult = {
   SideLoadingDisabled:
     "Your Microsoft 365 tenant admin hasn't enabled uploading custom apps permission for your account. You can't install your app to Teams!",
   NotSignIn: "No Microsoft 365 account login",
-  SignInSuccess: `Microsoft 365 Account (@account) is logged in and uploading custom apps permission enabled`,
+  SignInSuccess: `Microsoft 365 Account (@account) is logged in and uploading custom apps permission is enabled`,
   SkipTrustingCert: "Skip trusting development certificate for localhost",
   HelpLink: `Please refer to @Link for more information.`,
   NgrokWarning:

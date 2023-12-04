20c82f9408b5162e5fabe378e79980e334498492
Huajie Zhang <huajiezhang@microsoft.com>
2023-12-04 12:56:16 +0800

    refactor: add new vscode-ui package (#10470)
    * refactor: vscode-ui package
    
    * ci: ui package
    
    * test: ut
    
    * fix: error
    
    * fix: error
    
    * fix: error
    
    * test: ut
    
    * test: ut
    
    * docs: readme

---
+++ packages/vscode-ui/src/index.ts
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+export * from "./ui";
+export * from "./progressHandler";
+export * from "./localize";
+export * from "./error";

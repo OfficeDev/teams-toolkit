1235c5421253a8780faa3bfcbc7c3026abc83e5e
Yiqing Zhao <cissyzhao99@gmail.com>
2023-12-20 16:19:11 +0800

    build: add support for csharp templates dependency updates

--- .github/scripts/get-dailydigest-dependencies.js
+++ .github/scripts/get-dailydigest-dependencies.js
@@ -5,58 +5,61 @@ const path = require("path");
 
 const repoRoot = __dirname + "/../..";
 
+const codeOwnerMap = new Map([
+  ["copilot-plugin-from-scratch", "huimiao@microsoft.com"],
+  ["dashboard-tab", "huimiao@microsoft.com"],
+  ["non-sso-tab", "zhijie.huang@microsoft.com"],
+  ["non-sso-tab-ssr", "yiminjin@microsoft.com"],
+  ["sso-tab", "zhijie.huang@microsoft.com"],
+  ["sso-tab-ssr", "yiminjin@microsoft.com"],
+  ["default-bot", "yukundong@microsoft.com"],
+  ["link-unfurling", "yukundong@microsoft.com"],
+  ["message-extension-action", "yukundong@microsoft.com"],
+  ["message-extension-search", "yukundong@microsoft.com"],
+  ["message-extension-copilot", "yukundong@microsoft.com"],
+  ["non-sso-tab-default-bot/tab", "yuqzho@microsoft.com"],
+  ["non-sso-tab-default-bot/bot", "yuqzho@microsoft.com"],
+  ["default-bot-message-extension", "yuqzho@microsoft.com"],
+  ["message-extension", "yuqzho@microsoft.com"],
+  ["office-addin", "huajiezhang@microsoft.com"],
+  ["copilot-plugin-existing-api", "yuqzho@microsoft.com"],
+  ["copilot-plugin-existing-api-api-key", "yuqzho@microsoft.com"],
+  ["spfx-tab", "yuqzho@microsoft.com"],
+  ["spfx-tab-import", "yuqzho@microsoft.com"],
+  ["sso-tab-with-obo-flow", "bowen.song@microsoft.com"],
+  ["command-and-response", "qidon@microsoft.com"],
+  ["notification-http-timer-trigger", "qidon@microsoft.com"],
+  ["notification-http-trigger", "qidon@microsoft.com"],
+  ["notification-restify", "qidon@microsoft.com"],
+  ["notification-timer-trigger", "qidon@microsoft.com"],
+  ["notification-webapi", "qidon@microsoft.com"],
+  ["workflow", "qidon@microsoft.com"],
+  ["m365-message-extension", "kuojianlu@microsoft.com"],
+  ["m365-tab", "kuojianlu@microsoft.com"],
+  ["ai-bot", "kuojianlu@microsoft.com"],
+  ["ai-assistant-bot", "kuojianlu@microsoft.com"],
+]);
+
 async function getTemplatesDependencies() {
   var dependenciesMap = new Map();
   const templatePkgJsonPath = `${repoRoot}/templates/**/package.json.tpl`;
   const packageJsonFiles = await glob.glob(templatePkgJsonPath, {
     ignore: "node_modules/**",
   });
-  const codeOwnerMap = new Map([
-    ["copilot-plugin-from-scratch", "huimiao@microsoft.com"],
-    ["dashboard-tab", "huimiao@microsoft.com"],
-    ["non-sso-tab", "zhijie.huang@microsoft.com"],
-    ["sso-tab", "zhijie.huang@microsoft.com"],
-    ["default-bot", "yukundong@microsoft.com"],
-    ["link-unfurling", "yukundong@microsoft.com"],
-    ["message-extension-action", "yukundong@microsoft.com"],
-    ["message-extension-search", "yukundong@microsoft.com"],
-    ["message-extension-copilot", "yukundong@microsoft.com"],
-    ["non-sso-tab-default-bot/tab", "yuqzho@microsoft.com"],
-    ["non-sso-tab-default-bot/bot", "yuqzho@microsoft.com"],
-    ["default-bot-message-extension", "yuqzho@microsoft.com"],
-    ["message-extension", "yuqzho@microsoft.com"],
-    ["office-addin", "huajiezhang@microsoft.com"],
-    ["copilot-plugin-existing-api", "yuqzho@microsoft.com"],
-    ["copilot-plugin-existing-api-api-key", "yuqzho@microsoft.com"],
-    ["spfx-tab", "yuqzho@microsoft.com"],
-    ["spfx-tab-import", "yuqzho@microsoft.com"],
-    ["sso-tab-with-obo-flow", "bowen.song@microsoft.com"],
-    ["command-and-response", "qidon@microsoft.com"],
-    ["notification-http-timer-trigger", "qidon@microsoft.com"],
-    ["notification-http-trigger", "qidon@microsoft.com"],
-    ["notification-restify", "qidon@microsoft.com"],
-    ["notification-timer-trigger", "qidon@microsoft.com"],
-    ["notification-webapi", "qidon@microsoft.com"],
-    ["workflow", "qidon@microsoft.com"],
-    ["m365-message-extension", "kuojianlu@microsoft.com"],
-    ["m365-tab", "kuojianlu@microsoft.com"],
-    ["ai-bot", "kuojianlu@microsoft.com"],
-    ["ai-assistant-bot", "kuojianlu@microsoft.com"],
-  ]);
 
   packageJsonFiles.forEach((packageJsonFile) => {
     const packageJson = JSON.parse(fs.readFileSync(packageJsonFile, "utf8"));
-    let packageJsonDir = path.relative(
+    let templateDir = path.relative(
       `${repoRoot}/templates`,
       path.dirname(packageJsonFile)
     );
     let codeOwners = "";
     for (const [key, value] of codeOwnerMap) {
-      if (packageJsonDir.includes("non-sso-tab-default-bot")) {
+      if (templateDir.includes("non-sso-tab-default-bot")) {
         codeOwners = "yuqzho@microsoft.com";
         continue;
       }
-      if (key === path.basename(packageJsonDir)) {
+      if (key === path.basename(templateDir)) {
         codeOwners = value;
       }
     }
@@ -68,23 +71,85 @@ async function getTemplatesDependencies() {
         dependenciesMap.get(dependency).has(codeOwners)
       ) {
         dependenciesMap.get(dependency).get(codeOwners).push({
-          packageJsonDir,
+          templateDir,
           version: dependencies[dependency],
         });
       } else if (dependenciesMap.has(dependency)) {
         dependenciesMap
           .get(dependency)
           .set(codeOwners, [
-            { packageJsonDir, version: dependencies[dependency] },
+            { templateDir, version: dependencies[dependency] },
           ]);
       } else {
         const codeOwnerTemplateMap = new Map([
-          [codeOwners, [{ packageJsonDir, version: dependencies[dependency] }]],
+          [codeOwners, [{ templateDir, version: dependencies[dependency] }]],
         ]);
         dependenciesMap.set(dependency, codeOwnerTemplateMap);
       }
     }
   });
+
+  return dependenciesMap;
+}
+
+async function getCsharpTemplateDependencies() {
+  var dependenciesMap = new Map();
+  const templateCsprojPath = `${repoRoot}/templates/**/*.csproj.tpl`;
+  const csprojFiles = await glob.glob(templateCsprojPath);
+  csprojFiles.forEach((csprojFile) => {
+    const csproj = fs.readFileSync(csprojFile, "utf8");
+    let templateDir = path.relative(
+      `${repoRoot}/templates`,
+      path.dirname(csprojFile)
+    );
+    let codeOwners = "";
+    for (const [key, value] of codeOwnerMap) {
+      if (
+        path.basename(templateDir) === "copilot-plugin-existing-api" ||
+        path.basename(templateDir) === "copilot-plugin-existing-api-api-key" ||
+        path.basename(templateDir) === "copilot-plugin-from-oai-plugin"
+      ) {
+        codeOwners = "zhiyu.you@microsoft.com";
+        continue;
+      }
+      if (key === path.basename(templateDir)) {
+        codeOwners = value;
+      }
+    }
+
+    const dependencies = csproj
+      .split("\n")
+      .filter((line) => line.includes("<PackageReference"))
+      .map((line) => {
+        const name = line.match(/Include="(.*)" Version/)[1];
+        const version = line.match(/Version="(.*)"/)[1];
+        return { name, version };
+      });
+
+    for (dependency in dependencies) {
+      const dependencyName = dependencies[dependency].name;
+      const dependencyVersion = dependencies[dependency].version;
+      if (
+        dependenciesMap.has(dependencyName) &&
+        dependenciesMap.get(dependencyName).has(codeOwners)
+      ) {
+        dependenciesMap.get(dependencyName).get(codeOwners).push({
+          templateDir,
+          version: dependencyVersion,
+        });
+      } else if (dependenciesMap.has(dependencyName)) {
+        dependenciesMap
+          .get(dependencyName)
+          .set(codeOwners, [{ templateDir, version: dependencyVersion }]);
+      } else {
+        const codeOwnerTemplateMap = new Map([
+          [codeOwners, [{ templateDir, version: dependencyVersion }]],
+        ]);
+        dependenciesMap.set(dependencyName, codeOwnerTemplateMap);
+      }
+    }
+  });
+
   return dependenciesMap;
 }
 
@@ -172,7 +237,7 @@ function generateAdaptiveCardColumnSets(arr) {
                     items: [
                       {
                         type: "TextBlock",
-                        text: templateInfo.packageJsonDir,
+                        text: templateInfo.templateDir,
                         wrap: true,
                         size: "Small",
                       },
@@ -220,7 +285,14 @@ function generateAdaptiveCardColumnSets(arr) {
             {
               type: "TextBlock",
               text:
-                `[${package.name}](https://www.npmjs.com/package/${package.name})` +
+                `[${package.name}](${
+                  package.ownerMap
+                    .values()
+                    .next()
+                    .value[0].templateDir.includes("csharp")
+                    ? `https://www.nuget.org/packages/${package.name}`
+                    : `https://www.npmjs.com/package/${package.name}`
+                })` +
                 "\n\r" +
                 `LTS-${package.version}`,
               wrap: true,
@@ -260,6 +332,27 @@ async function main() {
         }
       });
   }
+
+  const csharpDependencyMap = await getCsharpTemplateDependencies();
+  for (const entry of csharpDependencyMap.entries()) {
+    await axios
+      .get(
+        `https://api.nuget.org/v3/registration5-gz-semver2/${entry[0].toLowerCase()}/index.json`
+      )
+      .then((response) => {
+        const ltsVersion = response.data["items"].at(-1).upper;
+        const ltsVersionTime = response.data.commitTimeStamp;
+        const timeDiff = (new Date() - new Date(ltsVersionTime)) / 1000;
+        if (timeDiff <= 86400) {
+          arr.push({
+            name: entry[0],
+            version: ltsVersion,
+            ownerMap: entry[1],
+          });
+        }
+      });
+  }
+
   const table = generateAdaptiveCardColumnSets(arr);
   const tableString = JSON.stringify(table);
   return JSON.stringify(tableString);

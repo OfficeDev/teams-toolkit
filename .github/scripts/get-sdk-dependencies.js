92b7cd85c1f372b8699dae118572c04eb920b636
Yiqing Zhao <cissyzhao99@gmail.com>
2023-12-27 14:38:57 +0800

    build: fix workflow

--- .github/scripts/get-sdk-dependencies.js
+++ .github/scripts/get-sdk-dependencies.js
@@ -316,7 +316,7 @@ async function main() {
 
   const table = generateAdaptiveCardColumnSets(arr);
   const tableString = JSON.stringify(table);
-  return tableString;
+  return JSON.stringify(tableString);
 }
 
 main().then((result) => {

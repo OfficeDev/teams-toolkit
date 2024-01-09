b8329f22b15b546ddf272481d00bd4b2c892dfc3
Bowen Song <bowsong@microsoft.com>
2024-01-09 14:54:54 +0800

    perf: add error handling

---
+++ templates/ts/teams-ai-bot/src/utility.ts
@@ -0,0 +1,29 @@
+import { CardFactory, TurnContext } from "botbuilder";
+import OpenAPIClientAxios from "openapi-client-axios";
+const ACData = require("adaptivecards-templating");
+
+export async function getClient(api: OpenAPIClientAxios, context: TurnContext) {
+  const client = await api.getClient();
+  client.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async function (error) {
+      await context.sendActivity(`axios error: ${JSON.stringify(error.message)}`);
+    }
+  );
+  return client;
+}
+
+export function generateAdaptiveCard(templatePath: string, result: any) {
+  if (!result || !result.data) {
+    throw new Error("Get empty result from api call.");
+  }
+  const adaptiveCardTemplate = require(templatePath);
+  const template = new ACData.Template(adaptiveCardTemplate);
+  const cardContent = template.expand({
+    $root: result.data,
+  });
+  const card = CardFactory.adaptiveCard(cardContent);
+  return card;
+}

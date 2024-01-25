c96011d65cb20b2e56cfbd15a19f297063c9bdd6
Bowen Song <bowsong@microsoft.com>
2024-01-25 16:29:30 +0800

    perf: init spec-parser package

---
+++ packages/spec-parser/src/index.browser.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+"use strict";
+
+export { SpecParser } from "./specParser.browser";
+export { SpecParserError } from "./specParserError";
+export { ValidationStatus, WarningType, ErrorType, ListAPIResult } from "./interfaces";
+export type { ErrorResult, APIInfo, ValidateResult, WarningResult } from "./interfaces";
+export { ConstantString } from "./constants";

893ece662b80749f4b32a121d517d8cb56432041
Huajie Zhang <huajiezhang@microsoft.com>
2024-01-29 14:25:35 +0800

    fix: regular expression performance in stack mask (#10775)
    * fix: regular expression
    
    * test: ut
    
    * test: ut
    
    * test: ut

--- packages/vscode-extension/test/extension/commonUtils.test.ts
+++ packages/vscode-extension/test/extension/commonUtils.test.ts
@@ -425,4 +425,46 @@ describe("CommonUtils", () => {
       chai.assert.isFalse(result);
     });
   });
+
+  describe("anonymizeFilePaths()", () => {
+    const sandbox = sinon.createSandbox();
+
+    afterEach(() => {
+      mockfs.restore();
+      sandbox.restore();
+    });
+
+    it("undefined", async () => {
+      const result = await commonUtils.anonymizeFilePaths();
+      chai.assert.equal(result, "");
+    });
+
+    it("happy path 1", async () => {
+      const result = await commonUtils.anonymizeFilePaths(
+        "at Object.require.extensions.<computed> [as .ts] (C:\\Users\\AppData\\Roaming\\npm\\node_modules\\ts-node\\src\\index.ts:1621:12)"
+      );
+      chai.assert.equal(
+        result,
+        "at Object.require.extensions.<computed> [as .ts] (<REDACTED: user-file-path>/index.ts:1621:12)"
+      );
+    });
+    it("happy path 2", async () => {
+      const result = await commonUtils.anonymizeFilePaths(
+        "at Object.require.extensions.<computed> [as .ts] (/user/test/index.ts:1621:12)"
+      );
+      chai.assert.equal(
+        result,
+        "at Object.require.extensions.<computed> [as .ts] (<REDACTED: user-file-path>/index.ts:1621:12)"
+      );
+    });
+    it("happy path 3", async () => {
+      const result = await commonUtils.anonymizeFilePaths(
+        "some user stack trace at (C:/fake_path/fake_file:1:1)"
+      );
+      chai.assert.equal(
+        result,
+        "some user stack trace at (<REDACTED: user-file-path>/fake_file:1:1)"
+      );
+    });
+  });
 });

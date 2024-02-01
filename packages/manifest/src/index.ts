bb4867f1222afa621c6f0dcd3e1fee1ea7efdcd7
Yuqi Zhou <yuqzho@microsoft.com>
2024-02-01 20:19:27 +0800

    refactor: update manifest for API plugin
    refactor: eslint
    
    refactor: more
    
    refactor: fix
    
    refactor: minor

--- packages/manifest/src/index.ts
+++ packages/manifest/src/index.ts
@@ -12,6 +12,7 @@ import fetch from "node-fetch";
 
 export * from "./manifest";
 export * as devPreview from "./devPreviewManifest";
+export * from "./pluginManifest";
 export type TeamsAppManifestJSONSchema = JSONSchemaType<TeamsAppManifest>;
 export type DevPreviewManifestJSONSchema = JSONSchemaType<DevPreviewSchema>;
 
@@ -132,6 +133,7 @@ export class ManifestUtil {
       manifestVersion: manifest.manifestVersion,
       isApiME: false,
       isSPFx: false,
+      isApiPlugin: false,
     };
 
     // If it's copilot plugin app
@@ -150,6 +152,12 @@ export class ManifestUtil {
       manifest.webApplicationInfo.id == SharePointAppId
     ) {
       properties.isSPFx = true;
+    }
+
+    if ((manifest as TeamsAppManifest).apiPlugins) {
+      const apiPlugins = (manifest as TeamsAppManifest).apiPlugins;
+      if (apiPlugins && apiPlugins.length > 0 && apiPlugins[0].pluginFile)
+        properties.isApiPlugin = true;
     }
 
     return properties;
---
+++ packages/manifest/src/pluginManifest.ts
@@ -0,0 +1,95 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+export interface IRenderingTemplate {
+  /**
+   * Url of the adaptive card template.
+   */
+  template_url: string;
+  /**
+   * Operation id.
+   */
+  operationId: string;
+}
+
+export interface ICapability {
+  /**
+   * Adaptive card describes how a function response can be rendered.
+   */
+  rendering_templates?: { [k: string]: IRenderingTemplate };
+}
+
+export interface IFunction {
+  /**
+   * Name which uniquely identifies the function.
+   */
+  name: string;
+  /**
+   * Description.
+   */
+  description: string;
+  /**
+   * A collection of data used to configure optional capabilities of the orchestrator while invoking the function
+   */
+  capabilities?: ICapability;
+}
+
+export interface ISpec {
+  /**
+   * Local runtime identifier that links to a specific function
+   */
+  local_endpoint?: string;
+  /**
+   * Url of the OpenAPI spec.
+   */
+  url?: string;
+}
+
+export interface IAuth {
+  type: string;
+}
+
+export interface IRuntime {
+  type: "openApi" | "localPlugin";
+  auth: IAuth;
+  run_for_functions: string[];
+  spec: ISpec;
+}
+
+export interface PluginBManifest {
+  /**
+   * The schema version of the manifest
+   */
+  schema_version: string;
+  /**
+   * Human-readable name for the plugin
+   */
+  name_for_human: string;
+  /**
+   * Description for the plugin.
+   */
+  description_for_model: string;
+  /**
+   * Human-readable description for the plugin.
+   */
+  description_for_human: string;
+  /**
+   * URL used to fetach the logo.
+   */
+  logo_url: string;
+  /**
+   * An emaill address of a contact.
+   */
+  contact_email: string;
+  /**
+   * An absolute URL that locates a document containing the legal information.
+   */
+  legal_info_url: string;
+  /**
+   * A JSON array that contains an set of function objects
+   */
+  functions?: IFunction[];
+  /**
+   * A JSON array that contains an set of runtime objects
+   */
+  runtimes?: IRuntime[];
+}

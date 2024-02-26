c0176d8adf1de5740f78893f9488ce86a34f3e65
Ning Tang <nintan@microsoft.com>
2024-02-26 10:25:57 +0800

    fix(vsc): sample gallery support LFS image in GitHub repo (#10908)

--- packages/fx-core/tests/component/generator/generator.test.ts
+++ packages/fx-core/tests/component/generator/generator.test.ts
@@ -64,7 +64,7 @@ const mockedSampleInfo: SampleConfig = {
   time: "",
   configuration: "test-configuration",
   suggested: false,
-  thumbnailUrl: "",
+  thumbnailPath: "",
   gifUrl: "",
   downloadUrlInfo: {
     owner: "OfficeDev",

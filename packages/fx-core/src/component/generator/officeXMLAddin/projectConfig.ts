8b7b2a9df68ff6461387b61fad97e1ee8f0ad170
huajiezhang <huajiezhang@microsoft.com>
2024-03-13 18:35:57 +0800

    refactor: up

--- packages/fx-core/src/component/generator/officeXMLAddin/projectConfig.ts
+++ packages/fx-core/src/component/generator/officeXMLAddin/projectConfig.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { ProjectType } from "@microsoft/m365-spec-parser";
 import { OfficeAddinHostOptions, ProjectTypeOptions } from "../../../question";
 
 /**

e016dce78505eb06835cf25a95da23083355a0fb
huajiezhang <huajiezhang@microsoft.com>
2024-03-19 16:51:51 +0800

    refactor: clean up

--- packages/cli/src/resource/index.ts
+++ packages/cli/src/resource/index.ts
@@ -3,4 +3,5 @@
 
 import * as strings from "./strings.json";
 import * as commands from "./commands.json";
-export { strings, commands };
+import * as errors from "./errors.json";
+export { strings, commands, errors };

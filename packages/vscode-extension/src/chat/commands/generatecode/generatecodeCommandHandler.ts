ffc64b9d275a96f76b76bde314a659e574283164
Li Jiang (from Dev Box) <lijia@microsoft.com>
2024-03-25 23:18:12 +0800

    feat: initial pipeline code

--- packages/vscode-extension/src/chat/commands/generatecode/generatecodeCommandHandler.ts
+++ packages/vscode-extension/src/chat/commands/generatecode/generatecodeCommandHandler.ts
@@ -7,13 +7,9 @@ import {
   CancellationToken,
   LanguageModelChatUserMessage,
 } from "vscode";
-import { getCopilotResponseAsString } from "../../utils";
-import { defaultSystemPrompt } from "../../prompts";
 import { OfficeAddinChatCommand } from "../../consts";
 import { ICopilotChatResult } from "../../types";
-import { ChatTelemetryData } from "../../telemetry";
-import { ExtTelemetry } from "../../../telemetry/extTelemetry";
-import { TelemetryEvent } from "../../../telemetry/extTelemetryEvents";
+import { Planner } from "../../officeCommon/planner";
 
 // TODO: Implement the function.
 export default async function generatecodeCommandHandler(
@@ -22,15 +18,11 @@ export default async function generatecodeCommandHandler(
   response: ChatResponseStream,
   token: CancellationToken
 ): Promise<ICopilotChatResult> {
-  const chatTelemetryData = ChatTelemetryData.createByCommand(OfficeAddinChatCommand.GenerateCode);
-  ExtTelemetry.sendTelemetryEvent(TelemetryEvent.CopilotChatStart, chatTelemetryData.properties);
-
-  const messages = [defaultSystemPrompt(), new LanguageModelChatUserMessage(request.prompt)];
-  await getCopilotResponseAsString("copilot-gpt-3.5-turbo", messages, token);
-  return {
-    metadata: {
-      command: OfficeAddinChatCommand.GenerateCode,
-      requestId: chatTelemetryData.requestId,
-    },
-  };
+  return await Planner.getInstance().processRequest(
+    new LanguageModelChatUserMessage(request.prompt),
+    request,
+    response,
+    token,
+    OfficeAddinChatCommand.GenerateCode
+  );
 }

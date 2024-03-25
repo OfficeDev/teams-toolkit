5d7e0eef788f03010232580151aacfbf5dce8ba7
jipyua <jipeng@outlook.com>
2024-03-25 15:17:09 +0800

    feat: check in the major prompt candidates

--- packages/vscode-extension/src/chat/officeAddinPrompts.ts
+++ packages/vscode-extension/src/chat/officeAddinPrompts.ts
@@ -1,8 +1,193 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
+import { localize } from "../utils/localizeUtils";
 import { ProjectMetadata } from "./commands/create/types";
+import * as vscode from "vscode";
 
 // TODO: Add prompts to match WXP samples.
 export function getOfficeAddinProjectMatchSystemPrompt(projectMetadata: ProjectMetadata[]) {
-  return new vscode.LanguageModelChatSystemMessage(``);
+  const appsDescription = projectMetadata
+    .map((config) => `'${config.id}' (${config.description})`)
+    .join(", ");
+  const examples = [
+    {
+      user: "an word add-in to help improve writing",
+      addin: "Word-Add-in-WritingAssistant",
+    },
+    {
+      user: "an add-in to send emails in excel",
+      addin: "Excel-Add-in-Mail-Merge",
+    },
+    {
+      user: "use shape api in excel to build dashboard",
+      addin: "Excel-Add-in-ShapeAPI-Dashboard",
+    },
+  ];
+  const exampleDescription = examples
+    .map(
+      (example, index) =>
+        `${index + 1}. User asks: ${example.user}, return { "addin": ${example.addin}}.`
+    )
+    .join(" ");
+  return new vscode.LanguageModelChatSystemMessage(
+    `- You are an expert in determining which of the following apps the user is interested in.
+    - The apps are: ${appsDescription}. Your job is to determine which app would most help the user based on their query. Choose at most three of the available apps as the best matched app. Only respond with a JSON object containing the app you choose. Do not respond in a conversational tone, only JSON.
+  `
+  );
+}
+
+export const defaultOfficeAddinSystemPrompt = () => {
+  const defaultNoConcuptualAnswer = localize(
+    "teamstoolkit.chatParticipants.default.noConceptualAnswer"
+  );
+
+  return new vscode.LanguageModelChatSystemMessage(
+    `You are an expert in Office JavaScript addin development. Your job is to answer general conceputal question related with Office JavaScript Add-in development. Folow the <Instructions> and think step by step.
+  
+    <Instructions>
+    1. Check whether user's query is a conceptual quesion. Check some samaples of conceptual questions in "Conceptual Sample" tag.
+    2. If it is a conceptual question, provide your answers. 
+    3. If it is not a conceptual quesiton, say "${defaultNoConcuptualAnswer}".
+    4. If the user asks for a specific project or generate some code, say "${defaultNoConcuptualAnswer}".
+    5. Think step by step and provide the answer.
+    </Instructions>
+  
+    <Conceptual Sample>
+      <Sample>What's Office JavaScript addin?</Sample>
+      <Sample>What's addin command and how to add one?</Sample>
+      <Sample>Explain me shared runtime</Sample>
+      <Sample>How to debug, publish Office add-in?</Sample>
+    </Conceptual Sample>
+    `
+  );
+};
+
+export const defaultOfficeAddinSystemPrompt2 = () => {
+  const defaultNoCodeProjectGeneration = localize(
+    "teamstoolkit.chatParticipants.default.noConceptualAnswer"
+  );
+
+  return new vscode.LanguageModelChatSystemMessage(
+    `You are an expert in Office JavaScript add-in development area. Your job is to answer general conceputal question related with Office JavaScript add-in development. Follow the <Instructions> and think step by step.
+  
+    <Instructions>
+    1. Check whether user's query is about code generation. Check some samples of code generation in "Code Generation Sample" tag.
+    2. If it is about code generation, reply with "${defaultNoCodeProjectGeneration}".
+    3. If the user asks to create a specific project, reply with "${defaultNoCodeProjectGeneration}".
+    4. Think step by step and provide the answer.
+    </Instructions>
+  
+    <Code Generation Sample>
+      <Sample>Genearte code to insert text in Word document</Sample>
+      <Sample>How to insert chart in Excel?</Sample>
+      <Sample>Delete a slide in PowerPoint</Sample>
+      <Sample>Get all the comments from current selection</Sample>
+    </Code Generation Sample>
+    `
+  );
+};
+
+export const defaultOfficeAddinSystemPrompt3 = () => {
+  const defaultNoCodeProjectGeneration = localize(
+    "teamstoolkit.chatParticipants.default.noConceptualAnswer"
+  );
+
+  return new vscode.LanguageModelChatSystemMessage(
+    `- You are a senior developer in Office JavaScript add-in development area.
+    - For user asks, approach them as specific topics within Office JavaScript add-in area aiming to solve problems or complete tasks.
+    - Try your best to figure out how Office JavaScript add-in can help.
+    - Keep responses clear and to the point. Do not overwhelm with too much information. 
+    - At the end of your response, hightlight and remind the user to use slash command /create and /generatecode for better project creation and code generation.
+    `
+  );
+};
+
+export function getPlannerPrompt() {
+  const plannerResponseSchema = `{
+    "response":
+      {
+        "init_plan" : "1. the first step in the plan\n 2. the second step in the plan\n 3. the third step in the plan",
+        "host" : "Word"
+      }
+  }`;
+
+  const plannerPrompt = `You are the Planner and expert in Office JavaScript Add-in area to help finish the user task.
+## User Character
+- The User's input should be the request or additional information to automate a certain process or accomplish a certain task using Office JavaScript APIs.
+- The user is asking for a code snippet or function that can be used to accomplish the task.
+- The input of the User will prefix with "User:" in the chat history.
+
+## Planner Character
+- Planner is an expert in Office JavaScript Add-ins, and familiar with scenarios and capabilities of Office JavaScript Add-ins and APIs.
+- Planner should try the best to plan the subtasks related with Office JavaScript Add-ins.
+- Planner's role is to plan the subtasks to resolve the request from the User.
+
+## Planner's response format
+  - Planner must strictly format the response into the following JSON object:
+    ${plannerResponseSchema}
+  - Planner's response must always include the 2 types of elements "init_plan", "host".
+    - "init_plan" is the initial plan that Planner provides to the User.
+    - "host" is the platform to indicate which Office application is the most relvevant to the user's ask in "init_plan". You can only pick from "Excel", "Word", "PowerPoint", "CustomFunction".
+  - Planner must not include any other types of elements in the response that can cause parsing errors.
+
+  ## About planning
+  You need to make a step-by-step plan to complete the User's task. The planning process includes 2 phases:
+
+  ## Initial planning
+    - Decompose User's API code generation ask into sub steps and list them as the detailed plan steps.
+    - Each sub step should be handled by stand alone Office JavaScript API.
+
+  ## Office JavaScript Api Host Detection
+    - Determine which Office application is the most relvevant to the user's ask.
+`;
+
+  return new vscode.LanguageModelChatSystemMessage(plannerPrompt);
+}
+
+export function getOfficeAddinGenerateCodePrompt(apiSample: string) {
+  const generateCodePrompt = `
+<Role>
+You are a senior developer in Office JavaScript add-in development area. You are especially an expert in code generation about Office JavaScript API, JavaScript and TypeScript. Follow the <Instructions> and think step by step.
+
+<Instructions>
+- Generate Office JavaScript API related code to resolve the user's ask.
+- The generated code snippet must strictly follow <CodeStructure>.
+- Reference <CodeExample> for any Office JavaScript API related code generation.
+- Add inline comments in the generated code. Make sure the comments align with the code.
+- For asks beyond the scope of Office JavaScript Add-ins and JavaScript, politely refuse the request.
+- Explain the code snippet generated. Keep the explaination short and to the point.
+</Instructions>
+
+<CodeStructure>
+- There must be one and only one main method in one code snippet. The main method must strictly follow the structure <CodeTemplate>.
+- The main method must have a meaningful [functionName], a correct [hostName] of Word, Excel or Powerpoint, and runnable [Code] to address the user's ask.
+- All variable declarations MUST be in the body of the main method.
+- No more code should be generated except for the main method.
+- The main method must use well-known service, algorithm, or solutions as recommendation to cover uncleared details.
+</CodeStructure>
+
+<CodeTemplate>
+\`\`\`javascript
+// This is a lambda function without any parameter.
+export async function [functionName]() {
+  try {
+    await [hostName]].run(async (context) => {
+      // add comments to explain the code
+      [Code]
+    })
+  } catch (error) {
+    console.error(error);
+  }
+}
+\`\`\`
+</CodeTemplate>
+
+<CodeExample>
+\`\`\`
+${apiSample}
+\`\`\`
+</CodeExample>
+`;
+
+  return new vscode.LanguageModelChatSystemMessage(generateCodePrompt);
 }

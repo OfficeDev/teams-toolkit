562fd1712b49199e7532976c970992dd6fa55312
Bowen Song <bowsong@microsoft.com>
2024-03-27 07:44:50 +0800

    docs: update messages

--- packages/fx-core/src/component/driver/apiKey/interface/updateApiKeyArgs.ts
+++ packages/fx-core/src/component/driver/apiKey/interface/updateApiKeyArgs.ts
@@ -6,6 +6,6 @@ export interface UpdateApiKeyArgs {
   name: string; // The name of Api Secret
   appId: string; // Teams app id
   apiSpecPath: string; // The location of api spec file
-  applicableToApps?: string; // What app can access the api key. Values can be "SpecificApp" or "AnyApp". Default is "AnyApp".
-  targetAudience?: string; // What tenant can access the api key. Values can be "HomeTenant" or "AnyTenant". Default is "HomeTenant".
+  applicableToApps?: string; // Which app can access the API key? Values can be "SpecificApp" or "AnyApp". Default is "AnyApp".
+  targetAudience?: string; // Which tenant can access the API key? Values can be "HomeTenant" or "AnyTenant". Default is "HomeTenant".
 }

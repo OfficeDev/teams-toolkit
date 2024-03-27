5467f4b8af2c95f8071ed12c09808d929939c391
rentu <5545529+SLdragon@users.noreply.github.com>
2024-03-27 10:01:36 +0800

    perf(spec-parser): fix issue with selecting APIs having same auth (#11196)
    Co-authored-by: turenlong <rentu@microsoft.com>

--- packages/spec-parser/test/specParser.test.ts
+++ packages/spec-parser/test/specParser.test.ts
@@ -1378,6 +1378,114 @@ describe("SpecParser", () => {
       }
     });
 
+    it("should work if two api contains same auth", async () => {
+      const specParser = new SpecParser("path/to/spec.yaml", { allowAPIKeyAuth: true });
+      const spec = {
+        openapi: "3.0.0",
+        components: {
+          securitySchemes: {
+            api_key: {
+              type: "apiKey",
+              name: "api_key",
+              in: "header",
+            },
+          },
+        },
+        paths: {
+          "/hello": {
+            get: {
+              operationId: "getHello",
+              security: [
+                {
+                  api_key: [],
+                },
+              ],
+              responses: {
+                200: {
+                  content: {
+                    "application/json": {
+                      schema: {
+                        type: "object",
+                        properties: {
+                          name: {
+                            type: "string",
+                          },
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+            post: {
+              security: [
+                {
+                  api_key: [],
+                },
+              ],
+              operationId: "postHello",
+              responses: {
+                200: {
+                  content: {
+                    "application/json": {
+                      schema: {
+                        type: "object",
+                        properties: {
+                          name: {
+                            type: "string",
+                          },
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      };
+      const parseStub = sinon.stub(specParser.parser, "parse").resolves(spec as any);
+      const dereferenceStub = sinon.stub(specParser.parser, "dereference").resolves(spec as any);
+      const specFilterStub = sinon.stub(SpecFilter, "specFilter").returns({} as any);
+      const outputFileStub = sinon.stub(fs, "outputFile").resolves();
+      const outputJSONStub = sinon.stub(fs, "outputJSON").resolves();
+      const JsyamlSpy = sinon.spy(jsyaml, "dump");
+
+      const manifestUpdaterStub = sinon
+        .stub(ManifestUpdater, "updateManifest")
+        .resolves([{}, []] as any);
+      const generateAdaptiveCardStub = sinon
+        .stub(AdaptiveCardGenerator, "generateAdaptiveCard")
+        .returns([
+          {
+            type: "AdaptiveCard",
+            $schema: "http://adaptivecards.io/schemas/adaptive-card.json",
+            version: "1.5",
+            body: [
+              {
+                type: "TextBlock",
+                text: "id: ${id}",
+                wrap: true,
+              },
+            ],
+          },
+          "$",
+        ]);
+
+      const filter = ["get /hello", "post /hello"];
+
+      const outputSpecPath = "path/to/output.yaml";
+      const result = await specParser.generate("path/to/manifest.json", filter, outputSpecPath);
+      expect(result.allSuccess).to.be.true;
+      expect(JsyamlSpy.calledOnce).to.be.true;
+      expect(specFilterStub.calledOnce).to.be.true;
+      expect(outputFileStub.calledOnce).to.be.true;
+      expect(manifestUpdaterStub.calledOnce).to.be.true;
+      expect(outputFileStub.firstCall.args[0]).to.equal(outputSpecPath);
+      expect(outputJSONStub.calledOnce).to.be.true;
+      expect(generateAdaptiveCardStub.notCalled).to.be.true;
+    });
+
     it("should work if contain multiple API key in spec when project Type is teams ai", async () => {
       const specParser = new SpecParser("path/to/spec.yaml", {
         allowAPIKeyAuth: true,

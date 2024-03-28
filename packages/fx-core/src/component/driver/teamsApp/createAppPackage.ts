e77112f14a920979ca9d68f7860184fda466f221
Yuqi Zhou <yuqzho@microsoft.com>
2024-03-28 17:25:23 +0800

    refactor: more

--- packages/fx-core/src/component/driver/teamsApp/createAppPackage.ts
+++ packages/fx-core/src/component/driver/teamsApp/createAppPackage.ts
@@ -2,15 +2,7 @@
 // Licensed under the MIT license.
 
 import { hooks } from "@feathersjs/hooks/lib";
-import {
-  Colors,
-  FxError,
-  Result,
-  err,
-  ok,
-  PluginManifestSchema,
-  TeamsAppManifest,
-} from "@microsoft/teamsfx-api";
+import { Colors, FxError, Result, err, ok, PluginManifestSchema } from "@microsoft/teamsfx-api";
 import AdmZip from "adm-zip";
 import fs from "fs-extra";
 import * as path from "path";

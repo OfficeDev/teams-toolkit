0c951254a064265511ae5054554e36d247b078c4
Ning Tang <nintan@microsoft.com>
2024-03-29 15:47:57 +0800

    test: add ut for create command handler

---
+++ packages/vscode-extension/test/chat/commands/create/createCommandHandler.test.ts
@@ -0,0 +1,171 @@
+import * as chai from "chai";
+import * as chaiPromised from "chai-as-promised";
+import * as sinon from "sinon";
+import * as vscode from "vscode";
+import * as createCommandHandler from "../../../../src/chat/commands/create/createCommandHandler";
+import * as helper from "../../../../src/chat/commands/create/helper";
+import { ProjectMetadata } from "../../../../src/chat/commands/create/types";
+import * as telemetry from "../../../../src/chat/telemetry";
+import * as util from "../../../../src/chat/utils";
+import { ExtTelemetry } from "../../../../src/telemetry/extTelemetry";
+import { CancellationToken } from "../../../mocks/vsc";
+
+chai.use(chaiPromised);
+
+describe("chat create command", () => {
+  const sandbox = sinon.createSandbox();
+
+  describe("createCommandHandler()", () => {
+    afterEach(async () => {
+      sandbox.restore();
+    });
+
+    it("returns no result answer", async () => {
+      const chatTelemetryDataMock = sandbox.createStubInstance(telemetry.ChatTelemetryData);
+      sandbox.stub(chatTelemetryDataMock, "properties").get(function getterFn() {
+        return undefined;
+      });
+      sandbox.stub(chatTelemetryDataMock, "measurements").get(function getterFn() {
+        return undefined;
+      });
+      sandbox.stub(telemetry.ChatTelemetryData, "createByCommand").returns(chatTelemetryDataMock);
+      const sendTelemetryEventStub = sandbox.stub(ExtTelemetry, "sendTelemetryEvent");
+      const matchProjectStub = sandbox.stub(helper, "matchProject").resolves([]);
+
+      const response = {
+        markdown: sandbox.stub(),
+      };
+      const token = new CancellationToken();
+      await createCommandHandler.default(
+        {} as unknown as vscode.ChatRequest,
+        {} as unknown as vscode.ChatContext,
+        response as unknown as vscode.ChatResponseStream,
+        token
+      );
+      chai.assert.isTrue(
+        response.markdown.calledOnceWith(
+          "No matching templates or samples found. Try a different app description or explore other templates.\n"
+        )
+      );
+    });
+
+    it("has exactly 1 matched sample", async () => {
+      const chatTelemetryDataMock = sandbox.createStubInstance(telemetry.ChatTelemetryData);
+      sandbox.stub(chatTelemetryDataMock, "properties").get(function getterFn() {
+        return undefined;
+      });
+      sandbox.stub(chatTelemetryDataMock, "measurements").get(function getterFn() {
+        return undefined;
+      });
+      chatTelemetryDataMock.chatMessages = [];
+      sandbox.stub(telemetry.ChatTelemetryData, "createByCommand").returns(chatTelemetryDataMock);
+      const sendTelemetryEventStub = sandbox.stub(ExtTelemetry, "sendTelemetryEvent");
+      const fakedSample = {
+        id: "test-sample",
+        type: "sample",
+        platform: "Teams",
+        name: "test sample",
+        description: "test sample",
+      } as ProjectMetadata;
+      sandbox.stub(helper, "matchProject").resolves([fakedSample]);
+      const showFileTreeStub = sandbox.stub(helper, "showFileTree");
+      sandbox.stub(util, "verbatimCopilotInteraction");
+
+      const response = {
+        markdown: sandbox.stub(),
+        button: sandbox.stub(),
+      };
+      const token = new CancellationToken();
+      await createCommandHandler.default(
+        {} as unknown as vscode.ChatRequest,
+        {} as unknown as vscode.ChatContext,
+        response as unknown as vscode.ChatResponseStream,
+        token
+      );
+      chai.assert.isTrue(showFileTreeStub.calledOnce);
+    });
+
+    it("has exactly 1 matched template", async () => {
+      const chatTelemetryDataMock = sandbox.createStubInstance(telemetry.ChatTelemetryData);
+      sandbox.stub(chatTelemetryDataMock, "properties").get(function getterFn() {
+        return undefined;
+      });
+      sandbox.stub(chatTelemetryDataMock, "measurements").get(function getterFn() {
+        return undefined;
+      });
+      chatTelemetryDataMock.chatMessages = [];
+      sandbox.stub(telemetry.ChatTelemetryData, "createByCommand").returns(chatTelemetryDataMock);
+      const sendTelemetryEventStub = sandbox.stub(ExtTelemetry, "sendTelemetryEvent");
+      const fakedSample = {
+        id: "test-template",
+        type: "template",
+        platform: "Teams",
+        name: "test template",
+        description: "test template",
+      } as ProjectMetadata;
+      sandbox.stub(helper, "matchProject").resolves([fakedSample]);
+      const showFileTreeStub = sandbox.stub(helper, "showFileTree");
+      sandbox.stub(util, "verbatimCopilotInteraction");
+
+      const response = {
+        markdown: sandbox.stub(),
+        button: sandbox.stub(),
+      };
+      const token = new CancellationToken();
+      await createCommandHandler.default(
+        {} as unknown as vscode.ChatRequest,
+        {} as unknown as vscode.ChatContext,
+        response as unknown as vscode.ChatResponseStream,
+        token
+      );
+      chai.assert.isTrue(showFileTreeStub.notCalled);
+    });
+
+    it("has multiple matched results", async () => {
+      const chatTelemetryDataMock = sandbox.createStubInstance(telemetry.ChatTelemetryData);
+      sandbox.stub(chatTelemetryDataMock, "properties").get(function getterFn() {
+        return undefined;
+      });
+      sandbox.stub(chatTelemetryDataMock, "measurements").get(function getterFn() {
+        return undefined;
+      });
+      chatTelemetryDataMock.chatMessages = [];
+      sandbox.stub(telemetry.ChatTelemetryData, "createByCommand").returns(chatTelemetryDataMock);
+      const sendTelemetryEventStub = sandbox.stub(ExtTelemetry, "sendTelemetryEvent");
+      const fakedSamples = [
+        {
+          id: "test-sample",
+          type: "sample",
+          platform: "Teams",
+          name: "test sample",
+          description: "test sample",
+        },
+        {
+          id: "test-sample",
+          type: "template",
+          platform: "Teams",
+          name: "test sample",
+          description: "test sample",
+        },
+      ] as ProjectMetadata[];
+      sandbox.stub(helper, "matchProject").resolves(fakedSamples);
+      const showFileTreeStub = sandbox.stub(helper, "showFileTree");
+      sandbox.stub(util, "verbatimCopilotInteraction");
+
+      const response = {
+        markdown: sandbox.stub(),
+        button: sandbox.stub(),
+      };
+      const token = new CancellationToken();
+      await createCommandHandler.default(
+        {} as unknown as vscode.ChatRequest,
+        {} as unknown as vscode.ChatContext,
+        response as unknown as vscode.ChatResponseStream,
+        token
+      );
+      chai.assert.isTrue(showFileTreeStub.notCalled);
+      chai.assert.isTrue(response.markdown.calledThrice);
+      chai.assert.isTrue(response.button.calledThrice);
+    });
+  });
+});

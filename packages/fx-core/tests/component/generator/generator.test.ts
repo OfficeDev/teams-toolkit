8f5e1985359b05063b581abc5279c1e678aa896c
Alex Wang <ice_b0und@hotmail.com>
2024-04-03 10:50:16 +0800

    test: add unit test

--- packages/fx-core/tests/component/generator/generator.test.ts
+++ packages/fx-core/tests/component/generator/generator.test.ts
@@ -799,6 +799,18 @@ describe("Generator happy path", async () => {
     assert.equal(vars.enableTestToolByDefault, "");
   });
 
+  it("template variables when ME test tool enabled", async () => {
+    sandbox.stub(process, "env").value({ TEAMSFX_ME_TEST_TOOL: "true" });
+    const vars = Generator.getDefaultVariables("test");
+    assert.equal(vars.enableMETestToolByDefault, "true");
+  });
+
+  it("template variables when ME test tool disabled", async () => {
+    sandbox.stub(process, "env").value({ TEAMSFX_ME_TEST_TOOL: "false" });
+    const vars = Generator.getDefaultVariables("test");
+    assert.equal(vars.enableMETestToolByDefault, "");
+  });
+
   it("template variables when new project enabled", async () => {
     sandbox.stub(process, "env").value({
       TEAMSFX_NEW_PROJECT_TYPE: "true",

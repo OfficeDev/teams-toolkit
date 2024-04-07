78bb0f97e4c56d2dcd4e85f51a2a3b8758d1e599
huajiezhang <huajiezhang@microsoft.com>
2024-04-07 10:53:24 +0800

    fix: extract method names from error stack

---
+++ packages/fx-core/tests/common/telemetry.test.ts
@@ -0,0 +1,48 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { assert } from "chai";
+import "mocha";
+import sinon from "sinon";
+import { extractMethodNamesFromErrorStack } from "../../src/common/telemetry";
+
+describe("telemetry", () => {
+  const sandbox = sinon.createSandbox();
+
+  afterEach(async () => {
+    sandbox.restore();
+  });
+  describe("extractMethodNamesFromErrorStack", () => {
+    it("happy path", async () => {
+      const stack = `FetchSampleInfoError: Unable to fetch sample info
+        at FetchSampleInfoError.toFxError (\\somapath\\TeamsFx\\packages\\fx-core\\build\\component\\error\\componentError.js:45:20)
+        at Object.sampleDefaultOnActionError [as onActionError] (\\somapath\\TeamsFx\\packages\\fx-core\\build\\component\\generator\\generator.js:173:59)
+        at async Generator.generate (\\somapath\TeamsFx\\packages\\fx-core\\build\\component\\generator\\generator.js:105:21)
+        at async Generator.generateSample (\\somapath\\TeamsFx\\packages\\fx-core\\build\\component\\generator\\generator.js:92:9)
+        at async Generator.<anonymous> (\\somapath\\TeamsFx\\packages\\fx-core\\build\\component\\middleware\\actionExecutionMW.js:71:13)
+        at async Coordinator.create (\\somapath\\TeamsFx\\packages\\fx-core\\build\\component\\coordinator\\index.js:165:25)
+        at async Coordinator.<anonymous> (\\somapath\\TeamsFx\\packages\\fx-core\\build\\component\\middleware\\actionExecutionMW.js:71:13)
+        at async Coordinator.<anonymous> (\\somapath\\TeamsFx\\packages\\fx-core\\build\\core\\globalVars.js:31:9)
+        at async FxCore.createSampleProject (\\somapath\\TeamsFx\\packages\\fx-core\\build\\core\\FxCore.js:102:21)
+        at async FxCore.<anonymous> (\\somapath\TeamsFx\\packages\\fx-core\\build\\component\\middleware\\questionMW.js:22:9)
+        at async FxCore.ErrorHandlerMW (\\somapath\\TeamsFx\\packages\\fx-core\\build\\core\\middleware\\errorHandler.js:19:9)
+        at async FxCore.<anonymous> (\\somapath\\TeamsFx\\packages\\fx-core\\build\\core\\globalVars.js:31:9)`;
+      const expectedOutput = [
+        "FetchSampleInfoError.toFxError",
+        "Object.sampleDefaultOnActionError [as onActionError]",
+        "async Generator.generate",
+        "async Generator.generateSample",
+        "async Generator.<anonymous>",
+        "async Coordinator.create",
+        "async Coordinator.<anonymous>",
+        "async Coordinator.<anonymous>",
+        "async FxCore.createSampleProject",
+        "async FxCore.<anonymous>",
+        "async FxCore.ErrorHandlerMW",
+        "async FxCore.<anonymous>",
+      ];
+      const output = extractMethodNamesFromErrorStack(stack);
+      assert.equal(output, expectedOutput.join(" | "));
+    });
+  });
+});

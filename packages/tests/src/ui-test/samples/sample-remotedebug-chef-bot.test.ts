cd006a0837b633fb500cd8199dd793a6a5fd2552
ayachensiyuan <ayachensiyuan@163.com>
2024-04-08 11:35:08 +0800

    test: fix chef bot test

--- packages/tests/src/ui-test/samples/sample-remotedebug-chef-bot.test.ts
+++ packages/tests/src/ui-test/samples/sample-remotedebug-chef-bot.test.ts
@@ -19,7 +19,7 @@ class ChefBotTestCase extends CaseFactory {
     sampledebugContext: SampledebugContext,
     env: "local" | "dev"
   ): Promise<void> {
-    const envFile = path.resolve(sampledebugContext.projectPath, "env", ".env.local.user");
+    const envFile = path.resolve(sampledebugContext.projectPath, "env", ".env.dev.user");
     // create .env.local.user file
     fs.writeFileSync(envFile, "SECRET_OPENAI_KEY=yourapikey");
     console.log(`add SECRET_OPENAI_KEY=yourapikey to .env file`);

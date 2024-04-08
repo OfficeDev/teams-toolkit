38bcdbe9031a823fbc652dbae4e60d264f11677b
turenlong <rentu@microsoft.com>
2024-04-08 13:23:40 +0800

    perf: add test cases

--- packages/fx-core/tests/component/generator/copilotPluginGenerator.test.ts
+++ packages/fx-core/tests/component/generator/copilotPluginGenerator.test.ts
@@ -962,7 +962,34 @@ describe("formatValidationErrors", () => {
       {
         type: ErrorType.NoSupportedApi,
         content: "test",
-        data: [{ api: "GET /api", reason: ["auth-type-is-not-supported"] }],
+        data: [
+          {
+            api: "GET /api",
+            reason: [
+              ErrorType.AuthTypeIsNotSupported,
+              ErrorType.MissingOperationId,
+              ErrorType.PostBodyContainMultipleMediaTypes,
+              ErrorType.ResponseContainMultipleMediaTypes,
+              ErrorType.ResponseJsonIsEmpty,
+              ErrorType.PostBodySchemaIsNotJson,
+              ErrorType.MethodNotAllowed,
+              ErrorType.UrlPathNotExist,
+            ],
+          },
+          {
+            api: "GET /api2",
+            reason: [
+              ErrorType.PostBodyContainsRequiredUnsupportedSchema,
+              ErrorType.ParamsContainRequiredUnsupportedSchema,
+              ErrorType.ParamsContainsNestedObject,
+              ErrorType.RequestBodyContainsNestedObject,
+              ErrorType.ExceededRequiredParamsLimit,
+              ErrorType.NoParameter,
+              ErrorType.NoAPIInfo,
+            ],
+          },
+          { api: "GET /api3", reason: ["unknown"] },
+        ],
       },
       {
         type: ErrorType.NoExtraAPICanBeAdded,
@@ -1007,10 +1034,37 @@ describe("formatValidationErrors", () => {
         getLocalizedString("core.common.invalidReason.NoAPIs")
       )
     );
+
+    const errorMessage1 = [
+      getLocalizedString("core.common.invalidReason.AuthTypeIsNotSupported"),
+      getLocalizedString("core.common.invalidReason.MissingOperationId"),
+      getLocalizedString("core.common.invalidReason.PostBodyContainMultipleMediaTypes"),
+      getLocalizedString("core.common.invalidReason.ResponseContainMultipleMediaTypes"),
+      getLocalizedString("core.common.invalidReason.ResponseJsonIsEmpty"),
+      getLocalizedString("core.common.invalidReason.PostBodySchemaIsNotJson"),
+      getLocalizedString("core.common.invalidReason.MethodNotAllowed"),
+      getLocalizedString("core.common.invalidReason.UrlPathNotExist"),
+    ];
+    const errorMessage2 = [
+      getLocalizedString("core.common.invalidReason.PostBodyContainsRequiredUnsupportedSchema"),
+      getLocalizedString("core.common.invalidReason.ParamsContainRequiredUnsupportedSchema"),
+      getLocalizedString("core.common.invalidReason.ParamsContainsNestedObject"),
+      getLocalizedString("core.common.invalidReason.RequestBodyContainsNestedObject"),
+      getLocalizedString("core.common.invalidReason.ExceededRequiredParamsLimit"),
+      getLocalizedString("core.common.invalidReason.NoParameter"),
+      getLocalizedString("core.common.invalidReason.NoAPIInfo"),
+    ];
+    const errorMessage3 = ["unknown"];
     expect(res[7].content).equals(
       getLocalizedString(
         "core.common.NoSupportedApi",
-        "GET /api: " + getLocalizedString("core.common.invalidReason.AuthTypeIsNotSupported")
+        "GET /api: " +
+          errorMessage1.join(", ") +
+          "\n" +
+          "GET /api2: " +
+          errorMessage2.join(", ") +
+          "\n" +
+          "GET /api3: unknown"
       )
     );
     expect(res[8].content).equals(getLocalizedString("error.copilotPlugin.noExtraAPICanBeAdded"));

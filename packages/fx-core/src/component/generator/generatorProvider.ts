67a3a92b9aa8cdcc06c3e52d6643177b51f752e8
huajiezhang <huajiezhang@microsoft.com>
2024-04-16 15:00:11 +0800

    refactor: office add-in new generator

--- packages/fx-core/src/component/generator/generatorProvider.ts
+++ packages/fx-core/src/component/generator/generatorProvider.ts
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
+import { OfficeAddinGeneratorNew } from "./officeAddin/generator";
 import { DefaultTemplateGenerator } from "./templates/templateGenerator";
 
-export const Generators = [new DefaultTemplateGenerator()];
+export const Generators = [new DefaultTemplateGenerator(), new OfficeAddinGeneratorNew()];

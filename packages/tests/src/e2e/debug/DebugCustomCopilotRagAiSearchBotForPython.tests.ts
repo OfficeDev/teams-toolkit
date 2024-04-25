2df96f5357125822ec27f2682ced12413f02c40d
frankqianms <frankqian@microsoft.com>
2024-04-25 10:45:43 +0800

    refactor: finish all openai cases

--- packages/tests/src/e2e/debug/DebugCustomCopilotRagAiSearchBotForPython.tests.ts
+++ packages/tests/src/e2e/debug/DebugCustomCopilotRagAiSearchBotForPython.tests.ts
@@ -49,7 +49,7 @@ describe("Debug V3 command-and-response template", () => {
   });
 
   it(
-    "happy path: provision and deploy",
+    "Azure OpenAI happy path: provision and deploy",
     { testPlanCaseId: 27454153, author: "frankqian@microsoft.com" },
     async function () {
       // create
@@ -61,6 +61,96 @@ describe("Debug V3 command-and-response template", () => {
       myRecordAzOpenAI["azure-openai-key"] = "fake";
       myRecordAzOpenAI["azure-openai-deployment-name"] = "fake";
       myRecordAzOpenAI["azure-openai-endpoint"] = "https://test.com";
+      const options = Object.entries(myRecordAzOpenAI)
+        .map(([key, value]) => "--" + key + " " + value)
+        .join(" ");
+      await CliHelper.createProjectWithCapability(
+        appName,
+        testFolder,
+        "custom-copilot-rag" as any,
+        undefined,
+        options
+      );
+      console.log(`[Successfully] scaffold to ${projectPath}`);
+
+      // create venv and pip install
+      const command = `python3 -m venv ./venv && . ./venv/bin/activate && pip install -r ./src/requirements.txt`;
+      const timeout = 200000;
+      await execAsync(command, {
+        cwd: projectPath,
+        env: process.env,
+        timeout: timeout,
+      });
+
+      // add extra envs
+      const userFile = path.resolve(projectPath, "env", `.env.local.user`);
+      const AZURE_OPENAI_EMBEDDING_DEPLOYMENT =
+        "AZURE_OPENAI_EMBEDDING_DEPLOYMENT=fake";
+      const SECRET_AZURE_SEARCH_KEY = "SECRET_AZURE_SEARCH_KEY=fake";
+      const AZURE_SEARCH_ENDPOINT = "AZURE_SEARCH_ENDPOINT=https://test.com";
+      const KEY =
+        "\n" +
+        AZURE_OPENAI_EMBEDDING_DEPLOYMENT +
+        "\n" +
+        SECRET_AZURE_SEARCH_KEY +
+        "\n" +
+        AZURE_SEARCH_ENDPOINT;
+      fs.appendFileSync(userFile, KEY);
+      console.log(`add key ${KEY} to .env.local.user file`);
+
+      // provision
+      await CliHelper.provisionProject(projectPath, "", "local", {
+        ...process.env,
+        BOT_DOMAIN: "test.ngrok.io",
+        BOT_ENDPOINT: "https://test.ngrok.io",
+      });
+      console.log(`[Successfully] provision for ${projectPath}`);
+
+      let context = await readContextMultiEnvV3(projectPath, "local");
+      chai.assert.isDefined(context);
+
+      // validate bot
+      chai.assert.isDefined(context.BOT_ID);
+      chai.assert.isNotEmpty(context.BOT_ID);
+      const aadApp = await getAadAppByClientId(context.BOT_ID);
+      chai.assert.isDefined(aadApp);
+      chai.assert.equal(aadApp?.appId, context.BOT_ID);
+      const bot = await getBot(context.BOT_ID);
+      chai.assert.equal(bot?.botId, context.BOT_ID);
+      chai.assert.equal(
+        bot?.messagingEndpoint,
+        "https://test.ngrok.io/api/messages"
+      );
+      chai.assert.deepEqual(bot?.configuredChannels, ["msteams"]);
+
+      // validate teams app
+      chai.assert.isDefined(context.TEAMS_APP_ID);
+      const teamsApp = await getTeamsApp(context.TEAMS_APP_ID);
+      chai.assert.equal(teamsApp?.teamsAppId, context.TEAMS_APP_ID);
+
+      // deploy
+      await CliHelper.deployAll(projectPath, "", "local");
+      console.log(`[Successfully] deploy for ${projectPath}`);
+
+      context = await readContextMultiEnvV3(projectPath, "local");
+      chai.assert.isDefined(context);
+
+      // validate .env
+      chai.assert.isTrue(await fs.pathExists(path.join(projectPath, ".env")));
+    }
+  );
+
+  it(
+    "OpenAI happy path: provision and deploy",
+    { testPlanCaseId: 27454158, author: "frankqian@microsoft.com" },
+    async function () {
+      // create
+      const myRecordAzOpenAI: Record<string, string> = {};
+      myRecordAzOpenAI["programming-language"] = "python ";
+      myRecordAzOpenAI["custom-copilot-rag"] =
+        "custom-copilot-rag-azureAISearch";
+      myRecordAzOpenAI["llm-service"] = "llm-service-openai";
+      myRecordAzOpenAI["openai-key"] = "fake";
       const options = Object.entries(myRecordAzOpenAI)
         .map(([key, value]) => "--" + key + " " + value)
         .join(" ");
--- packages/tests/src/e2e/debug/DebugCustomCopilotRagBasicBotForPython.tests.ts
+++ packages/tests/src/e2e/debug/DebugCustomCopilotRagBasicBotForPython.tests.ts
@@ -49,7 +49,7 @@ describe("Debug V3 command-and-response template", () => {
   });
 
   it(
-    "happy path: provision and deploy",
+    "Azure OpenAI happy path: provision and deploy",
     { testPlanCaseId: 27551381, author: "frankqian@microsoft.com" },
     async function () {
       // create
@@ -60,6 +60,79 @@ describe("Debug V3 command-and-response template", () => {
       myRecordAzOpenAI["azure-openai-key"] = "fake";
       myRecordAzOpenAI["azure-openai-deployment-name"] = "fake";
       myRecordAzOpenAI["azure-openai-endpoint"] = "https://test.com";
+      const options = Object.entries(myRecordAzOpenAI)
+        .map(([key, value]) => "--" + key + " " + value)
+        .join(" ");
+      await CliHelper.createProjectWithCapability(
+        appName,
+        testFolder,
+        "custom-copilot-rag" as any,
+        undefined,
+        options
+      );
+      console.log(`[Successfully] scaffold to ${projectPath}`);
+
+      // create venv and pip install
+      const command = `python3 -m venv ./venv && . ./venv/bin/activate && pip install -r ./src/requirements.txt`;
+      const timeout = 200000;
+      await execAsync(command, {
+        cwd: projectPath,
+        env: process.env,
+        timeout: timeout,
+      });
+
+      // provision
+      await CliHelper.provisionProject(projectPath, "", "local", {
+        ...process.env,
+        BOT_DOMAIN: "test.ngrok.io",
+        BOT_ENDPOINT: "https://test.ngrok.io",
+      });
+      console.log(`[Successfully] provision for ${projectPath}`);
+
+      let context = await readContextMultiEnvV3(projectPath, "local");
+      chai.assert.isDefined(context);
+
+      // validate bot
+      chai.assert.isDefined(context.BOT_ID);
+      chai.assert.isNotEmpty(context.BOT_ID);
+      const aadApp = await getAadAppByClientId(context.BOT_ID);
+      chai.assert.isDefined(aadApp);
+      chai.assert.equal(aadApp?.appId, context.BOT_ID);
+      const bot = await getBot(context.BOT_ID);
+      chai.assert.equal(bot?.botId, context.BOT_ID);
+      chai.assert.equal(
+        bot?.messagingEndpoint,
+        "https://test.ngrok.io/api/messages"
+      );
+      chai.assert.deepEqual(bot?.configuredChannels, ["msteams"]);
+
+      // validate teams app
+      chai.assert.isDefined(context.TEAMS_APP_ID);
+      const teamsApp = await getTeamsApp(context.TEAMS_APP_ID);
+      chai.assert.equal(teamsApp?.teamsAppId, context.TEAMS_APP_ID);
+
+      // deploy
+      await CliHelper.deployAll(projectPath, "", "local");
+      console.log(`[Successfully] deploy for ${projectPath}`);
+
+      context = await readContextMultiEnvV3(projectPath, "local");
+      chai.assert.isDefined(context);
+
+      // validate .env
+      chai.assert.isTrue(await fs.pathExists(path.join(projectPath, ".env")));
+    }
+  );
+
+  it(
+    "Azure OpenAI happy path: provision and deploy",
+    { testPlanCaseId: 27551384, author: "frankqian@microsoft.com" },
+    async function () {
+      // create
+      const myRecordAzOpenAI: Record<string, string> = {};
+      myRecordAzOpenAI["programming-language"] = "python ";
+      myRecordAzOpenAI["custom-copilot-rag"] = "custom-copilot-rag-customize";
+      myRecordAzOpenAI["llm-service"] = "llm-service-openai";
+      myRecordAzOpenAI["openai-key"] = "fake";
       const options = Object.entries(myRecordAzOpenAI)
         .map(([key, value]) => "--" + key + " " + value)
         .join(" ");

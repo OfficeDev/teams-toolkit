54c2b1072ddbc67792a248fd74be5fdb2d33cae1
Huajie Zhang <huajiezhang@microsoft.com>
2024-05-14 13:20:21 +0800

    refactor(core): office xml add-in new generator (#11580)
    * refactor: copilot new generator
    
    * refactor: copilot new generator
    
    * test: ut
    
    * test: ut
    
    * test: ut
    
    * test: ut
    
    * test: ut
    
    * test: ut
    
    * test: ut
    
    * test: ut
    
    * test: ut
    
    * test: ut
    
    * refactor: xml add-in generator
    
    * refactor: xml add-in generator
    
    * refactor: copilot template language
    
    * refactor: xml addin
    
    * test: ut
    
    * test: ut
    
    * feat: export fetch and unzip util

--- packages/fx-core/src/component/generator/generatorProvider.ts
+++ packages/fx-core/src/component/generator/generatorProvider.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT license.
 import { CopilotGenerator } from "./copilotPlugin/generator";
 import { OfficeAddinGeneratorNew } from "./officeAddin/generator";
+import { OfficeXmlAddinGeneratorNew } from "./officeXMLAddin/generator";
 import { SPFxGeneratorImport, SPFxGeneratorNew } from "./spfx/spfxGenerator";
 import { SsrTabGenerator } from "./templates/ssrTabGenerator";
 import { DefaultTemplateGenerator } from "./templates/templateGenerator";
@@ -9,6 +10,7 @@ import { DefaultTemplateGenerator } from "./templates/templateGenerator";
 // When multiple generators are activated, only the top one will be executed.
 export const Generators = [
   new OfficeAddinGeneratorNew(),
+  new OfficeXmlAddinGeneratorNew(),
   new SsrTabGenerator(),
   new DefaultTemplateGenerator(),
   new SPFxGeneratorNew(),

7b103cfc0474d77870385b2bb1c1f09b4a529d7d
Xiaofu Huang <xiaofhua@microsoft.com>
2024-05-28 12:01:28 +0800

    fix: update dotnet install version in test

--- packages/fx-core/tests/component/deps-checker/cases/dotnet.it.ts
+++ packages/fx-core/tests/component/deps-checker/cases/dotnet.it.ts
@@ -25,7 +25,6 @@ import {
   setExecutionPolicyForCurrentUser,
 } from "../utils/common";
 import * as dotnetUtils from "../utils/dotnet";
-import { dotnetInstallVersion } from "../utils/dotnet";
 
 describe("DotnetChecker E2E Test - first run", async () => {
   const sandbox = sinon.createSandbox();
@@ -183,7 +182,7 @@ describe("DotnetChecker E2E Test - first run", async () => {
     // user manually install
     await dotnetUtils.withDotnet(
       dotnetChecker,
-      dotnetInstallVersion,
+      dotnetUtils.dotnetInstallVersion,
       true,
       async (installedDotnetExecPath: string) => {
         // pre-check installed dotnet works
@@ -233,7 +232,7 @@ describe("DotnetChecker E2E Test - second run", () => {
     ) as DotnetChecker;
     await dotnetUtils.withDotnet(
       dotnetChecker,
-      dotnetInstallVersion,
+      dotnetUtils.dotnetInstallVersion,
       false,
       async (installedDotnetExecPath: string) => {
         // pre-check installed dotnet works
@@ -315,7 +314,7 @@ describe("DotnetChecker E2E Test - second run", () => {
 
     await dotnetUtils.withDotnet(
       dotnetChecker,
-      DotnetVersion.v31,
+      dotnetUtils.dotnetInstallVersion,
       true,
       async (installedDotnetExecPath: string) => {
         const invalidPath = "/this/path/does/not/exist";

99c59582f083f4b9bacc859e53241c12ebd20ef2
Ivan_Chen <v-ivanchen@microsoft.com>
2024-05-29 09:43:59 +0800

    test: fix migration failed

--- packages/tests/src/ui-test/migration/4.0.0-notification-bot-restify/helper.ts
+++ packages/tests/src/ui-test/migration/4.0.0-notification-bot-restify/helper.ts
@@ -4,8 +4,13 @@ import * as fs from "fs";
 export function updatePakcageJson(path: string): void {
   const content = fs.readFileSync(path);
   const x = JSON.parse(content.toString());
-  //@types/lodash@4.14.74 @types/node@^17.0.41
-  x.devDependencies["@types/lodash"] = "4.14.74";
-  x.devDependencies["@types/node"] = "^17.0.41";
+  x.devDependencies["@types/restify"] = "^8.5.5";
+  x.devDependencies["@types/node"] = "^18.0.0";
+  x.devDependencies["ts-node"] = "^10.4.0";
+  x.devDependencies["typescript"] = "^4.4.4";
+  x.dependencies["@microsoft/teamsfx"] = "^2.3.1";
+  x.dependencies["restify"] = "^10.0.0";
+  x.dependencies["botbuilder"] = "^4.20.0";
+
   fs.writeFileSync(path, JSON.stringify(x, null, 2));
 }

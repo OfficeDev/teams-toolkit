4b6745e6ff0957e43f2a9898e9d5e78c97bc13a4
frankqianms <109947924+frankqianms@users.noreply.github.com>
2024-05-29 10:25:40 +0800

    test: fix e2e for Python template and add 1 new case (#11722)
    * fix: try to increase restart timeout time
    
    * fix: to compare
    
    * fix: retry
    
    * fix: retry new
    
    * fix: retry 2 times
    
    * refactor: depart cases
    
    * refactor: new folder
    
    * test: add e2e for agent assistant api template
    
    * fix: rename
    
    * fix: revert WEBSITES_CONTAINER_START_TIME_LIMIT
    
    * fix: reduce retry times to 2
    
    * fix: reduce retry times to 2
    
    * fix: format fix
    
    * fix: remove extra import
    
    * fix: remove extra param

--- packages/tests/src/utils/executor.ts
+++ packages/tests/src/utils/executor.ts
@@ -14,7 +14,6 @@ import * as os from "os";
 import { spawn, ChildProcessWithoutNullStreams } from "child_process";
 import { expect } from "chai";
 import { Env } from "./env";
-import { EnvConstants } from "../../src/commonlib/constants";
 
 export class Executor {
   static async execute(
@@ -23,32 +22,50 @@ export class Executor {
     processEnv?: NodeJS.ProcessEnv,
     timeout?: number
   ) {
-    try {
-      const result = await execAsync(command, {
-        cwd,
-        env: processEnv ?? process.env,
-        timeout: timeout ?? 0,
-      });
-      if (result.stderr) {
-        /// the command exit with 0
+    let retryCount = 0;
+    const maxRetries = 2;
+
+    while (retryCount < maxRetries) {
+      // if failed, retry. 2 times at most.
+      try {
+        console.log(`[Start] "${command}" in ${cwd}.`);
+        const options = {
+          cwd,
+          env: processEnv ?? process.env,
+          timeout: timeout ?? 0,
+        };
+        const result = await execAsync(command, options);
+
+        if (result.stderr) {
+          /// the command exit with 0
+          console.log(
+            `[Pending] "${command}" in ${cwd} with some stderr: ${result.stderr}`
+          );
+          return { success: false, ...result };
+        } else {
+          console.log(`[Success] "${command}" in ${cwd}.`);
+          return { success: true, ...result };
+        }
+      } catch (e: any) {
+        if (e.killed && e.signal == "SIGTERM") {
+          console.error(`[Failed] "${command}" in ${cwd}. Timeout and killed.`);
+        } else {
+          console.error(
+            `[Failed] "${command}" in ${cwd} with error: ${e.message}`
+          );
+        }
+        retryCount++;
+        if (retryCount >= maxRetries) {
+          return { success: false, stdout: "", stderr: e.message as string };
+        }
+
         console.log(
-          `[Pending] "${command}" in ${cwd} with some stderr: ${result.stderr}`
-        );
-        return { ...result, success: false };
-      } else {
-        console.log(`[Success] "${command}" in ${cwd}.`);
-        return { ...result, success: true };
-      }
-    } catch (e: any) {
-      if (e.killed && e.signal == "SIGTERM") {
-        console.error(`[Failed] "${command}" in ${cwd}. Timeout and killed.`);
-      } else {
-        console.error(
-          `[Failed] "${command}" in ${cwd} with error: ${e.message}`
+          `Retrying "${command}" in ${cwd}. Attempt ${retryCount} of ${maxRetries}.`
         );
       }
-      return { stdout: "", stderr: e.message as string, success: false };
     }
+    console.log(`[Failed] Not executed command ${command}`);
+    return { success: false, stdout: "", stderr: "" };
   }
 
   static async login() {

5e9ee4543c4f9ad4bfdaa2a9ef0f7e965100b0f3
Yuqi Zhou <yuqzho@microsoft.com>
2024-06-03 14:57:47 +0800

    test: ut

--- packages/vscode-extension/test/extension/handlers.test.ts
+++ packages/vscode-extension/test/extension/handlers.test.ts
@@ -74,6 +74,7 @@ import * as localPrerequisites from "../../src/debug/prerequisitesHandler";
 import { TeamsAppMigrationHandler } from "../../src/migration/migrationHandler";
 import * as featureFlags from "@microsoft/teamsfx-core/build/common/featureFlags";
 import { TelemetryEvent } from "../../src/telemetry/extTelemetryEvents";
+import * as runIconHandlers from "../../src/debug/runIconHandler";
 
 describe("handlers", () => {
   describe("activate()", function () {
@@ -2901,6 +2902,7 @@ describe("autoOpenProjectHandler", () => {
     sandbox.stub(vscode.workspace, "openTextDocument");
     sandbox.stub(process, "platform").value("win32");
     sandbox.stub(fs, "pathExists").resolves(true);
+    const runLocalDebug = sandbox.stub(runIconHandlers, "selectAndDebug").resolves(ok(null));
 
     sandbox.stub(globalState, "globalStateGet").callsFake(async (key: string) => {
       if (key === "ShowLocalDebugMessage") {
@@ -2926,6 +2928,41 @@ describe("autoOpenProjectHandler", () => {
     await handlers.showLocalDebugMessage();
 
     chai.assert.isTrue(showMessageStub.calledOnce);
+    chai.assert.isTrue(runLocalDebug.called);
+  });
+
+  it("showLocalDebugMessage() - local env and non windows", async () => {
+    sandbox.stub(vscode.workspace, "workspaceFolders").value([{ uri: vscode.Uri.file("test") }]);
+    sandbox.stub(vscode.workspace, "openTextDocument");
+    sandbox.stub(process, "platform").value("linux");
+    sandbox.stub(fs, "pathExists").resolves(true);
+    const runLocalDebug = sandbox.stub(runIconHandlers, "selectAndDebug").resolves(ok(null));
+
+    sandbox.stub(globalState, "globalStateGet").callsFake(async (key: string) => {
+      if (key === "ShowLocalDebugMessage") {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    sandbox.stub(globalState, "globalStateUpdate");
+    sandbox.stub(ExtTelemetry, "sendTelemetryEvent");
+    sandbox.stub(globalVariables, "workspaceUri").value(vscode.Uri.file("test"));
+    const showMessageStub = sandbox
+      .stub(vscode.window, "showInformationMessage")
+      .callsFake(
+        (title: string, options: vscode.MessageOptions, ...items: vscode.MessageItem[]) => {
+          return Promise.resolve({
+            title: "Not Debug",
+            run: (options as any).run,
+          } as vscode.MessageItem);
+        }
+      );
+
+    await handlers.showLocalDebugMessage();
+
+    chai.assert.isTrue(showMessageStub.calledOnce);
+    chai.assert.isFalse(runLocalDebug.called);
   });
 
   it("showLocalDebugMessage() - no local env", async () => {

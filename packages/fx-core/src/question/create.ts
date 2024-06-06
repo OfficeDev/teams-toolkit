ad85d23cd2905f6c7831f438c3d96e4d2689083f
rentu <rentu@microsoft.com>
2024-06-06 14:31:04 +0800

    perf: update to fix merge issue

--- packages/fx-core/src/question/create.ts
+++ packages/fx-core/src/question/create.ts
@@ -29,7 +29,6 @@ import {
   isApiCopilotPluginEnabled,
   isCLIDotNetEnabled,
   isChatParticipantEnabled,
-  isCopilotAuthEnabled,
   isCopilotPluginEnabled,
   isOfficeJSONAddinEnabled,
 } from "../common/featureFlags";
@@ -1404,7 +1403,7 @@ export function capabilitySubTree(): IQTreeNode {
         condition: (inputs: Inputs) => {
           return (
             inputs[QuestionNames.MeArchitectureType] == MeArchitectureOptions.newApi().id ||
-            (isCopilotAuthEnabled() &&
+            (featureFlagManager.getBooleanValue(FeatureFlags.CopilotAuth) &&
               isCopilotPluginEnabled() &&
               inputs[QuestionNames.Capabilities] == CapabilityOptions.copilotPluginNewApi().id)
           );

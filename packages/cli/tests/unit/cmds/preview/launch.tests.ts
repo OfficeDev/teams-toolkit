cc863cb0f5eb31150c5f9f9425138b66d2a19da8
Yuan Tian <tianyuan@microsoft.com>
2024-06-14 10:52:02 +0800

    refactor: update test case

--- packages/cli/tests/unit/cmds/preview/launch.tests.ts
+++ packages/cli/tests/unit/cmds/preview/launch.tests.ts
@@ -89,60 +89,6 @@ describe("launch", () => {
   });
 });
 
-describe("launch Teams desktop client", () => {
-  const sandbox = sinon.createSandbox();
-  afterEach(() => {
-    sandbox.restore();
-  });
-
-  describe("openTeamsDesktopClientNew", () => {
-    let telemetries: any[] = [];
-
-    beforeEach(() => {
-      telemetries = [];
-
-      sandbox.stub(cliTelemetry, "sendTelemetryEvent").callsFake((eventName, properties) => {
-        telemetries.push([eventName, properties]);
-      });
-      sandbox
-        .stub(cliTelemetry, "sendTelemetryErrorEvent")
-        .callsFake((eventName, error, properties) => {
-          telemetries.push([eventName, error, properties]);
-        });
-      sandbox.stub(cliLogger, "necessaryLog").callsFake(() => {});
-      sandbox.stub(CLIUIInstance, "createProgressBar").returns(new MockProgressHandler());
-    });
-
-    it("happy path windows", async () => {
-      sandbox.stub(process, "platform").value("win32");
-      await openTeamsDesktopClient("http://test-url", "username", Browser.default);
-      expect(telemetries.length).to.deep.equals(0);
-    });
-
-    it("happy path mac", async () => {
-      sandbox.stub(process, "platform").value("darwin");
-      await openTeamsDesktopClient("http://test-url", "username", Browser.default);
-      expect(telemetries.length).to.deep.equals(0);
-    });
-
-    it("happy path others", async () => {
-      sandbox.stub(process, "platform").value("linux");
-      sandbox
-        .stub(commonUtils, "openBrowser")
-        .callsFake(async (browser, url, browserArguments) => {});
-      await openTeamsDesktopClient("http://test-url", "username", Browser.default);
-      expect(telemetries.length).to.deep.equals(0);
-    });
-
-    it("openBrowser error", async () => {
-      sandbox.stub(process, "platform").value("linux");
-      sandbox.stub(commonUtils, "openBrowser").throws();
-      await openTeamsDesktopClient("http://test-url", "username", Browser.default);
-      expect(telemetries.length).to.deep.equals(0);
-    });
-  });
-});
-
 class MockProgressHandler implements IProgressHandler {
   start(detail?: string): Promise<void> {
     return Promise.resolve();

b79451c94ae65f79c34ceb31a6167ae778fbd350
huajiezhang <huajiezhang@microsoft.com>
2024-06-19 15:43:21 +0800

    refactor: ut

--- packages/vscode-extension/test/handlers/lifecycleHandlers.test.ts
+++ packages/vscode-extension/test/handlers/lifecycleHandlers.test.ts
@@ -1,9 +1,11 @@
-import { err } from "@microsoft/teamsfx-api";
+import { err, Platform } from "@microsoft/teamsfx-api";
 import { UserCancelError } from "@microsoft/teamsfx-core";
 import { assert } from "chai";
 import * as sinon from "sinon";
 import { provisionHandler } from "../../src/handlers/lifecycleHandlers";
 import * as shared from "../../src/handlers/sharedOpts";
+import { processResult } from "../../src/handlers/sharedOpts";
+import * as telemetryUtils from "../../src/utils/telemetryUtils";
 
 describe("Lifecycle handlers", () => {
   const sandbox = sinon.createSandbox();
@@ -18,6 +20,19 @@ describe("Lifecycle handlers", () => {
       sandbox.stub(shared, "runCommand").resolves(err(new UserCancelError()));
       const res = await provisionHandler();
       assert.isTrue(res.isErr());
+    });
+  });
+
+  describe("processResult", () => {
+    it("UserCancelError", async () => {
+      sandbox.stub(telemetryUtils, "getTeamsAppTelemetryInfoByEnv").resolves({
+        appId: "mockId",
+        tenantId: "mockTenantId",
+      });
+      await processResult("", err(new UserCancelError()), {
+        platform: Platform.VSCode,
+        env: "dev",
+      });
     });
   });
 });

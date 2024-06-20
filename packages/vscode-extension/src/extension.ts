2b135ccd1ee175b32e6f094f8c4bf03d9bc2eccf
Huajie Zhang <huajiezhang@microsoft.com>
2024-06-20 13:46:46 +0800

    refactor: move create new project handler to seperate file (#11862)
    * refactor: move create project handler
    
    * refactor: move create project handler
    
    * refactor: ut
    
    * refactor: ut
    
    * refactor: ut
    
    * ci: script

--- packages/vscode-extension/src/extension.ts
+++ packages/vscode-extension/src/extension.ts
@@ -81,7 +81,12 @@ import { checkSideloadingCallback } from "./handlers/checkSideloading";
 import * as copilotChatHandlers from "./handlers/copilotChatHandlers";
 import { debugInTestToolHandler } from "./handlers/debugInTestTool";
 import { downloadSampleApp } from "./handlers/downloadSample";
-import { deployHandler, provisionHandler, publishHandler } from "./handlers/lifecycleHandlers";
+import {
+  createNewProjectHandler,
+  deployHandler,
+  provisionHandler,
+  publishHandler,
+} from "./handlers/lifecycleHandlers";
 import * as officeDevHandlers from "./handlers/officeDevHandlers";
 import {
   openAccountLinkHandler,
@@ -294,7 +299,7 @@ function registerActivateCommands(context: vscode.ExtensionContext) {
   registerInCommandController(
     context,
     CommandKeys.Create,
-    handlers.createNewProjectHandler,
+    createNewProjectHandler,
     "createProject"
   );
   context.subscriptions.push(

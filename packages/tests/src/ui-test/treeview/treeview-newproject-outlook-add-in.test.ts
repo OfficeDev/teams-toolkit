b85078df0be07fdee5af8623ff946e36499e8f99
Helly Zhang <v-helzha@microsoft.com>
2024-06-21 09:11:37 +0800

    test: remove node version check

--- packages/tests/src/ui-test/treeview/treeview-newproject-outlook-add-in.test.ts
+++ packages/tests/src/ui-test/treeview/treeview-newproject-outlook-add-in.test.ts
@@ -1,6 +1,6 @@
-/**
- * @author Darren Miller <darrmill@microsoft.com>
- */
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import * as path from "path";
 import * as fs from "fs-extra";
 import { expect } from "chai";
@@ -8,13 +8,11 @@ import { Timeout } from "../../utils/constants";
 import { TreeViewTestContext } from "./treeviewContext";
 import { createNewProject } from "../../utils/vscodeOperation";
 import { it } from "../../utils/it";
-import { getNodeVersion } from "../../utils/getNodeVersion";
 
 describe("New project Tests", function () {
   this.timeout(Timeout.testCase);
   let treeViewTestContext: TreeViewTestContext;
   let testRootFolder: string;
-  let nodeVersion: string | null;
   const appNameCopySuffix = "copy";
   let newAppFolderName: string;
   let projectPath: string;
@@ -24,7 +22,6 @@ describe("New project Tests", function () {
     this.timeout(Timeout.prepareTestCase);
     treeViewTestContext = new TreeViewTestContext("treeview");
     testRootFolder = treeViewTestContext.testRootFolder;
-    nodeVersion = await getNodeVersion();
     await treeViewTestContext.before();
   });
 

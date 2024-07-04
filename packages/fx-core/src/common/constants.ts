09e336cde609ece98ea499735849b9e2d0a40a6c
huajiezhang <huajiezhang@microsoft.com>
2024-07-04 14:00:36 +0800

    refactor: retire new generator feature flag

--- packages/fx-core/src/common/constants.ts
+++ packages/fx-core/src/common/constants.ts
@@ -49,7 +49,6 @@ export class FeatureFlagName {
   static readonly AsyncAppValidation = "TEAMSFX_ASYNC_APP_VALIDATION";
   static readonly NewProjectType = "TEAMSFX_NEW_PROJECT_TYPE";
   static readonly ChatParticipant = "TEAMSFX_CHAT_PARTICIPANT";
-  static readonly NewGenerator = "TEAMSFX_NEW_GENERATOR";
   static readonly SMEOAuth = "SME_OAUTH";
   static readonly CustomizeGpt = "TEAMSFX_DECLARATIVE_COPILOT";
 }

9389d4dc72f7306fa423035bc5013060974ba4ec
Siyuan Chen <67082457+ayachensiyuan@users.noreply.github.com>
2024-07-09 20:28:14 +0800

    test: update migration case clean up (#11984)
    * test: update migration case clean up
    
    * test: update sample large scale case clean up
    
    ---------
    
    Co-authored-by: Ivan_Chen <v-ivanchen@microsoft.com>

--- packages/tests/src/ui-test/migration/4.0.0-notification-bot-restify/4.0.0-notification-bot-restify-provision-upgrade-provision-debug-ts.test.ts
+++ packages/tests/src/ui-test/migration/4.0.0-notification-bot-restify/4.0.0-notification-bot-restify-provision-upgrade-provision-debug-ts.test.ts
@@ -23,6 +23,7 @@ import {
   validateNotification,
   upgradeByTreeView,
   validateUpgrade,
+  execCommandIfExist,
 } from "../../../utils/vscodeOperation";
 import {
   CLIVersionCheck,
@@ -55,7 +56,20 @@ describe("Migration Tests", function () {
 
   afterEach(async function () {
     this.timeout(Timeout.finishTestCase);
-    await mirgationDebugTestContext.after(false, true, "dev");
+    await mirgationDebugTestContext.after(true, true, "dev");
+
+    //Close the folder and cleanup local sample project
+    await execCommandIfExist("Workspaces: Close Workspace", Timeout.webView);
+    console.log(
+      `[Successfully] start to clean up for ${mirgationDebugTestContext.projectPath}`
+    );
+    await mirgationDebugTestContext.cleanUp(
+      mirgationDebugTestContext.appName,
+      mirgationDebugTestContext.projectPath,
+      true,
+      true,
+      false
+    );
   });
 
   it(

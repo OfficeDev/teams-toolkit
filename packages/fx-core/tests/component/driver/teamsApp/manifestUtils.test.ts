694499b73ef3b298d8f6ed835eef5388826a51e8
Chenyi An <anchenyi@microsoft.com>
2024-07-19 16:18:32 +0800

    fix: ut

--- packages/fx-core/tests/component/driver/teamsApp/manifestUtils.test.ts
+++ packages/fx-core/tests/component/driver/teamsApp/manifestUtils.test.ts
@@ -10,12 +10,14 @@ import {
   ok,
   Platform,
   ManifestCapability,
+  IBot,
 } from "@microsoft/teamsfx-api";
 import {
   getBotsTplBasedOnVersion,
   getConfigurableTabsTplBasedOnVersion,
   getConfigurableTabsTplExistingAppBasedOnVersion,
 } from "../../../../src/component/driver/teamsApp/constants";
+import { AppStudioError } from "../../../../src/component/driver/teamsApp/errors";
 
 const defaultManifestVersion = "1.17";
 const oldManifestVersion = "1.16";
@@ -68,6 +70,20 @@ describe("ManifestUtils", () => {
     const capabilities: ManifestCapability[] = [{ name: "configurableTab" }];
     const result = await manifestUtils.addCapabilities(inputs, capabilities);
     assert.isTrue(result.isOk());
+  });
+  it("should add a configurable tabs capability - exceed limit", async () => {
+    mockInputManifestFileExceedLimit(manifestUtils, defaultManifestVersion);
+    const inputs: InputsWithProjectPath = {
+      projectPath: "path/to/project",
+      addManifestPath: "path/to/manifest.json",
+      platform: Platform.CLI,
+    };
+    const capabilities: ManifestCapability[] = [{ name: "configurableTab" }];
+    const result = await manifestUtils.addCapabilities(inputs, capabilities);
+    assert.isTrue(result.isErr());
+    if (result.isErr()) {
+      assert.isTrue(result.error.name.includes(AppStudioError.CapabilityExceedLimitError.name));
+    }
   });
   it("should add a configurable tabs capability - existing app", async () => {
     mockInputManifestFile(manifestUtils, defaultManifestVersion);
@@ -132,10 +148,32 @@ describe("ManifestUtils", () => {
     const result = await manifestUtils.addCapabilities(inputs, capabilities);
     assert.isTrue(result.isOk());
   });
+  it("should add a bot capability - snippet", async () => {
+    mockInputManifestFile(manifestUtils, defaultManifestVersion);
+    const snippet: IBot = {
+      botId: "test",
+      scopes: ["personal"],
+    };
+    sinon.stub(fs, "writeFile").callsFake((path: any, data: string) => {
+      const writtenManifest = JSON.parse(data) as TeamsAppManifest;
+      assert.isArray(writtenManifest.bots);
+      assert.isNotEmpty(writtenManifest.bots);
+      assert.deepEqual(writtenManifest.bots![0], snippet);
+      return Promise.resolve();
+    });
+    const inputs: InputsWithProjectPath = {
+      projectPath: "path/to/project",
+      addManifestPath: "path/to/manifest.json",
+      platform: Platform.CLI,
+    };
+    const capabilities: ManifestCapability[] = [{ name: "Bot", snippet: snippet }];
+    const result = await manifestUtils.addCapabilities(inputs, capabilities);
+    assert.isTrue(result.isOk());
+  });
 });
 
 function mockInputManifestFile(manifestUtils: ManifestUtils, manifestVersion: string) {
-  const mockManifest = {
+  const mockManifest: TeamsAppManifest = {
     $schema:
       "https://developer.microsoft.com/en-us/json-schemas/teams/v1.17/MicrosoftTeams.schema.json",
     manifestVersion: manifestVersion,
@@ -154,6 +192,36 @@ function mockInputManifestFile(manifestUtils: ManifestUtils, manifestVersion: st
       outline: "https://your-app-outline-icon.png",
     },
     accentColor: "#FFFFFF",
+  };
+  sinon.stub(manifestUtils, "_readAppManifest").resolves(ok(mockManifest));
+}
+
+function mockInputManifestFileExceedLimit(manifestUtils: ManifestUtils, manifestVersion: string) {
+  const mockManifest: TeamsAppManifest = {
+    $schema:
+      "https://developer.microsoft.com/en-us/json-schemas/teams/v1.17/MicrosoftTeams.schema.json",
+    manifestVersion: manifestVersion,
+    version: "1.0.0",
+    id: "test-id",
+    developer: {
+      name: "Test Name",
+      websiteUrl: "https://your-website.com",
+      privacyUrl: "https://your-privacy-url.com",
+      termsOfUseUrl: "https://your-terms-of-use-url.com",
+    },
+    name: { short: "Test App Name" },
+    description: { short: "Test app description" },
+    icons: {
+      color: "https://your-app-color-icon.png",
+      outline: "https://your-app-outline-icon.png",
+    },
+    accentColor: "#FFFFFF",
+    configurableTabs: [
+      {
+        configurationUrl: "https://test.com",
+        scopes: ["team"],
+      },
+    ],
   };
   sinon.stub(manifestUtils, "_readAppManifest").resolves(ok(mockManifest));
 }

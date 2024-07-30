22ab9ce42975d51be60ea9b6321a77733384b411
Huajie Zhang <huajiezhang@microsoft.com>
2024-07-30 14:49:46 +0800

    fix: mask secret for progress title of script action (#12100)
    * feat: mask secret in script progress title
    
    * fix: use new mask secret
    
    * test: ut
    
    * test: ut
    
    * test: ut
    
    * test: ut

--- packages/fx-core/tests/component/util/stringUtils.test.ts
+++ packages/fx-core/tests/component/util/stringUtils.test.ts
@@ -6,9 +6,9 @@
 
 import "mocha";
 import { assert } from "chai";
-import { maskSecretValues } from "../../../src/common/stringUtils";
+import { maskSecretFromEnv } from "../../../src/common/stringUtils";
 
-describe("stringUtils.maskSecretValues", () => {
+describe("stringUtils.maskSecretFromEnv", () => {
   afterEach(() => {
     delete process.env["SECRET_KEY"];
     delete process.env["NON_SECRET_KEY"];
@@ -18,33 +18,33 @@ describe("stringUtils.maskSecretValues", () => {
     process.env["SECRET_KEY"] = "secretValue";
     process.env["NON_SECRET_KEY"] = "This is a";
     const stdout = "This is a secretValue";
-    const maskedStdout = maskSecretValues(stdout);
+    const maskedStdout = maskSecretFromEnv(stdout, "***");
     assert.equal(maskedStdout, "This is a ***");
   });
 
   it("should not mask non-secret values in stdout", () => {
     process.env["NON_SECRET_KEY"] = "nonSecretValue";
     const stdout = "This is a nonSecretValue";
-    const maskedStdout = maskSecretValues(stdout);
+    const maskedStdout = maskSecretFromEnv(stdout, "***");
     assert.equal(maskedStdout, stdout);
   });
 
   it("should not mask secret values if they are not in stdout", () => {
     process.env["SECRET_KEY"] = "secretValue";
     const stdout = "This is a stdout";
-    const maskedStdout = maskSecretValues(stdout);
+    const maskedStdout = maskSecretFromEnv(stdout, "***");
     assert.equal(maskedStdout, stdout);
   });
 
   it("should not mask secret values if they are not in process.env", () => {
     const stdout = "This is a secretValue";
-    const maskedStdout = maskSecretValues(stdout);
+    const maskedStdout = maskSecretFromEnv(stdout, "***");
     assert.equal(maskedStdout, stdout);
   });
 
   it("contains secret value but is blank", () => {
     process.env["SECRET_KEY"] = "";
-    const maskedStdout = maskSecretValues("This is a secretValue");
+    const maskedStdout = maskSecretFromEnv("This is a secretValue", "***");
     assert.equal(maskedStdout, "This is a secretValue");
   });
 });

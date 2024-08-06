89398ed8a9cb54652e0d849c81fc2e5c29cb3537
Chenyi An <anchenyi@microsoft.com>
2024-08-06 16:50:31 +0800

    feat: enable async app validation

--- packages/fx-core/src/common/featureFlags.ts
+++ packages/fx-core/src/common/featureFlags.ts
@@ -58,7 +58,7 @@ export class FeatureFlags {
   };
   static readonly AsyncAppValidation = {
     name: FeatureFlagName.AsyncAppValidation,
-    defaultValue: "false",
+    defaultValue: "true",
   };
   static readonly NewProjectType = { name: FeatureFlagName.NewProjectType, defaultValue: "true" };
   static readonly ChatParticipant = {

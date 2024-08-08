f8ce396ac63d0df61d81dd4e738f43128ce2e6c5
JerryYang <53805504+JerryYangKai@users.noreply.github.com>
2024-08-08 10:29:53 +0800

    fix: upgrade ai bot ts/js template based on Teams store policy (#12182)
    * fix: upgrade ai bot tsjs template based on Teams store policy
    
    * fix: upgrade ai bot tsjs template based on Teams store policy

--- templates/ts/ai-bot/src/app.ts
+++ templates/ts/ai-bot/src/app.ts
@@ -1,4 +1,4 @@
-import { MemoryStorage } from "botbuilder";
+import { MemoryStorage, MessageFactory, TurnContext } from "botbuilder";
 import * as path from "path";
 
 // See https://aka.ms/teams-ai-library to learn more about the Teams AI library.
@@ -36,6 +36,15 @@ const app = new Application({
   ai: {
     planner,
   },
+});
+
+app.conversationUpdate("membersAdded", async (turnContext: TurnContext) => {
+  const welcomeText = "How can I help you today?";
+  for (const member of turnContext.activity.membersAdded) {
+    if (member.id !== turnContext.activity.recipient.id) {
+      await turnContext.sendActivity(MessageFactory.text(welcomeText));
+    }
+  }
 });
 
 export default app;

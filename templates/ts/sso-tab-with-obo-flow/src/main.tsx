f4c5c655eb894b930de8b849cb952e6fb60ce2fd
Hui Miao <miuhui@outlook.com>
2024-08-25 23:09:22 +0800

    fix: migrate the obo tab template to vite

--- templates/ts/sso-tab-with-obo-flow/src/main.tsx
+++ templates/ts/sso-tab-with-obo-flow/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
-import "./index.css";
+import "./main.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);

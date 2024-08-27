829bcb4ae3bbc30087aec600316fc877a6fd47eb
Ivan_Chen <v-ivanchen@microsoft.com>
2024-08-27 15:04:44 +0800

    test: add meeting validation

--- packages/tests/src/ui-test/samples/sample-remotedebug-hello-world-meeting.test.ts
+++ packages/tests/src/ui-test/samples/sample-remotedebug-hello-world-meeting.test.ts
@@ -37,7 +37,7 @@ class MyFirstMeetingTestCase extends CaseFactory {
   }
 
   override async onValidate(page: Page): Promise<void> {
-    return await validateMeeting(page, Env.displayName);
+    return await validateMeeting(page, Env.username);
   }
 }
 

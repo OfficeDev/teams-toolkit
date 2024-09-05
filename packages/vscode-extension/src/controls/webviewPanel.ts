f7973ea1beb57f8f216fb58d1a28dae35e793516
Ning Tang <nintan@microsoft.com>
2024-09-05 09:23:53 +0800

    fix(vsc): repetitive images in sample detail page (#12329)

--- packages/vscode-extension/src/controls/webviewPanel.ts
+++ packages/vscode-extension/src/controls/webviewPanel.ts
@@ -283,9 +283,9 @@ export class WebviewPanel {
 
   private replaceRelativeImagePaths(htmlContent: string, sample: SampleConfig) {
     const urlInfo = sample.downloadUrlInfo;
-    const imageUrl = `https://github.com/${urlInfo.owner}/${urlInfo.repository}/blob/${urlInfo.ref}/${urlInfo.dir}/${sample.thumbnailPath}?raw=1`;
+    const imageUrl = `https://github.com/${urlInfo.owner}/${urlInfo.repository}/blob/${urlInfo.ref}/${urlInfo.dir}/`;
     const imageRegex = /img\s+src="(?!https:\/\/camo\.githubusercontent\.com\/.)([^"]+)"/gm;
-    return htmlContent.replace(imageRegex, `img src="${imageUrl}"`);
+    return htmlContent.replace(imageRegex, `img src="${imageUrl}$1?raw=1"`);
   }
 
   private replaceMermaidRelatedContent(htmlContent: string): string {

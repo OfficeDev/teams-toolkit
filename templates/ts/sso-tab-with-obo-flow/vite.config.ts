cb14cccedddd6879a1c40caa69e10611f4520866
Yimin-Jin <yiminjin@microsoft.com>
2024-09-11 16:16:22 +0800

    refactor: modify the vars import method to vite

--- templates/ts/sso-tab-with-obo-flow/vite.config.ts
+++ templates/ts/sso-tab-with-obo-flow/vite.config.ts
@@ -1,30 +1,14 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "fs";
 
-const cherryPickedKeys = [
-  "REACT_APP_CLIENT_ID",
-  "REACT_APP_START_LOGIN_PAGE_URL",
-  "REACT_APP_FUNC_NAME",
-  "REACT_APP_FUNC_ENDPOINT",
-];
-
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  const processEnv = {};
-  cherryPickedKeys.forEach((key) => (processEnv[key] = env[key]));
-
-  return {
-    define: {
-      "process.env": processEnv,
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 53000,
+    https: {
+      cert: process.env.SSL_CRT_FILE ? fs.readFileSync(process.env.SSL_CRT_FILE) : undefined,
+      key: process.env.SSL_KEY_FILE ? fs.readFileSync(process.env.SSL_KEY_FILE) : undefined,
     },
-    plugins: [react()],
-    server: {
-      port: 53000,
-      https: {
-        cert: process.env.SSL_CRT_FILE ? fs.readFileSync(process.env.SSL_CRT_FILE) : undefined,
-        key: process.env.SSL_KEY_FILE ? fs.readFileSync(process.env.SSL_KEY_FILE) : undefined,
-      },
-    },
-  };
+  },
 });

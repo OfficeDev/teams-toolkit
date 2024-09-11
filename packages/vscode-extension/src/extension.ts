53d51477ff84b44dc5a790e20bf0adadbe49c94b
Gavin Gu <gavingu@microsoft.com>
2024-09-11 16:23:08 +0800

    refactor: redesign office chat feature flag

--- packages/vscode-extension/src/extension.ts
+++ packages/vscode-extension/src/extension.ts
@@ -177,6 +177,7 @@ import {
   CHAT_CREATE_OFFICE_PROJECT_COMMAND_ID,
   officeChatParticipantId,
 } from "./officeChat/consts";
+import * as extensionPackage from "../package.json";
 import {
   chatCreateOfficeProjectCommandHandler,
   handleOfficeFeedback,
@@ -201,8 +202,11 @@ import { getSettingsVersion, projectVersionCheck } from "./utils/telemetryUtils"
 import { createPluginWithManifest } from "./handlers/createPluginWithManifestHandler";
 
 export async function activate(context: vscode.ExtensionContext) {
-  const value = IsChatParticipantEnabled && semver.gte(vscode.version, "1.90.0");
-  featureFlagManager.setBooleanValue(FeatureFlags.ChatParticipant, value);
+  const value =
+    (featureFlagManager.getBooleanValue(CoreFeatureFlags.OfficeChatParticipant) ||
+      extensionPackage.version.includes("alpha")) &&
+    semver.gte(vscode.version, "1.90.0");
+  featureFlagManager.setBooleanValue(CoreFeatureFlags.OfficeChatParticipant, value);
 
   context.subscriptions.push(new ExtTelemetry.Reporter(context));
 
@@ -219,10 +223,6 @@ export async function activate(context: vscode.ExtensionContext) {
   registerActivateCommands(context);
 
   registerInternalCommands(context);
-
-  if (featureFlagManager.getBooleanValue(CoreFeatureFlags.ChatParticipant)) {
-    registerOfficeChatParticipant(context);
-  }
 
   if (isTeamsFxProject) {
     activateTeamsFxRegistration(context);
@@ -247,6 +247,17 @@ export async function activate(context: vscode.ExtensionContext) {
     "fx-extension.isChatParticipantUIEntriesEnabled",
     featureFlagManager.getBooleanValue(CoreFeatureFlags.ChatParticipantUIEntries)
   );
+
+  // control whether to show office chat participant entry in chat
+  await vscode.commands.executeCommand(
+    "setContext",
+    "fx-extension.isOfficeChatParticipantEnabled",
+    featureFlagManager.getBooleanValue(CoreFeatureFlags.OfficeChatParticipant)
+  );
+
+  if (featureFlagManager.getBooleanValue(CoreFeatureFlags.OfficeChatParticipant)) {
+    registerOfficeChatParticipant(context);
+  }
 
   // Flags for "Build Intelligent Apps" walkthrough.
   // DEVEOP_COPILOT_PLUGIN: boolean in vscode settings

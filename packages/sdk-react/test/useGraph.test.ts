ea153ff7ddbeaf4aa546ab0a7b565e55d547ee56
Yiqing Zhao <yiqingzhao@microsoft.com>
2024-09-26 12:07:21 +0800

    test: fix unit test in sdk-react

--- packages/sdk-react/test/useGraph.test.ts
+++ packages/sdk-react/test/useGraph.test.ts
@@ -7,153 +7,11 @@
 
 import { renderHook, act, waitFor } from "@testing-library/react";
 import { Client, GraphError } from "@microsoft/microsoft-graph-client";
-import { useGraph, useGraphWithCredential } from "../src/useGraph";
-import { TeamsFx, ErrorWithCode, ErrorCode } from "@microsoft/teamsfx";
+import { useGraphWithCredential } from "../src/useGraph";
+import { ErrorWithCode, ErrorCode, UserInfo } from "@microsoft/teamsfx";
 import * as teamsfxlib from "@microsoft/teamsfx";
 
 require("isomorphic-fetch");
-
-describe("useGraph() hook tests", () => {
-  let spyTeamsFxLogin: jest.SpyInstance;
-
-  beforeEach(() => {
-    spyTeamsFxLogin = jest.spyOn(TeamsFx.prototype, "login");
-  });
-
-  afterEach(() => {
-    jest.resetAllMocks();
-    jest.clearAllMocks();
-  });
-
-  it("call function after initialized", async () => {
-    let authenticatedGraph: Client | undefined;
-    let graphScope: string[] | undefined;
-    const { result } = renderHook(() =>
-      useGraph((graph: Client, teamsfx: TeamsFx, scope: string[]) => {
-        authenticatedGraph = graph;
-        graphScope = scope;
-        return Promise.resolve("graph data");
-      })
-    );
-    expect(result.current.reload).toBeDefined();
-    expect(result.current.data).toBe(undefined);
-    expect(result.current.error).toBe(undefined);
-    expect(result.current.loading).toBe(true);
-
-    await waitFor(
-      () => {
-        expect(result.current.data).toBe("graph data");
-        expect(result.current.error).toBe(undefined);
-        expect(result.current.loading).toBe(false);
-      },
-      { interval: 1 }
-    );
-
-    expect((authenticatedGraph as any).config.authProvider.credentialOrTeamsFx).toBeInstanceOf(
-      TeamsFx
-    );
-    expect(graphScope && graphScope[0]).toBe("User.Read");
-  });
-
-  it("call login() automatically when user has not consented", async () => {
-    let authenticatedGraph: Client | undefined;
-    let graphScope: string[] | undefined;
-    let callTime = 0;
-    spyTeamsFxLogin.mockImplementation(() => {
-      return Promise.resolve();
-    });
-    const { result } = renderHook(() =>
-      useGraph((graph: Client, teamsfx: TeamsFx, scope: string[]) => {
-        authenticatedGraph = graph;
-        graphScope = scope;
-        if (callTime === 0) {
-          callTime++;
-          const error = new GraphError();
-          error.code = ErrorCode.UiRequiredError;
-          return Promise.reject(error);
-        } else {
-          return Promise.resolve("graph data");
-        }
-      })
-    );
-    expect(result.current.reload).toBeDefined();
-    expect(result.current.data).toBe(undefined);
-    expect(result.current.error).toBe(undefined);
-    expect(result.current.loading).toBe(true);
-
-    await waitFor(
-      () => {
-        expect(result.current.data).toBe(undefined);
-        expect(result.current.error).toBe(undefined);
-        expect(result.current.loading).toBe(false);
-      },
-      { interval: 1 }
-    );
-
-    expect((authenticatedGraph as any).config.authProvider.credentialOrTeamsFx).toBeInstanceOf(
-      TeamsFx
-    );
-    expect(graphScope && graphScope[0]).toBe("User.Read");
-
-    act(() => result.current.reload());
-    await waitFor(
-      () => {
-        expect(result.current.data).toBe("graph data");
-      },
-      { interval: 1 }
-    );
-  });
-
-  it("shows error message when user cancels consent dialog", async () => {
-    let authenticatedGraph: Client | undefined;
-    let graphScope: string[] | undefined;
-    const { result } = renderHook(() =>
-      useGraph((graph: Client, teamsfx: TeamsFx, scope: string[]) => {
-        authenticatedGraph = graph;
-        graphScope = scope;
-        const error = new GraphError();
-        error.code = ErrorCode.UiRequiredError;
-        return Promise.reject(error);
-      })
-    );
-    spyTeamsFxLogin.mockImplementation(() => {
-      throw new ErrorWithCode("CancelledByUser");
-    });
-    expect(result.current.reload).toBeDefined;
-    expect(result.current.data).toBe(undefined);
-    expect(result.current.error).toBe(undefined);
-    expect(result.current.loading).toBe(true);
-
-    await waitFor(
-      () => {
-        expect(result.current.data).toBe(undefined);
-        expect(result.current.error).toBe(undefined);
-        expect(result.current.loading).toBe(false);
-      },
-      { interval: 1 }
-    );
-
-    expect((authenticatedGraph as any).config.authProvider.credentialOrTeamsFx).toBeInstanceOf(
-      TeamsFx
-    );
-    expect(graphScope && graphScope[0]).toBe("User.Read");
-
-    act(() => result.current.reload());
-    await waitFor(
-      () => {
-        expect(result.current.data).toBe(undefined);
-        expect(result.current.error).toBeDefined;
-        expect((result.current.error as ErrorWithCode).message).toBe(
-          "CancelledByUser" +
-            '\nIf you see "AADSTS50011: The reply URL specified in the request does not match the reply URLs configured for the application" ' +
-            "in the popup window, you may be using unmatched version for TeamsFx SDK (version >= 0.5.0) and Teams Toolkit (version < 3.3.0) or " +
-            `cli (version < 0.11.0). Please refer to the help link for how to fix the issue: https://aka.ms/teamsfx-auth-code-flow`
-        );
-      },
-      { interval: 1 }
-    );
-  });
-});
 
 describe("useGraphWithCredential() hook tests", () => {
   beforeEach(() => {});
@@ -166,22 +24,21 @@ describe("useGraphWithCredential() hook tests", () => {
   it("call function after initialized", async () => {
     jest
       .spyOn(teamsfxlib, "TeamsUserCredential")
-      .mockImplementation(
-        (authConfig: teamsfxlib.AuthenticationConfiguration): teamsfxlib.TeamsUserCredential => {
-          return {
-            async login(): Promise<any> {},
-            async getToken(): Promise<any> {},
-            async getUserInfo(): Promise<any> {},
-          };
-        }
-      );
-
-    let authenticatedGraph: Client | undefined;
+      .mockImplementation((): teamsfxlib.TeamsUserCredential => {
+        return {
+          async login(): Promise<void> {},
+          async getToken(): Promise<null> {
+            return null;
+          },
+          async getUserInfo(): Promise<UserInfo> {
+            return {} as UserInfo;
+          },
+        };
+      });
     let graphScope: string[] | undefined;
     const { result } = renderHook(() =>
       useGraphWithCredential(
         (graph: Client, credential: teamsfxlib.TeamsUserCredential, scope: string[]) => {
-          authenticatedGraph = graph;
           graphScope = scope;
           return Promise.resolve("graph data");
         }
@@ -208,19 +65,21 @@ describe("useGraphWithCredential() hook tests", () => {
       .spyOn(teamsfxlib, "TeamsUserCredential")
       .mockImplementation((): teamsfxlib.TeamsUserCredential => {
         return {
-          async login(): Promise<any> {},
-          async getToken(): Promise<any> {},
-          async getUserInfo(): Promise<any> {},
+          async login(): Promise<void> {},
+          async getToken(): Promise<null> {
+            return null;
+          },
+          async getUserInfo(): Promise<UserInfo> {
+            return {} as UserInfo;
+          },
         };
       });
-    let authenticatedGraph: Client | undefined;
     let graphScope: string[] | undefined;
     let callTime = 0;
 
     const { result } = renderHook(() =>
       useGraphWithCredential(
         (graph: Client, credential: teamsfxlib.TeamsUserCredential, scope: string[]) => {
-          authenticatedGraph = graph;
           graphScope = scope;
           if (callTime === 0) {
             callTime++;
@@ -262,19 +121,21 @@ describe("useGraphWithCredential() hook tests", () => {
       .spyOn(teamsfxlib, "TeamsUserCredential")
       .mockImplementation((): teamsfxlib.TeamsUserCredential => {
         return {
-          async login(): Promise<any> {
+          async login(): Promise<void> {
             throw new ErrorWithCode("CancelledByUser");
           },
-          async getToken(): Promise<any> {},
-          async getUserInfo(): Promise<any> {},
+          async getToken(): Promise<null> {
+            return null;
+          },
+          async getUserInfo(): Promise<UserInfo> {
+            return {} as UserInfo;
+          },
         };
       });
-    let authenticatedGraph: Client | undefined;
     let graphScope: string[] | undefined;
     const { result } = renderHook(() =>
       useGraphWithCredential(
         (graph: Client, credential: teamsfxlib.TeamsUserCredential, scope: string[]) => {
-          authenticatedGraph = graph;
           graphScope = scope;
           const error = new GraphError();
           error.code = ErrorCode.UiRequiredError;

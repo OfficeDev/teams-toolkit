1c66d545c09c4a5e4c5e66ec9debdc0bbd8c200b
Siglud <siglud@gmail.com>
2024-10-24 14:48:49 +0800

    fix: add e2e test for storage driver

---
+++ packages/tests/src/e2e/bot/DeployToAzureStorageHappyPath.ts
@@ -0,0 +1,143 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+/**
+ * @author Siglud <siglud@gmail.com>
+ */
+
+import { describe } from "mocha";
+import {
+  cleanUp,
+  createResourceGroup,
+  execAsyncWithRetry,
+  getTestFolder,
+  getUniqueAppName,
+} from "../commonUtils";
+import * as path from "path";
+import * as fs from "fs";
+import { getUuid } from "../../commonlib";
+import { expect } from "chai";
+import { Executor } from "../../utils/executor";
+import { environmentNameManager } from "@microsoft/teamsfx-core";
+
+describe("Provision and deploy a Azure Storage", async function () {
+  // create a project with Azure Storage
+  const testFolder = getTestFolder();
+  const appName = getUniqueAppName();
+  const projectPath = path.resolve(testFolder, appName);
+  const projectId = getUuid();
+  const envName = environmentNameManager.getDefaultEnvName();
+
+  it("should be provision without problem", async () => {
+    // write teamsapp.yml
+    fs.writeFileSync(
+      path.join(projectPath, "teamsapp.yml"),
+      String.raw`
+# yaml-language-server: $schema=https://aka.ms/teams-toolkit/v1.7/yaml.schema.json
+version: v1.7
+
+environmentFolderPath: ./env
+
+provision:
+  - uses: arm/deploy
+    with:
+      subscriptionId: \${{AZURE_SUBSCRIPTION_ID}}
+      resourceGroupName: \${{AZURE_RESOURCE_GROUP_NAME}}
+      templates:
+        - path: ./infra/azure.bicep
+          parameters: ./infra/azure.parameters.json
+          deploymentName: Create-resources-for-tab
+      bicepCliVersion: v0.9.1
+  - uses: azureStorage/enableStaticWebsite
+    with:
+      storageResourceId: \${{TAB_AZURE_STORAGE_RESOURCE_ID}}
+      indexPage: index.html
+      errorPage: error.html
+
+deploy:
+  - uses: azureStorage/deploy
+    with:
+      artifactFolder: build
+      resourceId: \${{TAB_AZURE_STORAGE_RESOURCE_ID}}
+projectId: ${projectId}`
+    );
+    // mkdir for infra
+    fs.mkdirSync(path.join(projectPath, "infra"), { recursive: true });
+    // mkdir for env
+    fs.mkdirSync(path.join(projectPath, "env"), { recursive: true });
+    // write azure.bicep
+    fs.writeFileSync(
+      path.join(projectPath, "infra", "azure.bicep"),
+      String.raw`
+param resourceBaseName string
+
+resource storage 'Microsoft.Storage/storageAccounts@2021-06-01' = {
+  kind: 'StorageV2'
+  location: resourceGroup().location
+  name: resourceBaseName
+  properties: {
+    supportsHttpsTrafficOnly: true
+  }
+  sku: {
+    name: 'Standard_LRS'
+  }
+}
+
+output TAB_AZURE_STORAGE_RESOURCE_ID string = storage.id
+output TAB_DOMAIN string = storage.properties.primaryEndpoints.web`
+    );
+    // write azure.parameters.json
+    fs.writeFileSync(
+      path.join(projectPath, "infra", "azure.parameters.json"),
+      String.raw`{
+  "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#",
+  "contentVersion": "1.0.0.0",
+  "parameters": {
+    "resourceBaseName": {
+      "value": "helloWorld\${{RESOURCE_SUFFIX}}"
+    }
+  }
+}`
+    );
+    // write .env.dev
+    fs.writeFileSync(
+      path.join(projectPath, "env", ".env.dev"),
+      "TEAMSFX_ENV=dev\n" + "APP_NAME_SUFFIX=dev\n" + "RESOURCE_SUFFIX=381cc5"
+    );
+    // write build and build/index.html
+    fs.mkdirSync(path.join(projectPath, "build"), { recursive: true });
+    fs.writeFileSync(
+      path.join(projectPath, "build", "index.html"),
+      "<h1>Hello World</h1>"
+    );
+
+    // run provision
+    const result = await createResourceGroup(appName + "-rg", "westus");
+    expect(result).to.be.true;
+    process.env["AZURE_RESOURCE_GROUP_NAME"] = appName + "-rg";
+    const { success } = await Executor.provision(projectPath, envName);
+    expect(success).to.be.true;
+    console.log(`[Successfully] provision for ${projectPath}`);
+
+    const env = Object.assign({}, process.env);
+
+    // deploy
+    const cmdStr = "teamsapp deploy";
+    await execAsyncWithRetry(cmdStr, {
+      cwd: projectPath,
+      env: env,
+      timeout: 0,
+    });
+
+    console.log(`[Successfully] deploy for ${projectPath}`);
+
+    // request to index.html to check the deployment result
+    const TAB_DOMAIN = process.env["TAB_DOMAIN"];
+    const response = await fetch(`https://${TAB_DOMAIN}`);
+    expect(response.status).to.equal(200);
+  });
+
+  this.afterEach(async function () {
+    console.log(`[Successfully] start to clean up for ${projectPath}`);
+    await cleanUp(appName, projectPath, false, true, false, envName, undefined);
+  });
+});

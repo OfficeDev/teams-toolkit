ffb8b6f1119125778adff06c0e3562174e0afd2b
Hui Miao <miuhui@outlook.com>
2024-10-28 16:50:17 +0800

    test: add e2e tests for declarative agent

---
+++ packages/tests/src/e2e/copilotExtensions/DeclarativeAgentBasic.tests.ts
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+/**
+ * @author Hui Miao <huimaio@microsoft.com>
+ */
+
+import { Capability } from "../../utils/constants";
+import { ProgrammingLanguage } from "@microsoft/teamsfx-core";
+import { CaseFactory } from "../caseFactory";
+
+class DeclarativeAgentBasicTestCase extends CaseFactory {}
+
+const myRecord: Record<string, string> = {};
+myRecord["with-plugin"] = "no";
+
+new DeclarativeAgentBasicTestCase(
+  Capability.DeclarativeAgent,
+  27971545,
+  "huimaio@microsoft.com",
+  ["function"],
+  ProgrammingLanguage.JS,
+  { skipValidateAfterProvision: true },
+  myRecord
+).test();

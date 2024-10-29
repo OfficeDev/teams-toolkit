31aa443029a208e3b03960f9d0170284abfe59cf
Hui Miao <miuhui@outlook.com>
2024-10-29 11:43:10 +0800

    test: fix

--- packages/tests/src/e2e/copilotExtensions/DeclarativeAgentBasic.tests.ts
+++ packages/tests/src/e2e/copilotExtensions/DeclarativeAgentBasic.tests.ts
@@ -19,7 +19,7 @@ new DeclarativeAgentBasicTestCase(
   27971545,
   "huimaio@microsoft.com",
   ["function"],
-  ProgrammingLanguage.JS,
+  ProgrammingLanguage.Common,
   { skipValidateAfterProvision: true },
   myRecord
 ).test();

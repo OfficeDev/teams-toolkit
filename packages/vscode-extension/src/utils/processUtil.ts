2ded6e46410d27b994793385414fd708551cfb18
huajiezhang <huajiezhang@microsoft.com>
2024-10-30 17:55:21 +0800

    feat: process util

--- packages/vscode-extension/src/utils/processUtil.ts
+++ packages/vscode-extension/src/utils/processUtil.ts
@@ -1,10 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 import { exec } from "child_process";
-import detectPort from "detect-port";
 
 class ProcessUtil {
-  // 获取占用端口的进程ID (Linux/Mac)
   async getProcessId(port: number): Promise<string> {
     return new Promise((resolve, reject) => {
       const command =
@@ -15,7 +13,6 @@ class ProcessUtil {
         }
         if (stdout) {
           if (process.platform === "win32") {
-            // Windows: 从 netstat 的输出中提取PID
             const lines = stdout.split("\n");
             const pidLine = lines.find((line) => line.includes(`:${port}`));
             if (pidLine) {
@@ -25,7 +22,6 @@ class ProcessUtil {
               resolve("");
             }
           } else {
-            // Linux/Mac: 从 lsof 输出中提取PID
             const pid = stdout.split("\n")[1]?.split(/\s+/)[1];
             resolve(pid || "");
           }
@@ -36,7 +32,6 @@ class ProcessUtil {
     });
   }
 
-  // 杀死进程
   async killProcess(pid: string): Promise<void> {
     return new Promise((resolve, reject) => {
       const command = process.platform === "win32" ? `taskkill /PID ${pid} /F` : `kill -9 ${pid}`;
@@ -47,28 +42,6 @@ class ProcessUtil {
         resolve();
       });
     });
-  }
-
-  // 主逻辑：检查端口，获取进程ID并杀死进程
-  async killProcessOnPort(port: number) {
-    try {
-      const port2 = await detectPort(port);
-      if (port2 !== port) {
-        console.log(`Port ${port} is occupied.`);
-        const pid = await this.getProcessId(port);
-        if (pid) {
-          console.log(`Killing process with PID: ${pid}`);
-          await this.killProcess(pid);
-          console.log(`Process on port ${port} has been killed.`);
-        } else {
-          console.log(`No process found on port ${port}.`);
-        }
-      } else {
-        console.log(`Port ${port} is not occupied.`);
-      }
-    } catch (error: any) {
-      console.error(`Error: ${error.message as string}`);
-    }
   }
 
   async getProcessInfo(pid: number): Promise<string> {

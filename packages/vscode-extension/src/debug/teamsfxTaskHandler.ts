86ff457250a7c6de4e594722569bdbd5cab68c32
Huajie Zhang <huajiezhang@microsoft.com>
2024-11-06 10:12:47 +0800

    fix: kill process tree after debug termination (#12661)
    * fix: kill tree
    
    * fix: kill tree

--- packages/vscode-extension/src/debug/teamsfxTaskHandler.ts
+++ packages/vscode-extension/src/debug/teamsfxTaskHandler.ts
@@ -46,6 +46,7 @@ import {
 } from "./common/localDebugSession";
 import { allRunningDebugSessions } from "./officeTaskHandler";
 import { deleteAad } from "./deleteAadHelper";
+import kill from "tree-kill";
 
 class NpmInstallTaskInfo {
   private startTime: number;
@@ -532,7 +533,7 @@ export function terminateAllRunningTeamsfxTasks(): void {
   for (const task of allRunningTeamsfxTasks) {
     try {
       if (task[1] > 0) {
-        process.kill(task[1], "SIGTERM");
+        kill(task[1], "SIGTERM");
       }
     } catch (e) {
       // ignore and keep killing others

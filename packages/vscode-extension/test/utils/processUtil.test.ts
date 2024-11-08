f93b166f976893ec3608a648bb0cdd7bbc1d55d8
huajiezhang <huajiezhang@microsoft.com>
2024-11-08 12:26:16 +0800

    test: ut

--- packages/vscode-extension/test/utils/processUtil.test.ts
+++ packages/vscode-extension/test/utils/processUtil.test.ts
@@ -1,12 +1,8 @@
-// import child_process from "child_process";
 import sinon from "sinon";
 import { processUtil } from "../../src/utils/processUtil";
 
 describe("ProcessUtil", () => {
-  let execStub: any;
   const sandbox = sinon.createSandbox();
-  beforeEach(() => {
-  });
 
   afterEach(() => {
     sandbox.restore();
@@ -15,8 +11,6 @@ describe("ProcessUtil", () => {
   describe("killProcess", () => {
     it("error", async () => {
       sandbox.stub(process, "platform").value("win32");
-      const error = new Error("exec error");
-      execStub.yields(error);
       try {
         await processUtil.killProcess(-1);
         throw new Error("Expected method to reject.");

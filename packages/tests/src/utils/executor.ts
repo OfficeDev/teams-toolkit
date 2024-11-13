9802f8da3e7d57d32f5733ccbec0cea7d6186054
Siyuan Chen <67082457+ayachensiyuan@users.noreply.github.com>
2024-11-13 12:52:08 +0800

    test: e2e skip warning message (#12713)
    * test: e2e skip warning message
    
    ---------
    
    Co-authored-by: Ivan_Chen <v-ivanchen@microsoft.com>

--- packages/tests/src/utils/executor.ts
+++ packages/tests/src/utils/executor.ts
@@ -19,7 +19,6 @@ import {
 } from "child_process";
 import { expect } from "chai";
 import { Env } from "./env";
-import { on } from "events";
 
 export class Executor {
   static async execute(
@@ -45,8 +44,11 @@ export class Executor {
 
         if (result.stderr) {
           if (
-            skipErrorMessage &&
-            result.stderr.toLowerCase().includes(skipErrorMessage)
+            (skipErrorMessage &&
+              result.stderr.toLowerCase().includes(skipErrorMessage)) ||
+            result.stderr
+              .toLowerCase()
+              .includes(LocalDebugError.DeprecatedError)
           ) {
             console.log(`[Skip Warning] ${result.stderr}`);
             return { success: true, ...result };

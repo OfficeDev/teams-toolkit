ab5e450a2c12f3596a33865041ab69751717166f
Yuqi Zhou <yuqzho@microsoft.com>
2024-12-04 10:32:08 +0800

    fix: github copilot only shown if non stable release

---
+++ packages/vscode-extension/src/releaseBasedFeatureSettings.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+const shouldEnableTeamsCopilotChatUI = false;
+
+export const releaseControlledFeatureSettings = {
+  shouldEnableTeamsCopilotChatUI,
+};

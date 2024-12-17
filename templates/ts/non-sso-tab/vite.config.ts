e3886a292b20cde40ef98a37fd7b0e1a938dadb2
Yimin-Jin <yiminjin@microsoft.com>
2024-12-17 13:25:01 +0800

    refacator: change to dyn import vite

--- templates/ts/non-sso-tab/vite.config.ts
+++ templates/ts/non-sso-tab/vite.config.ts
@@ -1,23 +1,26 @@
-import { defineConfig } from "vite";
 import { resolve } from "path";
-import commonjs from "vite-plugin-commonjs";
 
-export default defineConfig({
-  plugins: [commonjs()],
-  define: {
-    global: "globalThis",
-  },
-  build: {
-    outDir: "lib/static",
-    rollupOptions: {
-      input: {
-        teamsapp: resolve(__dirname, "src/static/scripts/teamsapp.ts"),
-      },
-      output: {
-        entryFileNames: "scripts/[name].js",
-        chunkFileNames: "scripts/[name].js",
-        assetFileNames: "assets/[name].[ext]",
+async function getConfig() {
+  const { defineConfig } = await import("vite");
+
+  return defineConfig({
+    define: {
+      global: "globalThis",
+    },
+    build: {
+      outDir: "lib/static",
+      rollupOptions: {
+        input: {
+          teamsapp: resolve(__dirname, "src/static/scripts/teamsapp.ts"),
+        },
+        output: {
+          entryFileNames: "scripts/[name].js",
+          chunkFileNames: "scripts/[name].js",
+          assetFileNames: "assets/[name].[ext]",
+        },
       },
     },
-  },
-});
+  });
+}
+
+export default getConfig();
